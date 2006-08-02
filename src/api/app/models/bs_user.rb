f93d8c69898b313db18d7200f6b947b137ae191e
Klaas Freitag <freitag@suse.de>
2006-08-02 13:01:12 +0000

    removing the BsUser object again

--- src/api/app/models/bs_user.rb
+++ src/api/app/models/bs_user.rb
@@ -1,4 +1,6 @@
-class BsUser < User 
+class User < ActiveRecord::Base
+  include ActiveRbacMixins::UserMixin
+  
   has_many :watched_projects
   has_many :project_user_role_relationships
 
@@ -19,6 +21,37 @@ class BsUser < User
       password_confirmation = nil
     else 
       logger.debug "Error - skipping to create user"
+    end
+  end
+
+  # Returns true if the the state transition from "from" state to "to" state
+  # is valid. Returns false otherwise. +new_state+ must be the integer value
+  # of the state as returned by +User.states['state_name']+.
+  #
+  # Note that currently no permission checking is included here; It does not
+  # matter what permissions the currently logged in user has, only that the
+  # state transition is legal in principle.
+  def state_transition_allowed?(from, to)
+    from = from.to_i
+    to = to.to_i
+    logger.debug("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    return true if from == to # allow keeping state
+    logger.debug( "Here are we!")
+    return case from
+      when states['unconfirmed']
+        true
+      when states['confirmed']
+        (to == states['locked']) or (to == states['deleted'])
+      when states['locked']
+        (to == states['confirmed']) or (to == states['deleted'])
+      when states['deleted']
+        to == states['confirmed']
+      when states['ichainrequest']
+        (to == states['locked']) or (to == states['confirmed']) or (to == states['deleted'])
+      when 0
+        states.value?(to)
+      else
+        false
     end
   end
 
