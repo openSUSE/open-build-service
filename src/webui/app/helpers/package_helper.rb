a9ac2566aa31a65bbfe4778dc7e14a8514412f22
Andreas Bauer <abauer@suse.com>
2006-10-16 09:59:26 +0000

    replaced FRONTEND_* with hardcoded hostname in direct links to api

--- src/webui/app/helpers/package_helper.rb
+++ src/webui/app/helpers/package_helper.rb
@@ -1,13 +1,16 @@
 module PackageHelper
+  # TODO: change hardcoded url to some config variable, don't use FRONTEND_HOST
+  #       because it is used for the internal connection bypassing ichain
+  #       and is not accessible from outside
   def build_log_url( project, package, platform, arch )
-    "http://#{FRONTEND_HOST}:#{FRONTEND_PORT}/result/#{project}/#{platform}/#{package}/#{arch}/log"
+    "http://api.opensuse.org/result/#{project}/#{platform}/#{package}/#{arch}/log"
   end
 
   def file_url( project, package, filename )
-    "http://#{FRONTEND_HOST}:#{FRONTEND_PORT}/source/#{project}/#{package}/#{filename}"
+    "https://api.opensuse.org/source/#{project}/#{package}/#{filename}"
   end
 
   def rpm_url( project, package, repository, arch, filename )
-    "http://#{FRONTEND_HOST}:#{FRONTEND_PORT}/rpm/#{project}/#{repository}/#{package}/#{arch}/#{filename}"
+    "http://api.opensuse.org/rpm/#{project}/#{repository}/#{package}/#{arch}/#{filename}"
   end
 end
