942637a5b835fc3bce0ea4da57038f8fcf67fdf8
David Mayr <DMayr@suse.de>
2006-12-01 05:11:45 +0000

    added pulldown-menu for help-links to the layout (all pages)

--- src/webui/public/javascripts/application.js
+++ src/webui/public/javascripts/application.js
@@ -13,3 +13,14 @@ function toggle_display(element_id) {
   }
 }
 
+function goto_url(x) {
+  if(x == '') {
+    document.forms[0].reset();
+    document.forms[0].elements[0].blur();
+    return;
+  }
+  parent.document.location.href = x;
+  document.forms[0].reset();
+  document.forms[0].elements[0].blur();
+}
+
