5a91094bdd506644c624cb1561b1d6a759e55e35
David Mayr <DMayr@suse.de>
2006-12-01 02:43:21 +0000

    added [show/hide full description] to long descriptions in Package#show view

--- src/webui/public/javascripts/application.js
+++ src/webui/public/javascripts/application.js
@@ -1,2 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+function toggle_display(element_id) {
+  if (document.getElementById) {
+    element = document.getElementById(element_id);
+    if (element.style.display == "none") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  }
+}
+
