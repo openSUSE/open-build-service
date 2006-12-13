085d6bff9cb13b62d2273e550d3e36b10ca25d95
Thomas Scholz <th.sch@gmx.net>
2006-12-13 21:03:35 +0000

    added a Tag management (show, add, delete, remove) to the Administration Interface

--- src/api/app/controllers/admin_controller.rb
+++ src/api/app/controllers/admin_controller.rb
@@ -25,5 +25,59 @@ class AdminController < ApplicationController
   def say_hello
     render( :layout => false )
   end
+  
+  def list_tags
+    @tag_pages, @tags = paginate :tags, :per_page => 10
+  end
+  
+  def new_tag
+    Tag.new
+  end
 
+  def create_tag
+    @tag = Tag.new(params[:tag])
+    if @tag.save
+      flash[:notice] = 'Tag was successfully created.'
+      redirect_to :action => 'list_tags'
+    else
+      render :action => 'new_tag'
+    end
+  end
+
+  def show_tag
+    @tag = Tag.find(params[:id])
+    @tagged_items = @tag.db_projects
+    rescue
+      invalid_tag
+  end
+
+  def edit_tag
+    @tag = Tag.find(params[:id])
+    rescue
+    invalid_tag
+  end
+  
+  def update_tag
+    @tag = Tag.find(params[:id])
+    if @tag.update_attributes(params[:tag])
+      flash[:note] = 'Tag was successfully updated.'
+      redirect_to :action => 'show_tag', :id => @tag
+    else
+      render :action => 'edit_tag'
+    end
+  end
+  
+  def destroy_tag
+    Tag.find(params[:id]).destroy
+    redirect_to :action => 'list_tags'
+  end
+  
+  
+  def invalid_tag
+    logger.error("Attempt to access invalid tag #{params[:id]}")
+    flash[:note] = 'Invalid tag'
+    redirect_to :action => 'list_tags'
+  end
+  
+  
 end
