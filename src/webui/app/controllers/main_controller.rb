9f101b1f293f85c82e206ab358ebefa05f86d61b
David Mayr <DMayr@suse.de>
2007-01-02 04:48:21 +0000

    refined weighting of search results

--- src/webui/app/controllers/main_controller.rb
+++ src/webui/app/controllers/main_controller.rb
@@ -31,6 +31,20 @@ class MainController < ApplicationController
       @search_what = %w{ package project }
     end
 
+    weight_for = {
+      :is_a_project      => 10,
+      :name_exact_match  => 20,
+      :name_full_match   => 16,
+      :name_start_match  => 8,
+      :name_contained    => 4,
+      :title_full_match  => 8,
+      :title_start_match => 4,
+      :title_contained   => 2,
+      :description_full_match  => 4,
+      :description_start_match => 2,
+      :description_contained   => 1
+    }
+
     @results = []
     @search_what.each do |s_what|
 
@@ -57,23 +71,56 @@ class MainController < ApplicationController
         s = @search_text
         count = 0
         weight = 0
-        weight += 10 if s_what == 'project'
-        weight += 20 if data.name.to_s.downcase == @search_text.downcase
+
+        log_prefix = "weighting search result #{s_what} \"#{data}\" by"
+
+        # weight if result is a project
+        if s_what == 'project'
+          weight += weight_for[:is_a_project]
+          log_weight(log_prefix, 'is_a_project', weight)
+        end
+
+        # weight if name matches exact
+        if data.name.to_s.downcase == @search_text.downcase
+          weight += weight_for[:name_exact_match]
+          log_weight(log_prefix, 'name_exact_match', weight)
+        end
 
         # weight the name
-        weight += 9*count if count = data.name.to_s.scan(/\b#{s}\b/i).length
-        weight += 6*count if count = data.name.to_s.scan(/\b#{s}/i).length
-        weight += 4*count if count = data.name.to_s.scan(/#{s}/i).length
+        if    (match = data.name.to_s.scan(/\b#{s}\b/i)) != []
+          weight += match.length * weight_for[:name_full_match]
+          log_weight(log_prefix, 'name_full_match', weight)
+        elsif (match = data.name.to_s.scan(/\b#{s}/i)) != []
+          weight += match.length * weight_for[:name_start_match]
+          log_weight(log_prefix, 'name_start_match', weight)
+        elsif (match = data.name.to_s.scan(/#{s}/i)) != []
+          weight += match.length * weight_for[:name_contained]
+          log_weight(log_prefix, 'name_contained', weight)
+        end
 
         # weight the title
-        weight += 6*count if count = data.title.to_s.scan(/\b#{s}\b/i).length
-        weight += 4*count if count = data.title.to_s.scan(/\b#{s}/i).length
-        weight += 2*count if count = data.title.to_s.scan(/#{s}/i).length
+        if    (match = data.title.to_s.scan(/\b#{s}\b/i)) != []
+          weight += match.length * weight_for[:title_full_match]
+          log_weight(log_prefix, 'title_full_match', weight)
+        elsif (match = data.title.to_s.scan(/\b#{s}/i)) != []
+          weight += match.length * weight_for[:title_start_match]
+          log_weight(log_prefix, 'title_start_match', weight)
+        elsif (match = data.title.to_s.scan(/#{s}/i)) != []
+          weight += match.length * weight_for[:title_contained]
+          log_weight(log_prefix, 'title_contained', weight)
+        end
 
         # weight the description
-        weight += 3*count if count = data.description.to_s.scan(/\b#{s}\b/i).length
-        weight += 2*count if count = data.description.to_s.scan(/\b#{s}/i).length
-        weight += 1*count if count = data.description.to_s.scan(/#{s}/i).length
+        if    (match = data.description.to_s.scan(/\b#{s}\b/i)) != []
+          weight += match.length * weight_for[:description_full_match]
+          log_weight(log_prefix, 'description_full_match', weight)
+        elsif (match = data.description.to_s.scan(/\b#{s}/i)) != []
+          weight += match.length * weight_for[:description_start_match]
+          log_weight(log_prefix, 'description_start_match', weight)
+        elsif (match = data.description.to_s.scan(/#{s}/i)) != []
+          weight += match.length * weight_for[:description_contained]
+          log_weight(log_prefix, 'description_contained', weight)
+        end
 
         @results << { :type => s_what, :data => data, :weight => weight }
       end
@@ -82,4 +129,11 @@ class MainController < ApplicationController
     # reorder results by weight
     @results.sort! { |a,b| b[:weight] <=> a[:weight] }
   end
+
+
+  def log_weight(log_prefix, reason, new_weight)
+    logger.debug "#{log_prefix} #{reason}, new weight=#{new_weight}"
+  end
+
+
 end
