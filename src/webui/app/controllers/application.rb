f3bf7010a3c9c38475769aefbd09744dae79be1b
Andreas Bauer <abauer@suse.com>
2007-01-08 03:22:04 +0000

    allow + characters in project names

--- src/webui/app/controllers/application.rb
+++ src/webui/app/controllers/application.rb
@@ -183,7 +183,7 @@ class ApplicationController < ActionController::Base
   end
 
   def valid_project_name? name
-    name =~ /^\w[-_\w\.:]+$/
+    name =~ /^\w[-_+\w\.:]+$/
   end
 
   def valid_package_name? name
