2a841307b480e053d6393f244cbfcad30cd5cde3
Thomas Scholz <th.sch@gmx.net>
2007-01-10 22:46:38 +0000

    save the time stamp (date and time) as one value (not splitted)

--- src/api/db/migrate/008_add_time_stamp.rb
+++ src/api/db/migrate/008_add_time_stamp.rb
@@ -1,15 +1,11 @@
 class AddTimeStamp < ActiveRecord::Migration
   def self.up
-    add_column :taggings, :created_on, :date
-    add_column :taggings, :created_at, :time
-    add_column :tags, :created_on, :date
-    add_column :tags, :created_at, :time
+    add_column :taggings, :created_at, :datetime
+    add_column :tags, :created_at, :datetime
   end
 
   def self.down
-    remove_column :taggings, :created_on
     remove_column :taggings, :created_at
-    remove_column :tags, :created_on
     remove_column :tags, :created_at
   end
 end
