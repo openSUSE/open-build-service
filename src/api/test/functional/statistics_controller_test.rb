3c1f9151724c68b4344f71192b486199b8706d80
David Mayr <DMayr@suse.de>
2007-01-20 09:34:47 +0000

    - added first tests for statistics_controller - prepared templates for upcoming statistics_controller tests - updated fixtures - (updated vim project file)

--- src/api/test/functional/statistics_controller_test.rb
+++ src/api/test/functional/statistics_controller_test.rb
@@ -5,14 +5,94 @@ require 'statistics_controller'
 class StatisticsController; def rescue_action(e) raise e end; end
 
 class StatisticsControllerTest < Test::Unit::TestCase
+
+
+  fixtures :db_projects, :db_packages
+
+
   def setup
     @controller = StatisticsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+
+  def test_latest_added
+    prepare_request_with_user @request, 'tom', 'thunder'
+    get :latest_added
+    assert_response :success
+    assert_tag :tag => 'collection', :child => { :tag => 'item' }
+    assert_tag :tag => 'item', :attributes => {
+      :name => "kde4",
+      :created => "2005-05-05T05:05:05+02:00",
+      :kind => "project"
+    }
   end
+
+
+  def test_latest_updated
+    prepare_request_with_user @request, 'tom', 'thunder'
+    get :latest_updated
+    assert_response :success
+    assert_tag :tag => 'collection', :child => { :tag => 'item' }
+    assert_tag :tag => 'item', :attributes => {
+      :name => "kde4",
+      :updated => "2006-06-06T06:06:06+02:00",
+      :kind => "project"
+    }
+  end
+
+
+  def test_most_downloads
+    prepare_request_with_user @request, 'tom', 'thunder'
+    get :most_downloaded
+    assert_response :success
+    assert_tag :tag => 'collection', :child => { :tag => 'package' }
+    assert_tag :tag => 'package', :attributes => {
+      :name => "kdelibs",
+      :project => "kde4",
+      :downloads => "12345"
+    }
+  end
+
+
+  # TODO: implement test
+  def test_latest_built
+    #prepare_request_with_user @request, 'tom', 'thunder'
+    #get :latest_built
+    #assert_response :success
+    #assert_tag :tag => 'collection', :child => { :tag => 'xxxxx' }
+    #assert_tag :tag => 'package', :attributes => {
+    #  :name => "kdelibs",
+    #  :xxx => "xxx",
+    #}
+  end
+
+
+  # TODO: implement test
+  def test_most_active
+    #prepare_request_with_user @request, 'tom', 'thunder'
+    #get :most_active
+    #assert_response :success
+    #assert_tag :tag => 'collection', :child => { :tag => 'xxxxx' }
+    #assert_tag :tag => 'package', :attributes => {
+    #  :name => "kdelibs",
+    #  :xxx => "xxx",
+    #}
+  end
+
+
+  # TODO: implement test
+  def test_highest_rated
+    #prepare_request_with_user @request, 'tom', 'thunder'
+    #get :highest_rated
+    #assert_response :success
+    #assert_tag :tag => 'collection', :child => { :tag => 'xxxxx' }
+    #assert_tag :tag => 'package', :attributes => {
+    #  :name => "kdelibs",
+    #  :xxx => "xxx",
+    #}
+  end
+
+
 end
