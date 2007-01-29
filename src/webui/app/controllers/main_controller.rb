40f87ca78bd92854c0c328305d941dc68a784e14
David Mayr <DMayr@suse.de>
2007-01-29 01:47:12 +0000

    deactivated statistics on index-page for now, it's too early for them to be there

--- src/webui/app/controllers/main_controller.rb
+++ src/webui/app/controllers/main_controller.rb
@@ -6,9 +6,10 @@ class MainController < ApplicationController
 
 
   def index
-    @latest_added    = Collection.find( :statistics, :type => 'latest_added' , :limit => 10 )
-    @latest_updated  = Collection.find( :statistics, :type => 'latest_updated' , :limit => 10 )
-    @most_downloaded = Collection.find( :statistics, :type => 'most_downloaded' , :limit => 10 )
+    # deactivated statistics for now, it's too early for them to be on the index-page
+    #@latest_added    = Collection.find( :statistics, :type => 'latest_added' , :limit => 10 )
+    #@latest_updated  = Collection.find( :statistics, :type => 'latest_updated' , :limit => 10 )
+    #@most_downloaded = Collection.find( :statistics, :type => 'most_downloaded' , :limit => 10 )
   end
 
 
