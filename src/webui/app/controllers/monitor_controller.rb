447c8a34e71ca1e429af6ff63e4faa77646f94cf
Marcus Rueckert <mrueckert@suse.de>
2007-02-01 17:25:36 +0000

    - avoid warnings about resending data

--- src/webui/app/controllers/monitor_controller.rb
+++ src/webui/app/controllers/monitor_controller.rb
@@ -1,5 +1,9 @@
 class MonitorController < ApplicationController
   def index
+    if request.post? && ! params[:project].nil?
+      redirect_to :project => params[:project]
+    else
       @workerstatus = Workerstatus.find :all
+    end
   end
 end
