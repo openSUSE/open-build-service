877d0da81d943c2cbfbc5269c7594c6d16466963
David Mayr <DMayr@suse.de>
2007-02-01 17:51:50 +0000

    ajaxified filtering of build monitor projects

--- src/webui/app/controllers/monitor_controller.rb
+++ src/webui/app/controllers/monitor_controller.rb
@@ -1,9 +1,32 @@
 class MonitorController < ApplicationController
+
+
   def index
+    get_settings
     if request.post? && ! params[:project].nil?
       redirect_to :project => params[:project]
     else
       @workerstatus = Workerstatus.find :all
     end
   end
+
+
+  def filtered_list
+    get_settings
+    @workerstatus = Workerstatus.find :all
+    render :partial => 'building_table'
+  end
+
+
+  def get_settings
+    @project_filter = params[:project]
+
+    # @interval_steps must be > 0:
+    # @interval_steps * @max_color + @dead_line minutes
+    @interval_steps = 1
+    @max_color = 240
+    @time_now = Time.now
+    @dead_line = 1.hours.ago
+  end
+
 end
