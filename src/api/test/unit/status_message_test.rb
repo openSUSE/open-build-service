ed83542ab38dbb570ea727c453947a6d4129a99e
David Mayr <DMayr@suse.de>
2007-02-15 18:09:47 +0000

    - new migration for status_messages - new model for status_messages

---
+++ src/api/test/unit/status_message_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class StatusMessageTest < Test::Unit::TestCase
+  fixtures :status_messages
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
