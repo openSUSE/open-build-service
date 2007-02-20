e4a8624fdea271831a1d1905365d054d9d369ae2
David Mayr <DMayr@suse.de>
2007-02-20 11:16:43 +0000

    implementation of status_messages in webclient (in the monitor controller index page for now)

--- src/webui/app/controllers/monitor_controller.rb
+++ src/webui/app/controllers/monitor_controller.rb
@@ -7,7 +7,50 @@ class MonitorController < ApplicationController
       redirect_to :project => params[:project]
     else
       @workerstatus = Workerstatus.find :all
+      @status_messages = get_status_messages
     end
+  end
+
+
+  def add_message_form
+    render :partial => 'add_message_form'
+  end
+
+
+  def save_message
+    message = Statusmessage.new(
+      :message => params[:message],
+      :severity => params[:severity].to_i
+    )
+    begin
+      message.save
+    rescue ActiveXML::Transport::ForbiddenError
+      @denied = true
+    end
+    @status_messages = get_status_messages
+  end
+
+
+  def delete_message
+    message = Statusmessage.find( :id => params[:id] )
+    begin
+      message.delete( params[:id] )
+    rescue ActiveXML::Transport::ForbiddenError
+      @denied = true
+    end
+    @status_messages = get_status_messages
+  end
+
+
+  def show_more_messages
+    @status_messages = get_status_messages 100
+  end
+
+
+  def get_status_messages( limit=nil )
+    limit ||= params[:message_limit]
+    limit = 4 if limit.nil?
+    return Statusmessage.find :all, :limit => limit
   end
 
 
@@ -28,5 +71,6 @@ class MonitorController < ApplicationController
     @time_now = Time.now
     @dead_line = 1.hours.ago
   end
+
 
 end
---
+++ src/webui/app/models/statusmessage.rb
@@ -0,0 +1,27 @@
+require 'rexml/document'
+include REXML
+
+class Statusmessage < ActiveXML::Base
+
+
+  class << self
+
+    def make_stub( opt )
+      logger.debug "--> creating stub element for #{self.name}, arguments: #{opt.inspect}"
+      doc = Document.new
+      doc << XMLDecl.new( 1.0, 'UTF-8', 'no' )
+      doc.add_element( REXML::Element.new( 'message' ) )
+      doc.root.add_attribute( 'severity', opt[:severity] ) if opt[:severity]
+      doc.root.add_text( opt[:message] )
+      return doc
+    end
+
+  end #self
+
+
+  def delete( msg_id )
+    self.class.transport.delete( self, msg_id )
+  end
+
+
+end
