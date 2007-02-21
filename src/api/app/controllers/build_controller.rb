52e50ae9bfe7dcf05ffaf2b9fa535c1c569ad710
Andreas Bauer <abauer@suse.com>
2007-02-21 10:40:08 +0000

    added POST /build/<project> route

--- src/api/app/controllers/build_controller.rb
+++ src/api/app/controllers/build_controller.rb
@@ -1,2 +1,58 @@
 class BuildController < ApplicationController
+  def project_index
+    @path = request.path
+    unless request.query_string.empty?
+      @path += '?' + request.query_string
+    end
+
+    if request.get?
+      forward_data @path
+    elsif request.post?
+      allowed = false
+      allowed = true if permissions.global_project_create
+
+      if not allowed
+        #check if user has project modify rights
+        allowed = true if permissions.project_change? params[:project]
+      end
+
+      if not allowed and not params[:package].nil?
+        package_names = nil
+        if params[:package].kind_of? Array
+          package_names = params[:packge]
+        else
+          package_names = [params[:package]]
+        end
+
+        package_names.each do |pack_name|
+          allowed = permissions.package_change? pack_name, params[:project]
+          if not allowed
+            render_error :status => 403, :errorcode => "execute_cmd_no_permission",
+              :message => "No permission to execute command on package #{pack_name}"
+            return
+          end
+        end
+      end
+
+      if not allowed
+        render_error :status => 403, :errorcode => "execute_cmd_no_permission",
+          :message => "No permission to execute command on project #{params[:project]}"
+        return
+      end
+
+      #check for cmd parameter
+      if params[:cmd].nil?
+        render_error :status => 403, :errorcode => "missing_parameter",
+          :message => "Missing parameter 'cmd'"
+        return
+      end
+
+      forward_data @path, :method => :post
+      return
+    else
+      render_error :status => 400, :errorcode => 'illegal_request',
+        :message => "Illegal request: #{request.method.to_s.upcase} #{request.path}"
+      return
+    end
+  end
 end
