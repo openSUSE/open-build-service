e42ada2f8403fd5e27bc18844ffd39e8939622eb
Thomas Scholz <th.sch@gmx.net>
2007-05-16 13:42:16 +0000

    prefix should be test_

--- src/api/test/unit/tag_test.rb
+++ src/api/test/unit/tag_test.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 class TagTest < Test::Unit::TestCase
   fixtures :tags
 
-  def reject_tag
+  def test_reject_tag
     t = Tag.new
     t.name = 'pr0n'
     t.created_at = "2007-03-09 14:57:54"
