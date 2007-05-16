d1a9280c2ecc66bc941bc46a07cb05d23b4a9d88
Thomas Scholz <th.sch@gmx.net>
2007-05-16 13:38:24 +0000

    unit test: reject a tag

--- src/api/test/unit/tag_test.rb
+++ src/api/test/unit/tag_test.rb
@@ -3,6 +3,14 @@ require File.dirname(__FILE__) + '/../test_helper'
 class TagTest < Test::Unit::TestCase
   fixtures :tags
 
+  def reject_tag
+    t = Tag.new
+    t.name = 'pr0n'
+    t.created_at = "2007-03-09 14:57:54"
+    assert false
+  end
+
+
   # Replace this with your real tests.
   def test_truth
     assert true
