2a64c2fc97df56474df729967e259ab3c5ffff65
Andreas Bauer <abauer@suse.com>
2007-12-19 17:18:41 +0000

    made standard repos draggable

--- src/webui/app/helpers/project_helper.rb
+++ src/webui/app/helpers/project_helper.rb
@@ -75,4 +75,11 @@ module ProjectHelper
 
     button_to label, btn_to_opt
   end
+
+  def draggable_repo( label, opt={} )
+    out = String.new
+    out << "<li id='repo_#{label}' class='draggable_sub_element'>#{label}</li>"
+    out << draggable_element("repo_#{label}", :ghosting => true, :revert => true, :snap => true)
+    out
+  end
 end
