e07c5b40cfb21871c2779bc47e00d88a913f67a8
Andreas Bauer <abauer@suse.com>
2008-04-09 12:04:44 +0000

    - use /build instead of deprecated /rpm

--- src/webui/app/helpers/package_helper.rb
+++ src/webui/app/helpers/package_helper.rb
@@ -18,7 +18,7 @@ module PackageHelper
   def rpm_url( project, package, repository, arch, filename )
     #get_frontend_url_for( :controller => 'rpm' ) +
     #  "/#{project}/#{repository}/#{package}/#{arch}/#{filename}"
-    "https://api.opensuse.org/rpm/#{project}/#{repository}/#{package}/#{arch}/#{filename}"
+    "https://api.opensuse.org/build/#{project}/#{repository}/#{arch}/#{package}/#{filename}"
   end
 
   def human_readable_fsize( bytes )
