372050b945cf75ddf6bda0ce87dc319087f3bd84
Andreas Bauer <abauer@suse.com>
2008-05-21 12:34:42 +0000

    make sure map is called on an array, not on a string, to prevent splitting up at newlines (#392881)

--- src/api/app/controllers/application.rb
+++ src/api/app/controllers/application.rb
@@ -370,7 +370,7 @@ class ApplicationController < ActionController::Base
   def build_query_from_hash(hash, key_list=nil)
     key_list ||= hash.keys
     query = key_list.map do |key|
-      hash[key].map {|x| "#{key}=#{esc hash[key].to_s}"}.join("&") if hash.has_key?(key)
+      [hash[key]].flatten.map {|x| "#{key}=#{esc hash[key].to_s}"}.join("&") if hash.has_key?(key)
     end
 
     if query.empty?
