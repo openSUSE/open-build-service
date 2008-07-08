3c175ee2ea9bf26297204c25cb9dcc17b4fc7cf7
Andreas Bauer <abauer@suse.com>
2008-07-08 17:01:10 +0000

    - removed hardcoded links to api.opensuse.org - added optional EXTERNAL_FRONTEND_HOST config value for situations when FRONTEND_HOST isn't available publicly (ichain setups)

--- src/webclient/app/helpers/package_helper.rb
+++ src/webclient/app/helpers/package_helper.rb
@@ -1,29 +1,23 @@
 module PackageHelper
-
-
   def build_log_url( project, package, platform, arch )
-    #get_frontend_url_for( :controller => 'result' ) +
-    #  "/#{project}/#{platform}/#{package}/#{arch}/log"
-    "https://api.opensuse.org/result/#{project}/#{platform}/#{package}/#{arch}/log"
+    get_frontend_url_for( :controller => 'result' ) +
+      "/#{project}/#{platform}/#{package}/#{arch}/log"
   end
 
 
   def file_url( project, package, filename )
-    #get_frontend_url_for( :controller => 'source') +
-    #  "/#{project}/#{package}/#{filename}"
-    "https://api.opensuse.org/source/#{project}/#{package}/#{filename}"
+    get_frontend_url_for( :controller => 'source') +
+      "/#{project}/#{package}/#{filename}"
   end
 
 
   def rpm_url( project, package, repository, arch, filename )
-    #get_frontend_url_for( :controller => 'rpm' ) +
-    #  "/#{project}/#{repository}/#{package}/#{arch}/#{filename}"
-    "https://api.opensuse.org/build/#{project}/#{repository}/#{arch}/#{package}/#{filename}"
+    get_frontend_url_for( :controller => 'build' ) +
+      "/#{project}/#{repository}/#{arch}/#{package}/#{filename}"
   end
 
   def human_readable_fsize( bytes )
     number_to_human_size(bytes, 2)
   end
-
 end
 
