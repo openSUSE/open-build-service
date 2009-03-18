e2c6675d08e7523f6aa9a2001a0721f4c580a5c4
Susanne Oberhauser <froh@suse.de>
2009-03-18 11:54:29 +0000

    Merged revisions 6862 via svnmerge from https://forgesvn1.novell.com/svn/opensuse/trunk/buildservice
    Adjusted to match rails-2.0.* in obs-1.5, not rails-2.1.
    
    ........
      r6862 | froh | 2009-03-18 11:37:17 +0100 (Wed, 18 Mar 2009) | 4 lines
      
      - Be a tad more accepting to different rails patchlevels.
        This helps to simplify package dependencies.
    ........

--- src/webclient/config/environment.rb
+++ src/webclient/config/environment.rb
@@ -7,7 +7,7 @@
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
-RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '~> 2.0' unless defined? RAILS_GEM_VERSION
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
