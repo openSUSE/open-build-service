aa3046fe84b55ff26d40cff2dd76d2d6f2be72cf
Thomas Schmidt <tom@opensuse.org>
2009-10-15 12:23:35 +0000

    rework environment config loading

--- src/webui/config/environments/development.rb
+++ src/webui/config/environments/development.rb
@@ -1,12 +1,15 @@
 # base settings have moved to development_base.rb
 
+require 'socket'
+local_path = "#{RAILS_ROOT}/config/environments/development.#{Socket.gethostname}.rb"
+base_path = "#{RAILS_ROOT}/config/environments/development_base.rb"
+
 begin
-  require 'socket'
-  fname = "#{RAILS_ROOT}/config/environments/development.#{Socket.gethostname}.rb"
-  eval File.read(fname)
-  STDERR.puts "Using local environment #{fname}"
-rescue Object
-  fname = "#{RAILS_ROOT}/config/environments/development_base.rb"
-  eval File.read(fname)
-  STDERR.puts "Using global environment #{fname}" 
+  eval File.read(local_path)
+  STDERR.puts "Using local development environment #{local_path}"
+rescue Object => e
+  STDERR.puts "No local development environment found: #{e}"
+  STDERR.puts "Using global development environment #{base_path}"
+  eval File.read(base_path)
+  
 end
