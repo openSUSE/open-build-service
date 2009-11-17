1bf7e3617accddf943168aef9b8f2224f3e9bbf5
Thomas Schmidt <tom@opensuse.org>
2009-11-17 12:03:50 +0000

    maintenance routes

--- src/webui/app/controllers/maintenance_controller.rb
+++ src/webui/app/controllers/maintenance_controller.rb
@@ -5,14 +5,5 @@ class MaintenanceController < ApplicationController
   def index
     redirect_to :action => :released
   end
-
-  def released
-  end
-
-  def qa
-  end
-
-  def new
-  end
   
 end
