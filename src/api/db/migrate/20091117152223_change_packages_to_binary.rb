9a1ed7d97a9f9d013a552004918a8febc7a99917
Adrian Schröter <adrian@suse.de>
2009-11-18 10:06:03 +0000

    fix index of db_packages, must include projectid to make a package unique

--- src/api/db/migrate/20091117152223_change_packages_to_binary.rb
+++ src/api/db/migrate/20091117152223_change_packages_to_binary.rb
@@ -3,8 +3,8 @@ class ChangePackagesToBinary < ActiveRecord::Migration
   def self.up
         remove_index "db_packages", :name => "packages_all_index"
         change_column :db_packages, :name, :binary, :limit => 255
-        # not supported by rails add_index "db_packages", ["name"], :name => "packages_all_index", :unique => true, :limit => 255
-        execute "CREATE UNIQUE INDEX packages_all_index ON db_packages (name(255));"
+        # add_index "db_packages", ["db_project_id", "name"], :name => "packages_all_index", :unique => true
+        execute "CREATE UNIQUE INDEX packages_all_index ON db_packages (db_project_id,name(255));"
 
   end
 
