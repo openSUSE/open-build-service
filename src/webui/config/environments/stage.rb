ac2a101f697e6bdd1b9385e658c9e474157f1663
Stephan Kulow <coolo@suse.de>
2009-11-21 08:25:40 +0000

    stage is meant to be almost like production

--- src/webui/config/environments/stage.rb
+++ src/webui/config/environments/stage.rb
@@ -2,7 +2,7 @@
 
 # The production environment is meant for finished, "live" apps.
 # Code is not reloaded between requests
-config.cache_classes = false
+config.cache_classes = true
 
 # Use a different logger for distributed setups
 # config.logger        = SyslogLogger.new
