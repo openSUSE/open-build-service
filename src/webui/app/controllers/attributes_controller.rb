4e543e5bc23b00aa24d6fc7bad0ced0c7e01ec88
Thomas Schmidt <tom@opensuse.org>
2009-11-24 15:45:38 +0000

    simplyfied add_attribute

--- src/webui/app/controllers/attributes_controller.rb
+++ src/webui/app/controllers/attributes_controller.rb
@@ -1,50 +1,22 @@
 class AttributesController < ApplicationController
-  
-  def index
-    redirect_to :action => "add_attribute"
-  end
+
+  before_filter :require_project
 
   def add_attribute
-    @project = Project.find(params[:project]) 
     @package = params[:package]
-    given_attribute = params[:attribute]
-    if @project
-      if given_attribute
-        if @package
-          attributes = Attributes.find(:project => @project, :package => @package)
-        else
-          attributes = Attributes.find(:project, :project => @project)
-        end
-        attributes.each do |a|
-          if a.data.attributes["name"].to_s == given_attribute
-            @attribute = given_attribute
-            @values_split = []
-            @values = ""
-            if a.has_element? :value
-              a.each do |b|
-                @values += ","+b.to_s
-              end
-              @values = @values[1..@values.length]
-              @values_split = @values.split(",")
-            end
-          end 
-        end
+    if( attribute_name = params[:attribute] )
+      if @package
+        @attribute = Attributes.find(:project => @project, :package => @package, :attribute => attribute_name).attribute
       else
-        @attribute = ""
-        @values_split = []
-        @values = ""
+        @attribute = Attributes.find(:project, :project => @project, :attribute => attribute_name).attribute
       end
-    else
-      flash[:error] = "No project given!"
-      redirect_to :controller => "project", :action => "list_public"
     end
   end
 
   def save_attribute
-    project = Project.find(params[:project])
     package = params[:package]
     attribute = params[:attribute]
-    if (project.is_maintainer? session[:login]) && attribute 
+    if (@project.is_maintainer? session[:login]) && attribute
       data2,data = "",""
       values = params[:values].split(",")
       values.each do |a|
@@ -52,7 +24,7 @@ class AttributesController < ApplicationController
       end
       data = "<attributes><attribute name='#{attribute}'>#{data2}</attribute></attributes>"
       transport ||= ActiveXML::Config::transport_for(:project)
-      path = package ? "/source/#{project}/#{package}/_attribute" : "/source/#{project}/_attribute"
+      path = package ? "/source/#{@project}/#{package}/_attribute" : "/source/#{@project}/_attribute"
       begin
         transport.direct_http URI("https://#{path}"), :method => "POST", :data => data
         flash[:note] = "Attribute sucessfully added!" 
@@ -60,9 +32,9 @@ class AttributesController < ApplicationController
         flash[:error] = "Something went wrong! Maybe the attribute is not allowed." 
       end
       if package
-        redirect_to :controller => "package", :action => "show", :project => project, :package => package
+        redirect_to :controller => "package", :action => "show", :project => @project, :package => package
       else
-        redirect_to :controller => "project", :action => "show", :project => project
+        redirect_to :controller => "project", :action => "show", :project => @project
       end
  
     else
@@ -72,19 +44,31 @@ class AttributesController < ApplicationController
   end
 
   def delete_attribute
-    project = Project.find(params[:project])
     package = params[:package]
     attribute = params[:attribute]
-    if (project.is_maintainer? session[:login]) && attribute
+    if (@project.is_maintainer? session[:login]) && attribute
       transport ||= ActiveXML::Config::transport_for(:project)
-      path = package ? "/source/#{project}/#{package}/_attribute/#{attribute}" : "/source/#{project}/_attribute/#{attribute}"
+      path = package ? "/source/#{@project}/#{package}/_attribute/#{attribute}" : "/source/#{@project}/_attribute/#{attribute}"
       transport.direct_http URI("https://#{path}"), :method => "DELETE", :data => ""
       flash[:note] = "Attribute successfully deleted!"
     end
     if package
-      redirect_to :controller => "package", :action => "show", :project => project, :package => package
+      redirect_to :controller => "package", :action => "show", :project => @project, :package => package
     else
-      redirect_to :controller => "project", :action => "show", :project => project
+      redirect_to :controller => "project", :action => "show", :project => @project
     end
   end
+
+private
+
+  def require_project
+    begin
+      @project = Project.find( params[:project] )
+    rescue ActiveXML::Transport::NotFoundError => e
+      flash[:error] = "Project not found: #{params[:project]}"
+      redirect_to :controller => "project", :action => "list_public"
+      return
+    end
+  end
+
 end
---
+++ src/webui/app/helpers/attributes_helper.rb
@@ -0,0 +1,9 @@
+module AttributesHelper
+
+  def attribute_to_s attribute
+    attribute.each_value.collect{|a| a.to_s + ", "}.to_s.chop.chop
+  end
+
+end
+
+
