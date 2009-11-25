914e5bffc6eca6753f094dd97471e6a819c96485
Thomas Schmidt <tom@opensuse.org>
2009-11-25 16:45:53 +0000

    add error handling

--- src/webui/app/controllers/attributes_controller.rb
+++ src/webui/app/controllers/attributes_controller.rb
@@ -6,25 +6,33 @@ class AttributesController < ApplicationController
     @package = params[:package]
     if( attribute_name = params[:attribute] )
       if @package
-        @attribute = Attributes.find(:project => @project, :package => @package, :attribute => attribute_name).attribute
+        attribute = Attributes.find(:project => @project, :package => @package, :attribute => attribute_name)
       else
-        @attribute = Attributes.find(:project, :project => @project, :attribute => attribute_name).attribute
+        attribute = Attributes.find(:project, :project => @project, :attribute => attribute_name)
       end
     end
+    @attribute = attribute.attribute  if attribute && (attribute.has_element? 'attribute')
   end
 
   def save_attribute
+    valid_http_methods :post
     package = params[:package]
     attribute = params[:attribute]
     data2,data = "",""
     values = params[:values].split(",")
     values.each do |a|
-      data2 += "<value>#{a}</value>"
+      data2 += "<value>#{a.strip}</value>"
     end
     data = "<attributes><attribute name='#{attribute}'>#{data2}</attribute></attributes>"
     path = package ? "/source/#{@project}/#{package}/_attribute" : "/source/#{@project}/_attribute"
-    frontend.transport.direct_http URI("#{path}"), :method => "POST", :data => data
-    flash[:note] = "Attribute sucessfully added!"
+    begin
+      frontend.transport.direct_http URI("#{path}"), :method => "POST", :data => data
+      flash[:success] = "Attribute sucessfully added!"
+    rescue ActiveXML::Transport::Error => e
+      flash[:error] = "Saving attribute failed: #{ActiveXML::Transport.extract_error_message( e )[0]}"
+      redirect_to :action => :add_attribute, :project => @project,
+        :package => package, :attribute => attribute and return
+    end
     if package
       redirect_to :controller => "package", :action => "show", :project => @project, :package => package
     else
