fcd5594da50c8e4be229576ce661eae19f2b37ee
Stephan Kulow <coolo@suse.de>
2009-11-25 14:05:20 +0000

    let the script also harvest the devel projects

--- src/api/lib/project_status_helper.rb
+++ src/api/lib/project_status_helper.rb
@@ -4,10 +4,14 @@ class PackInfo
 	attr_accessor :version, :release
 	attr_accessor :devel_project, :devel_package
 	attr_accessor :srcmd5, :error
-	attr_reader :name
+	attr_reader :name, :project
+        attr_reader :key
+        attr_accessor :develpack
 
-	def initialize(name)
+	def initialize(projname, name)
+	        @project = projname
 		@name = name
+	        @key = projname + "/" + name
 		@failed = Hash.new
 		@last_success = Hash.new
 		@devel_project = nil
@@ -54,6 +58,7 @@ class PackInfo
 	def to_xml(options = {}) 
              xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
              xml.package(
+	       :project => project,
                :name => name,
                :version => version,
                :srcmd5 => srcmd5,
@@ -61,8 +66,10 @@ class PackInfo
                  self.fails.each do |repo,time|
                    xml.failure(:repo => repo, :time => time)
                  end
-                 if devel_project || devel_package
-                   xml.develpack(:proj => devel_project, :pack => devel_package)
+                 if develpack
+                   xml.develpack(:proj => devel_project, :pack => devel_package) do
+		      develpack.to_xml(:builder => xml)
+		   end
                  end
 		 if @error then xml.error(error) end
 		 @links.each do |proj,pack|
@@ -74,67 +81,107 @@ end
 
 class ProjectStatusHelper
 
-  def self.calc_status(dbproj, backend)
-     mypackages = Hash.new
-
-     if ! dbproj
-	puts "invalid project " + proj
-	return mypackages
-     end
-     dbproj.db_packages.each do |dbpack|
-	  name = dbpack.name
-          pack = PackInfo.new(name)
-          
-          if dbpack.develpackage
-	       pack.devel_project = dbpack.develpackage.db_project.name
-	       pack.devel_package = dbpack.develpackage.name
-          end
-          mypackages[name] = pack
-     end
-     dbproj.repositories.each do |r|
-        r.architectures.each do |arch|
-           reponame = r.name + "/" + arch.name
-           #puts 'get "build/%s/%s/%s/_jobhistory?code=lastfailures"' % [dbproj.name, r.name, arch.name]
-           d = backend.direct_http( URI('/build/%s/%s/%s/_jobhistory?code=lastfailures' % [dbproj.name, r.name, arch.name]) , :timeout => 1000 )
-           data = XML::Parser.string(d).parse
-           if data then 
-              data.find('/jobhistlist/jobhist').each do |p|
-		packname = p.attributes['package']
-                next unless mypackages.has_key?(packname)
-		code = p.attributes['code']
-		if code == "unchanged" || code == "succeeded"
-			mypackages[packname].success(reponame, Integer(p.attributes['readytime']))
-		else
-			mypackages[packname].failure(reponame, Integer(p.attributes['readytime']))
-		end
-		versrel=p.attributes['versrel'].split('-')
-		mypackages[packname].version = versrel[0..-2].join('-')
-		mypackages[packname].release = versrel[-1]
-             end
-           end
-        end
-     end 
-     #d = File.read('getprojpack.xml')
-     #puts 'get /getprojpack?project=%s&withsrcmd5=1&ignoredisable=1' % dbproj.name
-     d = backend.direct_http( URI('/getprojpack?project=%s&withsrcmd5=1&ignoredisable=1' % dbproj.name), :timeout => 1000 )
-     data = XML::Parser.string(d).parse
-     if data then data.find('/projpack/project/package').each do |p|
+  def self.update_projpack(proj, backend, mypackages)
+    puts 'get /getprojpack?project=%s&withsrcmd5=1&ignoredisable=1' % proj
+    d = backend.direct_http( URI('/getprojpack?project=%s&withsrcmd5=1&ignoredisable=1' % proj), :timeout => 1000 )
+    data = XML::Parser.string(d).parse
+    if data then data.find('/projpack/project/package').each do |p|
         packname = p.attributes['name']
-        next unless mypackages.has_key?(packname)
+	key = proj + "/" + packname
+        next unless mypackages.has_key?(key)
         if p.attributes['verifymd5']
-	    mypackages[packname].srcmd5 = p.attributes['verifymd5']
+	  mypackages[key].srcmd5 = p.attributes['verifymd5']
         else
-	    mypackages[packname].srcmd5 = p.attributes['srcmd5']
+	  mypackages[key].srcmd5 = p.attributes['srcmd5']
         end
         p.find('linked').each do |l|
-		mypackages[packname].linked(l.attributes['project'], l.attributes['package'])
+	  mypackages[key].linked(l.attributes['project'], l.attributes['package'])
         end
 	p.find('error').each do |e|
-		mypackages[packname].error = e.text
+	  mypackages[key].error = e.content
 	end
-       end
-     end
-     return mypackages
+      end
+    end
+  end
+
+  def self.update_jobhistory(dbproj, backend, mypackages)
+    dbproj.repositories.each do |r|
+      r.architectures.each do |arch|
+	reponame = r.name + "/" + arch.name
+	puts 'get "build/%s/%s/%s/_jobhistory?code=lastfailures"' % [dbproj.name, r.name, arch.name]
+	d = backend.direct_http( URI('/build/%s/%s/%s/_jobhistory?code=lastfailures' % [dbproj.name, r.name, arch.name]) , :timeout => 1000 )
+	data = XML::Parser.string(d).parse
+	if data then 
+	  data.find('/jobhistlist/jobhist').each do |p|
+	    packname = p.attributes['package']
+	    key = dbproj.name + "/" + packname
+	    next unless mypackages.has_key?(key)
+	    code = p.attributes['code']
+	    if code == "unchanged" || code == "succeeded"
+	      mypackages[key].success(reponame, Integer(p.attributes['readytime']))
+	    else
+	      mypackages[key].failure(reponame, Integer(p.attributes['readytime']))
+	    end
+	    versrel=p.attributes['versrel'].split('-')
+	    mypackages[key].version = versrel[0..-2].join('-')
+	    mypackages[key].release = versrel[-1]
+	  end
+	end
+      end
+    end 
+  end
+
+  def self.add_recursively(mypackages, projects, dbpack)
+    name = dbpack.name
+    pack = PackInfo.new(dbpack.db_project.name, name)
+    return if mypackages.has_key? pack.key
+
+    if dbpack.develpackage
+      pack.devel_project = dbpack.develpackage.db_project.name
+      pack.devel_package = dbpack.develpackage.name
+      projects[pack.devel_project] = dbpack.develpackage.db_project
+      add_recursively(mypackages, projects, dbpack.develpackage)
+    end
+    mypackages[pack.key] = pack
+  end
+
+  def self.move_devel_package(mypackages, key)
+    return unless mypackages.has_key? key
+
+    pack = mypackages[key]
+    return unless pack.devel_project
+    
+    newkey = pack.devel_project + "/" + pack.devel_package
+    return unless mypackages.has_key? newkey
+    develpack = mypackages.delete newkey
+    pack.develpack = develpack
+  end
+
+  def self.calc_status(dbproj, backend)
+    mypackages = Hash.new
+
+    if ! dbproj
+      puts "invalid project " + proj
+      return mypackages
+    end
+    projects = Hash.new
+    projects[dbproj.name] = dbproj
+    dbproj.db_packages.each do |dbpack|
+      #next unless dbpack.name =~ /^perl-Tk.*/
+      add_recursively(mypackages, projects, dbpack)
+    end
+
+    projects.each do |name,proj|
+      update_jobhistory(proj, backend, mypackages)
+      update_projpack(name, backend, mypackages)
+    end
+
+    dbproj.db_packages.each do |dbpack|
+      key = dbproj.name + "/" + dbpack.name
+      move_devel_package(mypackages, key)
+    end
+    
+    return mypackages
   end
 end
  
