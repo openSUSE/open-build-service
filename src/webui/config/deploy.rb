49c33d5ae81e7ded6fd851e69e5802ae9cad5ea0
Thomas Schmidt <tschmidt@suse.de>
2009-12-08 19:01:32 +0100

    prepare multistage, add deploy notifications

--- src/webui/config/deploy.rb
+++ src/webui/config/deploy.rb
@@ -1,3 +1,5 @@
+require 'net/smtp'
+
 set :application, "obs-webui"
 
 # git settings
@@ -8,23 +10,31 @@ set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 set :git_subdir, '/src/webui'
 
+set :deploy_notification_to, ['tschmidt@suse.de', 'coolo@suse.de']
+server "buildserviceapi.suse.de", :app, :web, :db, :primary => true
+
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/srv/www/vhosts/opensuse.org/#{application}"
+
+# set variables for different target deployments
+task :stage do
+  set :deploy_to, "/srv/www/vhosts/opensuse.org/stage/#{application}"
+end
+task :ibs do
+
+end
 
 
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 set :normalize_asset_timestamps, false
 
-
 # tasks are run with this user
 set :user, "root"
 # spinner is run with this user
 set :runner, "root"
-server "buildserviceapi.suse.de", :app, :web, :db, :primary => true
-
 
 after "deploy:update_code", "config:symlink_shared_config"
 after "deploy:symlink", "config:permissions"
@@ -32,6 +42,7 @@ after "deploy:symlink", "config:permissions"
 # workaround because we are using a subdirectory of the git repo as rails root
 before "deploy:finalize_update", "deploy:use_subdir"
 after "deploy:finalize_update", "deploy:reset_subdir"
+after "deploy:finalize_update", "deploy:notify"
 
 after :deploy, 'deploy:cleanup' # only keep 5 releases
 
@@ -82,8 +93,24 @@ namespace :deploy do
     set :latest_release, latest_release_bak
   end
 
+
+  desc "Send email notification of deployment"
+  task :notify do
+    #diff = `#{source.local.diff(current_revision)}`
+    diff_log = `#{source.local.log( source.next_revision(current_revision) )}`
+    user = `whoami`
+    body = %Q[From: obs-webui-deploy@suse.de
+To: #{deploy_notification_to.join(", ")}
+Subject: obs-webui deployed by #{user}
+
+Git log:
+#{diff_log}]
+
+    Net::SMTP.start('relay.suse.de', 25) do |smtp|
+      smtp.send_message body, 'obs-webui-deploy@suse.de', deploy_notification_to
+    end
+  end
+  
 end
-
-
 
 
