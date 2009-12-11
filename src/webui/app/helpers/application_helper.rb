9e24c521a353d098ecb6502318703469cc6254db
Pavol Rusnak <stick@gk2.sk>
2009-12-11 16:48:30 +0100

    change gravatar default to identicon (404 is not OK for all browsers)

--- src/webui/app/helpers/application_helper.rb
+++ src/webui/app/helpers/application_helper.rb
@@ -112,7 +112,7 @@ module ApplicationHelper
 
   def gravatar_image email
     hash = MD5::md5(email.downcase)
-    return image_tag "http://www.gravatar.com/avatar/#{hash}?d=404&s=20", :alt => ""
+    return image_tag "http://www.gravatar.com/avatar/#{hash}?d=identicon&s=20", :alt => ""
   end
 
 end
