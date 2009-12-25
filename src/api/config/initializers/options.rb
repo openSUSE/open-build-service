22f91050b667a1a435ccc9fe07a01eb911a5413c
Stephan Kulow <coolo@suse.de>
2009-12-25 14:53:47 +0100

    add the initializer to load the config too

---
+++ src/api/config/initializers/options.rb
@@ -0,0 +1,11 @@
+path = "#{RAILS_ROOT}/config/options.yml"
+
+begin
+  CONFIG = YAML.load_file(path)
+rescue Exception => e
+  puts "Error while parsing config file #{path}"
+  CONFIG = Hash.new
+  raise e
+end
+
+#puts "Loaded openSUSE buildservice api config from #{path}"
