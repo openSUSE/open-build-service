ff0d91fb22a26621f2a4f54d75ccafc887d83eb1
Adrian Schröter <adrian@suse.de>
2010-01-02 15:19:09 +0100

    add sparc64v, v9 and v8

---
+++ src/api/db/migrate/20100102150000_add_sparc.rb
@@ -0,0 +1,15 @@
+class AddArchs  < ActiveRecord::Migration
+
+
+  def self.up
+    Architecture.create :name => "sparcv8"
+    Architecture.create :name => "sparcv9"
+  end
+
+
+  def self.down
+    Architecture.find_by_name("sparcv8").destroy
+    Architecture.find_by_name("sparcv9").destroy
+  end
+
+end
