1acd387055455a88d6cd2e211ee761e86b8ee90b
Adrian Schröter <adrian@suse.de>
2010-01-16 16:07:14 +0100

    * Switch to rails 2.3.5 * Extendend runlevel scripts for OBS Appliance.   - Database gets automatically setup and updated   - LVM VG named "OBS" is used automatically

--- dist/webui-production.rb
+++ dist/webui-production.rb
@@ -13,15 +13,15 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors if you bad email addresses should just be ignored
 # config.action_mailer.raise_delivery_errors = false
 
-FRONTEND_HOST = "api.opensuse.org"
+FRONTEND_HOST = "localhost"
 FRONTEND_PORT = 443
 FRONTEND_PROTOCOL = 'https'
 
 # use this when the users see the api at another url (for rpm-, file-downloads)
 #EXTERNAL_FRONTEND_HOST = "api.opensuse.org"
 
-BUGZILLA_HOST = "https://bugzilla.novell.com"
-DOWNLOAD_URL = "http://download.opensuse.org/repositories"
+#BUGZILLA_HOST = "https://bugzilla.novell.com"
+DOWNLOAD_URL = "http://localhost:82"
 
 # ICHAIN_MODE can be one of  'on', 'off' or 'simulate'
 ICHAIN_MODE = "off"
--- src/api/config/environment.rb
+++ src/api/config/environment.rb
@@ -7,7 +7,7 @@
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 require "common/libxmlactivexml"
 require 'custom_logger'
