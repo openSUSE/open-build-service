ee39f3572ee79e45e18afb7b2197eddd727e65a7
Michael Schroeder <mls@suse.de>
2010-01-18 17:55:43 +0100

    - do cmd=xxx switching in dispatcher - clean up lsrev calls a bit, make lsrev die on all errors

--- src/backend/BSServer.pm
+++ src/backend/BSServer.pm
@@ -373,7 +373,7 @@ sub parse_cgi {
   #      key does not exist - multiple cgi values are not allowed
   #      key is undef - multiple cgi values are put into array
   #      key is - then value is used as separator between cgi values
-  my ($req, $multis) = @_;
+  my ($req, $multis, $singles) = @_;
 
   my $query_string = $req->{'query'};
   my %cgi;
@@ -399,12 +399,46 @@ sub parse_cgi {
           $cgi{$name} = $value;
         }
       } else {
-        push @{$cgi{$name}}, $value; # this can possibly initialize $cgi{$name}
+        push @{$cgi{$name}}, $value;
+      }
+    } elsif ($singles && $multis && !exists($singles->{$name}) && exists($multis->{'*'})) {
+      if (defined($multis->{'*'})) {
+        if (exists($cgi{$name})) {
+	  $cgi{$name} .= "$multis->{'*'}$value";
+        } else {
+          $cgi{$name} = $value;
+        }
+      } else {
+        push @{$cgi{$name}}, $value;
       }
     } else {
       die("parameter '$name' set multiple times\n") if exists $cgi{$name};
       $cgi{$name} = $value;
     }
+  }
+  return \%cgi;
+}
+
+# return only the singles from a query
+sub parse_cgi_singles {
+  my ($req) = @_;
+  my $query_string = $req->{'query'};
+  my %cgi;
+  for my $qu (split('&', $query_string)) {
+    my ($name, $value) = split('=', $qu, 2);
+    $name  =~ tr/+/ /;
+    $name  =~ s/%([a-fA-F0-9]{2})/chr(hex($1))/ge;
+    if (exists $cgi{$name}) {
+      $cgi{$name} = undef;
+      next;
+    }
+    $value = 1 unless defined $value;
+    $value =~ tr/+/ /;
+    $value =~ s/%([a-fA-F0-9]{2})/chr(hex($1))/ge;
+    $cgi{$name} = $value;
+  }
+  for (keys %cgi) {
+    delete $cgi{$_} unless defined $cgi{$_};
   }
   return \%cgi;
 }
@@ -599,13 +633,15 @@ sub compile_dispatches {
     my $p = shift @disps;
     my $f = shift @disps;
     my $needsauth;
+    my $cgisingles;
     if ($p =~ /^!([^\/\s]*)\s*(.*?)$/) {
       $needsauth = $1 || 'auth';
       $p = $2;
     }
     if ($p eq '/') {
-      push @out, '^(?:GET|HEAD|POST):/$', $f;
-      $out[-1] = [ $needsauth eq '-' ? undef : $needsauth, $out[-1] ] if $needsauth;
+      my $cpld = [ qr/^(?:GET|HEAD|POST):\/$/ ];
+      $cpld->[2] = $needsauth eq '-' ? undef : $needsauth if $needsauth;
+      push @out, $cpld, $f;
       next;
     }
     my @cgis = split(' ', $p);
@@ -637,42 +673,60 @@ sub compile_dispatches {
     $p[0] = '.*' if $p[0] eq '\\:.*';
     $p[0] = "(?:GET|HEAD|POST):$p[0]" if $p[0] !~ /:/;
     my $multis = '';
+    my $singles = '';
+    my $hasstar;
     for my $pp (@cgis) {
       my ($arg, $qual) = (0, '{1}');
       $arg = 1 if $pp =~ s/^\$//;
       $qual = $1 if $pp =~ s/([+*?])$//;
       my $var = $pp;
+      if ($var =~ /^(.*)=(.*)$/) {
+	$cgisingles ||= {};
+	$cgisingles->{$1} = $2;
+	$singles .= ', ' if $singles ne '';
+	$singles .= "'$1' => undef";
+	$known .= ", '$1'";
+	next;
+      }
       my $vartype = $var;
       ($var, $vartype) = ($1, $2) if $var =~ /^(.*):(.*)/;
       die("no verifyer for $vartype\n") unless $vartype eq '' || $verifyers->{$vartype};
       $code2 .= "die(\"parameter '$var' is missing\\n\") unless exists \$cgi->{'$var'};\n" if $qual ne '*' && $qual ne '?';
+      $hasstar = 1 if $var eq '*';
       if ($qual eq '+' || $qual eq '*') {
 	$multis .= ', ' if $multis ne '';
 	$multis .= "'$var' => undef";
         $code2 .= "\$verifyers->{'$vartype'}->(\$_) for \@{\$cgi->{'$var'} || []};\n" if $vartype ne '';
       } else {
+	$singles .= ', ' if $singles ne '';
+	$singles .= "'$var' => undef";
         $code2 .= "\$verifyers->{'$vartype'}->(\$cgi->{'$var'}) if exists \$cgi->{'$var'};\n" if $vartype ne '';
       }
       push @args, $var if $arg;
       $known .= ", '$var'";
     }
-    $code = "my \$cgi = parse_cgi(\$req, {$multis});\n$code";
-    #if ($needsauth) {
-    #  $code = "die(\"500 no authenticate method defined\\n\") unless \$conf->{'authenticate'};\nmy \@auth = \$conf->{'authenticate'}->(\$conf, \$req);\nreturn \@auth if \@auth;\n$code";
-    #}
+    if ($hasstar) {
+      $code = "my \$cgi = parse_cgi(\$req, {$multis}, {$singles});\n$code";
+    } else {
+      $code = "my \$cgi = parse_cgi(\$req, {$multis});\n$code";
+    }
     $code2 .= "push \@args, \$cgi->{'$_'};\n" for @args;
-    $code2 .= "&dispatch_checkcgi(\$cgi$known);\n";
+    $code2 .= "&dispatch_checkcgi(\$cgi$known);\n" unless $hasstar;
     if ($callfunction) {
       $code .= "$code2\$callfunction->(\$f, \$cgi, \@args);\n";
     } else {
       $code .= "$code2\$f->(\$cgi, \@args);\n";
     }
     my $np = join('/', @p);
-    push @out, "^$np\$";
-    eval "push \@out, sub {my (\$conf, \$req) = \@_;\n$code};";
-    $out[-1] = undef unless defined $f;
-    die("compile_dispatches: $@\n") if $@;
-    $out[-1] = [ $needsauth eq '-' ? undef : $needsauth, $out[-1] ] if $needsauth;
+    my $cpld = [ qr/^$np$/ ];
+    $cpld->[1] = $cgisingles if $cgisingles;
+    $cpld->[2] = $needsauth eq '-' ? undef : $needsauth if $needsauth;
+    my $fnew;
+    if ($f) {
+      eval "\$fnew = sub {my (\$conf, \$req) = \@_;\n$code};";
+      die("compile_dispatches: $@\n") if $@;
+    }
+    push @out, $cpld, $fnew;
   }
   return \@out;
 }
@@ -688,14 +742,16 @@ sub dispatch {
   # strip trailing slash
   $ppath =~ s/\/+$// if substr($ppath, -1, 1) eq '/' && $ppath !~ /^[A-Z]*:\/$/s;
   my $auth;
+  my $cgisingles;
   while (@disps) {
     my ($p, $f) = splice(@disps, 0, 2);
-    next unless $ppath =~ /$p/;
-    if (ref($f) eq 'ARRAY') {
-      $auth = $f->[0];
-      $f = $f->[1];
-      next unless $f;
+    next unless $ppath =~ /$p->[0]/;
+    if ($p->[1]) {
+      $cgisingles ||= parse_cgi_singles($req);
+      next if grep {($cgisingles->{$_} || '') ne $p->[1]->{$_}} keys %{$p->[1]};
     }
+    $auth = $p->[2] if @$p > 2;	# optional auth overwrite
+    next unless $f;
     if ($auth) {
       die("500 no authenticate method defined\n") unless $conf->{'authenticate'};
       my @r = $conf->{'authenticate'}->($conf, $req, $auth);
