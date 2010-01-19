b71724757870e68f29278a1267b763614a4b72ca
Stephan Kulow <coolo@suse.de>
2010-01-19 20:10:27 +0100

    fix more of the attributes

--- src/webui/app/models/attribute.rb
+++ src/webui/app/models/attribute.rb
@@ -14,8 +14,8 @@ class Attribute < ActiveXML::Base
     return result 
   end
 
-  def delete(attribute)
-    path = self.init_options[:package] ? "/source/#{self.init_options[:project]}/#{self.init_options[:package]}/_attribute/#{attribute}" : "/source/#{self.init_options[:project]}/_attribute/#{attribute}"
+  def delete(namespace, name)
+    path = self.init_options[:package] ? "/source/#{self.init_options[:project]}/#{self.init_options[:package]}/_attribute/#{attribute}" : "/source/#{self.init_options[:project]}/_attribute/#{namespace}:#{name}"
     begin
       frontend = ActiveXML::Config::transport_for( :package )
       frontend.direct_http URI("#{path}"), :method => "DELETE", :data => ""
@@ -28,16 +28,15 @@ class Attribute < ActiveXML::Base
 
   end
 
-  def set(namespace, attribute, values)
+  def set(namespace, name, values)
     self.each do |f|
-      if f.namespace == namespace && f.name == attribute
+      if f.namespace == namespace && f.name == name
         # delete attribute when already set
         self.delete_element(f)
       end
     end
     
-    # add the new one with values
-    new_attr = self.add_element 'attribute', 'name' => attribute, 'namespace' => namespace
+    new_attr = self.add_element 'attribute', 'name' => name, 'namespace' => namespace
     for x in values do
       value = new_attr.add_element 'value'
       value.text = x
