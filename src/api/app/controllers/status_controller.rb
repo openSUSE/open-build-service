da8045ac8dc14d530a964ba1788414447e5dc10f
Stephan Kulow <coolo@suse.de>
2010-01-25 11:14:53 +0100

    api: if there is nothing in cache, ask the backend about worker status

--- src/api/app/controllers/status_controller.rb
+++ src/api/app/controllers/status_controller.rb
@@ -77,7 +77,10 @@ class StatusController < ApplicationController
   end
 
   def workerstatus
-     send_data ( Rails.cache.read 'workerstatus')
+     data = Rails.cache.fetch('workerstatus') do
+       update_workerstatus_cache
+     end
+     send_data data
   end
 
   def history
@@ -137,7 +140,9 @@ class StatusController < ApplicationController
         line.save
       end
 
+      ret
   end
+  private :update_workerstatus_cache
 
   def project
      dbproj = DbProject.find_by_name(params[:id])
