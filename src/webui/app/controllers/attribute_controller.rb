ce48159d2012388ace7159a235a1662e25c124bd
Thomas Schmidt <tschmidt@suse.de>
2010-02-04 19:42:14 +0100

    [webui] fix editing of non OBS: attributes, added FIXME notes

--- src/webui/app/controllers/attribute_controller.rb
+++ src/webui/app/controllers/attribute_controller.rb
@@ -3,17 +3,24 @@ class AttributeController < ApplicationController
   before_filter :requires
 
   def edit
-    all_attributes = Attribute.find(:all, :namespace => "OBS")
-    @attribute_list = []  
-    return unless all_attributes
-    all_attributes.each_entry do |d|
-      @attribute_list << "OBS:#{d.name}"
-    end
-    @attributes.each_attribute do |d|
-      @attribute_list.delete(d.name)
-    end
     if params[:namespace] and params[:name]
-      @selected_attribute = "%s:%s" % [params[:namespace], params[:name]]
+      selected_attribute = @attributes.data.find_first( "attribute[@name='#{params[:name]}' and @namespace='#{params[:namespace]}']/value")
+      if selected_attribute
+        @selected_attribute_name =  "%s:%s" % [params[:namespace], params[:name]]
+        @selected_attribute_value = selected_attribute.content
+      end
+    else
+      # FIXME: why is 'add attribute' mangled in 'edit'?
+      # FIXME: load from all namespaces
+      all_attributes = Attribute.find(:all, :namespace => "OBS")
+      @attribute_list = []
+      return unless all_attributes
+      all_attributes.each_entry do |d|
+        @attribute_list << "OBS:#{d.name}"
+      end
+      @attributes.each_attribute do |d|
+        @attribute_list.delete(d.name)
+      end
     end
   end
 
