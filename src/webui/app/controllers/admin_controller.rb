adc6b6881cc015225ed832e7ac61508554f6b0fa
Adrian Schröter <adrian@suse.de>
2010-03-10 15:09:15 +0100

    simplify code as suggested by Tom

--- src/webui/app/controllers/admin_controller.rb
+++ src/webui/app/controllers/admin_controller.rb
@@ -1,19 +1,8 @@
 class AdminController < ApplicationController
 
-  skip_before_filter :require_login, :only => [:login, :do_login]
-
   def connect_instance
     # nothing to do yet, we show all existing and possible remote
     # instances here later
   end
-
-
-  def save
-   redirect_to :controller => "project", :action => :save_new, 
-               :name => params[:project], :remoteurl => params[:remoteurl],
-               :title => "Remote OBS instance for " + params[:project], 
-               :description => "This project is representing a remote build service instance."
-  end
-
 
 end
