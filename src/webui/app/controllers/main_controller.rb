6295ca5444fdc3a2b8050e03104c1715a69115f0
Thomas Schmidt <tschmidt@suse.de>
2010-03-12 15:43:07 +0100

    fix anonymous startpage

--- src/webui/app/controllers/main_controller.rb
+++ src/webui/app/controllers/main_controller.rb
@@ -3,7 +3,8 @@ class MainController < ApplicationController
   skip_before_filter :require_login, :only => [ :index ]
 
   def index
-    @user ||= Person.find :login => session[:login] 
+    
+    @user ||= Person.find :login => session[:login] if session[:login]
     #@tagcloud ||= Tagcloud.find( :alltags, :user => session[:login], :limit => "60") if session[:login]
   end
 
