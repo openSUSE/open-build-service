c08b989b1a803a6ef3c5ce1922daf9bbf36eb09f
Adrian Schröter <adrian@suse.de>
2010-03-12 10:53:43 +0100

    support listing of all roles also for package view

--- src/webui/app/models/package.rb
+++ src/webui/app/models/package.rb
@@ -387,9 +387,33 @@ class Package < ActiveXML::Base
     logger.debug "[PACKAGE-FLAGS] Update done."
   end
 
+  def maintainer
+    if has_element? "person[@role='maintainer']"
+      return person("@role='maintainer'").userid.to_s
+    else
+      return nil
+    end
+  end
+
   def bugowner
     if has_element? "person[@role='bugowner']"
       return person("@role='bugowner'").userid.to_s
+    else
+      return nil
+    end
+  end
+
+  def reviewer
+    if has_element? "person[@role='reviewer']"
+      return person("@role='reviewer'").userid.to_s
+    else
+      return nil
+    end
+  end
+
+  def downloader
+    if has_element? "person[@role='downloader']"
+      return person("@role='downloader'").userid.to_s
     else
       return nil
     end
