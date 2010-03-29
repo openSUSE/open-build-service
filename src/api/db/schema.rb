76a331d32d6a4f4f710ad11700af89c650dccc3b
Stephan Kulow <coolo@suse.de>
2010-03-29 10:02:22 +0200

    update schema version

--- src/api/db/schema.rb
+++ src/api/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100402100000) do
+ActiveRecord::Schema.define(:version => 20100327100000) do
 
   create_table "architectures", :force => true do |t|
     t.string  "name",                          :null => false
