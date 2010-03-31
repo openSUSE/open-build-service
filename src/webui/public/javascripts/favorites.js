46370a9e701e78a997579d00089a4c22369b85ae
Thomas Schmidt <tschmidt@suse.de>
2010-03-31 17:46:34 +0200

    [webui] bento-ified watchlist!

---
+++ src/webui/public/javascripts/favorites.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+var top = $('#global-navigation').height()-12;
+if ($.browser.webkit) top += 1;
+var left = $('#global-favorites').offset().left-15;
+$('#menu-favorites').offset({left:left,top:top});
+$('#menu-favorites').hide();
+
+$('#global-favorites').click(function(){
+  //alert ($('#global-navigation li.selected').name);
+  $('#global-navigation li.selected').removeClass('selected');
+  $(this).addClass('selected');
+  $("ul[id^=menu-]").each(function() { $(this).fadeOut(); } );
+  $('#menu-favorites').fadeIn();
+  return false;
+});
+
+
+});
