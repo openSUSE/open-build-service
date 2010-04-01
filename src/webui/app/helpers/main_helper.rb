f1d8e0aa73d541ee709064c69ef0f375bbe15833
Stephan Kulow <coolo@suse.de>
2010-04-01 12:37:20 +0200

    [webui] don't underline the images on the main page

--- src/webui/app/helpers/main_helper.rb
+++ src/webui/app/helpers/main_helper.rb
@@ -1,4 +1,11 @@
 module MainHelper
 
+  def proceed_link(_image, _text, link_opts)
+    out = "<li>"
+    out += link_to(image_tag(_image), link_opts)
+    out += "<br/>"
+    out += link_to("<span class='proceed_text'>#{_text}</span>", link_opts)
+    out += "</li>"
+  end
 
 end
