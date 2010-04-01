a1ce030e6631dee6d4fe95a04c61d1045c496350
Stephan Kulow <coolo@suse.de>
2010-04-01 07:59:57 +0200

    [webui] move the favorites function into application and call it from the header - and fix it

--- src/webui/public/javascripts/application.js
+++ src/webui/public/javascripts/application.js
@@ -138,3 +138,24 @@ function br_trigger_mouseout() {
   $(".build_result_trigger").mouseover(br_trigger_mouseover).mouseout(br_trigger_mouseout);
 
 }
+
+function setup_favorites() {
+  var top = $('#global-navigation').height()-12;
+  if ($.browser.webkit) top += 1;
+  if (!$('#global-favorites')) {
+     return;
+  }
+  var left = $('#global-favorites').offset().left-16;
+  $('#menu-favorites').offset({left:left,top:top});
+  $('#menu-favorites').hide();
+
+  $('#global-favorites').click(function(){
+    //alert ($('#global-navigation li.selected').name);
+    $('#global-navigation li.selected').removeClass('selected');
+    $(this).addClass('selected');
+    $("ul[id^=menu-]").each(function() { $(this).fadeOut(); } );
+    $('#menu-favorites').fadeIn();
+    return false;
+  });
+
+});
