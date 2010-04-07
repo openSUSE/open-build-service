be40c5b7db7f69017653e3ab5945ab424ebc0f75
Thomas Schmidt <tschmidt@suse.de>
2010-04-07 17:58:52 +0200

    [webui] add example production config

---
+++ src/webui/config/environments/production.rb
@@ -0,0 +1,46 @@
+# Settings specified here will take precedence over those in config/environment.rb
+
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
+config.cache_classes = true
+
+# Full error reports are disabled and caching is turned on
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
+config.action_view.cache_template_loading            = true
+
+# See everything in the log (default is :info)
+config.log_level = :debug
+
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
+
+# Use a different cache store in production
+# config.cache_store = :mem_cache_store
+config.cache_store = :compressed_mem_cache_store, 'localhost:11211', {:namespace => 'obs-webclient-stage'}
+
+
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
+
+# Enable threaded mode
+# config.threadsafe!
+
+config.action_controller.session = {
+    :prefix => "ruby_webclient_stage_session",
+    :session_key => "opensuse_webclient_stage_session",
+    :secret => "iofupo3i4u6097p09gfsnaf7g8974lh1j3khdlsufdzg9p877234"
+}
+
+FRONTEND_HOST = "api.opensuse.org"
+FRONTEND_PORT = 443
+FRONTEND_PROTOCOL = 'https'
+
+BUGZILLA_HOST = nil
+DOWNLOAD_URL = nil
+
+
+# ICHAIN_MODE can be one of  'on', 'off' or 'simulate'
+ICHAIN_MODE = "off"
+
+BASE_NAMESPACE = nil
