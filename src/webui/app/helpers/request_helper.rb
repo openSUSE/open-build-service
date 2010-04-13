327b6e2f17ab6d0bc2b39aba51083fb7aad4a1d8
Thomas Schmidt <tschmidt@suse.de>
2010-04-13 13:20:16 +0200

    [webui] support very old requests

--- src/webui/app/helpers/request_helper.rb
+++ src/webui/app/helpers/request_helper.rb
@@ -1,9 +1,13 @@
 module RequestHelper
+
   def reqtype(req)
-    type = req.action.method_missing(:type)
-    if type == "change_devel"
-      type = "chgdev"
+    if req.has_element? :action
+      type = req.action.method_missing(:type)
+    else
+      type = req.method_missing(:type)
     end
+    type = "chgdev" if type == "change_devel"
     type
   end
+
 end
