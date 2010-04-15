676f4e0ec0e05fb21dc129c4c0eafc4e09e317af
Stephan Kulow <coolo@suse.de>
2010-04-15 11:02:19 +0200

    "1213 days" is a bit hard - 40 months may be easier to grasp

--- src/webui/app/helpers/application_helper.rb
+++ src/webui/app/helpers/application_helper.rb
@@ -163,8 +163,14 @@ module ApplicationHelper
     diff = Time.now - Time.parse(time)
     return "now" if diff < 60
     return (diff/60).to_i.to_s + " min ago" if diff < 3600
-    return (diff/3600).to_i.to_s + ((diff/3600).to_i == 1 ? " hour ago" : " hours ago") if diff < 86400
-    return (diff/86400).to_i.to_s + ((diff/86400).to_i == 1 ? " day ago" : " days ago")
+    diff = Integer(diff/3600) # now hours
+    return diff.to_s + (diff == 1 ? " hour ago" : " hours ago") if diff < 24
+    diff = Intger(diff/24) # now days
+    return diff.to_s + (diff == 1 ? " day ago" : " days ago") if diff < 14
+    diff_w = Integer(diff/7) # now weeks
+    return diff_w.to_s + (diff_w == 1 ? " week ago" : " weeks ago") if diff < 50
+    diff_m = Integer(diff/30.5) # roughly months
+    return diff_m.to_s + " months ago"
   end
 
   def setup_buildresult_trigger
