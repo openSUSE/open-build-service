2fae520d1aacb4673a555a720570d32cf3bcd3b9
Thomas Schmidt <tschmidt@suse.de>
2010-04-15 12:53:28 +0200

    [webui] typo

--- src/webui/app/helpers/application_helper.rb
+++ src/webui/app/helpers/application_helper.rb
@@ -165,7 +165,7 @@ module ApplicationHelper
     return (diff/60).to_i.to_s + " min ago" if diff < 3600
     diff = Integer(diff/3600) # now hours
     return diff.to_s + (diff == 1 ? " hour ago" : " hours ago") if diff < 24
-    diff = Intger(diff/24) # now days
+    diff = Integer(diff/24) # now days
     return diff.to_s + (diff == 1 ? " day ago" : " days ago") if diff < 14
     diff_w = Integer(diff/7) # now weeks
     return diff_w.to_s + (diff_w == 1 ? " week ago" : " weeks ago") if diff < 50
