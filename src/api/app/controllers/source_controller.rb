3fcef7dc2c319ab351c302d730cf7e71ddc6cd31
Adrian Schröter <adrian@suse.de>
2010-04-20 09:47:01 +0200

    do not allow creation of packages with invalid name

--- src/api/app/controllers/source_controller.rb
+++ src/api/app/controllers/source_controller.rb
@@ -553,6 +553,12 @@ class SourceController < ApplicationController
       return
     end
 
+    unless valid_package_name? package_name
+      render_error :status => 400, :errorcode => "invalid_package_name",
+        :message => "invalid package name '#{package_name}'"
+      return
+    end
+
     if request.get?
       unless pack = pro.db_packages.find_by_name(package_name)
         render_error :status => 404, :errorcode => "unknown_package",
