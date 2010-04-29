a4b6eb28f291c5ffa5a9405ac22b14101f381983
Thomas Schmidt <tschmidt@suse.de>
2010-04-29 17:19:12 +0200

    [webui] fix frontpage if anonymous is not allowed

--- src/webui/app/controllers/main_controller.rb
+++ src/webui/app/controllers/main_controller.rb
@@ -7,22 +7,28 @@ class MainController < ApplicationController
   def index
     @user ||= Person.find :login => session[:login] if session[:login]
 
-    @workerstatus = Rails.cache.fetch('frontpage_workerstatus', :expires_in => 15.minutes) do
-      Workerstatus.find :all
+    begin
+      @workerstatus = Rails.cache.fetch('frontpage_workerstatus', :expires_in => 15.minutes) do
+        Workerstatus.find :all
+      end
+
+      @waiting_packages = 0
+      @workerstatus.each_waiting do |waiting|
+        @waiting_packages += waiting.jobs.to_i
+      end
+
+      @global_counters = Rails.cache.fetch('global_stats', :expires_in => 15.minutes) do
+        GlobalCounters.find( :all )
+      end
+
+      @latest_updates = Rails.cache.fetch('latest_updates', :expires_in => 5.minutes) do
+        LatestUpdated.find( :limit => 6 )
+      end
+
+    rescue ActiveXML::Transport::UnauthorizedError => e
+      logger.error "Could not load all frontpage data, probably due to forbidden anonymous access in the api."
     end
 
-    @waiting_packages = 0
-    @workerstatus.each_waiting do |waiting|
-      @waiting_packages += waiting.jobs.to_i
-    end
-
-    @global_counters = Rails.cache.fetch('global_stats', :expires_in => 15.minutes) do
-      GlobalCounters.find( :all )
-    end
-
-    @latest_updates = Rails.cache.fetch('latest_updates', :expires_in => 5.minutes) do
-      LatestUpdated.find( :limit => 6 )
-    end
   end
   
 end
