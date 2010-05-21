d2253704216615ee63355d4903996b6297b1478b
Adrian Schröter <adrian@suse.de>
2010-05-21 17:34:15 +0200

    show at least that there are multiple actions in a request

--- src/webui/app/helpers/request_helper.rb
+++ src/webui/app/helpers/request_helper.rb
@@ -1,12 +1,18 @@
 module RequestHelper
 
+  # for a simplified view on a request, must be used only for lists
   def reqtype(req)
     if req.has_element? :action
       type = req.action.value :type
+      if req.each_action.length > 1
+         type = "multiple"
+      end
     else
       type = req.value :type
     end
     type = "chgdev" if type == "change_devel"
+    type = "bugowner" if type == "set_bugowner"
+    type = "addrole" if type == "add_role"
     type
   end
 
