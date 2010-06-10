cada1068fb63fdf44be2e214fdbc49fce19ba4c7
Adrian Schröter <adrian@suse.de>
2010-06-10 08:19:03 +0200

    deploy 2.0 branch, not master

--- src/api/config/deploy.rb
+++ src/api/config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "obs-api"
 # git settings
 set :scm, :git
 set :repository,  "git://gitorious.org/opensuse/build-service.git"
-set :branch, "master"
+set :branch, "2.0"
 set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 set :git_subdir, '/src/api'
