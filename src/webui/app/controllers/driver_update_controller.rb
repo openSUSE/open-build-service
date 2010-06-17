2bac150d74663e5348dcb0c39bdb5e7b08e2f825
Thomas Schmidt <tschmidt@suse.de>
2010-06-17 13:54:29 +0200

    [webui] add package selection to dud wizard

--- src/webui/app/controllers/driver_update_controller.rb
+++ src/webui/app/controllers/driver_update_controller.rb
@@ -6,19 +6,20 @@ class DriverUpdateController < PackageController
 
   def create
     @repositories = @project.each_repository.map{|repo| {:project => @project.name, :repo => repo.name, :archs => repo.each_arch.map{|arch| arch.to_s} } }
-    @packages = []
+    @packages = find_cached(Package, :all, :project => @project.name, :expires_in => 30.seconds ).each_entry.map{|package| {:name => package.name}}[0..50]
   end
 
 
   def edit
     # load repos etc from services file
     @repositories = {}
-    @packages = []
+    @packages = {}
   end
 
 
   def save
-
+    flash[:warn] = "Saving of DUD kiwi configs is not yet implemented"
+    redirect_to :controller => :package, :action => :show, :project => @project, :package => @package
   end
 
 end
