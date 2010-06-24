2f65ecf940005590fb47e7a10934e7a1f82b4383
Stephan Kulow <coolo@suse.de>
2010-06-24 10:31:49 +0200

    [api] copy over the custom logger from webui

--- src/api/lib/custom_logger.rb
+++ src/api/lib/custom_logger.rb
@@ -3,7 +3,17 @@
 class NiceLogger < Logger
 
   def format_message(severity, timestamp, progname, msg)
-    "[%s|#%5d] %s\n" % [severity[0..0], $$, msg]
+    out=""
+    while msg[0] == 13 or msg[0] == 10
+      out = out.concat(msg[0])
+      msg = msg[1..-1]
+    end
+   
+    out + "[%s|#%5d] %s\n" % [severity[0..0], $$, msg]
   end
+
+  # def format_message(severity, timestamp, msg, progname)
+  #   "#{timestamp.strftime("%b %d %H:%M:%S")} #{Socket.gethostname.split('.').first} rails[#{$PID}]: #{progname.gsub(/\n/, '').lstrip}\n"
+  # end
 
 end
