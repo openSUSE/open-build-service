085cc1a16cb88b11c85dc760b3beb9def822197c
Adrian Schröter <adrian@suse.de>
2010-06-28 17:03:29 +0200

    fix testsuite check for apidoc controller

--- src/api/app/controllers/apidocs_controller.rb
+++ src/api/app/controllers/apidocs_controller.rb
@@ -1,13 +1,11 @@
 class ApidocsController < ApplicationController
 
-  @@apidocsbase = File.expand_path(APIDOCS_LOCATION)+"/"
-
   def index
     logger.debug "PATH: #{request.path}"
     if ( request.path !~ /\/$/ )
       redirect_to "/apidocs/"
     else
-      filename = @@apidocsbase + "index.html"
+      filename = File.expand_path(APIDOCS_LOCATION) + "/index.html"
       if ( !File.exist?( filename ) )
         render :text => "Unable to load API documentation source file", :layout => "rbac"
       else
@@ -19,8 +17,8 @@ class ApidocsController < ApplicationController
   def method_missing symbol, *args
     file = symbol.to_s
     if ( file =~ /\.(xml|xsd|rng)$/ )
-      if File.exist?( @@apidocsbase + file )
-        send_file( @@apidocsbase + file, :type => "text/xml",
+      if File.exist?( File.expand_path(SCHEMA_LOCATION) + "/" + file )
+        send_file( File.expand_path(SCHEMA_LOCATION) + "/" + file, :type => "text/xml",
           :disposition => "inline" )
       else
         render_error :status => 404, :errorcode => 'file_not_found', :message => 'file was not found'
