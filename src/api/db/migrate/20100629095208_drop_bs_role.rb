b9ce627a998e45a8daff8166ef73a57dc55f3888
Thomas Schmidt <tschmidt@suse.de>
2010-06-29 14:34:09 +0200

    fix migration after model was removed some days ago

--- src/api/db/migrate/20100629095208_drop_bs_role.rb
+++ src/api/db/migrate/20100629095208_drop_bs_role.rb
@@ -1,6 +1,6 @@
 class DropBsRole < ActiveRecord::Migration
   def self.up
-    drop_table "bs_roles"
+    drop_table "bs_roles" if ActiveRecord::Base.connection.tables.include?("bs_roles")
   end
 
   def self.down
