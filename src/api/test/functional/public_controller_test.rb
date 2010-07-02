87fd5932924a1e7b1cf5fc338209b4bcaa429ae3
Adrian Schröter <adrian@suse.de>
2010-07-02 07:21:17 +0200

    [api] make the public controller a bit more complete, binary result downloading tests are still missing

--- src/api/test/functional/public_controller_test.rb
+++ src/api/test/functional/public_controller_test.rb
@@ -1,17 +1,16 @@
 require File.dirname(__FILE__) + '/../test_helper'
+require 'public_controller'
 
 class PublicControllerTest < ActionController::IntegrationTest 
-
   fixtures :all
-
+  
   def setup
-    prepare_request_valid_user
     @controller = PublicController.new
     @controller.start_test_backend
 
-    Suse::Backend.delete( '/source/home:tscholz' )
     Suse::Backend.put( '/source/home:tscholz/_meta', DbProject.find_by_name('home:tscholz').to_axml)
     Suse::Backend.put( '/source/home:tscholz/TestPack/_meta', DbPackage.find_by_name('TestPack').to_axml)
+    Suse::Backend.put( '/source/home:tscholz/TestPack/_prjconf', "DUMMY")
   end
  
   def test_index
@@ -19,7 +18,25 @@ class PublicControllerTest < ActionController::IntegrationTest
     assert_response 302
   end
 
-  def test_build
+  def test_basic_read_tests
+    get "/public/source/home:tscholz"
+    assert_response :success
+    get "/public/source/home:tscholz/_meta"
+    assert_response :success
+    get "/public/source/home:tscholz/_config"
+    assert_response :success
+    get "/public/source/home:tscholz/TestPack"
+    assert_response :success
+    get "/public/source/home:tscholz/TestPack/_meta"
+    assert_response :success
+
+    get "/public/source"
+    assert_response 403
+    get "/public/source/DoesNotExist/_meta"
+    assert_response 404
+    get "/public/source/home:tscholz/DoesNotExist/_meta"
+    assert_response 404
+
     get "/public/build/home:tscholz/10.2/i586/TestPack"
     assert_response :success
   end
@@ -38,15 +55,14 @@ class PublicControllerTest < ActionController::IntegrationTest
     assert_response :success
   end
 
-  def test_binaries
-    get "/public/binary_packages/home:tscholz/TestPack"
-    assert_response :success
-  end
-
-  def test_binaries
+  def test_get_files
     get "/public/source/home:tscholz/TestPack/myfile"
     assert_response 404
     assert_match /myfile: no such file/, @response.body
+
+    get "/public/build/home:tscholz/10.2/i586/TestPack/doesnotexist"
+    assert_response 404
+    # FIXME: do a working getbinary call
   end
 
 end
