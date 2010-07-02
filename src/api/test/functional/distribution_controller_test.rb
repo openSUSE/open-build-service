4f5ec8fa6f3a9629f840cf6c4e04ecc8ba08c668
Adrian Schröter <adrian@suse.de>
2010-07-02 12:02:12 +0200

    [api] do not store empty distribution file, it breaks the webui

--- src/api/test/functional/distribution_controller_test.rb
+++ src/api/test/functional/distribution_controller_test.rb
@@ -3,45 +3,43 @@ require 'distribution_controller'
 
 class DistributionControllerTest < ActionController::IntegrationTest 
   fixtures :all
-
-  @data = '<distributions>
-             <distribution vendor="openSUSE" version="Factory" id="opensuse-Factory">
-               <name>openSUSE Factory</name>
-               <project>openSUSE:Factory</project>
-               <reponame>openSUSE_Factory</reponame>
-               <repository>snapshot</repository>
-               <icon width="8" height="8" url="https://static.opensuse.org/distributions/logos/opensuse-Factory-8.png"/>
-               <icon width="16" height="16" url="https://static.opensuse.org/distributions/logos/opensuse-Factory-16.png"/>
-               <link>http://www.opensuse.org/</link>
-             </distribution>
-           </distributions>
-           ' 
  
   def setup
   end
 
-  def test_get_list
+  def test_put_and_get_list
+    data = '<distributions>
+               <distribution vendor="openSUSE" version="Factory" id="opensuse-Factory">
+                 <name>openSUSE Factory</name>
+                 <project>openSUSE:Factory</project>
+                 <reponame>openSUSE_Factory</reponame>
+                 <repository>snapshot</repository>
+                 <icon width="8" height="8" url="https://static.opensuse.org/distributions/logos/opensuse-Factory-8.png"/>
+                 <icon width="16" height="16" url="https://static.opensuse.org/distributions/logos/opensuse-Factory-16.png"/>
+                 <link>http://www.opensuse.org/</link>
+               </distribution>
+             </distributions>
+             ' 
+
     ActionController::IntegrationTest::reset_auth
-    get "/distribution"
+    put "/distributions", data
     assert_response 401
 
     prepare_request_with_user "tom", "thunder"
-    get "/distribution"
-    assert_response :success
-  end
-  
-  def test_put_list
-    ActionController::IntegrationTest::reset_auth
-    put "/distribution", @data
-    assert_response 401
-
-    prepare_request_with_user "tom", "thunder"
-    put "/distribution", @data
+    put "/distributions", data
     assert_response 403
 
     prepare_request_with_user "king", "sunflower"
-    put "/distribution", @data
+    put "/distributions", data
     assert_response 200
+
+    ActionController::IntegrationTest::reset_auth
+    get "/distributions"
+    assert_response 401
+
+    prepare_request_with_user "tom", "thunder"
+    get "/distributions"
+    assert_response :success
   end
   
   # FIXME: write distribution schema and add a check with broken XML
