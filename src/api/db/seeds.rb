c0ea5d3e6e5c2b8b701d1d90fca4f40050da9252
Adrian Schröter <adrian@suse.de>
2010-07-07 11:57:51 +0200

    [api] use db:setup on initial setup, fix default seeds

--- src/api/db/seeds.rb
+++ src/api/db/seeds.rb
@@ -1,35 +1,48 @@
+    Architecture.create :name => "armv4l"
+    Architecture.create :name => "armv5el"
     Architecture.create :name => "armv6el"
+    Architecture.create :name => "armv7el"
     Architecture.create :name => "armv8el"
-    Architecture.create :name => "i686"
-    Architecture.create :name => "mips64"
-    Architecture.create :name => "mips32"
     Architecture.create :name => "hppa"
     Architecture.create :name => "i586"
-    Architecture.create :name => "x86_64"
-    Architecture.create :name => "sparcv8"
-    Architecture.create :name => "sparcv9"
-    Architecture.create :name => "sparcv9v"
-    Architecture.create :name => "sparc64v"
+    Architecture.create :name => "i686"
+    Architecture.create :name => "ia64"
+    Architecture.create :name => "local"
+    Architecture.create :name => "mips"
+    Architecture.create :name => "mips32"
+    Architecture.create :name => "mips64"
     Architecture.create :name => "ppc"
     Architecture.create :name => "ppc64"
     Architecture.create :name => "s390"
     Architecture.create :name => "s390x"
-    Architecture.create :name => "ia64"
-    Architecture.create :name => "mips"
-    Architecture.create :name => "armv4l"
     Architecture.create :name => "sparc"
     Architecture.create :name => "sparc64"
-    Architecture.create :name => "armv7el"
-    Architecture.create :name => 'src'
-    Architecture.create :name => 'noarch'
-    Architecture.create :name => "armv5el"
-    Architecture.create :name => "local"
-    Architecture.create :name => "mipsel"
-    Architecture.create :name => "mips64el"
-    Role.create :title => 'bugowner'
-    Role.create :title => 'reviewer'
+    Architecture.create :name => "sparc64v"
+    Architecture.create :name => "sparcv8"
+    Architecture.create :name => "sparcv9"
+    Architecture.create :name => "sparcv9v"
+    Architecture.create :name => "x86_64"
+
     Role.create :title => "Admin"
-    Role.create :title => "User"
+    Role.create :title => 'bugowner'
+    Role.create :title => 'downloader'
     Role.create :title => "maintainer"
     Role.create :title => 'reader'
-    Role.create :title => 'downloader'
+    Role.create :title => 'reviewer'
+    Role.create :title => "User"
+
+    admin  = User.create :login => 'Admin', :email => "root@localhost", :realname => "OBS Instance Superuser", :state => "2", :password => "opensuse"
+    nobody = User.create :login => "_nobody_", :email => "nobody@localhost", :realname => "Anonymous User", :state => "3", :password => "123456"
+
+    p={}
+    p[:user] = admin
+    ans=AttribNamespace.create :name => "OBS"
+    ans.attrib_namespace_modifiable_bies.create(p)
+    at=AttribType.create( :attrib_namespace => ans, :name => "VeryImportantProject", :value_count=>0 )
+    at.attrib_type_modifiable_bies.create(p)
+    at=AttribType.create( :attrib_namespace => ans, :name => "UpdateProject", :value_count=>1 )
+    at.attrib_type_modifiable_bies.create(p)
+    at=AttribType.create( :attrib_namespace => ans, :name => "Maintained", :value_count=>0 )
+    at.attrib_type_modifiable_bies.create(p)
+    at=AttribType.create( :attrib_namespace => ans, :name => "ScreenShots" )
+    at.attrib_type_modifiable_bies.create(p)
