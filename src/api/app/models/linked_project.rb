8e49fc4686e80ad1f1b8f2d31000543e77f3a473
Stephan Kulow <coolo@suse.de>
2010-07-08 15:02:06 +0200

    [api] test one more model

--- src/api/app/models/linked_project.rb
+++ src/api/app/models/linked_project.rb
@@ -3,11 +3,11 @@ class LinkedProject < ActiveRecord::Base
   belongs_to :linked_db_project, :class_name => "DbProject", :foreign_key => "linked_db_project_id"
 
   def validate_on_create
-    unless self.db_project
+    if not self.db_project
       errors.add "Can not link project to not existing project"
-    end
-
-    if LinkedProject.find(:first, :conditions => ["db_project_id = ? AND linked_db_project_id = ?", self.db_project, self.linked_db_project])
+    elsif not self.linked_db_project
+      errors.add "It must be linked to somewhere"
+    elsif LinkedProject.find(:first, :conditions => ["db_project_id = ? AND linked_db_project_id = ?", self.db_project, self.linked_db_project])
       errors.add "Already linked with that project"
     end
   end
---
+++ src/api/test/unit/linked_project_test.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class LinkedProjectTest < ActiveSupport::TestCase
+  fixtures :linked_projects, :db_projects
+
+  def test_validation
+    prj = LinkedProject.new
+    assert_equal false, prj.valid?
+    prj.db_project = DbProject.find_by_name("home:tscholz")
+    assert_equal false, prj.valid?
+    prj.linked_db_project = DbProject.find_by_name("BaseDistro2")
+    assert_equal true, prj.valid?
+    assert_equal true, prj.save
+    prj2 = prj.clone
+    # already linked
+    assert_equal false, prj2.valid?
+  end
+  
+end
