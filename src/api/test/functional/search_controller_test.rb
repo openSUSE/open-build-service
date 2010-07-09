e1ff0e74c2fd94dbfc650d9e3f5295320fa5cae1
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2010-07-09 19:41:21 +0200

    [api] Test search on HiddenProject and HiddenProject/pack with valid and invalid user.
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- src/api/test/functional/search_controller_test.rb
+++ src/api/test/functional/search_controller_test.rb
@@ -7,6 +7,13 @@ class SearchControllerTest < ActionController::IntegrationTest
   def setup
     @tom = User.find_by_login("tom")
     @tscholz = User.find_by_login("tscholz")
+
+    @controller = SourceController.new
+    @controller.start_test_backend
+
+    Suse::Backend.put( '/source/HiddenProject/_meta', DbProject.find_by_name('HiddenProject').to_axml)
+    Suse::Backend.put( '/source/HiddenProject/pack/_meta', DbPackage.find_by_project_and_name("HiddenProject", "pack").to_axml)
+    Suse::Backend.put( '/source/HiddenProject/pack/my_file', "Protected Content")
   end
 
   def test_search_unknown
@@ -77,6 +84,43 @@ class SearchControllerTest < ActionController::IntegrationTest
     assert_response 400
     assert_select "status[code] > summary", /illegal xpath attribute/
   end
+
+  # >>> Testing HiddenProject - flag "access" set to "disabled"
+  def test_search_hidden_project_with_valid_user
+    # user is maintainer, thus access to hidden project is allowed
+    prepare_request_with_user "adrian", "so_alone"
+    get "/search/project", :match => '[@name="HiddenProject"]'
+    assert_response :success
+    assert_tag :tag => 'collection', :children => { :count => 1 }
+    #<project name="HiddenProject">
+    assert_tag :child => { :tag => 'project', :attributes => { :name => 'HiddenProject'} }
+  end
+  def test_search_hidden_project_with_invalid_user
+    # user is not maintainer - project has to be invisible
+    prepare_request_with_user "tscholz", "asdfasdf"
+    get "/search/project", :match => '[@name="HiddenProject"]'
+    assert_response :success
+    assert_tag :tag => 'collection', :children => { :count => 0 }
+  end
+  # <<< Testing HiddenProject - flag "access" set to "disabled"
+
+  # >>> Testing package inside HiddenProject - flag "access" set to "disabled" in Project
+  def test_search_hidden_project_with_valid_user
+    # user is maintainer, thus access to hidden package is allowed
+    prepare_request_with_user "adrian", "so_alone"
+    get "/search/package", :match => '[@name="pack" and @project="HiddenProject"]'
+    assert_response :success
+    assert_tag :tag => 'collection', :children => { :count => 1 }
+    assert_tag :child => { :tag => 'package', :attributes => { :name => 'pack', :project => "HiddenProject"} }
+  end
+  def test_search_hidden_project_with_invalid_user
+    # user is not maintainer - package has to be invisible
+    prepare_request_with_user "tscholz", "asdfasdf"
+    get "/search/package", :match => '[@name="pack" and @project="HiddenProject"]'
+    assert_response :success
+    assert_tag :tag => 'collection', :children => { :count => 0 }
+  end
+  # <<< Testing package inside HiddenProject - flag "access" set to "disabled" in Project
 
 end
 
