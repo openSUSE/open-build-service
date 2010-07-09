0c5f09ffaf6334858f4eeecea9262179ac903216
Stephan Kulow <coolo@suse.de>
2010-07-09 12:08:17 +0200

    [api] one more unit test

--- src/api/app/models/package_group_role_relationship.rb
+++ src/api/app/models/package_group_role_relationship.rb
@@ -7,7 +7,14 @@ class PackageGroupRoleRelationship < ActiveRecord::Base
     unless self.group
       errors.add "Can not assign role to nonexistent group"
     end
+    unless self.db_package
+      errors.add "Need a package"
+    end
+    unless self.role
+      errors.add "Need a role"
+    end
 
+    return unless errors.empty?
     if PackageGroupRoleRelationship.find(:first, :conditions => ["db_package_id = ? AND role_id = ? AND bs_group_id = ?", self.db_package, self.role, self.group])
       errors.add "Group already has this role"
     end
