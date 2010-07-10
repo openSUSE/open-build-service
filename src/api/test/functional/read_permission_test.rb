656b4fc550e1bb7ae4cb6fe6face101b258b1ff3
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2010-07-10 04:15:05 +0200

    [api] Start with tests in read_permission_test.rb.
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- src/api/test/functional/read_permission_test.rb
+++ src/api/test/functional/read_permission_test.rb
@@ -5,60 +5,21 @@ class ReadPermissionTest < ActionController::IntegrationTest
 
   fixtures :all
   
+  def setup
+    @controller = SourceController.new
+    @controller.start_test_backend
+
+    Suse::Backend.put( '/source/HiddenProject/_meta', DbProject.find_by_name('HiddenProject').to_axml)
+    Suse::Backend.put( '/source/HiddenProject/pack/_meta', DbPackage.find_by_project_and_name("HiddenProject", "pack").to_axml)
+    Suse::Backend.put( '/source/HiddenProject/pack/my_file', "Protected Content")
+  end
+
   def test_basic_read_tests
     ActionController::IntegrationTest::reset_auth 
     prepare_request_with_user "tom", "thunder"
     get "/source"
     assert_response :success
-    # Do we want a check if the project is visible here ?
-
-    # Access as a maintainer to a hidden project
-# FIXME: a maintainer should always able to have read access, a write-only access makes no sense, does it ?
-#    do_read_access_all_pathes( "adrian", :success )
-# FIXME: file read access seems not to be possible atm
-#    do_read_access_all_pathes( "adrian_reader", :success )
-# FIXME: it looks like access is always possible atm
-#    do_read_access_all_pathes( "adrian_downloader", 403 )
-#    do_read_access_all_pathes( "adrian_nobody", 403 )
   end
-
-  def do_read_access_all_pathes(user, response)
-    ActionController::IntegrationTest::reset_auth 
-    prepare_request_with_user user, "so_alone" #adrian users have all the same password
-
-    get "/source/HiddenProject/_meta"
-    # comment out for debugging:
-#    print @response.body
-    assert_response response
-    get "/source/HiddenProject"
-    assert_response response
-    get "/source/HiddenProject/pack"
-    assert_response response
-    get "/source/HiddenProject/pack/_meta"
-    assert_response response
-    get "/source/HiddenProject/pack/my_file"
-    assert_response response
-
-  end
-  protected :do_read_access_all_pathes
-
-  # FIXME: to be implemented:
-  # For source access:
-  # * test operations on a project or package
-  # * test package link creation
-  # * test project link creation
-  # * test creation and "accept" of requests
-  # * test search for hidden objects
-  # * test public controller
-  # * test tag controller
-  # For binary access
-  # * test aggregate creation
-  # * test kiwi live image file creation
-  # * test kiwi product file creation
-
-  # Everything needs to be tested as user with various roles and as a group member with various roles
-
-  # the very same must be tested also for public project, but protected package
   def test_deleted_projectlist
     prepare_request_valid_user
     get "/source?deleted"
@@ -71,4 +32,60 @@ class ReadPermissionTest < ActionController::IntegrationTest
     # can't do any check on the list without also deleting projects, which is too much for this test
     assert_tag( :tag => "directory" )
   end 
+
+  # ACL
+  def do_read_access_all_pathes(user, response)
+    ActionController::IntegrationTest::reset_auth 
+    prepare_request_with_user user, "so_alone" #adrian users have all the same password
+    get "/source/HiddenProject/_meta"
+    assert_response response
+    get "/source/HiddenProject"
+    assert_response response
+    get "/source/HiddenProject/pack"
+    assert_response response
+    get "/source/HiddenProject/pack/_meta"
+    assert_response response
+    get "/source/HiddenProject/pack/my_file"
+    assert_response response
+  end
+  protected :do_read_access_all_pathes
+
+  def test_read_hidden_prj_maintainer
+    # Access as a maintainer to a hidden project
+    do_read_access_all_pathes( "adrian", :success )
+  end
+  def test_read_hidden_prj_reader
+    # Hidden project is not visible to reader
+    do_read_access_all_pathes( "adrian_reader", 404 )
+  end
+  def test_read_hidden_prj_downloader
+    # FIXME: it looks like access is always possible atm
+    do_read_access_all_pathes( "adrian_downloader", 404 )
+  end
+  def test_read_hidden_prj_nobody
+    # Hidden project not visible to external user
+    do_read_access_all_pathes( "adrian_nobody", 404 )
+  end
+  # ACL end
+
+  # FIXME: to be implemented:
+  # For source access:
+  # * test write operations on a project or package
+  # * test package link creation
+  # * test project link creation
+  # * test creation and "accept" of requests
+  # * test public controller
+  # * test tag controller
+  # For binary access
+  # * test aggregate creation
+  # * test kiwi live image file creation
+  # * test kiwi product file creation
+  # Everything needs to be tested as user with various roles and as a group member with various roles
+  # the very same must be tested also for public project, but protected package
+
+
+  # Done
+  # * test search for hidden objects - in search controller test
+  # * test read operations on a project or package
+
 end
