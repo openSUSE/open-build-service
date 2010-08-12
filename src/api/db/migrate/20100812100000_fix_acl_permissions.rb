065a86b689512ff01b0b38b3c8b1e8479172a75c
Martin Mohring <martinmohring@linuxfoundation.org>
2010-08-12 20:59:33 +0200

    [api] fixed: comment out currently the permissions fix. seems not to work like this anymore, due to type migrations

--- src/api/db/migrate/20100812100000_fix_acl_permissions.rb
+++ src/api/db/migrate/20100812100000_fix_acl_permissions.rb
@@ -4,20 +4,16 @@ class FixAclPermissions < ActiveRecord::Migration
     maintainer = Role.find_by_title 'maintainer'
     downloader = Role.find_by_title 'downloader'
 
-    sourceperm = StaticPermission.find_by_title('source_access')
-    [maintainer,reader].each do |role|
-      role.static_permissions << sourceperm
-    end
-
-    privperm = StaticPermission.find_by_title('private_view')
-    [maintainer,reader,downloader].each do |role|
-      role.static_permissions << privperm
-    end
-
+    sourceperm   = StaticPermission.find_by_title('source_access')
+    privperm     = StaticPermission.find_by_title('private_view')
     downloadperm = StaticPermission.find_by_title('download_binaries')
-    [maintainer,downloader].each do |role|
-      role.static_permissions << downloadperm
-    end
+    accessperm   = StaticPermission.find_by_title('access')
+
+    #reader.static_permissions     << [sourceperm, privperm]
+    #downloader.static_permissions << [downloadperm, privperm]
+    #maintainer.static_permissions << [accessperm, downloadperm, sourceperm, privperm]
+    #maintainer.static_permissions << [accessperm, downloadperm, privperm]
+
   end
 
   def self.down
