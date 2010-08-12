e1f4f629a9bd3f7450cd93a7ee380076f97f074c
Adrian Schröter <adrian@suse.de>
2010-08-12 13:45:14 +0200

    [api] fix linked project indexing for remote projects

--- src/api/app/models/linked_project.rb
+++ src/api/app/models/linked_project.rb
@@ -5,8 +5,10 @@ class LinkedProject < ActiveRecord::Base
   def validate_on_create
     if not self.db_project
       errors.add "Can not link project to not existing project"
-    elsif not self.linked_db_project
+    elsif not self.linked_db_project and not self.linked_remote_project_name
       errors.add "It must be linked to somewhere"
+    elsif self.linked_db_project and self.linked_remote_project_name
+      errors.add "Can't have a local and a remote link"
     elsif LinkedProject.find(:first, :conditions => ["db_project_id = ? AND linked_db_project_id = ?", self.db_project, self.linked_db_project])
       errors.add "Already linked with that project"
     end
