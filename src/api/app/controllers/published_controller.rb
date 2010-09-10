59596564ce67d17f1fa7392650739b08ba4af4fd
Martin Mohring <martinmohring@linuxfoundation.org>
2010-09-10 22:50:05 +0200

    [api] marked published controller to have calls not instrumented with ACL

--- src/api/app/controllers/published_controller.rb
+++ src/api/app/controllers/published_controller.rb
@@ -1,6 +1,8 @@
 class PublishedController < ApplicationController
   def binary
     valid_http_methods :get, :post
+
+    # ACL(binary) TODO: this is an uninstrumented call
     pass_to_backend
   end
 end
