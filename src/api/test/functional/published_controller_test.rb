d872d6a8bc426be12e0015e7e9a47ccc0611fc47
Martin Mohring <martinmohring@linuxfoundation.org>
2010-09-12 15:55:46 +0200

    [testsuite] check also all the 401 errors in published controller

--- src/api/test/functional/published_controller_test.rb
+++ src/api/test/functional/published_controller_test.rb
@@ -12,6 +12,18 @@ class PublishedControllerTest < ActionController::IntegrationTest
     get "/published"
     assert_response 401
 
+    get "/published/HiddenProject"
+    assert_response 401
+
+    get "/published/kde4"
+    assert_response 401
+
+    get "/published/kde4/openSUSE_11.3"
+    assert_response 401
+
+    get "/published/kde4/openSUSE_11.3/i586"
+    assert_response 401
+
     prepare_request_with_user "tom", "thunder"
     get "/published"
     assert_response :success
