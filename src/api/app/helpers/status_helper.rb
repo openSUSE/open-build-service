5adf62ae1716485327694224ebe9dcc021773452
Stephan Kulow <coolo@suse.de>
2010-09-28 11:51:54 +0200

    [api] work on getting status_history compact

--- src/api/app/helpers/status_helper.rb
+++ src/api/app/helpers/status_helper.rb
@@ -1,2 +1,35 @@
 module StatusHelper
+
+  def self.resample(values, samples = 400)
+    values.sort! {|a,b| a[0] <=> b[0]}
+
+    result = Array.new
+    
+    lastvalue = 0
+    now = values[0][0]
+    samplerate = (values[-1][0] - now) / samples
+
+    index = 0
+    
+    1.upto(samples) do |i|
+      value = 0.0
+      count = 0
+      while index < values.length && values[index][0] < now + samplerate
+	value += values[index][1]
+	index += 1
+	count += 1
+      end
+      if count > 0
+	value = value / count
+      else
+	value = lastvalue
+      end
+      result << [now + samplerate / 2, value]
+      now += samplerate
+      lastvalue = value
+    end
+    
+    return result
+  end
+
 end
