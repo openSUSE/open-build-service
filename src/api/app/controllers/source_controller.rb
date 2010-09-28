b0f9510fd7e12776162304aef6939616360cf7bd
Adrian Schröter <adrian@suse.de>
2010-09-28 17:08:24 +0200

    [api] allow package copy without opackage parameter, but do not create a new package then

--- src/api/app/controllers/source_controller.rb
+++ src/api/app/controllers/source_controller.rb
@@ -1732,18 +1732,12 @@ class SourceController < ApplicationController
     valid_http_methods :post
     params[:user] = @http_user.login
 
-    unless params[:oproject] and params[:opackage]
-        render_error :status => 404, :errorcode => 'unknown_package',
-          :message => "Not specified origin package via oproject and opackage parameter"
-        return
-    end
-
     tprj = DbProject.find_by_name(params[:project])
     tpkg = tprj.find_package(params[:package]) if tprj
 
     # create target package, if it does not exist
     tpkg = DbPackage.find_by_project_and_name(params[:project], params[:package])
-    if tpkg.nil?
+    if tpkg.nil? and params[:oproject] and params[:opackage]
       answer = Suse::Backend.get("/source/#{CGI.escape(params[:oproject])}/#{CGI.escape(params[:opackage])}/_meta")
       if answer
         p = Package.new(answer.body, :project => params[:project])
