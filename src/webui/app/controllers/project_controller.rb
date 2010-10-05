300086fadeeac44fbe83e51e1fbc682eed7390b5
Stephan Kulow <coolo@suse.de>
2010-10-05 15:48:39 +0200

    [webui] fix boolean logic for limit_to_old

--- src/webui/app/controllers/project_controller.rb
+++ src/webui/app/controllers/project_controller.rb
@@ -947,7 +947,7 @@ class ProjectController < ApplicationController
     @current_develproject = params[:filter_devel] || all_packages
     @ignore_pending = params[:ignore_pending] || false
     @limit_to_fails = !(!params[:limit_to_fails].nil? && params[:limit_to_fails] == 'false')
-    @limit_to_old = !(!params[:limit_to_old].nil? && params[:limit_to_old] == 'false')
+    @limit_to_old = !(params[:limit_to_old].nil? || params[:limit_to_old] == 'false')
     @include_versions = !(!params[:include_versions].nil? && params[:include_versions] == 'false')
     
     attributes = find_cached(PackageAttribute, :namespace => 'OBS',
