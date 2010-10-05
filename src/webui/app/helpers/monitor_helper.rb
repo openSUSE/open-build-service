7ba265076b76ade3b500d2f907124517bc6920bf
Stephan Kulow <coolo@suse.de>
2010-10-05 14:53:31 +0200

    [webui] avoid crash on empty history

--- src/webui/app/helpers/monitor_helper.rb
+++ src/webui/app/helpers/monitor_helper.rb
@@ -1,6 +1,8 @@
 module MonitorHelper
 
   def self.print_statistics_array array
+    # safe guard
+    array = [] unless array
     "[" + array.map { |time,value| "[#{time * 1000}, #{value}]"}.join(',') + "]"
   end
 
