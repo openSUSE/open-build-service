21a5f72e58694e91ef8ccabd0d6822994cab6707
Sascha Peilicke <saschpe@suse.de>
2010-11-26 08:52:35 +0100

    [webui] New request controller test case (stub).

---
+++ src/webui/test/functional/request_controller_test.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class RequestControllerTest < ActionController::IntegrationTest
+
+  def setup
+    login_tom
+  end
+
+  def test_request
+    get "/requests"
+    assert_response 404
+  end
+
+  def teardown
+    logout
+  end
+ 
+end
