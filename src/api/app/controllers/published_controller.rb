42bc838d32de45386940ad3aa7166cac7c0d5f44
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2010-11-27 17:32:48 +0100

    [api] ACL2.2: Rewrite of 'access' code. Migrate published_controller to new mechanism.
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

--- src/api/app/controllers/published_controller.rb
+++ src/api/app/controllers/published_controller.rb
@@ -9,13 +9,15 @@ class PublishedController < ApplicationController
     valid_http_methods :get, :post
 
     prj = DbProject.find_by_name(params[:project]) if params[:project]
-    if prj
-      # ACL(index): project link to project with access behaves like target project not existing
-      if prj.disabled_for?('access', params[:repository], params[:arch]) and not @http_user.can_access?(prj)
+    # ACL(index): prj = nil in case of hidden project
+    if params[:project] and prj.nil?
         render_error :status => 404, :errorcode => 'not_found',
         :message => "The link target project #{params[:project]} does not exist"
         return
-      end
+    end
+
+    # ACL(index): prj = nil in case of hidden project 
+    if prj
       if request.post?
         # ACL(index): binarydownload denies access to build files
         if prj.disabled_for?('binarydownload', params[:repository], params[:arch]) and not @http_user.can_download_binaries?(prj)
@@ -31,13 +33,6 @@ class PublishedController < ApplicationController
     answer = Suse::Backend.get(request.path)
     data=REXML::Document.new(answer.body.to_s)
     if answer
-        # ACL(index): projects with flag 'access' are not listed
-        accessprjs = DbProject.find( :all, :joins => "LEFT OUTER JOIN flags f ON f.db_project_id = db_projects.id", :conditions => [ "f.flag = 'access'", "ISNULL(f.repo)", "ISNULL(f.architecture_id)"] )
-        data.elements.each("directory/entry") do |e|
-          project_name = e.attributes["name"]
-          prj = DbProject.find_by_name(project_name)
-          e.remove if accessprjs.include?(prj) and not @http_user.can_access?(prj)
-        end
       render :text => data.to_s, :content_type => "text/xml"
     end
   end
