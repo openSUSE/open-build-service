13f13176336cddc265cd6bd27bd7c6855f197e65
Stephan Kulow <coolo@suse.de>
2011-01-04 15:18:31 +0100

    [webui] make project status really html safe for even empty strings

--- src/webui/app/helpers/project_helper.rb
+++ src/webui/app/helpers/project_helper.rb
@@ -11,28 +11,28 @@ module ProjectHelper
   end
 
   def show_status_comment( comment, package, firstfail, comments_to_clear )
-    status_comment_html = ""
+    status_comment_html = "".html_safe
     if comment
       status_comment_html = ERB::Util::h(comment)
       if !firstfail
         if @project.can_edit?( session[:login] )
-          status_comment_html += " " + link_to_remote( image_tag('icons/comment_delete.png', :size => "16x16", :alt => 'Clear'), :update => "comment_#{package.gsub(':', '-')}",
+          status_comment_html += " ".html_safe + link_to_remote( image_tag('icons/comment_delete.png', :size => "16x16", :alt => 'Clear'), :update => "comment_#{package.gsub(':', '-')}",
           :url => { :action => :clear_failed_comment, :project => @project, :package => package })
           comments_to_clear << package
         end
       elsif @project.can_edit?( session[:login] )
-        status_comment_html += " "
+        status_comment_html += " ".html_safe
         status_comment_html += link_to_remote image_tag('icons/comment_edit.png', :alt => "Edit"), :update => "comment_edit_#{package.gsub(':', '-')}",
           :url => { :action => "edit_comment_form", :comment=> ERB::Util::h(comment), :package => package, :project => @project }
       end 
     elsif firstfail
       if @project.can_edit?( session[:login] )
-        status_comment_html += " <span class='unknown_failure'>Unknown build failure " + link_to_remote( image_tag('icons/comment_edit.png', :size => "16x16", :alt => "Edit"),
+        status_comment_html += " <span class='unknown_failure'>Unknown build failure ".html_safe + link_to_remote( image_tag('icons/comment_edit.png', :size => "16x16", :alt => "Edit"),
           :update => valid_xml_id("comment_edit_#{package}"),
           :url => { :action => "edit_comment_form", :comment=> "", :package => package, :project => @project } )
-        status_comment_html += "</span>"
+        status_comment_html += "</span>".html_safe
       else
-        status_comment_html += "<span class='unknown_failure'>Unknown build failure</span>"
+        status_comment_html += "<span class='unknown_failure'>Unknown build failure</span>".html_safe
       end
     end
     status_comment_html += "<span id='".html_safe + valid_xml_id("comment_edit_#{package}") + "'></span>".html_safe
