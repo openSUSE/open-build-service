4380ce472254ae945a2e7a5103033cb17c4df256
Adrian Schröter <adrian@suse.de>
2011-03-07 14:48:25 +0100

    [api] fix and test history access of deleted packages.

--- src/api/app/controllers/source_controller.rb
+++ src/api/app/controllers/source_controller.rb
@@ -950,7 +950,7 @@ class SourceController < ApplicationController
       if pack # local package
         path = "/source/#{URI.escape(pack.db_project.name)}/#{URI.escape(pack.name)}/#{URI.escape(file)}"
       end
-      path += build_query_from_hash(params, [:rev, :meta])
+      path += build_query_from_hash(params, [:rev, :meta, :deleted])
       pass_to_backend path
       return
     end
