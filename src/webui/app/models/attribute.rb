openSUSE/open-build-service

commit b71724757870e68f29278a1267b763614a4b72ca

fix more of the attributes

src/webui/app/models/attribute.rb
@@ -14,8 +14,8 @@ class Attribute < ActiveXML::Base
     return result 
   end
 
-  def delete(attribute)
-    path = self.init_options[:package] ? "/source/#{self.init_options[:project]}/#{self.init_options[:package]}/_attribute/#{attribute}" : "/source/#{self.init_options[:project]}/_attribute/#{attribute}"
+  def delete(namespace, name)
+    path = self.init_options[:package] ? "/source/#{self.init_options[:project]}/#{self.init_options[:package]}/_attribute/#{attribute}" : "/source/#{self.init_options[:project]}/_attribute/#{namespace}:#{name}"
     begin
       frontend = ActiveXML::Config::transport_for( :package )
       frontend.direct_http URI("#{path}"), :method => "DELETE", :data => ""
@@ -28,16 +28,15 @@ class Attribute < ActiveXML::Base
 
   end
 
-  def set(namespace, attribute, values)
+  def set(namespace, name, values)
     self.each do |f|
-      if f.namespace == namespace && f.name == attribute
+      if f.namespace == namespace && f.name == name
         # delete attribute when already set
         self.delete_element(f)
       end
     end
     
-    # add the new one with values
-    new_attr = self.add_element 'attribute', 'name' => attribute, 'namespace' => namespace
+    new_attr = self.add_element 'attribute', 'name' => name, 'namespace' => namespace
     for x in values do
       value = new_attr.add_element 'value'
       value.text = x

src/webui/app/controllers/attribute_controller.rb
@@ -9,15 +9,18 @@ class AttributeController < ApplicationController
     all_attributes.each_entry do |d|
       @attribute_list << "OBS:#{d.name}"
     end
-    @attributes.each do |d|
+    @attributes.each_attribute do |d|
       @attribute_list.delete(d.name)
     end
-    @selected_attribute = params[:attribute]
+    if params[:namespace] and params[:name]
+      @selected_attribute = "%s:%s" % [params[:namespace], params[:name]]
+    end
   end
 
   def save
     values = params[:values].split(',')
-    @attributes.set("OBS", params[:attribute], values)
+    namespace, name = params[:attribute].split /:/
+    @attributes.set(namespace, name, values)
     result = @attributes.save
     opt = {:controller => "attribute", :action => "show", :project => @project.name }
     opt.store( :package, params[:package] ) if params[:package]
@@ -26,7 +29,7 @@ class AttributeController < ApplicationController
   end
 
   def delete
-    result = @attributes.delete(params[:attribute])
+    result = @attributes.delete(params[:namespace], params[:name])
     flash[result[:type]] = result[:msg]
     opt = {:controller => "attribute", :action => "show", :project => @project.name }
     opt.store( :package, params[:package] ) if params[:package]

src/webui/app/views/attribute/_attributes.rhtml
@@ -30,8 +30,8 @@
         <%   end %>
       </td>
       <td>
-        <%=  link_to image_tag('edit.png', :alt => "Edit values", :title => "Edit values"), :controller => "attribute", :action => "edit", :project => @project.name, :package => @package, :attribute => a.name %>
-        <%=  link_to image_tag('list-remove.png', :alt => "Delete attribute", :title => "Delete attribute"), :controller => "attribute", :action => "delete", :project => @project.name, :package => @package, :attribute => a.name %>
+        <%=  link_to image_tag('edit.png', :alt => "Edit values", :title => "Edit values"), :controller => "attribute", :action => "edit", :project => @project.name, :package => @package, :namespace => a.namespace, :name => a.name %>
+        <%=  link_to image_tag('list-remove.png', :alt => "Delete attribute", :title => "Delete attribute"), :controller => "attribute", :action => "delete", :project => @project.name, :package => @package, :namespace => a.namespace, :name => a.name %>
       </td>
       </tr>
       <% i+=1 %>

commit dc2415d211663a515c7a041dbd5c6b1507443b0a

[webui] Fix attribute removal for packages, broke with b7172475.

src/webui/app/models/attribute.rb
@@ -24,7 +24,13 @@ class Attribute < ActiveXML::Base
   end
 
   def delete(namespace, name)
-    path = self.init_options[:package] ? "/source/#{self.init_options[:project]}/#{self.init_options[:package]}/_attribute/#{attribute}" : "/source/#{self.init_options[:project]}/_attribute/#{namespace}:#{name}"
+    package = self.init_options[:package]
+    if package
+      path = "/source/#{self.init_options[:project]}/#{package}/_attribute/#{namespace}:#{name}"
+    else
+      path = "/source/#{self.init_options[:project]}/_attribute/#{namespace}:#{name}"
+    end
+
     begin
       frontend = ActiveXML::Config::transport_for( :package )
       frontend.direct_http URI("#{path}"), :method => "DELETE", :data => ""