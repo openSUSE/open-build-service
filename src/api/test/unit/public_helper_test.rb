337c3e3e3c06131644673b50afae876307f29f6c
Sascha Peilicke <saschpe@suse.de>
2011-04-11 14:47:48 +0200

    [api] Add a (trivial) test-case for public helper.

---
+++ src/api/test/unit/public_helper_test.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+include PublicHelper
+
+PublicHelper::DOWNLOAD_URL = "http://example.com/download"
+PublicHelper::YMP_URL = "http://example.com/ymp"
+
+class PublicHelperTest < ActiveSupport::TestCase
+  def test_download_url
+    assert_equal "#{PublicHelper::DOWNLOAD_URL}/foo", download_url("foo")
+    assert_equal "#{PublicHelper::DOWNLOAD_URL}/", download_url("")
+  end
+
+  def test_ymp_url
+    assert_equal "#{PublicHelper::YMP_URL}/foo", ymp_url("foo")
+    assert_equal "#{PublicHelper::YMP_URL}/", ymp_url("")
+  end
+end
+
