621d7c73a0a1b89eaf43be64a741f5ced77bc23b
Adrian Schröter <adrian@suse.de>
2011-04-14 12:05:54 +0200

    [webui] limit request search for user in review elements

--- src/webui/app/models/person.rb
+++ src/webui/app/models/person.rb
@@ -90,7 +90,7 @@ class Person < ActiveXML::Base
     Rails.cache.delete cachekey unless opts[:cache]
 
     return Rails.cache.fetch(cachekey, :expires_in => 10.minutes) {
-        BsRequest.list({:state => 'review', :reviewstate => 'new', :user => login.to_s}) + BsRequest.list({:state => 'new', :user => login.to_s}) 
+        BsRequest.list({:state => 'review', :reviewstate => 'new', :reviewuser => login.to_s}) + BsRequest.list({:state => 'new', :user => login.to_s}) 
       }
   end
 
