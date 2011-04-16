4338ba60d0120b0cc6378693ea4e270441e638ee
Martin Mohring <martin.mohring@5edatasoft.com>
2011-04-17 01:04:26 +0200

    [webui] plot idle and busy workers on frontpage

--- src/webui/app/controllers/main_controller.rb
+++ src/webui/app/controllers/main_controller.rb
@@ -42,6 +42,7 @@ class MainController < ApplicationController
           end
         end
       end
+      @idle = MonitorController.addarrays (@busy, @idle)
 
       @global_counters = Rails.cache.fetch('global_stats', :expires_in => 15.minutes, :shared => true) do
         GlobalCounters.find( :all )
