a2a66c6fe341d247374fd6f922d958220d0b7288
Adrian Schröter <adrian@suse.de>
2011-04-28 11:29:15 +0200

    [api] fix access to deleted package sources which still exists in a linked project

--- src/api/app/controllers/source_controller.rb
+++ src/api/app/controllers/source_controller.rb
@@ -342,33 +342,7 @@ class SourceController < ApplicationController
 
     # check read access rights when the package does not exist anymore
     if tpkg.nil? and deleted_package
-      raise DbProject::ReadAccessError, "#{target_project_name}" if tprj.nil? or tprj.disabled_for? 'access', nil, nil
-      raise DbPackage::ReadSourceAccessError, "#{target_project_name}/#{target_package_name}" if tprj.disabled_for? 'sourceaccess', nil, nil
-
-      unless tpkg
-        # load last package meta file and just check if sourceaccess flag was used at all, no per user checking atm
-        begin
-          r = Suse::Backend.get("/source/#{CGI.escape(target_project_name)}/#{target_package_name}/_history?deleted=1&meta=1")
-        rescue
-          raise DbPackage::UnknownObjectError, "#{target_project_name}/#{target_package_name}"
-        end
-
-        data = ActiveXML::XMLNode.new(r.body.to_s)
-        lastrev = nil
-        data.each_revision {|rev| lastrev = rev}
-        metapath = "/source/#{CGI.escape(target_project_name)}/#{target_package_name}/_meta"
-        if lastrev
-          srcmd5 = lastrev.value('srcmd5')
-          metapath += "?rev=#{srcmd5}" # only add revision if package has some
-        end
-
-        r = Suse::Backend.get(metapath)
-        raise DbPackage::UnknownObjectError, "#{target_project_name}/#{target_package_name}" unless r
-        dpkg = Package.new(r.body)
-        raise DbPackage::UnknownObjectError, "#{target_project_name}/#{target_package_name}" unless dpkg
-        raise DbPackage::ReadAccessError, "#{target_project_name}/#{target_package_name}" if dpkg.disabled_for? 'access'
-        raise DbPackage::ReadSourceAccessError, "#{target_project_name}/#{target_package_name}" if dpkg.disabled_for? 'sourceaccess'
-      end
+      validate_read_access_of_deleted_package(target_project_name, target_package_name)
     end
     
     # GET /source/:project/:package
@@ -964,19 +938,23 @@ class SourceController < ApplicationController
     if package_name == "_project" or package_name == "_pattern"
       allowed = permissions.project_change? prj
     else
-      if request.get?
-        # a readable package, even on remote instance is enough here
-        begin
-          pack = DbPackage.get_by_project_and_name(project_name, package_name)
-        rescue DbPackage::UnknownObjectError
-        end
+      if params.has_key? :deleted
+        validate_read_access_of_deleted_package(project_name, package_name)
+        pass_to_backend
+        return
       else
-        # we need a local package here in any case for modifications
-        pack = DbPackage.get_by_project_and_name(project_name, package_name)
-        allowed = permissions.package_change? pack
-      end
+        if request.get? 
+          # a readable package, even on remote instance is enough here
+          begin
+            pack = DbPackage.get_by_project_and_name(project_name, package_name)
+          rescue DbPackage::UnknownObjectError
+          end
+        else
+          # we need a local package here in any case for modifications
+          pack = DbPackage.get_by_project_and_name(project_name, package_name)
+          allowed = permissions.package_change? pack
+        end
 
-      unless params.has_key? :deleted and ["_history",].include?(file)
         if pack.nil? and request.get?
           # Check if this is a package on a remote OBS instance
           answer = Suse::Backend.get(request.path)
@@ -2064,5 +2042,33 @@ class SourceController < ApplicationController
     return true if name =~ /^_product:[-_+\w\.:]*$/
     return true if name =~ /^_patchinfo:[-_+\w\.:]*$/ # obsolete, just for backward compatibility
     name =~ /^\w[-_+\w\.:]*$/
+  end
+
+  # load last package meta file and just check if sourceaccess flag was used at all, no per user checking atm
+  def validate_read_access_of_deleted_package(project, name)
+    prj = DbProject.get_by_name project
+    raise DbProject::ReadAccessError, "#{project}" if prj.disabled_for? 'access', nil, nil
+    raise DbPackage::ReadSourceAccessError, "#{target_project_name}/#{target_package_name}" if prj.disabled_for? 'sourceaccess', nil, nil
+
+    begin
+      r = Suse::Backend.get("/source/#{CGI.escape(project)}/#{name}/_history?deleted=1&meta=1")
+    rescue
+      raise DbPackage::UnknownObjectError, "#{project}/#{name}"
+    end
+
+    data = ActiveXML::XMLNode.new(r.body.to_s)
+    lastrev = nil
+    data.each_revision {|rev| lastrev = rev}
+    metapath = "/source/#{CGI.escape(project)}/#{name}/_meta"
+    if lastrev
+      srcmd5 = lastrev.value('srcmd5')
+      metapath += "?rev=#{srcmd5}" # only add revision if package has some
+    end
+
+    r = Suse::Backend.get(metapath)
+    raise DbPackage::UnknownObjectError, "#{project}/#{name}" unless r
+    dpkg = Package.new(r.body)
+    raise DbPackage::UnknownObjectError, "#{project}/#{name}" unless dpkg
+    raise DbPackage::ReadSourceAccessError, "#{project}/#{name}" if dpkg.disabled_for? 'sourceaccess'
   end
 end
