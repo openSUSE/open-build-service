b2abd252fcd9d27bdd82baa7040f542c75161952
Sascha Peilicke <saschpe@suse.de>
2011-05-02 13:44:22 +0200

    Revert "[webui] Use 'each' instead of 'each_package' for enumeration a"
    This reverts commit 76543d90962ea5b92e813f8012ed62e1e6d1b694.
    Wasn't such a good idea. In fact, this fixed the wrong issue as it was
    find_cached returning nil.

--- src/webui/app/controllers/main_controller.rb
+++ src/webui/app/controllers/main_controller.rb
@@ -100,10 +100,8 @@ class MainController < ApplicationController
       predicate = "starts-with(@project,'openSUSE:')"
     elsif category == 'main'
       predicate = "not(starts-with(@project,'home:')) and not(starts-with(@project,'DISCONTINUED:')) and not(starts-with(@project,'openSUSE:'))"
-    else
-      render :text => 'No category given' and return
     end
-    find_cached(Collection, :id, :what => 'package', :predicate => predicate).each do |p|
+    find_cached(Collection, :id, :what => 'package', :predicate => predicate).each_package do |p|
       @packages << [p.value(:project), p.value(:name)]
     end
     render :template => 'main/sitemap_packages', :layout => false, :locals => {:action => params[:listaction]}
