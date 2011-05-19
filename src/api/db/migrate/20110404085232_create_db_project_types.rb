df25c1ddf2ab514784dabcfd220b5472e842e4a5
Adrian Schröter <adrian@suse.de>
2011-05-19 16:11:58 +0200

    [api] move entries creation into seperate transaction of table creation.

--- src/api/db/migrate/20110404085232_create_db_project_types.rb
+++ src/api/db/migrate/20110404085232_create_db_project_types.rb
@@ -3,11 +3,6 @@ class CreateDbProjectTypes < ActiveRecord::Migration
     create_table :db_project_types do |t|
       t.string :name, :null => false
     end
-
-    # Create standard content (can be done by running 'rake db:seed' too)
-    DbProjectType.find_or_create_by_name("standard")
-    DbProjectType.find_or_create_by_name("maintenance")
-    DbProjectType.find_or_create_by_name("maintenance_incident")
   end
 
   def self.down
