370ff4ad0cb3cc32fd1889f14fdb1c01f3929a76
Sascha Peilicke <saschpe@suse.de>
2011-05-24 10:58:20 +0200

    [api] Support for rake gems:install
    Need to run initializers to gain access to CONFIG (options.yaml).

--- src/api/config/environment.rb
+++ src/api/config/environment.rb
@@ -78,8 +78,10 @@ Rails::Initializer.run do |config|
   # See Rails::Configuration for more options
 end
 
-ActionController::Base.perform_caching = true
+# rake gems:install doesn't load initializers, load options manually if CONFIG is undefined
+require File.join(File.dirname(__FILE__), 'initializers', 'options') unless defined?(CONFIG)
 
+ActionController::Base.perform_caching = true
 ActiveRbac.controller_layout = "rbac"
 
 unless defined?(SOURCE_PROTOCOL) and not SOURCE_PROTOCOL.blank?
