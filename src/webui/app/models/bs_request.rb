openSUSE/open-build-service

commit 9a0d1f0b3aed8dd18f04b7929738267019c961e2

[webui] add link to create a basic maintenance release request

src/webui/app/models/bs_request.rb
@@ -15,28 +15,32 @@ class BsRequest < ActiveXML::Base
       case opt[:type]
         when "submit" then
           # set target package is the same as the source package if no target package is specified
-          target_package = "package=\"#{opt[:package].to_xs}\"" if target_package.blank?
           revision_option = "rev=\"#{opt[:rev].to_xs}\"" unless opt[:rev].blank?
-          option = "<source project=\"#{opt[:project]}\" package=\"#{opt[:package]}\" #{revision_option}/>"
-          option += "<options><sourceupdate>#{opt[:sourceupdate]}</sourceupdate></options>" unless opt[:sourceupdate].blank?
+          action = "<source project=\"#{opt[:project]}\" package=\"#{opt[:package]}\" #{revision_option}/>"
+          action += "<target project=\"#{opt[:targetproject].to_xs}\" #{target_package}/>"
+          action += "<options><sourceupdate>#{opt[:sourceupdate]}</sourceupdate></options>" unless opt[:sourceupdate].blank?
         when "add_role" then
-          option = "<group name=\"#{opt[:group]}\" role=\"#{opt[:role]}\"/>" unless opt[:group].blank?
-          option = "<person name=\"#{opt[:person]}\" role=\"#{opt[:role]}\"/>" unless opt[:person].blank?
+          action = "<group name=\"#{opt[:group]}\" role=\"#{opt[:role]}\"/>" unless opt[:group].blank?
+          action = "<person name=\"#{opt[:person]}\" role=\"#{opt[:role]}\"/>" unless opt[:person].blank?
+          action += "<target project=\"#{opt[:targetproject].to_xs}\" #{target_package}/>"
         when "set_bugowner" then
-          option = "<person name=\"#{opt[:person]}\" role=\"#{opt[:role]}\"/>"
+          action = "<person name=\"#{opt[:person]}\" role=\"#{opt[:role]}\"/>"
+          action += "<target project=\"#{opt[:targetproject].to_xs}\" #{target_package}/>"
         when "change_devel" then
-          option = "<source project=\"#{opt[:project]}\" package=\"#{opt[:package]}\"/>"
+          action = "<source project=\"#{opt[:project]}\" package=\"#{opt[:package]}\"/>"
+          action += "<target project=\"#{opt[:targetproject].to_xs}\" #{target_package}/>"
         when "maintenance_incident" then
-          option = "<source project=\"#{opt[:project]}\"/>"
+          action = "<source project=\"#{opt[:project]}\" />"
+          action += "<target project=\"#{opt[:targetproject].to_xs}\" />" unless opt[:targetproject].blank?
         when "maintenance_release" then
-          option = "<source project=\"#{opt[:project]}\"/>"
+          action = "<source project=\"#{opt[:project]}\" />"
+          action += "<target project=\"#{opt[:targetproject].to_xs}\" />" unless opt[:targetproject].blank?
       end
       # build the request XML
       reply = <<-EOF
         <request>
           <action type="#{opt[:type]}">
-            #{option}
-            <target project="#{opt[:targetproject].to_xs}" #{target_package}/>
+            #{action}
           </action>
           <state name="new"/>
           <description>#{opt[:description].to_xs}</description>

src/webui/app/controllers/project_controller.rb
@@ -180,6 +180,28 @@ class ProjectController < ApplicationController
     return
   end
 
+  def new_release_request
+    valid_http_methods(:post)
+
+    if params[:skiprequest]
+      # FIXME2.3: do it directly here, api function missing
+    else
+      begin
+        params[:type] = "maintenance_release"
+        req = BsRequest.new(:project => params[:project], :type => "maintenance_release")
+        req.save(:create => true)
+        flash[:success] = "Created maintenance release request"
+      rescue ActiveXML::Transport::NotFoundError => e
+        message, _, _ = ActiveXML::Transport.extract_error_message(e)
+        flash[:error] = message
+        redirect_to(:action => "show", :project => params[:project]) and return
+      end
+    end
+
+    redirect_to :controller => 'project', :action => 'show', :project => params[:project]
+    return
+  end
+
   def load_packages_mainpage
     @packages = Rails.cache.fetch("%s_packages_mainpage" % @project, :expires_in => 30.minutes) do
       find_cached(Package, :all, :project => @project.name, :expires_in => 30.seconds )
@@ -493,7 +515,8 @@ class ProjectController < ApplicationController
     @patchinfo = []
     unless @packages.blank?
       @packages.each do |p|
-        @patchinfo << p.name if p.name =~ %r{^_patchinfo}
+        # FIXME: correct check would be to check if a source file "_patchinfo" exists
+        @patchinfo << p.name if p.name =~ %r{^_patchinfo} or p.name =~ %r{^patchinfo}
       end
     end
     @filterstring = params[:searchtext] || ''

src/webui/app/views/project/show.html.erb
@@ -89,14 +89,13 @@
           <% end -%>
           <% if session[:login] %>
             <% if @project.can_edit?( session[:login] ) %>
-              <% if @is_incident_project %>
-                <li>
-                  <%= link_to(image_tag('tools-report-bug.png', :title => 'Release this maintenance update'), :action => 'releaserequest', :project => @project) -%>
-                  <%= link_to('Release this maintenance update', :action => 'releaserequest', :project => @project) -%>
-                </li>
-              <% end -%>
               <% unless @project.is_remote? %>
-                <% if @is_maintenance_project %>
+                <% if @is_incident_project %>
+                  <li>
+                    <%= link_to_remote(image_tag('icons/brick_go.png', :title => 'Release this maintenance incident'), :url => { :action => :release_request_dialog, :project => @project }) -%>
+                    <%= link_to_remote('Release this maintenance incident', :url => { :action => :release_request_dialog, :project => @project }) -%>
+                  </li>
+                <% elsif @is_maintenance_project %>
                   <li>
                     <%= link_to(image_tag('icons/brick_add.png', :title => 'Create incident'), :controller => 'project', :action => 'new_incident', :ns => @project.name) -%>
                     <%= link_to('Create incident', :controller => 'project', :action => 'new_incident', :ns => @project.name) -%>

commit f8ade4d9f9ccd6bce241a6857f82f917dd11e480

[webui] Fix delete request submission regression.
Broke with 9a0d1f0b, was fixed only partly with 1ad51669, which itself
was a fix for an API-incompatible API change that broke new package
submit requests (i.e. it doesn't accept submit requests w/o target
package anymore).

src/webui/app/models/bs_request.rb
@@ -11,13 +11,13 @@ class BsRequest < ActiveXML::Base
       opt[:description] = "" if !opt.has_key? :description or opt[:description].nil?
       if opt[:targetpackage] and not opt[:targetpackage].empty?
         target_package = "package=\"#{opt[:targetpackage].to_xs}\""
-      else
-        target_package = "package=\"#{opt[:package].to_xs}\""
       end
 
       # set request-specific options
       case opt[:type]
         when "submit" then
+          # use source package name if no target package name is given for a submit request
+          target_package = "package=\"#{opt[:package].to_xs}\"" if target_package.empty?
           # set target package is the same as the source package if no target package is specified
           revision_option = "rev=\"#{opt[:rev].to_xs}\"" unless opt[:rev].blank?
           action = "<source project=\"#{opt[:project]}\" package=\"#{opt[:package]}\" #{revision_option}/>"
@@ -39,6 +39,8 @@ class BsRequest < ActiveXML::Base
         when "maintenance_release" then
           action = "<source project=\"#{opt[:project]}\" />"
           action += "<target project=\"#{opt[:targetproject].to_xs}\" />" unless opt[:targetproject].blank?
+        when "delete_request" then
+          action = "<target project=\"#{opt[:targetproject].to_xs}\" #{target_package}/>"
       end
       # build the request XML
       reply = <<-EOF