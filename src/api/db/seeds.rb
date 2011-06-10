1db6a901ba00bdb8b282c3872a7d64b4e712f25b
Adrian Schröter <adrian@suse.de>
2011-06-10 14:12:07 +0200

    [api] setup configuration values also on db:setup

--- src/api/db/seeds.rb
+++ src/api/db/seeds.rb
@@ -90,3 +90,19 @@ r = Repository.new( :name => "deleted", :db_project => d )
 d.repositories << r
 d.save
 
+# set default configuration settings
+Configuration.find_or_create_by_title_and_description(:title => "Open Build Service", :description => <<-EOT
+  <p class="description">
+    The <%= link_to 'Open Build Service (OBS)', 'http://openbuildservice.org', :title => 'OBS Project Page' %>
+    is an open and complete distribution development platform that provides a transparent infrastructure for development of Linux distributions, used by openSUSE, MeeGo and other distributions.
+    Supporting also Fedora, Debian, Ubuntu, RedHat and other Linux distributions.
+  </p>
+  <p class="description">
+    The OBS is developed under the umbrella of the <%= link_to 'openSUSE project', 'http://www.opensuse.org', :title => 'openSUSE project page' %>. Please find further informations on the <%= link_to 'openSUSE Project wiki pages', 'http://wiki.opensuse.org/openSUSE:Build_Service', :title => 'OBS wiki pages' %>.
+  </p>
+
+  <p class="description">
+    The Open Build Service developer team is greeting you. In case you use your OBS productive in your facility, please do us a favor and add yourself at <%= link_to 'this wiki page', 'http://wiki.opensuse.org/openSUSE:Build_Service_installations' %>. Have fun and fast build times!
+  </p>
+EOT
+)
