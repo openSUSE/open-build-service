11a99976ca4203d2cdffbeec1d80ce1e92f17c78
Adrian Schröter <adrian@suse.de>
2011-06-16 09:10:09 +0200

    [api] use correct escaping in instance descriptions

--- src/api/db/seeds.rb
+++ src/api/db/seeds.rb
@@ -93,16 +93,16 @@ d.save
 # set default configuration settings
 Configuration.create(:title => "Open Build Service", :description => <<-EOT
   <p class="description">
-    The <%= link_to 'Open Build Service (OBS)', 'http://openbuildservice.org', :title => 'OBS Project Page' %>
+    The <a href="http://openbuildservice.org">Open Build Service (OBS)</a>
     is an open and complete distribution development platform that provides a transparent infrastructure for development of Linux distributions, used by openSUSE, MeeGo and other distributions.
     Supporting also Fedora, Debian, Ubuntu, RedHat and other Linux distributions.
   </p>
   <p class="description">
-    The OBS is developed under the umbrella of the <%= link_to 'openSUSE project', 'http://www.opensuse.org', :title => 'openSUSE project page' %>. Please find further informations on the <%= link_to 'openSUSE Project wiki pages', 'http://wiki.opensuse.org/openSUSE:Build_Service', :title => 'OBS wiki pages' %>.
+    The OBS is developed under the umbrella of the <a href="http://www.opensuse.org">openSUSE project</a>. Please find further informations on the <a href="http://wiki.opensuse.org/openSUSE:Build_Service">openSUSE Project wiki pages</a>.
   </p>
 
   <p class="description">
-    The Open Build Service developer team is greeting you. In case you use your OBS productive in your facility, please do us a favor and add yourself at <%= link_to 'this wiki page', 'http://wiki.opensuse.org/openSUSE:Build_Service_installations' %>. Have fun and fast build times!
+    The Open Build Service developer team is greeting you. In case you use your OBS productive in your facility, please do us a favor and add yourself at <a href="http://wiki.opensuse.org/openSUSE:Build_Service_installations">this wiki page</a>. Have fun and fast build times!
   </p>
 EOT
 )
