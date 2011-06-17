254038fab11b429dd6d4cb213bb71d49133c6aaa
Adrian Schröter <adrian@suse.de>
2011-06-17 14:08:31 +0200

    [webui] fix syntax error for working services ...

--- src/webui/app/controllers/package_controller.rb
+++ src/webui/app/controllers/package_controller.rb
@@ -252,7 +252,7 @@ class PackageController < ApplicationController
     # check source service state
     @services = find_cached(Service,  :project => @project, :package => @package )
     @serviceerror = nil
-    @serviceerror = @package.serviceinfo.error.text if @package.serviceinfo and @package.serviceinfo.error
+    @serviceerror = @package.serviceinfo.value(:error) if @package.serviceinfo
   end
   private :set_file_details
 
