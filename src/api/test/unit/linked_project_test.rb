786961f06dbeaf0be501cd49e9a104582948d90e
Stephan Kulow <coolo@suse.de>
2011-07-07 15:30:27 +0200

    [api] look for test_helper in absolute path

--- src/api/test/unit/linked_project_test.rb
+++ src/api/test/unit/linked_project_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require File.expand_path(File.dirname(__FILE__) + "/..") + "/test_helper"
 
 class LinkedProjectTest < ActiveSupport::TestCase
   fixtures :linked_projects, :db_projects
