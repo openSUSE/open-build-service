9cccfcd23f88ec593f87fbe8c26b03321a579e19
Adrian Schröter <adrian@suse.de>
2011-07-20 15:17:47 +0200

    [api] flags without the flag type can't be saved, fix unit test

--- src/api/test/unit/flag_test.rb
+++ src/api/test/unit/flag_test.rb
@@ -6,7 +6,7 @@ class FlagTest < ActiveSupport::TestCase
 
   def test_validation
       #only a flag wirh a set project_id OR package_id can be saved!
-      f = Flag.new(:db_project_id => 502, :db_package_id => 10095)
+      f = Flag.new(:db_project_id => 502, :db_package_id => 10095, :flag => 'build')
       
       #the flag shouldn't be saved
       assert_equal  false, f.save
@@ -19,8 +19,6 @@ class FlagTest < ActiveSupport::TestCase
   def test_to_xml_error
     #if no flagstatus set, an error should be raised!
     f = Flag.new(:db_project_id => 502, :architecture_id => 1, :repo => '999.999')
-    # no flag
-    assert_equal false, f.save
     f.flag = 'build'
     assert_equal false, f.save
     f.status = 'enabled'
