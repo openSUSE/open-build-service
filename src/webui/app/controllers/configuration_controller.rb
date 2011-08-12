320b4b6be258237e44053bfe9a1c8d4f8b4509e0
Sascha Peilicke <saschpe@suse.de>
2011-08-12 14:19:01 +0200

    [webui] Allow admins to configure architecture recommendations

--- src/webui/app/controllers/configuration_controller.rb
+++ src/webui/app/controllers/configuration_controller.rb
@@ -1,6 +1,7 @@
 class ConfigurationController < ApplicationController
 
   before_filter :require_admin
+  before_filter :require_available_architectures, :only => [:index, :update_architectures]
 
   def index
   end
@@ -23,6 +24,24 @@ class ConfigurationController < ApplicationController
     rescue ActiveXML::Transport::Error => e
       logger.debug "Failed to update configuration"
       flash[:error] = "Failed to update configuration"
+    end
+    redirect_to :action => 'index'
+  end
+
+  def update_architectures
+    valid_http_methods :post
+
+    @available_architectures.each do |arch_elem|
+      arch = Architecture.find_cached(arch_elem.name) # fetch a real 'Architecture' from 'directory' entry
+      if params[:arch_recommended] and params[:arch_recommended].include?(arch.name) and arch.recommended.text == 'false'
+        arch.recommended.text = 'true'
+        arch.save
+        Architecture.free_cache(:available)
+      elsif arch.recommended.text == 'true'
+        arch.recommended.text = 'false'
+        arch.save
+        Architecture.free_cache(:available)
+      end
     end
     redirect_to :action => 'index'
   end
