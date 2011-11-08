openSUSE/open-build-service

commit 96a5712afe800fb6e7f660b783bdae93101de386

[api] some fixes for the validator

src/api/lib/opensuse/validator.rb
@@ -51,7 +51,9 @@ module ActionController
       opt = params()
       opt[:method] = request.method.to_s
       opt[:type] = "response"
-      Suse::Validator.validate(opt, response.body.to_s)
+      if response.status.to_s == "200"
+        Suse::Validator.validate(opt, response.body.to_s)
+      end
     end
 
   end
@@ -122,15 +124,10 @@ module Suse
         else
           raise "illegal option; need Hash/Symbol/String, seen: #{opt.class.name}"
         end
-        if content.nil?
-          raise "illegal option; need content"
-        end
-        if content.empty?
-          logger.debug "no content, skipping validation"
-          raise ValidationError, "Document is empty"
-        end
 
         schema_base_filename = schema_location + "/" + schema_file
+        schema = nil
+        s = Benchmark.realtime do
         if File.exists? schema_base_filename + ".rng"
           schema = Nokogiri::XML::RelaxNG(File.open(schema_base_filename + ".rng"))
         elsif File.exists? schema_base_filename + ".xsd"
@@ -139,7 +136,17 @@ module Suse
           logger.debug "no schema found, skipping validation"
           return true
         end
+        end
 
+        if content.nil?
+          raise "illegal option; need content for #{schema_base_filename}"
+        end
+        if content.empty?
+          logger.debug "no content, skipping validation"
+          raise ValidationError, "Document is empty, not allowed for #{schema_base_filename}"
+        end
+
+        v = Benchmark.realtime do
         begin
           doc = Nokogiri::XML(content, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
           schema.validate(doc).each do |error|
@@ -147,11 +154,12 @@ module Suse
             # Only raise an exception for user-input validation!
             raise ValidationError, "#{opt[:type]} validation error: #{error}"
           end
-          #logger.debug "#{opt[:type]} validation succeeded: #{opt.inspect}"
-          return true
         rescue Nokogiri::XML::SyntaxError => error
           raise ValidationError, "#{opt[:type]} validation error: #{error}"
         end
+        end
+        logger.debug "Validatated #{schema_file} schema:#{s} validate:#{v}"
+        return true
       end
     end
 

docs/api/api/project.rng
@@ -71,9 +71,6 @@
         </attribute>
       </optional>
       <optional>
-        <attribute name="mountproject"/>
-      </optional>
-      <optional>
         <attribute name="created"/>
       </optional>
       <optional>
@@ -87,6 +84,10 @@
       <element ns="" name="description">
         <text/>
       </element>
+
+     <element ns="" name="mountproject">
+        <text/>
+      </optional>
 
       <interleave>
         <zeroOrMore>

commit ff118baee70a8a513b53cebacb51ed2edea9c054

[api] Actually do validate XML responses and use Rails controller name mapping
Commit 96a5712a broke it, also remove benchmarking code.

Rails has special controller name mapping (to routes, database tables,
etc.), as we name or schema files accordingly, we should use the Rails
mechanism instead of our own.

src/api/lib/opensuse/validator.rb
@@ -31,9 +31,8 @@ module ActionController
       #   end
       # end
       def validate_action( opt )
-        controller = self.name.match(/^(.*?)Controller/)[1].downcase
         opt.each do |action, action_opt|
-          Suse::Validator.add_schema_mapping(controller, action, action_opt)
+          Suse::Validator.add_schema_mapping(self.controller_path, action, action_opt)
         end
       end
     end
@@ -43,6 +42,7 @@ module ActionController
       opt = params()
       opt[:method] = request.method.to_s
       opt[:type] = "request"
+      logger.debug "Validate XML request: #{request}"
       Suse::Validator.validate(opt, request.raw_post.to_s)
     end
 
@@ -51,7 +51,8 @@ module ActionController
       opt = params()
       opt[:method] = request.method.to_s
       opt[:type] = "response"
-      if response.status.to_s == "200"
+      logger.debug "Validate XML response: #{response}"
+      if response.status.to_s == "200 OK"
         Suse::Validator.validate(opt, response.body.to_s)
       end
     end
@@ -127,26 +128,23 @@ module Suse
 
         schema_base_filename = schema_location + "/" + schema_file
         schema = nil
-        s = Benchmark.realtime do
         if File.exists? schema_base_filename + ".rng"
           schema = Nokogiri::XML::RelaxNG(File.open(schema_base_filename + ".rng"))
         elsif File.exists? schema_base_filename + ".xsd"
           schema = Nokogiri::XML::Schema(File.open(schema_base_filename + ".xsd"))
         else
-          logger.debug "no schema found, skipping validation"
+          logger.debug "no schema found, skipping validation for #{opt}"
           return true
-        end
         end
 
         if content.nil?
           raise "illegal option; need content for #{schema_base_filename}"
         end
         if content.empty?
-          logger.debug "no content, skipping validation"
+          logger.debug "no content, skipping validation for #{opt}"
           raise ValidationError, "Document is empty, not allowed for #{schema_base_filename}"
         end
 
-        v = Benchmark.realtime do
         begin
           doc = Nokogiri::XML(content, nil, nil, Nokogiri::XML::ParseOptions::STRICT)
           schema.validate(doc).each do |error|
@@ -157,8 +155,6 @@ module Suse
         rescue Nokogiri::XML::SyntaxError => error
           raise ValidationError, "#{opt[:type]} validation error: #{error}"
         end
-        end
-        logger.debug "Validatated #{schema_file} schema:#{s} validate:#{v}"
         return true
       end
     end