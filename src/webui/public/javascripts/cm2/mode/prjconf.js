68a62887da5ac913875385ace723649d4750abad
Sascha Peilicke <saschpe@suse.de>
2011-11-15 16:04:29 +0100

    [webui] Some work on rpm/obs syntax highlighting.

--- src/webui/public/javascripts/cm2/mode/prjconf.js
+++ src/webui/public/javascripts/cm2/mode/prjconf.js
@@ -3,13 +3,16 @@
 CodeMirror.defineMode("prjconf", function(config, modeConfig) {
   var arch = /^(i386|i586|i686|x86_64|ppc64|ppc|ia64|s390x|s390|sparc64|sparcv9|sparc|noarch|alphaev6|alpha|hppa|mipsel)/;
   var prjconf = /^(Conflict|Ignore|Keep|Macros|Optflags|Order|Prefer|ExportFilter|Type|Patterntype|Preinstall|Repotype|Required|Runscripts|Substitute|Support|VMinstall):/;
-  var control_flow_macros = /^%(ifnarch|ifarch|if|else|endif|\|\||\&\&)/; // rpm control flow macros
+  var control_flow_complex = /^%(ifnarch|ifarch|if)/; // rpm control flow macros
+  var control_flow_simple = /^%(else|endif)/; // rpm control flow macros
+  var operators = /^(\!|\?|\<\=|\<|\>\=|\>|\=\=|\&\&|\|\|)/; // operators in control flow macros
 
   return {
     startState: function () {
         return {
-          exportFilter: false,
           controlFlow: false,
+          exportFilter: false,
+          macroParameters: false,
         };
     },
     token: function (stream, state) {
@@ -36,34 +39,38 @@ CodeMirror.defineMode("prjconf", function(config, modeConfig) {
       if (stream.match(/^\$\w+/)) { return "def"; } // Variables like '$RPM_BUILD_ROOT'
       if (stream.match(/^\$\{\w+\}/)) { return "def"; } // Variables like '${RPM_BUILD_ROOT}'
 
-      //TODO: Support expressions in control flow macros like '%if ! %foo || %bar && %spam':
-      if (stream.match(control_flow_macros)) { 
+      if (stream.match(control_flow_simple)) { return "keyword"; }
+      if (stream.match(control_flow_complex)) {
         state.controlFlow = true;
-        return "keyword"; 
+        return "keyword";
       }
-      //TODO: Match architectures only behind '%ifarch' ?!?
+      if (state.controlFlow) {
+        if (stream.match(operators)) { return "operator"; }
+        if (stream.match(/^(\d+)/)) { return "number"; }
+        if (stream.eol()) { state.controlFlow = false; }
+      }
+
       if (stream.match(arch)) { return "number"; }
-      //TODO: Include stuff like %attr(0775,root,root), not possible with stateless parser
-      if (stream.match(/^%[\w]+/)) { return "macro"; } // Macros like '%make_install'
+
+      // Macros like '%make_install' or '%attr(0775,root,root)'
+      if (stream.match(/^%[\w]+/)) {
+        if (stream.match(/^\(/)) { state.macroParameters = true; }
+        return "macro";
+      }
+      if (state.macroParameters) {
+        if (stream.match(/^\d+/)) { return "number";}
+        if (stream.match(/^\)/)) {
+          state.macroParameters = false;
+          return "macro";
+        }
+      }
       if (stream.match(/^%\{\??[\w \-]+\}/)) { return "macro"; } // Macros like '%{defined fedora}'
 
-      if (state.controlFlow) {
-        if (stream.match(/^(\!|\?|\=\=|\&\&|\|\|)/)) {
-          return "keyword"; // Even though 'operator' would be more correct, 'keyword' looks nicer ;-)
-        }
-        //TODO: Match '\"foo\"' strings, but don't forget that there may be macros inside,
-        if (stream.eol()) {
-          state.controlFlow = false;
-        }
-      }
-
       //TODO: Include bash script sub-parser (CodeMirror supports that)
-
       stream.next();
       return null;
     }
   };
 });
 
-CodeMirror.defineMIME("text/x-prjconf", "prjconf");
-
+CodeMirror.defineMIME("text/vnd.openbuildservice.prjconf", "prjconf");
--- src/webui/public/javascripts/cm2/mode/spec.js
+++ src/webui/public/javascripts/cm2/mode/spec.js
@@ -5,17 +5,21 @@ CodeMirror.defineMode("spec", function(config, modeConfig) {
 
   var preamble = /^(Name|Version|Release|License|Summary|Url|Group|Source|BuildArch|BuildRequires|BuildRoot|AutoReqProv|Provides|Requires(\(\w+\))?|Obsoletes|Conflicts|Recommends|Source\d*|Patch\d*|ExclusiveArch|NoSource|Supplements):/;
   var section = /^%(debug_package|package|description|prep|build|install|files|clean|changelog|preun|postun|pre|post|triggerin|triggerun|pretrans|posttrans|verifyscript|check|triggerpostun|triggerprein|trigger)/;
-
-  var control_flow_macros = /^%(ifnarch|ifarch|if|else|endif|\|\||\&\&)/; // rpm control flow macros
+  var control_flow_complex = /^%(ifnarch|ifarch|if)/; // rpm control flow macros
+  var control_flow_simple = /^%(else|endif)/; // rpm control flow macros
+  var operators = /^(\!|\?|\<\=|\<|\>\=|\>|\=\=|\&\&|\|\|)/; // operators in control flow macros
 
   return {
-    token: function(stream) {
+    startState: function () {
+        return {
+          controlFlow: false,
+          macroParameters: false,
+          section: false,
+        };
+    },
+    token: function (stream, state) {
       var ch = stream.peek();
       if (ch == "#") { stream.skipToEnd(); return "comment"; }
-
-      //if (stream.match(/^\s+\d+\s+/)) { return "number"; }
-      //TODO: Doesn't work with current comment matching:
-      //if (stream.match(/^[A-Za-z]\#\d#+/)) { return "issue"; } // Issue tracker abbreviations like 'bnc#1234'
 
       if (stream.sol()) {
         if (stream.match(preamble)) { return "preamble"; }
@@ -25,25 +29,38 @@ CodeMirror.defineMode("spec", function(config, modeConfig) {
       if (stream.match(/^\$\w+/)) { return "def"; } // Variables like '$RPM_BUILD_ROOT'
       if (stream.match(/^\$\{\w+\}/)) { return "def"; } // Variables like '${RPM_BUILD_ROOT}'
 
-      //TODO: Support expressions in control flow macros like '%if ! %foo || %bar && %spam':
-      if (stream.match(control_flow_macros)) { return "builtin"; }
-      //TODO: Match architectures only behind '%ifarch' ?!?
+      if (stream.match(control_flow_simple)) { return "keyword"; }
+      if (stream.match(control_flow_complex)) {
+        state.controlFlow = true;
+        return "keyword";
+      }
+      if (state.controlFlow) {
+        if (stream.match(operators)) { return "operator"; }
+        if (stream.match(/^(\d+)/)) { return "number"; }
+        if (stream.eol()) { state.controlFlow = false; }
+      }
+
       if (stream.match(arch)) { return "number"; }
-      //TODO: Include stuff like %attr(0775,root,root), not possible with stateless parser
-      if (stream.match(/^%[\w]+/)) { return "macro"; } // Macros like '%make_install'
+
+      // Macros like '%make_install' or '%attr(0775,root,root)'
+      if (stream.match(/^%[\w]+/)) {
+        if (stream.match(/^\(/)) { state.macroParameters = true; }
+        return "macro";
+      }
+      if (state.macroParameters) {
+        if (stream.match(/^\d+/)) { return "number";}
+        if (stream.match(/^\)/)) {
+          state.macroParameters = false;
+          return "macro";
+        }
+      }
       if (stream.match(/^%\{\??[\w \-]+\}/)) { return "macro"; } // Macros like '%{defined fedora}'
 
       //TODO: Include bash script sub-parser (CodeMirror supports that)
-      //var shell_cmds = /^(export|mkdir|cd|cp|rm|mv|chmod|chown|install|make)\s+(-\w+)*/;
-      //if (stream.match(/^\$\(.*\)/)) { return "script"; }
-      //if (stream.match(shell_cmds)) { return "script"; }
-
       stream.next();
       return null;
     }
   };
 });
 
-CodeMirror.defineMIME("text/x-spec", "spec");
-
-
+CodeMirror.defineMIME("text/x-rpm-spec", "spec");
