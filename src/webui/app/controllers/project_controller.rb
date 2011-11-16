809c57be09c5ee14dc2639fcc24e00384a1a3305
Sascha Peilicke <saschpe@suse.de>
2011-11-16 09:57:58 +0100

    [webui] Catch empty projects case...

--- src/webui/app/controllers/project_controller.rb
+++ src/webui/app/controllers/project_controller.rb
@@ -242,10 +242,12 @@ class ProjectController < ApplicationController
 
     # An incident has a patchinfo if there is a package 'patchinfo' with file '_patchinfo', try to find that:
     @has_patchinfo = false
-    @packages.each do |pkg_element|
-      if pkg_element.name == 'patchinfo'
-        Package.find_cached(pkg_element.name, :project => @project).files.each do |pkg_file|
-          @has_patchinfo = true if pkg_file[:name] == '_patchinfo'
+    if @packages
+      @packages.each do |pkg_element|
+        if pkg_element.name == 'patchinfo'
+          Package.find_cached(pkg_element.name, :project => @project).files.each do |pkg_file|
+            @has_patchinfo = true if pkg_file[:name] == '_patchinfo'
+          end
         end
       end
     end if @packages
