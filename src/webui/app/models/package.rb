fa3acef951397ac8c3d32931cdb1837ee212e81b
Sascha Peilicke <saschpe@suse.de>
2011-11-17 10:38:06 +0100

    [webui] Add more binary endings to black list.
    This asymtotically moves towards using file magic... There are already
    guys naming binary firmware like "$FOO.fw-$VERSION", how are we supposed to
    catch this?

--- src/webui/app/models/package.rb
+++ src/webui/app/models/package.rb
@@ -331,9 +331,9 @@ class Package < ActiveXML::Base
   end
 
   def self.is_binary_file?(filename)
-    binary_extensions = %w{.bz2 .dll .exe .gem .gif .gz .jar .jpeg .jpg .lzma .ogg .pdf .pk3 .png .ps .rpm .svgz .tar .taz .tb2 .tbz .tbz2 .tgz .tlz .txz .xpm .xz .z .zip .ttf .0 .otf .ccf .bin .bin_mid}
+    binary_extensions = %w{.0 .bin .bin_mid .bz .bz2 .ccf .cert .der .dll .exe .fw .gem .gif .gz .jar .jpeg .jpg .lzma .ogg .otf.pdf .pk3 .png .ps .rpm .svgz .tar .taz .tb2 .tbz .tbz2 .tgz .tlz .txz .xpm .xz .z .zip .ttf}
     binary_extensions.each do |ext|
-      return true if filename.downcase.ends_with?(ext)
+      return true if filename.downcase.end_with?(ext)
     end
     return false
   end
