036c0fac2f32234a308e275b75227828c48c394c
Sascha Peilicke <saschpe@suse.de>
2011-11-18 11:41:54 +0100

    [api] Add IssueTracker#details experimental code.
    Isn't yet exposed through any controller, but able to fetch details for
    bugzilla and fate entries. What's left is to define a commonly returned
    JSON format (i.e. least common denominator of issue details). Some
    upstream issue trackers demand a login in order to use their API (most
    Trac instances), this would have to be added to the IssueTracker
    model...

--- src/api/app/models/issue_tracker.rb
+++ src/api/app/models/issue_tracker.rb
@@ -1,3 +1,5 @@
+require 'xmlrpc/client'
+
 class IssueTracker < ActiveRecord::Base
   validates_presence_of :name, :regex, :url
   validates_uniqueness_of :name, :regex
@@ -20,5 +22,54 @@ class IssueTracker < ActiveRecord::Base
     # Always use the last capture group for the upstream part (i.e. the '1234' in 'bnc#1234')
     return show_url.gsub('@@@', match[-1]) if match
     return nil
+  end
+
+  def details(issue)
+    #NOTE: Experimental code, subject to change
+    match = matches?(issue)
+    if match
+      if kind == "bugzilla"
+        # Try with 'IssueTracker.find_by_name('bnc').details('bnc#470611')' on script/console
+        begin
+          server = XMLRPC::Client.new2("#{self.url}/xmlrpc.cgi")
+          result = server.proxy('Bug').get(:ids => [match[-1]])
+          # TODO: The returned JSON data may be worth filtering
+          return result['bugs'][0] if result and result['bugs']
+        rescue XMLRPC::FaultException => e
+          logger.error "Error: #{e.faultCode} #{e.faultString}"
+        end
+      elsif kind == "fate"
+        # Try with 'IssueTracker.find_by_name('fate').details('fate#123')' on script/console
+        url = URI.parse("#{self.url}/#{match[-1]}?contenttype=text%2Fxml")
+        begin # Need a loop to follow redirects...
+          http = Net::HTTP.new(url.host, url.port)
+          http.use_ssl = (url.scheme == 'https')
+          request = Net::HTTP::Get.new(url.path)
+          resp = http.start {|http| http.request(request) }
+          url = URI.parse(resp.header['location']) if resp.header['location']
+        end while resp.header['location']
+        # TODO: Parse returned XML and return proper JSON
+        return resp.body
+      elsif kind == "cve"
+        # TODO: Probably hardest of all, is there any common API to CVE trackers?!?
+      elsif kind == "launchpad"
+        # TODO:
+      elsif kind == "sourceforge"
+        # TODO:
+      elsif kind == "trac"
+        # TODO: Most trac instances demand a login, maybe worth having one ;-)
+        server = XMLRPC::Client.new2("#{self.url}/rpc")
+        begin
+          server.proxy('system').listMethods()
+        rescue XMLRPC::FaultException => e
+          logger.error "Error: #{e.faultCode} #{e.faultString}"
+          if e.faultCode == 403
+            # The url would be http://user:pass@trac-inst.com/login/rpc
+            #server = XMLRPC::Client.new2("#{self.url}/login/rpc")
+          end
+        end
+      end
+    end
+    return {}
   end
 end
