63ce4587cb9737f95f8fdfc33cd70d9bdb916f7c
Adrian Schröter <adrian@suse.de>
2011-12-09 17:28:52 +0100

    [api] simple issue unit test

---
+++ src/api/test/unit/issue.rb
@@ -0,0 +1,20 @@
+require File.expand_path(File.dirname(__FILE__) + '/..') + '/test_helper'
+
+class IssueTest < ActiveSupport::TestCase
+  fixtures :users
+  fixtures :db_packages
+  fixtures :issue_trackers
+
+  def test_create_and_destroy
+    pkg = DbPackage.find( 10095 )
+    iggy = User.find_by_email("Iggy@pop.org")
+    issue = Issue.create :name => '0815', :long_name => "bnc#0815", :issue_tracker => 'bnc'
+    issue.save
+    issue.description = 'This unit test is not working'
+    issue.state = 'INVALID'
+    issue.owner = iggy
+    issue.save
+    issue.destroy
+  end
+  
+end
