692631cae719dba02e4fb30220c0400a83d78563
Adrian Schröter <adrian@suse.de>
2011-12-14 16:49:45 +0100

    [api] fix logging of requestid on maintenance release

--- src/api/app/helpers/maintenance_helper.rb
+++ src/api/app/helpers/maintenance_helper.rb
@@ -171,24 +171,24 @@ module MaintenanceHelper
         targetProject.db_packages << new
         new.store
       end
-      cp_params = {
+      upload_params = {
         :user => @http_user.login,
         :rev => 'repository',
         :comment => "Set link to #{targetPackageName} via maintenance_release request",
       }
-      cp_params[:comment] += ", for updateinfo ID #{updateinfoId}" if updateinfoId
-      cp_params[:requestid] = request.id if request
-      cp_path = "/source/#{CGI.escape(targetProject.name)}/#{CGI.escape(basePackageName)}/_link"
-      cp_path << build_query_from_hash(cp_params, [:user, :comment, :rev, :requestid])
+      upload_params[:comment] += ", for updateinfo ID #{updateinfoId}" if updateinfoId
+      upload_path = "/source/#{CGI.escape(targetProject.name)}/#{CGI.escape(basePackageName)}/_link"
+      upload_path << build_query_from_hash(upload_params, [:user, :rev])
       link = "<link package='#{CGI.escape(targetPackageName)}' cicount='copy' />\n"
       md5 = Digest::MD5.hexdigest(link)
-      answer = Suse::Backend.put cp_path, link
+      answer = Suse::Backend.put upload_path, link
       # commit
-      cp_params[:cmd] = 'commitfilelist'
-      cp_params[:noservice] = '1'
-      cp_path = "/source/#{CGI.escape(targetProject.name)}/#{CGI.escape(basePackageName)}"
-      cp_path << build_query_from_hash(cp_params, [:user, :comment, :cmd, :noservice])
-      answer = Suse::Backend.post cp_path, "<directory> <entry name=\"_link\" md5=\"#{md5}\" /> </directory>"
+      upload_params[:cmd] = 'commitfilelist'
+      upload_params[:noservice] = '1'
+      upload_params[:requestid] = request.id if request
+      upload_path = "/source/#{CGI.escape(targetProject.name)}/#{CGI.escape(basePackageName)}"
+      upload_path << build_query_from_hash(upload_params, [:user, :comment, :cmd, :noservice, :requestid])
+      answer = Suse::Backend.post upload_path, "<directory> <entry name=\"_link\" md5=\"#{md5}\" /> </directory>"
     end
   end
 end
