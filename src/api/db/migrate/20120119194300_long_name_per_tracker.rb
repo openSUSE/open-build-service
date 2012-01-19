3bc87c9da71672f9549d3a69466d70cd6a60e8e6
Adrian Schröter <adrian@suse.de>
2012-01-19 15:25:49 +0100

    [api] forgot migration in last commit

---
+++ src/api/db/migrate/20120119194300_long_name_per_tracker.rb
@@ -0,0 +1,30 @@
+require 'delayed_job'
+require File.join(RAILS_ROOT, 'lib/workers/update_package_meta_job.rb')
+
+class LongNamePerTracker < ActiveRecord::Migration
+  def self.up
+    self.transaction do
+      remove_column :issues, :long_name
+      add_column :issue_trackers, :long_name, :string
+
+      IssueTracker.find(:all).each do |t|
+        long_name="#{t.name}#%s"
+        long_name="%s" if t.name=="cve"
+
+        # do not using the model, because we have changed it
+        execute("update issue_trackers SET long_name='#{long_name}' where name='#{t.name}'")
+      end
+      
+
+      execute("alter table issue_trackers modify long_name text NOT NULL")
+
+    end
+    # and update backend
+    Delayed::Job.enqueue UpdatePackageMetaJob.new
+  end
+
+  def self.down
+    add_column :issues, :long_name, :string
+    remove_column :issue_trackers, :long_name
+  end
+end
