0622144a36413771d095b7d95024ed8f189aba33
Adrian Schröter <adrian@suse.de>
2012-01-20 10:12:30 +0100

    [api] use valid time format

--- src/api/db/migrate/20120119204300_add_update_timestamp.rb
+++ src/api/db/migrate/20120119204300_add_update_timestamp.rb
@@ -1,6 +1,6 @@
 class AddUpdateTimestamp < ActiveRecord::Migration
   def self.up
-    add_column :issue_trackers, :issues_updated, :timestamp, :default => 0
+    add_column :issue_trackers, :issues_updated, :timestamp, :default => Time.now
   end
 
   def self.down
