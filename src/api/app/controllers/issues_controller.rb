e8fdadf2d470eb3daf4541259aff9cf3e25c7f91
Adrian Schröter <adrian@suse.de>
2012-01-20 14:36:51 +0100

    [api] create local issue entry on api request if lacking

--- src/api/app/controllers/issues_controller.rb
+++ src/api/app/controllers/issues_controller.rb
@@ -7,7 +7,15 @@ class IssuesController < ApplicationController
     required_parameters :id, :issue_tracker_id
 
     # NOTE: issue_tracker_id is here actually the name
-    issue = Issue.get_by_name_and_tracker( params[:id], params[:issue_tracker_id], params[:force_update] )
+    issue = nil
+    begin
+      issue = Issue.get_by_name_and_tracker( params[:id], params[:issue_tracker_id], params[:force_update] )
+    rescue
+      # not yet existing, try to create
+      issue = Issue.find_or_create_by_name_and_tracker( params[:id], params[:issue_tracker_id] )
+      issue = Issue.get_by_name_and_tracker( params[:id], params[:issue_tracker_id], 1 ) if params[:force_update]
+    end
+
     render :text => issue.render_axml, :content_type => 'text/xml'
   end
 end
