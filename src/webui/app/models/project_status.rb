11bbcab0e709dc08d337d1fe6ccdca31425b57d0
Sascha Peilicke <saschpe@suse.de>
2012-01-23 12:48:43 +0100

    [webui] Nasty ActiveXML collision between Project and ProjectStatus
    Both models have "handles_xml_element 'project'" set. The fun starts
    when you do a search for projects, like this:
    
      Collection.find(:what => 'project', :predicate => "@name='openSUSE:Factory'")
    
    Until now, the result was a Collection with ProjectStatus children
    because ActiveXML::LibXMLNode.get_class picks the first 'Model' that
    handles 'project'.

--- src/webui/app/models/project_status.rb
+++ src/webui/app/models/project_status.rb
@@ -1,6 +1,9 @@
 class ProjectStatus < ActiveXML::Base
  
-  handles_xml_element 'project'
+  # The following collides with the 'Project' model when a "Collection.find(:what => 'project', ...)" is done.
+  # ProjectStatus is only used in one place anyway and not (yet) through the search interface, thus it won't hurt
+  # currently:
+  #handles_xml_element 'project'
 
 end
 
