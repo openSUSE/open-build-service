956d309642b64e47ba5513fe57d63e1559c1d704
Adrian Schröter <adrian@suse.de>
2012-01-26 11:06:26 +0100

    [api] fix migration from OBS 2.1

--- src/api/db/migrate/20110405151201_rename_arch_flags_selectable_and_enabled_to_recommended_and_available.rb
+++ src/api/db/migrate/20110405151201_rename_arch_flags_selectable_and_enabled_to_recommended_and_available.rb
@@ -2,10 +2,12 @@ class RenameArchFlagsSelectableAndEnabledToRecommendedAndAvailable < ActiveRecor
   def self.up
     rename_column :architectures, :enabled, :available
     rename_column :architectures, :selectable, :recommended
+    Architecture.reset_column_information
   end
 
   def self.down
     rename_column :architectures, :available, :enabled
     rename_column :architectures, :recommended, :selectable
+    Architecture.reset_column_information
   end
 end
