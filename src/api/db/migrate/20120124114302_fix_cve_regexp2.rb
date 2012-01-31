e2a7749e68a858edf9298a1475bf862d4d4d01b8
Adrian Schröter <adrian@suse.de>
2012-01-31 14:17:43 +0100

    [api] fix quoting in last commit

--- src/api/db/migrate/20120124114302_fix_cve_regexp2.rb
+++ src/api/db/migrate/20120124114302_fix_cve_regexp2.rb
@@ -1,7 +1,7 @@
 class FixCveRegexp2 < ActiveRecord::Migration
   def self.up
     t=IssueTracker.find_by_name('cve')
-    t.regex="(CVE-\d\d\d\d-\d\d\d\d)" # the only way how it works in ruby AND perl
+    t.regex='(CVE-\d\d\d\d-\d\d\d\d)' # the only way how it works in ruby AND perl
     t.save
     Delayed::Job.enqueue IssueTrackersToBackendJob.new
   end
