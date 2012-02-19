8cd9e4d5c16efece324b8009f8a5f0d2f490b1ae
Stephan Kulow <coolo@suse.de>
2012-02-19 20:06:11 +0100

    [webui] every once in a while a stupid spider comes along and tries to fetch ajax links
    This is not a problem in itself, but it throws an exception ... into my inbox

--- src/webui/app/controllers/main_controller.rb
+++ src/webui/app/controllers/main_controller.rb
@@ -27,7 +27,7 @@ class MainController < ApplicationController
 
   # This action does the heavy lifting for the index method and is only invoked by an AJAX request
   def systemstatus
-    return if not request.xhr? # Only serve AJAX-requests
+    render :text => 'no ajax', :status => 400 and return unless request.xhr? # Only serve AJAX-requests
     if not @spider_bot
       @workerstatus = Rails.cache.fetch('frontpage_workerstatus', :expires_in => 5.minutes, :shared => true) do
         Workerstatus.find :all
