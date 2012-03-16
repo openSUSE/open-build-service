5b1a9dd1641f9c93ba74693154c01f4fe929dd2a
Sascha Peilicke <saschpe@suse.de>
2012-03-16 14:11:22 +0100

    [api,webui] Update deploy scripts to Capistrano-2.11 changes
    Also make sure that the correct Capistrano gem version is actually
    installed. Deploy will fail beforehand if this isn't the case.

--- src/webui/config/deploy.rb
+++ src/webui/config/deploy.rb
@@ -1,11 +1,13 @@
 require 'net/smtp'
 
+depend :local, :gem, 'capistrano', '>=2.11.2'
+
 set :application, "obs-webui"
 
 # git settings
 set :scm, :git
 set :repository,  "git://github.com/openSUSE/open-build-service.git"
-set :branch, "master"
+set :branch, "2.3"
 set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 set :git_subdir, '/src/webui'
@@ -13,6 +15,7 @@ set :migrate_target, :current
 
 set :deploy_notification_to, ['tschmidt@suse.de', 'coolo@suse.de', 'adrian@suse.de', 'saschpe@suse.de', 'mls@suse.de']
 server "buildserviceapi.suse.de", :app, :web, :db, :primary => true
+
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
@@ -27,7 +30,7 @@ set :rails_env, "production"
 task :stage do
   set :deploy_to, "/srv/www/vhosts/opensuse.org/stage/#{application}"
   set :runit_name, "webclient_stage"
-  set :branch, "master"
+  set :branch, "2.3"
   set :static, "build.o.o-stage/stage"
   set :owner, "swebuirun"
   set :rails_env, "stage"
@@ -44,7 +47,7 @@ set :runner, "root"
 
 after "deploy:update_code", "config:symlink_shared_config"
 after "deploy:update_code", "config:sync_static"
-after "deploy:symlink", "config:permissions"
+after "deploy:create_symlink", "config:permissions"
 
 # workaround because we are using a subdirectory of the git repo as rails root
 before "deploy:finalize_update", "deploy:use_subdir"
@@ -105,7 +108,7 @@ namespace :deploy do
     set :latest_release, latest_release_bak
   end
 
-  task :symlink, :except => { :no_release => true } do
+  task :create_symlink, :except => { :no_release => true } do
     on_rollback do
       if previous_release
         run "rm -f #{current_path}; ln -s #{previous_release}#{git_subdir} #{current_path}; true"
