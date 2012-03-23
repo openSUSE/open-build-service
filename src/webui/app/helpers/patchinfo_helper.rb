a45d01e1520b7a32208819a02080f67101e11cf7
Stephan Kulow <coolo@suse.de>
2012-03-23 19:55:49 +0100

    [webui] fix the name of the action in the breadcrumb

--- src/webui/app/helpers/patchinfo_helper.rb
+++ src/webui/app/helpers/patchinfo_helper.rb
@@ -1,7 +1,7 @@
 module PatchinfoHelper
   include ProjectHelper
   def patchinfo_bread_crumb( *args )
-    args.insert(0, link_to( @package, :action => :show, :project => @project, :package => @package ))
+    args.insert(0, link_to( @package, :action => :show_patchinfo, :project => @project, :package => @package ))
     project_bread_crumb( *args )
   end
 end
