1f3411fbe689bb176a957d612b70c1d0a43b0565
Stephan Kulow <coolo@suse.de>
2012-03-27 15:48:53 +0200

    [webui-testsuite] make the spider test a bit smarter

--- src/webui-testsuite/tests/TC80__Spider.rb
+++ src/webui-testsuite/tests/TC80__Spider.rb
@@ -1,25 +1,58 @@
+require 'selenium/client'
+
 class TC80__Spider < TestCase
 
   def getlinks
-    @driver.find_elements(:tag_name => "a").each do |link|
-      link = link.attribute("href")
-      next unless link =~ %r{^http://localhost:};
-      unless @pages_visited.has_key? link
-        @pages_to_visit[link] ||= @driver.current_url
+    baseuri = URI.parse(@driver.current_url)
+    @driver.find_elements(:tag_name => "a").each do |element|
+      next unless element.displayed?
+      link = element.attribute("href")
+      begin
+        link = baseuri.merge(link)
+      rescue ArgumentError 
+        # if merge does not like it, it's not a valid link
+        next
+      end
+      link.fragment = nil
+      link.normalize!
+      next unless link.hostname == 'localhost'
+      next unless link.port == @port
+      unless @pages_visited.has_key? link.to_s
+        @pages_to_visit[link.to_s] ||= [baseuri.to_s, element.text]
       end
     end
+  end
+
+  def raiseit(message, url)
+    puts "Found #{message} on #{url}, crawling path"
+    indent = ' '
+    while @pages_visited.has_key? url
+      url, text = @pages_visited[url]
+      puts "#{indent}#{url} ('#{text}')"
+      indent += '  '
+    end
+    #raise "Found #{message}"
   end
 
   def crawl
     while @pages_to_visit.length > 0
       theone = @pages_to_visit.keys.sort.first
-      wherefrom = @pages_to_visit[theone]
+      @pages_visited[theone] = @pages_to_visit[theone]
       @pages_to_visit.delete theone
-      @pages_visited[theone] = 1
-      puts "crawl #{theone}"
+
       begin
-        @driver.get(theone)
-      rescue Timeout::Error
+        @driver.navigate.to(theone)
+        puts "crawled #{theone}"
+        @wait.until { 
+          @driver.execute_script('return jQuery.active') == 0
+        }
+        unless @driver.find_elements(:css => "div#flash-messages div.ui-state-error").empty?
+          raiseit("flash alert", theone) 
+        end
+        unless @driver.find_elements(:css => "#exception-error").empty?
+          raiseit("error", theone)
+        end
+      rescue Timeout::Error, Selenium::WebDriver::Error::JavascriptError
         next
       end
       getlinks
@@ -30,6 +63,8 @@ class TC80__Spider < TestCase
     @pages_to_visit = Hash.new
     @pages_visited = Hash.new
 
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 6, :interval => 0.1)
+    @port = URI.parse( $data[:url] ).port
     @driver = $page.driver
     @driver.navigate.to($data[:url])
     getlinks
