c0b5c8ff05cdfc6c11289527ab8b525655f5334a
Adrian Schröter <adrian@suse.de>
2012-03-30 10:47:18 +0200

    [api] verify that maintains data get not corrupted

--- src/api/test/functional/zzz_post_consistency_test.rb
+++ src/api/test/functional/zzz_post_consistency_test.rb
@@ -6,5 +6,15 @@ class ZZZPostConsistency < ActionController::IntegrationTest
   def test_resubmit_fixtures
     resubmit_all_fixtures
   end
+
+  def test_check_maintenance_project
+    get "/source/My:Maintenance/_meta"
+    assert_response :success
+    
+    get "/search/project", :match => '[maintenance/maintains/@project="BaseDistro2.0:LinkedUpdateProject"]'
+    assert_response :success
+    assert_tag :tag => 'collection', :children => { :count => 1 }
+    assert_tag :tag => 'project', :attributes => { :name => "My:Maintenance" }
+  end
 end
 
