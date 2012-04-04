ba28ed1e0711708cc5bde672b6e5c9290e353b44
Sascha Peilicke <saschpe@suse.de>
2012-04-04 15:26:15 +0200

    [webui] Move jquery.dataTables setup to application.js

--- src/webui/public/javascripts/application.js
+++ src/webui/public/javascripts/application.js
@@ -247,3 +247,10 @@ $(document).ajaxSend(function(event, request, settings) {
 
 // Could be handy elsewhere ;-)
 var URL_REGEX = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi;
+
+// jquery.dataTables setup:
+$(document).ready(function() {
+  $.extend($.fn.dataTable.defaults, {
+    'iDisplayLength': 25,
+  });
+});
