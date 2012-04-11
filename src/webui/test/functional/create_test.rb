5bc0c79e5ce4650d228ea65e8492b40199702e14
Sascha Peilicke <saschpe@suse.de>
2012-04-11 16:00:53 +0200

    [webui] Fix some more testsuite issues...
    ...another empty <ul> missing <li> issue is left (and catched by the
    spider test). I've got to package rubygem-ruby-debug19 first to debug
    the condition in package/show

--- src/webui/test/functional/create_test.rb
+++ src/webui/test/functional/create_test.rb
@@ -14,9 +14,7 @@ class CreateProjectTest < ActionController::IntegrationTest
 
    def test_create_package
       visit '/project/show?project=home:tom'
-      assert_contain(/Packages (0)/)
-      
-      click_link 'Packages'
+      assert_contain(/Packages \(0\)/)
       assert_contain(/This project does not contain any packages/)
 
       click_link 'Create package'
@@ -38,7 +36,7 @@ class CreateProjectTest < ActionController::IntegrationTest
      assert_response :success 
 
      assert_contain 'home:tom:coolstuff'
-     assert_contain(/Packages (0)/)
+     assert_contain(/Packages \(0\)/)
    end
 end
 
