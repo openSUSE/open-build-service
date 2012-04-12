9db769dade3e57c903f8760d1ba669a278dd569c
Adrian Schröter <adrian@suse.de>
2012-04-12 10:45:53 +0200

    [api] do not crash on set_flag and unlock operations on remote packages

--- src/api/app/models/user.rb
+++ src/api/app/models/user.rb
@@ -230,6 +230,7 @@ class User < ActiveRecord::Base
 
   # package is instance of DbPackage
   def can_modify_package?(package, ignoreLock=nil)
+    return false if package.nil? # happens with remote packages easily
     unless package.kind_of? DbPackage
       raise ArgumentError, "illegal parameter type to User#can_modify_package?: #{package.class.name}"
     end
