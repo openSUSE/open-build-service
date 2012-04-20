9846fb4a8205db0d35d2fb9ee96958952ab04f52
Sascha Peilicke <saschpe@suse.de>
2012-04-20 14:00:28 +0200

    [webui] link_to_remote went the way of the dodo (together with prototype)
    add a stub method to get rid of the warnings for now

--- src/webui/app/helpers/application_helper.rb
+++ src/webui/app/helpers/application_helper.rb
@@ -413,5 +413,11 @@ module ApplicationHelper
   def is_advanced_tab?
     ["prjconf", "attributes", "meta", "status"].include? @action_name
   end
+
+  #TODO/FIXME: Port all link_to_remote to link_to(..., :remote => true), http://code.alexreisner.com/articles/link-to-remote-with-ujs.html
+  #TODO/FIXME: This method only silences the errors for now, there's more important stuff waiting...
+  def link_to_remote(body, url_options = {}, html_options = {})
+    return 'PORT link_to_remote!!!'
+  end
 end
 
