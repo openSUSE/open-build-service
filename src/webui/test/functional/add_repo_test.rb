09e55fa61198717bf730f3c3cfa8c6bf56ec40be
Stephan Kulow <coolo@suse.de>
2012-06-26 20:55:42 +0200

    [webui] some fixes for bundler

--- src/webui/test/functional/add_repo_test.rb
+++ src/webui/test/functional/add_repo_test.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + "/..") + "/test_helper"
 
 require 'webrat'
-require 'webrat/integrations/rails'
 
 class AddRepoTest < ActionController::IntegrationTest
 
