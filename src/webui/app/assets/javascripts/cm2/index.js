b839469aac0e6ad8fcb3f2c6be15ade8c33e4fdf
Stephan Kulow <coolo@suse.de>
2012-07-18 09:29:18 +0200

    [webui] do not put codemirror javascript into the default asset
    it's 100K only needed if touching files, so loading that only later sounds feasible to me

---
+++ src/webui/app/assets/javascripts/cm2/index.js
@@ -0,0 +1,58 @@
+// This is a manifest file that'll be compiled into codemirror, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require cm2/codemirror
+//= require cm2/codemirror-ui-find
+//= require cm2/codemirror-ui
+//= require cm2/mode/changes
+//= require cm2/mode/clike
+//= require cm2/mode/clojure
+//= require cm2/mode/coffeescript
+//= require cm2/mode/css
+//= require cm2/mode/diff
+//# require cm2/mode/ecl
+//# require cm2/mode/gfm
+//= require cm2/mode/go
+//= require cm2/mode/groovy
+//= require cm2/mode/haskell
+//= require cm2/mode/htmlembedded
+//= require cm2/mode/htmlmixed
+//= require cm2/mode/javascript
+//= require cm2/mode/jinja2
+//= require cm2/mode/less
+//= require cm2/mode/lua
+//= require cm2/mode/markdown
+//= require cm2/mode/mysql
+//# require cm2/mode/ntriples
+//= require cm2/mode/pascal
+//= require cm2/mode/perl
+//= require cm2/mode/php
+//# require cm2/mode/plsql
+//= require cm2/mode/prjconf
+//= require cm2/mode/properties
+//= require cm2/mode/python
+//= require cm2/mode/r
+//= require cm2/mode/rst
+//= require cm2/mode/ruby
+//# require cm2/mode/rust
+//= require cm2/mode/scheme
+//= require cm2/mode/smalltalk
+//# require cm2/mode/sparql
+//= require cm2/mode/spec
+//# require cm2/mode/stex
+//# require cm2/mode/tiddlywiki
+//# require cm2/mode/velocity
+//# require cm2/mode/verilog
+//= require cm2/mode/xml
+//= require cm2/mode/xmlpure
+//= require cm2/mode/yaml
+
