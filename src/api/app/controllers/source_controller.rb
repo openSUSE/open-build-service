705dcff0bf80451a5376927bd723e0542063aa85
Islam Amer <pharon@gmail.com>
2012-08-11 15:19:50 +0300

    [api] Try harder to preserve flags.
    Also preserve package flags in the project meta.
    
    Signed-off-by: Islam Amer <pharon@gmail.com>

--- src/api/app/controllers/source_controller.rb
+++ src/api/app/controllers/source_controller.rb
@@ -1226,25 +1226,17 @@ class SourceController < ApplicationController
       else # local project
         @project = Project.new :name => project_name, :title => oprj.title, :description => oprj.description
         @project.save
-        build_disabled = false
-        publish_disabled = false
 
         oprj.flags.each do |f|
           @project.flags.create(:status => f.status, :flag => f.flag, :architecture => f.architecture, :repo => f.repo) unless f.flag == 'lock'
-          if f.flag == "build" and f.status == "disable"
-            build_disabled = true
-          end
-          if f.flag == "publish" and f.status == "disable"
-            publish_disabled = true
-          end
         end
 
-        @project.flags.create(:status => 'disable', :flag => 'build') if not build_disabled
-        @project.flags.create(:status => 'disable', :flag => 'publish') if not publish_disabled
+        @project.flags.create(:status => 'disable', :flag => 'build')
+        @project.flags.create(:status => 'disable', :flag => 'publish')
 
         oprj.linkedprojects.each do |l|
           @project.linkedprojects.create( :linked_remote_project_name => l.linked_remote_project_name , :position => l.position ) if l.linked_remote_project_name
-          @project.linkedprojects.create( :linked_db_project => l.linked_db_project.name , :position => l.position ) if l.linked_db_project
+          @project.linkedprojects.create( :linked_db_project => Project.get_by_name(l.linked_db_project.name) , :position => l.position ) if l.linked_db_project
         end
 
         oprj.repositories.each do |repo|
@@ -1265,8 +1257,8 @@ class SourceController < ApplicationController
 
     if params.has_key? :nodelay
       @project.do_project_copy(params)
-      @project.flags.delete_if { |f| f.flag == "build" and f.status == "disable" }
-      @project.flags.delete_if { |f| f.flag == "publish" and f.status == "disable" }
+      @project.flags.delete_if { |f| f.flag == "build" and f.status == "disable" and f.architecture.nil? and f.repo.nil? and f.package.nil? }
+      @project.flags.delete_if { |f| f.flag == "publish" and f.status == "disable" and f.architecture.nil? and f.repo.nil? and f.package.nil? }
       @project.store
       render_ok
     else
