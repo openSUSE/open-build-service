a06ef2656a4efd0e677ed537ce346caf59a517f3
Adrian Schröter <adrian@suse.de>
2012-09-07 15:21:42 +0200

    [api] do not fail on deleted requests

--- src/api/lib/workers/import_requests.rb
+++ src/api/lib/workers/import_requests.rb
@@ -6,9 +6,14 @@ class ImportRequestsDelayedJob
   def perform
     require 'opensuse/backend'
 
-    lastrq =  Suse::Backend.get("/request/_lastid").body.to_i
+    lastrq = Suse::Backend.get("/request/_lastid").body.to_i
     while lastrq > 0
-      xml = Suse::Backend.get( "/request/#{lastrq}" ).body
+      begin
+        xml = Suse::Backend.get( "/request/#{lastrq}" ).body
+      rescue Suse::Backend::HTTPError
+        lastrq -= 1
+        next
+      end
       r = BsRequest.new_from_xml xml
       unless r.save
         puts "Request ##{lastrq}:", r.errors.full_messages.join("\n")
