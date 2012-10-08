a3371cf846151ca3743de09e0749a89aebbc2fa4
Stephan Kulow <coolo@suse.de>
2012-10-08 11:20:14 +0200

    [api,webui] fix logging of entries with starting newlines

--- src/api/config/initializers/logging.rb
+++ src/api/config/initializers/logging.rb
@@ -13,7 +13,7 @@ module ActiveSupport
       color = NUMBER_TO_COLOR_MAP[severity]
       message = (message || (block && block.call) || progname).to_s
       prefix=""
-      while message[0] == 13 or message[0] == 10
+      while message[0].ord == 13 or message[0].ord == 10
         prefix = prefix.concat(message[0])
         message = message[1..-1]
       end
