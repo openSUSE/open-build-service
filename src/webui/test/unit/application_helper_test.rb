82613e3cd251d58534a85d5256c80d8864106247
Lukas Ocilka <lukas.ocilka@gmail.com>
2012-10-30 16:51:28 +0000

    [webui] Speeding up the application_helper
    - Moving date computed on-the-fly to constants
    - And comes with a nice testsuite as well!

---
+++ src/webui/test/unit/application_helper_test.rb
@@ -0,0 +1,28 @@
+require File.join File.dirname(__FILE__), '..', 'test_helper'
+
+include ApplicationHelper
+include ActionView::Helpers::TagHelper
+
+class ApplicationHelperTest < ActiveSupport::TestCase
+  def test_repo_status_icon
+    # Regular
+    status = ApplicationHelper::repo_status_icon("blocked")
+    assert status.match(/icons-time/)
+    assert status.match(/No build possible atm/)
+
+    # Outdated
+    status = ApplicationHelper::repo_status_icon("outdated_scheduling")
+    assert status.match(/icons-cog_error/)
+    assert status.match(/state is being calculated/)
+    assert status.match(/needs recalculations/)
+
+    # Fallback
+    status = ApplicationHelper::repo_status_icon("undefined")
+    assert status.match(/icons-eye/)
+    assert status.match(/Unknown state/)
+  end
+
+  def test_sponsors
+    assert_not_nil get_random_sponsor_image
+  end
+end
