20a3ca7e822c32e5e91584457db6fa9d352f6b68
Lukas Ocilka <lukas.ocilka@gmail.com>
2012-10-31 09:06:52 +0000

    Testsuite adapted to the quality metrics required by code_quality test

--- src/webui/test/unit/application_helper_test.rb
+++ src/webui/test/unit/application_helper_test.rb
@@ -7,19 +7,19 @@ class ApplicationHelperTest < ActiveSupport::TestCase
   def test_repo_status_icon
     # Regular
     status = ApplicationHelper::repo_status_icon("blocked")
-    assert status.match /icons-time/
-    assert status.match /No build possible atm/
+    assert status.match(/icons-time/)
+    assert status.match(/No build possible atm/)
 
     # Outdated
     status = ApplicationHelper::repo_status_icon("outdated_scheduling")
-    assert status.match /icons-cog_error/
-    assert status.match /state is being calculated/
-    assert status.match /needs recalculations/
+    assert status.match(/icons-cog_error/)
+    assert status.match(/state is being calculated/)
+    assert status.match(/needs recalculations/)
 
     # Fallback
     status = ApplicationHelper::repo_status_icon("undefined")
-    assert status.match /icons-eye/
-    assert status.match /Unknown state/
+    assert status.match(/icons-eye/)
+    assert status.match(/Unknown state/)
   end
 
   def test_sponsors
