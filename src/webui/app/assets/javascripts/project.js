c10fa5b0467c693e90423b675a28b438776ab619
Sascha Peilicke <saschpe@suse.de>
2012-11-06 00:41:03 +0100

    Use jquery.dataTable defaults (from application.js)
    In this case it's only the default table size (i.e. 25 instead of 10).

--- src/webui/app/assets/javascripts/project.js
+++ src/webui/app/assets/javascripts/project.js
@@ -3,7 +3,6 @@ function renderPackagesTable(packages)
     var packageurl = $("#packages_table_wrapper").data("url");
     $("#packages_table_wrapper").html( '<table cellpadding="0" cellspacing="0" border="0" class="display" id="packages_table"></table>' );
     $("#packages_table").dataTable( {"aaData": packages, 
-				     "iDisplayLength": 10,
 				     "bSort": false,
 				     "bPaginate": packages.length > 12,
 				     "aoColumns": [
