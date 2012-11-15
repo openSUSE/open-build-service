fd165bb9b1db3d78cdebcd487b733ffd0166e2c3
Stephan Kulow <coolo@suse.de>
2012-11-15 13:11:12 +0100

    [api] watched project without name is nonsense

--- src/api/app/models/watched_project.rb
+++ src/api/app/models/watched_project.rb
@@ -1,5 +1,8 @@
 class WatchedProject < ActiveRecord::Base
-  belongs_to :user, :foreign_key => 'bs_user_id'
+  belongs_to :user, foreign_key: 'bs_user_id'
 
   attr_accessible :name
+
+  validates :name, presence: true
+  validates :bs_user_id, presence: true
 end
