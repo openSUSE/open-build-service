4a64c47d55bddc18d4b34cb8b9a805270f93ffd9
Stephan Kulow <coolo@suse.de>
2012-11-20 22:20:41 +0100

    [webui] test the groups with capybara

---
+++ src/webui/test/functional/group_controller_test.rb
@@ -0,0 +1,22 @@
+require File.expand_path(File.dirname(__FILE__) + "/..") + "/test_helper"        
+
+class GroupControllerTest < ActionDispatch::IntegrationTest
+
+  test "list all groups" do
+    login_tom
+    visit groups_path
+    
+    assert find(:id, 'group_table_wrapper').has_text? "Showing 1 to 2 of 2 entries"
+    find(:link, 'test_group_b').click
+    find(:id, 'content').has_text? "This group does not contain users"
+    
+    visit group_path(id: 'test_group')
+    within(:id, 'group_members_table_wrapper') do
+      page.has_text? "Showing 1 to 1 of 1 entries"
+      find(:link, 'adrian').click
+    end
+    
+    assert page.current_url.end_with? home_path(user: 'adrian')
+  end
+
+end
