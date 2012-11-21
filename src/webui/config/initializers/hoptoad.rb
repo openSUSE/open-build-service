1c08ed07862146d9a7fb942b685dc022a908dd81
Stephan Kulow <coolo@suse.de>
2012-11-21 19:42:35 +0100

    [webui] move the configuration of the errbit installation to options.yml

--- src/webui/config/initializers/hoptoad.rb
+++ src/webui/config/initializers/hoptoad.rb
@@ -1,9 +1,12 @@
 HoptoadNotifier.configure do |config|
   # Change this to some sensible data for your errbit instance
-  config.api_key = 'YOUR_ERRBIT_API_KEY'
-  config.host    = 'YOUR_ERRBIT_HOST'
-  # Remove production from this option to enable the notification
-  config.development_environments = "production development test"
+  config.api_key = CONFIG['errbit_api_key'] || 'YOUR_ERRBIT_API_KEY'
+  config.host    = CONFIG['errbit_host'] || 'YOUR_ERRBIT_HOST'
+  if CONFIG['errbit_api_key'].blank? || CONFIG['errbit_host'].blank?
+    config.development_environments = "production development test"
+  else
+    config.development_environments = "development test"  
+  end
   # We don't want to know about timeout errors, the api will tell us the real reason 
   config.ignore << Timeout::Error
   # The api sometimes sends responses without a proper "Status:..." line (when it restarts?)
