ac986ce4da2dfdc46d63d267ad7836395fb2c3b2
Stephan Kulow <coolo@suse.de>
2012-11-25 14:06:48 +0100

    [dist] someone changed factory!!!

--- dist/ci/obs_testsuite_travis_before.sh
+++ dist/ci/obs_testsuite_travis_before.sh
@@ -46,9 +46,9 @@ esac
 
 pushd `mktemp -d`
 
-wget https://api.opensuse.org/public/source/openSUSE:Factory/perl-BSSolv/libsolv-0.1.0.tar.bz2
-tar xf libsolv-0.1.0.tar.bz2
-mv libsolv-0.1.0 libsolv
+wget https://api.opensuse.org/public/source/openSUSE:Factory/perl-BSSolv/libsolv-0.2.0.tar.bz2
+tar xf libsolv-0.2.0.tar.bz2
+mv libsolv-0.2.0 libsolv
 pushd libsolv
 cmake   -DFEDORA=1 \
         -DDISABLE_SHARED=1 \
