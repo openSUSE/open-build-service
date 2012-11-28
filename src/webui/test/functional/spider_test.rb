openSUSE/open-build-service

commit 2b182595604ae657fe7a3380fc3038ea13f71ff9

[webui] Include group check in is_maintainer? method.
This is a follow-up on b9ec2cbb5

src/webui/app/models/package.rb
@@ -186,6 +186,9 @@ class Package < ActiveXML::Node
   end
 
   def is_maintainer?(user)
+    groups("maintainer").each do |group|
+      return true if user.is_in_group?(group)
+    end
     return user_has_role?(user, 'maintainer')
   end
 

src/webui/app/models/project.rb
@@ -315,6 +315,9 @@ class Project < ActiveXML::Node
   end
 
   def is_maintainer?(user)
+    groups("maintainer").each do |group|
+      return true if user.is_in_group?(group)
+    end
     return user_has_role?(user, 'maintainer')
   end
 

commit 9c8a91b75eff3bc2e87db929aa669c8e06c91e09

[webui] add a "spider as admin" test
This pointed out the regression 2b182595604ae657fe7a3380fc3038ea13f71ff9 caused

src/webui/app/controllers/package_controller.rb
@@ -601,7 +601,7 @@ class PackageController < ApplicationController
           redirect_back_or_to :action => 'add_file', :project => params[:project], :package => params[:package] and return
         end
         if !@package.save_file :filename => filename
-          flash[:error] = @package.last_error.summary
+          flash[:error] = @package.last_save_error.summary
           redirect_back_or_to :action => 'add_file', :project => params[:project], :package => params[:package] and return
         end
       end
@@ -702,7 +702,7 @@ class PackageController < ApplicationController
     @rev = params[:rev]
     @expand = params[:expand]
     @addeditlink = false
-    if @package.can_edit?( session[:login] )
+    if @package.can_edit?( @user )
       begin
         files = @package.files(@rev, @expand)
       rescue ActiveXML::Transport::Error => e

src/webui/app/helpers/project_helper.rb
@@ -16,14 +16,14 @@ module ProjectHelper
       # TODO: Port _to_remote helpers to jQuery
       status_comment_html = ERB::Util::h(comment)
       if !firstfail
-        if @project.can_edit?( session[:login] )
+        if @project.can_edit?( @user )
           status_comment_html += " ".html_safe + link_to( image_tag('comment_delete.png', :size => "16x16", :alt => 'Clear'), 
                                                           { action: :clear_failed_comment, project: @project, 
                                                             package: package, update: valid_xml_id("comment_#{package}") }, 
                                                           remote: true )
           comments_to_clear << package
         end
-      elsif @project.can_edit?( session[:login] )
+      elsif @project.can_edit?( @user )
         status_comment_html += " ".html_safe
         status_comment_html += link_to(image_tag('comment_edit.png', :alt => "Edit"), 
                                        { action: "edit_comment_form", comment: comment, 
@@ -32,7 +32,7 @@ module ProjectHelper
                                        remote: true)
       end 
     elsif firstfail
-      if @project.can_edit?( session[:login] )
+      if @project.can_edit?( @user )
         status_comment_html += " <span class='unknown_failure'>Unknown build failure ".html_safe + 
           link_to( image_tag('comment_edit.png', size: "16x16", alt: "Edit"),
                    { action: "edit_comment_form", comment: "", package: package, 

src/webui/app/models/package.rb
@@ -204,6 +204,7 @@ class Package < ActiveXML::Node
 
   def can_edit?(user)
     return false unless user
+    raise "user needs to be a User" unless user.kind_of? User
     return true if is_maintainer?(user)
     return true if p=Project.find_cached(project) and p.can_edit?(user)
   end

src/webui/app/models/project.rb
@@ -323,10 +323,7 @@ class Project < ActiveXML::Node
 
   def can_edit?(user)
     return false if not user
-    if user.class == String or user.class == ActiveXML::Node
-      user = Person.find_cached(user.to_s)
-      return false if not user
-    end
+    raise "user needs to be a User" unless user.kind_of? User
     return true if user.is_admin?
     return is_maintainer?(user)
   end

src/webui/test/functional/spider_test.rb
@@ -110,7 +110,15 @@ class SpiderTest < ActionDispatch::IntegrationTest
 
   test "spider anonymously" do
     visit "/"
-    puts page.current_url
+    @pages_to_visit = { page.current_url => [nil, nil] }
+    @pages_visited = Hash.new
+    
+    crawl
+  end
+
+  test "spider as admin" do
+    login_king
+    visit "/"
     @pages_to_visit = { page.current_url => [nil, nil] }
     @pages_visited = Hash.new
     