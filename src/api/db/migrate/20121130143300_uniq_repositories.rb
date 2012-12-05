f68392dc11965e2249c9691aaad96835256763b8
Adrian Schröter <adrian@suse.de>
2012-12-05 13:15:45 +0100

    [api] cleanup multiple respositories in one project only for local repos. We can not have a uniq key therefore unfortunatly.

--- src/api/db/migrate/20121130143300_uniq_repositories.rb
+++ src/api/db/migrate/20121130143300_uniq_repositories.rb
@@ -7,7 +7,7 @@ class UniqRepositories < ActiveRecord::Migration
       # make it case insensitive first
       execute("ALTER TABLE `repositories` CHANGE `name` `name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL");
 
-      Project.find_by_sql("SELECT DISTINCT projects.* FROM repositories AS r1, repositories AS r2 LEFT JOIN projects on r2.db_project_id = projects.id WHERE r1.db_project_id = r2.db_project_id AND r1.name = r2.name and r1.id != r2.id;").each do |prj|
+      Project.find_by_sql("SELECT DISTINCT projects.* FROM repositories AS r1, repositories AS r2 LEFT JOIN projects on r2.db_project_id = projects.id WHERE ISNULL(projects.remoteurl) AND r1.db_project_id = r2.db_project_id AND r1.name = r2.name and r1.id != r2.id;").each do |prj|
         rep_ids = {}
         prj.repositories.each do |r|
           if rep_ids.has_key? r.name
@@ -23,7 +23,8 @@ class UniqRepositories < ActiveRecord::Migration
         prj.save
       end
 
-      execute("alter table repositories ADD UNIQUE(db_project_id,name);");
+# This fails unfortunatly with repositories in remote projects
+#      execute("alter table repositories ADD UNIQUE(db_project_id,name);");
     end
 
     CONFIG['global_write_through'] = old
