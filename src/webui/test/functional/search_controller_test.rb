fbc7d6ec872e3c6717e733b55ad6c2b4d1a69260
Henne Vogelsang <hvogel@hennevogel.de>
2012-12-07 16:37:40 +0100

    [webui] Fix defaults logic in search

--- src/webui/test/functional/search_controller_test.rb
+++ src/webui/test/functional/search_controller_test.rb
@@ -10,8 +10,9 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     assert page.has_text? "Advanced"
   end
 
-  def search options
+  def search options  
     validate_search_page
+    find("#advanced_link").click
 
     options[:for]    ||= [:projects, :packages]
     options[:in]     ||= [:name]
@@ -23,7 +24,7 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     title       = page.find(:id, 'title')
     description = page.find(:id, 'description')
 
-    fill_in "Search term:", with: options[:text]
+    fill_in "search_input", with: options[:text]
 
     project.click     if options[:for].include?(:projects) != project.selected?
     package.click     if options[:for].include?(:packages) != package.selected?
@@ -31,9 +32,9 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     title.click       if options[:in].include?(:title)     != title.selected?
     description.click if options[:in].include?(:description) != description.selected?
     if options[:attribute]
-      select(options[:attribute], from: "Require attribute:")
+      select(options[:attribute], from: "attribute_list")
     end
-    find("#search-term").click
+    find("#search_button").click
 
     if options[:expect] == :success
       if !options[:text].blank?
@@ -45,19 +46,27 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
         search_details = "with attribute \"#{options[:attribute]}\""
       end
       found_text = find("div#content h3").text
-      assert_match found_text, %r{^Search Results #{search_details}\s+\(\d+\)$}, 
-      "'#{found_text}' did not match /^Search Results #{search_details}\s+\(\d+\)$/"
+      assert_match found_text, %r{^Search results #{search_details}}, 
+      "'#{found_text}' did not match /^Search results #{search_details}/"
     elsif options[:expect] == :invalid_search_text
-      assert_equal "Search String must contain at least 2 characters OR you search for an attribute.", flash_message
+      assert_equal "Search string must contain at least two characters.", flash_message
       assert_equal :alert, flash_message_type
       validate_search_page
     elsif options[:expect] == :invalid_search_options
-      #TODO: IMP
+      assert_equal "You have to search for #{options[:text]} in something. Click the advanced button...", flash_message
+      assert_equal :alert, flash_message_type
+      assert search_results.empty?
+      validate_search_page
+    elsif options[:expect] == :no_results
+      assert_equal "Your search did not return any results.", flash_message
+      assert_equal :info, flash_message_type
+      assert search_results.empty?
+      validate_search_page
     end
   end
 
   def search_results
-    raw_results = page.all("table#search_result tr")
+    raw_results = page.all("div#search_results table tr")
     raw_results.collect do |row|
       theclass = row.first("img")["class"]
       case theclass
@@ -74,13 +83,13 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     end
   end
   
-  test "find search link in footer" do
+  test "find_search_link_in_footer" do
     visit "/"
     find(:css, "div#footer a.search-link").click
     validate_search_page
   end
   
-  test "basic search functionality" do
+  test "basic_search_functionality" do
     visit '/search'
     validate_search_page
 
@@ -89,20 +98,20 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     assert page.has_link? 'kdebase'
   end
 
-  test "search by baseurl" do
+  test "search_by_baseurl" do
     visit '/search?search_text=obs://build.opensuse.org/openSUSE:Factory/standard/fd6e76cd402226c76e65438a5e3df693-bash'
     assert find('#flash-messages').has_text? "Project not found: openSUSE:Factory"
 
     visit '/search?search_text=obs://foo'
-    assert find('#flash-messages').has_text?(%{This obs:// disturl doesn't compute!})
+    assert find('#flash-messages').has_text?(%{This disturl does not compute!})
   end
 
-  test "search for home projects" do
+  test "search_for_home_projects" do
   
     visit search_path
 
     search(
-      :text => "Home", 
+      :text => "Home",
       :for  => [:projects],
       :in   => [:title])
 
@@ -119,7 +128,7 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
   end
 
 
-  test "search for subprojects" do
+  test "search_for_subprojects" do
 
     visit search_path
 
@@ -130,7 +139,7 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
 
     results = search_results
     assert results.include? :type => :project, :project_name => "home:Iggy:branches:kde4"
-    assert results.count == 1
+    assert_equal 1, results.count
   end
 
 
@@ -143,8 +152,9 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
       :for  => [:projects], 
       :in   => [:name])
 
-    assert search_results.include? :type => :project, :project_name => "LocalProject"
-    assert search_results.count == 1
+    results = search_results
+    assert results.include? :type => :project, :project_name => "LocalProject"
+    assert_equal 1, results.count
   end
 
 
@@ -180,29 +190,27 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
   end
 
 
-  test "search non existing by name" do
+  test "search_non_existing_by_name" do
 
     visit search_path
   
     search(
       :text => "no such name, please!", 
       :for  => [:projects, :packages], 
-      :in   => [:name])
-
-    assert search_results.empty?
+      :in   => [:name],
+      :expect => :no_results)
   end
 
 
-  test "search non existing by title" do
+  test "search_non_existing_by_title" do
 
     visit search_path
 
     search(
       :text => "Perhaps a non-existing title.", 
       :for  => [:projects, :packages], 
-      :in   => [:title])
-
-    assert search_results.empty?
+      :in   => [:title],
+      :expect => :no_results)
   end
 
 
@@ -213,22 +221,20 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     search(
       :text => "Some non-existing description I hope.", 
       :for  => [:projects, :packages], 
-      :in   => [:description])
-
-    assert search_results.empty?
+      :in   => [:description],
+      :expect => :no_results)
   end
 
 
-  test "search non existing by attributes" do
+  test "search_non_existing_by_attributes" do
     visit search_path
 
     search(
       :text => "", 
       :for  => [:projects, :packages], 
       :in   => [],
-      :attribute => "OBS:RequestCloned")
-
-    assert search_results.empty?
+      :attribute => "OBS:RequestCloned",
+      :expect => :no_results)
   end
 
   test "search_for_nothing" do
@@ -236,16 +242,18 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
 
     search(
       :text => "Some empty search.", 
-      :for  => [], 
-      :in   => [:name, :title, :description])
-
-    assert search_results.empty?
+      :for  => [:projects, :packages], 
+      :in   => [:name, :title, :description],
+      :expect => :no_results)
   end
   
   test "search russian" do
     visit search_path
     
-    search(text: "вокябюч", :for  => [:projects, :packages], :in   => [:name, :title, :description])
+    search(
+      text: "вокябюч",
+      :for  => [:projects, :packages],
+      :in   => [:name, :title, :description])
     
     results = search_results
     assert page.has_text? "Этёам вокябюч еюж эи"
