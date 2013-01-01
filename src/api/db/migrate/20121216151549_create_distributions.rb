cae0a900bc1b05808e32fb10b75252dec544ab05
Johannes Obermayr <johannesobermayr@gmx.de>
2013-01-01 23:58:40 +0100

    Fix rake db:migrate.

--- src/api/db/migrate/20121216151549_create_distributions.rb
+++ src/api/db/migrate/20121216151549_create_distributions.rb
@@ -1,34 +1,43 @@
 class CreateDistributions < ActiveRecord::Migration
   def up
-    create_table :distributions do |t|
-      t.string :vendor, null: false
-      t.string :version, null: false
-      t.string :name, null: false
-      t.string :project, null: false
-      t.string :reponame, null: false
-      t.string :repository, null: false
-      t.string :link
+    
+    unless table_exists?("distributions")
+      create_table :distributions do |t|
+        t.string :vendor, null: false
+        t.string :version, null: false
+        t.string :name, null: false
+        t.string :project, null: false
+        t.string :reponame, null: false
+        t.string :repository, null: false
+        t.string :link
+      end
     end
 
-    create_table :distribution_icons do |t|
-      t.string :url, null: false
-      t.integer :width
-      t.integer :height
+    unless table_exists?("distribution_icons")
+      create_table :distribution_icons do |t|
+        t.string :url, null: false
+        t.integer :width
+        t.integer :height
+      end
     end
 
     # Create JOIN-table
-    create_table :distribution_icons_distributions do |t|
-      t.integer :distribution_id
-      t.integer :distribution_icon_id
+    unless table_exists?("distribution_icons_distributions")
+      create_table :distribution_icons_distributions do |t|
+        t.integer :distribution_id
+        t.integer :distribution_icon_id
+      end
     end
 
     path = Rails.root.join("files", "distributions.xml")
-    begin
-      req = Xmlhash.parse(File.read(path))
-      if req
-        Distribution.parse(req)
+    if File.exists?(path)
+      begin
+        req = Xmlhash.parse(File.read(path))
+        if req
+          Distribution.parse(req)
+        end
+      rescue IOError
       end
-    rescue IOError
     end
   end
 
