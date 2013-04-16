84e1026866838ed6265c2fcf60a3002c7db8518b
Stephan Kulow <coolo@suse.de>
2013-04-16 13:12:09 +0200

    [api] refactor status controller
    move function to find a buildstatus of a specific package revision into the package model

--- src/api/app/models/package.rb
+++ src/api/app/models/package.rb
@@ -994,4 +994,145 @@ class Package < ActiveRecord::Base
   def valid_name
     errors.add(:name, "is illegal") unless Package.valid_name?(self.name)
   end
+
+  class NoRepositoriesFound < APIException
+    setup "No repositories build against target", 404
+  end
+
+  class FailedToRetrieveBuildInfo < APIException
+    setup 404
+  end
+
+  def buildstatus(opts)
+
+    tproj  = opts[:target_project]
+    srcmd5 = opts[:srcmd5]
+
+    # check current srcmd5
+    begin
+      cdir = Directory.find(:project => self.project,
+                            :package => self.name,
+                            :expand  => 1)
+      csrcmd5 = cdir.value('srcmd5') if cdir
+    rescue ActiveXML::Transport::Error => e
+      csrcmd5 = nil
+    end
+
+    tocheck_repos = self.project.repositories_linking_project(tproj, ActiveXML.transport)
+    raise NoRepositoriesFound.new if tocheck_repos.empty?
+
+    output = {}
+    tocheck_repos.each do |srep|
+      output[srep.name] ||= {}
+      trepo             = []
+      archs             = []
+      srep.each_path do |p|
+        if p.project != self.project.name
+          r = Repository.find_by_project_and_repo_name(p.project, p.value(:repository))
+          r.architectures.each { |a| archs << a.name }
+          trepo << [p.project, p.value(:repository)]
+        end
+      end
+      archs.uniq!
+      if !trepo or trepo.nil?
+        raise NoRepositoriesFound.new "Can not find repository building against target"
+      end
+
+      tpackages = Hash.new
+      vprojects = Hash.new
+      trepo.each do |p, r|
+        next if vprojects.has_key? p
+        Project.find_by_name(p).packages.select(:name).each { |n| tpackages[n.name] = p }
+        vprojects[p] = 1
+      end
+
+      archs.each do |arch|
+        everbuilt     = 0
+        eversucceeded = 0
+        buildcode     =nil
+        hist          = Jobhistory.find(:project    => self.project.name,
+                                        :repository => srep.name,
+                                        :package    => self.name,
+                                        :arch       => arch.to_s, :limit => 20)
+        next unless hist
+        hist.each_jobhist do |jh|
+          next if jh.srcmd5 != srcmd5
+          everbuilt = 1
+          if jh.code == 'succeeded' || jh.code == 'unchanged'
+            buildcode     ='succeeded'
+            eversucceeded = 1
+            break
+          end
+        end
+        logger.debug "arch:#{arch} md5:#{srcmd5} successed:#{eversucceeded} built:#{everbuilt}"
+        missingdeps=[]
+        if eversucceeded == 1
+          uri = URI("/build/#{CGI.escape(self.project.name)}/#{CGI.escape(srep.name)}/#{CGI.escape(arch.to_s)}/_builddepinfo?package=#{CGI.escape(self.name)}&view=pkgnames")
+          begin
+            buildinfo = Xmlhash.parse(ActiveXML.transport.direct_http(uri))
+          rescue ActiveXML::Transport::Error => e
+            # if there is an error, we ignore
+            raise FailedToRetrieveBuildInfo.new "Can't get buildinfo: #{e.summary}"
+          end
+
+          buildinfo["package"].elements("pkgdep") do |b|
+            unless tpackages.has_key? b
+              missingdeps << b
+            end
+          end
+
+        end
+
+        # if the package does not appear in build history, check flags
+        if everbuilt == 0
+          buildflag=self.find_flag_state("build", srep.name, arch.to_s)
+          logger.debug "find_flag_state #{srep.name} #{arch.to_s} #{buildflag}"
+          if buildflag == 'disable'
+            buildcode='disabled'
+          end
+        end
+
+        if !buildcode && srcmd5 != csrcmd5 && everbuilt == 1
+          buildcode='failed' # has to be
+        end
+
+        unless buildcode
+          buildcode="unknown"
+          begin
+            uri         = URI("/build/#{CGI.escape(self.project.name)}/_result?package=#{CGI.escape(self.name)}&repository=#{CGI.escape(srep.name)}&arch=#{CGI.escape(arch.to_s)}")
+            resultlist  = ActiveXML::Node.new(ActiveXML.transport.direct_http(uri))
+            currentcode = nil
+            resultlist.each_result do |r|
+              r.each_status { |s| currentcode = s.value(:code) }
+            end
+          rescue ActiveXML::Transport::Error
+            currentcode = nil
+          end
+          if ['unresolvable', 'failed', 'broken'].include?(currentcode)
+            buildcode='failed'
+          end
+          if ['building', 'scheduled', 'finished', 'signing', 'blocked'].include?(currentcode)
+            buildcode='building'
+          end
+          if currentcode == 'excluded'
+            buildcode='excluded'
+          end
+          # if it's currently succeeded but !everbuilt, it's different sources
+          if currentcode == 'succeeded'
+            if srcmd5 == csrcmd5
+              buildcode='building' # guesssing
+            else
+              buildcode='outdated'
+            end
+          end
+        end
+
+        output[srep.name][arch.to_s] = { result: buildcode }
+        output[srep.name][arch.to_s][:missing] = missingdeps.uniq if (missingdeps.size > 0 && buildcode == 'succeeded')
+      end
+    end
+
+    output
+  end
+
 end
