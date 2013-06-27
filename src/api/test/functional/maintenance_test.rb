0e3c94a666071f63fd04c6bc8d4a24bbf96d9dc4
Adrian Schröter <adrian@suse.de>
2013-06-27 15:41:10 +0200

    [api] our build roots do not have /etc/SuSE-release, but /etc/init.d/boot.local :)

--- src/api/test/functional/maintenance_test.rb
+++ src/api/test/functional/maintenance_test.rb
@@ -1290,7 +1290,8 @@ class MaintenanceTests < ActionDispatch::IntegrationTest
     assert_equal "package(x86-64)", pac["format"]["rpm:provides"]['rpm:entry'][2]['name']
     assert_equal "something", pac["format"]["rpm:conflicts"]['rpm:entry']['name']
     assert_equal "old_crap", pac["format"]["rpm:obsoletes"]['rpm:entry']['name']
-    if File.exist? "/etc/SuSE-release"
+    if File.exist? "/etc/init.d/boot.local"
+      # seems to be a SUSE system
       assert_equal "pure_optional", pac["format"]["rpm:suggests"]['rpm:entry']['name']
       assert_equal "would_be_nice", pac["format"]["rpm:recommends"]['rpm:entry']['name']
       assert_equal "other_package_likes_it", pac["format"]["rpm:supplements"]['rpm:entry']['name']
