18fedb480f21fd3052e68d34c7287a8323737e8c
Dirk Mueller <dmueller@suse.com>
2013-07-04 10:57:03 +0200

    [backend] Add armv6hl
    Adds support for armv6hl, which is an ARMv6 little endian hard float
    target.

--- src/backend/BSCando.pm
+++ src/backend/BSCando.pm
@@ -33,10 +33,10 @@ package BSCando;
 our %cando = (
   'aarch64'  => [ 'aarch64' ],
 
-  'armv4l'  => [ 'armv4l'                                                                                      ],
-  'armv5l'  => [ 'armv4l', 'armv5l'                    , 'armv5el'                                             ],
-  'armv6l'  => [ 'armv4l', 'armv5l', 'armv6l'          , 'armv5el', 'armv6el'                                  ],
-  'armv7l'  => [ 'armv4l', 'armv5l', 'armv6l', 'armv7l', 'armv5el', 'armv6el', 'armv7el', 'armv7hl', 'armv8el' ],
+  'armv4l'  => [ 'armv4l'                                                                                                 ],
+  'armv5l'  => [ 'armv4l', 'armv5l'                    , 'armv5el'                                                        ],
+  'armv6l'  => [ 'armv4l', 'armv5l', 'armv6l'          , 'armv5el', 'armv6el'                                             ],
+  'armv7l'  => [ 'armv4l', 'armv5l', 'armv6l', 'armv7l', 'armv5el', 'armv6el', 'armv6hl', 'armv7el', 'armv7hl', 'armv8el' ],
 
   'sh4'     => [ 'sh4' ],
 
