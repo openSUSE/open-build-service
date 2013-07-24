da1cb227a42b926e0eeefaae7c67e2903e17a869
Stephan Kulow <coolo@suse.de>
2013-07-24 10:02:14 +0200

    [api] remove duplicated test and fix one (invalids are 400 and not 404)

--- src/api/test/functional/maintenance_test.rb
+++ src/api/test/functional/maintenance_test.rb
@@ -1373,12 +1373,8 @@ class MaintenanceTests < ActionDispatch::IntegrationTest
   def test_create_invalid_patchinfo
     prepare_request_with_user "tom", "thunder"
     # collons in patchinfo names are not allowed but common mistake
-    post "https://api.opensuse.org/source/home:tom?cmd=createpatchinfo&force=1&name=home:tom"
+    post "/source/home:tom?cmd=createpatchinfo&force=1&name=home:tom"
     assert_response 400
-    assert_xml_tag :tag => "status", :attributes => { :code => "invalid_package_name" }
-
-    post "https://api.opensuse.org/source/home:tom?cmd=createpatchinfo&force=1&name=home:tom"
-    assert_response 404
     assert_xml_tag :tag => "status", :attributes => { :code => "invalid_package_name" }
   end
 
