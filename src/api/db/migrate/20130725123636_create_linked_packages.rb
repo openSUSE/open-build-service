4ddfc5f86b1efc648326ee63f103445ad9eda3c6
Stephan Kulow <coolo@suse.de>
2013-07-28 12:00:47 +0200

    [api] fix the latest migration to the actual code
    I screwed that up and test suite don't test migrations ;(

--- src/api/db/migrate/20130725123636_create_linked_packages.rb
+++ src/api/db/migrate/20130725123636_create_linked_packages.rb
@@ -3,7 +3,6 @@ class CreateLinkedPackages < ActiveRecord::Migration
     create_table :linked_packages, id: false do |t|
       t.belongs_to :links_to, null: false
       t.belongs_to :package, null: false
-      t.integer :event, null: false
       t.datetime :updated_at
       t.primary_key :package_id
     end
