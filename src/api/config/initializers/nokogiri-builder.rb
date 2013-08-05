563cfdeb89d13f9f7e2efe03ad756b7e3d172151
Stephan Kulow <coolo@suse.de>
2013-08-05 12:00:20 +0200

    [api] move XML renderer to views where easily possible

--- src/api/config/initializers/nokogiri-builder.rb
+++ src/api/config/initializers/nokogiri-builder.rb
@@ -11,7 +11,8 @@ module ActionView
       def call(template)
         "xml = ::Nokogiri::XML::Builder.new { |xml|" +
           template.source +
-          "}.to_xml;"
+          "}.to_xml :indent => 2, :encoding => 'UTF-8',
+            :save_with => Nokogiri::XML::Node::SaveOptions::NO_DECLARATION | Nokogiri::XML::Node::SaveOptions::FORMAT;"
       end
       
     end    
