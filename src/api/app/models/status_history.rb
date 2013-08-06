f92fef0077cc93d47097e5e7616b178123304555
Stephan Kulow <coolo@suse.de>
2013-08-06 09:09:24 +0200

    [api] move code around in statushistory to make the functions less complex

--- src/api/app/models/status_history.rb
+++ src/api/app/models/status_history.rb
@@ -7,32 +7,41 @@ class StatusHistory < ActiveRecord::Base
       StatusHistory.where("time < ?", maxtime-365*24*3600).delete_all
     end
 
-    def cleanup(key, offset, maxtimeoffset)
-
-      # we try to make sure all keys are in the same time slots, so start with the overall time
+    def find_items_for_maxtime(offset, maxtimeoffset)
       maxtime = StatusHistory.maximum(:time)
       maxtime -= maxtimeoffset
       maxtime = (maxtime / offset) * offset
 
+      StatusHistory.where('`key` = ? and `time` < ?', key, maxtime).order(:time).to_a
+    end
+
+    def find_start_items(allitems, max)
+      items = []
+
+      while allitems.length > 0 && allitems[0].time < max do
+        items << allitems.shift
+      end
+      return items
+    end
+
+    def cleanup(key, offset, maxtimeoffset)
+
+      # we try to make sure all keys are in the same time slots, so start with the overall time
+      allitems = find_items_for_maxtime(offset, maxtimeoffset)
+      return if allitems.empty?
+
       mintime = StatusHistory.minimum(:time)
-      mintime = (mintime / offset) * offset
+      curmintime = (mintime / offset) * offset
 
-      allitems = StatusHistory.where('`key` = ? and `time` < ?', key, maxtime).order(:time).to_a
-      return unless allitems.length > 0
-      curmintime = mintime
-      while allitems.length > 0
-        items = []
-
-        while allitems.length > 0 && allitems[0].time < curmintime + offset do
-          items << allitems.shift
-        end
+      while !allitems.empty?
+        items = find_start_items(allitems, curmintime + offset)
 
         if items.length > 1
           timeavg = curmintime + offset / 2
           valuavg = (items.inject(0) { |sum, item| sum+item.value }).to_f / items.length
           Rails.logger.debug "scaling #{key} #{curmintime} #{items.length} #{Time.at(timeavg)} #{valuavg}"
           StatusHistory.delete(items.map { |i| i.id })
-          StatusHistory.create :key => key, :time => timeavg, :value => valuavg
+          StatusHistory.create key: key, time: timeavg, value: valuavg
         end
         curmintime += offset
       end
