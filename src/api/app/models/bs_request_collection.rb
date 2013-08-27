8b6dde13c471d5869d2b101a57da9a1d5c77cfc4
Stephan Kulow <coolo@suse.de>
2013-08-27 20:19:59 +0200

    [api] refactor BsRequestCollection further

--- src/api/app/models/bs_request_collection.rb
+++ src/api/app/models/bs_request_collection.rb
@@ -1,37 +1,17 @@
 class BsRequestCollection
 
-  def extend_relation_for_project(source_or_target)
-    if @roles.count == 0 or @roles.include? source_or_target
-      @rel = @rel.references(:bs_request_actions)
-      if @subprojects.blank?
-        @inner_or << "bs_request_actions.#{source_or_target}_project=#{quote(@project)}"
-      else
-        @inner_or << "(bs_request_actions.#{source_or_target}_project like #{quote(@project + ':%')})"
-      end
-    end
-  end
-
-  def quote(str)
-    conn.quote(str)
-  end
-  
-  def conn
-    @conn ||= ActiveRecord::Base.connection  
-  end
-  
   def initialize(opts)
     @roles = opts[:roles] || []
-    states = opts[:states] || []
+    @states = opts[:states] || []
     types = opts[:types] || []
-    review_states = opts[:review_states] || %w(new)
+    @review_states = opts[:review_states] || %w(new)
     @subprojects = opts[:subprojects]
     @project = opts[:project]
-    
     @rel = BsRequest.joins(:bs_request_actions)
 
     # filter for request state(s)
-    unless states.blank?
-      @rel = @rel.where('bs_requests.state in (?)', states).references(:bs_requests)
+    unless @states.blank?
+      @rel = @rel.where('bs_requests.state in (?)', @states).references(:bs_requests)
     end
 
     # Filter by request type (submit, delete, ...)
@@ -39,140 +19,21 @@ class BsRequestCollection
       @rel = @rel.where('bs_request_actions.type in (?)', types).references(:bs_request_actions)
     end
 
-    unless opts[:project].blank?
-      @inner_or = []
-
-      if opts[:package].blank?
-        extend_relation_for_project('source')
-        extend_relation_for_project('target')
-
-        if @roles.count == 0 or @roles.include? 'reviewer'
-          if states.count == 0 or states.include? 'review'
-            @rel = @rel.references(:reviews)
-            review_states.each do |r|
-              @rel = @rel.includes(:reviews)
-              @inner_or << "(reviews.state=#{quote(r)} and reviews.by_project=#{quote(opts[:project])})"
-            end
-          end
-        end
-      else
-        if @roles.count == 0 or @roles.include? 'source'
-          @rel = @rel.references(:bs_request_actions)
-          @inner_or << "(bs_request_actions.source_project=#{quote(opts[:project])} and bs_request_actions.source_package=#{quote(opts[:package])})"
-        end
-        if @roles.count == 0 or @roles.include? 'target'
-          @rel = @rel.references(:bs_request_actions)
-          @inner_or << "(bs_request_actions.target_project=#{quote(opts[:project])} and bs_request_actions.target_package=#{quote(opts[:package])})"
-        end
-        if @roles.count == 0 or @roles.include? 'reviewer'
-          if states.count == 0 or states.include? 'review'
-            @rel = @rel.references(:reviews)
-            review_states.each do |r|
-              @rel = @rel.includes(:reviews)
-              @inner_or << "(reviews.state=#{quote(r)} and reviews.by_project=#{quote(opts[:project])} and reviews.by_package=#{quote(opts[:package])})"
-            end
-          end
-        end
-      end
-
-      if @inner_or.count > 0
-        @rel = @rel.where(@inner_or.join(' or '))
-      end
+    unless @project.blank?
+      @package = opts[:package]
+      wrapper_for_inner_or { extend_query_for_project }
     end
 
     if opts[:user]
-      @inner_or = []
-      user = User.get_by_login(opts[:user])
-      # user's own submitted requests
-      if @roles.count == 0 or @roles.include? 'creator'
-        @inner_or << "bs_requests.creator = #{quote(user.login)}"
-      end
-
-      # find requests where user is maintainer in target project
-      if @roles.count == 0 or @roles.include? 'maintainer'
-        names = user.involved_projects.map { |p| p.name }
-        @rel = @rel.references(:bs_request_actions)
-        @inner_or << "bs_request_actions.target_project in ('" + names.join("','") + "')"
-
-        ## find request where user is maintainer in target package, except we have to project already
-        user.involved_packages.each do |ip|
-          @rel = @rel.references(:bs_request_actions)
-          @inner_or << "(bs_request_actions.target_project='#{ip.project.name}' and bs_request_actions.target_package='#{ip.name}')"
-        end
-      end
-
-      if @roles.count == 0 or @roles.include? 'reviewer'
-        @rel = @rel.includes(:reviews).references(:reviews)
-        review_states.each do |r|
-
-          # requests where the user is reviewer or own requests that are in review by someone else
-          or_in_and = ["reviews.by_user=#{quote(user.login)}"]
-          # include all groups of user
-          usergroups = user.groups.map { |g| "'#{g.title}'" }
-          or_in_and << "reviews.by_group in (#{usergroups.join(',')})" unless usergroups.blank?
-
-          # find requests where user is maintainer in target project
-          userprojects = user.involved_projects.select('projects.name').map { |p| "'#{p.name}'" }
-          or_in_and << "reviews.by_project in (#{userprojects.join(',')})" unless userprojects.blank?
-
-          ## find request where user is maintainer in target package, except we have to project already
-          user.involved_packages.select('name,db_project_id').includes(:project).each do |ip|
-            or_in_and << "(reviews.by_project='#{ip.project.name}' and reviews.by_package='#{ip.name}')"
-          end
-
-          @inner_or << "(reviews.state=#{quote(r)} and (#{or_in_and.join(' or ')}))"
-        end
-      end
-
-      unless @inner_or.empty?
-        @rel = @rel.where(@inner_or.join(' or '))
-      end
+      wrapper_for_inner_or { extend_query_for_user(opts[:user]) }
     end
 
     if opts[:group]
-      @inner_or = []
-      group = Group.get_by_title(opts[:group])
-
-      # find requests where group is maintainer in target project
-      if @roles.count == 0 or @roles.include? 'maintainer'
-        names = group.involved_projects.map { |p| p.name }
-        @rel = @rel.references(:bs_request_actions)
-        @inner_or << "bs_request_actions.target_project in ('" + names.join("','") + "')"
-
-        ## find request where group is maintainer in target package, except we have to project already
-        group.involved_packages.each do |ip|
-          @inner_or << "(bs_request_actions.target_project='#{ip.project.name}' and bs_request_actions.target_package='#{ip.name}')"
-        end
-      end
-
-      if @roles.count == 0 or @roles.include? 'reviewer'
-        @rel = @rel.includes(:reviews).references(:reviews)
-
-        review_states.each do |r|
-
-          # requests where the user is reviewer or own requests that are in review by someone else
-          or_in_and = ["reviews.by_group='#{group.title}'"]
-
-          # find requests where group is maintainer in target project
-          groupprojects = group.involved_projects.select('projects.name').map { |p| "'#{p.name}'" }
-          or_in_and << "reviews.by_project in (#{groupprojects.join(',')})" unless groupprojects.blank?
-
-          ## find request where user is maintainer in target package, except we have to project already
-          group.involved_packages.select('name,db_project_id').includes(:project).each do |ip|
-            or_in_and << "(reviews.by_project='#{ip.project.name}' and reviews.by_package='#{ip.name}')"
-          end
-
-          @inner_or << "(reviews.state='#{r}' and (#{or_in_and.join(' or ')}))"
-        end
-      end
-
-      unless @inner_or.empty?
-        @rel = @rel.where(@inner_or.join(' or '))
-      end
+      wrapper_for_inner_or { extend_query_for_group(opts[:group]) }
     end
 
     if opts[:ids]
-      @rel = @rel.where(:id => opts[:ids])
+      @rel = @rel.where(id: opts[:ids])
     end
   end
 
@@ -182,6 +43,128 @@ class BsRequestCollection
 
   def relation
     @rel
+  end
+
+  private
+
+  def extend_query_for_maintainer(obj)
+    if @roles.count == 0 or @roles.include? 'maintainer'
+      names = obj.involved_projects.pluck('name').map { |p| quote(p) }
+      @rel = @rel.references(:bs_request_actions)
+      @inner_or << "bs_request_actions.target_project in (#{names.join(',')})" unless names.empty?
+
+      ## find request where group is maintainer in target package, except we have to project already
+      obj.involved_packages.each do |ip|
+        @inner_or << "(bs_request_actions.target_project='#{ip.project.name}' and bs_request_actions.target_package='#{ip.name}')"
+      end
+    end
+  end
+
+  def wrapper_for_inner_or
+    @inner_or = []
+    yield
+    unless @inner_or.empty?
+      @rel = @rel.where(@inner_or.join(' or '))
+    end
+  end
+
+  def extend_query_for_involved_reviews(obj, or_in_and)
+    @review_states.each do |r|
+      # find requests where obj is maintainer in target project
+      projects = obj.involved_projects.pluck('projects.name').map { |p| quote(p) }
+      or_in_and << "reviews.by_project in (#{projects.join(',')})" unless projects.blank?
+
+      ## find request where user is maintainer in target package, except we have to project already
+      obj.involved_packages.select('name,db_project_id').includes(:project).each do |ip|
+        or_in_and << "(reviews.by_project='#{ip.project.name}' and reviews.by_package='#{ip.name}')"
+      end
+
+      @inner_or << "(reviews.state=#{quote(r)} and (#{or_in_and.join(' or ')}))"
+    end
+
+  end
+
+  def extend_query_for_group(group)
+    group = Group.get_by_title(group)
+
+    # find requests where group is maintainer in target project
+    extend_query_for_maintainer(group)
+
+    if @roles.count == 0 or @roles.include? 'reviewer'
+      @rel = @rel.includes(:reviews).references(:reviews)
+      # requests where the user is reviewer or own requests that are in review by someone else
+      or_in_and = ["reviews.by_group=#{quote(group.title)}"]
+
+      extend_query_for_involved_reviews(group, or_in_and)
+    end
+  end
+
+  def extend_query_for_user(user)
+    user = User.get_by_login(user)
+
+    # user's own submitted requests
+    if @roles.count == 0 or @roles.include? 'creator'
+      @inner_or << "bs_requests.creator = #{quote(user.login)}"
+    end
+
+    # find requests where user is maintainer in target project
+    extend_query_for_maintainer(user)
+
+    if @roles.count == 0 or @roles.include? 'reviewer'
+      @rel = @rel.includes(:reviews).references(:reviews)
+
+      # requests where the user is reviewer or own requests that are in review by someone else
+      or_in_and = ["reviews.by_user=#{quote(user.login)}"]
+
+      # include all groups of user
+      usergroups = user.groups.map { |g| "'#{g.title}'" }
+      or_in_and << "reviews.by_group in (#{usergroups.join(',')})" unless usergroups.blank?
+
+      extend_query_for_involved_reviews(user, or_in_and)
+    end
+  end
+
+  def extend_relation(source_or_target)
+    if @roles.count == 0 or @roles.include? source_or_target
+      @rel = @rel.references(:bs_request_actions)
+      if @package.blank?
+        if @subprojects.blank?
+          @inner_or << "bs_request_actions.#{source_or_target}_project=#{quote(@project)}"
+        else
+          @inner_or << "(bs_request_actions.#{source_or_target}_project like #{quote(@project + ':%')})"
+        end
+      else
+        @inner_or << "(bs_request_actions.#{source_or_target}_project=#{quote(@project)} and " +
+            "bs_request_actions.#{source_or_target}_package=#{quote(@package)})"
+      end
+    end
+  end
+
+  def quote(str)
+    conn.quote(str)
+  end
+
+  def conn
+    @conn ||= ActiveRecord::Base.connection
+  end
+
+  def extend_query_for_project
+    extend_relation('source')
+    extend_relation('target')
+
+    if @roles.count == 0 or @roles.include? 'reviewer'
+      if @states.count == 0 or @states.include? 'review'
+        @rel = @rel.references(:reviews)
+        @review_states.each do |r|
+          @rel = @rel.includes(:reviews)
+          if @package.blank?
+            @inner_or << "(reviews.state=#{quote(r)} and reviews.by_project=#{quote(@project)})"
+          else
+            @inner_or << "(reviews.state=#{quote(r)} and reviews.by_project=#{quote(@project)} and reviews.by_package=#{quote(@package)})"
+          end
+        end
+      end
+    end
   end
 
 end
