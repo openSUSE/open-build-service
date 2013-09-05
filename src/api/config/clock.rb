b0ac6187588626b385a393760bf33c40bc242da4
Stephan Kulow <coolo@suse.de>
2013-09-05 21:03:07 +0200

    [api] avoid leaking connections in threads
    I assume this is a bug in clockwork, but it's easy to work around

--- src/api/config/clock.rb
+++ src/api/config/clock.rb
@@ -33,9 +33,10 @@ every(49.minutes, 'rescale history') do
   StatusHistory.first.delay.rescale
 end
 
-every(1.day, 'optimize history', thread: true) do
-  sql = ActiveRecord::Base.connection
-  sql.execute "optimize table status_histories;"
+every(1.day, 'optimize history', thread: true) do 
+  ActiveRecord::Base.connection_pool.with_connection do |sql|
+    sql.execute "optimize table status_histories;"
+  end
 end
 
 every(5.minutes, 'check last events') do
@@ -50,17 +51,21 @@ every(30.seconds, 'send notifications') do
 end
 
 every(17.seconds, 'fetch notificatoins', thread: true) do
-  # this will return if there is already a thread running
-  UpdateNotificationEvents.new.perform
+  ActiveRecord::Base.connection_pool.with_connection do |sql| 
+    # this will return if there is already a thread running
+    UpdateNotificationEvents.new.perform
+  end
 end
 
 # We want Sphinx to be started everytime clockworkd starts. Scheduling a restart
 # every week ensures that initial start and doesn't really hurt. Not the
 # cleanest solution, but avoids creating/modifying init.d scripts
 every(1.week, 're(start) sphinx', thread: true) do
-  interface = ThinkingSphinx::RakeInterface.new
-  interface.stop
-  interface.start
+  ActiveRecord::Base.connection_pool.with_connection do |sql|
+    interface = ThinkingSphinx::RakeInterface.new
+    interface.stop
+    interface.start
+  end
 end
 
 every(1.hour, 'reindex sphinx', thread: true) do
