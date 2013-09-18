182ce38e3a644c715dbd9c4b30b40872df52204c
Adrian Schröter <adrian@suse.de>
2013-09-18 15:42:20 +0200

    [api] make /about public available. it is used for live checks and is a route causing minimal load.

--- src/api/app/controllers/about_controller.rb
+++ src/api/app/controllers/about_controller.rb
@@ -1,6 +1,7 @@
 class AboutController < ApplicationController
 
   validate_action :index => {:method => :get, :response => :about}
+  skip_before_action :extract_user
 
   def index
     @api_revision = CONFIG['version'].to_s
--- src/api/test/functional/about_controller_test.rb
+++ src/api/test/functional/about_controller_test.rb
@@ -2,17 +2,21 @@ require File.expand_path(File.dirname(__FILE__) + "/..") + "/test_helper"
 
 class AboutControllerTest < ActionDispatch::IntegrationTest 
 
-  def setup
-    prepare_request_valid_user
-  end
- 
   def test_about
+    prepare_request_valid_user
+    get "/about"
+    assert_response :success
+    assert_xml_tag( :tag => "about", :descendant => { :tag => "revision" } )
+  end
+
+  def test_about_anonymous
     get "/about"
     assert_response :success
     assert_xml_tag( :tag => "about", :descendant => { :tag => "revision" } )
   end
 
   def test_application_controller
+    prepare_request_valid_user
     get "/about?user[asd]=yxc"
     assert_response 400
     assert_xml_tag( :tag => "status", :attributes => { :code => "invalid_parameter" } )
