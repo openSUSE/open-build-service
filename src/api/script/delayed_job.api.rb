f42fc7bfd73302f82168c13e87fe7ea9796b6c91
Stephan Kulow <coolo@suse.de>
2013-10-09 09:02:55 +0200

    [api] remove no longer existant job file

--- src/api/script/delayed_job.api.rb
+++ src/api/script/delayed_job.api.rb
@@ -2,7 +2,6 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
 require 'delayed/command'
-require 'workers/issue_trackers_to_backend_job.rb'
 require 'workers/import_requests.rb'
 require 'workers/update_issues.rb'
 Delayed::Command.new(ARGV).daemonize
