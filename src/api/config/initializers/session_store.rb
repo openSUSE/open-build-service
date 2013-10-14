e90efb295b5ae4cf00ae7f17b1dc9a7f906d7b1a
Stephan Kulow <coolo@suse.de>
2013-10-14 19:32:56 +0200

    [api] make use memcached in test environment
    but make sure not to reuse old caches

--- src/api/config/initializers/session_store.rb
+++ src/api/config/initializers/session_store.rb
@@ -1,6 +1,2 @@
 # cookies are too small and active record sessions cause too much load
-if Rails.env.test?
-  Rails.application.config.session_store ActionDispatch::Session::CookieStore
-else
-  Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.day
-end
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.day
