5342cba3528e2c65dba337383b08917ecdc184e0
Victor Pereira <vpereira@web.de>
2013-10-15 16:05:20 +0200

    Update has_ratings.rb
    one more divide by zero corner

--- src/api/app/mixins/has_ratings.rb
+++ src/api/app/mixins/has_ratings.rb
@@ -14,7 +14,7 @@ module HasRatings
     end
     count = self.ratings.length
     score = score.to_f
-    score /= count
+    score /= (count.nonzero? || 1)
     score = -1 if score.nan?
     score = (score * 100).round.to_f / 100
     if user_rating = self.ratings.find_by_user_id(user_id)
