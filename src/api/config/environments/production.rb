a5edd9854f3e83f738d8be94fe11c8c7736b51be
Ancor Gonzalez Sosa <ancor@suse.de>
2013-10-17 15:39:14 +0200

    [api] Sane configuration params in production environment

--- src/api/config/environments/production.rb
+++ src/api/config/environments/production.rb
@@ -25,6 +25,25 @@ OBSApi::Application.configure do
  
   config.cache_store = :dalli_store, 'localhost:11211', {namespace: 'obs-api', compress: true, expires_in: 1.day }
 
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
 end
 
 
