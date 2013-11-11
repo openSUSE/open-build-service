3136a6b92f19c84f793ae4961edd9cb1db82d900
Stephan Kulow <coolo@suse.de>
2013-11-11 16:48:17 +0100

    [webui] add a test case for /monitor events

--- src/api/test/functional/webui/monitor_controller_test.rb
+++ src/api/test/functional/webui/monitor_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class Webui::MonitorControllerTest < Webui::IntegrationTest
 
+  uses_transaction :test_reload_monitor
+
   def test_monitor
     visit webui_engine.monitor_path
     assert find(:id, "header-logo")
@@ -9,5 +11,37 @@ class Webui::MonitorControllerTest < Webui::IntegrationTest
     visit webui_engine.monitor_old_path
     assert find(:id, "header-logo")
   end
- 
+
+  def teardown
+    Timecop.return
+  end
+
+  test 'reload monitor' do
+    use_js
+
+    # as soon as we have only one API process...
+    #Timecop.travel(2010, 7, 12)
+
+    StatusHistory.transaction do
+      time = Time.now.to_i
+      400.times do |i|
+        StatusHistory.create(time: time - i * 1.day, key: 'squeue_med_x86_64', value: i)
+        StatusHistory.create(time: time - i * 1.day, key: 'squeue_high_x86_64', value: 0)
+        StatusHistory.create(time: time - i * 1.day, key: 'building_x86_64', value: Random.rand(10..42))
+        StatusHistory.create(time: time - i * 1.day, key: 'waiting_x86_64', value: Random.rand(10..42) * 1000)
+      end
+    end
+
+    visit webui_engine.monitor_path
+    select 'x86_64', from: 'architecture_display'
+    select '1 year', from: 'time_display'
+
+    page.wont_have_selector '.plotspinner'
+
+    # this is pure guessing - hopeing May is always visible throughout the year
+    # we can't use timecop as long as we have 2 processes
+    page.must_have_selector(:xpath, '//div[text()="May"]')
+    all('.tickLabel').each.map { |n| n.text }.must_include 'May'
+
+  end
 end
