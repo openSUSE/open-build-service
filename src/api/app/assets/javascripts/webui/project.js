c228eb62679f5393d04776fbafd90d9aa6128bb8
Stephan Kulow <coolo@suse.de>
2013-11-13 11:06:08 +0100

    [webui] improve the subprojects view code a bit and write test cases
    fixes #204 (well, not really - it just proves it was fixed before)

--- src/api/app/assets/javascripts/webui/project.js
+++ src/api/app/assets/javascripts/webui/project.js
@@ -1,122 +1,137 @@
-function renderPackagesTable(packages, length)
-{
+function renderPackagesTable(packages, length) {
     length = (typeof length === "undefined") ? 25 : length;
     var packageurl = $("#packages_table_wrapper").data("url");
-    $("#packages_table_wrapper").html( '<table cellpadding="0" cellspacing="0" border="0" class="display" id="packages_table"></table>' );
-    $("#packages_table").dataTable( {"aaData": packages, 
-				     "bSort": false,
-				     "bPaginate": packages.length > 12,
-				     "aoColumns": [
-					 {
-					     "fnRender": function ( obj ) {
-						 var url = packageurl.replace(/REPLACEIT/, obj.aData);
-						 return '<a href="' + url +'">' + obj.aData + '</a>';
-					     }
-					 } ],
-					 "bStateSave": true
-				    });
+    $("#packages_table_wrapper").html('<table cellpadding="0" cellspacing="0" border="0" class="display" id="packages_table"></table>');
+    $("#packages_table").dataTable({"aaData": packages,
+        "bSort": false,
+        "bPaginate": packages.length > 12,
+        "aoColumns": [
+            {
+                "fnRender": function (obj) {
+                    var url = packageurl.replace(/REPLACEIT/, obj.aData);
+                    return '<a href="' + url + '">' + obj.aData + '</a>';
+                }
+            }
+        ],
+        "bStateSave": true
+    });
 }
 
-function renderProjectsTable(length)
-{
+function renderProjectsTable(length) {
     length = (typeof length === "undefined") ? 25 : length;
     var projects = main_projects;
     if (!$('#excludefilter').is(":checked"))
-	projects = projects.concat(excl_projects);
+        projects = projects.concat(excl_projects);
     var projecturl = $("#projects_table_wrapper").data("url");
-    $("#projects_table_wrapper").html( '<table cellpadding="0" cellspacing="0" border="0" class="display" id="projects_table"></table>' );
-    $("#projects_table").dataTable( {"aaData": projects, 
-				     "bPaginate": (main_projects.length + excl_projects.length) > 20,
-				     "aoColumns": [
-					 {
-					     "sTitle": "Name",
-					     "fnRender": function ( obj ) {
-						 var url = projecturl.replace(/REPLACEIT/, obj.aData[0]);
-						 return '<a href="' + url +'">' + obj.aData[0] + '</a>';
-					     }
-					 }, { "sTitle": "Title" } ],
-					 "iDisplayLength": length,
-					 "bStateSave": true
-				    });
+    $("#projects_table_wrapper").html('<table cellpadding="0" cellspacing="0" border="0" class="display" id="projects_table"></table>');
+    $("#projects_table").dataTable({"aaData": projects,
+        "bPaginate": (main_projects.length + excl_projects.length) > 20,
+        "aoColumns": [
+            {
+                "sTitle": "Name",
+                "fnRender": function (obj) {
+                    var url = projecturl.replace(/REPLACEIT/, obj.aData[0]);
+                    return '<a href="' + url + '">' + obj.aData[0] + '</a>';
+                }
+            },
+            { "sTitle": "Title" }
+        ],
+        "iDisplayLength": length,
+        "bStateSave": true
+    });
 }
 
-function renderPackagesProjectsTable(options)
-{
+function renderPackagesProjectsTable(options) {
     var length = options.length || 25
     var name = options.name || "packages_projects_wrapper"
 
     var packageurl = $("#" + name).data("url");
-    $("#" + name).html("<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" class=\"display\" id=\"" + name + '_table' + "\"></table>" );
+    $("#" + name).html("<table cellpadding=\"0\" cellspacing=\"0\" border=\"0\" class=\"display\" id=\"" + name + '_table' + "\"></table>");
     $("#" + name + "_table").dataTable(
-          {
-          "aaData": options.packages, 
-          "bPaginate": options.packages.length > 12,
-          "aoColumns": [
-            {
-            "sTitle": "Package",
-            "fnRender": function ( obj ) {
-              var url1 = packageurl.replace(/REPLACEPKG/, obj.aData[0]);
-              var url = url1.replace(/REPLACEPRJ/, obj.aData[1]);
-              return '<a href="' + url +'">' + obj.aData[0] + '</a>';
-              }
-            },
-            {
-            "sTitle": "Project"
-            }],
-          "iDisplayLength": length,
-          "bStateSave": true
+        {
+            "aaData": options.packages,
+            "bPaginate": options.packages.length > 12,
+            "aoColumns": [
+                {
+                    "sTitle": "Package",
+                    "fnRender": function (obj) {
+                        var url1 = packageurl.replace(/REPLACEPKG/, obj.aData[0]);
+                        var url = url1.replace(/REPLACEPRJ/, obj.aData[1]);
+                        return '<a href="' + url + '">' + obj.aData[0] + '</a>';
+                    }
+                },
+                {
+                    "sTitle": "Project"
+                }
+            ],
+            "iDisplayLength": length,
+            "bStateSave": true
         });
 }
 
 
-
-function autocomplete_repositories(project_name) 
-{
-    if(project_name === "")
-      return;
+function autocomplete_repositories(project_name) {
+    if (project_name === "")
+        return;
     $('#loader-repo').show();
     $('#add_repository_button').attr('disabled', 'true');
     $('#target_repo').attr('disabled', 'true');
     $('#repo_name').attr('disabled', 'true');
     $.ajax({
-	url: $('#target_repo').data('ajaxurl'),
-	data: {project: project_name},
-	success: function(data){
-	    $('#target_repo').html('');
-	    // suggest a name:
-            $('#repo_name').attr('value', project_name.replace(/:/g,'_') + '_' + data[0]);
-	    var foundoptions = false;
-            $.each(data, function(idx, val) {
-		$('#target_repo').append( new Option( val ) );
-		$('#target_repo').removeAttr('disabled');
-         	$('#repo_name').removeAttr('disabled');
-		$('#add_repository_button').removeAttr('disabled');
-		foundoptions = true;
+        url: $('#target_repo').data('ajaxurl'),
+        data: {project: project_name},
+        success: function (data) {
+            $('#target_repo').html('');
+            // suggest a name:
+            $('#repo_name').attr('value', project_name.replace(/:/g, '_') + '_' + data[0]);
+            var foundoptions = false;
+            $.each(data, function (idx, val) {
+                $('#target_repo').append(new Option(val));
+                $('#target_repo').removeAttr('disabled');
+                $('#repo_name').removeAttr('disabled');
+                $('#add_repository_button').removeAttr('disabled');
+                foundoptions = true;
             });
-	    if (!foundoptions)
-		$('#target_repo').append( new Option( 'No repos found') );
-	},
-	complete: function(data){
+            if (!foundoptions)
+                $('#target_repo').append(new Option('No repos found'));
+        },
+        complete: function (data) {
             $('#loader-repo').hide();
-	}
+        }
     });
 }
 
-function repositories_setup_autocomplete()
-{
+function repositories_setup_autocomplete() {
     $("#target_project").autocomplete({
-	source: $('#target_project').data('ajaxurl'),
-	minLength: 2,
-	select: function (event, ui) { autocomplete_repositories(ui.item.value); },
-	change: function () { autocomplete_repositories($('#target_project').attr('value')); },
+        source: $('#target_project').data('ajaxurl'),
+        minLength: 2,
+        select: function (event, ui) {
+            autocomplete_repositories(ui.item.value);
+        },
+        change: function () {
+            autocomplete_repositories($('#target_project').attr('value'));
+        },
     });
-    
-    $("#target_project").change(function() {
-	autocomplete_repositories($('#target_project').attr('value'));
+
+    $("#target_project").change(function () {
+        autocomplete_repositories($('#target_project').attr('value'));
     });
-  
-    $('#target_repo').select(function() {
-	$('#repo_name').attr('value', $("#target_project").attr('value').replace(/:/g,'_') + '_' + $(this).val());
+
+    $('#target_repo').select(function () {
+        $('#repo_name').attr('value', $("#target_project").attr('value').replace(/:/g, '_') + '_' + $(this).val());
     });
 }
 
+function setup_subprojects_tables() {
+    $('#parentprojects_table').dataTable({
+        'bPaginate': false,
+        'bFilter': false,
+        'bInfo': false,
+    });
+    $('#subprojects_table').dataTable({
+        'bPaginate': false,
+        'bFilter': false,
+        'bInfo': false,
+    });
+
+}
--- src/api/test/functional/webui/project_create_test.rb
+++ src/api/test/functional/webui/project_create_test.rb
@@ -3,13 +3,15 @@
 require 'test_helper'
 
 class Webui::ProjectCreateTest < Webui::IntegrationTest
-  
+
   uses_transaction :test_create_global_project
-  uses_transaction :test_create_home_project_for_second_user 
-  uses_transaction :test_create_home_project_for_user 
-  uses_transaction :test_create_subproject_for_user 
-  uses_transaction :test_create_subproject_with_long_description 
-  uses_transaction :test_create_subproject_with_only_name 
+  uses_transaction :test_create_home_project_for_second_user
+  uses_transaction :test_create_home_project_for_user
+  uses_transaction :test_create_subproject_for_user
+  uses_transaction :test_create_subproject_with_long_description
+  uses_transaction :test_create_subproject_with_only_name
+  uses_transaction :test_first_case_of_issue_204
+  uses_transaction :test_second_case_of_issue_204
 
   # ============================================================================
   # Returns the description of the viewed project as is displayed.
@@ -30,12 +32,12 @@ class Webui::ProjectCreateTest < Webui::IntegrationTest
   end
 
   def create_project new_project
-    new_project[:expect]      ||= :success
-    new_project[:title]       ||= ''
+    new_project[:expect] ||= :success
+    new_project[:title] ||= ''
     new_project[:description] ||= ''
     new_project[:maintenance] ||= false
-    new_project[:hidden]      ||= false
-    new_project[:namespace]   ||= ''
+    new_project[:hidden] ||= false
+    new_project[:namespace] ||= ''
 
     if creating_home_project?
       new_project[:name] ||= current_user
@@ -46,7 +48,7 @@ class Webui::ProjectCreateTest < Webui::IntegrationTest
     end
 
     new_project[:description].squeeze!(' ')
-    new_project[:description].gsub!(/ *\n +/ , "\n")
+    new_project[:description].gsub!(/ *\n +/, "\n")
     new_project[:description].strip!
     message_prefix = "Project '#{new_project[:namespace] + new_project[:name]}' "
 
@@ -59,14 +61,14 @@ class Webui::ProjectCreateTest < Webui::IntegrationTest
     if new_project[:expect] == :success
       flash_message.must_equal message_prefix + 'was created successfully'
       flash_message_type.must_equal :info
-      
+
       new_project[:description] = 'No description set' if new_project[:description].empty?
       assert_equal new_project[:description].gsub(%r{\s+}, ' '), project_description
     elsif new_project[:expect] == :invalid_name
       flash_message.must_equal "Invalid project name '#{new_project[:name]}'."
       flash_message_type.must_equal :alert
     elsif new_project[:expect] == :no_permission
-      permission_error  = 'You lack the permission to create '
+      permission_error = 'You lack the permission to create '
       permission_error += "the project '#{new_project[:namespace] + new_project[:name]}'. "
       permission_error += "Please create it in your home:#{current_user} namespace"
       flash_message.must_equal permission_error
@@ -88,7 +90,7 @@ class Webui::ProjectCreateTest < Webui::IntegrationTest
 
 
   test 'create_home_project_for_user' do
-    
+
     login_user('user1', '123456')
     visit webui_engine.root_path
     open_new_project_from_main
@@ -96,8 +98,8 @@ class Webui::ProjectCreateTest < Webui::IntegrationTest
     create_project(title: 'HomeProject Title', namespace: 'home:',
                    description: 'Test generated empty home project.')
   end
-  
-  
+
+
   test 'create_home_project_for_second_user' do
 
     login_user('user2', '123456')
@@ -105,9 +107,8 @@ class Webui::ProjectCreateTest < Webui::IntegrationTest
 
     open_new_project_from_main
     assert creating_home_project?
-    create_project(
-                   title: 'HomeProject Title',
-		   namespace: 'home:',
+    create_project(title: 'HomeProject Title',
+                   namespace: 'home:',
                    description: 'Test generated empty home project for second user.')
   end
 
@@ -117,106 +118,144 @@ class Webui::ProjectCreateTest < Webui::IntegrationTest
     login_Iggy
     open_create_subproject(project: 'home:Iggy')
     create_project(
-      name: 'SubProject1',
-      namespace: 'home:Iggy:',
-      title: 'SubProject1 Title',
-      description: 'Test generated empty subproject.')
+        name: 'SubProject1',
+        namespace: 'home:Iggy:',
+        title: 'SubProject1 Title',
+        description: 'Test generated empty subproject.')
 
     open_create_subproject(project: 'home:Iggy')
-    create_project( 
-      name: 'SubProject1',
-      namespace: 'home:Iggy:',
-      title: 'NewTitle' + Time.now.to_i.to_s,
-      description: 'Empty subproject with duplicated name. Should give error!',
-      expect: :already_exists)
+    create_project(
+        name: 'SubProject1',
+        namespace: 'home:Iggy:',
+        title: 'NewTitle' + Time.now.to_i.to_s,
+        description: 'Empty subproject with duplicated name. Should give error!',
+        expect: :already_exists)
 
   end
 
-  
+
   test 'create_subproject_without_name' do
-  
+
     login_Iggy
     open_create_subproject(project: 'home:Iggy')
-    create_project( 
-      name: '',
-      title: 'NewTitle' + Time.now.to_i.to_s,
-      description: 'Test generated empty project without name. Should give error!',
-      expect: :invalid_name)    
-  end
-  
-  
-  test 'create_subproject_name_with_spaces' do
-    login_Iggy
-    
-    open_create_subproject(project: 'home:Iggy')
-    create_project( 
-      name: 'project name with spaces',
-      title: 'NewTitle' + Time.now.to_i.to_s,
-      description: 'Test generated empty project without name. Should give error!',
-      expect: :invalid_name)    
+    create_project(
+        name: '',
+        title: 'NewTitle' + Time.now.to_i.to_s,
+        description: 'Test generated empty project without name. Should give error!',
+        expect: :invalid_name)
   end
 
-  
-  test 'create_subproject_with_only_name' do
-    
+
+  test 'create_subproject_name_with_spaces' do
     login_Iggy
 
     open_create_subproject(project: 'home:Iggy')
     create_project(
-      name: 'SubProject2',
-      title: '',
-      namespace: 'home:Iggy:',
-      description: '')
+        name: 'project name with spaces',
+        title: 'NewTitle' + Time.now.to_i.to_s,
+        description: 'Test generated empty project without name. Should give error!',
+        expect: :invalid_name)
   end
 
-  
+
+  test 'create_subproject_with_only_name' do
+
+    login_Iggy
+
+    open_create_subproject(project: 'home:Iggy')
+    create_project(
+        name: 'SubProject2',
+        title: '',
+        namespace: 'home:Iggy:',
+        description: '')
+  end
+
+
   test 'create_subproject_with_long_description' do
 
     login_Iggy
-    
+
     open_create_subproject(project: 'home:Iggy')
-    create_project( 
-      name: 'SubProject3',
-      title: 'Subproject with long text',
-      namespace: 'home:Iggy:',
-      description: LONG_DESCRIPTION)
+    create_project(
+        name: 'SubProject3',
+        title: 'Subproject with long text',
+        namespace: 'home:Iggy:',
+        description: LONG_DESCRIPTION)
   end
 
-  
+
   test 'create_global_project' do
-  
-    login_king
-    visit webui_engine.project_list_all_path
+
+    login_king to: webui_engine.project_list_all_path
 
     click_link('Create new project')
     create_project(
-      name: 'PublicProject1',
-      title: 'NewTitle' + Time.now.to_i.to_s,
-      description: "Test generated empty public project by #{current_user}.")
+        name: 'PublicProject1',
+        title: 'NewTitle' + Time.now.to_i.to_s,
+        description: "Test generated empty public project by #{current_user}.")
   end
 
-  
+
   test 'create_global_project_as_user' do
-  
-    login_Iggy
-    visit webui_engine.project_list_all_path
-    
+
+    login_Iggy to: webui_engine.project_list_all_path
+
     click_link('Create new project')
     create_project(
-      name: 'PublicProj-' + Time.now.to_i.to_s,
-      title: 'NewTitle' + Time.now.to_i.to_s,
-      description: 'Test generated empty public project by user. Should give error.',
-      expect: :no_permission)
+        name: 'PublicProj-' + Time.now.to_i.to_s,
+        title: 'NewTitle' + Time.now.to_i.to_s,
+        description: 'Test generated empty public project by user. Should give error.',
+        expect: :no_permission)
   end
-  
-   
+
+  test "first case of issue 204" do
+    login_king to: webui_engine.project_new_path
+
+    prjroot = Faker::Lorem.characters(20)
+    create_project(
+        name: prjroot,
+        title: 'none',
+        description: 'none')
+
+    visit webui_engine.project_subprojects_path project: prjroot
+    click_link 'Create subproject'
+
+    fill_in :name, with: 'b'
+    click_button 'Create Project'
+
+    # the parent project should be clickable
+    within '#breadcrump' do
+      find(:link, prjroot).text.must_equal prjroot
+    end
+  end
+
+  test "second case of issue 204" do
+    prjroot = Faker::Lorem.characters(20)
+    subproject = prjroot + ':b'
+
+    login_king to: webui_engine.project_new_path
+
+    fill_in :name, with: subproject
+    click_button 'Create Project'
+
+    # now create the parent project
+    visit webui_engine.project_new_path
+    fill_in :name, with: prjroot
+    click_button 'Create Project'
+
+    visit webui_engine.project_show_path project: subproject
+    # the parent project should be clickable
+    within '#breadcrump' do
+      find(:link, prjroot).text.must_equal prjroot
+    end
+  end
+
   # RUBY CODE ENDS HERE.
   # BELOW ARE APPENDED ALL DATA STRUCTURES USED BY THE TESTS.
-  
 
 
-# -------------------------------------------------------------------------------------- #
-LONG_DESCRIPTION = <<LICENSE_END
+  # -------------------------------------------------------------------------------------- #
+  LONG_DESCRIPTION = <<LICENSE_END
 This
 is äüß
 a very
