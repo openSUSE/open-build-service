048c09c5c867846a8f4c7ae42d07b056a16c9a91
Ancor Gonzalez Sosa <ancor@suse.de>
2013-11-27 10:47:32 +0100

    [api] add db index to ProjectLogEntry#datetime

--- src/api/db/structure.sql
+++ src/api/db/structure.sql
@@ -660,6 +660,7 @@ CREATE TABLE `project_log_entries` (
   KEY `index_project_log_entries_on_package_name` (`package_name`),
   KEY `index_project_log_entries_on_bs_request_id` (`bs_request_id`),
   KEY `index_project_log_entries_on_event_type` (`event_type`),
+  KEY `index_project_log_entries_on_datetime` (`datetime`),
   CONSTRAINT `project_log_entries_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -1390,6 +1391,8 @@ INSERT INTO schema_migrations (version) VALUES ('20131125101042');
 INSERT INTO schema_migrations (version) VALUES ('20131126074753');
 
 INSERT INTO schema_migrations (version) VALUES ('20131126205430');
+
+INSERT INTO schema_migrations (version) VALUES ('20131127091624');
 
 INSERT INTO schema_migrations (version) VALUES ('21');
 
