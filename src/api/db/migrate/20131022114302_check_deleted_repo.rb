6f910b3d542e2d67d3da97baeb5256ba31032a8c
Stephan Kulow <coolo@suse.de>
2013-12-03 21:39:20 +0100

    [api] give the deleted repo another shot

--- src/api/db/migrate/20131022114302_check_deleted_repo.rb
+++ src/api/db/migrate/20131022114302_check_deleted_repo.rb
@@ -1,10 +1,11 @@
 class CheckDeletedRepo < ActiveRecord::Migration
   
   class Repository < ActiveRecord::Base; end
+  class Project < ActiveRecord::Base; end
 
   def self.up
     # default repository to link when original one got removed
-    d = Project.find_or_create_by_name("deleted")
+    d = Project.where(name: 'deleted').first_or_create
     Repository.where(project_id: d.id, name: 'deleted').first_or_create
   end
 
