1572ca04569fe7d0f755119adbdca8de5e9e1bcc
Stephan Kulow <coolo@suse.de>
2013-12-03 11:35:59 +0100

    [api] also fix the deleted repo if not existant
    This is already part of the 2.4 fixtures, but not in real life

--- src/api/db/migrate/20131022114302_check_deleted_repo.rb
+++ src/api/db/migrate/20131022114302_check_deleted_repo.rb
@@ -1,10 +1,11 @@
 class CheckDeletedRepo < ActiveRecord::Migration
+  
+  class Repository < ActiveRecord::Base; end
+
   def self.up
     # default repository to link when original one got removed
     d = Project.find_or_create_by_name("deleted")
-    if d.repositories.find_by_name("deleted").nil?
-      d.repositories.create name: "deleted"
-    end
+    Repository.where(project_id: d.id, name: 'deleted').first_or_create
   end
 
   def self.down
