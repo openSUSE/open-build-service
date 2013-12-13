48e831d7a8aaa15003f6eee2919d20f5946271f9
Stephan Kulow <coolo@suse.de>
2013-12-13 13:43:13 +0100

    [api] work on mail content for BuildFail emails

--- src/api/app/models/event/build.rb
+++ src/api/app/models/event/build.rb
@@ -11,9 +11,35 @@ class Event::BuildSuccess < Event::Build
 end
 
 class Event::BuildFail < Event::Build
+  include BuildLogSupport
+
   self.raw_type = 'BUILD_FAIL'
   self.description = 'Package has failed to build'
   receiver_roles :maintainer
+
+  def subject
+    "Build failure of #{payload['project']}/#{payload['package']} in #{payload['repository']}/#{payload['arch']}"
+  end
+
+  def faillog
+    begin
+      size = get_size_of_log(payload['project'], payload['package'], payload['repository'], payload['arch'])
+      offset = size - 18 * 1024
+      offset = 0 if offset < 0
+      log = get_log_chunk(payload['project'], payload['package'], payload['repository'], payload['arch'], offset, size).lines
+      if log.length > 20
+        log = log.slice(-19, log.length)
+      end
+      log.join
+    rescue ActiveXML::Transport::NotFoundError => e
+      nil
+    end
+  end
+
+  def expanded_payload
+    payload.merge('faillog' => faillog)
+  end
+
 end
 
 class Event::BuildUnchanged < Event::Build
