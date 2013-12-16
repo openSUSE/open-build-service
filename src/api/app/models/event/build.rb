3a07be571d74d9cde9328745448f3d69eb20483a
Stephan Kulow <coolo@suse.de>
2013-12-16 10:34:19 +0100

    [api] add threading support for build failure mails
    And enable actual sendmail

--- src/api/app/models/event/build.rb
+++ src/api/app/models/event/build.rb
@@ -3,6 +3,22 @@ class Event::Build < Event::Package
   self.abstract_class = true
   payload_keys :repository, :arch, :release, :readytime, :srcmd5,
                :rev, :reason, :bcnt, :verifymd5, :hostarch, :starttime, :endtime, :workerid, :versrel
+
+  def my_message_id
+    # we put the verifymd5 sum in the message id, so new checkins get new thread, but it doesn't have to be very correct
+    md5 = payload['verifymd5'][0..6]
+    domain = URI.parse(::Configuration.first.obs_url)
+    mid = Digest::MD5.hexdigest("#{payload['project']}-#{payload['package']}-#{payload['repository']}-#{md5}")
+    "build-#{mid}@#{domain.host.downcase}"
+  end
+
+  def custom_headers
+    mid = my_message_id
+    h = super
+    h['In-Reply-To'] = mid
+    h['References'] = mid
+    h
+  end
 end
 
 class Event::BuildSuccess < Event::Build
@@ -28,13 +44,13 @@ class Event::BuildFail < Event::Build
       offset = 0 if offset < 0
       log = raw_log_chunk(payload['project'], payload['package'], payload['repository'], payload['arch'], offset, size)
       begin
-        log.encode!(invalid: :replace, undef: :replace, cr_newline: true)
+        log.encode!(invalid: :replace, undef: :replace, universal_newline: true)
       rescue Encoding::UndefinedConversionError
         # encode is documented not to throw it if undef: is :replace, but at least we tried - and ruby 1.9.3 is buggy
       end
-      log = log.lines
-      if log.length > 20
-        log = log.slice(-19, log.length)
+      log = log.chomp.lines
+      if log.length > 30
+        log = log.slice(-29, log.length)
       end
       log.join
     rescue ActiveXML::Transport::Error
