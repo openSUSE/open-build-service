9c510cf774b182a5c51456e92d52c5698f29d3d2
Stephan Kulow <coolo@suse.de>
2013-12-17 14:56:44 +0100

    [backend] add the information to the build event if a failed log file exists

--- src/api/app/models/event/build.rb
+++ src/api/app/models/event/build.rb
@@ -2,7 +2,7 @@ class Event::Build < Event::Package
   self.description = 'Package has finished building'
   self.abstract_class = true
   payload_keys :repository, :arch, :release, :readytime, :srcmd5,
-               :rev, :reason, :bcnt, :verifymd5, :hostarch, :starttime, :endtime, :workerid, :versrel
+               :rev, :reason, :bcnt, :verifymd5, :hostarch, :starttime, :endtime, :workerid, :versrel, :previouslyfailed
 
   def my_message_id
     # we put the verifymd5 sum in the message id, so new checkins get new thread, but it doesn't have to be very correct
