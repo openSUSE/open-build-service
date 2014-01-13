788810e0e827b96bdf4b6e04cc7e64381efc50b0
Stephan Kulow <coolo@suse.de>
2014-01-13 12:10:54 +0100

    [webui] merge the existant comment titles into the body
    Fixes issue 546 as discussed in 2.5 meeting

--- src/api/test/unit/comment_test.rb
+++ src/api/test/unit/comment_test.rb
@@ -4,7 +4,7 @@ class CommentTest < ActiveSupport::TestCase
 
   test "Comment checks" do
     user = User.find_by_login('Admin')
-    com = CommentPackage.new(:user => user, :title => "Comment title", :body => "Comment body")
+    com = CommentPackage.new(user: user, body: "Comment body")
     assert_equal false, com.save
     assert_equal ["can't be blank"], com.errors[:package]
   end
