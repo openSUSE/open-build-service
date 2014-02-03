acd296842a6560d205cb79e3c9f522ffac61d473
Stephan Kulow <coolo@suse.de>
2014-02-03 16:13:30 +0100

    [webui] display packages in tabs
    In project/show we now open a 2nd tab if packages are inherited through
    project links. Later on we might want to link from inherited packages
    to another action, that only shows build results and not the inherited
    package

--- src/api/app/assets/javascripts/webui/application/project.js
+++ src/api/app/assets/javascripts/webui/application/project.js
@@ -1,8 +1,8 @@
-function renderPackagesTable(packages, length) {
+function renderPackagesTable(wrapper, packages, length) {
     length = (typeof length === "undefined") ? 25 : length;
-    var packageurl = $("#packages_table_wrapper").data("url");
-    $("#packages_table_wrapper").html('<table cellpadding="0" cellspacing="0" border="0" class="display" id="packages_table"></table>');
-    $("#packages_table").dataTable({"aaData": packages,
+    var packageurl = $("#" + wrapper).data("url");
+    $("#" + wrapper).html('<table cellpadding="0" cellspacing="0" border="0" class="display" id="' + wrapper + '_table"></table>');
+    $("#" + wrapper + "_table").dataTable({"aaData": packages,
         "bSort": false,
         "bPaginate": packages.length > 12,
         "aoColumns": [
