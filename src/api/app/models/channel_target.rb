a277fe80d1e848ac2c5e53a332120d775f958f91
Adrian Schröter <adrian@suse.de>
2014-04-07 15:49:18 +0200

    [api] improve multiple channel target error message

--- src/api/app/models/channel_target.rb
+++ src/api/app/models/channel_target.rb
@@ -16,7 +16,7 @@ class ChannelTarget < ActiveRecord::Base
       ct.each do |cti|
         msg << "#{cti.channel.package.project.name}/#{cti.channel.package.name}, "
       end
-      raise "Multiple channel targets found in #{msg}"
+      raise "Multiple channel targets found in #{msg} for repository #{repo.project.name}/#{repo.name}"
     end
     return ct.first
   end
