f02ab99dc3005000502e807d48951d83e617dcf8
Adrian Schröter <adrian@suse.de>
2014-04-25 14:07:30 +0200

    [backend] test run of bs_check_consistency at the end of test suite

--- src/api/test/functional/zzz_post_consistency_test.rb
+++ src/api/test/functional/zzz_post_consistency_test.rb
@@ -1,7 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + "/..") + "/test_consistency_helper"
 
 class ZZZPostConsistency < ActionDispatch::IntegrationTest 
+  require 'source_controller'
   fixtures :all
+
+  def setup
+    super
+    wait_for_scheduler_start
+  end
   
   def test_resubmit_fixtures
     resubmit_all_fixtures
@@ -16,6 +22,43 @@ class ZZZPostConsistency < ActionDispatch::IntegrationTest
     assert_response :success
     assert_tag :tag => 'collection', :children => { :count => 1 }
     assert_tag :tag => 'project', :attributes => { :name => "My:Maintenance" }
+  end
+
+  def test_fsck_backend
+
+    perlopts="-I#{Rails.root}/../backend -I#{Rails.root}/../backend/build"
+
+    progress=nil
+    failed=nil
+    IO.popen("cd #{Rails.root}/tmp/backend_config; exec perl #{perlopts} ./bs_check_consistency --check-all --do-check-meta --do-check-signatures 2>&1") do |io|
+      io.each do |line|
+#        puts ">#{line}<"
+        next if line.blank?
+
+        # catch progress lines
+        if line.starts_with? "PROGRESS:"
+          progress=line
+          next
+        end
+        next if line.starts_with? "DBPROGRESS:"
+
+        # acceptable during test suite run
+        next if line =~ /jobs.dispatchprios missing/;
+        next if line =~ /jobs.load missing/;
+        next if line =~ /^check finished/;
+        next if line =~ /status file without existing job/;
+        next if line =~ /build\/home:Iggy\/10.2\/i586\/:full\/rpm.rpm broken rpm/;
+        next if line =~ /build\/home:Iggy\/10.2\/i586\/:full\/rpm.rpm broken signature/;
+
+        # unhandled line, dump a failure
+        failed=true
+        puts progress if progress
+        progress=nil
+        puts line
+      end
+    end
+
+    assert_nil failed
   end
 end
 
