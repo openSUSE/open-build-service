9f765cb3d7af9961b5cdc8e24be382e25aab2345
Ancor Gonzalez Sosa <ancor@suse.de>
2014-06-16 12:28:33 +0200

    [webui] Small fixes in the OBS specific markup
     * @user was not working at the very beginning of the comment.
     * Req# and SR# are now case insensitive (and Request# added)

--- src/api/lib/obsapi/markdown_renderer.rb
+++ src/api/lib/obsapi/markdown_renderer.rb
@@ -8,13 +8,13 @@ module OBSApi
 
     def preprocess(fulldoc)
       # OBS requests
-      out = fulldoc.gsub(/(sr|req)#(\d+)/) {|s| "<a href=\"#{request_show_url(id: $2)}\">#{s}</a>" }
+      out = fulldoc.gsub(/(sr|req|request)#(\d+)/i) {|s| "<a href=\"#{request_show_url(id: $2)}\">#{s}</a>" }
       # issues
       IssueTracker.all.each do |t|
         out = t.get_html(out)
       end
       # users
-      out.gsub!(/([^\w])@(\w+)([^\w]|$)/) {|s| "#{$1}<a href=\"#{user_show_url($2)}\">@#{$2}</a>#{$3}" }
+      out.gsub!(/([^\w]|^)@(\w+)([^\w]|$)/) {|s| "#{$1}<a href=\"#{user_show_url($2)}\">@#{$2}</a>#{$3}" }
       out
     end
   end
