aed0b6ba7885e79bc58efb0efde5f3aaa8eea7c1
Jack Leigh <jack.leigh@collabora.com>
2014-06-17 17:24:22 +0100

    [dist] Don't redirect to /dev/null
    We surely want it to tell us if these commands don't succeed (because
    e.g. your tar file has the wrong name).

--- dist/find-requires.sh
+++ dist/find-requires.sh
@@ -6,12 +6,12 @@ shift
 tdir=`mktemp -d`
 
 # extract files
-tar xJf $sourcearchive -C $tdir >&/dev/null
+tar xJf $sourcearchive -C $tdir
 
-pushd $tdir/open-build-service*/src/api >& /dev/null
+pushd $tdir/open-build-service*/src/api
 
 ruby -rbundler -e 'Bundler.definition.resolve.to_a.each { |s| puts "rubygem(2.1.0:#{s.name}) = #{s.version}" }' | grep -v ':webui' | while read i; do echo -n $i", "; done
-popd >& /dev/null
+popd
 
 #cleanup
 rm -rf $tdir
