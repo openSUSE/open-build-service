edc1b15d90ba8d8ce91b7ceeb384828b6415e978
Adrian Schröter <adrian@suse.de>
2014-07-01 13:00:51 +0200

    [api] fix migration numbering

--- src/api/db/migrate/20141302101042_add_admin_email_configuration.rb
+++
@@ -1,9 +0,0 @@
-class AddAdminEmailConfiguration < ActiveRecord::Migration
-  def self.up
-    add_column :configurations, :admin_email, :string, :default => "unconfigured@openbuildservice.org"
-  end
-
-  def self.down
-    remove_column :configurations, :admin_email
-  end
-end
