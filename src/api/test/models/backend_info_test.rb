9e473e6637953d9bbe9be62fa4332b3d15b491dc
Adrian Schröter <adrian@suse.de>
2014-07-10 15:59:27 +0200

    [ci] do not rely on test case order here
    and make it possible to execute it alone

--- src/api/test/models/backend_info_test.rb
+++ src/api/test/models/backend_info_test.rb
@@ -1,10 +1,12 @@
-require 'test_helper'
+require_relative '../test_helper'
 
 class BackendInfoTest < ActiveSupport::TestCase
   test "basics" do
-     assert_equal BackendInfo.lastnotification_nr, 0
+     old = BackendInfo.lastnotification_nr
      BackendInfo.lastnotification_nr = 42
      assert_equal BackendInfo.lastnotification_nr, 42
+     BackendInfo.lastnotification_nr = old
+     assert_equal BackendInfo.lastnotification_nr, old
   end
 
 end
