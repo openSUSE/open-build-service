5b2c691b9e6607e23f84299dee9b834bf75dde98
Adrian Schröter <adrian@suse.de>
2014-09-11 09:54:11 +0200

    [api] fix default priortity value for old requests

--- src/api/db/migrate/20140903105426_add_request_priority.rb
+++ src/api/db/migrate/20140903105426_add_request_priority.rb
@@ -1,8 +1,9 @@
 class AddRequestPriority < ActiveRecord::Migration
   def self.up
-    add_column :bs_requests, :priority, :integer
+    add_column :bs_requests, :priority, :integer, default: "moderate"
 
     execute("alter table bs_requests modify column `priority` enum('critical','important','moderate','low') DEFAULT 'moderate';")
+    execute("UPDATE bs_requests SET priority = 'moderate' WHERE ISNULL(priority);")
   end
 
   def self.down
