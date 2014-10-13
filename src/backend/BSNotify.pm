9932c642a5064f34c5fcb347ab40dc1660a0fbf2
Jan Blunck <jblunck@infradead.org>
2014-10-13 11:28:15 +0200

    [backend] Move generate_commit_flist to bs_srcserver
    The function is only used in source server.
    
    Signed-off-by: Jan Blunck <jblunck@infradead.org>

--- src/backend/BSNotify.pm
+++ src/backend/BSNotify.pm
@@ -89,39 +89,4 @@ sub loadPackage {
   return $obj;    
 }
 
-##
-# generate_commit_flist($files_old, $files_new)
-#
-#   $files_old/$files_new are hash references as returned by lsrep
-#
-#   returns a list of changed files categorized similar to svn commit mails
-#
-sub generate_commit_flist {
-  my $ret = "";
-  my %categorized_files;
-  my ($files_old, $files_new) = @_;
-  my %files_all = (%$files_new, %$files_old);
-  for my $fname (sort keys %files_all) {
-    if(!$files_old->{$fname}) {
-      my $flist = $categorized_files{"Added:"} ||= [];
-      push(@$flist, $fname);
-    } elsif(!$files_new->{$fname}) {
-      my $flist = $categorized_files{"Deleted:"} ||= [];
-      push(@$flist, $fname);
-    } elsif($files_old->{$fname} ne $files_new->{$fname}) {
-      my $flist = $categorized_files{"Modified:"} ||= [];
-      push(@$flist, $fname);
-    }
-  }
-
-  for my $cat (sort keys %categorized_files) {
-    $ret .= "$cat\n";
-    for my $fname (@{$categorized_files{$cat}}) {
-      $ret .= "  $fname\n";
-    }
-    $ret .= "\n";
-  }
-  return $ret;
-}
-
 1;
