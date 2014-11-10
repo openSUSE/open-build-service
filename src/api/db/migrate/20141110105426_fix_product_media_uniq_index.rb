5b386bbb9032ccf2cd96bb49f6bf5f935af30dca
Adrian Schröter <adrian@suse.de>
2014-11-10 17:41:47 +0100

    [api] try to workaround migration problem from OBS 2.5

--- src/api/db/migrate/20141110105426_fix_product_media_uniq_index.rb
+++ src/api/db/migrate/20141110105426_fix_product_media_uniq_index.rb
@@ -1,17 +1,31 @@
 class FixProductMediaUniqIndex < ActiveRecord::Migration
   def self.up
+    execute("alter table product_media drop foreign key `product_media_ibfk_1`")
+    execute("alter table product_media drop foreign key `product_media_ibfk_2`")
+    execute("alter table product_media drop foreign key `product_media_ibfk_3`")
+
     remove_index :product_media, unique: true, :name => "index_unique"
     remove_index :product_update_repositories, unique: true, :name => "index_unique"
 
     add_index :product_media, [:product_id, :repository_id, :name, :arch_filter_id], unique: true, :name => "index_unique"
     add_index :product_update_repositories, [:product_id, :repository_id, :arch_filter_id], unique: true, :name => "index_unique"
+
+    execute("alter table product_media add FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)")
+    execute("alter table product_media add FOREIGN KEY (`repository_id`) REFERENCES `repositories` (`id`)")
+    execute("alter table product_media add FOREIGN KEY (`arch_filter_id`) REFERENCES `architectures` (`id`)")
   end
 
   def self.down
+    execute("alter table product_media drop foreign key `product_media_ibfk_1`")
+    execute("alter table product_media drop foreign key `product_media_ibfk_2`")
+    execute("alter table product_media drop foreign key `product_media_ibfk_3`")
     remove_index :product_media, unique: true, :name => "index_unique"
     remove_index :product_update_repositories, unique: true, :name => "index_unique"
 
     add_index :product_media, [:product_id, :repository_id, :name], unique: true, :name => "index_unique"
     add_index :product_update_repositories, [:product_id, :repository_id], unique: true, :name => "index_unique"
+    execute("alter table product_media add FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)")
+    execute("alter table product_media add FOREIGN KEY (`repository_id`) REFERENCES `repositories` (`id`)")
+    execute("alter table product_media add FOREIGN KEY (`arch_filter_id`) REFERENCES `architectures` (`id`)")
   end
 end
