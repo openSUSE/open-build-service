ca12973ca806036fecb84ee189483da4c6e2802e
Adrian Schröter <adrian@suse.de>
2015-01-13 09:40:30 +0100

    [api] make migration a transaction
    for speed and savety reasons

--- src/api/db/migrate/20150112135426_adapt_cve_tracker.rb
+++ src/api/db/migrate/20150112135426_adapt_cve_tracker.rb
@@ -1,31 +1,37 @@
 class AdaptCveTracker < ActiveRecord::Migration
 
   def up
-    t=IssueTracker.find_by_name('cve')
-    t.regex='(?:cve|CVE)-(\d\d\d\d-\d+)'
-    t.label="CVE-@@@"
-    t.save
-    Delayed::Worker.delay_jobs = true
-    IssueTracker.write_to_backend
+    ActiveRecord::Base.transaction do
+      t=IssueTracker.find_by_name('cve')
+      t.regex='(?:cve|CVE)-(\d\d\d\d-\d+)'
+      t.label="CVE-@@@"
+      t.show_url="http://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-@@@"
+      t.save
+      Delayed::Worker.delay_jobs = true
+      IssueTracker.write_to_backend
 
-    t.issues.each do |i|
-      i.name.gsub!(/^CVE-/,'')
-      i.name.gsub!(/^cve-/,'')
-      i.save
+      t.issues.each do |i|
+        i.name.gsub!(/^CVE-/,'')
+        i.name.gsub!(/^cve-/,'')
+        i.save
+      end
     end
   end
 
   def down
-    t=IssueTracker.find_by_name('cve')
-    t.regex='(CVE-\d\d\d\d-\d+)'
-    t.label="@@@"
-    t.save
-    Delayed::Worker.delay_jobs = true
-    IssueTracker.write_to_backend
+    ActiveRecord::Base.transaction do
+      t=IssueTracker.find_by_name('cve')
+      t.regex='(CVE-\d\d\d\d-\d+)'
+      t.label="@@@"
+      t.show_url="http://cve.mitre.org/cgi-bin/cvename.cgi?name=@@@"
+      t.save
+      Delayed::Worker.delay_jobs = true
+      IssueTracker.write_to_backend
 
-    t.issues.each do |i|
-      i.name = "CVE-" + i.name
-      i.save
+      t.issues.each do |i|
+        i.name = "CVE-" + i.name
+        i.save
+      end
     end
   end
 
