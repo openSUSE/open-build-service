60e32cd0ad7755d8462230e974188a6eb999c108
Victor Pereira <vpereira@suse.de>
2015-02-04 10:36:48 +0100

    improve the ruby dialect, refactored variable index, using each_with_index method

--- src/api/app/models/buildresult.rb
+++ src/api/app/models/buildresult.rb
@@ -5,24 +5,22 @@ class Buildresult < ActiveXML::Node
   @@status_hash = nil
 
   def self.avail_status_values
-    return Avail_status_values
+    Avail_status_values
   end
 
   def self.code2index(code)
     unless @@status_hash
-      index = 0
       @@status_hash = Hash.new
-      Avail_status_values.each do |s|
+      Avail_status_values.each_with_index do |s,index|
         @@status_hash[s] = index
-        index += 1
       end
     end
     raise ArgumentError, "code '#{code}' unknown #{@@status_hash.inspect}" unless @@status_hash[code]
-    return @@status_hash[code]
+    @@status_hash[code]
   end
 
   def self.index2code(index)
-    return Avail_status_values[index]
+    Avail_status_values[index]
   end
 
 end
