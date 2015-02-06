4dc8a0c349e32b5cb360cc8810ee115e0d91e976
Victor Pereira <vpereirabr@gmail.com>
2015-02-06 17:12:45 +0100

    [api] SQL injection vector and reduced complexity of method

--- src/api/app/models/maintenance_incident.rb
+++ src/api/app/models/maintenance_incident.rb
@@ -7,70 +7,85 @@ class MaintenanceIncident < ActiveRecord::Base
 
   def project_name
       unless self.incident_id
-        sql = ActiveRecord::Base.connection()
-        r = sql.execute( "SELECT counter FROM incident_counter WHERE maintenance_db_project_id = " + self.maintenance_db_project_id.to_s + " FOR UPDATE" ).first
-
+        r = MaintenanceIncident.exec_query(["SELECT counter FROM incident_counter WHERE maintenance_db_project_id = ? FOR UPDATE",
+                                            self.maintenance_db_project_id]).first
         if r.nil?
           # no counter exists, initialize it and select again
-          sql.execute( "INSERT INTO incident_counter(maintenance_db_project_id) VALUES('" + self.maintenance_db_project_id.to_s + "')" )
-          r = sql.execute( "SELECT counter FROM incident_counter WHERE maintenance_db_project_id = " + self.maintenance_db_project_id.to_s + " FOR UPDATE" ).first
+          MaintenanceIncident.exec_query ["INSERT INTO incident_counter(maintenance_db_project_id) VALUES('?')", self.maintenance_db_project_id]
+
+          r = MaintenanceIncident.exec_query(["SELECT counter FROM incident_counter WHERE maintenance_db_project_id = ? FOR UPDATE",
+                                              self.maintenance_db_project_id]).first
         end
+
         # do an atomic increase of counter
-        sql.execute( "UPDATE incident_counter SET counter = counter+1 WHERE maintenance_db_project_id = " + self.maintenance_db_project_id.to_s )
+        MaintenanceIncident.exec_query ["UPDATE incident_counter SET counter = counter+1 WHERE maintenance_db_project_id = ?", self.maintenance_db_project_id]
         self.incident_id = r[0]
       end
-
-      name = self.maintenance_db_project.name + ":" + self.incident_id.to_s 
-      return name
+      name = self.maintenance_db_project.name + ":" + self.incident_id.to_s
+      name
   end
 
   def initUpdateinfoId(template = "%Y-%C", patch_name = nil)
     return if self.released_at
 
-    # set current time, to be used 
+
+    # set current time, to be used
     self.released_at = Time.now.utc
     self.name = patch_name
 
+    counterType = ""
+    values = []
+
     # Run an atomar counter++ based on the used scheme
     if template =~ /%Y/
-      counterType = " AND year  = " + self.released_at.year.to_s
-      year = "'" + self.released_at.year.to_s + "'"
+      counterType << " AND year = ? "
+      values << self.released_at.year.to_s
+      year =  self.released_at.year.to_s
     else
-      counterType = " AND ISNULL(year)"
+      counterType = " AND ISNULL(year) "
       year = "NULL"
     end
     if template =~ /%M/
-      counterType << " AND month = " + self.released_at.month.to_s
-      month = "'" + self.released_at.month.to_s + "'"
+      counterType << " AND month = ? "
+      values << self.released_at.month.to_s
+      month = self.released_at.month.to_s
     else
-      counterType << " AND ISNULL(month)"
+      counterType << " AND ISNULL(month) "
       month = "NULL"
     end
+
     if template =~ /%D/
-      counterType << " AND day   = " + self.released_at.day.to_s
-      day = "'" + self.released_at.day.to_s + "'"
+      counterType << " AND day   = ? "
+      values << self.released_at.day.to_s
+      day = self.released_at.day.to_s
     else
-      counterType << " AND ISNULL(day)"
+      counterType << " AND ISNULL(day) "
       day = "NULL"
     end
     if template =~ /%N/
-      name = ActiveRecord::Base.connection.quote(self.name||"")
-      counterType << " AND name   = " + name
+      name = (self.name||"")
+      counterType << " AND name   = ? "
+      values << name
     else
-      counterType << " AND ISNULL(name)"
+      counterType << " AND ISNULL(name) "
       name = "NULL"
     end
-    sql = ActiveRecord::Base.connection()
-    r = sql.execute( "SELECT counter FROM updateinfo_counter WHERE maintenance_db_project_id = " + self.maintenance_db_project.id.to_s + counterType + " FOR UPDATE" ).first
+
+    r = MaintenanceIncident.exec_query(["SELECT counter FROM updateinfo_counter WHERE maintenance_db_project_id = ? #{counterType} FOR UPDATE",
+                                        self.maintenance_db_project_id,*values]).first
+
     unless r
       # no counter exists, initialize it and select again
-      sql.execute( "INSERT INTO updateinfo_counter(maintenance_db_project_id, year, month, day, name) VALUES('" + self.maintenance_db_project.id.to_s + "', " + year + ", " + month + ", " + day + "," + name + ")" )
-      r = sql.execute( "SELECT counter FROM updateinfo_counter WHERE maintenance_db_project_id = " + self.maintenance_db_project.id.to_s + counterType + " FOR UPDATE" ).first
+      r = MaintenanceIncident.exec_query ["INSERT INTO updateinfo_counter(maintenance_db_project_id, year, month, day, name) VALUES(?,?,?,?,?)",
+                                          self.maintenance_db_project_id,year,month,day,name]
+
+      r =  MaintenanceIncident.exec_query(["SELECT counter FROM updateinfo_counter WHERE maintenance_db_project_id = ? #{counterType} FOR UPDATE",
+                                           self.maintenance_db_project_id, *values]).first
     end
     # do an atomic increase of counter
-    sql.execute( "UPDATE updateinfo_counter SET counter = counter+1 WHERE maintenance_db_project_id = " + self.maintenance_db_project.id.to_s + counterType )
+     MaintenanceIncident.exec_query ["UPDATE updateinfo_counter SET counter = counter+1  WHERE maintenance_db_project_id = ? #{counterType}",
+                                     self.maintenance_db_project_id,*values]
     self.counter = r[0].to_i + 1
-
     self.save!
   end
 
@@ -93,5 +108,10 @@ class MaintenanceIncident < ActiveRecord::Base
     my_id.gsub!( /%g/, self.id.to_s )
 
     return my_id
+  end
+
+  # execute a sql query + escaped string
+  def self.exec_query(query)
+    self.connection.execute self.escape_sql query
   end
 end
