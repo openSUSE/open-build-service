f96d11d685dabae4025642d5846fc8d7b2ec2a0b
Adrian Schröter <adrian@suse.de>
2015-06-26 13:12:06 +0200

    [api] migration fixes
    after testing with real-life data

--- src/api/db/migrate/20150625105426_multiple_incident_counters.rb
+++ src/api/db/migrate/20150625105426_multiple_incident_counters.rb
@@ -5,37 +5,38 @@ end
 
 class MultipleIncidentCounters < ActiveRecord::Migration
   def self.up
-   create_table :incident_updateinfo_counter_values do |t|
-     t.references :updateinfo_counter, null: false
-     t.references :project,     null: false
-     t.integer    :value,       null: false
-     t.datetime   :released_at, null: false
-   end
-   add_index :incident_updateinfo_counter_values, [:updateinfo_counter_id, :project_id], :name => "uniq_id_index"
-   execute("alter table incident_updateinfo_counter_values add foreign key (project_id) references projects(id)")
+    create_table :incident_updateinfo_counter_values do |t|
+      t.references :updateinfo_counter, null: false
+      t.references :project,     null: false
+      t.integer    :value,       null: false
+      t.datetime   :released_at, null: false
+    end
+    add_index :incident_updateinfo_counter_values, [:updateinfo_counter_id, :project_id], :name => "uniq_id_index"
+    execute("alter table incident_updateinfo_counter_values add foreign key (project_id) references projects(id)")
+  
+    rename_table  :updateinfo_counter, :updateinfo_counters
 
-   rename_table  :updateinfo_counter, :updateinfo_counters
+    TempMI.all.each do |mi|
+      value = mi.counter
+      next unless value
 
-   TempMI.all.each do |mi|
-     value = mi.counter
-     next unless value
+      # guess the last one is valid, info is lacking in old model
+      uc = UpdateinfoCounter.where(maintenance_db_project_id: mi.maintenance_db_project_id).last
 
-     mp = mi.maintenance_db_project
+      p = Project.find_by_id mi.db_project_id
+      IncidentUpdateinfoCounterValue.create(updateinfo_counter: uc, project: p, value: value, released_at: p.updated_at)
+    end
 
-     # there could have been just one so far
-     uc = UpdateinfoCounter.find_by_maintenance_db_project( mp ).first
-
-     IncidentUpdateinfoCounterValue.create(updateinfo_counter: uc, project: mp, value: value)
-   end
-
-   remove_column :maintenance_incidents, :counter
-   remove_column :maintenance_incidents, :name
+    remove_column :maintenance_incidents, :counter
+    remove_column :maintenance_incidents, :name
+    remove_column :updateinfo_counters, :name
   end
 
   def self.down
     drop_table   :incident_updateinfo_counter_values
     add_column   :maintenance_incidents, :counter, :integer
     add_column   :maintenance_incidents, :name, :string
+    add_column   :updateinfo_counters, :name, :string
     rename_table :updateinfo_counters, :updateinfo_counter
   end
 end
