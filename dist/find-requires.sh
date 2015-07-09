f9c2e1ca8c4290fc2c9dfb6e08b199c29a55391b
Ruediger Oertel <ro@suse.de>
2015-07-09 17:51:46 +0200

    [dist] find_requires.sh: handle not installed bundler more gracefully

--- dist/find-requires.sh
+++ dist/find-requires.sh
@@ -11,7 +11,7 @@ tdir=`mktemp -d`
 tar xJf $sourcearchive -C $tdir >&/dev/null
 
 pushd $tdir/open-build-service*/src/api >& /dev/null
-
+ruby -rbundler -e 'exit' || echo "_ERROR_BUNDLER_NOT_INSTALLED_"
 ruby -rbundler -e 'Bundler.definition.resolve.to_a.each { |s| puts "rubygem('$prefix':#{s.name}) = #{s.version}" }' | grep -v ':webui' | while read i; do echo -n $i", "; done
 popd >& /dev/null
 
