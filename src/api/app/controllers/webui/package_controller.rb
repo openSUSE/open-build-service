09da519eaccc3f815acf8d516cbfd0010313550e
Björn Geuken <bgeuken@suse.de>
2015-07-16 14:56:40 +0200

    [webui] Refactor package controller - revisions action, part II

--- src/api/app/controllers/webui/package_controller.rb
+++ src/api/app/controllers/webui/package_controller.rb
@@ -179,8 +179,7 @@ class Webui::PackageController < Webui::WebuiController
       flash[:error] = 'Could not access revisions'
       redirect_to :action => :show, :project => @project.name, :package => @package.name and return
     end
-    @lastrev = @package.rev.to_i
-    @lastrev = params[:rev].to_i if params[:rev]
+    @lastrev = params[:rev].try(:to_i) || @package.rev.to_i
     if params[:showall] || @lastrev < 21
       @revisions = (1..@lastrev).to_a.reverse
     else
