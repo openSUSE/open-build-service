ef494f63530ba6275316e3076029dd8858a85378
Björn Geuken <bgeuken@suse.de>
2015-07-16 14:52:44 +0200

    [webui] Refactor package controller - revisions action

--- src/api/app/controllers/webui/package_controller.rb
+++ src/api/app/controllers/webui/package_controller.rb
@@ -181,15 +181,11 @@ class Webui::PackageController < Webui::WebuiController
     end
     @lastrev = @package.rev.to_i
     @lastrev = params[:rev].to_i if params[:rev]
-    if params[:showall]
+    if params[:showall] || @lastrev < 21
       @revisions = (1..@lastrev).to_a.reverse
     else
-      if @lastrev < 21
-        @revisions = (1..@lastrev).to_a.reverse
-      else
-        @revisions = []
-        @lastrev.downto(@lastrev-19) { |n| @revisions << n }
-      end
+      @revisions = []
+      @lastrev.downto(@lastrev-19) { |n| @revisions << n }
     end
   end
 
