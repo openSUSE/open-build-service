03c97705c18e2b882fe584a360f17c41507b2543
Björn Geuken <bgeuken@suse.de>
2015-07-21 11:32:33 +0200

    [api] Use a static hash variable instead of evaluating static data

--- src/api/app/models/buildresult.rb
+++ src/api/app/models/buildresult.rb
@@ -1,42 +1,37 @@
 class Buildresult < ActiveXML::Node
 
-  AVAIL_STATUS_VALUES = [
-    "succeeded",
-    "failed",
-    "unresolvable",
-    "broken",
-    "blocked",
-    "dispatching",
-    "scheduled",
-    "building",
-    "finished",
-    "signing",
-    "disabled",
-    "excluded",
-    "locked",
-    "deleting",
-    "unknown"
-  ]
-
-  @@status_hash = nil
+  AVAIL_STATUS_VALUES = {
+    succeeded:    0,
+    failed:       1,
+    unresolvable: 2,
+    broken:       3,
+    blocked:      4,
+    dispatching:  5,
+    scheduled:    6,
+    building:     7,
+    finished:     8,
+    signing:      9,
+    disabled:    10,
+    excluded:    11,
+    locked:      12,
+    deleting:    13,
+    unknown:     14
+  }
 
   def self.avail_status_values
-    AVAIL_STATUS_VALUES
+    AVAIL_STATUS_VALUES.keys.map(&:to_s)
   end
 
   def self.code2index(code)
-    unless @@status_hash
-      @@status_hash = Hash.new
-      AVAIL_STATUS_VALUES.each_with_index do |s,index|
-        @@status_hash[s] = index
-      end
+    index = AVAIL_STATUS_VALUES[code.to_sym]
+    if index
+      index
+    else
+      raise ArgumentError, "code '#{code}' unknown #{AVAIL_STATUS_VALUES.inspect}"
     end
-    raise ArgumentError, "code '#{code}' unknown #{@@status_hash.inspect}" unless @@status_hash[code]
-    @@status_hash[code]
   end
 
   def self.index2code(index)
-    AVAIL_STATUS_VALUES[index]
+    AVAIL_STATUS_VALUES.key(index)
   end
-
 end
