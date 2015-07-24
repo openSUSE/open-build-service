58435c5047178014a83d397a4f9ecdaa2e96de33
Adrian Schröter <adrian@suse.de>
2015-07-24 10:12:52 +0200

    [ci] code improved, avoid notice

--- src/api/test/unit/code_quality_test.rb
+++ src/api/test/unit/code_quality_test.rb
@@ -106,7 +106,7 @@ class CodeQualityTest < ActiveSupport::TestCase
       'UserLdapStrategy::find_with_ldap' => 119.04,
       'UserLdapStrategy::render_grouplist_ldap' => 100.3,
       'Webui::DriverUpdateController#save' => 97.16,
-      'Webui::PackageController#submit_request' => 149.9,
+      'Webui::PackageController#submit_request' => 140.91,
       'Webui::PatchinfoController#save' => 246.65,
       'Webui::ProjectController#check_devel_package_status' => 81.95, 
       'Webui::ProjectController#save_new' => 90,
