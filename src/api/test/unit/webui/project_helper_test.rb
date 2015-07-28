e5dfcdcfd8894bb2e980d6d8f3a7a32fd9119175
Björn Geuken <bgeuken@suse.de>
2015-07-28 15:44:24 +0200

    [webui] Fix project helper test

--- src/api/test/unit/webui/project_helper_test.rb
+++ src/api/test/unit/webui/project_helper_test.rb
@@ -5,17 +5,17 @@ class Webui::ProjectHelperTest < ActiveSupport::TestCase
 
   def test_patchinfo_rating_color
     color = patchinfo_rating_color('important')
-    color.must_equal 'red'
+    assert_equal 'red', color
   end
 
   def test_patchinfo_category_color
     color = patchinfo_category_color('security')
-    color.must_equal 'maroon'
+    assert_equal 'maroon', color
   end
 
   def test_request_state_icon
-    map_request_state_to_flag('new').must_equal 'flag_green'
-    map_request_state_to_flag(nil).must_equal ''
+    assert_equal map_request_state_to_flag('new'), 'flag_green'
+    assert_equal map_request_state_to_flag(nil),  ''
   end
 
 end
