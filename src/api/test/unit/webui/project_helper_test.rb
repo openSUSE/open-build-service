5d54ea834482ba1f7908e091128f81bb94f195b6
Björn Geuken <bgeuken@suse.de>
2015-07-29 11:59:34 +0200

    Revert "[webui] Fix project helper test"
    This reverts commit ae4538713f2fde3652b956be7d6cca8336065273.

--- src/api/test/unit/webui/project_helper_test.rb
+++ src/api/test/unit/webui/project_helper_test.rb
@@ -5,17 +5,17 @@ class Webui::ProjectHelperTest < ActiveSupport::TestCase
 
   def test_patchinfo_rating_color
     color = patchinfo_rating_color('important')
-    assert_equal 'red', color
+    color.must_equal 'red'
   end
 
   def test_patchinfo_category_color
     color = patchinfo_category_color('security')
-    assert_equal 'maroon', color
+    color.must_equal 'maroon'
   end
 
   def test_request_state_icon
-    assert_equal map_request_state_to_flag('new'), 'flag_green'
-    assert_equal map_request_state_to_flag(nil),  ''
+    map_request_state_to_flag('new').must_equal 'flag_green'
+    map_request_state_to_flag(nil).must_equal ''
   end
 
 end
