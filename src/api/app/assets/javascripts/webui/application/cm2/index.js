ec1120238877c5f421985dd9ffa3abd11263d9b9
chrisbr <cbruckmayer@suse.com>
2015-08-04 13:47:48 +0200

    [webui] Reformat code

--- src/api/app/assets/javascripts/webui/application/cm2/index.js
+++ src/api/app/assets/javascripts/webui/application/cm2/index.js
@@ -65,65 +65,66 @@
 
 var editors = new Array();
 
-function use_codemirror(id, read_only, mode)
-{
-    var codeMirrorOptions = {
-	lineNumbers: true,
-	matchBrackets: false,
-	fontSize: '9pt',
-	mode: mode
-    }
-    if (read_only) {
-	codeMirrorOptions['readOnly'] = true;
-    }
-    else {
-	codeMirrorOptions['addToolBars'] = 0;
-	if (mode.length)
-	    codeMirrorOptions['fileType'] = mode;
-        codeMirrorOptions['extraKeys'] = {"Tab": "defaultTab", "Shift-Tab": "indentLess"};
-    }
+function use_codemirror(id, read_only, mode) {
+  var codeMirrorOptions = {
+    lineNumbers: true,
+    matchBrackets: false,
+    fontSize: '9pt',
+    mode: mode
+  }
+  if (read_only) {
+    codeMirrorOptions['readOnly'] = true;
+  }
+  else {
+    codeMirrorOptions['addToolBars'] = 0;
+    if (mode.length)
+      codeMirrorOptions['fileType'] = mode;
+    codeMirrorOptions['extraKeys'] = {"Tab": "defaultTab", "Shift-Tab": "indentLess"};
+  }
 
-    var textarea = $('#editor_' + id);
-    var editor = CodeMirror.fromTextArea(document.getElementById("editor_" + id), codeMirrorOptions);
-    editor.id = id;
-    if (!read_only) {
-	editor.setSelections(editor);
-	
-        editor.on('change', function(cm) {
-	    changed=true; 
-	    cm.updateHistory(cm); 
-	    if (cm.historySize().undo>0)
-		$("#save_" + id).removeClass('inactive');
-	    else
-		$("#save_" + id).addClass('inactive');
-	});
-	CodeMirror.signal(editor, 'cursorActivity', editor);
+  var textarea = $('#editor_' + id);
+  var editor = CodeMirror.fromTextArea(document.getElementById("editor_" + id), codeMirrorOptions);
+  editor.id = id;
+  if (!read_only) {
+    editor.setSelections(editor);
 
-    }
+    editor.on('change', function (cm) {
+      changed = true;
+      cm.updateHistory(cm);
+      if (cm.historySize().undo > 0)
+        $("#save_" + id).removeClass('inactive');
+      else
+        $("#save_" + id).addClass('inactive');
+    });
+    CodeMirror.signal(editor, 'cursorActivity', editor);
 
-    if (textarea.data('save-url')) {
-	$('#save_' + id).click(function() {
-	    data = textarea.data('data');
-	    data[data['submit']] = editors[id].getValue();
-	    $("#save_" + id).addClass("inactive").addClass("working");
-            $.ajax({
-		url: textarea.data('save-url'),
-		type: (textarea.data('save-method') || 'put'),
-		data: data,
-		success: function(data, textStatus, xhdr) { $("#save_" + id).removeClass("working"); },
-		error: function(xhdr, textStatus,errorThrown) {
-		    $("#save_" + id).removeClass("inactive").removeClass("working"); 
-		    /* alert("XHR" + xhdr.responseText + " TS " + textStatus); */
-		},
-            });
-        });
-    } else {
-	$("#save_" + id).hide();
-    }
-    editors[id] = editor;
+  }
 
-    // $('#find_' + id).click(function() { editors[id].Find(this); });
-    //$('#line_' + id).keydown(function(event) { if(event.keyCode==13) { editors[id].gotoLine(this) }});
-    // $('#search_disable_' + id).click( function() { editors[id].Search(this) } );
+  if (textarea.data('save-url')) {
+    $('#save_' + id).click(function () {
+      data = textarea.data('data');
+      data[data['submit']] = editors[id].getValue();
+      $("#save_" + id).addClass("inactive").addClass("working");
+      $.ajax({
+        url: textarea.data('save-url'),
+        type: (textarea.data('save-method') || 'put'),
+        data: data,
+        success: function (data, textStatus, xhdr) {
+          $("#save_" + id).removeClass("working");
+        },
+        error: function (xhdr, textStatus, errorThrown) {
+          $("#save_" + id).removeClass("inactive").removeClass("working");
+          /* alert("XHR" + xhdr.responseText + " TS " + textStatus); */
+        },
+      });
+    });
+  } else {
+    $("#save_" + id).hide();
+  }
+  editors[id] = editor;
+
+  // $('#find_' + id).click(function() { editors[id].Find(this); });
+  //$('#line_' + id).keydown(function(event) { if(event.keyCode==13) { editors[id].gotoLine(this) }});
+  // $('#search_disable_' + id).click( function() { editors[id].Search(this) } );
 
 }
