54257e6faf1dffb5f5746d4936d1f9316c287f45
Moisés Déniz Alemán <mdeniz@suse.de>
2015-08-06 18:12:11 +0200

    [docs] Adding a basic guide of coding style

---
+++ docs/dev/GUIDE.md
@@ -0,0 +1,11 @@
+# Development Style Guide
+This development guide tries to show you how we style the code of OBS.
+
+## Models
+All the ActiveRecord::Base models should use the same structure to be easy to follow for everyone.
+
+### Model Template
+[Here](model_empty_template.rb) you have an empty template of the structure for the models.
+
+### Model Example
+For a better comprehension [here](model_template_example.rb) you have an example with code in it's place.
---
+++ docs/dev/model_empty_template.rb
@@ -0,0 +1,33 @@
+# Comment explaining what is this XXXX class for
+class XXXX < ActiveRecord::Base
+  #### Includes and extends ###################################################
+
+  #### Constants ##############################################################
+
+  #### Self config ############################################################
+
+  #### Attributes #############################################################
+
+  #### Associations macros (Belongs to, Has one, Has many) ####################
+
+  #### Callbacks macros: before_save, after_save, etc. ########################
+
+  #### Scopes (first the default_scope macro if is used) ######################
+
+  #### Validations macros #####################################################
+
+  #### Class methods using self. (public and then private) ####################
+  #### To define class methods as private use private_class_method
+
+  ### private
+
+
+  #### Instance methods (public and then protected/private) ###################
+
+  protected
+
+
+  private
+
+
+end
