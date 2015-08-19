5d5e45c8329843cb04c1775cdfffda642f22f99e
Henne Vogelsang <hvogel@opensuse.org>
2015-08-19 11:51:09 +0200

    [ci] Remove NOP cleanup function

--- dist/ci/obs_testsuite_travis.sh
+++ dist/ci/obs_testsuite_travis.sh
@@ -4,6 +4,9 @@
 # Be verbose and fail script on the first error
 set -xe
 
+# Everything happens here
+pushd src/api
+
 # Which test suite should run? By default: all
 if [ -z $1 ]; then
   TEST_SUITE="all"
@@ -11,7 +14,6 @@ else
   TEST_SUITE="$1"
 fi 
 
-pushd src/api
 
 if test -z "$SUBTEST"; then
   export DO_COVERAGE=1
@@ -27,21 +29,9 @@ if test -z "$SUBTEST"; then
       bundle exec rake rubocop 
       ;;
     *)
+      bundle exec rake rubocop
       bundle exec rake test:api
       bundle exec rake test:webui
-      bundle exec rake rubocop
       ;;
   esac
-fi
-
-cd ../..
-
-echo "Killing backend processes"
-if fuser -v $PWD | egrep 'perl|ruby'; then
-  list=`fuser -v $PWD 2>&1 | egrep 'perl|ruby' | sed -e "s,^ *$USER *,,;"  | cut '-d ' -f1`
-  for p in $list; do
-    echo "Kill $p"
-    # the process might have gone away on its own, so use || true
-    kill $p || true
-  done
 fi
