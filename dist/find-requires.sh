d7841527dfa794a0d3566717300a06f0352c33cd
Adrian Schröter <adrian@suse.de>
2015-08-21 10:56:28 +0200

    [dist] avoid skipping of webui rubygems

--- dist/find-requires.sh
+++ dist/find-requires.sh
@@ -12,7 +12,7 @@ tar xJf $sourcearchive -C $tdir >&/dev/null
 
 pushd $tdir/open-build-service*/src/api >& /dev/null
 ruby -rbundler -e 'exit' || echo "_ERROR_BUNDLER_NOT_INSTALLED_"
-ruby -rbundler -e 'Bundler.definition.resolve.to_a.each { |s| puts "rubygem('$prefix':#{s.name}) = #{s.version}" }' | grep -v ':webui' | while read i; do echo -n $i", "; done
+ruby -rbundler -e 'Bundler.definition.resolve.to_a.each { |s| puts "rubygem('$prefix':#{s.name}) = #{s.version}" }' | while read i; do echo -n $i", "; done
 popd >& /dev/null
 
 #cleanup
