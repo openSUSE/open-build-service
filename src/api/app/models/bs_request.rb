2130a161f75065200e4da5a6084c020e1b986169
Björn Geuken <bgeuken@suse.de>
2015-08-25 09:54:48 +0200

    [webui] Fix indentation

--- src/api/app/models/bs_request.rb
+++ src/api/app/models/bs_request.rb
@@ -117,10 +117,10 @@ class BsRequest < ActiveRecord::Base
 
   def self.open_requests_for_source(obj)
     if obj.kind_of? Project
-      return BsRequest.order(:id).where(state: [:new, :review, :declined]).joins(:bs_request_actions).
+      return BsRequest.order(:id).in_states([:new, :review, :declined]).
                 where(bs_request_actions: {source_project: obj.name})
     elsif obj.kind_of? Package
-      return BsRequest.order(:id).where(state: [:new, :review, :declined]).joins(:bs_request_actions).
+      return BsRequest.order(:id).in_states([:new, :review, :declined]).
                 where(bs_request_actions: {source_project: obj.project.name, source_package: obj.name})
     else
       raise "Invalid object #{obj.class}"
@@ -129,10 +129,10 @@ class BsRequest < ActiveRecord::Base
 
   def self.open_requests_for_target(obj)
     if obj.kind_of? Project
-      return BsRequest.order(:id).where(state: [:new, :review, :declined]).joins(:bs_request_actions).
+      return BsRequest.order(:id).in_states([:new, :review, :declined]).
                 where(bs_request_actions: {target_project: obj.name})
     elsif obj.kind_of? Package
-      return BsRequest.order(:id).where(state: [:new, :review, :declined]).joins(:bs_request_actions).
+      return BsRequest.order(:id).in_states([:new, :review, :declined]).
                 where(bs_request_actions: {target_project: obj.project.name, target_package: obj.name})
     else
       raise "Invalid object #{obj.class}"
