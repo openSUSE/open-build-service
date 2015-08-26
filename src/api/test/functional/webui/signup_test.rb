d27f674b3e5da6898928a203c9a691527044b40b
Björn Geuken <bgeuken@suse.de>
2015-08-26 14:56:18 +0200

    [webui] Update OBS redirect after login
    After login OBS users get redirected to the page they initially visited. So far
    this was done via hidden fields in the views and parameters that were processed
    in the controller.
    An attacker could use those parameters to redirect to an untrusted side.
    This commit stores the last visited page in the session store to avoid that kind
    of attack.

--- src/api/test/functional/webui/signup_test.rb
+++ src/api/test/functional/webui/signup_test.rb
@@ -12,7 +12,8 @@ class Webui::SignupTest < Webui::IntegrationTest
 
   def test_signup_allow
     signup_user root_path
-    flash_message.must_equal 'The account "eisendieter" is now active.'
+    flash_message.must_equal "The account 'eisendieter' is now active."
+    assert User.find_by(login: "eisendieter").is_active?
   end
 
   def test_signup_confirmation
