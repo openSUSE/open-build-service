961dc5156adeef1dc775d540b8d17a2658c53b36
Björn Geuken <bgeuken@suse.de>
2015-08-27 14:00:32 +0200

    [webui] Cleanup can_register method

--- src/api/app/models/unregistered_user.rb
+++ src/api/app/models/unregistered_user.rb
@@ -12,8 +12,9 @@ class UnregisteredUser < User
       logger.debug 'Someone tried to register with "ldap_mode" turned on'
       raise ErrRegisterSave.new 'Sorry, new users can only sign up via LDAP'
     end
+
     # No registering if we use an authentification proxy
-    if CONFIG['proxy_auth_mode'] == :on or CONFIG['ichain_mode'] == :on
+    if CONFIG['proxy_auth_mode'] == :on || CONFIG['ichain_mode'] == :on
       logger.debug 'Someone tried to register with "proxy_auth_mode" turned on'
       if CONFIG['proxy_auth_register_page'].blank?
         raise ErrRegisterSave.new "Sorry, please sign up using the authentification proxy"
@@ -21,16 +22,19 @@ class UnregisteredUser < User
         raise ErrRegisterSave.new "Sorry, please sign up using #{CONFIG['proxy_auth_register_page']}"
       end
     end
+
     # Turn off registration if its disabled
     if ::Configuration.registration == 'deny'
-      return if User.current and User.current.is_admin?
+      return if User.current && User.current.is_admin?
       logger.debug 'Someone tried to register but its disabled'
       raise ErrRegisterSave.new 'Sorry, sign up is disabled'
     end
+
     # Turn on registration if it's enabled
-    if ::Configuration.registration == 'allow' or ::Configuration.registration == 'confirmation'
+    if ::Configuration.registration == 'allow' || ::Configuration.registration == 'confirmation'
       return
     end
+
     # This shouldn't happen, but disable registration by default.
     logger.debug "Huh? This shouldn't happen. UnregisteredUser.can_register ran out of options"
     raise ErrRegisterSave.new 'Sorry, sign up is disabled'
