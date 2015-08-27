9bd8735eef4a0aadbc8d27c86853a0862a23039e
Björn Geuken <bgeuken@suse.de>
2015-08-27 13:16:48 +0200

    [webui] Fix indentation

--- src/api/test/functional/webui/signup_test.rb
+++ src/api/test/functional/webui/signup_test.rb
@@ -1,37 +1,36 @@
 require_relative '../../test_helper'
 
 class Webui::SignupTest < Webui::IntegrationTest
+  def signup_user page
+    # Signup a user
+    visit page
+    fill_in 'login', with: 'eisendieter'
+    fill_in 'email', with: 'dieter.eilts@werder.de'
+    fill_in 'pwd', with: 'alemao'
+    click_button('Sign Up')
+  end
 
-    def signup_user page
-      # Signup a user
-      visit page
-      fill_in 'login', with: 'eisendieter'
-      fill_in 'email', with: 'dieter.eilts@werder.de'
-      fill_in 'pwd', with: 'alemao'
-      click_button('Sign Up')
-    end
+  def test_signup_allow
+    signup_user root_path
+    flash_message.must_equal 'The account "eisendieter" is now active.'
+  end
 
-    def test_signup_allow
-      signup_user root_path
-      flash_message.must_equal 'The account "eisendieter" is now active.'
-    end
+  def test_signup_confirmation
+    # Configure confirmation for signups
+    ::Configuration.stubs(:registration).returns("confirmation")
+    signup_user root_path
+    flash_message.must_equal 'Thank you for signing up! An admin has to confirm your account now. Please be patient.'
+  end
 
-    def test_signup_confirmation
-      # Configure confirmation for signups
-      ::Configuration.stubs(:registration).returns("confirmation")
-      signup_user root_path
-      flash_message.must_equal 'Thank you for signing up! An admin has to confirm your account now. Please be patient.'
-    end
-
-    def test_signup_deny
-      # Configure denying signups
-      ::Configuration.stubs(:registration).returns("deny")
-      visit user_register_user_path
-      page.must_have_content "Sorry, sign up is disabled"
-      # but still works for admin
-      login_king
-      visit user_register_user_path
-      page.must_have_content "Sign Up for an Open Build Service account"
-    end
+  def test_signup_deny
+    # Configure denying signups
+    ::Configuration.stubs(:registration).returns("deny")
+    visit user_register_user_path
+    page.must_have_content "Sorry, sign up is disabled"
+    # but still works for admin
+    login_king
+    visit user_register_user_path
+    page.must_have_content "Sign Up for an Open Build Service account"
+  end
 end
 
