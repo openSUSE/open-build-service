e2930650767f0530cf810e0afafda1a014647864
Moisés Déniz Alemán <mdeniz@suse.de>
2015-08-28 14:41:57 +0200

    [webui] Move stuff inside BsRequestActionChangeDevel model to fit the template

--- src/api/app/models/bs_request_action_change_devel.rb
+++ src/api/app/models/bs_request_action_change_devel.rb
@@ -1,9 +1,23 @@
+#
 class BsRequestActionChangeDevel < BsRequestAction
+  #### Includes and extends
+  #### Constants
+  #### Self config
+  #### Attributes
+  #### Associations macros (Belongs to, Has one, Has many)
+  #### Callbacks macros: before_save, after_save, etc.
+  #### Scopes (first the default_scope macro if is used)
+  #### Validations macros
 
+  #### Class methods using self. (public and then private)
   def self.sti_name
     return :change_devel
   end
 
+  #### To define class methods as private use private_class_method
+  #### private
+
+  #### Instance methods (public and then protected/private)
   def execute_accept(opts)
     target_project = Project.get_by_name(self.target_project)
     target_package = target_project.packages.find_by_name(self.target_package)
@@ -12,4 +26,6 @@ class BsRequestActionChangeDevel < BsRequestAction
     target_package.resolve_devel_package
     target_package.store(comment: "change_devel request #{self.bs_request.id}", requestid: self.bs_request.id)
   end
+
+  #### Alias of methods
 end
