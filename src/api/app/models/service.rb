b6a5d0eeb3823fca33c6fe0480a75b2cfbc1a087
Moisés Déniz Alemán <mdeniz@suse.de>
2015-09-08 16:24:21 +0200

    [webui] Adapt to the template for models.

--- src/api/app/models/service.rb
+++ src/api/app/models/service.rb
@@ -1,112 +1,14 @@
+# Class to access and/or write the "_services" file on the Backend
 class Service < ActiveXML::Node
+  class InvalidParameter < APIException; end
 
+  #### Class methods using self. (public and then private)
   def self.make_stub(_opt)
     "<services/>"
   end
 
-  class InvalidParameter < APIException
-  end
-
-  def addDownloadURL(url, filename = nil)
-    begin
-      uri = URI.parse(url)
-    rescue
-      return false
-    end
-
-    # default for download_url and download_src_package
-    p = []
-    p << {:name => "host", :value => uri.host}
-    p << {:name => "protocol", :value => uri.scheme}
-    p << {:name => "path", :value => uri.path}
-    unless (uri.scheme == "http" and uri.port == 80) or (uri.scheme == "https" and uri.port == 443) or (uri.scheme == "ftp" and uri.port == 21)
-      # be nice and skip it for simpler _service file
-      p << {:name => "port", :value => uri.port}
-    end
-
-    if uri.path =~ /.src.rpm$/ or uri.path =~ /.spm$/
-      # download and extract source package
-      addService("download_src_package", -1, p)
-    elsif uri.scheme == "git"
-      p = []
-      p << {:name => "scm", :value => "git"}
-      p << {:name => "url", :value => url}
-      addService("tar_scm", -1, p)
-      p = []
-      p << {:name => "compression", :value => "xz"}
-      p << {:name => "file", :value => "*.tar"}
-      addService("recompress", -1, p)
-      addService("set_version")
-    else
-      # just download
-      p << {:name => "filename", :value => filename} unless filename.blank?
-      addService("download_url", -1, p)
-    end
-    return true
-  end
-
-  def removeService(serviceid)
-    each("/services/service") do |service|
-      serviceid=serviceid-1
-      if serviceid == 0
-        delete_element service
-        return true
-      end
-    end
-    return false
-  end
-
-  def fill_params(element, parameters)
-    parameters.each { |p|
-      param = element.add_element('param', :name => p[:name])
-      param.text = p[:value]
-    }
-    true
-  end
-
-  # parameters need to be given as an array with hash pairs :name and :value
-  def addService(name, position = -1, parameters = [])
-    element = add_element 'service', 'name' => name
-    if position >= 0
-      service_elements = each("/services/service")
-      if service_elements.count >= position
-        element.move_before(service_elements[position-1])
-      end
-    end
-    fill_params(element, parameters)
-  end
-
-  def error
-    opt = Hash.new
-    opt[:project] = self.init_options[:project]
-    opt[:package] = self.init_options[:package]
-    opt[:expand] = self.init_options[:expand]
-    opt[:rev] = self.init_options[:revision]
-    begin
-      fc = FrontendCompat.new
-      answer = fc.get_source opt
-      doc = ActiveXML::Node.new(answer)
-      doc.each('/directory/serviceinfo/error') do |e|
-        return e.text
-      end
-    rescue
-      return nil
-    end
-  end
-
-  def execute
-    opt = Hash.new
-    opt[:project] = self.init_options[:project]
-    opt[:package] = self.init_options[:package]
-    opt[:expand] = self.init_options[:expand]
-    opt[:cmd] = 'runservice'
-    logger.debug 'execute services'
-    fc = FrontendCompat.new
-    fc.do_post nil, opt
-  end
-
   def self.valid_name?(name)
-    return false unless name.kind_of? String
+    return false unless name.kind_of?(String)
     return false if name.length > 200 || name.blank?
     return false if name =~ %r{^[_\.]}
     return false if name =~ %r{::}
@@ -115,14 +17,109 @@ class Service < ActiveXML::Node
   end
 
   def self.verify_xml!(raw_post)
-    data = Xmlhash.parse(raw_post)
-    data.elements('service').each do |s|
+    Xmlhash.parse(raw_post).elements('service').each do |s|
       raise InvalidParameter.new "service name #{s['name']} contains invalid chars" unless valid_name?(s['name'])
       s.elements('param').each do |p|
         raise InvalidParameter.new "service parameter #{p['name']} contains invalid chars" unless valid_name?(p['name'])
       end
     end
   end
+
+  #### Instance methods (public and then protected/private)
+  def addDownloadURL(url, filename = nil)
+    begin
+      uri = URI.parse(url)
+    rescue
+      return false
+    end
+
+    # default for download_url and download_src_package
+    service_content = [
+      {:name => "host", :value => uri.host},
+      {:name => "protocol", :value => uri.scheme},
+      {:name => "path", :value => uri.path}
+      ]
+    unless (uri.scheme == "http" && uri.port == 80) ||
+           (uri.scheme == "https" && uri.port == 443) ||
+           (uri.scheme == "ftp" && uri.port == 21)
+      service_content << {:name => "port", :value => uri.port} # be nice and skip it for simpler _service file
+    end
+
+    if uri.path =~ /.src.rpm$/ || uri.path =~ /.spm$/ # download and extract source package
+      addService("download_src_package", -1, service_content)
+    elsif uri.scheme == "git"
+      service_content = [{:name => "scm", :value => "git"}, {:name => "url", :value => url}]
+      addService("tar_scm", -1, service_content)
+      service_content = [{:name => "compression", :value => "xz"}, {:name => "file", :value => "*.tar"}]
+      addService("recompress", -1, service_content)
+      addService("set_version")
+    else # just download
+      service_content << {:name => "filename", :value => filename} unless filename.blank?
+      addService("download_url", -1, service_content)
+    end
+    true
+  end
+
+  def removeService(serviceid)
+    each("/services/service") do |service|
+      serviceid -= 1
+      if serviceid == 0
+        delete_element service
+        return true
+      end
+    end
+    false
+  end
+
+  def fill_params(element, parameters)
+    parameters.each { |parameter|
+      param = element.add_element('param', :name => parameter[:name])
+      param.text = parameter[:value]
+    }
+    true
+  end
+
+  # parameters need to be given as an array with hash pairs :name and :value
+  def addService(name, position = -1, parameters = [])
+    element = add_element('service', 'name' => name)
+    if position >= 0
+      service_elements = each("/services/service")
+      element.move_before(service_elements[position-1]) if service_elements.count >= position
+    end
+    fill_params(element, parameters)
+  end
+
+  def error
+    opt = {
+      project: self.init_options[:project],
+      package: self.init_options[:package],
+      expand: self.init_options[:expand],
+      rev: self.init_options[:revision]
+    }
+    begin
+      fc = FrontendCompat.new
+      answer = fc.get_source opt
+      doc = ActiveXML::Node.new(answer)
+      doc.each('/directory/serviceinfo/error') do |e|
+        return e.text
+      end
+    rescue
+      nil
+    end
+  end
+
+  def execute
+    opt = {
+      project: self.init_options[:project],
+      package: self.init_options[:package],
+      expand: self.init_options[:expand],
+      cmd: 'runservice'
+    }
+    logger.debug 'execute services'
+    fc = FrontendCompat.new
+    fc.do_post nil, opt
+  end
+
 
   def save
     if !has_element?('/services/service')
@@ -138,5 +135,4 @@ class Service < ActiveXML::Node
     end
     true
   end
-
 end
