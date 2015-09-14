4d4accddb1888ec9a027c18d1872b129ff6cb00b
Adrian Schröter <adrian@suse.de>
2015-09-14 16:45:57 +0200

    [api] fix rollback of migration

--- src/api/db/migrate/20150902130939_remove_index_from_path_element_position.rb
+++ src/api/db/migrate/20150902130939_remove_index_from_path_element_position.rb
@@ -1,5 +1,8 @@
 class RemoveIndexFromPathElementPosition < ActiveRecord::Migration
-  def change
+  def self.up
     remove_index :path_elements, name: :parent_repo_pos_index
+  end
+  def self.down
+    add_index :path_elements, [:parent_id, :position], unique: true, name: :parent_repo_pos_index
   end
 end
