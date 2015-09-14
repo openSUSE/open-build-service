bbf4abf7ea3d32be05bae07ff2d0c9b71520c979
Moisés Déniz Alemán <mdeniz@suse.de>
2015-09-14 15:15:54 +0200

    [webui] Emptying test cases and images repo that wil not be used.

--- src/api/test/functional/webui/driver_update_controller_test.rb
+++ src/api/test/functional/webui/driver_update_controller_test.rb
@@ -14,10 +14,7 @@ class Webui::DriverUpdateControllerTest < Webui::IntegrationTest
 
   # TODO: End this test cases if they should. We saw that the DriverUpdateController is unused.
   def test_create
-    visit driver_update_create_path(package: 'TestPack', project: 'home:Iggy')
-    page.must_have_text 'Driver update disk wizard'
-    fill_in 'name', with: 'Test Driver Update Image'
-    check 'arch[x86_64]'
+
   end
 
   # TODO: End this test cases if they should. We saw that the DriverUpdateController is unused.
