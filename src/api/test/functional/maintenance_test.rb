openSUSE/open-build-service

commit 8b4d426e63a02440ca76359f6a163ae17cdd6ca3

[webui][api] Get rid of WebuiProject in webui/project_controller.rb * Make some of the rubocop metric rules not fail * revoke requests when projects get deleted * Introduce set_project before_filter and use it * Limit :require_project before filter (and hence WebuiProject) to move_path_*   The path_elements list of repositories needs a little more thought.   acts_as_list would be an option but that means removing the uniqe key   in the db for partent_id and position * Move all the support methods down in the file * Move some of the methods into the project model * authorize all calls that change the project * Rename CRUD the methods * Use strong parameters * Concat all the stupid history_element files into one for lazy loading * Also fix the history_elements fixtures, rake db:fixtures:load works again now...

src/api/app/controllers/source_controller.rb
@@ -187,7 +187,6 @@ class SourceController < ApplicationController
       path = pro.source_path
       path << build_query_from_hash(params, [:user, :comment])
 
-      pro.revoke_requests
       pro.destroy
 
       Suse::Backend.delete path

src/api/app/models/project.rb
@@ -5,7 +5,6 @@ class Project < ActiveRecord::Base
   include FlagHelper
   include CanRenderModel
   include HasRelationships
-  has_many :relationships, dependent: :destroy, inverse_of: :project
   include HasRatings
   include HasAttributes
 
@@ -39,13 +38,18 @@ class Project < ActiveRecord::Base
   after_rollback 'Relationship.discard_cache'
   after_initialize :init
 
+  has_many :relationships, dependent: :destroy, inverse_of: :project
   has_many :packages, :dependent => :destroy, inverse_of: :project do
     def autocomplete(search)
       where(['lower(packages.name) like lower(?)',"#{search}%"])
     end
   end
   has_many :attribs, :dependent => :destroy
+
   has_many :repositories, :dependent => :destroy, foreign_key: :db_project_id
+  has_many :repository_architectures, -> { order("position") }, :dependent => :destroy, through: :repositories
+  has_many :architectures, -> { order("position") }, :through => :repository_architectures
+
   has_many :messages, :as => :db_object, :dependent => :delete_all
   has_many :watched_projects, :dependent => :destroy, inverse_of: :project
 
@@ -130,10 +134,39 @@ class Project < ActiveRecord::Base
         pkg.save
       end
     end
+
+    # revoke all requests
+    revoke_requests
+  end
+
+  def delete_on_backend(comment = nil)
+    path = source_path
+    path << Suse::Backend.build_query_from_hash({user: User.current.login, comment: comment}, [:user, :comment])
+    Suse::Backend.delete path
   end
 
   def subprojects
     Project.where("name like ?", "#{name}:%")
+  end
+
+  # Check if the project has a path_element matching project and repository
+  def has_distribution(project_name, repository)
+    project = Project.find_by(name: project_name)
+    return false unless project
+    repositories.joins(path_elements: :link).where(links_path_elements: {name: repository, db_project_id: project.id  }).exists?
+  end
+
+  def number_of_build_problems
+    begin
+      result = ActiveXML.backend.direct_http("/build/#{URI.escape(name)}/_result?view=status&code=failed&code=broken&code=unresolvable")
+    rescue ActiveXML::Transport::NotFoundError
+      return 0
+    end
+    ret = {}
+    Xmlhash.parse(result).elements('result') do |r|
+      r.elements('status') { |p| ret[p['package']] = 1 }
+    end
+    ret.keys.size
   end
 
   def revoke_requests
@@ -408,6 +441,27 @@ class Project < ActiveRecord::Base
     !self.remoteurl.nil?
   end
 
+  def can_free_repositories?
+    expand_all_repositories.each do |repository|
+      if !User.current.can_modify_project?(repository.project)
+        errors.add(:base, "a repository in project #{repository.project.name} depends on this")
+        return false
+      end
+    end
+    return true
+  end
+
+  def can_be_really_deleted?
+    begin
+      can_be_deleted?
+    rescue DeleteError
+      false
+    end
+    # Get all my repositories and linking_repositories and check if I can modify the
+    # associated projects
+    can_free_repositories?
+  end
+
   # NOTE: this is no permission check, should it be added ?
   def can_be_deleted?
     # check all packages
@@ -429,7 +483,6 @@ class Project < ActiveRecord::Base
         raise DeleteError.new 'This maintenance project has incident projects and can therefore not be deleted.'
       end
     end
-
   end
 
   def update_from_xml(xmlhash, force=nil)
@@ -488,6 +541,8 @@ class Project < ActiveRecord::Base
     #--- end update repositories ---#
     self.updated_at = Time.now
   end
+
+
 
   def update_repositories(xmlhash, force)
     fill_repo_cache
@@ -939,7 +994,15 @@ class Project < ActiveRecord::Base
     return nil
   end
 
-  def expand_all_projects(project_map={})
+  def expand_all_repositories
+    all_repositories = self.repositories.to_a
+    repositories.each do |repository|
+      all_repositories.concat(repository.expand_all_repositories)
+    end
+    all_repositories.uniq
+  end
+
+  def expand_all_projects(project_map={}, allow_remote_projects=true)
     # cycle check
     return [] if project_map[self]
     project_map[self] = 1
@@ -949,7 +1012,9 @@ class Project < ActiveRecord::Base
     # add all linked and indirect linked projects
     self.linkedprojects.each do |lp|
       if lp.linked_db_project.nil?
-        projects << lp.linked_remote_project_name
+        if allow_remote_projects
+          projects << lp.linked_remote_project_name
+        end
       else
         lp.linked_db_project.expand_all_projects(project_map).each do |p|
           projects << p
@@ -972,6 +1037,21 @@ class Project < ActiveRecord::Base
     return projects
   end
 
+  def packages_from_linked_projects
+    linkedprojects.each do |project|
+      next if project.linked_db_project.nil?
+      project.linked_db_project.packages_from_linked_projects.each do |name, prj_id|
+        unless p_map[name]
+          packages << [name, prj_id]
+          p_map[name] = 1
+        end
+      end
+    end
+  end
+
+  def linked_packages
+    expand_all_projects({}, false).reject{|project| project == self}.map {|project| project.packages.to_a }.flatten
+  end
 
   # return array of [:name, :project_id] tuples
   def expand_all_packages(project_map={}, package_map={})
@@ -1542,11 +1622,24 @@ class Project < ActiveRecord::Base
     Suse::Backend.get(source_path(file, opts)).body
   end
 
+  # FIXME: will be cleaned up after implementing FATE #308899
+  def prepend_kiwi_config
+    prjconf = source_file('_config')
+    unless prjconf =~ /^Type:/
+      prjconf = "%if \"%_repository\" == \"images\"\nType: kiwi\nRepotype: none\nPatterntype: none\n%endif\n" << prjconf
+      Suse::Backend.put_source(self.source_path('_config'), prjconf)
+    end
+  end
+
   def api_obj
     self
   end
 
   def to_s
+    name
+  end
+
+  def to_param
     name
   end
 

src/api/test/functional/request_controller_test.rb
@@ -196,6 +196,7 @@ class RequestControllerTest < ActionDispatch::IntegrationTest
                      {"who"=>"Iggy", "when"=>"2010-07-12T00:00:01", "description"=>"Request got a new priority: critical => low", "comment"=>"dontcare"},
                      {"who"=>"Iggy", "when"=>"2010-07-12T00:00:02", "description"=>"Request got declined", "comment"=>"notgood"},
                      {"who"=>"Iggy", "when"=>"2010-07-12T00:00:03", "description"=>"Request got reopened", "comment"=>"oops"},
+                     {"who"=>"Iggy", "when"=>"2010-07-12T00:00:04", "description"=>"Request got revoked", "comment"=>"The source project 'home:Iggy:branches:home:Iggy' was removed"},
                      {"who"=>"Iggy", "when"=>"2010-07-12T00:00:04", "description"=>"Request got accepted", "comment"=>"approved"}
                    ],
                    'description' => 'DESCRIPTION IS HERE'

src/api/.rubocop.yml
@@ -19,7 +19,10 @@ Metrics/LineLength:
     - 'db/**/*'
 
 Metrics/ClassLength:
-  Severity: warning
+  Severity: refactor
+
+Metrics/ModuleLength:
+  Severity: refactor
 
 Style/CommandLiteral:
   EnforcedStyle: percent_x

src/api/app/controllers/webui/package_controller.rb
@@ -15,7 +15,8 @@ class Webui::PackageController < Webui::WebuiController
 
   helper 'webui/comment'
 
-  before_filter :require_project, :except => [:submit_request, :devel_project]
+  before_filter :require_project, :except => [:submit_request, :devel_project, :users]
+  before_filter :set_project, :only => [:repositories, :users]
   before_filter :require_package, :except => [:submit_request, :save_new_link, :save_new, :devel_project ]
   # make sure it's after the require_, it requires both
   before_filter :require_login, :except => [:show, :linking_packages, :show , :linking_packages , :dependency,
@@ -170,8 +171,8 @@ class Webui::PackageController < Webui::WebuiController
   end
 
   def users
-    @users = [@project.api_obj.users, @package.users].flatten.uniq
-    @groups = [@project.api_obj.groups, @package.groups].flatten.uniq
+    @users = [@project.users, @package.users].flatten.uniq
+    @groups = [@project.groups, @package.groups].flatten.uniq
     @roles = Role.local_roles
   end
 

src/api/app/controllers/webui/webui_controller.rb
@@ -103,6 +103,15 @@ class Webui::WebuiController < ActionController::Base
     session[:return_path] = path
   end
 
+  def set_project
+    @project = Project.find_by(name: params[:project])
+    raise ActiveRecord::RecordNotFound unless @project
+  end
+
+  def set_project_by_id
+    @project = Project.find(params[:id])
+  end
+
   protected
 
   def set_return_to
@@ -326,7 +335,7 @@ class Webui::WebuiController < ActionController::Base
   end
 
   def require_available_architectures
-    @available_architectures = Architecture.where(available: 1)
+    @available_architectures = Architecture.available
   end
 
   def setup_view_path

src/api/app/helpers/webui/webui_helper.rb
@@ -111,10 +111,16 @@ module Webui::WebuiHelper
     elsif %w(- excluded).include? code
       out += code
     elsif @localpackages and not @localpackages.has_key? packname
-      out += link_to( code.gsub(/\s/, '&nbsp;'), raw_logfile_path(package: packname,
-                                                                  project: @project.to_s,
-                                                                  arch: arch, repository: repo),
-                      title: link_title, rel: 'nofollow')
+      # Scheduled packages have no raw log file...
+      if 'scheduled' == code
+        out += code
+      else
+        out += link_to( code.gsub(/\s/, '&nbsp;'),
+                        raw_logfile_path(package: packname,
+                                         project: @project.to_s,
+                                         arch: arch, repository: repo),
+                        title: link_title, rel: 'nofollow')
+      end
     else
       out += link_to code.gsub(/\s/, '&nbsp;'), { action: :live_build_log,
                                                   package: packname, project: @project.to_s, arch: arch,

commit 90d50c9f036894db092cf87bec0110d39b7f26ca

[api] hot-fix accepting of maintenance incident requests with cleanup again
Regression from 8b4d426e63a02440ca76359f6a163ae17cdd6ca3

Doing request revoke in model is right, but we need to be
able to opt-out the request which is currently processed

src/api/app/controllers/source_controller.rb
@@ -185,6 +185,7 @@ class SourceController < ApplicationController
       path = project.source_path
       path << build_query_from_hash(params, [:user, :comment])
 
+      project.revoke_requests
       project.destroy
 
       Suse::Backend.delete path

src/api/app/models/project.rb
@@ -136,7 +136,9 @@ class Project < ActiveRecord::Base
     end
 
     # revoke all requests
-    revoke_requests
+    # FIXME: this is breaking request accepting with cleanup, if the project
+    #        will be removed during that
+#    revoke_requests
   end
 
   def delete_on_backend(comment = nil)

src/api/test/functional/maintenance_test.rb
@@ -225,6 +225,9 @@ class MaintenanceTests < ActionDispatch::IntegrationTest
                                    <action type="maintenance_incident">
                                      <source project="home:tom:branches:kde4" package="kdelibs" />
                                      <target project="My:Maintenance" releaseproject="BaseDistro2.0" />
+                                     <options>
+                                       <sourceupdate>cleanup</sourceupdate>
+                                     </options>
                                    </action>
                                    <description>To fix my bug</description>
                                    <state name="new" />
@@ -252,6 +255,10 @@ class MaintenanceTests < ActionDispatch::IntegrationTest
     # accept request
     prepare_request_with_user 'maintenance_coord', 'power'
 
+    # not allowed to remove project
+    delete "/source/home:tom:branches:kde4"
+    assert_response 403
+
     post "/request/#{id2}?cmd=changestate&newstate=accepted&force=1"
     assert_response :success
     get "/request/#{id2}"
@@ -261,6 +268,9 @@ class MaintenanceTests < ActionDispatch::IntegrationTest
     assert_xml_tag( :tag => 'acceptinfo' )
     post "/request/#{id2}?cmd=diff&view=xml", nil
     assert_response :success
+    # project got cleaned up
+    get "/source/home:tom:branches:kde4"
+    assert_response 404
 
     get "/request/#{id2}"
     assert_response :success
@@ -288,8 +298,6 @@ class MaintenanceTests < ActionDispatch::IntegrationTest
     delete "/source/#{incidentProject}"
     assert_response :success
     delete '/source/BaseDistro2.0:LinkedUpdateProject/kdelibs'
-    assert_response :success
-    delete '/source/home:tom:branches:kde4'
     assert_response :success
   end
 

src/api/test/functional/request_controller_test.rb
@@ -196,7 +196,6 @@ class RequestControllerTest < ActionDispatch::IntegrationTest
                      {"who"=>"Iggy", "when"=>"2010-07-12T00:00:01", "description"=>"Request got a new priority: critical => low", "comment"=>"dontcare"},
                      {"who"=>"Iggy", "when"=>"2010-07-12T00:00:02", "description"=>"Request got declined", "comment"=>"notgood"},
                      {"who"=>"Iggy", "when"=>"2010-07-12T00:00:03", "description"=>"Request got reopened", "comment"=>"oops"},
-                     {"who"=>"Iggy", "when"=>"2010-07-12T00:00:04", "description"=>"Request got revoked", "comment"=>"The source project 'home:Iggy:branches:home:Iggy' was removed"},
                      {"who"=>"Iggy", "when"=>"2010-07-12T00:00:04", "description"=>"Request got accepted", "comment"=>"approved"}
                    ],
                    'description' => 'DESCRIPTION IS HERE'