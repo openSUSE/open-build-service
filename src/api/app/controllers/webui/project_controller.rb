2741247b6edb913f8e76a69715f2f9368ae01208
Björn Geuken <bgeuken@suse.de>
2015-09-30 14:41:15 +0200

    [webui] Fix N+1 query in project controller

--- src/api/app/controllers/webui/project_controller.rb
+++ src/api/app/controllers/webui/project_controller.rb
@@ -835,10 +835,7 @@ class Webui::ProjectController < Webui::WebuiController
   end
 
   def maintained_projects
-    @maintained_projects = []
-    @project.maintained_projects.each do |maintained_project|
-       @maintained_projects << maintained_project.project.name
-    end
+    @maintained_projects = @project.maintained_projects.includes(:project).pluck('projects.name')
   end
 
   def add_maintained_project_dialog
@@ -965,10 +962,7 @@ class Webui::ProjectController < Webui::WebuiController
           where("release_targets.trigger = 'maintenance'")
       @open_maintenance_incidents = subprojects.pluck('projects.name').sort.uniq
 
-      @maintained_projects = []
-      @project.maintained_projects.each do |mp|
-        @maintained_projects << mp.project.name
-      end
+      @maintained_projects = @project.maintained_projects.includes(:project).pluck('projects.name')
     end
     @is_incident_project = @project.is_maintenance_incident?
     if @is_incident_project
