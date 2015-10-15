a69fffe729e1a2f306c9fbc899a8d7a9e55230e3
Henne Vogelsang <hvogel@opensuse.org>
2015-10-15 16:24:14 +0200

    [webui] Raise if we have no user during policy check

--- src/api/app/policies/application_policy.rb
+++ src/api/app/policies/application_policy.rb
@@ -2,6 +2,7 @@ class ApplicationPolicy
   attr_reader :user, :record
 
   def initialize(user, record)
+    raise Pundit::NotAuthorizedError, "must be logged in" unless user
     @user = user
     @record = record
   end
