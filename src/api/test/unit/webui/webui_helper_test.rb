9780165bad1782e64ed496243cd7491934674d47
Christian Bruckmayer <cbruckmayer@suse.de>
2015-10-15 16:33:35 +0200

    [ci] Improve webui_helper test#escape_nested_list
    Cherry picked from master
    6b4c202569f1f72b66038a1d258dddeefb519055

--- src/api/test/unit/webui/webui_helper_test.rb
+++ src/api/test/unit/webui/webui_helper_test.rb
@@ -2,11 +2,13 @@ require 'test_helper'
 
 class Webui::WebuiHelperTest < ActiveSupport::TestCase
   include Webui::WebuiHelper
-  
-  def test_escape_project_list_escaped_forbidden_chars
-    input = ['<p>home:Iggy</p>', '<p>This is a paragraph</p>']
-    output = "['&lt;p&gt;home:Iggy&lt;/p&gt;', '&lt;p&gt;This is a paragraph&lt;/p&gt;']"
-    assert escape_nested_list(input), output
+
+  def test_escape_nested_list_escapes_forbidden_chars
+    input = [['<p>home:Iggy</p>', '<p>This is a paragraph</p>'], ['<p>home:Iggy</p>', '<p>"This is a paragraph"</p>']]
+    output = "['&lt;p&gt;home:Iggy&lt;\\/p&gt;', '&lt;p&gt;This is a paragraph&lt;\\/p&gt;'],\n"
+    output += "['&lt;p&gt;home:Iggy&lt;\\/p&gt;', '&lt;p&gt;\\&quot;This is a paragraph\\&quot;&lt;\\/p&gt;']"
+
+    assert_equal escape_nested_list(input), output
   end
 
   def test_escape_list_escapes_forbidden_chars
