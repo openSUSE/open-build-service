2cd4395f2f9e409f4fa4e014736ac5bd573ed4b0
Henne Vogelsang <hvogel@opensuse.org>
2015-10-19 14:55:56 +0200

    [api] Sum up squeues in WorkerStatus

--- src/api/test/unit/worker_status_test.rb
+++ src/api/test/unit/worker_status_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class WorkerStatusTest < ActiveSupport::TestCase
 
   test "update cache" do
+    Suse::Backend.wait_for_scheduler_start
     WorkerStatus.new.update_workerstatus_cache
   end
 
