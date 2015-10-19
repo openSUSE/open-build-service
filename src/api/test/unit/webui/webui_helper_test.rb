d66b4ff2b415ec88ddc4d986d6505fc129a2d92d
Björn Geuken <bgeuken@suse.de>
2015-10-19 15:31:33 +0200

    [ci] Merge webui_helper_test files together

--- src/api/test/unit/webui/webui_helper_test.rb
+++ src/api/test/unit/webui/webui_helper_test.rb
@@ -8,4 +8,14 @@ class Webui::WebuiHelperTest < ActiveSupport::TestCase
     output = "['&lt;p&gt;home:Iggy&lt;/p&gt;', '&lt;p&gt;This is a paragraph&lt;/p&gt;']"
     assert escape_nested_list(input), output
   end
+
+  def test_format_projectname
+    assert_equal "some:project:foo:bar", format_projectname("some:project:foo:bar", "bob")
+    assert_equal "~", format_projectname("home:bob", "bob")
+    assert_equal "~alice", format_projectname("home:alice", "bob")
+    assert_equal "~:foo", format_projectname("home:bob:foo", "bob")
+    assert_equal "~alice:foo", format_projectname("home:alice:foo", "bob")
+    assert_equal "~:branch", format_projectname("home:bob:branch", "bob")
+    assert_equal "~alice:branch", format_projectname("home:alice:branch", "bob")
+  end
 end
