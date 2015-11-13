ce737cd856cdb47719f5ebd2e8f75a4b691bba64
Stephan Kulow <coolo@suse.de>
2015-11-13 13:09:27 +0100

    [api] make sure the event migration works with old delay jobs present

--- src/api/app/jobs/send_event_emails.rb
+++ src/api/app/jobs/send_event_emails.rb
@@ -1,5 +1,10 @@
 class SendEventEmails
 
+  # we don't need this outside of the migration - but we need to be able
+  # to load old jobs from the database (and mark their events) before deleting
+  # (see 20151030130011_mark_events)
+  attr_accessor :event
+
   def perform
     Event::Base.where(mails_sent: false).order(:created_at).limit(1000).each do |event|
       event.mails_sent = true
