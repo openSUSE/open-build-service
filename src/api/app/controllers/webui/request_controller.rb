1880354e371cf96c6504e09b2cef755250c8db4b
Henne Vogelsang <hvogel@opensuse.org>
2015-12-02 14:41:57 +0100

    [api] Sync projects in changerequest to the backend

--- src/api/app/controllers/webui/request_controller.rb
+++ src/api/app/controllers/webui/request_controller.rb
@@ -176,6 +176,7 @@ class Webui::RequestController < Webui::WebuiController
             # are allowed to modify the object
             target.add_user(@req.creator, 'maintainer')
             target.save
+            target.store if target.kind_of? Project
           end
         end
       end
