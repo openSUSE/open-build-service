e834fae28bf4ee3e6aab7c3e846fdbf1584d5b33
Henne Vogelsang <hvogel@opensuse.org>
2016-01-20 12:29:20 +0100

    [ci] Use the User factory in Sign Up spec

--- src/api/spec/features/webui/sign_up_spec.rb
+++ src/api/spec/features/webui/sign_up_spec.rb
@@ -1,16 +1,18 @@
 require "browser_helper"
 
 RSpec.feature "Sign up", :type => :feature do
+  let!(:user) { build(:user) }
+
   scenario "User" do
     visit root_path
 
-    fill_in 'login', with: 'eisendieter'
-    fill_in 'email', with: 'dieter.eilts@werder.de'
+    fill_in 'login', with: user.login
+    fill_in 'email', with: user.email
     fill_in 'pwd', with: 'alemao'
     click_button('Sign Up')
 
-    expect(page).to have_text("The account 'eisendieter' is now active.")
-    assert User.find_by(login: "eisendieter").is_active?
+    expect(page).to have_text("The account '#{user.login}' is now active.")
+    assert User.find_by(login: user.login).is_active?
   end
 
   scenario "User with confirmation" do
@@ -19,8 +21,8 @@ RSpec.feature "Sign up", :type => :feature do
 
     visit root_path
 
-    fill_in 'login', with: 'eisendieter'
-    fill_in 'email', with: 'dieter.eilts@werder.de'
+    fill_in 'login', with: user.login
+    fill_in 'email', with: user.email
     fill_in 'pwd', with: 'alemao'
     click_button('Sign Up')
 
