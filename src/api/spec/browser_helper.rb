89ccc096c1410f44bbe56438a946db4ffef09c45
Henne Vogelsang <hvogel@opensuse.org>
2016-01-20 11:39:13 +0100

    [dist] Make poltergeist available in browser_helper

--- src/api/spec/browser_helper.rb
+++ src/api/spec/browser_helper.rb
@@ -3,3 +3,7 @@ require 'rails_helper'
 
 # for capybara rspec support
 require 'support/capybara'
+require 'capybara/poltergeist'
+
+# support log in/log out...
+require 'support/functional/authentification'
