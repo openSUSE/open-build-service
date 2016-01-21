68c6dca0b542a45e6ba32f616e48c77354781544
Christian Bruckmayer <christian@bruckmayer.net>
2016-01-21 15:43:03 +0100

    Add obsolete comment to tests

--- src/api/test/functional/webui/signup_test.rb
+++ src/api/test/functional/webui/signup_test.rb
@@ -10,20 +10,20 @@ class Webui::SignupTest < Webui::IntegrationTest
     click_button('Sign Up')
   end
 
-  def test_signup_allow
+  def test_signup_allow # -> spec/features/webui/sign_up_spec.rb
     signup_user root_path
     flash_message.must_equal "The account 'eisendieter' is now active."
     assert User.find_by(login: "eisendieter").is_active?
   end
 
-  def test_signup_confirmation
+  def test_signup_confirmation # -> spec/features/webui/sign_up_spec.rb
     # Configure confirmation for signups
     ::Configuration.stubs(:registration).returns("confirmation")
     signup_user root_path
     flash_message.must_equal 'Thank you for signing up! An admin has to confirm your account now. Please be patient.'
   end
 
-  def test_signup_deny
+  def test_signup_deny # -> spec/features/webui/sign_up_spec.rb
     # Configure denying signups
     ::Configuration.stubs(:registration).returns("deny")
     visit user_register_user_path
