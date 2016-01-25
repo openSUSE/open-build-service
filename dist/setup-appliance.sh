23ffe397a92ff017e94e02e2e02841b08cd79e3e
Frank Schreiner <schreiner@suse.de>
2016-01-25 14:19:54 +0100

    [dist] run prepare_passenger detached from apache

--- dist/setup-appliance.sh
+++ dist/setup-appliance.sh
@@ -427,10 +427,15 @@ function prepare_apache2 {
     a2enflag $flag >/dev/null
   done
 
+}
+
+function prepare_passenger {
+
   perl -p -i -e \
     's#^(\s*)PassengerRuby "/usr/bin/ruby"#$1\PassengerRuby "/usr/bin/ruby.ruby2.3"#' \
       /etc/apache2/conf.d/mod_passenger.conf
  
+
 }
 ###############################################################################
 #
@@ -520,6 +525,8 @@ if [[ ! $BOOTSTRAP_TEST_MODE == 1 ]];then
   fix_permissions
 
   prepare_apache2
+
+  prepare_passenger 
 
   check_service apache2
 
