c594fe5920140a9d86c984f6584668e80a363237
Moises Deniz Aleman <mdeniz@suse.com>
2016-01-26 15:38:38 +0100

    [ci] Be sure that the user is logged in inside feature tests

--- src/api/spec/support/features/features_authentification.rb
+++ src/api/spec/support/features/features_authentification.rb
@@ -1,13 +1,16 @@
 module FeaturesAuthentification
   def login(user, password = 'buildservice')
     visit user_login_path
-    fill_in 'Username', with: user.login
-    fill_in 'Password', with: password
-    click_button 'Log In'
+    expect(page).to have_text "Please Log In"
+    fill_in 'user_login', with: user.login
+    fill_in 'user_password', with: password
+    click_button 'Log In »'
+    expect(page).to have_link 'link-to-user-home'
   end
 
   def logout
     visit user_logout_path
+    expect(page).to have_no_link('link-to-user-home')
   end
 end
 
