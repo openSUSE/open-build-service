99e2cb7b89c1de99814e787f4e0abc18e6940fea
Christian Bruckmayer <christian@bruckmayer.net>
2016-02-05 15:55:36 +0100

    [ci] Enhance project factory with remote_project

--- src/api/spec/factories/project.rb
+++ src/api/spec/factories/project.rb
@@ -2,5 +2,11 @@ FactoryGirl.define do
   factory :project do
     name Faker::Internet.domain_word
     title Faker::Book.title
+
+    # remote projects validate additional the description and remoteurl
+    factory :remote_project do
+      description Faker::Lorem.sentence
+      remoteurl Faker::Internet.url
+    end
   end
 end
