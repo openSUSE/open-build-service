6526cf457c7ee5f79abaf14c2f589183da83b2e8
Björn Geuken <bgeuken@suse.de>
2016-02-09 17:27:56 +0100

    [ci] Extend attribute test to also test attribute values

--- src/api/spec/features/webui/attributes_spec.rb
+++ src/api/spec/features/webui/attributes_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Attributes", :type => :feature, :js => true do
     }
     let!(:attribute) { create(:attrib_type_with_namespace) }
 
-    scenario "add attribute" do
+    scenario "add attribute with values" do
       login user
 
       visit index_attribs_path(project: user.home_project_name, package: package.name)
@@ -18,6 +18,24 @@ RSpec.feature "Attributes", :type => :feature, :js => true do
       find("select#attrib_attrib_type_id").select(attribute.name)
       click_button "Create Attribute"
       expect(page).to have_content("Attribute was successfully created.")
+
+      # Add two values
+      click_link "add value"
+      click_link "add value"
+
+      fill_in "Value", with: "test 1", :match => :first
+      # Workaround to enter data into second textfield
+      within("div.nested-fields:nth-of-type(2)") do
+        fill_in "Value", with: "test 2"
+      end
+
+      click_button "Save Attribute"
+      expect(page).to have_content("Attribute was successfully updated.")
+
+      visit index_attribs_path(project: user.home_project_name, package: package.name)
+      within("tr.attribute-values") do
+        expect(page).to have_content("#{attribute.namespace}:#{attribute.name} test 2, test 1")
+      end
     end
   end
 
