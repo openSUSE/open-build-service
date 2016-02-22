2ee6557ef7e0afc0dc66900cf8326f4f728fbd5b
Björn Geuken <bgeuken@suse.de>
2016-02-22 17:45:26 +0100

    [ci] Cleanup project controller spec
    * Split preparation of test from actual test
    * Ensure that allow_forgery_protection get's set back to false after test run

--- src/api/spec/controllers/webui/project_controller_spec.rb
+++ src/api/spec/controllers/webui/project_controller_spec.rb
@@ -2,17 +2,20 @@ require 'rails_helper'
 
 RSpec.describe Webui::ProjectController do
   describe 'CSRF protection' do
-    it 'will protect forms without authenticity token' do
-      ActionController::Base.allow_forgery_protection = true # Needed because Rails disables it in test mode by default
+    before do
+      # Needed because Rails disables it in test mode by default
+      ActionController::Base.allow_forgery_protection = true
+
       login(create(:confirmed_user, login: 'tom'))
       create(:confirmed_user, login: 'moi')
-      expect do
-        post :save_person, { userid:  "moi",
-                             role:    "maintainer",
-                             project: "home:tom",
-                             commit:  "Add user" }
-      end.to raise_error ActionController::InvalidAuthenticityToken
+    end
+
+    after do
       ActionController::Base.allow_forgery_protection = false
+    end
+
+    it 'will protect forms without authenticity token' do
+      expect { post :save_person, project: "home:tom" }.to raise_error ActionController::InvalidAuthenticityToken
     end
   end
 end
