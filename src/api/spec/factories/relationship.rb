8c905d76f277e2c2552049c3cd73369b39e8e5b7
Moises Deniz Aleman <mdeniz@suse.com>
2016-02-22 16:13:16 +0100

    [ci] Change naming of Relationship factories

--- src/api/spec/factories/relationship.rb
+++ src/api/spec/factories/relationship.rb
@@ -1,7 +1,25 @@
 FactoryGirl.define do
-  factory :relationship do
+  factory :relationship_project_user, class: Relationship do
     project
     user
+    role { Role.find_by_title('maintainer') }
+  end
+
+  factory :relationship_project_group, class: Relationship do
+    project
+    group
+    role { Role.find_by_title('maintainer') }
+  end
+
+  factory :relationship_package_user, class: Relationship do
+    package
+    user
+    role { Role.find_by_title('maintainer') }
+  end
+
+  factory :relationship_package_group, class: Relationship do
+    package
+    group
     role { Role.find_by_title('maintainer') }
   end
 end
