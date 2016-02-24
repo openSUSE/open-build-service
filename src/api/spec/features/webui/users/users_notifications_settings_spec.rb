d58a911702047f1fa4bd5e51d95dc59431e1fd9b
Moises Deniz Aleman <mdeniz@suse.com>
2016-02-24 16:04:23 +0100

    [ci] Remove non sense spec

--- src/api/spec/features/webui/users/users_notifications_settings_spec.rb
+++ src/api/spec/features/webui/users/users_notifications_settings_spec.rb
@@ -1,7 +1,6 @@
 require "browser_helper"
 
 RSpec.feature "User's notifications settings", type: :feature, js: true do
-  let!(:user) { create(:confirmed_user, login: 'moi') }
   let!(:user_with_groups) { create(:user_with_groups, login: 'moi_wg') }
 
   scenario "when a user is in some group" do
@@ -16,34 +15,5 @@ RSpec.feature "User's notifications settings", type: :feature, js: true do
     expect(page).to have_content 'Notifications settings updated'
     expect(page).to have_content 'Get mails if in group'
     expect(page).to have_unchecked_field(group_title)
-  end
-
-  scenario "when a user isn't in any group" do
-    login user
-    visit user_notifications_path
-
-    expect(page).not_to have_content 'Get mails if in group'
-  end
-
-  scenario "when a user have some events" do
-    login user
-    visit user_notifications_path
-
-    expect(page).to have_content 'Events to get email for'
-    expect(page).to have_unchecked_field 'Event::RequestStatechange_creator'
-    check 'Event::RequestStatechange_creator'
-    check 'Event::CommentForPackage_maintainer'
-    check 'Event::CommentForPackage_commenter'
-    check 'Event::CommentForProject_maintainer'
-    check 'Event::CommentForProject_commenter'
-    click_button 'Update'
-
-    expect(page).to have_content 'Notifications settings updated'
-    expect(page).to have_content 'Events to get email for'
-    expect(page).to have_checked_field 'Event::RequestStatechange_creator'
-    expect(page).to have_checked_field 'Event::CommentForPackage_maintainer'
-    expect(page).to have_checked_field 'Event::CommentForPackage_commenter'
-    expect(page).to have_checked_field 'Event::CommentForProject_maintainer'
-    expect(page).to have_checked_field 'Event::CommentForProject_commenter'
   end
 end
