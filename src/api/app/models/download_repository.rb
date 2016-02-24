8939396d29d904efdb51276b907c8906f2118158
Björn Geuken <bgeuken@suse.de>
2016-02-24 16:33:10 +0100

    [webui][api] Ensure that download repository's repotypes are among valid / know repotypes

--- src/api/app/models/download_repository.rb
+++ src/api/app/models/download_repository.rb
@@ -1,12 +1,13 @@
 class DownloadRepository < ActiveRecord::Base
+  REPOTYPES = ["rpmmd", "susetags", "deb", "arch", "mdk"]
+
   belongs_to :repository
 
   validates :repository_id, presence: true
   validates :arch, uniqueness: { scope: :repository_id}, presence: true
   validates :url, presence: true
   validates :repotype, presence: true
-
-  REPOTYPES = ["rpmmd", "susetags", "deb", "arch", "mdk"]
+  validates :repotype, inclusion: { in: REPOTYPES }
 
   delegate :to_s, to: :id
 end
--- src/api/spec/models/download_repository_spec.rb
+++ src/api/spec/models/download_repository_spec.rb
@@ -8,5 +8,6 @@ RSpec.describe DownloadRepository do
     it { is_expected.to validate_presence_of(:repotype) }
     it { is_expected.to validate_presence_of(:repository_id) }
     it { expect(create(:download_repository)).to validate_uniqueness_of(:arch).scoped_to(:repository_id) }
+    it { is_expected.to validate_inclusion_of(:repotype).in_array(["rpmmd", "susetags", "deb", "arch", "mdk"]) }
   end
 end
