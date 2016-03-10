90a555c9cab5c51dc4686721bc25f4acc269cbf3
Shyukri Shyukriev <shshyukriev@suse.com>
2016-03-10 12:39:15 +0100

    [ci] Modify spec_helper to catch screenshots on failures.

--- dist/t/spec/spec_helper.rb
+++ dist/t/spec/spec_helper.rb
@@ -2,7 +2,7 @@
 #
 RSpec.configure do |config|
   # rspec-expectations config goes here.
-  config.expect_with :rspec do |expectations|
+    config.expect_with :rspec do |expectations|
     # to disable deprecated should syntax
     expectations.syntax = :expect
   end
@@ -16,3 +16,28 @@ end
 
 # for capybara rspec support
 require 'support/capybara'
+
+SCREENSHOT_DIR = "/tmp/rspec_screens"
+
+def take_screenshot(example)
+  meta            = example.metadata
+  filename        = File.basename(meta[:file_path])
+  line_number     = meta[:line_number]
+  screenshot_name = "screenshot-#{filename}-#{line_number}.png"
+  screenshot_path = File.join(SCREENSHOT_DIR, screenshot_name)
+  page.save_screenshot(screenshot_path)
+  puts meta[:full_description] + "\n Screenshot: #{screenshot_path}"
+end
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    FileUtils.rm_rf(SCREENSHOT_DIR)
+    FileUtils.mkdir_p(SCREENSHOT_DIR)
+  end
+
+  config.after(:each) do |example|
+    if example.exception
+      take_screenshot(example)
+    end
+  end
+end
