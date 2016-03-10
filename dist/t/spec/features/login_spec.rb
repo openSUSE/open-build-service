5602fe2e188daa80e8753c1710fb889e86ec44aa
Shyukri Shyukriev <shshyukriev@suse.com>
2016-03-10 12:39:44 +0100

    [ci] Signup spec added.

--- dist/t/spec/features/login_spec.rb
+++ dist/t/spec/features/login_spec.rb
@@ -1,10 +1,15 @@
 require "spec_helper"
 
-RSpec.describe "Login" do
+RSpec.describe "Sign Up & Login" do
 
-  it "should be able to sign up" do
+  it "should be able to sign up successfully" do
     visit "/"
     expect(page).to have_content("Log In")
+    fill_in 'login', with: 'test_user'
+    fill_in 'email', with: 'test_user@openqa.com'
+    fill_in 'pwd', with: 'opensuse'
+    click_button('Sign Up')
+    expect(page).to have_content("The account 'test_user' is now active.")
   end
 
 end
