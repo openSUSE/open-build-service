d05b4dc1c25cf722df07e05a9c5c126beccbfd44
Björn Geuken <bgeuken@suse.de>
2016-03-22 13:01:33 +0100

    [api][webui] Add rspec test for Project#update_repositories

--- src/api/spec/models/project_spec.rb
+++ src/api/spec/models/project_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Project do
+  let!(:project) { create(:project) }
+
   describe "validations" do
     it {
       is_expected.to validate_inclusion_of(:kind).
@@ -14,5 +16,28 @@ RSpec.describe Project do
     it { should_not allow_value("foo::bar").for(:name) }
     it { should_not allow_value("ends_with_:").for(:name) }
     it { should allow_value("fOO:+-").for(:name) }
+  end
+
+  describe "#update_repositories" do
+    let!(:repository_1) { create(:repository, name: 'repo_1', project: project) }
+    let!(:repository_2) { create(:repository, name: 'repo_2', project: project) }
+    let!(:repository_3) { create(:repository, name: 'repo_3', project: project) }
+
+    it "updates repositories association of a project" do
+      xml_hash = Xmlhash.parse(
+        <<-EOF
+          <project name="#{project.name}">
+            <repository name="repo_1" />
+            <repository name="new_repo" />
+          </project>
+        EOF
+      )
+
+      project.update_repositories(xml_hash, force = false)
+
+      expect(project.repositories.count).to eq 2
+      expect(project.repositories.where(name: "repo_1")).to exist
+      expect(project.repositories.where(name: "new_repo")).to exist
+    end
   end
 end
