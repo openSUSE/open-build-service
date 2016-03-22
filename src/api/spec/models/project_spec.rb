61f65ca3829099501815f01bc4af8a02184be358
Björn Geuken <bgeuken@suse.de>
2016-03-22 13:51:47 +0100

    [ci] Add tests for updating repository attributes

--- src/api/spec/models/project_spec.rb
+++ src/api/spec/models/project_spec.rb
@@ -19,25 +19,41 @@ RSpec.describe Project do
   end
 
   describe "#update_repositories" do
-    let!(:repository_1) { create(:repository, name: 'repo_1', project: project) }
+    let!(:repository_1) { create(:repository, name: 'repo_1', rebuild: "direct", project: project) }
     let!(:repository_2) { create(:repository, name: 'repo_2', project: project) }
     let!(:repository_3) { create(:repository, name: 'repo_3', project: project) }
 
-    it "updates repositories association of a project" do
-      xml_hash = Xmlhash.parse(
-        <<-EOF
-          <project name="#{project.name}">
-            <repository name="repo_1" />
-            <repository name="new_repo" />
-          </project>
-        EOF
-      )
+    context "updating repository elements" do
+      before do
+        xml_hash = Xmlhash.parse(
+          <<-EOF
+            <project name="#{project.name}">
+              <repository name="repo_1" />
+              <repository name="new_repo" rebuild="local" block="never" linkedbuild="all" />
+            </project>
+          EOF
+        )
+        project.update_repositories(xml_hash, force = false)
+      end
 
-      project.update_repositories(xml_hash, force = false)
+      it "updates repositories association of a project" do
+        expect(project.repositories.count).to eq 2
+        expect(project.repositories.where(name: "repo_1")).to exist
+        expect(project.repositories.where(name: "new_repo")).to exist
+      end
 
-      expect(project.repositories.count).to eq 2
-      expect(project.repositories.where(name: "repo_1")).to exist
-      expect(project.repositories.where(name: "new_repo")).to exist
+      it "updates repository attributes of existing repositories" do
+        expect(repository_1.reload.rebuild).to be nil
+        expect(repository_1.block).to be nil
+        expect(repository_1.linkedbuild).to be nil
+      end
+
+      it "imports repository attributes of newly created repositories" do
+        new_repo = project.repositories.find_by(name: "new_repo")
+        expect(new_repo.rebuild).to eq "local"
+        expect(new_repo.block).to eq "never"
+        expect(new_repo.linkedbuild).to eq "all"
+      end
     end
   end
 end
