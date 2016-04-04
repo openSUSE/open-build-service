a5609d53da89c736a94e8a770ce207a8ccd6a374
Moises Deniz Aleman <mdeniz@suse.com>
2016-04-04 13:41:31 +0200

    [ci] Migration of project controller tests: new action

--- src/api/spec/controllers/webui/project_controller_spec.rb
+++ src/api/spec/controllers/webui/project_controller_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Webui::ProjectController do
+  let(:user_moi) { create(:confirmed_user, login: "moi") }
+  let(:user_tom) { create(:confirmed_user, login: "tom") }
+
   describe 'CSRF protection' do
     before do
       # Needed because Rails disables it in test mode by default
       ActionController::Base.allow_forgery_protection = true
 
-      login(create(:confirmed_user, login: 'tom'))
-      create(:confirmed_user, login: 'moi')
+      login(user_tom)
+      user_moi
     end
 
     after do
@@ -171,197 +174,12 @@ RSpec.describe Webui::ProjectController do
   end
 
   describe 'GET #new' do
-  end
-
-  describe 'GET #new_incident' do
-  end
-
-  describe 'GET #new_package' do
-  end
-
-  describe 'GET #new_package_branch' do
-  end
-
-  describe 'GET #incident_request_dialog' do
-  end
-
-  describe 'GET #new_incident_request' do
-  end
-
-  describe 'GET #release_request_dialog' do
-  end
-
-  describe 'GET #new_release_request' do
-  end
-
-  describe 'GET #show' do
-  end
-
-  describe 'GET #linking_projects' do
-  end
-
-  describe 'GET #add_repository_from_default_list' do
-  end
-
-  describe 'GET #add_repository' do
-  end
-
-  describe 'GET #add_person' do
-  end
-
-  describe 'GET #add_group' do
-  end
-
-  describe 'GET #buildresult' do
-  end
-
-  describe 'GET #delete_dialog' do
-  end
-
-  describe 'GET #destroy' do
-  end
-
-  describe 'GET #update_target' do
-  end
-
-  describe 'GET #repositories' do
-  end
-
-  describe 'GET #repository_state' do
-  end
-
-  describe 'GET #rebuild_time' do
-  end
-
-  describe 'GET #rebuild_time_png' do
-  end
-
-  describe 'GET #requests' do
-  end
-
-  describe 'GET #create' do
-  end
-
-  describe 'GET #update' do
-  end
-
-  describe 'GET #save_repository' do
-  end
-
-  describe 'GET #save_distributions' do
-  end
-
-  describe 'GET #remove_target_request_dialog' do
-  end
-
-  describe 'GET #remove_target_request' do
-  end
-
-  describe 'GET #remove_target' do
-  end
-
-  describe 'GET #remove_path_from_target' do
-  end
-
-  describe 'GET #move_path' do
-  end
-
-  describe 'GET #monitor' do
-  end
-
-  describe 'GET #package_buildresult' do
-  end
-
-  describe 'GET #toggle_watch' do
-  end
-
-  describe 'GET #meta' do
-  end
-
-  describe 'GET #save_meta' do
-  end
-
-  describe 'GET #prjconf' do
-  end
-
-  describe 'GET #save_prjconf' do
-  end
-
-  describe 'GET #clear_failed_comment' do
-  end
-
-  describe 'GET #edit' do
-  end
-
-  describe 'GET #edit_comment_form' do
-  end
-
-  describe 'GET #edit_comment' do
-  end
-
-  describe 'GET #status' do
-  end
-
-  describe 'GET #maintained_projects' do
-  end
-
-  describe 'GET #add_maintained_project_dialog' do
-  end
-
-  describe 'GET #add_maintained_project' do
-  end
-
-  describe 'GET #remove_maintained_project' do
-  end
-
-  describe 'GET #maintenance_incidents' do
-  end
-
-  describe 'GET #unlock_dialog' do
-  end
-
-  describe 'GET #unlock' do
-  end
-
-  describe 'GET #main_object' do
-  end
-
-  describe 'GET #project_params' do
-  end
-
-  describe 'GET #set_maintained_project' do
-  end
-
-  describe 'GET #load_project_info' do
-  end
-
-  describe 'GET #load_releasetargets' do
-  end
-
-  describe 'GET #require_maintenance_project' do
-  end
-
-  describe 'GET #load_buildresult' do
-  end
-
-  describe 'GET #find_maintenance_infos' do
-  end
-
-  describe 'GET #convert_buildresult' do
-  end
-
-  describe 'GET #status_filter_packages' do
-  end
-
-  describe 'GET #status_gather_requests' do
-  end
-
-  describe 'GET #status_gather_attributes' do
-  end
-
-  describe 'GET #users_path' do
-  end
-
-  describe 'GET #set_project_by_name' do
+    before do
+      login(user_moi)
+      get :new, name: 'ProjectName'
+    end
+
+    it { expect(assigns(:project)).to be_a(Project) }
+    it { expect(assigns(:project).name).to eq('ProjectName') }
   end
 end
