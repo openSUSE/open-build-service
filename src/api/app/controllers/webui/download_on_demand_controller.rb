60438fa0921623ee18e6101ea37bafe0c5388017
Björn Geuken <bgeuken@suse.de>
2016-04-12 14:16:59 +0200

    [api][webui] Catch backend errors when storing download repositories
    This implements exception handling for storing download repository data in
    the backend. This is needed to avoid inconsistency between frontend DB and
    backend.

--- src/api/app/controllers/webui/download_on_demand_controller.rb
+++ src/api/app/controllers/webui/download_on_demand_controller.rb
@@ -4,34 +4,51 @@ class Webui::DownloadOnDemandController < Webui::WebuiController
   def create
     @download_on_demand = DownloadRepository.new(permitted_params)
     authorize @download_on_demand
-    if @download_on_demand.save
-      @project.store
-      redirect_to project_repositories_path(@project), notice: "Successfully created Download on Demand"
-    else
-      redirect_to :back, error: "Download on Demand can't be created: #{@download_on_demand.errors.full_messages.to_sentence}"
+
+    begin
+      ActiveRecord::Base.transaction do
+        @download_on_demand.save!
+        @project.store
+      end
+    rescue ActiveRecord::RecordInvalid, ActiveXML::Transport::Error => exception
+      redirect_to :back, error: "Download on Demand can't be created: #{exception.message}"
+      return
     end
+
+    redirect_to project_repositories_path(@project), notice: "Successfully created Download on Demand"
   end
 
   def update
     @download_on_demand = DownloadRepository.find(params[:id])
     authorize @download_on_demand
-    if @download_on_demand.update_attributes(permitted_params)
-      @project.store
-      redirect_to project_repositories_path(@project), notice: "Successfully updated Download on Demand"
-    else
-      redirect_to :back, error: "Download on Demand can't be updated: #{@download_on_demand.errors.full_messages.to_sentence}"
+
+    begin
+      ActiveRecord::Base.transaction do
+        @download_on_demand.update_attributes!(permitted_params)
+        @project.store
+      end
+    rescue ActiveRecord::RecordInvalid, ActiveXML::Transport::Error => exception
+      redirect_to :back, error: "Download on Demand can't be updated: #{exception.message}"
+      return
     end
+
+    redirect_to project_repositories_path(@project), notice: "Successfully updated Download on Demand"
   end
 
   def destroy
     @download_on_demand = DownloadRepository.find(params[:id])
     authorize @download_on_demand
-    if @download_on_demand.destroy
-      @project.store
-      redirect_to project_repositories_path(@project), notice: "Successfully removed Download on Demand"
-    else
-      redirect_to :back, error: "Download on Demand can't be removed: #{@download_on_demand.errors.full_messages.to_sentence}"
+
+    begin
+      ActiveRecord::Base.transaction do
+        @download_on_demand.destroy!
+        @project.store
+      end
+    rescue ActiveRecord::RecordInvalid, ActiveXML::Transport::Error => exception
+      redirect_to :back, error: "Download on Demand can't be removed: #{exception.message}"
     end
+
+    redirect_to project_repositories_path(@project), notice: "Successfully removed Download on Demand"
   end
 
   private
