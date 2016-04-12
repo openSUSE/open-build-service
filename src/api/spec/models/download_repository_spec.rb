cdeee3d87100512a7d36cd641855241287ba57e1
Björn Geuken <bgeuken@suse.de>
2016-04-12 15:32:48 +0200

    [api][webui] Fix architecture validation
    DownloadRepository architecture has to be within the valid architectures
    and the architectures that the associated repositories have. Otherwise the
    backend validation would fail.

--- src/api/spec/models/download_repository_spec.rb
+++ src/api/spec/models/download_repository_spec.rb
@@ -2,17 +2,20 @@ require "rails_helper"
 
 RSpec.describe DownloadRepository do
   describe "validations" do
+    subject(:download_repository) { create(:download_repository) }
     it { is_expected.to validate_presence_of(:url) }
     it { is_expected.to validate_presence_of(:arch) }
     it { is_expected.to validate_presence_of(:repotype) }
     it { is_expected.to validate_presence_of(:repository_id) }
-    it { expect(create(:download_repository)).to validate_uniqueness_of(:arch).scoped_to(:repository_id) }
+    it { is_expected.to validate_uniqueness_of(:arch).scoped_to(:repository_id) }
     it { is_expected.to validate_inclusion_of(:repotype).in_array(["rpmmd", "susetags", "deb", "arch", "mdk"]) }
 
-    it "validates that architecture is supported by scheduler" do
-      # FIXME: This is required because of seeding our test DB
-      architectures = Architecture.all.pluck(:name)
-      is_expected.to validate_inclusion_of(:arch).in_array(architectures)
+    describe "architecture_inclusion validation" do
+      subject(:download_repository) { create(:download_repository) }
+      it {
+        expect { download_repository.update_attributes!(arch: "s390x") }.to raise_error(
+          ActiveRecord::RecordInvalid, "Validation failed: Architecture has to be available via repository association.")
+      }
     end
   end
 end
