openSUSE/open-build-service

commit e1f4858bec674cf350b577e9e2fc32c0dcf76f5b

[api][webui] Fix Nameless repositories, issue#1317

src/api/app/models/repository.rb
@@ -18,6 +18,11 @@ class Repository < ActiveRecord::Base
 
   scope :not_remote, -> { where(:remote_project_name => nil) }
 
+  validates :name, length: { in: 1..200 }
+  # Keep in sync with src/backend/BSVerify.pm
+  validates :name, format: { with:    /\A[^_:\/\000-\037][^:\/\000-\037]+\Z/,
+                             message: "Repository name must not start with '_' or contain any of these characters ':/'" }
+
   validate :validate_duplicates, :on => :create
   def validate_duplicates
     if Repository.where("db_project_id = ? AND name = ? AND ( remote_project_name = ? OR remote_project_name is NULL)",

src/api/spec/models/repository_spec.rb
@@ -1,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe Repository do
+  describe "validations" do
+    it { is_expected.to validate_length_of(:name).is_at_least(1).is_at_most(200) }
+    it { should_not allow_value("_foo").for(:name) }
+    it { should_not allow_value("f:oo").for(:name) }
+    it { should_not allow_value("f/oo").for(:name) }
+    it { should_not allow_value("f\noo").for(:name) }
+    it { should allow_value("fOO_-§$&!#+~()=?\\\"").for(:name) }
+  end
+end

commit 633349f372c940294548e7ce66860bffb6d77d51

[api][webui] Don't reject single character repository names
This fixes a regression in commit e1f4858.

src/api/app/models/repository.rb
@@ -20,7 +20,7 @@ class Repository < ActiveRecord::Base
 
   validates :name, length: { in: 1..200 }
   # Keep in sync with src/backend/BSVerify.pm
-  validates :name, format: { with:    /\A[^_:\/\000-\037][^:\/\000-\037]+\Z/,
+  validates :name, format: { with:    /\A[^_:\/\000-\037][^:\/\000-\037]*\Z/,
                              message: "Repository name must not start with '_' or contain any of these characters ':/'" }
 
   # Name has to be unique among local repositories and remote_repositories of the associated db_project.

src/api/spec/models/repository_spec.rb
@@ -15,5 +15,6 @@ RSpec.describe Repository do
     it { should_not allow_value("f/oo").for(:name) }
     it { should_not allow_value("f\noo").for(:name) }
     it { should allow_value("fOO_-§$&!#+~()=?\\\"").for(:name) }
+    it { should allow_value("f").for(:name) }
   end
 end