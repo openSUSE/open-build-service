13c5f1d696419505220b2ac09a919fcc503442e6
Frank Schreiner <schreiner@suse.de>
2016-04-28 14:57:55 +0200

    [ci] changed rspec tests build package O:S:2.7

--- dist/t/spec/features/0100_delete_home_Admin_spec.rb
+++ dist/t/spec/features/0100_delete_home_Admin_spec.rb
@@ -9,6 +9,8 @@ RSpec.describe "Delete project home:Admin" do
   it "should be able to execute delete" do
     visit("/project/show/home:Admin")
     click_link("Delete project")
+    find("input[type=submit][value='Ok']").click
+    expect(page).to have_content("Project was successfully removed.")
   end
 
 end
