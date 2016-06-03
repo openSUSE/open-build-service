411397bc82095474721d1f8a87af8ce3cfa671d6
Michael Schroeder <mls@suse.de>
2016-06-03 15:41:36 +0200

    [backend] switch worker to use a dispatch function like the other servers

--- src/backend/BSServer.pm
+++ src/backend/BSServer.pm
@@ -339,7 +339,7 @@ sub server {
         alarm(0);
       };
       my @r = $conf->{'dispatch'}->($conf, $req);
-      if (!$req->{'replying'} && !$req->{'returnfromserver'}) {
+      if (!$req->{'replying'}) {
         if ($conf->{'stdreply'}) {
           $conf->{'stdreply'}->(@r);
         } else {
@@ -347,7 +347,7 @@ sub server {
         }
       }
     };
-    return @{$req->{'returnfromserver'}} if $req->{'returnfromserver'};
+    return @{$req->{'returnfromserver'}} if $req->{'returnfromserver'} && !$@;
     reply_error($conf, $@) if $@;
     close CLNT;
     exit(0);
