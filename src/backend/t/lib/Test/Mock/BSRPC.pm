a52bdad999b725ace8f5ef2efd534c39b8fc44de
Michael Schroeder <mls@suse.de>
2016-06-16 14:43:23 +0200

    [backend] clean up Mock::BSRPC code

--- src/backend/t/lib/Test/Mock/BSRPC.pm
+++ src/backend/t/lib/Test/Mock/BSRPC.pm
@@ -19,63 +19,49 @@ package Test::Mock::BSRPC;
 use strict;
 use Carp qw/cluck/;
 
+use BSRPC;
+use BSUtil;
+
 BEGIN {
-  require BSRPC;
-  import BSRPC;
-
   *BSRPC::rpc = sub {
-	my ($param, $xmlargs, @args) = @_;
-	my $f2r;
-	my $ret;
-	$param = {'uri' => $param} if ref($param) ne 'HASH';
-	my $uri = $param->{'uri'};
-	$uri =~ s#https?://##;
+    my ($param, $xmlargs, @args) = @_;
+    $param = {'uri' => $param} if ref($param) ne 'HASH';
+    my $uri = $param->{'uri'};
+    $uri =~ s#https?://##;
 
-	for (@args) {
-	  $_ = BSRPC::urlencode($_);
-	  s/%3D/=/;
-	}
-	$uri = "$uri?" . join('&', @args);
-	if ( $Test::Mock::BSRPC::fixtures_map->{$uri} ) {
-	  $uri = $Test::Mock::BSRPC::fixtures_map->{$uri}
-	} else {
+    for (@args) {
+      $_ = BSRPC::urlencode($_);
+      s/%3D/=/;
+    }
+    $uri = "$uri?" . join('&', @args);
+    if ($Test::Mock::BSRPC::fixtures_map->{$uri}) {
+      $uri = $Test::Mock::BSRPC::fixtures_map->{$uri}
+    } else {
       $uri =~ s/\//_/g;
       $uri =~ s/_/\//;
-	}
-	$uri = "$BSConfig::bsdir/$uri";
+    }
+    $uri = "$BSConfig::bsdir/$uri";
 
-	# hack to get smaller fixtures
-	if ( -e "$uri.xz" ) {
-	  system("xz",'--decompress','-k',"$uri.xz");
-	  $f2r = $uri;
-	}
-
-	die("missing fixture: $uri\n") unless -e $uri;
-
-	if ($xmlargs) {
-	  $ret = BSUtil::readxml($uri, $xmlargs);
-	} else {
-	  my $receiver = $param->{'receiver'};
-	  $xmlargs ||= $param->{'receiverarg'};
-	  if ($receiver) {
-		#$ans = $receiver->($ansreq, $param, $xmlargs);
-		#$xmlargs = undef;
-		open(S,'<',$uri) || die "$uri: $!\n";
-		my $ansreq = {
-			__socket => \*S
-		};
-		$ret = $receiver->($ansreq, $param, $xmlargs);
-	  } else {
-		$ret =  BSUtil::readstr($uri, $xmlargs);
-	  }
-	}
-
-	if ( $f2r )  { unlink $f2r };
-
-	return $ret;
-
-  }
-
+    my $ret = '';
+    # hack to get smaller fixtures
+    if (-e "$uri.xz") {
+      local *F;
+      open(F, '-|', 'xz', '--decompress', '-c', "$uri.xz");
+      1 while sysread(F, $ret, 8192, length($ret));
+      close(F) || die("$uri.xz: $?\n");
+    } elsif (-e $uri) {
+      $ret= readstr($uri);
+    } else {
+      die("missing fixture: $uri\n") unless -e $uri;
+    }
+    my $receiver = $param->{'receiver'};
+    if ($receiver) {
+      $ret = $receiver->(BSHTTP::str2req($ret), $param, $xmlargs || $param->{'receiverarg'}) if $receiver;
+    } elsif ($xmlargs) {
+      $ret = BSUtil::fromxml($ret, $xmlargs);
+    }
+    return $ret;
+  };
 }
 
 1; 
