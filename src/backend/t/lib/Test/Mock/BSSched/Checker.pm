987bf34c748a4f05cdf613fd5606e6ce1f8c7a0d
Michael Schroeder <mls@suse.de>
2016-06-24 13:53:50 +0200

    [backend] Mock::BSSched::Checker: support compressed solv and bininfo files

--- src/backend/t/lib/Test/Mock/BSSched/Checker.pm
+++ src/backend/t/lib/Test/Mock/BSSched/Checker.pm
@@ -24,11 +24,38 @@ sub addrepo {
   my ($ctx, $pool, $prp, $arch) = @_;
   my $gctx = $ctx->{'gctx'};
   $arch ||= $gctx->{'arch'};
-  return $pool->repofromfile($prp, "$gctx->{'reporoot'}/$prp/$arch/:full.solv");
+  my ($projid, $repoid) = split('/', $prp, 2);
+  my $remoteprojs = $gctx->{'remoteprojs'};
+  my $r;
+  if ($remoteprojs->{$projid}) {
+    $r = BSSched::Remote::addrepo_remote($ctx, $pool, $prp, $arch, $remoteprojs->{$projid});
+  } else {
+    my $d = Test::OBS::Utils::readstrxz("$gctx->{'reporoot'}/$prp/$arch/:full.solv", 1);
+    if ($d) {
+      $r = $pool->repofromstr($prp, $d);
+    } else {
+      $r = $pool->repofrombins($prp, '.');
+    }
+  }
+  return $r;
 }
 
+sub read_gbininfo {
+  my ($ctx, $prp, $arch, $ps) = @_;
+  my $gctx = $ctx->{'gctx'};
+  $arch ||= $gctx->{'arch'};
+  my ($projid, $repoid) = split('/', $prp, 2);
+  my $remoteprojs = $gctx->{'remoteprojs'};
+  if ($remoteprojs->{$projid}) {
+    return BSSched::Remote::read_gbininfo_remote($ctx, "$prp/$arch", $remoteprojs->{$projid}, $ps);
+  }
+  my $d = Test::OBS::Utils::readstrxz("$gctx->{'reporoot'}/$prp/$arch/:bininfo");
+  return BSUtil::fromstorable($d);
+}
+  
 sub writejob {
   my ($ctx, $job, $binfo, $reason) = @_;
+  $ctx = $ctx->{'realctx'} if $ctx->{'realctx'};
   $binfo->{'job'} = $job if $job;
   $binfo->{'reason'} = $reason->{'explain'} if $reason;
   $binfo->{'srcserver'} ||= 'srcserver';
