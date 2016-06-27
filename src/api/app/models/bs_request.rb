34d05a6df89740fe12b3c163407f7c1af3a26aa1
Björn Geuken <bgeuken@suse.de>
2016-06-27 10:31:17 +0200

    [api] Refactor filter validation of BsRequest::list_ids

--- src/api/app/models/bs_request.rb
+++ src/api/app/models/bs_request.rb
@@ -1152,7 +1152,7 @@ class BsRequest < ActiveRecord::Base
       opts.delete(:types)
     end
     # Do not allow a full collection to avoid server load
-    if opts[:project].blank? && opts[:user].blank? && opts[:package].blank?
+    if [:project, :user, :package].all? { |filter| opts[filter].blank? }
       raise RuntimeError, 'This call requires at least one filter, either by user, project or package'
     end
     roles = opts[:roles] || []
