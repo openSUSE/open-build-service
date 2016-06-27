openSUSE/open-build-service

commit 9672ab3861376b81edc925481f7cda27041d6dc8

[webui] simplify request_show_path usage
(and fix one wrong redirection)

src/api/app/views/webui/project/_incident_table_entries.html.erb
@@ -33,7 +33,7 @@
                 <% rqs_in = BsRequest.list_ids(roles: %w(target), states: %w(new review), project: incident.name) %>
                 <% if rqs_in.count > 0 %>
                     <% if rqs_in.count == 1 %>
-                        <% path = request_show_path(id: rqs_in.first) %>
+                        <% path = request_show_path(rqs_in.first) %>
                     <% else %>
                         <% path = project_requests_path(project: incident.name) %>
                     <% end %>
@@ -45,7 +45,7 @@
                     <% BsRequest.where(id: rqs_out).order('bs_requests.number').each do |rq_out| %>
                         <% text = "Release request in state '#{rq_out['state']}'" %>
                         <%= link_to(sprite_tag(map_request_state_to_flag(rq_out['state'].to_s), title: text),
-                              request_show_path(number: rq_out["number"])) %>
+                              request_show_path(rq_out["number"])) %>
                     <% end %>
                 <% else %>
                     <% if incident.is_locked? %>

src/api/app/views/webui/request/show.html.erb
@@ -8,10 +8,10 @@
 
 <div id="request_navigation_box" style="position: absolute; right: 0.3em;">
   <% if @request_before -%>
-      <%= link_to '<<', request_show_path(number: @request_before) %>
+      <%= link_to '<<', request_show_path(@request_before) %>
   <% end -%>
   <% if @request_after -%>
-      <%= link_to '>>', request_show_path(number: @request_after) %>
+      <%= link_to '>>', request_show_path(@request_after) %>
   <% end -%>
 </div>
 

commit 82811924f38f0216733b7f239315c5b89dfa2422

[api] Fix issue#1874 - webui: wrong request listed in maintenance incident list
This will add BsRequest::list_numbers for fetching request numbers and uses it to
get the correct value for request_show_path helper.
With 9c897f7ff1a56537d0cd request_show_path is using the BsRequest's number instead
of it's id. This caused links to point to the wrong requests.

This broke with 9672ab3861376b81edc925481f7cda27041d6dc8

src/api/app/models/bs_request.rb
@@ -1146,7 +1146,7 @@ class BsRequest < ActiveRecord::Base
     requests
   end
 
-  def self.list_ids(opts)
+  def self.list(opts)
     # All types means don't pass 'type'
     if opts[:types] == 'all' || (opts[:types].respond_to?(:include?) && opts[:types].include?('all'))
       opts.delete(:types)
@@ -1162,10 +1162,18 @@ class BsRequest < ActiveRecord::Base
     if opts[:project] && roles.empty? && (states.empty? || states.include?("review"))
       # Create a single ActiveRecord query object (=one SQL query)
       collection(opts.merge(roles: ["reviewer"])).
-        merge(collection(opts.merge(roles: ["target", "source"]))).ids
+        merge(collection(opts.merge(roles: ["target", "source"])))
     else
-      collection(opts).ids
+      collection(opts)
     end
+  end
+
+  def self.list_ids(opts)
+    self.list(opts).pluck(:id)
+  end
+
+  def self.list_numbers(opts)
+    self.list(opts).pluck(:number)
   end
 
   def self.extend_query_for_group(group, requests, roles, review_states)

src/api/app/views/webui/project/_incident_table_entries.html.erb
@@ -30,7 +30,7 @@
                 <% end %>
               </td>
               <td rowspan="<%= release_targets_ng.length %>" class="nowrap">
-                <% rqs_in = BsRequest.list_ids(roles: %w(target), states: %w(new review), project: incident.name) %>
+                <% rqs_in = BsRequest.list_numbers(roles: %w(target), states: %w(new review), project: incident.name) %>
                 <% if rqs_in.count > 0 %>
                     <% if rqs_in.count == 1 %>
                         <% path = request_show_path(rqs_in.first) %>