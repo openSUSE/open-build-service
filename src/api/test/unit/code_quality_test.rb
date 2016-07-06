a1594f5e3e73f88e5167e90100cbf75eca611116
Björn Geuken <bgeuken@suse.de>
2016-07-06 15:43:14 +0200

    [api] Fix evaluation of target package for kgraft maintenance releases
    Pair-debugging session with Adrian;-)

--- src/api/test/unit/code_quality_test.rb
+++ src/api/test/unit/code_quality_test.rb
@@ -70,7 +70,7 @@ class CodeQualityTest < ActiveSupport::TestCase
       'BsRequest::new_from_xml'                                                 => 113.77,
       'BsRequestAction#check_action_permission!'                                => 117.71,
       'BsRequestAction#check_action_permission_target!'                         => 89.68,
-      'BsRequestAction#create_expand_package'                                   => 443.16,
+      'BsRequestAction#create_expand_package'                                   => 453.98,
       'BsRequestAction#default_reviewers'                                       => 141.02,
       'BsRequestAction#store_from_xml'                                          => 88.01,
       'BsRequestActionMaintenanceIncident#_merge_pkg_into_maintenance_incident' => 130.81,
