openSUSE/open-build-service

commit 3d1db64c04d77e79f116b0e7a96e96bd41c92ee2

[api] allow to set or modify the accept_at time in a request
can be used also to pre-approve requests with open reviews

src/api/app/models/bs_request.rb
@@ -663,6 +663,11 @@ class BsRequest < ActiveRecord::Base
       end
       self.save!
       history.create(p) if history
+
+      # we want to check right now if pre-approved requests can be processed
+      if go_new_state == :new and self.accept_at
+        Delayed::Job.enqueue AcceptRequestsJob.new
+      end
     end
   end
 
@@ -920,6 +925,15 @@ class BsRequest < ActiveRecord::Base
     end
 
     apply_default_reviewers
+  end
+
+  def set_accept_at!(time = nil)
+    # Approve a request to be accepted when the reviews finished
+    self.permission_check_change_state!({:newstate => 'accepted'})
+
+    self.accept_at = time || Time.now
+    self.save!
+    reset_cache
   end
 
   def apply_default_reviewers

src/api/.rubocop_todo.yml
@@ -100,7 +100,7 @@ Metrics/BlockNesting:
 # Offense count: 99
 # Configuration parameters: CountComments.
 Metrics/ClassLength:
-  Max: 2725
+  Max: 2753
 
 # Offense count: 249
 Metrics/CyclomaticComplexity:

src/api/app/controllers/request_controller.rb
@@ -86,9 +86,9 @@ class RequestController < ApplicationController
     # FIXME: this should be moved into the model functions, doing
     #        these actions
     case params[:cmd]
-    when 'create', 'changestate', 'addreview', 'setpriority', 'setincident'
+    when 'create', 'changestate', 'addreview', 'setpriority', 'setincident', 'setacceptat'
       # create -> noop
-      # changestate,addressview,setpriority,setincident -> the model is checking already
+      # permissions are checked by the model
     when 'changereviewstate', 'assignreview'
       @req.permission_check_change_review!(params)
     when 'addrequest', 'removerequest'
@@ -213,6 +213,12 @@ class RequestController < ApplicationController
 
   def request_command_setincident
     @req.setincident(params[:incident])
+    render_ok
+  end
+
+  def request_command_setacceptat
+    time = DateTime.parse(params[:time]) unless params[:time].blank?
+    @req.set_accept_at!(time)
     render_ok
   end
 

src/api/test/functional/request_controller_test.rb
@@ -2594,6 +2594,47 @@ XML
     assert_response :success
   end
 
+  def test_set_auto_accept
+    login_tom
+
+    Timecop.freeze(2010, 07, 12)
+
+    req = "<request>
+            <action type='delete'>
+              <target project='home:Iggy' package='TestPack' />
+            </action>
+            <description>delete</description>
+            <state/>
+          </request>"
+
+    # works as user with write permission in target
+    login_Iggy
+    post '/request?cmd=create', req
+    assert_response :success
+    assert_xml_tag(:tag => 'request')
+    node = Xmlhash.parse(@response.body)
+    id = node['id']
+    assert id.present?
+
+    # accept it now
+    post "/request/#{id}?cmd=setacceptat"
+    assert_response :success
+
+    # correct rendered
+    get "/request/#{id}"
+    assert_response :success
+    assert_xml_tag(:tag => 'accept_at', :content => '2010-07-12 00:00:00 UTC')
+
+    # modify accept time
+    post "/request/#{id}?cmd=setacceptat&time='2013-12-01%2000:00:00%20UTC'"
+    assert_response :success
+
+    # correct rendered
+    get "/request/#{id}"
+    assert_response :success
+    assert_xml_tag(:tag => 'accept_at', :content => '2013-12-01 00:00:00 UTC')
+  end
+
   def test_branch_version_update_and_submit_request_back
     # branch a package which does not exist in project, but project is linked
     login_tom

src/api/test/unit/code_quality_test.rb
@@ -65,7 +65,7 @@ class CodeQualityTest < ActiveSupport::TestCase
       'BranchPackage#determine_details_about_package_to_branch'                 => 91.39,
       'BranchPackage#lookup_incident_pkg'                                       => 83.09,
       'BranchPackage#extend_packages_to_link'                                   => 80.23,
-      'BsRequest#change_review_state'                                           => 203.53,
+      'BsRequest#change_review_state'                                           => 209.72,
       'BsRequest#apply_default_reviewers'                                       => 129.52,
       'BsRequest#webui_actions'                                                 => 130.13,
       'BsRequest::new_from_xml'                                                 => 113.77,

commit 419c56c83147e876964dd28f05cac696d03ea64b

[api] Fix NameError: uninitialized constant error when accepting requests with autoaccept
Adds missing require, caused by 3d1db64c04d77e79f116b0e7a96e96bd41c92ee2

======

NameError: uninitialized constant BsRequest::AcceptRequestsJob

app/models/bs_request.rb:703→ block in change_review_state
activerecord-4.2.5.2/lib/active_record/locking/pessimistic.rb:72→ block in with_lock
activerecord-4.2.5.2/lib/active_record/connection_adapters/abstract/database_statements.rb:213→ block in transaction
activerecord-4.2.5.2/lib/active_record/connection_adapters/abstract/transaction.rb:184→ within_new_transaction

src/api/app/models/bs_request.rb
@@ -1,6 +1,7 @@
 require 'xmlhash'
 require 'event'
 require 'opensuse/backend'
+require File.join(Rails.root, "lib/workers/accept_requests")
 
 include MaintenanceHelper
 