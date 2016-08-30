0aa25704d26076706dbd0377b44bda4f7b33be96
Björn Geuken <bgeuken@suse.de>
2016-08-30 10:28:35 +0200

    [ci] Add tests for public routes

--- src/api/spec/routing/api_matcher_spec.rb
+++ src/api/spec/routing/api_matcher_spec.rb
@@ -8,4 +8,23 @@ RSpec.describe 'APIMatcher' do
   it 'distributions in html format should not be routable' do
     expect(get('/distributions')).not_to be_routable
   end
+
+  it 'routes public routes correctly' do
+    expect(get('/public/source/project/package/file')).to route_to(
+      controller: 'public',
+      action:     'source_file',
+      project:    'project',
+      package:    'package',
+      filename:   'file'
+    )
+    expect(get('/public/build/project/repository/arch/package/file')).to route_to(
+      controller: 'public',
+      action:     'build',
+      project:    'project',
+      repository: 'repository',
+      arch:       'arch',
+      package:    'package',
+      file:       'file'
+    )
+  end
 end
