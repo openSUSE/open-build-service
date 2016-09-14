ae5734d2cab4fdbcee2a0365a6325cac1a057870
Andrew Lee (李健秋) <ajqlee@debian.org>
2016-09-14 17:34:46 +0800

    Drop crontab as there is no jobs:workerstatus rake task listed anymore.

--- debian/obs-api.cron.d
+++
@@ -1,6 +0,0 @@
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-MAILTO="root@localhost"
-
-* * * * *       www-data    cd /usr/share/obs/api/ && RAILS_ENV=production rake -s jobs:workerstatus > /dev/null
-# Disabled: it is apparently used for updating non existing obs bugzilla. SIN: #55
-#1 * * * *       www-data    cd /usr/share/obs/api/ && RAILS_ENV=production rake -s jobs:updateissues > /dev/null
