openSUSE/open-build-service

commit f8ac9e47fcfd18cec5d146e1c4b2ced5c47136d9

[webui][api] Fix commit_opts omitted by store
Project#store always omitted the commit_opts set previously to the function call.
because the check for the empty hash always returned true.

While project.store({ options }) worked, setting the options in two steps did not:
project.commit_opts = { options }
project.store

This fix brings the store function in accordance with the destroy function.

src/api/app/models/project.rb
@@ -878,7 +878,7 @@ class Project < ActiveRecord::Base
   end
 
   def store(opts = {})
-    self.commit_opts = opts if opts
+    self.commit_opts = opts if opts.present?
     self.transaction do
       save!
       write_to_backend

src/api/spec/models/project_spec.rb
@@ -326,4 +326,26 @@ RSpec.describe Project do
       end
     end
   end
+
+  describe "#store" do
+    before do
+      project.stubs(:save!).returns(true)
+      project.stubs(:write_to_backend).returns(true)
+      project.commit_opts = { comment: 'the comment' }
+    end
+
+    context "without commit_opts parameter" do
+      it "does not overwrite the commit_opts" do
+        project.store
+        expect(project.commit_opts).to eq({ comment: 'the comment' })
+      end
+    end
+
+    context "with commit_opts parameter" do
+      it "does overwrite the commit_opts" do
+        project.store({ comment: 'a new comment'})
+        expect(project.commit_opts).to eq({ comment: 'a new comment' })
+      end
+    end
+  end
 end

commit 6a69050a5deb7ccdb56d4925e7a8950d811b5179

[api] Fix issue #2150 - webui project meta editor broken
With f8ac9e47fcfd18cec5d146e1c4b2ced5c47136d9 some previously dead code,
that enabled no_backend_write, became active and prevented update_to_xml
to write to the backend.

Group debugging with Ana, Eduardo, Henne and Manuel.

src/api/app/models/project.rb
@@ -542,9 +542,6 @@ class Project < ApplicationRecord
     self.remoteurl = xmlhash.value('remoteurl')
     self.remoteproject = xmlhash.value('remoteproject')
     self.kind = xmlhash.value('kind') unless xmlhash.value('kind').blank?
-
-    # give us an id
-    self.commit_opts = { no_backend_write: 1 }
     self.save!
 
     update_linked_projects(xmlhash)