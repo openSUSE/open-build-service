94d3e4286e32cf4da0e1fb609e57e2925fde6b44
Michael Schroeder <mls@suse.de>
2016-09-28 12:29:23 +0200

    [backend] rename the slow logfile so that logrotate sees it

--- src/backend/BSStdServer.pm
+++ src/backend/BSStdServer.pm
@@ -222,7 +222,7 @@ sub server {
     $conf->{'serverstatus'} ||= "$rundir/$name.status";
     $conf->{'setkeepalive'} = 1 unless defined $conf->{'setkeepalive'};
     $conf->{'run'} ||= \&BSServer::server;
-    $conf->{'slowrequestlog'} ||= "$bsdir/log/$name.slow" if $conf->{'slowrequestthr'};
+    $conf->{'slowrequestlog'} ||= "$bsdir/log/$name.slow.log" if $conf->{'slowrequestthr'};
     $conf->{'name'} = $name;
     BSDispatch::compile($conf);
   }
