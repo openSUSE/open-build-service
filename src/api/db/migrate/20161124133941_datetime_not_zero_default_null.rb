0116d805ae6bc1154c6553ab02e160eecb4d8dfb
Josua Mayer <josua.mayer97@gmail.com>
2016-10-10 18:47:53 +0200

    [api] Don't use 0 as default datetime value. workaround for #2222
    The SQL Server might be running with NO_ZERO_DATE set. Use NULL as
    default value instead, like it is done in all other tables.
    
    Signed-off-by: Josua Mayer <josua.mayer97@gmail.com>

---
+++ src/api/db/migrate/20161124133941_datetime_not_zero_default_null.rb
@@ -0,0 +1,15 @@
+class DatetimeNotZeroDefaultNull < ActiveRecord::Migration
+  def self.up
+    execute("alter table packages modify created_at datetime default NULL;")
+    execute("alter table packages modify updated_at datetime default NULL;")
+    execute("alter table projects modify created_at datetime default NULL;")
+    execute("alter table projects modify updated_at datetime default NULL;")
+  end
+
+  def self.down
+    execute("alter table packages modify created_at datetime default '0000-00-00 00:00:00';")
+    execute("alter table packages modify updated_at datetime default '0000-00-00 00:00:00';")
+    execute("alter table projects modify created_at datetime default '0000-00-00 00:00:00';")
+    execute("alter table projects modify updated_at datetime default '0000-00-00 00:00:00';")
+  end
+end
