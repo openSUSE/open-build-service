f091880b8d71cd1852a41a44deef224b8f1e01c9
Moises Deniz Aleman <mdeniz@suse.com>
2016-10-17 18:06:15 +0200

    [ci] Add Maintained attribute factory

--- src/api/spec/factories/attribs.rb
+++ src/api/spec/factories/attribs.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :attrib do
     attrib_type { create(:attrib_type_with_namespace) }
+
+    factory :maintained_attrib do
+      attrib_type AttribType.find_by_namespace_and_name!('OBS', 'Maintained')
+    end
   end
 end
