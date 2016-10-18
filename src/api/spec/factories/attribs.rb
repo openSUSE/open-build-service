7f24d51588b3da55498d89b1fda5c562a6835c1e
Moises Deniz Aleman <mdeniz@suse.com>
2016-10-18 17:35:22 +0200

    [ci] Add UpdateProject attribute factory

--- src/api/spec/factories/attribs.rb
+++ src/api/spec/factories/attribs.rb
@@ -9,5 +9,14 @@ FactoryGirl.define do
     factory :maintainance_project_attrib do
       attrib_type AttribType.find_by_namespace_and_name!('OBS', 'MaintenanceProject')
     end
+
+    factory :update_project_attrib do
+      transient do
+        update_project nil
+      end
+
+      attrib_type AttribType.find_by_namespace_and_name!('OBS', 'UpdateProject')
+      values { [ build(:attrib_value, value: update_project.name) ] }
+    end
   end
 end
