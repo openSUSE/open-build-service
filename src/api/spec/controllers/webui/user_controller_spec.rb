1bd7c09fe521fd204e4e38f772ed358deed28eb2
Eduardo Navarro <enavarro@suse.com>
2016-10-26 12:10:02 +0200

    [ci] Fix test of requests json
    Pair programming with @mdeniz.

--- src/api/spec/controllers/webui/user_controller_spec.rb
+++ src/api/spec/controllers/webui/user_controller_spec.rb
@@ -119,7 +119,7 @@ RSpec.describe Webui::UserController do
 
     it 'renders the requests as json' do
       get :requests, { format: :json }
-      expect(response.body).to eq({sEcho: 2, iTotalRecords: 1, iTotalDisplayRecords: 1, data: []}.to_json)
+      expect(response.body).to eq({ draw: 1, recordsTotal: 1, recordsFiltered: 1, data: [] }.to_json)
     end
   end
 
