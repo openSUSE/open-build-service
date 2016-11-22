5c1acdb2f5c943e605c2eece309adfcbfa68e181
Ana María Martínez Gómez <ammartinez@suse.de>
2016-11-22 13:41:45 +0100

    [api] Remove unused columns in comments table
    Remove the old relations and columns from the comments table, as after
    moving the data in 984b89c1303155c0eb663332861094e20f83494b they are not
    needed any more.

---
+++ src/api/db/migrate/20161122123708_remove_old_relations_from_comments.rb
@@ -0,0 +1,8 @@
+class RemoveOldRelationsFromComments < ActiveRecord::Migration[5.0]
+  def change
+    remove_reference :comments, :project, index: true, foreign_key: true
+    remove_reference :comments, :package, index: true, foreign_key: true
+    remove_reference :comments, :bs_request, index: true
+    remove_column :comments, :type, :string
+  end
+end
