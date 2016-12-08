f027c49132eee81029604e30c1d52598b3defb79
Henne Vogelsang <hvogel@opensuse.org>
2016-12-08 13:17:01 +0100

    [webui] Remove extra white space

--- src/api/app/controllers/webui/project_controller.rb
+++ src/api/app/controllers/webui/project_controller.rb
@@ -767,7 +767,7 @@ class Webui::ProjectController < Webui::WebuiController
     find_maintenance_infos
     @packages = Array.new
     @project.packages.order_by_name.pluck(:name, :updated_at).each do |p|
-      @packages << [p[0], p[1].to_i.to_s]  # convert Time to epoch ts and then to string
+      @packages << [p[0], p[1].to_i.to_s] # convert Time to epoch ts and then to string
     end
     @ipackages = @project.expand_all_packages.find_all{ |ip| !@packages.map{|p| p[0]}.include?(ip[0]) }
     @linking_projects = @project.linked_by_projects.pluck(:name)
