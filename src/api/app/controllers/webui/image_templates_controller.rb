1c4831ab0d031f09886bce55c31e7994e535116d
Christian Bruckmayer <cbruckmayer@suse.com>
2016-12-13 11:46:18 +0100

    [api] Publish image templates to /public/image_templates
    To make it possible to fetch the image templates via interconnect

--- src/api/app/controllers/webui/image_templates_controller.rb
+++ src/api/app/controllers/webui/image_templates_controller.rb
@@ -1,7 +1,10 @@
 class Webui::ImageTemplatesController < Webui::WebuiController
-  before_action :require_login
-
   def index
     @projects = Project.image_templates
+
+    respond_to do |format|
+      format.html
+      format.xml
+    end
   end
 end
