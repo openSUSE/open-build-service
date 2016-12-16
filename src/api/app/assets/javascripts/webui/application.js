openSUSE/open-build-service

commit 23dedb58d9923fb7d9389a39d23acac90c71be8b

[webui] DRY css for .close_dialog and #remove_dialog
* Combine duplicated css of .close_dialog and #remove_dialog
* Rename .close_dialog to .close-dialog

src/api/app/assets/javascripts/webui/application.js
@@ -252,7 +252,7 @@ $(function() {
     $($(this).data('target')).removeClass('hidden');
   });
 
-  $('.close_dialog').on('click', function() {
+  $('.close-dialog').on('click', function() {
     $($(this).data('target')).addClass('hidden');
   });
 });

src/api/app/assets/stylesheets/webui/application/dialog.scss
@@ -32,15 +32,7 @@
         color: #666;
       }
     }
-    #remove_dialog {
-      border-radius: 5px;
-      border: 1px solid #eee;
-      padding: 8px;
-      /* Make it look like the default option*/
-      font-weight: bold;
-      font-size: 1.2em;
-    }
-    .close_dialog {
+    .close-dialog {
       border-radius: 5px;
       border: 1px solid #eee;
       padding: 8px;

src/api/app/helpers/webui/webui_helper.rb
@@ -233,7 +233,7 @@ module Webui::WebuiHelper
   end
 
   def remove_dialog_tag(text)
-    link_to(text, '#', title: 'Close', id: 'remove_dialog')
+    link_to(text, '#', title: 'Close', id: 'remove_dialog', class: "close-dialog")
   end
 
   # @param [String] user login of the user

src/api/spec/helpers/webui/webui_helper_spec.rb
@@ -151,9 +151,9 @@ RSpec.describe Webui::WebuiHelper do
   describe '#remove_dialog_tag' do
     it "generates a link element and uses it's parameter as text field" do
       expect(remove_dialog_tag('Some text')).to eq(
-        '<a title="Close" id="remove_dialog" href="#">Some text</a>')
+        '<a title="Close" id="remove_dialog" class="close-dialog" href="#">Some text</a>')
       expect(remove_dialog_tag('Some other text')).to eq(
-        '<a title="Close" id="remove_dialog" href="#">Some other text</a>')
+        '<a title="Close" id="remove_dialog" class="close-dialog" href="#">Some other text</a>')
     end
   end
 

commit 684cb501ab0dd3ccbec5ae25056304464fd2a301

[webui] Fix closing of package maintainer list
This broke with 23dedb58d9923fb7d9389a39d23acac90c71be8b

src/api/app/assets/javascripts/webui/application.js
@@ -255,8 +255,8 @@ $(function() {
   $('.show_dialog').on('click', function() {
     $($(this).data('target')).removeClass('hidden');
   });
+});
 
-  $('.close-dialog').on('click', function() {
-    $($(this).data('target')).addClass('hidden');
-  });
+$(document).on('click','.close-dialog', function() {
+  $($(this).data('target')).addClass('hidden');
 });

src/api/app/views/webui/request/_package_maintainers_dialog.html.erb
@@ -15,7 +15,7 @@
          </table>
         
         <div class="dialog-buttons">
-          <%= link_to('close', '#', title: 'Close', class: 'close_dialog', data: { target: '#maintainers_dialog' }) %>
+          <%= link_to('close', '#', title: 'Close', class: 'close-dialog', data: { target: '#maintainers_dialog' }) %>
         </div>
       </div>
     </div>