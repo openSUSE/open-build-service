04e6625ecaaffecb3685fa30e9b45376273f1137
Ana María Martínez Gómez <ammartinez@suse.de>
2016-12-19 12:32:40 +0100

    [ci] Tests for Image Templates

---
+++ src/api/spec/features/webui/image_templates_spec.rb
@@ -0,0 +1,32 @@
+require "browser_helper"
+# WARNING: This test require real backend answers for projects/packages, make
+# sure you uncomment this line and start a test backend.
+# CONFIG['global_write_through'] = true
+
+RSpec.feature "ImageTemplates", type: :feature, js: true do
+  let(:admin) { create(:admin_user, login: 'admin') }
+  let(:project) { create(:project, name: "my_project") }
+  let!(:attrib) { create(:template_attrib, project: project) }
+  let!(:package1) { create(:package, project: project, name: "first_package") }
+  let!(:package2) { create(:package, project: project, name: "second_package") }
+
+  scenario "branch image template" do
+    login(admin)
+
+    visit root_path
+    find('.proceed_text > a', text: "New Image").click
+
+    expect(page).to have_text(package1)
+    expect(find("input[data-package='#{package1}']", visible: false)['checked']).to be true
+    expect(find("input[data-package='#{package2}']", visible: false)['checked']).to be false
+
+    expect(page).to have_field('target_package', with: package1)
+    find("input[data-package='#{package2}']", visible: false).trigger(:click)
+    expect(page).to have_field('target_package', with: package2)
+    fill_in 'target_package', with: "custom_name"
+
+    click_button("Create appliance")
+    expect(page).to have_text("Successfully branched package")
+    expect(page).to have_text("home:admin > custom_name")
+  end
+end
