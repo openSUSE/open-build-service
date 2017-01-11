248091db6ade8ec9469ef3b66417f34cea92de3f
Evan Rolfe <evanrolfe@gmail.com>
2017-01-11 15:22:16 +0100

    [webui] refactor specs

--- src/api/spec/controllers/projects/public_key_controller_controller_spec.rb
+++ src/api/spec/controllers/projects/public_key_controller_controller_spec.rb
@@ -1,17 +1,11 @@
 require 'rails_helper'
 require 'webmock/rspec'
 
-RSpec.describe Projects::PublicKeyController, type: :controller, vcr: true do
+RSpec.describe Projects::PublicKeyController, type: :controller do
   describe 'GET #show' do
-    let(:project) do
-      create(
-        :project,
-        name: "test_project",
-        title: "Test Project"
-      )
-    end
-
+    let(:project) { create(:project, name: "test_project", title: "Test Project") }
     let(:backend_url) { CONFIG['source_url'] + PublicKey.send(:backend_url, project.name) }
+    let(:gpg_public_key) { Faker::Lorem.characters(1024) }
 
     before do
       stub_request(:get, backend_url).and_return(body: keyinfo_response)
@@ -20,7 +14,6 @@ RSpec.describe Projects::PublicKeyController, type: :controller, vcr: true do
     end
 
     context 'with a project that has a public key and an ssl certificate' do
-      let(:gpg_public_key) { Faker::Lorem.characters(1024) }
       let(:ssl_certificate) { Faker::Lorem.characters(1024) }
       let(:keyinfo_response) do
         %(<keyinfo project="Test"><pubkey algo="rsa">#{gpg_public_key}</pubkey><sslcert>#{ssl_certificate}</sslcert></keyinfo>)
@@ -31,7 +24,6 @@ RSpec.describe Projects::PublicKeyController, type: :controller, vcr: true do
     end
 
     context 'with a project that has a public key and no ssl certificate' do
-      let(:gpg_public_key) { Faker::Lorem.characters(1024) }
       let(:keyinfo_response) do
         %(<keyinfo project="Test"><pubkey algo="rsa">#{gpg_public_key}</pubkey></keyinfo>)
       end
