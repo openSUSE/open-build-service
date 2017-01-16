388c2c2fdb9b4364c08639cf8e17ba2fe7a3253d
Moises Deniz Aleman <mdeniz@suse.com>
2017-01-16 17:42:38 +0100

    [webui] Set the default value for project listing filters

--- src/api/db/migrate/20170111114943_add_exclude_projects_to_configuration.rb
+++ src/api/db/migrate/20170111114943_add_exclude_projects_to_configuration.rb
@@ -1,7 +1,7 @@
 class AddExcludeProjectsToConfiguration < ActiveRecord::Migration[5.0]
   def self.up
-    add_column :configurations, :unlisted_projects_filter, :string
-    add_column :configurations, :unlisted_projects_filter_description, :string
+    add_column :configurations, :unlisted_projects_filter, :string, default: '^home:.+'
+    add_column :configurations, :unlisted_projects_filter_description, :string, default: 'home projects'
   end
 
   def self.down
