openSUSE/open-build-service

commit b9f387ec96b89e8fe07f1d50f7ce614a5b1755ac

[ci] Enable the Performance/RedundantMatch cop
Enable the Performance/RedundantMatch Rubocop cop and let Rubocop to
correct the offenses.

src/api/lib/tasks/databases.rake
@@ -62,7 +62,7 @@ namespace :db do
       constraints = Array.new
       added_comma = false
       structure.each_line do |line|
-        if line.match(%{[ ]*CONSTRAINT})
+        if line =~ %{[ ]*CONSTRAINT}
           unless line.end_with?(",\n")
             added_comma = true
             line = line[0..-2] + ",\n"

src/api/.rubocop.yml
@@ -198,6 +198,10 @@ Lint/UselessAssignment:
 Performance/RedundantBlockCall:
   Enabled: true
 
+# Identifies uses of `Regexp#match` and `String#match where `=~` could be used as well
+Performance/RedundantMatch:
+  Enabled: true
+
 # Identifies places where Hash#merge! can be replaced by Hash#[]=
 Performance/RedundantMerge:
   Enabled: true

src/api/.rubocop_todo.yml
@@ -125,18 +125,6 @@ Metrics/ParameterLists:
 Metrics/PerceivedComplexity:
   Max: 73
 
-# Offense count: 10
-# Cop supports --auto-correct.
-Performance/RedundantMatch:
-  Exclude:
-    - 'app/controllers/public_controller.rb'
-    - 'app/controllers/webui/package_controller.rb'
-    - 'app/controllers/webui/patchinfo_controller.rb'
-    - 'app/helpers/webui/package_helper.rb'
-    - 'app/mixins/parse_package_diff.rb'
-    - 'app/models/project.rb'
-    - 'lib/tasks/databases.rake'
-
 # Offense count: 68
 # Cop supports --auto-correct.
 # Configuration parameters: EnforcedStyle, SupportedStyles, Include.

src/api/app/controllers/public_controller.rb
@@ -186,7 +186,7 @@ class PublicController < ApplicationController
 
   # removes /private prefix from path
   def unshift_public(path)
-    if path.match %r{/public(.*)}
+    if path =~ %r{/public(.*)}
       return $1
     else
       return path

src/api/app/controllers/webui/patchinfo_controller.rb
@@ -288,7 +288,7 @@ class Webui::PatchinfoController < Webui::WebuiController
     issue_tracker = IssueTracker.find_by(name: tracker)
     return unless issue_tracker
 
-    if bug.match(/^#{issue_tracker.regex}$/)
+    if bug =~ /^#{issue_tracker.regex}$/
       issue = Issue.find_or_create_by_name_and_tracker( issueid, issue_tracker.name )
       if issue && issue.summary.blank?
         issue.fetch_updates

src/api/app/helpers/webui/package_helper.rb
@@ -22,11 +22,11 @@ module Webui::PackageHelper
 
   def guess_code_class( filename )
     return 'xml' if %w(_aggregate _link _patchinfo _service).include?(filename) || filename.match(/.*\.service/)
-    return 'shell' if filename.match(/^rc[\w-]+$/) # rc-scripts are shell
-    return 'python' if filename.match(/^.*rpmlintrc$/)
+    return 'shell' if filename =~ /^rc[\w-]+$/ # rc-scripts are shell
+    return 'python' if filename =~ /^.*rpmlintrc$/
     return 'makefile' if filename == 'debian.rules'
     return 'baselibs' if filename == 'baselibs.conf'
-    return 'spec' if filename.match(/^macros\.\w+/)
+    return 'spec' if filename =~ /^macros\.\w+/
     ext = Pathname.new(filename).extname.downcase
     case ext
       when '.group' then return 'xml'

src/api/app/mixins/parse_package_diff.rb
@@ -28,7 +28,7 @@ module ParsePackageDiff
         spec_file_keys << filename
       elsif filename.ends_with?('.changes')
         changes_file_keys << filename
-      elsif filename.match(/.*.(patch|diff|dif)/)
+      elsif filename =~ /.*.(patch|diff|dif)/
         patch_file_keys << filename
       else
         other_file_keys << filename

src/api/app/models/project.rb
@@ -168,7 +168,7 @@ class Project < ApplicationRecord
 
   def self.autocomplete(search)
     projects = Project.where(["lower(name) like lower(?)", "#{search}%"])
-    if search.to_s.match(/home:./)
+    if search.to_s =~ /home:./
       projects.home
     else
       projects.not_home

commit a78ddb4c66bd982f8214998ff45975ecac70501c

[dist] Fix regex evaluation in databases.rake
While String#match converts strings into a regular expression before
parsing, =~ doesn't. This broke obs-server package builds because of
failing migrations.

This broke with b9f387ec96b89e8fe07f1d50f7ce614a5b1755ac

====

[  495s]
[  495s] == 20170111114943 AddExcludeProjectsToConfiguration: migrating ================
[  495s] -- add_column(:configurations, :unlisted_projects_filter, :string, {:default=>"^home:.+"})
[  495s]    -> 0.0215s
[  495s] -- add_column(:configurations, :unlisted_projects_filter_description, :string, {:default=>"home projects"})
[  495s]    -> 0.0217s
[  495s] == 20170111114943 AddExcludeProjectsToConfiguration: migrated (0.0434s) =======
[  495s]
[  495s] rake aborted!
[  495s] TypeError: type mismatch: String given
[  495s] /home/abuild/rpmbuild/BUILD/open-build-service-2.7.51.git20170125.bd9b62c/src/api/lib/tasks/databases.rake:65:in `=~'
[  495s] /home/abuild/rpmbuild/BUILD/open-build-service-2.7.51.git20170125.bd9b62c/src/api/lib/tasks/databases.rake:65:in `block (4 levels) in <top (required)>'

src/api/lib/tasks/databases.rake
@@ -62,7 +62,7 @@ namespace :db do
       constraints = Array.new
       added_comma = false
       structure.each_line do |line|
-        if line =~ %{[ ]*CONSTRAINT}
+        if line =~ /[ ]*CONSTRAINT/
           unless line.end_with?(",\n")
             added_comma = true
             line = line[0..-2] + ",\n"