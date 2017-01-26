51e57f309853fba027d1e0b13f9c4aff733b598b
Moises Deniz Aleman <mdeniz@suse.com>
2017-01-26 14:06:29 +0100

    [webui][api] Use default values for feature switch

--- src/api/config/initializers/feature.rb
+++ src/api/config/initializers/feature.rb
@@ -1,1 +1,2 @@
-Feature.set_repository(Feature::Repository::YamlRepository.new("#{Rails.root}/config/feature.yml"))
+require_dependency 'feature_switch/obs_repository'
+Feature.set_repository(Feature::Repository::ObsRepository.new("#{Rails.root}/config/feature.yml"))
