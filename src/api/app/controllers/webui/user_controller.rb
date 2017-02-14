openSUSE/open-build-service

commit 509370aff8fa1f44f3a96ff4f6f1a74aa538a9a8

[webui] Adapt all request action to new version of dataTables.
Pair programming with @mdeniz

src/api/app/controllers/webui/user_controller.rb
@@ -81,19 +81,18 @@ class Webui::UserController < Webui::WebuiController
         3 => :creator,
         5 => :priority
       }
-    sorting_field = sortable_fields[params[:iSortCol_0].to_i]
+    sorting_field = sortable_fields[params['order[0][column]'].to_i]
     sorting_field ||= :created_at
-    sorting_dir = params[:sSortDir_0].try(:to_sym)
-    sorting_dir = :asc unless ["asc", "desc"].include?(params[:sSortDir_0])
+    sorting_dir = params['order[0][dir]'].try(:to_sym)
+    sorting_dir = :asc unless ["asc", "desc"].include?(params['order[0][dir]'])
     @requests = @displayed_user.requests(params[:search].permit![:value])
     @requests_count = @requests.count
-    page_length = (params[:pageLength] || 1).to_i
-    @requests = @requests.offset(params[:displayStart].to_i).limit(page_length).reorder(sorting_field => sorting_dir)
+    @requests = @requests.offset(params[:start].to_i).limit(params[:length].to_i).reorder(sorting_field => sorting_dir)
     respond_to do |format|
       # For jquery dataTable
       format.json {
         render_json_response_for_dataTable(
-          echo_next_count: params[:sEcho].to_i + 1,
+          draw: params[:draw].to_i + 1,
           total_records_count: @displayed_user.requests.count,
           total_filtered_records_count: @requests_count,
           records: @requests.includes(:bs_request_actions)

src/api/app/controllers/webui/webui_controller.rb
@@ -116,14 +116,14 @@ class Webui::WebuiController < ActionController::Base
 
   # Renders a json response for jquery dataTables
   def render_json_response_for_dataTable(options)
-    options[:echo_next_count] ||= 1
+    options[:draw] ||= 1
     options[:total_records_count] ||= 0
     options[:total_displayed_records] ||= 0
     response = {
-      sEcho:                options[:echo_next_count].to_i + 1,
-      iTotalRecords:        options[:total_records_count].to_i,
-      iTotalDisplayRecords: options[:total_filtered_records_count].to_i,
-      data:                 options[:records].map do |record|
+      draw:            options[:draw].to_i,
+      recordsTotal:    options[:total_records_count].to_i,
+      recordsFiltered: options[:total_filtered_records_count].to_i,
+      data:            options[:records].map do |record|
         if block_given?
           yield record
         else

commit 80a2dfcf2379bfe9ce9bd925d78d7ea34faa3966

[webui] Fix request sorting ordering by column
Regression introduced by 509370aff8fa1f44f3a96ff4f6f1a74aa538a9a8.
Fixes #2572.

src/api/app/controllers/webui/user_controller.rb
@@ -78,10 +78,10 @@ class Webui::UserController < Webui::WebuiController
         3 => :creator,
         5 => :priority
       }
-    sorting_field = sortable_fields[params['order[0][column]'].to_i]
-    sorting_field ||= :created_at
-    sorting_dir = params['order[0][dir]'].try(:to_sym)
-    sorting_dir = :asc unless ["asc", "desc"].include?(params['order[0][dir]'])
+    order_params = params.fetch('order', {}).fetch('0', {})
+    sorting_field = sortable_fields[order_params.fetch('column', nil).to_i]
+    sorting_dir = order_params['dir'].try(:to_sym)
+    sorting_dir = :asc unless [:asc, :desc].include?(sorting_dir)
     search = params[:search] ? params[:search].permit![:value] : ""
     request_methods = {
       'all_requests_table'      => :requests,