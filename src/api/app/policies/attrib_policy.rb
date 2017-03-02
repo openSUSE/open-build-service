eefe935a824700fcc64a01d1b0282c8489fa5d21
Björn Geuken <bgeuken@suse.de>
2017-03-02 17:52:03 +0100

    [webui] Remove unnecessary nil checks in AttribPolicy#create?

--- src/api/app/policies/attrib_policy.rb
+++ src/api/app/policies/attrib_policy.rb
@@ -12,19 +12,20 @@ class AttribPolicy < ApplicationPolicy
         namespace_perms = @record.attrib_type.attrib_namespace.attrib_namespace_modifiable_bies
       end
     end
+
     # no specific rules set for the attribute, check if the user can modify the container
     if type_perms.empty? && namespace_perms.empty? && @record.container.present?
       return @user.can_modify_project?(@record.container) if @record.container.kind_of? Project
       return @user.can_modify_package?(@record.container)
     else
       type_perms.each do |rule|
-        next if rule.user && rule.user != @user
-        next if rule.group && !@user.is_in_group?(rule.group)
+        next if rule.user != @user
+        next if !@user.is_in_group?(rule.group)
         next if rule.role && !@user.has_local_role?(rule.role, @record.container)
         return true
       end
       namespace_perms.each do |rule|
-        next if rule.user && rule.user != @user
+        next if rule.user != @user
         next if rule.group && !@user.is_in_group?(rule.group)
         return true
       end
