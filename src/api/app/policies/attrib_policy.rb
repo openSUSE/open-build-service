2254fe2253acddc4cb67d54aa606b5b64608be6c
Henne Vogelsang <hvogel@opensuse.org>
2017-03-09 11:26:20 +0100

    [api][webui] Fix attrib policy scope
    Only check AttribType rules and not AttribNamespace rules for
    the Attrib policy. Fixes #2782

--- src/api/app/policies/attrib_policy.rb
+++ src/api/app/policies/attrib_policy.rb
@@ -3,13 +3,16 @@ class AttribPolicy < ApplicationPolicy
     # Admins can write everything
     return true if @user.is_admin?
 
-    at_modifiables = modifiables
-    if at_modifiables.empty? && @record.container.present?
+    if @record.attrib_type.nil? || @record.attrib_type.attrib_type_modifiable_bies.empty?
       # No specific rules set for the attribute, check if the user can modify the container
       @record.container.can_be_modified_by?(@user)
     else
-      # check for modifiable_by rules
-      permissions_for_modifiables?(at_modifiables)
+      # check for type modifiable_by rules
+      @record.attrib_type.attrib_type_modifiable_bies.any? do |rule|
+        rule.user == @user ||
+        @user.is_in_group?(rule.group) ||
+          (rule.try(:role) && @user.has_local_role?(rule.role, @record.container))
+      end
     end
   end
 
@@ -19,29 +22,5 @@ class AttribPolicy < ApplicationPolicy
 
   def destroy?
     create?
-  end
-
-  private
-
-  # Parses AttribTypeModifiableBy and AttribNamespaceModifiableBy association
-  # collections and checks the users permissions.
-  #
-  # Permission check is based on:
-  #   - User
-  #   - Group association of user
-  #   - Role association of user (Only for AttribTypeModifiableBy)
-  def permissions_for_modifiables?(attrib_modifiables)
-    attrib_modifiables.any? do |rule|
-      rule.user == @user ||
-      @user.is_in_group?(rule.group) ||
-        (rule.try(:role) && @user.has_local_role?(rule.role, @record.container))
-    end
-  end
-
-  # Collects AttribTypeModifiableBy and AttribNamespaceModifiableBy associated to
-  #  an AttribType object and returns them in an Array
-  def modifiables
-    @record.attrib_type.try(:attrib_type_modifiable_bies).to_a |
-      @record.attrib_type.try(:attrib_namespace).try(:attrib_namespace_modifiable_bies).to_a
   end
 end
