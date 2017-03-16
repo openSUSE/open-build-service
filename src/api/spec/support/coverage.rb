af2320138f2f218761f5aaf97f4857279d42080d
Björn Geuken <bgeuken@suse.de>
2017-03-16 16:45:31 +0100

    [ci] Update & sync simplecov filters

--- src/api/spec/support/coverage.rb
+++ src/api/spec/support/coverage.rb
@@ -3,8 +3,10 @@ Coveralls.wear_merged!('rails')
 
 # SimpleCov configuration
 SimpleCov.start 'rails' do
+  # NOTE: Keep filters in sync with test/test_helper.rb
   add_filter '/app/indices/'
   add_filter '/app/models/user_ldap_strategy.rb'
+  add_filter '/lib/templates/'
   merge_timeout 3600
   formatter Coveralls::SimpleCov::Formatter
 end
