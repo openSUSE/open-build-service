21a692250bb4d4190e0a3c77b312519a1f51f98d
Adrian Schröter <adrian@suse.de>
2017-03-16 08:25:46 +0100

    [api] don't fail on existing double repository entries

--- src/api/db/migrate/20170306084558_change_repositories_remote_project_name_to_not_null.rb
+++ src/api/db/migrate/20170306084558_change_repositories_remote_project_name_to_not_null.rb
@@ -1,9 +1,13 @@
 class ChangeRepositoriesRemoteProjectNameToNotNull < ActiveRecord::Migration[5.0]
   def up
-    execute 'UPDATE repositories SET remote_project_name = "" WHERE remote_project_name is null'
+    transaction do
+      execute 'UPDATE repositories SET remote_project_name = "" WHERE remote_project_name is null'
+      # drop existsing double entries, must be a ruby loop for the path_elements and reference cleanup
+      Repository.find_by_sql("select A.* from repositories as A  LEFT JOIN repositories as B ON A.db_project_id = B.db_project_id and A.name = B.name where A.id != B.id").each.destroy
 
-    change_column_null :repositories, :remote_project_name, false
-    change_column_default :repositories, :remote_project_name, ''
+      change_column_null :repositories, :remote_project_name, false
+      change_column_default :repositories, :remote_project_name, ''
+    end
   end
 
   def down
