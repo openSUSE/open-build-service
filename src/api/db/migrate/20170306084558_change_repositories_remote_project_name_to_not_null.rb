881f566eb41f1634f903396e303ae6b938048cd4
Evan Rolfe <evanrolfe@gmail.com>
2017-03-17 13:05:21 +0000

    [api] fix migration to only delete one of each duplicate pair of repositories

--- src/api/db/migrate/20170306084558_change_repositories_remote_project_name_to_not_null.rb
+++ src/api/db/migrate/20170306084558_change_repositories_remote_project_name_to_not_null.rb
@@ -1,9 +1,34 @@
 class ChangeRepositoriesRemoteProjectNameToNotNull < ActiveRecord::Migration[5.0]
   def up
-    transaction do
+    Repository.transaction do
+      sql = <<-SQL
+        SELECT a.*
+        FROM repositories AS a
+        LEFT JOIN repositories AS b
+        ON a.db_project_id = b.db_project_id
+        AND a.name = b.name
+        WHERE a.id != b.id
+        GROUP BY a.db_project_id, a.name
+      SQL
+
+      repos_with_duplicates = Repository.find_by_sql(sql)
+
+      sql = <<-SQL
+        SELECT b.*
+        FROM repositories a
+        LEFT JOIN repositories AS b
+        ON a.db_project_id = b.db_project_id
+        AND a.name = b.name
+        WHERE a.id != b.id
+        AND a.id IN (#{repos_with_duplicates.map(&:id).join(', ')})
+      SQL
+
+      duplicate_repos = Repository.find_by_sql(sql)
+
+      # must be a ruby loop for the path_elements and reference cleanup
+      duplicate_repos.each(&:destroy)
+
       execute 'UPDATE repositories SET remote_project_name = "" WHERE remote_project_name is null'
-      # drop existsing double entries, must be a ruby loop for the path_elements and reference cleanup
-      Repository.find_by_sql("select A.* from repositories as A  LEFT JOIN repositories as B ON A.db_project_id = B.db_project_id and A.name = B.name where A.id != B.id").each.destroy
 
       change_column_null :repositories, :remote_project_name, false
       change_column_default :repositories, :remote_project_name, ''
