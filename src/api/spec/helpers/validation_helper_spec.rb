533487d20d948b8a07574a6be294f5d280669175
Manuel Schnitzer <mschnitzer@suse.com>
2017-03-22 16:37:32 +0100

    [ci] Add validation_helper_spec.rb
    This implements the missing tests for the rspec test suite.
    
    Related to card:
    https://trello.com/c/CJtlSMoy/268-app-helpers-validation-helper-rb

---
+++ src/api/spec/helpers/validation_helper_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe ValidationHelper do
+  describe '#valid_project_name?' do
+    it 'calls Project.valid_name?' do
+      expect(Project).to receive(:valid_name?).with("home:mschnitzer")
+      valid_project_name?("home:mschnitzer")
+    end
+  end
+
+  describe '#valid_project_name!' do
+    it 'raises an exception if a project name is invalid' do
+      expect { valid_project_name!("home mschnitzer") }.to raise_error(ValidationHelper::InvalidProjectNameError)
+    end
+
+    it 'does not raise an exception on valid project names' do
+      expect { valid_project_name!("home:mschnitzer") }.to_not raise_error(ValidationHelper::InvalidProjectNameError)
+    end
+  end
+end
