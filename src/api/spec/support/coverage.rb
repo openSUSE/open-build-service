6dab7556216a04079a887f8eeedb59d120ceb6a6
Henne Vogelsang <hvogel@opensuse.org>
2017-03-23 13:37:49 +0100

    [api][webui] Switch from coveralls to codecov
    coveralls stopped merging results, try out a new
    service.

--- src/api/spec/support/coverage.rb
+++ src/api/spec/support/coverage.rb
@@ -1,6 +1,3 @@
-# start coverage tracking
-Coveralls.wear_merged!('rails')
-
 # SimpleCov configuration
 SimpleCov.start 'rails' do
   # NOTE: Keep filters in sync with test/test_helper.rb
@@ -10,5 +7,5 @@ SimpleCov.start 'rails' do
   add_filter '/lib/memory_debugger.rb'
   add_filter '/lib/memory_dumper.rb'
   merge_timeout 3600
-  formatter Coveralls::SimpleCov::Formatter
+  formatter SimpleCov::Formatter::Codecov
 end
