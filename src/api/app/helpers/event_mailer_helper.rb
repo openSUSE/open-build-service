e4cafccbf3739cd1175504aa911f1a4e8f05b4d3
Moises Deniz Aleman <mdeniz@suse.com>
2017-03-28 02:59:02 +0200

    [ci][webui] Refactor and add specs for even_mailer_helper

--- src/api/app/helpers/event_mailer_helper.rb
+++ src/api/app/helpers/event_mailer_helper.rb
@@ -1,12 +1,6 @@
 module EventMailerHelper
-  def project_or_package_text(project, package, opts = {})
-    if package.present?
-      text = "#{project}/#{package}"
-    else
-      text = project
-    end
-    return unless opts[:short].nil?
-    return "package #{text}" if package.present?
-    "project #{text}"
+  def project_or_package_text(project, package)
+    return "package #{project}/#{package}" if package.present?
+    "project #{project}"
   end
 end
