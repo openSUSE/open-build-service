d000147cb5a5cfe239a1411587eaadf563d11ee3
Björn Geuken <bgeuken@suse.de>
2017-04-19 14:58:43 +0200

    [ci] Make testing submit requests more stable
    This ensures that the element we want to test is there before we verify
    correctness of the flash message.

--- src/api/test/functional/webui/request_controller_test.rb
+++ src/api/test/functional/webui/request_controller_test.rb
@@ -343,7 +343,7 @@ class Webui::RequestControllerTest < Webui::IntegrationTest
     click_button 'Ok'
 
     assert_equal package_show_path(project: 'Apache', package: 'apache2'), page.current_path
-    assert page.has_content?(/Created submit request \d+ to kde4/)
+    assert find('.flash-content').has_content?(/Created submit request \d+ to kde4/)
     within '#flash-messages' do
       click_link 'submit request'
     end
