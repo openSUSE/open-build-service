2008d4934343b1aef99489592bb0dde5e26cfd37
Ana María Martínez Gómez <ammartinez@suse.de>
2017-05-16 14:38:33 +0200

    Add bs requests graph to OBS-statistics.R
    Bs requests over time graph can be generated with the OBS-statistics.R
    file.

--- dist/OBS-statistics.R
+++ dist/OBS-statistics.R
@@ -18,3 +18,25 @@ users_data <- data.frame(
 
 ggplot(data=users_data, aes(x=date, y=users)) +
   geom_line()
+
+
+###############################################################################################
+
+# NUMBER OF BS REQUESTS GRAPH
+
+# Read bs_requests data and save it in a dataframe variable
+dataBsRequests <- read.csv('number_bs_requests.txt')
+
+# In this case, we are going to use a simple geom_line without points
+
+# require library, in case that you don't have then installed, use: install.packages("ggplot2")
+library(ggplot2)
+
+bs_requests_data <- data.frame(
+  bs_requests = dataBsRequests$bs_requests,
+  date = as.Date(dataBsRequests$date, format = "%Y-%m-%d") # convert String to Date
+)
+
+ggplot(data=bs_requests_data, aes(x=date, y=bs_requests)) +
+  geom_line()
+
