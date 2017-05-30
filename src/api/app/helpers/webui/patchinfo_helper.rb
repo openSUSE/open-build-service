7aa9674830a2bfe35c8b6c9e37cc7d29808f0b4d
Björn Geuken <bgeuken@suse.de>
2017-05-30 08:57:13 +0200

    [webui] Move patchinfo issue handling from helper to view
    The code is only used once and not very complex. No need to have it in a
    helper.

--- src/api/app/helpers/webui/patchinfo_helper.rb
+++ src/api/app/helpers/webui/patchinfo_helper.rb
@@ -4,18 +4,4 @@ module Webui::PatchinfoHelper
     args.insert(0, link_to(@package, package_show_path(project: @project, package: @package)))
     project_bread_crumb(*args)
   end
-
-  def issue_link( issue )
-    # list issue-names with urls and summary from the patchinfo-file
-    # issue[0] = tracker-name
-    # issue[1] = issueid
-    # issue[2] = issue-url
-    # issue[3] = issue-summary
-
-    if issue[0] == "CVE"
-      content_tag(:li, link_to((issue[1]).to_s, issue[2]) + ": #{issue[3]}")
-    else
-      content_tag(:li, link_to("#{issue[0]}##{issue[1]}", issue[2]) + ": #{issue[3]}")
-    end
-  end
 end
