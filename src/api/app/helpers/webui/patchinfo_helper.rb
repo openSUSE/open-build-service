ea652f20df582c54e8e0f67f666f0136902c7694
Björn Geuken <bgeuken@suse.de>
2017-05-30 08:14:15 +0200

    [ci] Add tests Webui::PatchinfoHelper#patchinfo_bread_crumb
    The generation of the package link was failing calling patchinfo_bread_crumb
    in the helper spec. Thus I changed this in the helper.
    
    ====
    
    ActionController::UrlGenerationError: No route matches {:action=>"show", :package=>"package_1", :project=>"project_1"}

--- src/api/app/helpers/webui/patchinfo_helper.rb
+++ src/api/app/helpers/webui/patchinfo_helper.rb
@@ -1,8 +1,8 @@
 module Webui::PatchinfoHelper
   include Webui::ProjectHelper
-  def patchinfo_bread_crumb( *args )
-    args.insert(0, link_to( @package, action: :show, project: @project, package: @package ))
-    project_bread_crumb( *args )
+  def patchinfo_bread_crumb(*args)
+    args.insert(0, link_to(@package, package_show_path(project: @project, package: @package)))
+    project_bread_crumb(*args)
   end
 
   def issue_link( issue )
