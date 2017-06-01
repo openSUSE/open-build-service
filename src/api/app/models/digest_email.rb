bc07f576ce4165f6efe35cba942699b11b5324c8
Evan Rolfe <evanrolfe@gmail.com>
2017-06-01 14:27:56 +0100

    [webui][api] DigestEmail now belongs to a user or a group

--- src/api/app/models/digest_email.rb
+++ src/api/app/models/digest_email.rb
@@ -1,5 +1,6 @@
 class DigestEmail < ApplicationRecord
-  belongs_to :event_subscription
+  belongs_to :user
+  belongs_to :group
   has_and_belongs_to_many :events, through: :digest_email_events,
                                    class_name: 'Event::Base',
                                    foreign_key: :digest_email_id,
@@ -10,9 +11,15 @@ end
 #
 # Table name: digest_emails
 #
-#  id                    :integer          not null, primary key
-#  event_subscription_id :integer          not null
-#  sent_at               :datetime
-#  created_at            :datetime         not null
-#  updated_at            :datetime         not null
+#  id         :integer          not null, primary key
+#  user_id    :integer          indexed
+#  group_id   :integer          indexed
+#  sent_at    :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_digest_emails_on_group_id  (group_id)
+#  index_digest_emails_on_user_id   (user_id)
 #
--- src/api/db/structure.sql
+++ src/api/db/structure.sql
@@ -420,11 +420,14 @@ CREATE TABLE `delayed_jobs` (
 
 CREATE TABLE `digest_emails` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `event_subscription_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `group_id` int(11) DEFAULT NULL,
   `sent_at` datetime DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `index_digest_emails_on_user_id` (`user_id`),
+  KEY `index_digest_emails_on_group_id` (`group_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `digest_emails_events` (
