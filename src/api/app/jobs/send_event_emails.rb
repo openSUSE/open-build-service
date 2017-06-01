c611842ddffd1dcfb4534a95a9a1f9bf16879000
Evan Rolfe <evanrolfe@gmail.com>
2017-06-01 15:02:14 +0100

    [webui][api] SendEventEmails add emails to digest for users with digest_email_enabled == true

--- src/api/app/jobs/send_event_emails.rb
+++ src/api/app/jobs/send_event_emails.rb
@@ -13,9 +13,28 @@ class SendEventEmails
         # if someone else saved it too, better don't continue - we're not alone
         return false
       end
-      subscribers = event.subscribers
-      next if subscribers.empty?
-      EventMailer.event(subscribers, event).deliver_now
+
+      next if event.subscribers.empty?
+
+      subscribers = event.subscribers.select { |subscriber| subscriber.digest_email_enabled == false }
+      digest_subscribers = event.subscribers.select { |subscriber| subscriber.digest_email_enabled == true }
+
+      # Send an email to the subscribers with digest_email_enabled == false
+      if subscribers.any?
+        EventMailer.event(subscribers, event).deliver_now
+      end
+
+      # Add to a digest email for the subscribers with digest_email_enabled == true
+      digest_subscribers.each do |subscriber|
+        digest_email =
+          if subscriber.is_a? User
+            DigestEmail.find_or_create_by(user: subscriber, sent_at: nil)
+          elsif subscriber.is_a? Group
+            DigestEmail.find_or_create_by(group: subscriber, sent_at: nil)
+          end
+
+        digest_email.events << event
+      end
     end
     true
   end
