bc83f29aa8aa7780e2fe08ab50ea8670ce30ae49
Adrian Schröter <adrian@suse.de>
2017-06-22 12:12:01 +0200

    [webui] Check permissions before doing backend build operations
    Verify that user has write permissions for project before doing backend
    call. Added tests to cover new code.
    
    Pair-programmed with @eduardoj and @bgeuken

--- src/api/spec/models/package_spec.rb
+++ src/api/spec/models/package_spec.rb
@@ -333,4 +333,40 @@ RSpec.describe Package, vcr: true do
     it { expect(results_test_package_source.first.state).to eq('finished') }
     it { expect(results_test_package_source.first.details).to be_nil }
   end
+
+  describe '#backend_build_command' do
+    let(:params) { ActionController::Parameters.new(arch: 'x86') }
+    let(:backend_url) { "#{CONFIG['source_url']}/build/#{package.project.name}?cmd=rebuild&arch=x86" }
+
+    subject { package.backend_build_command(:rebuild, package.project.name, params) }
+
+    context 'backend response is successful' do
+      before { stub_request(:post, backend_url) }
+
+      it { is_expected.to be_truthy }
+    end
+
+    context 'backend response fails' do
+      before { stub_request(:post, backend_url).and_raise(ActiveXML::Transport::Error) }
+
+      it { is_expected.to be_falsey }
+    end
+
+    context 'user has no access rights for the project' do
+      let(:other_project) { create(:project) }
+
+      before do
+        # check_write_access! depends on the Rails env. We have to workaround this here.
+        allow(Rails.env).to receive(:test?).and_return false
+        # also check_write_access! relies on User.current
+        login(user)
+
+        allow(Suse::Backend).to receive(:post).never
+      end
+
+      subject { package.backend_build_command(:rebuild, other_project.name, params) }
+
+      it { is_expected.to be_falsey }
+    end
+  end
 end
