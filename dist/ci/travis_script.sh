dd4bb8fd43bf34502011e9181a7a517f3f42f900
Björn Geuken <bgeuken@suse.de>
2017-06-27 14:47:10 +0200

    [ci] Disable codecov in parts our test suite

--- dist/ci/travis_script.sh
+++ dist/ci/travis_script.sh
@@ -47,13 +47,15 @@ if test -z "$SUBTEST"; then
       make test_unit
       ;;
     *)
+      unset DO_COVERAGE
       make -C ../../ rubocop
       bundle exec rails assets:precompile &> /dev/null
+      export DO_COVERAGE=1
       bundle exec rails test:api
       bundle exec rails test:webui
       bundle exec rspec
-      jshint .
       unset DO_COVERAGE
+      jshint .
       bundle exec rails test:spider
       ;;
   esac
