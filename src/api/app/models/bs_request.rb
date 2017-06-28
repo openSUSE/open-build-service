c69c8bfcbcde4c046eb37fcbec6761b0aa8618d0
David Kang <dkang@suse.com>
2017-06-28 10:42:39 +0200

    [webui] Refactor Project#open_requests
    Project#open_requests has been refactored to get rid of BsRequest.collection

--- src/api/app/models/bs_request.rb
+++ src/api/app/models/bs_request.rb
@@ -32,12 +32,15 @@ class BsRequest < ApplicationRecord
   scope :to_accept, -> { where(state: 'new').where('accept_at < ?', DateTime.now) }
   # Scopes for collections
   scope :with_actions, -> { joins(:bs_request_actions).distinct.order(priority: :asc, id: :desc) }
-  scope :with_involved_projects, ->(project_ids) { where(bs_request_actions: { target_project_id: project_ids } ) }
-  scope :with_involved_packages, ->(package_ids) { where(bs_request_actions: { target_package_id: package_ids } ) }
+  scope :with_involved_projects, ->(project_ids) { where(bs_request_actions: { target_project_id: project_ids }) }
+  scope :with_involved_packages, ->(package_ids) { where(bs_request_actions: { target_package_id: package_ids }) }
+
+  scope :with_source_subprojects, ->(project_name) { where('bs_request_actions.source_project like ?', project_name) }
+  scope :with_target_subprojects, ->(project_name) { where('bs_request_actions.target_project like ?', project_name) }
 
   scope :in_states, ->(states) { where(state: states) }
-  scope :with_types, ->(types) { where('bs_request_actions.type in (?)', types) }
-  scope :from_source_project, ->(source_project) { where('bs_request_actions.source_project = ?', source_project) }
+  scope :with_types, ->(types) { where(bs_request_actions: { type: types}) }
+  scope :from_source_project, ->(source_project) { where(bs_request_actions: { source_project: source_project }) }
   scope :in_ids, ->(ids) { where(id: ids) }
   scope :not_creator, ->(login) { where.not(creator: login) }
   # Searching capabilities using dataTable (1.9)
