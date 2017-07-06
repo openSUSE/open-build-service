57d81053fef81f0a1dcae6aa2ecc04069740cb1f
Evan Rolfe <evanrolfe@gmail.com>
2017-07-06 14:00:06 +0100

    [webui] add mailcatcher gem for testing emails

--- src/api/config/environments/development.rb
+++ src/api/config/environments/development.rb
@@ -19,7 +19,8 @@ OBSApi::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: '0.0.0.0', port: 1025 }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
