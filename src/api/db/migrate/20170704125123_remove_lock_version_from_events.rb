21f1cca0a51e0eaa045a186720dd5d134cbe906a
Evan Rolfe <evanrolfe@gmail.com>
2017-07-07 11:25:20 +0100

    [webui][api] fix migration by added the column type

--- src/api/db/migrate/20170704125123_remove_lock_version_from_events.rb
+++ src/api/db/migrate/20170704125123_remove_lock_version_from_events.rb
@@ -1,5 +1,5 @@
 class RemoveLockVersionFromEvents < ActiveRecord::Migration[5.1]
   def change
-    remove_column :events, :lock_version, default: 0, null: false
+    remove_column :events, :lock_version, :integer, default: 0, null: false
   end
 end
