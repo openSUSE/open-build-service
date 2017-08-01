4ab53e488ee8c220d6f5dd0f4851bc0660ee5c81
Evan Rolfe <evanrolfe@gmail.com>
2017-08-01 10:54:46 +0100

    [ci] update IssueTracker spec to make use of ActiveJob inline queue adapter

--- src/api/spec/models/issue_tracker_spec.rb
+++ src/api/spec/models/issue_tracker_spec.rb
@@ -2,20 +2,29 @@ require 'rails_helper'
 
 RSpec.describe IssueTracker do
   describe '.write_to_backend' do
-    subject { IssueTracker.write_to_backend }
+    before do
+      allow(Backend::Connection).to receive(:put_source)
+    end
 
-    it 'queues a job' do
-      expect { subject }.to have_enqueued_job(IssueTrackerWriteToBackendJob)
+    subject! { IssueTracker.write_to_backend }
+
+    it 'writes to the backend' do
+      expect(Backend::Connection).to have_received(:put_source)
     end
   end
 
   describe '.update_all_issues' do
     let!(:issue_tracker) { create(:issue_tracker, enable_fetch: true) }
 
-    subject { IssueTracker.update_all_issues }
+    before do
+      allow(IssueTracker).to receive(:find).and_return(issue_tracker)
+      allow(issue_tracker).to receive(:update_issues)
+    end
 
-    it 'queues a job' do
-      expect { subject }.to have_enqueued_job(IssueTrackerUpdateIssuesJob).with(issue_tracker.id)
+    subject! { IssueTracker.update_all_issues }
+
+    it 'updates the issues' do
+      expect(issue_tracker).to have_received(:update_issues)
     end
   end
 end
