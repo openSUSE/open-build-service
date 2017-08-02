4461e1a090fad3d10e48310274daf0d468545f15
David Kang <dkang@suse.com>
2017-08-02 16:46:35 +0200

    [webui] Allow edit kiwi package attributes
    In the same way as in Kiwi repositories.
    
    Pair-programmed by: @Ana06 and @DavidKang.

--- src/api/app/assets/javascripts/webui/application/kiwi_editor.js
+++ src/api/app/assets/javascripts/webui/application/kiwi_editor.js
@@ -21,17 +21,23 @@ function editDialog(){
   $('.overlay').show();
 }
 
-function closeDialog(){
+function closeDialog() {
   var fields = $(this).parents('.nested-fields');
-  var name = fields.find(".kiwi_repository_name");
+  var name = fields.find('.kiwi_element_name');
   var dialog = fields.find('.dialog');
-  var alias = dialog.find("[id$='alias']");
-  if (alias.val() != '') {
-    name.text(alias.val());
+  var name_package = dialog.find("[id$='name']").val();
+  if (name_package != '') {
+    name.text(name_package);
   }
   else {
-    var source_path = dialog.find("[id$='source_path']");
-    name.text(source_path.val().replace(/\//g, '_'));
+    var alias = dialog.find("[id$='alias']");
+    if (alias.val() != '') {
+      name.text(alias.val());
+    }
+    else {
+      var source_path = dialog.find("[id$='source_path']");
+      name.text(source_path.val().replace(/\//g, '_'));
+    }
   }
   $('.overlay').hide();
   dialog.addClass('hidden');
@@ -56,15 +62,23 @@ $(document).ready(function(){
     $('.overlay').show();
   });
 
-  // Edit dialog for Repositories
-  $('.repository_edit').click(editDialog);
-  $('#kiwi-repositories-list .close-dialog').click(closeDialog);
-  $('.kiwi_list_item').hover(hoverListItem, hoverListItem);
+  // Edit dialog for Repositories and Packages
+  $('.repository_edit, .package_edit').click(editDialog);
+  $('#kiwi-repositories-list .close-dialog, #kiwi-packages-list .close-dialog').click(closeDialog);
+  $('#kiwi-repositories-list .kiwi_list_item, #kiwi-packages-list .kiwi_list_item').hover(hoverListItem, hoverListItem);
 
   // After inserting new repositories add the Callbacks
   $('#kiwi-repositories-list').on('cocoon:after-insert', function(e, addedFields) {
     $('.overlay').show();
     $(addedFields).find('.repository_edit').click(editDialog);
+    $(addedFields).find('.close-dialog').click(closeDialog);
+    $(addedFields).find('.kiwi_list_item').hover(hoverListItem, hoverListItem);
+  });
+
+  // After inserting new packages add the Callbacks
+  $('#kiwi-packages-list').on('cocoon:after-insert', function(e, addedFields) {
+    $('.overlay').show();
+    $(addedFields).find('.package_edit').click(editDialog);
     $(addedFields).find('.close-dialog').click(closeDialog);
     $(addedFields).find('.kiwi_list_item').hover(hoverListItem, hoverListItem);
   });
