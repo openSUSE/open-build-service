1211787c01c78e5579ab748e91d658b1c8d6de72
Christian Bruckmayer <cbruckmayer@suse.com>
2017-08-02 11:42:46 +0200

    [webui] Refactor feature switch repository
    - Fixing YAML inheritance, hash merge is not recursive so defaults were not inherited
    - After fixing the YAML it was not necessary to merge with defaults in the Repository anymore
    - Defaults should be now defined in the defaults production hash in the feature.yml
    - Code is now basically the default behaviour of YamlRepository but we keep the
      custom ObsRepository to enable feature for a beta group in a later commit

--- src/api/lib/feature_switch/obs_repository.rb
+++ src/api/lib/feature_switch/obs_repository.rb
@@ -3,18 +3,13 @@ module Feature
     # ObsRepository for active and inactive features based on YamlRepository having default values for each key in OBS
     #
     class ObsRepository < YamlRepository
-      DEFAULTS = {
-        image_templates: true
-      }
-
       # Returns list of active features
       #
       # @return [Array<Symbol>] list of active features
       #
       def active_features
-        data = read_file(@yaml_file_name).with_indifferent_access
-        data[@environment]['features'] = DEFAULTS.merge(data[@environment]['features'])
-        data['features'] = DEFAULTS.merge(data[@environment]['features'])
+        data = read_file(@yaml_file_name)
+        data[@environment]['features'] = data[@environment]['features']
         get_active_features(data, @environment)
       end
     end
