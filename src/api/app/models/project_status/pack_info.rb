6ca7bdf4c43f28072cc22c593566d6dc92fd4645
Christian Bruckmayer <cbruckmayer@suse.com>
2017-08-02 16:21:41 +0200

    [webui][api] Move PackInfo#to_xml to xml builder view

--- src/api/app/models/project_status/pack_info.rb
+++ src/api/app/models/project_status/pack_info.rb
@@ -29,11 +29,8 @@ module ProjectStatus
       @groups << [title, role]
     end
 
-    def to_xml(options = {})
-      # return packages not having sources
-      return if srcmd5.blank?
-      xml = options[:builder] ||= Builder::XmlMarkup.new(indent: options[:indent])
-      opts = {
+    def header
+      options = {
         project:    project,
         name:       name,
         version:    version,
@@ -43,34 +40,9 @@ module ProjectStatus
         release:    release
       }
       unless verifymd5.blank? || verifymd5 == srcmd5
-        opts[:verifymd5] = verifymd5
+        options[:verifymd5] = verifymd5
       end
-      xml.package(opts) do
-        fails.each do |repository, _architecture, time, md5|
-          xml.failure(repo: repository, time: time, srcmd5: md5)
-        end
-        if develpack
-          xml.develpack(proj: develpack.project, pack: develpack.name) do
-            develpack.to_xml(builder: xml)
-          end
-        end
-
-        relationships_to_xml(xml, :persons, :person, :userid)
-        relationships_to_xml(xml, :groups, :group, :groupid)
-
-        xml.error(error) if error
-        xml.link(project: links_to.project, package: links_to.name) if links_to
-      end
-    end
-
-    def relationships_to_xml(builder, arrayname, elementname, tag)
-      arr = send(arrayname)
-      return if arr.empty?
-      builder.send(arrayname) do
-        arr.each do |element, role_name|
-          builder.send(elementname, tag => element, :role => role_name)
-        end
-      end
+      options
     end
 
     def set_versrel(versrel, time)
