9db2f70e52ffd9058c130652c11bee4c1330df0c
Henne Vogelsang <hvogel@opensuse.org>
2017-08-04 10:56:49 +0200

    [dist] Move tasks out of Rakefile/lib
    Consolidate all scattered tasks that you can call with rake into one file

--- src/api/script/delayed_job.api.rb
+++ src/api/script/delayed_job.api.rb
@@ -2,5 +2,4 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
 require 'delayed/command'
-require 'workers/import_requests.rb'
 Delayed::Command.new(ARGV).daemonize
