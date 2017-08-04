153370f03edbbcb9f7c3aa2bfb8e76cce264c967
Henne Vogelsang <hvogel@opensuse.org>
2017-08-04 10:36:34 +0200

    [frontend] Remove duplicated UpdateIssuesJob

--- src/api/script/delayed_job.api.rb
+++ src/api/script/delayed_job.api.rb
@@ -3,5 +3,4 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'environment'))
 require 'delayed/command'
 require 'workers/import_requests.rb'
-require 'workers/update_issues.rb'
 Delayed::Command.new(ARGV).daemonize
