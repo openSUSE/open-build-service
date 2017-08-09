ea643293afbf9ac228332c921ceb0321d4a164aa
Henne Vogelsang <hvogel@opensuse.org>
2017-08-09 16:17:14 +0200

    [frontend] Order clock triggers by frequency

--- src/api/config/clock.rb
+++ src/api/config/clock.rb
@@ -7,9 +7,34 @@ require 'event'
 require 'clockwork'
 
 module Clockwork
+  every(17.seconds, 'fetch notifications', thread: true) do
+    ActiveRecord::Base.connection_pool.with_connection do |_|
+      # this will return if there is already a thread running
+      UpdateNotificationEvents.new.perform
+    end
+  end
+
   every(30.seconds, 'status.refresh') do
     # this should be fast, so don't delay
     WorkerStatus.new.update_workerstatus_cache
+  end
+
+  every(30.seconds, 'send notifications') do
+    EventNotifyBackend.perform_later
+    SendEventEmailsJob.perform_later
+  end
+
+  every(10.minutes, 'project log rotates') do
+    ProjectLogRotateJob.perform_later
+  end
+
+  every(49.minutes, 'rescale history') do
+    StatusHistoryRescalerJob.perform_later
+  end
+
+  # Ensure that sphinx's searchd is running and reindex
+  every(1.hour, 'reindex sphinx') do
+    FullTextSearch.new.delay.index_and_start unless Rails.env.test? || Rails.env.development?
   end
 
   every(1.hour, 'refresh issues') do
@@ -21,44 +46,19 @@ module Clockwork
     BsRequest.delayed_auto_accept
   end
 
-  every(49.minutes, 'rescale history') do
-    StatusHistoryRescalerJob.perform_later
+  every(1.hour, 'cleanup notifications') do
+    CleanupNotifications.perform_later
   end
 
-  every(1.day, 'optimize history', thread: true) do
+  every(1.day, 'optimize history', thread: true ) do
     ActiveRecord::Base.connection_pool.with_connection do |sql|
       sql.execute 'optimize table status_histories;'
     end
   end
 
-  every(1.hour, 'cleanup notifications') do
-    CleanupNotifications.perform_later
-  end
-
-  every(30.seconds, 'send notifications') do
-    EventNotifyBackend.perform_later
-    SendEventEmailsJob.perform_later
-  end
-
-  every(17.seconds, 'fetch notifications', thread: true) do
-    ActiveRecord::Base.connection_pool.with_connection do |_|
-      # this will return if there is already a thread running
-      UpdateNotificationEvents.new.perform
-    end
-  end
-
-  # Ensure that sphinx's searchd is running and reindex
-  every(1.hour, 'reindex sphinx') do
-    FullTextSearch.new.delay.index_and_start unless Rails.env.test? || Rails.env.development?
-  end
-
   every(1.day, 'refresh dirties') do
     # inject a delayed job for every dirty project
     BackendPackage.refresh_dirty
-  end
-
-  every(10.minutes, 'project log rotates') do
-    ProjectLogRotateJob.perform_later
   end
 
   every(1.day, 'clean old events') do
