fbb07588df6ad740663f413a9fa8ab123da24477
Evan Rolfe <evanrolfe@gmail.com>
2017-08-16 09:30:53 +0100

    [api] Use find_by instead of find in UpdatePackagesIfDirtyJob
    Because otherwise it will raise ActiveRecordNotFoundError if the project
    doesn't exist which is not what we want.

--- src/api/app/jobs/update_packages_if_dirty_job.rb
+++ src/api/app/jobs/update_packages_if_dirty_job.rb
@@ -4,7 +4,7 @@ class UpdatePackagesIfDirtyJob < ApplicationJob
   self.priority = 10
 
   def perform(project_id)
-    project = Project.find(project_id)
+    project = Project.find_by(id: project_id)
     project.update_packages_if_dirty if project.present?
   end
 end
--- src/api/spec/jobs/update_packages_if_dirty_job_spec.rb
+++ src/api/spec/jobs/update_packages_if_dirty_job_spec.rb
@@ -9,13 +9,23 @@ RSpec.describe UpdatePackagesIfDirtyJob, type: :job, vcr: true do
   include ActiveJob::TestHelper
 
   describe '#perform' do
-    let!(:project) { create(:project, name: 'apache') }
-    let!(:package) { create(:package_with_file, project: project, name: 'mod_ssl') }
+    context 'the project is found' do
+      let!(:project) { create(:project, name: 'apache') }
+      let!(:package) { create(:package_with_file, project: project, name: 'mod_ssl') }
 
-    subject { UpdatePackagesIfDirtyJob.new.perform(project.id) }
+      subject { UpdatePackagesIfDirtyJob.new.perform(project.id) }
 
-    it 'creates a BackendPackge for the Package' do
-      expect { subject }.to change { BackendPackage.count }.by(1)
+      it 'creates a BackendPackge for the Package' do
+        expect { subject }.to change { BackendPackage.count }.by(1)
+      end
+    end
+
+    context 'the project is not found' do
+      subject { UpdatePackagesIfDirtyJob.new.perform(123) }
+
+      it 'returns nil' do
+        is_expected.to be_nil
+      end
     end
   end
 end
