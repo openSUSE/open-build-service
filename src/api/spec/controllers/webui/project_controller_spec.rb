80539458f2d1afbae28ff2f52217d31a7fdeebc8
Evan Rolfe <evanrolfe@gmail.com>
2017-08-23 09:25:17 +0100

    [ci] Fix flickering failure of this spec.
    The problem was that all the contexts in this spec used the same
    project & package and they also rely on the test backend running.
    So once one context ran it meant the backend package existed in the
    backend for a project with that name already for the other contexts.
    This cause failures when running without the test backend using the
    record VCR cassettes.

--- src/api/spec/controllers/webui/project_controller_spec.rb
+++ src/api/spec/controllers/webui/project_controller_spec.rb
@@ -1659,57 +1659,106 @@ RSpec.describe Webui::ProjectController, vcr: true do
     end
   end
 
-  describe '#status' do
-    let!(:package) { create(:package, project: apache_project, name: 'mod_rewrite') }
+  describe 'GET #status' do
+    let(:params) { { project: project.name } }
 
     before do
       get :status, params: params
     end
 
     context 'no params set' do
-      let(:params) { { project: apache_project.name } }
+      # NOTE: These project names need to be different for each context because otherwise the
+      # test backend will fail to cleanup the backend packages which causes failures in this spec
+      let!(:project) do
+        create(:project, name: 'Apache22', title: 'Apache WebServer', description: 'The best webserver ever.')
+      end
+      let!(:package) do
+        create(:package, project: project, name: 'mod_rewrite3', title: 'mod_rewrite', description: 'url rewrite module')
+      end
 
       it_behaves_like 'a project status controller'
     end
 
     context 'param format=json set' do
-      let(:params) { { project: apache_project.name, format: 'json' } }
+      let!(:project) do
+        create(:project, name: 'Apache3', title: 'Apache WebServer', description: 'The best webserver ever.')
+      end
+      let!(:package) do
+        create(:package, project: project, name: 'mod_rewrite3', title: 'mod_rewrite', description: 'url rewrite module')
+      end
+      let(:params) { { project: project.name, format: 'json' } }
 
       it_behaves_like 'a project status controller'
     end
 
     context 'param filter_devel is set' do
-      let(:params) { { project: apache_project.name, filter_devel: 'No Project' } }
+      let!(:project) do
+        create(:project, name: 'Apache4', title: 'Apache WebServer', description: 'The best webserver ever.')
+      end
+      let!(:package) do
+        create(:package, project: project, name: 'mod_rewrite4', title: 'mod_rewrite', description: 'url rewrite module')
+      end
+      let(:params) { { project: project.name, filter_devel: 'No Project' } }
 
       it { expect(assigns[:filter]).to eq('_none_') }
     end
 
     context 'param ignore_pending is set' do
-      let(:params) { { project: apache_project.name, ignore_pending: true } }
+      let!(:project) do
+        create(:project, name: 'Apache5', title: 'Apache WebServer', description: 'The best webserver ever.')
+      end
+      let!(:package) do
+        create(:package, project: project, name: 'mod_rewrite5', title: 'mod_rewrite', description: 'url rewrite module')
+      end
+      let(:params) { { project: project.name, ignore_pending: true } }
 
       it { expect(assigns[:ignore_pending]).to be_truthy }
     end
 
     context 'param limit_to_fails is set' do
-      let(:params) { { project: apache_project.name, limit_to_fails: 'false' } }
+      let!(:project) do
+        create(:project, name: 'Apache6', title: 'Apache WebServer', description: 'The best webserver ever.')
+      end
+      let!(:package) do
+        create(:package, project: project, name: 'mod_rewrite6', title: 'mod_rewrite', description: 'url rewrite module')
+      end
+      let(:params) { { project: project.name, limit_to_fails: 'false' } }
 
       it { expect(assigns[:limit_to_fails]).to be_falsey }
     end
 
     context 'param limit_to_old is set' do
-      let(:params) { { project: apache_project.name, limit_to_old: 'true' } }
+      let!(:project) do
+        create(:project, name: 'Apache7', title: 'Apache WebServer', description: 'The best webserver ever.')
+      end
+      let!(:package) do
+        create(:package, project: project, name: 'mod_rewrite7', title: 'mod_rewrite', description: 'url rewrite module')
+      end
+      let(:params) { { project: project.name, limit_to_old: 'true' } }
 
       it { expect(assigns[:limit_to_old]).to be_truthy }
     end
 
     context 'param include_versions is set' do
-      let(:params) { { project: apache_project.name, include_versions: 'true' } }
+      let!(:project) do
+        create(:project, name: 'Apache8', title: 'Apache WebServer', description: 'The best webserver ever.')
+      end
+      let!(:package) do
+        create(:package, project: project, name: 'mod_rewrite8', title: 'mod_rewrite', description: 'url rewrite module')
+      end
+      let(:params) { { project: project.name, include_versions: 'true' } }
 
       it { expect(assigns[:include_versions]).to be_truthy }
     end
 
     context 'param filter_for_user is set' do
-      let(:params) { { project: apache_project.name, filter_for_user: admin_user.login } }
+      let!(:project) do
+        create(:project, name: 'Apache9', title: 'Apache WebServer', description: 'The best webserver ever.')
+      end
+      let!(:package) do
+        create(:package, project: project, name: 'mod_rewrite9', title: 'mod_rewrite', description: 'url rewrite module')
+      end
+      let(:params) { { project: project.name, filter_for_user: admin_user.login } }
 
       it { expect(assigns[:filter_for_user]).to eq(admin_user.login) }
     end
