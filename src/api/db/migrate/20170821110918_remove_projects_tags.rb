cfa1a5c02e959ea62f00c3b3b096ce9508898bf5
Ana María Martínez Gómez <ammartinez@suse.de>
2017-08-24 10:32:08 +0200

    [frontend] Fix drop tags rollback
    Tags where dropped in
    https://github.com/openSUSE/open-build-service/pull/3644
    
    The problem is that from Rails 5.1 the default primary keys are
    BIGINT(20). That means that the references is trying to use BIGINT(20)
    while the old tables use INT(11). So we have to take this into account,
    otherwise the rollback will fail.
    
    I also removed some duplicated indexes, as over some field we were
    creating an index and an unique index.
    
    Mob programmed by @bgeuken and @Ana06.

--- src/api/db/migrate/20170821110918_remove_projects_tags.rb
+++ src/api/db/migrate/20170821110918_remove_projects_tags.rb
@@ -1,16 +1,8 @@
 class RemoveProjectsTags < ActiveRecord::Migration[5.1]
   def change
-    reversible do |dir|
-      dir.down do
-        add_foreign_key "db_projects_tags", "projects", column: "db_project_id", name: "db_projects_tags_ibfk_1"
-        add_foreign_key "db_projects_tags", "tags", name: "db_projects_tags_ibfk_2"
-      end
-    end
     drop_table "db_projects_tags", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-      t.integer "db_project_id", null: false
-      t.integer "tag_id",        null: false
-      t.index ["db_project_id", "tag_id"], name: "projects_tags_all_index", unique: true, using: :btree
-      t.index ["tag_id"], name: "tag_id", using: :btree
+      t.references :db_project, type: :integer, index: { unique: true }, foreign_key: { to_table: :projects }
+      t.references :tag, index: { unique: true }, foreign_key: true
     end
   end
 end
