3a95bf0d318e61b99b7a750b034844e821001ee5
Henne Vogelsang <hvogel@opensuse.org>
2017-08-30 11:41:37 +0200

    [dist] Add Event stats

--- src/api/script/delayed_job_stats.rb
+++ src/api/script/delayed_job_stats.rb
@@ -1,3 +1,16 @@
+puts "-----------EVENT-STATS------------"
+event_classes = [Event::Package, Event::Project]
+event_types = event_classes.flat_map(&:descendants).map(&:name)
+oldest_date = 10.days.ago
+project_logged = Event::Base.where(project_logged: false, eventtype: event_types).where(["created_at > ?", oldest_date]).count
+mails_sent = Event::Base.where(mails_sent: false).count
+undone_jobs = Event::Base.where.not(undone_jobs: 0).count
+puts "There are currently #{project_logged + mails_sent + undone_jobs} Events to be processed"
+puts "---------Events by type-----------"
+puts "ProjectLogs to create".ljust(30) + project_logged
+puts "Mails to send".ljust(30) + mails_sent
+puts "Backend jobs to do".ljust(30) + undone_jobs
+
 puts "-------------DJ-STATS-------------"
 puts "There are currently #{Delayed::Job.count} jobs in the queues"
 puts "-----------Jobs by type-----------"
@@ -11,4 +24,3 @@ queues.each do |dj_queue|
   job_count = Delayed::Job.where(queue: dj_queue).count
   puts "#{dj_queue.to_s.ljust(30)} #{job_count}" if job_count > 0
 end
-
