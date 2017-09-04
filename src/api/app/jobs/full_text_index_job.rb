701ca068511fa50ae8c6c8ecdba3c9209c1cddb2
Eduardo Navarro <enavarro@suse.com>
2017-09-04 13:23:30 +0200

    [dist] Sphinx is started in obsapidelayed script
    Add starting of sphinx in the start section of obsapidelayed.
    Remove starting sphinx from the reindex task.
    Reorder tasks in clean-restart: reindex sphinx after sphinx is started.

--- src/api/app/jobs/full_text_index_job.rb
+++ src/api/app/jobs/full_text_index_job.rb
@@ -9,14 +9,6 @@ class FullTextIndexJob < ApplicationJob
       # Use the RakeInterface provided by ThinkingSphinx
       interface = ThinkingSphinx::RakeInterface.new
 
-      begin
-        interface.daemon.start
-      rescue ThinkingSphinx::SphinxAlreadyRunning, RuntimeError => e
-        # Most likely, this means that searchd is already running.
-        # Nothing to worry about
-        Rails.logger.info "Handled exception: #{e.message}"
-      end
-
       interface.sql.index
     end
   end
