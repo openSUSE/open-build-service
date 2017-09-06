aa391ff1d57703ae57efc35cec7e10ac1183e6be
Evan Rolfe <evanrolfe@gmail.com>
2017-09-06 14:56:05 +0100

    [webui] Make "source change" a link in job history.

--- src/api/app/models/local_job_history.rb
+++ src/api/app/models/local_job_history.rb
@@ -1,4 +1,16 @@
 class LocalJobHistory
   include ActiveModel::Model
-  attr_accessor :revision, :build_counter, :worker_id, :host_arch, :reason, :ready_time, :start_time, :end_time, :total_time, :code, :srcmd5
+  attr_accessor :revision,
+                :build_counter,
+                :worker_id,
+                :host_arch,
+                :reason,
+                :ready_time,
+                :start_time,
+                :end_time,
+                :total_time,
+                :code,
+                :srcmd5,
+                :verifymd5,
+                :prev_verifymd5
 end
