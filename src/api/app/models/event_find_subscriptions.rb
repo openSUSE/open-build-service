3267262f21d49df004a0efa0db075288b6636be4
Henne Vogelsang <hvogel@opensuse.org>
2017-09-21 11:43:13 +0200

    [frontend] Document EventFindSubscriptions
    Add some comments, use speaking variables and move code around for clarity.

--- src/api/app/models/event_find_subscriptions.rb
+++ src/api/app/models/event_find_subscriptions.rb
@@ -8,12 +8,12 @@ class EventFindSubscriptions
     @payload = @event.payload
     @subscriptions = EventSubscription.where(eventtype: @event.class.classnames)
 
-    # Get the defaults
+    # Get the defaults created by the admin via Webui::SubscriptionsController
     @toconsider = @subscriptions.where('user_id is null AND group_id is null').to_a
     # Create defaults for receiver_roles of this Event
-    @event.class.receiver_roles.each do |r|
-      unless receiver_role_set(r)
-        @toconsider << EventSubscription.new(eventtype: @event.class.name, receiver_role: r, channel: 'disabled')
+    @event.class.receiver_roles.each do |receiver_role|
+      unless receiver_role_set(receiver_role)
+        @toconsider << EventSubscription.new(eventtype: @event.class.name, receiver_role: receiver_role, channel: 'disabled')
       end
     end
 
@@ -25,49 +25,103 @@ class EventFindSubscriptions
 
   private
 
+  # Expand the EventSubscriptions receiver_role.
+  # Receivers can be many Users and Groups. We need to instantiate EventSubscriptions
+  # for all of them.
   def expand_toconsider
     new_toconsider = []
     @toconsider.each do |subscription|
-      new_toconsider.concat(expand_one_rule(subscription))
+      new_toconsider.concat(expand_subscription(subscription))
     end
     @toconsider = new_toconsider
   end
 
-  def expand_one_rule(r)
-    receivers = @event.send("#{r.receiver_role}s")
+  def expand_subscription(subscription_to_expand)
+    # Fetch all User/Groups from the Event that match the receiver_role of
+    # the EventSubscription
+    receivers = @event.send("#{subscription_to_expand.receiver_role}s")
 
-    # fetch database settings
-    user_ids = receivers.select { |rcv| rcv.kind_of? User }.map(&:id)
-    groups = receivers.select { |rcv| rcv.kind_of? Group }
+    # Fetch User ids
+    user_ids = receivers.select { |reciver| reciver.kind_of? User }.map(&:id)
+
+    # Fetch Group ids
+    groups = receivers.select { |reciver| reciver.kind_of? Group }
     group_ids = []
     groups.each do |group|
+      # If the group has an email set we'll consider that one
       if group.email
         group_ids << group.id
       else
-        # it has not, so write to all users individually
-        group.users.each do |u|
-          next unless user_subscribed_to_group_email?(group, u)
-          user_ids << u.id
+        # if the Group has no email set we consider all users of it individually
+        group.users.each do |user|
+          # of course only when the User is "subscribed" to the group
+          next unless user_subscribed_to_group_email?(group, user)
+          user_ids << user.id
         end
       end
     end
 
     table = EventSubscription.arel_table
 
-    rel = EventSubscription.where(eventtype: r.eventtype, receiver_role: r.receiver_role)
-    ret = rel.where(table[:user_id].in(user_ids).or(table[:group_id].in(group_ids))).to_a
+    # First find all the subscriptions that are in the database for Users/Groups
+    rel = EventSubscription.where(eventtype: subscription_to_expand.eventtype, receiver_role: subscription_to_expand.receiver_role)
+    subscriptions = rel.where(table[:user_id].in(user_ids).or(table[:group_id].in(group_ids))).to_a
 
-    receivers.each do |ug|
-      # add a default
-      nes = EventSubscription.new(eventtype: r.eventtype, receiver_role: r.receiver_role, channel: r.channel)
-      if ug.kind_of? User
-        nes.user = ug
+    # Then instantiate subscriptions for all Users/Groups in memory
+    receivers.each do |receiver|
+      # copy some settings from the original subscription
+      new_subscription = EventSubscription.new(eventtype: subscription_to_expand.eventtype,
+                                               receiver_role: subscription_to_expand.receiver_role,
+                                               channel: subscription_to_expand.channel)
+      if receiver.kind_of? User
+        new_subscription.user = receiver
       else
-        nes.group = ug
+        new_subscription.group = receiver
       end
-      ret << nes
+      subscriptions << new_subscription
     end
-    ret
+    # Return all EventSubscription we need to consider
+    subscriptions
+  end
+
+  # Filter out EventSubscriptions that make no sense or that have an EventSubscription
+  # with higher priority
+  def filter_toconsider
+    subscribers_and_subscriptions = Hash.new
+
+    # Filter out subscriptions without an email. no need to consider it if we
+    # can't send mail to anyway...
+    @toconsider.each do |subscription|
+      next if subscription.subscriber.email.blank?
+      subscribers_and_subscriptions[subscription.subscriber] ||= []
+      subscribers_and_subscriptions[subscription.subscriber] << subscription
+    end
+
+    # Find the most important subscription.
+    subscriptions_to_receive = []
+    subscribers_and_subscriptions.each do |_subscriber, subscriptions|
+      priority_subscription = sort_subscriptions_by_priority(subscriptions).first
+
+      if priority_subscription.enabled?
+        subscriptions_to_receive << priority_subscription
+      end
+    end
+
+    # Filter out subscriptions for the User that has caused the Event. They know
+    # what they did, no need to send mail about it...
+    subscriptions_to_receive.reject! do |subscription|
+      subscription.subscriber == @event.originator
+    end
+
+    subscriptions_to_receive
+  end
+
+  # Compare two EventSubscription by priority (high to low):
+  # 1. EventSubscriptions the admin/Users explicitely have set in the database over
+  #    those we have instantiated in memory for all the receiver_roles of the Event
+  # 2. EventSubscriptions that are enabled over those that are disabled
+  def sort_subscriptions_by_priority(subscriptions)
+    subscriptions.sort { |x, y| compare_two_subscriptions(x, y) }
   end
 
   def compare_two_subscriptions(x, y)
@@ -87,37 +141,11 @@ class EventFindSubscriptions
     -1
   end
 
-  def sort_subscriptions_by_priority(subscriptions)
-    subscriptions.sort { |x, y| compare_two_subscriptions(x, y) }
-  end
-
+  # FIXME: The email boolean is a 'feature' that you can only access by manually
+  #        changing the data of GroupUser. Either we'll do an interface for it
+  #        or settle on a default...
   def user_subscribed_to_group_email?(group, user)
     GroupsUser.find_by(group: group, user: user).email
-  end
-
-  def filter_toconsider
-    subscribers_and_subscriptions = Hash.new
-
-    @toconsider.each do |subscription|
-      next if subscription.subscriber.email.blank?
-      subscribers_and_subscriptions[subscription.subscriber] ||= []
-      subscribers_and_subscriptions[subscription.subscriber] << subscription
-    end
-
-    subscriptions_to_receive = []
-    subscribers_and_subscriptions.each do |_subscriber, subscriptions|
-      priority_subscription = sort_subscriptions_by_priority(subscriptions).first
-
-      if priority_subscription.enabled?
-        subscriptions_to_receive << priority_subscription
-      end
-    end
-
-    subscriptions_to_receive.reject! do |subscription|
-      subscription.subscriber == @event.originator
-    end
-
-    subscriptions_to_receive
   end
 
   def receiver_role_set(role)
