80df6d88dd1c978bb7470c098cf19abba284d310
Moises Deniz Aleman <mdeniz@suse.com>
2017-09-24 01:00:32 +0200

    [api] Refactor Backend::Api::Search
    ...to use Backend::Api::Connection calls

--- src/api/lib/backend/api/search.rb
+++ src/api/lib/backend/api/search.rb
@@ -2,10 +2,12 @@
 module Backend
   module Api
     class Search
+      extend Backend::ConnectionHelper
+
       # Performs a search of the binary in a project list
       def self.binary(projects, name)
         project_list = projects.map { |project| "@project='#{CGI.escape(project.name)}'" }.join('+or+')
-        Backend::Connection.post("/search/published/binary/id?match=(@name='#{CGI.escape(name)}'+and+(#{project_list}))").body
+        post("/search/published/binary/id?match=(@name='#{CGI.escape(name)}'+and+(#{project_list}))")
       end
     end
   end
