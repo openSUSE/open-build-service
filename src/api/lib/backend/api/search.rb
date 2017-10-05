b9e0a9f10037f697d07473b32a930c0ab4b134b3
Moises Deniz Aleman <mdeniz@suse.com>
2017-10-05 18:41:21 +0200

    [doc] Add documentation for Backend::Api::Search class

--- src/api/lib/backend/api/search.rb
+++ src/api/lib/backend/api/search.rb
@@ -1,10 +1,13 @@
-# API for searching in the backend
 module Backend
   module Api
+    # Class that connect to endpoints related to the search
     class Search
       extend Backend::ConnectionHelper
 
       # Performs a search of the binary in a project list
+      # @param projects [Array of Projects] List of projects where to perform the search in.
+      # @param name [String] Name of the binary to look for.
+      # @return [String] XML  with the binaries collection.
       def self.binary(projects, name)
         project_list = projects.map { |project| "@project='#{CGI.escape(project.name)}'" }.join('+or+')
         post("/search/published/binary/id?match=(@name='#{CGI.escape(name)}'+and+(#{project_list}))")
