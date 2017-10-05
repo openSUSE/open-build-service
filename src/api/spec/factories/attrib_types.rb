b8f81f8fac57ecf3dcbddc85e785586c852aaca7
Christian Bruckmayer <cbruckmayer@suse.com>
2017-10-05 10:58:43 +0200

    [ci] Simplify attrib factories

--- src/api/spec/factories/attrib_types.rb
+++ src/api/spec/factories/attrib_types.rb
@@ -2,5 +2,11 @@ FactoryGirl.define do
   factory :attrib_type do
     sequence(:name) { |n| "attribute_factory_#{n}" }
     attrib_namespace
+
+    factory :attrib_type_with_default_value do
+      after(:create) do |attrib_type, _evaluator|
+        create(:attrib_default_value, attrib_type: attrib_type)
+      end
+    end
   end
 end
