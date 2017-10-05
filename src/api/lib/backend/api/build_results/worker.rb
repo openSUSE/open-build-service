54db2a03cb6cf1e6bc90ee6bec1ba8278e53aae0
Moises Deniz Aleman <mdeniz@suse.com>
2017-10-05 19:06:07 +0200

    [doc] Add documentation for Backend::Api::BuildResults::Worker

--- src/api/lib/backend/api/build_results/worker.rb
+++ src/api/lib/backend/api/build_results/worker.rb
@@ -1,11 +1,12 @@
-# API for accessing to the backend
 module Backend
   module Api
     module BuildResults
+      # Class that connect to endpoints related to the workers
       class Worker
         extend Backend::ConnectionHelper
 
         # Returns the worker status
+        # @return [String] XML with the status of the workers.
         def self.status
           get('/build/_workerstatus')
         end
