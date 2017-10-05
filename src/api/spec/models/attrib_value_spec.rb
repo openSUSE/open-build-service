80041afe4545b244d15b43da15c4e12c2f2bfb6a
Christian Bruckmayer <cbruckmayer@suse.com>
2017-10-05 11:46:19 +0200

    [ci] Refactor attrib value spec

--- src/api/spec/models/attrib_value_spec.rb
+++ src/api/spec/models/attrib_value_spec.rb
@@ -2,16 +2,23 @@ require 'rails_helper'
 
 RSpec.describe AttribValue, type: :model do
   describe '#to_s' do
-    let(:attrib_value) { create(:attrib_value, default_value: 'this is my default value') }
+    let(:attrib) { create(:attrib_with_default_value) }
 
     context 'without setting a value' do
+      let(:attrib_value) { create(:attrib_value, attrib: attrib) }
+      let(:default_value) { attrib.attrib_type.default_values.first.value }
+
+      before do
+        attrib_value.reload
+      end
+
       it 'uses the default value' do
-        expect(attrib_value.to_s).to eq('this is my default value')
+        expect(attrib_value.to_s).to eq(default_value)
       end
     end
 
     context 'with a value' do
-      let(:attrib_value) { create(:attrib_value, value: 'value that is set') }
+      let(:attrib_value) { create(:attrib_value, attrib: attrib, value: 'value that is set') }
 
       it 'returns the value which is set' do
         expect(attrib_value.to_s).to eq('value that is set')
