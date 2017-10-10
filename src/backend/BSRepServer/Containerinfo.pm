0e13eda9ee2eed1f767efdf815a6a11062708587
Frank Schreiner <schreiner@suse.de>
2017-10-10 13:52:13 +0000

    [backend] upload docker image into registry
    This patch adds new funcionality to upload docker images to a docker registry
    via skopeo.

--- src/backend/BSRepServer/Containerinfo.pm
+++ src/backend/BSRepServer/Containerinfo.pm
@@ -48,6 +48,51 @@ BSRepServer::Containerinfo
 
 sub containerinfo2obsbinlnk {
   my ($dir, $containerinfo, $packid) = @_;
+  my $d= readcontainerinfo($dir, $containerinfo);
+  return unless $d;
+  my $name = $d->{name};
+  my $lnk = {};
+  $lnk->{'name'} = "container:$name";
+  $lnk->{'version'} = defined($d->{'version'}) ? $d->{'version'} : '0'; 
+  $lnk->{'arch'} = defined($d->{'arch'}) ? $d->{'arch'} : 'noarch';
+  # need to have a source so that it goes into the :full tree
+  $lnk->{'source'} = $lnk->{'name'};
+  # add self-provides
+  push @{$lnk->{'provides'}}, "$lnk->{'name'} = $lnk->{'version'}";
+  for my $tag (@{$d->{tags}}) {
+    push @{$lnk->{'provides'}}, "container:$tag" unless "container:$tag" eq $lnk->{'name'};
+  }
+  eval {
+    BSVerify::verify_nevraquery($lnk);
+  };
+  return undef if $@;
+  my $annotation = {};
+  $annotation->{'repo'} = $d->{'repos'} if $d->{'repos'};
+  if (%$annotation) {
+    eval { $lnk->{'annotation'} = BSUtil::toxml($annotation, $BSXML::binannotation) };
+    warn($@) if $@;
+  }
+  local *F;
+  return undef unless open(F, '<', "$dir/$d->{file}");
+  my $ctx = Digest::MD5->new;
+  $ctx->addfile(*F);
+  close F;
+  $lnk->{'hdrmd5'} = $ctx->hexdigest();
+  $lnk->{'path'} = "../$packid/$d->{file}";
+  return $lnk;
+}
+
+=head2  readcontainerinfo - read data from containerinfo file and verify data
+
+ input: $dir - directory of the built container
+        $containerinfo - containerinfo filename in $dir
+
+ output: HashRef containing data from containerinfo or undef in case of an error
+
+=cut
+
+sub readcontainerinfo {
+  my ($dir, $containerinfo) = @_;
   return undef unless -e "$dir/$containerinfo";
   return undef unless (-s _) < 100000;
   my $m = readstr("$dir/$containerinfo");
@@ -67,43 +112,16 @@ sub containerinfo2obsbinlnk {
     $name = $tags->[0];
     $name =~ s/[:\/]/-/g;
   }
+  $d->{name} = $name;
   my $file = $d->{'file'};
-  $file = undef unless defined($file) && ref($file) eq '';
+  $d->{'file'} = $file = undef unless defined($file) && ref($file) eq '';
   return undef unless defined($name) && defined($file);
   eval {
     BSVerify::verify_simple($file);
     BSVerify::verify_filename($file);
   };
   return undef if $@;
-  my $lnk = {};
-  $lnk->{'name'} = "container:$name";
-  $lnk->{'version'} = defined($d->{'version'}) ? $d->{'version'} : '0'; 
-  $lnk->{'arch'} = defined($d->{'arch'}) ? $d->{'arch'} : 'noarch';
-  # need to have a source so that it goes into the :full tree
-  $lnk->{'source'} = $lnk->{'name'};
-  # add self-provides
-  push @{$lnk->{'provides'}}, "$lnk->{'name'} = $lnk->{'version'}";
-  for my $tag (@$tags) {
-    push @{$lnk->{'provides'}}, "container:$tag" unless "container:$tag" eq $lnk->{'name'};
-  }
-  eval {
-    BSVerify::verify_nevraquery($lnk);
-  };
-  return undef if $@;
-  my $annotation = {};
-  $annotation->{'repo'} = $d->{'repos'} if $d->{'repos'};
-  if (%$annotation) {
-    eval { $lnk->{'annotation'} = BSUtil::toxml($annotation, $BSXML::binannotation) };
-    warn($@) if $@;
-  }
-  local *F;
-  return undef unless open(F, '<', "$dir/$file");
-  my $ctx = Digest::MD5->new;
-  $ctx->addfile(*F);
-  close F;
-  $lnk->{'hdrmd5'} = $ctx->hexdigest();
-  $lnk->{'path'} = "../$packid/$file";
-  return $lnk;
+  return $d;
 }
 
 1;
