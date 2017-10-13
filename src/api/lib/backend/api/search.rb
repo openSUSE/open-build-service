9d33a4b730f0df06626bfff852870d5d7c76aa94
Moises Deniz Aleman <mdeniz@suse.com>
2017-10-13 16:30:35 +0200

    [api][webui] Move backend calls of UpdatePackageMetaJob

--- src/api/lib/backend/api/search.rb
+++ src/api/lib/backend/api/search.rb
@@ -10,6 +10,12 @@ module Backend
         project_list = project_names.map { |project_name| "@project='#{CGI.escape(project_name)}'" }.join('+or+')
         post("/search/published/binary/id?match=(@name='#{CGI.escape(binary_name)}'+and+(#{project_list}))")
       end
+
+      # Performs a search of packages with a link
+      def self.packages_with_link(package_names)
+        packages_list = package_names.map { |name| "linkinfo/@package='#{CGI.escape(name)}'" }.join("+or+")
+        get("/search/package/id?match=(#{packages_list})")
+      end
     end
   end
 end
