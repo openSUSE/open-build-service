07db0f442be09e95b4fb476ba8d90a3e3bab3742
Moises Deniz Aleman <mdeniz@suse.com>
2017-10-18 16:07:37 +0200

    [webui] Fix breadcrumbs of Kiwi Editor
    Now it shows the package link

--- src/api/app/helpers/webui/kiwi/image_helper.rb
+++ src/api/app/helpers/webui/kiwi/image_helper.rb
@@ -3,10 +3,10 @@ module Webui::Kiwi::ImageHelper
 
   def kiwi_image_breadcrumb(kiwi_image, *args)
     @project = kiwi_image.package.try(:project)
+    @package = kiwi_image.package
     return unless @project
 
-    args.insert(0, link_to_if(params['action'] != 'show', 'Image',
-                              kiwi_image_path(id: kiwi_image)))
+    args.insert(0, link_to(@package, package_show_path(project: @project, package: @package)))
     project_bread_crumb( *args )
   end
 end
