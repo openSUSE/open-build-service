bd34552b62bce957d83a5602bb3ffc4feda85e66
Moises Deniz Aleman <mdeniz@suse.com>
2017-10-19 13:07:49 +0200

    [ci] Add feature tests for workflow changes
    in the Kiwi Editor

--- src/api/spec/features/webui/image_templates_spec.rb
+++ src/api/spec/features/webui/image_templates_spec.rb
@@ -11,6 +11,8 @@ RSpec.feature "ImageTemplates", type: :feature, js: true do
     let!(:package1) { create(:package_with_file, project: project, name: "first_package",  title: "a") }
     let!(:package2) { create(:package_with_file, project: project, name: "second_package", title: "c") }
     let!(:package3) { create(:package_with_file, project: project, name: "third_package",  title: "b") }
+    let!(:kiwi_image) { create(:kiwi_image_with_package, with_kiwi_file: true, project: project) }
+    let(:kiwi_package) { kiwi_image.package}
     let!(:attrib) { create(:template_attrib, project: project) }
 
     scenario "branch image template" do
@@ -25,6 +27,7 @@ RSpec.feature "ImageTemplates", type: :feature, js: true do
       expect(find("input[data-package='#{package1}']", visible: false)['checked']).to be true
       expect(find("input[data-package='#{package3}']", visible: false)['checked']).to be false
       expect(find("input[data-package='#{package2}']", visible: false)['checked']).to be false
+      expect(find("input[data-package='#{kiwi_package}']", visible: false)['checked']).to be false
 
       expect(page).to have_field('target_package', with: package1)
       find("input[data-package='#{package2}']", visible: false).trigger(:click)
@@ -35,6 +38,30 @@ RSpec.feature "ImageTemplates", type: :feature, js: true do
       find('#package_tabs')
       expect(page).to have_text("Successfully branched package")
       expect(page).to have_text("home:tom:branches:my_project > custom_name")
+    end
+
+    scenario "branch Kiwi image template" do
+      visit image_templates_path
+      expect(page).to have_css("input.create_appliance[disabled]")
+
+      login(user)
+      visit root_path
+      find('.proceed_text > a', text: "New Image").click
+
+      expect(page).to have_text(package1.title)
+      expect(find("input[data-package='#{package1}']", visible: false)['checked']).to be true
+      expect(find("input[data-package='#{package3}']", visible: false)['checked']).to be false
+      expect(find("input[data-package='#{package2}']", visible: false)['checked']).to be false
+      expect(find("input[data-package='#{kiwi_package}']", visible: false)['checked']).to be false
+
+      expect(page).to have_field('target_package', with: package1)
+      find("input[data-package='#{kiwi_package}']", visible: false).trigger(:click)
+      expect(page).to have_field('target_package', with: kiwi_package)
+      fill_in 'target_package', with: "package_with_kiwi_image"
+
+      click_button("Create appliance")
+      find('#kiwi-image-update-form')
+      expect(page).to have_text("home:tom:branches:my_project > package_with_kiwi_image")
     end
   end
 
