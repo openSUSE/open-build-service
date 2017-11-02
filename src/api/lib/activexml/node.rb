f80e77d2d515fa2962ea86e946e9ca7baa8e06d7
Eduardo Navarro <enavarro@suse.com>
2017-11-02 14:59:39 +0100

    [webui] Replace tabs with spaces

--- src/api/lib/activexml/node.rb
+++ src/api/lib/activexml/node.rb
@@ -118,9 +118,9 @@ module ActiveXML
               obj = new( objdata )
               [objdata, params, obj.to_hash]
             end
-	    if fromcache
-	      logger.debug "returning #{args.inspect} from rails cache #{cache_key}"
-	    end
+            if fromcache
+              logger.debug "returning #{args.inspect} from rails cache #{cache_key}"
+            end
           else
             objdata, params = transport.find( self, *args)
           end
@@ -153,9 +153,9 @@ module ActiveXML
           free_args[free_args.length - 1] = free_args.last.dup
           free_args.last.delete :expires_in
         end
-	free_args = prepare_args( free_args )
+        free_args = prepare_args( free_args )
         key = calc_key( free_args )
-	logger.debug "free_cache #{free_args.inspect} #{key}"
+        logger.debug "free_cache #{free_args.inspect} #{key}"
         Rails.cache.delete( key )
       end
     end
