639e946804b6a3dd3974cf2b6232f289b826a862
Eduardo Navarro <enavarro@suse.com>
2017-11-12 16:16:04 +0100

    [webui] Fix sorting in job history table
    - Disable sorting in first (Revision) and last column.
    - Enable sorting by default with "Time" column, descending.
    - Fix sorting by "Time" column.
    - Also fix sorting in "Build time" column.
    - Prevent searching for the introduced hidden columns with no formatted
    data.
    
    This should fix #3986.

--- src/api/app/assets/javascripts/webui/application/package.js
+++ src/api/app/assets/javascripts/webui/application/package.js
@@ -155,6 +155,12 @@ $( document ).ready(function() {
   });
 
   $('#jobhistory-table').dataTable({
-    order: [[0, 'desc']]
+    columnDefs: [
+      { orderable: false, targets: [0, 8] },
+      { visible: false, searchable: false, targets: [1, 5] },
+      { orderData: 1, targets: 2 },
+      { orderData: 5, targets: 6 },
+    ],
+    order: [[2, 'desc']]
   });
 });
