c42765f505b6ce9d28d0dddf337150a68dd38a7e
Eduardo Navarro <enavarro@suse.com>
2017-11-29 12:48:00 +0100

    [dist] Use docker-bootstrap.sh for common tasks
    - Place in the same file commands used for installing packages for the
    backend, frontend and memcached, so they can be used in more docker
    containers.

---
+++ contrib/docker-bootstrap.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+for option in "$@"
+do
+  case "${option}" in
+  frontend)
+    zypper -n install --no-recommends --replacefiles \
+      sphinx \
+      phantomjs \
+      nodejs6 npm6 \
+      mariadb-client \
+      git-core \
+      ruby2.4-devel cyrus-sasl-devel openldap2-devel libxml2-devel zlib-devel libxslt-devel \
+      perl-XML-Parser \
+      ruby2.4-rubygem-mysql2 \
+      ruby2.4-rubygem-bundler ruby2.4-rubygem-thor-0_19 ruby2.4-rubygem-foreman
+    ;;
+
+  backend)
+    zypper -n install --no-recommends --replacefiles \
+      inst-source-utils \
+      obs-server obs-signd \
+      obs-service-download_src_package obs-service-download_files \
+      obs-service-download_url \
+      obs-service-format_spec_file obs-service-kiwi_import \
+      perl-Devel-Cover \
+      osc
+    ;;
+
+  memcached)
+    zypper -n install --no-recommends --replacefiles memcached
+    ;;
+
+  *)
+    echo "Error: possible options are: frontend|backend|memcached"
+    exit
+    ;;
+
+  esac
+done
