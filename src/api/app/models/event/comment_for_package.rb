c46bb892aa994acb8e5809a454ce08948081d9a2
Ana María Martínez Gómez <ammartinez@suse.de>
2017-12-01 15:26:38 +0100

    [api] Do not use shortcut for Comment clases
    Using `Event::Class` oblies us to include `CommentEvent` with an
    explicit namespace qualifier (`Event::CommentEvent`), because Ruby will
    not look inside `Event` for this name.

--- src/api/app/models/event/comment_for_package.rb
+++ src/api/app/models/event/comment_for_package.rb
@@ -1,15 +1,17 @@
-class Event::CommentForPackage < ::Event::Package
-  include CommentEvent
-  receiver_roles :maintainer, :watcher
-  after_create_commit :send_to_bus
+module Event
+  class CommentForPackage < Package
+    include CommentEvent
+    receiver_roles :maintainer, :watcher
+    after_create_commit :send_to_bus
 
-  def self.message_bus_routing_key
-    "#{Configuration.amqp_namespace}.package.comment"
-  end
+    def self.message_bus_routing_key
+      "#{Configuration.amqp_namespace}.package.comment"
+    end
 
-  self.description = 'New comment for package created'
+    self.description = 'New comment for package created'
 
-  def subject
-    "New comment in package #{payload['project']}/#{payload['package']} by #{User.find(payload['commenter']).login}"
+    def subject
+      "New comment in package #{payload['project']}/#{payload['package']} by #{User.find(payload['commenter']).login}"
+    end
   end
 end
--- src/api/app/models/event/comment_for_project.rb
+++ src/api/app/models/event/comment_for_project.rb
@@ -1,15 +1,17 @@
-class Event::CommentForProject < ::Event::Project
-  include CommentEvent
-  receiver_roles :maintainer, :watcher
-  after_create_commit :send_to_bus
+module Event
+  class CommentForProject < Project
+    include CommentEvent
+    receiver_roles :maintainer, :watcher
+    after_create_commit :send_to_bus
 
-  def self.message_bus_routing_key
-    "#{Configuration.amqp_namespace}.project.comment"
-  end
+    def self.message_bus_routing_key
+      "#{Configuration.amqp_namespace}.project.comment"
+    end
 
-  self.description = 'New comment for project created'
+    self.description = 'New comment for project created'
 
-  def subject
-    "New comment in project #{payload['project']} by #{User.find(payload['commenter']).login}"
+    def subject
+      "New comment in project #{payload['project']} by #{User.find(payload['commenter']).login}"
+    end
   end
 end
