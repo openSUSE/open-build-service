ea0a7d664584000eee1cf6532a8ba6f313e84ea9
Ana María Martínez Gómez <ammartinez@suse.de>
2017-12-01 15:39:17 +0100

    [api] Add schema information for all new classes
    And now that we are following Rails way, they are correct!

--- src/api/app/models/event/update_project.rb
+++ src/api/app/models/event/update_project.rb
@@ -9,3 +9,27 @@ module Event
     end
   end
 end
+
+# == Schema Information
+#
+# Table name: projects
+#
+#  id              :integer          not null, primary key
+#  name            :string(200)      not null, indexed
+#  title           :string(255)
+#  description     :text(65535)
+#  created_at      :datetime
+#  updated_at      :datetime         indexed
+#  remoteurl       :string(255)
+#  remoteproject   :string(255)
+#  develproject_id :integer          indexed
+#  delta           :boolean          default(TRUE), not null
+#  kind            :string(20)       default("standard")
+#  url             :string(255)
+#
+# Indexes
+#
+#  devel_project_id_index  (develproject_id)
+#  projects_name_index     (name) UNIQUE
+#  updated_at_index        (updated_at)
+#
