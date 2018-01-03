71f27119b05dfd9498a5c1485c4edd73f2f6f19f
Andreas Schwab <schwab@suse.de>
2018-01-03 12:30:53 +0100

    [api][backend] Add support for riscv64

---
+++ src/api/db/data/20180131174510_add_riscv64.rb
@@ -0,0 +1,9 @@
+class AddRiscv64 < ActiveRecord::Migration[5.1]
+  def self.up
+    Architecture.where(name: 'riscv64').first_or_create
+  end
+
+  def self.down
+    Architecture.find_by_name('riscv64').destroy
+  end
+end
