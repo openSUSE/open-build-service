039147c17df9d977d742352f1773d0d7b5df17f4
Andreas Schwab <schwab@suse.de>
2018-01-03 12:30:53 +0100

    [api][backend] Add support for riscv64

--- src/api/db/seeds.rb
+++ src/api/db/seeds.rb
@@ -5,7 +5,7 @@ puts 'Seeding architectures table...'
 # but kept for compatibility reasons. armv7hl is in for compatibility (soft/hard).
 ['aarch64', 'aarch64_ilp32', 'armv4l', 'armv5l', 'armv6l', 'armv7l', 'armv5el', 'armv6el', 'armv7el',
  'armv7hl', 'armv8el', 'hppa', 'i586', 'i686', 'ia64', 'k1om', 'local', 'm68k', 'mips', 'mips32',
- 'mips64', 'ppc', 'ppc64', 'ppc64p7', 'ppc64le', 's390', 's390x', 'sparc', 'sparc64', 'sparc64v',
+ 'mips64', 'ppc', 'ppc64', 'ppc64p7', 'ppc64le', 'riscv64', 's390', 's390x', 'sparc', 'sparc64', 'sparc64v',
  'sparcv8', 'sparcv9', 'sparcv9v', 'x86_64'].each do |arch_name|
 
   Architecture.where(name: arch_name).first_or_create
