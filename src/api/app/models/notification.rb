6d37c43f7617518f3c26a4bacbc0a51a02e72018
Eduardo Navarro <enavarro@suse.com>
2018-01-03 18:31:58 +0100

    [webui] Add shorten payload in notifications
    - Add shorten_payload_if_necessary method in Notification model to
    ensure that a payload is not bigger than the field size in database.
    - Modify the notification factory to allow passing parameters to
    initialize.
    - This should fix #4161.
    
    Mobprogrammed with @mdeniz and @DavidKang.

--- src/api/app/models/notification.rb
+++ src/api/app/models/notification.rb
@@ -3,8 +3,29 @@ class Notification < ApplicationRecord
 
   serialize :event_payload, JSON
 
+  def initialize(params = {})
+    super
+    shorten_payload_if_necessary
+  end
+
   def event
     @event ||= event_type.constantize.new(event_payload)
+  end
+
+  private
+
+  def shorten_payload_if_necessary
+    return if event.shortenable_key.nil? # If no shortenable_key is set then we cannot shorten the payload
+
+    # NOTE: ActiveSupport::JSON is used for serializing ActiveRecord models attributes
+    overflow_bytes = ActiveSupport::JSON.encode(event_payload).bytesize - 65535
+
+    return if overflow_bytes <= 0
+
+    # Shorten the payload so it will fit into the database column
+    shortenable_content = event_payload[event.shortenable_key.to_s]
+    new_size = shortenable_content.bytesize - overflow_bytes
+    event_payload[event.shortenable_key.to_s] = shortenable_content.mb_chars.limit(new_size)
   end
 end
 
