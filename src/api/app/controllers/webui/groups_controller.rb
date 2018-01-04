06a9408ae12687be455198af0b6e13105dfc8e61
Björn Geuken <bgeuken@suse.de>
2018-01-04 12:49:53 +0100

    [frontend] DRY GroupsController#edit
    * Use pluck to fetch the needed attributes directly via SQL
    * Use map to simplify setting @members

--- src/api/app/controllers/webui/groups_controller.rb
+++ src/api/app/controllers/webui/groups_controller.rb
@@ -17,11 +17,7 @@ class Webui::GroupsController < Webui::WebuiController
   def edit
     authorize @group, :update?
     @roles = Role.global_roles
-    @members = []
-    @group.users.each do |person|
-      user = { 'name' => person.login }
-      @members << user
-    end
+    @members = @group.users.pluck(:login).map { |login| { 'name' => login } }
   end
 
   def create
