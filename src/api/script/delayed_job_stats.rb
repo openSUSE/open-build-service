0cc7f9ad8f41be92227fbeea8482b1f4a969ed39
Ana María Martínez Gómez <ammartinez@suse.de>
2018-01-09 13:07:58 +0100

    [frontend] Modify delayed_job_stats
    As the _ProjectLogs to create_ doesn't make sense any more.

--- src/api/script/delayed_job_stats.rb
+++ src/api/script/delayed_job_stats.rb
@@ -1,12 +1,8 @@
 puts '-----------EVENT-STATS------------'
-event_types = Event::PROJECT_CLASSES | Event::PACKAGE_CLASSES
-oldest_date = 10.days.ago
-project_logged = Event::Base.where(project_logged: false, eventtype: event_types).where(['created_at > ?', oldest_date]).count
 mails_sent = Event::Base.where(mails_sent: false).count
 undone_jobs = Event::Base.where.not(undone_jobs: 0).count
-puts "There are currently #{project_logged + mails_sent + undone_jobs} Events to be processed"
+puts "There are currently #{mails_sent + undone_jobs} Events to be processed"
 puts '---------Events by type-----------'
-puts 'ProjectLogs to create'.ljust(30) + project_logged.to_s
 puts 'Mails to send'.ljust(30) + mails_sent.to_s
 puts 'Backend jobs to do'.ljust(30) + undone_jobs.to_s
 
