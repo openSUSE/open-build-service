65a0e7bc64dd8ea0c3be6ea6eb989902ae1c154f
Ana María Martínez Gómez <ammartinez@suse.de>
2018-01-09 13:05:15 +0100

    [frontend] Clean Events are now are always logged
    Now events are logged when being created, so we can clean them.

--- src/api/app/jobs/cleanup_events.rb
+++ src/api/app/jobs/cleanup_events.rb
@@ -1,10 +1,7 @@
 class CleanupEvents < ApplicationJob
   def perform
     Event::Base.transaction do
-      Event::Base.where(project_logged: true, mails_sent: true, undone_jobs: 0).lock(true).delete_all
+      Event::Base.where(mails_sent: true, undone_jobs: 0).lock(true).delete_all
     end
-
-    event_types = Event::PROJECT_CLASSES | Event::PACKAGE_CLASSES
-    Event::Base.where(project_logged: false, mails_sent: true, undone_jobs: 0).where.not(eventtype: event_types).delete_all
   end
 end
