5d153c151862bfa5616e95c1f200efb0de379239
Moises Deniz Aleman <mdeniz@suse.com>
2018-01-09 17:29:36 +0100

    [frontend] Remove overwrite of JSON encoding in controllers
    Removed the ActionController::Renderers overwrite, now is set back to
    ActiveSupport:JSON (the default one)

--- src/api/config/initializers/fast_json.rb
+++
@@ -1,9 +0,0 @@
-# the default render json: uses to_json from json gem using the pure variant
-# which has horrible performance - see https://github.com/rails/rails/issues/9212
-require 'yajl'
-
-ActionController::Renderers.add :json do |json, _|
-  json = Yajl::Encoder.encode(json) unless json.is_a?(String)
-  self.content_type ||= Mime[:json]
-  json
-end
