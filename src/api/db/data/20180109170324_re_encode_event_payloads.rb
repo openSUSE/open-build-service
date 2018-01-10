1439fe16c8d41dd9dc8d91f543c1f90f1ffd2c49
Moises Deniz Aleman <mdeniz@suse.com>
2018-01-10 10:04:36 +0100

    [frontend] Refactor shortening of event payload and migrate
    We now shorten the payload to fit into the table column at initializing
    time instead of before_save. Also a migration is added for "converting"
    the event's payload already store in the db with Yajl::Encoder.encode
    (it was like this before).

---
+++ src/api/db/data/20180109170324_re_encode_event_payloads.rb
@@ -0,0 +1,17 @@
+class ReEncodeEventPayloads < ActiveRecord::Migration[5.1]
+  def up
+    # Re-encode the events that creates notifications for using only one encoder (ActiveSupport::JSON)
+    Event::Base.notification_events.each do |event_type|
+      event_type.where(mails_sent: false).find_in_batches do |batch|
+        batch.each do |event|
+          event.set_payload(event.payload, event.payload_keys)
+          event.save!
+        end
+      end
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
