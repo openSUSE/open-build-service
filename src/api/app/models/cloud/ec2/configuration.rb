b12940c7d1271fc753966d9497e0baa1148da09f
Christian Bruckmayer <cbruckmayer@suse.com>
2018-01-11 10:42:46 +0100

    [webui] Make all regions available for ec2 uploader

--- src/api/app/models/cloud/ec2/configuration.rb
+++ src/api/app/models/cloud/ec2/configuration.rb
@@ -1,6 +1,24 @@
 module Cloud
   module Ec2
     class Configuration < ApplicationRecord
+      REGIONS = [
+        ['US East (N. Virginia)', 'us-east-1'],
+        ['US East (Ohio)', 'us-east-2'],
+        ['US West (N. California)', 'us-west-1'],
+        ['US West (Oregon)', 'us-west-2'],
+        ['Canada (Central)', 'ca-central-1'],
+        ['EU (Frankfurt)', 'eu-central-1'],
+        ['EU (Ireland)', 'eu-west-1'],
+        ['EU (London)', 'eu-west-2'],
+        ['EU (Paris)', 'eu-west-3'],
+        ['Asia Pacific (Tokyo)', 'ap-northeast-1'],
+        ['Asia Pacific (Seoul)', 'ap-northeast-2'],
+        ['Asia Pacific (Singapore)', 'ap-southeast-1'],
+        ['Asia Pacific (Sydney)', 'ap-southeast-2'],
+        ['Asia Pacific (Mumbai)', 'ap-south-1'],
+        ['South America (São Paulo)', 'sa-east-1']
+      ].freeze
+
       has_secure_token :external_id
       belongs_to :user, required: true
 
