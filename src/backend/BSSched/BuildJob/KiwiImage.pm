0f91e9c195637df35ff066c495f2344fb2176a2a
Michael Schroeder <mls@suse.de>
2018-01-12 16:42:53 +0100

    [backend] stylistic change for kiwi meta sorting

--- src/backend/BSSched/BuildJob/KiwiImage.pm
+++ src/backend/BSSched/BuildJob/KiwiImage.pm
@@ -265,7 +265,7 @@ sub check {
     return ('blocked', join(', ', @blocked));
   }
   push @new_meta, $cmeta if $cmeta;
-  @new_meta = sort {substr($a, 34) cmp substr($b, 34)} @new_meta;
+  @new_meta = sort {substr($a, 34) cmp substr($b, 34) || $a cmp $b} @new_meta;
   unshift @new_meta, map {"$_->{'srcmd5'}  $_->{'project'}/$_->{'package'}"} @{$info->{'extrasource'} || []};
   my ($state, $data) = BSSched::BuildJob::metacheck($ctx, $packid, $pdata, 'kiwi-image', \@new_meta, [ $bconf, \@edeps, $pool, \%dep2pkg, $cbdep, $cprp, $unorderedrepos ]);
   if ($BSConfig::enable_download_on_demand && $state eq 'scheduled') {
