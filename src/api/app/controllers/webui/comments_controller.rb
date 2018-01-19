96d8742e8001c7a31844506a8e11bf3a39ea2122
Manuel Schnitzer <mschnitzer@suse.com>
2018-01-19 10:43:33 +0100

    [webui] Create comments via ajax requests
    Comments will now be created via Ajax requests. The comment section is
    reloaded every time, when a new comment has been added by the user.
    
    Affects:
    * Requests
    * Packages
    * Projects

--- src/api/app/controllers/webui/comments_controller.rb
+++ src/api/app/controllers/webui/comments_controller.rb
@@ -6,10 +6,14 @@ class Webui::CommentsController < Webui::WebuiController
     comment = @commented.comments.new(permitted_params)
     User.current.comments << comment
 
+    # required for the form construction
+    @comment = Comment.new
+
     respond_to do |format|
       if comment.save
         format.html { redirect_back(fallback_location: root_path, notice: 'Comment was successfully created.') }
         format.json { render json: 'ok' }
+        format.js { render partial: 'webui/comment/show', locals: { commentable: comment.commentable }, status: :ok }
       else
         format.html { redirect_back(fallback_location: root_path, error: "Comment can't be saved: #{comment.errors.full_messages.to_sentence}.") }
         format.json { render json: comment.errors, status: :unprocessable_entity }
