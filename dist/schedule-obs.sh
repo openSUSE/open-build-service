2473628f47e816231161eb47ed776ecd4a9fae00
Eduardo Navarro <enavarro@suse.com>
2018-01-23 10:16:18 +0100

    [ci] Remove apikey and apisecret options
    apikey and apisecret configuration parameters should be in the openQA
    client configuration file (/etc/openqa/client.conf or
    ~/.config/openqa/client.conf).

--- dist/schedule-obs.sh
+++ dist/schedule-obs.sh
@@ -9,7 +9,7 @@ function trigger_run {
   filename=`curl -s $FULL_URL | grep "obs-server.x86_64-.*qcow2" | head -n1 | sed -e 's,.*href=",,; s,".*,,; s,\.mirrorlist,,'`
   ofilename=`cat /tmp/.last.obs_$OBS_VERSION`
   if test "x$ofilename" != "x$filename"; then
-    /usr/share/openqa/script/client isos post --host https://openqa.opensuse.org HDD_1_URL=$FULL_URL$filename DISTRI=obs ARCH=x86_64 VERSION=$OBS_VERSION BUILD=`echo $filename | sed -e 's,obs-server.x86_64-,,; s,Build,,; s,\.qcow2,,'` FLAVOR=Appliance --apikey B21ADBFF1E8C26AD --apisecret BD471AE6BA32781D > /dev/null
+    /usr/share/openqa/script/client isos post --host https://openqa.opensuse.org HDD_1_URL=$FULL_URL$filename DISTRI=obs ARCH=x86_64 VERSION=$OBS_VERSION BUILD=`echo $filename | sed -e 's,obs-server.x86_64-,,; s,Build,,; s,\.qcow2,,'` FLAVOR=Appliance > /dev/null
     echo $filename > /tmp/.last.obs_$OBS_VERSION
   fi
 }
