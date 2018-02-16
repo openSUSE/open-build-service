6232ff0371b9019a8a684f00d3308d776df85874
Michael Schroeder <mls@suse.de>
2018-02-16 13:53:29 +0100

    [backend] support view=info in cloudupload pubkey query

--- src/backend/BSXML.pm
+++ src/backend/BSXML.pm
@@ -1820,17 +1820,20 @@ our $multibuild = [
 	  [ 'flavor' ],
 ];
 
+our $pubkeyinfo = [
+    'pubkey' =>
+	'keyid',
+	'algo',
+	'keysize',
+	'expires',
+	'fingerprint',
+	'_content',
+];
+
 our $keyinfo = [
     'keyinfo' =>
 	'project',
-      [ 'pubkey' =>
-	    'keyid',
-	    'algo',
-	    'keysize',
-	    'expires',
-	    'fingerprint',
-	    '_content',
-      ],
+        $pubkeyinfo,
 	'sslcert',
 ];
 
