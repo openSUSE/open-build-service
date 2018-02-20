328067fb82c2f97e158cd8bb549bb85f4abbba70
Henne Vogelsang <hvogel@opensuse.org>
2018-02-20 16:36:20 +0100

    [frontend] Introduces ProjectLogEntry view
    If you just want to check what happened project wide in the last couple of
    hours to find the cause of some problem. There is already the RSS feed for this
    but it's harder to skim than a nice *cough* web view.

--- src/api/db/data/20180214132015_project_log_entry_user_name.rb
+++ src/api/db/data/20180214132015_project_log_entry_user_name.rb
@@ -2,7 +2,7 @@ class ProjectLogEntryUserName < ActiveRecord::Migration[5.1]
   def up
     ProjectLogEntry.where('event_type like "%comment_for%"').in_batches do |log_entry|
       user = User.find_by(id: log_entry.user_name).try(:login)
-      user ||= '_nobody_'
+      user ||= User::NOBODY_LOGIN
       # rubocop:disable Rails/SkipsModelValidations
       log_entry.update_attribute(user_name: user)
       # rubocop:enable Rails/SkipsModelValidations
@@ -12,7 +12,7 @@ class ProjectLogEntryUserName < ActiveRecord::Migration[5.1]
   def down
     ProjectLogEntry.where('event_type like "%comment_for%"').in_batches do |log_entry|
       user = User.find_by(id: log_entry.user_name).try(:id)
-      user ||= User.find_nobody!.try(:id) 
+      user ||= User.find_nobody!.try(:id)
       # rubocop:disable Rails/SkipsModelValidations
       log_entry.update_attribute(user_name: user)
       # rubocop:enable Rails/SkipsModelValidations
