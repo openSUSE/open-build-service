8e34c61d4a6b3b1b901ce60e2f5567cfcf36309b
Björn Geuken <bgeuken@suse.de>
2018-02-26 08:56:59 +0100

    [frontend] Refactor attribute event handler
    Use JQuery on function to simplify event handler.

--- src/api/app/assets/javascripts/webui/application/attribute.js
+++ src/api/app/assets/javascripts/webui/application/attribute.js
@@ -1,9 +1,5 @@
-$(function() {
-  $('#attrib_attrib_type_id').on(
-    { "change": function() {
-        $("#first-help").hide();
-        $(".attrib-type").hide();
-        $('#' + $(this).val() + '-help').show();
-      }
-    });
-  });
+$(document).on('change', '#attrib_attrib_type_id', function() {
+  $("#first-help").hide();
+  $(".attrib-type").hide();
+  $('#' + $(this).val() + '-help').show();
+});
