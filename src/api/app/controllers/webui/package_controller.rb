a335835808ff7a92ebe1c44af5e10513b2c5adda
Eduardo Navarro <enavarro@suse.com>
2018-03-09 16:51:14 +0100

    [webui] Remove duplicated redirect_to
    Co-authored-by: David Kang <dkang@suse.com>

--- src/api/app/controllers/webui/package_controller.rb
+++ src/api/app/controllers/webui/package_controller.rb
@@ -883,11 +883,11 @@ class Webui::PackageController < Webui::WebuiController
 
     if @package.wipe_binaries(params)
       flash[:notice] = "Triggered wipe binaries for #{@project.name}/#{@package.name} successfully."
-      redirect_to package_binaries_path(project: @project, package: @package, repository: params[:repository])
     else
       flash[:error] = "Error while triggering wipe binaries for #{@project.name}/#{@package.name}: #{@package.errors.full_messages.to_sentence}."
-      redirect_to package_binaries_path(project: @project, package: @package, repository: params[:repository])
     end
+
+    redirect_to package_binaries_path(project: @project, package: @package, repository: params[:repository])
   end
 
   def devel_project
