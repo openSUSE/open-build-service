959220d1099692d43792d2182ac2334fe3fa1623
David Díaz <diazglezdavid@gmail.com>
2018-03-24 13:16:50 +0100

    [webui] Refactor `SessionController`
    To make it slightly more readable

--- src/api/app/controllers/webui/session_controller.rb
+++ src/api/app/controllers/webui/session_controller.rb
@@ -8,36 +8,51 @@ class Webui::SessionController < Webui::WebuiController
   def create
     user = User.find_with_credentials(params[:username], params[:password])
 
-    if user && !user.is_active?
-      redirect_to(root_path, error: 'Your account is disabled. Please contact the administrator for details.')
-      return
-    end
-
     unless user
       redirect_to(session_new_path, error: 'Authentication failed')
       return
     end
 
-    Rails.logger.debug "Authenticated as user '#{user.try(:login)}'"
+    unless user.is_active?
+      redirect_to(root_path, error: 'Your account is disabled. Please contact the administrator for details.')
+      return
+    end
 
-    session[:login] = user.login
     User.current = user
+    session[:login] = user.login
+    Rails.logger.debug "Authenticated as user '#{user.login}'"
 
-    if request.referer && request.referer.end_with?(session_new_path)
+    redirect_on_login
+  end
+
+  def destroy
+    Rails.logger.info "Logging out: #{session[:login]}"
+
+    reset_session
+    User.current = nil
+
+    redirect_on_logout
+  end
+
+  private
+
+  def redirect_on_login
+    if referer_was_login?
       redirect_to user_show_path(User.current)
     else
       redirect_back(fallback_location: root_path)
     end
   end
 
-  def destroy
-    logger.info "Logging out: #{session[:login]}"
-    reset_session
-    User.current = nil
+  def redirect_on_logout
     if CONFIG['proxy_auth_mode'] == :on
       redirect_to CONFIG['proxy_auth_logout_page']
     else
       redirect_back(fallback_location: root_path)
     end
+  end
+
+  def referer_was_login?
+    request.referer && request.referer.end_with?(session_new_path)
   end
 end
