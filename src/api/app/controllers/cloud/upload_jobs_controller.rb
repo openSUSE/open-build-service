17a0d0fc4fd0d7f9b35f2be05975ac8a7affeb4b
Björn Geuken <bgeuken@suse.de>
2018-03-28 10:16:02 +0200

    [frontend] Abort cloud uploads via API
    Add API route that allows to abort cloud upload jobs.

--- src/api/app/controllers/cloud/upload_jobs_controller.rb
+++ src/api/app/controllers/cloud/upload_jobs_controller.rb
@@ -3,17 +3,14 @@ module Cloud
     before_action :require_login
     before_action -> { feature_active?(:cloud_upload) }
     before_action :validate_configuration_presence, only: [:index, :create]
+    before_action :set_upload_job, only: [:destroy, :show]
 
     def index
       render xml: ::Cloud::Backend::UploadJob.all(::User.current, format: :xml)
     end
 
     def show
-      if ::User.current.upload_jobs.where(job_id: params[:id]).exists?
-        render xml: ::Cloud::Backend::UploadJob.find(params[:id], format: :xml)
-      else
-        render_error status: 404
-      end
+      render xml: ::Cloud::Backend::UploadJob.find(@upload_job.job_id, format: :xml)
     end
 
     def create
@@ -25,6 +22,15 @@ module Cloud
                      errorcode: 'cloud_upload_job_invalid',
                      message: "Failed to create upload job: #{upload_job.errors.full_messages.to_sentence}."
       end
+    end
+
+    def destroy
+      ::Backend::Api::Cloud.destroy(@upload_job.job_id)
+      render_ok
+    rescue ActiveXML::Transport::NotFoundError, ActiveXML::Transport::Error => exception
+      render_error status: 500,
+                   errorcode: 'cloud_upload_job_error',
+                   message:  exception.message
     end
 
     private
@@ -47,6 +53,12 @@ module Cloud
         slice(:project, :package, :repository, :arch, :filename, :region, :ami_name, :target, :vpc_subnet_id).
         to_h.
         merge(user: ::User.current)
+    end
+
+    def set_upload_job
+      @upload_job = ::Cloud::User::UploadJob.find_by(job_id: params[:id])
+      return if @upload_job.present? && (@upload_job.user == ::User.current || ::User.current.is_admin?)
+      render_error status: 404
     end
   end
 end
