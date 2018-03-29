ac2b5ced59b0e9dd011600fd938bfcb29284e90b
Christian Bruckmayer <cbruckmayer@suse.com>
2018-03-29 10:03:55 +0200

    [webui] Implement cache for superseded requests
    as we implemented diffs between superseded requests it is necessary to warm up the cache in the backend for these diffs.
    See #4741.

--- src/api/app/jobs/bs_request_action_webui_infos_job.rb
+++ src/api/app/jobs/bs_request_action_webui_infos_job.rb
@@ -10,12 +10,46 @@ class BsRequestActionWebuiInfosJob < ApplicationJob
 
   def perform(bs_request_action)
     # We don't need to do an access check as this is only for warming the cache in the backend
-    source_package_names = SourcePackageFinder.new(bs_request_action: bs_request_action, skip_access_check: true)
-    ForSource.new(
+    source_package_names = BsRequestAction::Differ::SourcePackageFinder.new(
       bs_request_action: bs_request_action,
-      source_package_names: source_package_names,
-    ).perform
-  rescue DiffError, Project::UnknownObjectError, Package::UnknownObjectError
-    # pass
+      options: { skip_access_check: true }
+    ).all
+    for_superseded_requests(bs_request_action, source_package_names)
+    for_target_package(bs_request_action, source_package_names)
+  end
+
+  private
+
+  def silent
+    yield
+  rescue BsRequestAction::DiffError, Project::UnknownObjectError, Package::UnknownObjectError
+    # as this is only for caching, we can ignore these errors
+  end
+
+  def for_target_package(bs_request_action, source_package_names)
+    silent do
+      BsRequestAction::Differ::ForSource.new(
+        bs_request_action: bs_request_action,
+        source_package_names: source_package_names
+      ).perform
+    end
+  end
+
+  def for_superseded_requests(bs_request_action, source_package_names)
+    superseded_requests = bs_request_action.bs_request.superseding
+    superseded_requests.each do |superseded_request|
+      for_a_superseded_request(superseded_request, bs_request_action, source_package_names)
+    end
+  end
+
+  def for_a_superseded_request(superseded_request, bs_request_action, source_package_names)
+    silent do
+      superseded_bs_request_action = bs_request_action.find_action_with_same_target(superseded_request)
+      BsRequestAction::Differ::ForSource.new(
+        bs_request_action: bs_request_action,
+        source_package_names: source_package_names,
+        options: { superseded_bs_request_action: superseded_bs_request_action }
+      ).perform
+    end
   end
 end
