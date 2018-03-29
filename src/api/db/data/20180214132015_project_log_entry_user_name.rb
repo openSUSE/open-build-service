51c3202586fe16d1f76cda55bb48b813a93cf944
Henne Vogelsang <hvogel@opensuse.org>
2018-03-29 17:23:34 +0200

    [frontend] Fix ProjectLogEntryUserName migration
    The migration did not work. Let's test this so it won't happen again.

--- src/api/db/data/20180214132015_project_log_entry_user_name.rb
+++ src/api/db/data/20180214132015_project_log_entry_user_name.rb
@@ -1,21 +1,21 @@
 class ProjectLogEntryUserName < ActiveRecord::Migration[5.1]
   def up
-    ProjectLogEntry.where('event_type like "%comment_for%"').in_batches do |log_entry|
-      user = User.find_by(id: log_entry.user_name).try(:login)
-      user ||= User::NOBODY_LOGIN
-      # rubocop:disable Rails/SkipsModelValidations
-      log_entry.update_attribute(user_name: user)
-      # rubocop:enable Rails/SkipsModelValidations
+    ProjectLogEntry.where('event_type like "%comment_for%"').in_batches do |batch|
+      batch.each do |log_entry|
+        user = User.find_by(id: log_entry.user_name).try(:login)
+        user ||= User::NOBODY_LOGIN
+        log_entry.update_attributes(user_name: user)
+      end
     end
   end
 
   def down
-    ProjectLogEntry.where('event_type like "%comment_for%"').in_batches do |log_entry|
-      user = User.find_by(id: log_entry.user_name).try(:id)
-      user ||= User.find_nobody!.try(:id)
-      # rubocop:disable Rails/SkipsModelValidations
-      log_entry.update_attribute(user_name: user)
-      # rubocop:enable Rails/SkipsModelValidations
+    ProjectLogEntry.where('event_type like "%comment_for%"').in_batches do |batch|
+      batch.each do |log_entry|
+        user = User.find_by(login: log_entry.user_name).try(:id)
+        user ||= User.find_nobody!.try(:id)
+        log_entry.update_attributes(user_name: user)
+      end
     end
   end
 end
--- src/api/spec/factories/project_log_entry.rb
+++ src/api/spec/factories/project_log_entry.rb
@@ -3,5 +3,16 @@ FactoryBot.define do
     project
     datetime { Time.zone.today }
     event_type 'commit'
+
+    factory :project_log_entry_comment_for_project do
+      event_type 'comment_for_project'
+      sequence(:user_name) { |n| "user_#{n}" }
+    end
+    factory :project_log_entry_comment_for_package do
+      event_type 'comment_for_package'
+      sequence(:user_name) { |n| "user_#{n}" }
+      sequence(:package_name) { |n| "package_#{n}" }
+      additional_info '{ commenters: [97], comment_body: \'A comment\'}'
+    end
   end
 end
