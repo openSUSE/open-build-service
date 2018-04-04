86201d6a3c78e468faa9bf9b95e84dec70983508
Christian Bruckmayer <cbruckmayer@suse.com>
2018-04-04 16:01:39 +0200

    [webui][api] Fix ChangeRepositoriesRemoteProjectNameToNotNull migration
    The migration ChangeRepositoriesRemoteProjectNameToNotNull changes the default
    of remote_project_name to '' instead of NULL.
    This was necessary in order to make the combination of remote_project_name,
    db_project_id and name unique because MySQL does not consider rows with same column content
    but one NULL value as equal. The migration back then was removing duplicates, however,
    it also migrated repositories which are associated with remote projects. These
    repositories do not need to get migrated as they're anyway unique
    (there can not be a NULL value for these repositories).
    
    This commit splits this migration in a schema and a data migration and introduces specs
    for the data migration.
    The data migration gets changed to NOT migrate repositories of a remote project.
    If there is duplicate repository found, we simply destroy it as the data is already inconsistent
    and it is not possible to do a full migration.
    
    Fix #4761.

--- src/api/db/migrate/20170306084558_change_repositories_remote_project_name_to_not_null.rb
+++ src/api/db/migrate/20170306084558_change_repositories_remote_project_name_to_not_null.rb
@@ -1,48 +1,15 @@
 class ChangeRepositoriesRemoteProjectNameToNotNull < ActiveRecord::Migration[5.0]
   def up
-    old = CONFIG['global_write_through']
-    CONFIG['global_write_through'] = false
-
-    Repository.transaction do
-      sql = <<-SQL
-        SELECT a.*
-        FROM repositories AS a
-        LEFT JOIN repositories AS b
-        ON a.db_project_id = b.db_project_id
-        AND a.name = b.name
-        WHERE a.id != b.id
-        GROUP BY a.db_project_id, a.name
-      SQL
-
-      repos_with_duplicates = Repository.find_by_sql(sql)
-
-      if repos_with_duplicates.any?
-        sql = <<-SQL
-          SELECT b.*
-          FROM repositories a
-          LEFT JOIN repositories AS b
-          ON a.db_project_id = b.db_project_id
-          AND a.name = b.name
-          WHERE a.id != b.id
-          AND a.id IN (#{repos_with_duplicates.map(&:id).join(', ')})
-        SQL
-
-        duplicate_repos = Repository.find_by_sql(sql)
-
-        # must be a ruby loop for the path_elements and reference cleanup
-        duplicate_repos.each(&:destroy)
-      end
-
-      execute 'UPDATE repositories SET remote_project_name = "" WHERE remote_project_name is null'
-
-      change_column_null :repositories, :remote_project_name, false
-      change_column_default :repositories, :remote_project_name, ''
-    end
-
-    CONFIG['global_write_through'] = old
+    # We need to run src/api/db/data/20170306084550_remove_duplicate_repositories.rb first,
+    # otherwise we will get duplicate entry exception when we set remote_project_name to an empty string
+    msg = 'Pending data migration 20170306084550. Please run rake db:migrate:with_data.'
+    raise ActiveRecord::ActiveRecordError, msg unless DataMigrate::DataMigrator.get_all_versions.include?(20_170_306_084_550)
+    change_column_null :repositories, :remote_project_name, false
+    change_column_default :repositories, :remote_project_name, ''
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+    change_column_null :repositories, :remote_project_name, true
+    change_column_default :repositories, :remote_project_name, nil
   end
 end
---
+++ src/api/spec/db/data/remove_duplicate_repositories_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+require Rails.root.join('db/data/20170306084550_remove_duplicate_repositories.rb')
+require Rails.root.join('db/migrate/20170306084558_change_repositories_remote_project_name_to_not_null.rb')
+
+RSpec.describe RemoveDuplicateRepositories, type: :migration do
+  # This migration does not allow NULL values for remote_project_name column
+  # We need this migration to not be run to create test data
+  # 20170306084558_change_repositories_remote_project_name_to_not_null.rb
+  let(:schema_migration) { ChangeRepositoriesRemoteProjectNameToNotNull.new }
+  let(:data_migration) { RemoveDuplicateRepositories.new }
+
+  describe '.up' do
+    before do
+      schema_migration.down
+    end
+
+    after do
+      DataMigrate::DataSchemaMigration.find_or_create_by(version: 20_170_306_084_550.to_s)
+      schema_migration.up
+    end
+
+    # We need to set Repository.deleted_instance.remote_project_name = '',
+    # because reverting the schema_migration will set the default value of remote_project_name
+    # to NULL. In the :cleanup_before_destroy callback in repository, associated path elements get
+    # associated with a global deleted repository (Repository.deleted_instance) which
+    # uses Repository.find_or_create_by(..).
+    # This would fail as there is a repository model validation which does not allow nil values.
+    let!(:deleted_repository) { create(:repository, name: 'deleted', remote_project_name: '', project: Project.deleted_instance) }
+
+    let(:remote_project) { create(:remote_project) }
+    let!(:remote_repository) { create(:repository, project: remote_project, name: 'standard', remote_project_name: 'openSUSE.org:Foo') }
+    let!(:remote_repository_with_same_name) do
+      create(:repository, project: remote_project, name: 'standard', remote_project_name: 'openSUSE.org:Bar')
+    end
+
+    let(:project) { create(:project) }
+    let!(:repository) do
+      # we need to bypass the validation
+      r = build(:repository, project: project, name: 'standard', remote_project_name: nil, architectures: ['x86_64', 'i586'])
+      r.save(validate: false)
+      r
+    end
+    let!(:duplicate_repository) do
+      # we need to bypass the validation
+      r = build(:repository, project: project, name: 'standard', remote_project_name: nil, architectures: ['x86_64', 'i586'])
+      r.save(validate: false)
+      r
+    end
+    let!(:path_element) { create(:path_element, link: repository, repository: remote_repository) }
+    let!(:duplicate_path_element) { create(:path_element, link: duplicate_repository, repository: remote_repository) }
+
+    it 'removes the local and duplicate repository' do
+      expect do
+        data_migration.up
+      end.to change { Repository.count }.by(-1)
+
+      expect(Repository.all).to include(repository)
+      expect(Repository.all).to include(remote_repository)
+      expect(Repository.all).to include(remote_repository_with_same_name)
+      expect(Repository.all).not_to include(duplicate_repository)
+    end
+
+    it 'moves duplicate path elements link to the deleted repository' do
+      data_migration.up
+      duplicate_path_element.reload
+      path_element.reload
+      expect(duplicate_path_element.link).to eq(deleted_repository)
+      expect(path_element.link).to eq(repository)
+    end
+  end
+end
