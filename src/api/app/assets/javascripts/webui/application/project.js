openSUSE/open-build-service

commit a439c3e6a5bfa181bfda5eb5f29e16ca8197838a

[frontend] Add js style rule: Require triple equals
Use === instead of ==. The == and != operator are not only comparing two
values. But also trying to convert the variable on the right side, if their
types are not identical. This can be quite a complex logic due to the
various combinations[*] that are possible.

To simplify things and avoid confusion for people that are js experts,
who is?, it's better to stick with ===.

https://dorey.github.io/JavaScript-Equality-Table/

src/api/app/assets/javascripts/webui/application/project.js
@@ -1,4 +1,4 @@
-function renderPackagesTable(wrapper, packages, length) {
+function renderPackagesTable(wrapper, packages, length) { // jshint ignore:line
   length = (typeof length === "undefined") ? 25 : length;
   var packageurl = $("#" + wrapper).data("url");
   $("#" + wrapper).html('<table cellpadding="0" cellspacing="0" border="0" class="compact stripe" id="' + wrapper + '_table"></table>');
@@ -34,7 +34,7 @@ function renderPackagesTable(wrapper, packages, length) {
   });
 }
 
-function renderProjectsTable(length) {
+function renderProjectsTable(length) { // jshint ignore:line 
   length = (typeof length === "undefined") ? 25 : length;
   var projects = main_projects;
   if (!$('#excludefilter').is(":checked"))
@@ -48,7 +48,7 @@ function renderProjectsTable(length) {
     "columns": [
       {
         "title": "Name",
-        "render": function (obj, type, data_row, meta) {
+        "render": function (obj, type, data_row) {
           var url = projecturl.replace(/REPLACEIT/, data_row[0]);
           return '<a href="' + url + '">' + data_row[0] + '</a>';
         }
@@ -60,7 +60,7 @@ function renderProjectsTable(length) {
   });
 }
 
-function renderPackagesProjectsTable(options) {
+function renderPackagesProjectsTable(options) { // jshint ignore:line
   var length = options.length || 25;
   var name = options.name || "packages_projects_wrapper";
 
@@ -74,7 +74,7 @@ function renderPackagesProjectsTable(options) {
       "columns": [
         {
           "title": "Package",
-          "render": function (obj, type, data_row, meta) {
+          "render": function (obj, type, data_row) {
             var url1 = packageurl.replace(/REPLACEPKG/, data_row[0]);
             var url = url1.replace(/REPLACEPRJ/, data_row[1]);
             return '<a href="' + url + '">' + data_row[0] + '</a>';
@@ -115,26 +115,26 @@ function autocomplete_repositories(project_name) {
       if (!foundoptions)
         $('#target_repo').append(new Option('No repos found'));
     },
-    complete: function (data) {
+    complete: function() {
       $('#loader-repo').hide();
     }
   });
 }
 
-function repositories_setup_autocomplete() {
+function repositories_setup_autocomplete() { // jshint ignore:line
   $("#target_project").autocomplete({
     source: $('#target_project').data('ajaxurl'),
     minLength: 2,
-    select: function (event, ui) {
+    select: function() {
       autocomplete_repositories(ui.item.value);
     },
-    change: function () {
+    change: function() {
       autocomplete_repositories($('#target_project').attr('value'));
     },
-    search: function(event, ui) {
+    search: function() {
       $(this).addClass('loading-spinner');
     },
-    response: function(event, ui) {
+    response: function() {
       $(this).removeClass('loading-spinner');
     }
   });
@@ -148,7 +148,7 @@ function repositories_setup_autocomplete() {
   });
 }
 
-function setup_subprojects_tables() {
+function setup_subprojects_tables() { // jshint ignore:line
   $('#parentprojects-table').dataTable({
     'paging': false,
     'searching': false,

src/api/.jshintrc
@@ -1,0 +1,8 @@
+{
+  "eqeqeq" : true, // true: Require triple equals (===) for comparison
+  "unused" : true, // Unused variables: true : all variables, last function parameter
+
+  "globals": {
+    "$": false
+  }
+}

src/api/app/assets/javascripts/webui/application/comment.js
@@ -1,6 +1,6 @@
 // Expand the comment textarea to fit the text
 // as it's being typed.
-function sz(t) {
+function sz(t) { // jshint ignore:line
   var a = t.value.split('\n');
   var b = 1;
   for (var x = 0; x < a.length; x++) {
@@ -11,11 +11,11 @@ function sz(t) {
 }
 
 function reloadCommentBindings() {
-  $('a.delete_link').on('ajax:success', function(event, data, status, xhr){
+  $('a.delete_link').on('ajax:success', function(event, data){
     $('#flash-messages').remove();
     $(data.flash).filter('#flash-messages').insertAfter('#subheader').fadeIn('slow');
     $(this).parent().parent().parent().fadeOut("slow");
-  }).on('ajax:error',function(event, xhr, status, error){
+  }).on('ajax:error',function(event, xhr){
     var response = $.parseJSON(xhr.responseText);
     $('#flash-messages').remove();
     $(response.flash).filter('#flash-messages').insertAfter('#subheader').fadeIn('slow');
@@ -35,7 +35,7 @@ function reloadCommentBindings() {
       $(this).find('input[type="submit"]').prop('disabled', true);
   });
 
-  $('.comment_new').on('ajax:complete', function(event, data, status, xhr) {
+  $('.comment_new').on('ajax:complete', function(event, data) {
     $('#comments').html(data.responseText);
 
     // as the comments get loaded again, the jQuery bindings are lost. We need to reload them.

src/api/app/assets/javascripts/webui/application/image_templates.js
@@ -1,5 +1,5 @@
 $(function() {
-  $(".image_template").on("click", function(e){
+  $(".image_template").on("click", function(){
     $('#target_package').val(this.getAttribute('data-package'));
     $('#linked_package').val(this.getAttribute('data-package'));
     $('#linked_project').val(this.getAttribute('data-project'));

src/api/app/assets/javascripts/webui/application/kiwi_editor.js
@@ -106,7 +106,7 @@ function closeDescriptionDialog() {
   var elements = fields.find('.fill');
   for(var i=0; i < elements.length; i++) {
     var object = dialog.find("[id$='" + $(elements[i]).data('tag') + "']");
-    if ( object.val() != "") {
+    if ( object.val() !== "") {
       $(elements[i]).text(object.val());
     }
   }
@@ -130,8 +130,8 @@ function closePreferencesDialog() {
   var elements = fields.find('.fill');
   for(var i=0; i < elements.length; i++) {
     var object = dialog.find("[id$='" + $(elements[i]).data('tag') + "']");
-    if ( object.val() != "") {
-      if ( $(elements[i]).data('tag') == 'type_image' ) {
+    if ( object.val() !== "") {
+      if ( $(elements[i]).data('tag') === 'type_image' ) {
         $(elements[i]).text(object.find(":selected").text());
       }
       else {
@@ -280,7 +280,7 @@ function autocompleteKiwiRepositories(project, repo_field) {
         if (!foundoptions)
           repo_field.append(new Option('No repos found'));
       },
-      complete: function (data) {
+      complete: function () {
         $('.ui-autocomplete-loading').hide();
         repo_field.trigger("change");
       }
@@ -298,10 +298,10 @@ function kiwiRepositoriesSetupAutocomplete(fields) {
     select: function (event, ui) {
       autocompleteKiwiRepositories(ui.item.value, repo_field);
     },
-    search: function(event, ui) {
+    search: function() {
       $(this).addClass('loading-spinner');
     },
-    response: function(event, ui) {
+    response: function() {
       $(this).removeClass('loading-spinner');
     }
   });
@@ -312,7 +312,7 @@ function kiwiRepositoriesSetupAutocomplete(fields) {
 
   repo_field.change(function () {
     var repo_field_value = repo_field.val();
-    if (repo_field.val() == 'No repos found') {
+    if (repo_field.val() === 'No repos found') {
       repo_field_value = '';
     }
     var source_path = fields.find("[id$='source_path']");
@@ -337,7 +337,7 @@ function kiwiPackagesSetupAutocomplete(fields) {
       var repository_destroy_fields = $("#kiwi-repositories-list [id$='_destroy']");
       var using_project_repositories =  $('#kiwi_image_use_project_repositories').prop('checked');
       repository_destroy_fields.each(function(index, input){ // remove destroyed repositories from the payload
-        if ($(input).val() == "1") {
+        if ($(input).val() === "1") {
           repositories.splice(index, 1);
         }
       });
@@ -350,10 +350,10 @@ function kiwiPackagesSetupAutocomplete(fields) {
           response(data);
         });
     },
-    search: function(event, ui) {
+    search: function() {
       $(this).addClass('loading-spinner');
     },
-    response: function(event, ui) {
+    response: function() {
       $(this).removeClass('loading-spinner');
     },
     minLength: 2

src/api/app/assets/javascripts/webui/application/main.js
@@ -1,4 +1,4 @@
-function plotbusyworkers(data) {
+function plotbusyworkers(data) { // jshint ignore:line
   $.plot($("#overallgraph"), [
     { data: data, label: "Busy workers", color: 3}
   ],

src/api/app/assets/javascripts/webui/application/package.js
@@ -1,4 +1,4 @@
-function makeServicesDragable() {
+function makeServicesDragable() { // jshint ignore:line
   $("#services").sortable( {
     placeholder: "empty_service",
     update: function(event, ui) {
@@ -37,7 +37,7 @@ function makeServicesDragable() {
 var DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 var MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-function addChangesEntryTemplate() {
+function addChangesEntryTemplate() { // jshint ignore:line
   var date = new Date(),
     day = date.getUTCDate().toString(),
     hours,
@@ -77,7 +77,7 @@ function autoscroll() {
   lw.data("lastScroll", window.pageYOffset);
 }
 
-function build_finished() {
+function build_finished() { // jshint ignore:line
   stop_refresh();
   $('#status').html('Build finished');
 }
@@ -92,7 +92,7 @@ function start_refresh() {
   return false;
 }
 
-function remove_ajaxreq() {
+function remove_ajaxreq() { // jshint ignore:line
   var lw = $('#log_space_wrapper');
   lw.data("ajaxreq", 0);
 }
@@ -125,17 +125,17 @@ function refresh(newoffset, initial) {
   }
 }
 
-function show_abort() {
+function show_abort() { // jshint ignore:line
   $(".link_abort_build").show();
   $(".link_trigger_rebuild").hide();
 }
 
-function hide_abort() {
+function hide_abort() { // jshint ignore:line
   $(".link_abort_build").hide();
   $(".link_trigger_rebuild").show();
 }
 
-function live_build_log_ready() {
+function live_build_log_ready() { // jshint ignore:line, unused:true
   var lw = $('#log_space_wrapper');
   lw.data("autorefresh", 1);
   lw.data("lastScroll", 0);

src/api/app/assets/javascripts/webui/application/request.js
@@ -44,17 +44,17 @@ function updateSupersedeAndDevelPackageDisplay() {
   }
 }
 
-function setup_request_dialog() {
+function setup_request_dialog() { // jshint ignore:line
   $('#devel-project-name').click(function () {
     $('#targetproject').attr('value', $('#devel-project-name').html());
   });
 
   $('#targetproject').autocomplete({
     source: $('#targetproject').data('autocomplete-url'),
-    search: function(event, ui) {
+    search: function() {
       $(this).addClass('loading-spinner');
     },
-    response: function(event, ui) {
+    response: function() {
       $(this).removeClass('loading-spinner');
     },
     minLength: 2,
@@ -70,8 +70,8 @@ function setup_request_dialog() {
  minChars: 0, matchCase: true, max: 50, extraParams: {project: function() { return $("#target_project").val(); }}
  });*/
 
-function requestAddAcceptRequestButton() {
-  $('#accept_request_button').click(function (data) {
+function requestAddAcceptRequestButton() { // jshint ignore:line
+  $('#accept_request_button').click(function () {
     var additional_element;
 
     /* Add some hidden elements to carry HTML form data that is found at other DOM places for design reasons.  */
@@ -94,7 +94,7 @@ function requestAddAcceptRequestButton() {
   });
 }
 
-function requestShowReview() {
+function requestShowReview() { // jshint ignore:line
   var index;
   $('.review_descision_link').click(function (event) {
     $('#review_descision_select li.selected').attr('class', '');
@@ -106,7 +106,7 @@ function requestShowReview() {
   });
 }
 
-function requestAddReviewAutocomplete() {
+function requestAddReviewAutocomplete() { // jshint ignore:line
 
   $('#review_type').change(function () {
     switch ($('#review_type option:selected').attr('value')) {
@@ -146,24 +146,24 @@ function requestAddReviewAutocomplete() {
   });
 
   $("#review_group").autocomplete({source: '/group/autocomplete', minChars: 2, matchCase: true, max: 50,
-  search: function(event, ui) {
+  search: function() {
     $(this).addClass('loading-spinner');
   },
-  response: function(event, ui) {
+  response: function() {
     $(this).removeClass('loading-spinner');
   }});
   $("#review_user").autocomplete({source: '/user/autocomplete', minChars: 2, matchCase: true, max: 50,
-  search: function(event, ui) {
+  search: function() {
     $(this).addClass('loading-spinner');
   },
-  response: function(event, ui) {
+  response: function() {
     $(this).removeClass('loading-spinner');
   }});
   $("#review_project").autocomplete({source: '/project/autocomplete_projects', minChars: 2, matchCase: true, max: 50,
-  search: function(event, ui) {
+  search: function() {
     $(this).addClass('loading-spinner');
   },
-  response: function(event, ui) {
+  response: function() {
     $(this).removeClass('loading-spinner');
   }});
   $("#review_package").autocomplete({
@@ -180,10 +180,10 @@ function requestAddReviewAutocomplete() {
         }
       });
     },
-    search: function(event, ui) {
+    search: function() {
       $(this).addClass('loading-spinner');
     },
-    response: function(event, ui) {
+    response: function() {
       $(this).removeClass('loading-spinner');
     },
     minLength: 2,
@@ -193,7 +193,7 @@ function requestAddReviewAutocomplete() {
   });
 }
 
-function setupActionLink() {
+function setupActionLink() { // jshint ignore:line
   var index;
   $('.action_select_link').click(function (event) {
     $('#action_select li.selected').attr('class', '');

commit 866924064a1bd20bff1df4c27cd541aa97c7e81d

[frontend] Fix expert mode for adding repositories
This broke with a439c3e6a5bfa181bfda5eb5f29e16ca8197838a

Fixes #4740

src/api/app/assets/javascripts/webui/application/project.js
@@ -125,7 +125,7 @@ function repositoriesSetupAutocomplete() { // jshint ignore:line
   $("#target_project").autocomplete({
     source: $('#target_project').data('ajaxurl'),
     minLength: 2,
-    select: function() {
+    select: function(event, ui) {
       autocompleteRepositories(ui.item.value);
     },
     change: function() {