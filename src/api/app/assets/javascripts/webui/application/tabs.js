698fe010ca7add63189a8599b587a5beff3f400c
Ana María Martínez Gómez <ammartinez@suse.de>
2018-04-10 11:31:33 +0200

    [webui] Make tabs javascript generic
    - Rename `content-tab` to make clear what it is about.
    - Call the javascript file tabs
    - Put the specific groups code (1 line) into application.js

--- src/api/app/assets/javascripts/webui/application/tabs.js
+++ src/api/app/assets/javascripts/webui/application/tabs.js
@@ -1,15 +1,14 @@
 $(document).ready(function(){
-  $('#group-members-table').dataTable();
   $('.header-tabs li a').click(function() {
     // Select tab
     $(this).closest('ul').children('li').removeClass('selected');
     $(this).parent().addClass('selected');
     // Show and hide content
     var id = $(this).data("id");
-    $('.table-container').addClass('hidden');
+    $(this).closest('.header-tabs').next('.content-tabs').find('.content-tab').addClass('hidden');
     $('#' + id).removeClass('hidden');
     // Show reload button when tab is changed
-    $(this).parent().parent().find('.result_reload').hide();
+    $(this).closest('ul').find('.result_reload').hide();
     $(this).siblings('.result_reload').show();
   });
 });
