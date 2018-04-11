a27edf5355acaa24b75a9eda78fff412093ac493
Moises Deniz Aleman <mdeniz@suse.com>
2018-04-11 10:52:15 +0200

    [ci] Use fixtures for files in Azure Cloud upload specs
    We were reading the files via File.read and instead we are now using
    file_fixture('filename').read from Rspec

--- src/api/spec/models/cloud/azure/configuration_spec.rb
+++ src/api/spec/models/cloud/azure/configuration_spec.rb
@@ -4,8 +4,8 @@ require 'webmock/rspec'
 RSpec.describe Cloud::Azure::Configuration, type: :model do
   describe 'data encryption' do
     let(:config) { create(:azure_configuration, application_id: 'Hey OBS!', application_key: 'Hey OBS?') }
-    let(:secret_key) { OpenSSL::PKey::RSA.new(File.read(Rails.root.join('spec', 'support', 'files', 'cloudupload_secret_key_tests.pem'))) }
-    let(:public_key) { File.read(Rails.root.join('spec', 'support', 'files', 'cloudupload_public_key.txt')) }
+    let(:secret_key) { OpenSSL::PKey::RSA.new(file_fixture('cloudupload_secret_key_tests.pem').read) }
+    let(:public_key) { file_fixture('cloudupload_public_key.txt').read }
 
     before do
       stub_request(:get, "#{CONFIG['source_url']}/cloudupload/_pubkey").and_return(body: public_key)
