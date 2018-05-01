ed0c247c02799fbe3ffb03e68519ed7756ab870f
Moises Deniz Aleman <mdeniz@suse.com>
2018-05-01 23:31:41 +0200

    [frontend] Improve tabs and package#show
    Adapted the style of the tabs and improved part of the show view

---
+++ src/api/app/helpers/webui/webui2_helper.rb
@@ -0,0 +1,11 @@
+module Webui::Webui2Helper
+  def webui2_tab(id, text, opts, link_opts = {})
+    opts[:package] = @package.to_s if @package
+    opts[:project] = @project.to_s if @project
+    link_opts.merge!({ id: "tab-#{id}" })
+    if (action_name == opts[:action].to_s && (opts[:controller].to_s.include? controller_name)) || opts[:selected]
+      link_opts[:class] = 'is-active'
+    end
+    content_tag('li', link_to(h(text), opts), link_opts)
+  end
+end
