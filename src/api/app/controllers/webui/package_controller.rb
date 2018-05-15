e364e0e07388a479290c6fb828625400298fba6e
bgeuken <bgeuken@suse.de>
2018-05-15 15:54:01 +0200

    [frontend] Fix authorization for trigger_service
    The webui was permitting triggering of services to any logged in user.
    This commit adds the missing authorization code.

--- src/api/app/controllers/webui/package_controller.rb
+++ src/api/app/controllers/webui/package_controller.rb
@@ -619,6 +619,8 @@ class Webui::PackageController < Webui::WebuiController
   end
 
   def trigger_services
+    authorize @package, :update?
+
     begin
       Backend::Api::Sources::Package.trigger_services(@project.name, @package.name, User.current.to_s)
       flash[:notice] = 'Services successfully triggered'
