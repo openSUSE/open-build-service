1308e36a1701c25ab64aca96b54f1d8b97da1640
Björn Geuken <bgeuken@suse.de>
2018-05-22 17:22:31 +0200

    [frontend] Rails 5.2: Fix checking if data migration was run
    This changed with Rails 5.2.

--- src/api/db/migrate/20170306084558_change_repositories_remote_project_name_to_not_null.rb
+++ src/api/db/migrate/20170306084558_change_repositories_remote_project_name_to_not_null.rb
@@ -3,7 +3,7 @@ class ChangeRepositoriesRemoteProjectNameToNotNull < ActiveRecord::Migration[5.0
     # We need to run src/api/db/data/20170306084550_remove_duplicate_repositories.rb first,
     # otherwise we will get duplicate entry exception when we set remote_project_name to an empty string
     msg = 'Pending data migration 20170306084550. Please run rake db:migrate:with_data.'
-    raise ActiveRecord::ActiveRecordError, msg unless DataMigrate::DataMigrator.get_all_versions.include?(20_170_306_084_550)
+    raise ActiveRecord::ActiveRecordError, msg unless DataMigrate::DataSchemaMigration.where(version: 20_170_306_084_550).exists?
     change_column_null :repositories, :remote_project_name, false
     change_column_default :repositories, :remote_project_name, ''
   end
