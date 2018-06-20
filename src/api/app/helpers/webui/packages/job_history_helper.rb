051e2a7f6ee1a300e05b8e86b9667ba8cc4b92de
David Kang <dkang@suse.com>
2018-06-20 11:55:25 +0200

    [webui] Use package_name instead of package
    In case of multibuild, we always stripped the multibuild suffix.

--- src/api/app/helpers/webui/packages/job_history_helper.rb
+++ src/api/app/helpers/webui/packages/job_history_helper.rb
@@ -8,7 +8,7 @@ module Webui::Packages::JobHistoryHelper
   end
 
   def link_to_package_from_job_history(project, package, jobhistory, is_link)
-    title = "Package:#{package.name} | revision:#{jobhistory.revision}"
+    title = "Package:#{package} | revision:#{jobhistory.revision}"
     params = { project: project, package: package }
     params = is_link ? params.merge(srcmd5: jobhistory.srcmd5) : params.merge(rev: jobhistory.revision)
 
