openSUSE/open-build-service

commit 536e6dc161d14f34404f17749e38dac8560cf136

[frontend] Reduce complexity of update_binary_releases_via_json
Move code that compares binary release objects with a hash entry to a
seperate method.

This makes the code easier to understand, hopefully...

src/api/app/models/binary_release.rb
@@ -55,9 +55,7 @@ class BinaryRelease < ApplicationRecord
         # compare with existing entry
         if existing.count == 1
           entry = existing.first
-          if entry.binary_disturl                   == binary['disturl'] &&
-             entry.binary_supportstatus             == binary['supportstatus'] &&
-             entry.binary_buildtime.to_datetime.utc == ::Time.at(binary['buildtime'].to_i).to_datetime.utc
+          if entry.indentical_to?(binary)
             # same binary, don't touch
             processed_item[entry.id] = true
             next
@@ -184,6 +182,11 @@ class BinaryRelease < ApplicationRecord
     Rails.cache.delete("xml_binary_release_#{cache_key}")
   end
 
+  def indentical_to?(binary_hash)
+    binary_disturl == binary_hash['disturl'] &&
+      binary_supportstatus == binary_hash['supportstatus'] &&
+      entry.binary_buildtime.to_datetime.utc == ::Time.at(binary['buildtime'].to_i).to_datetime.utc
+  end
   #### Alias of methods
 end
 

commit 9ca4eea4628c11abb7f3d627f604f66d435f9a0a

[frontend] Fix typo in BinaryRelease model
Caused by 536e6dc161d14f34404f17749e38dac8560cf136

src/api/app/models/binary_release.rb
@@ -187,7 +187,7 @@ class BinaryRelease < ApplicationRecord
     binary_disturl == binary_hash['disturl'] &&
       binary_supportstatus == binary_hash['supportstatus'] &&
       binary_buildtime &&
-      binary_buildtime.to_datetime.utc == DateTime.strptime(binary['buildtime'].to_s, '%s').utc
+      binary_buildtime.to_datetime.utc == DateTime.strptime(binary_hash['buildtime'].to_s, '%s').utc
   end
   # rubocop:enable Style/DateTime
   #### Alias of methods