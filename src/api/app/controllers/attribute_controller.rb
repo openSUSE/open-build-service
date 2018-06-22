a6896254628c9b3a64891b5fb44575c86cc1fc54
Stephan Kulow <coolo@suse.de>
2018-06-22 07:23:10 +0200

    [api] Split out attribute_namespace controller
    These routes are distinct in their requirements

--- src/api/app/controllers/attribute_controller.rb
+++ src/api/app/controllers/attribute_controller.rb
@@ -1,70 +1,11 @@
 class AttributeController < ApplicationController
   include ValidationHelper
 
-  validate_action index: { method: :get, response: :directory }
-  validate_action show_namespace_definition: { method: :get, response: :attribute_namespace_meta }
-  validate_action delete_namespace_definition: { method: :delete, response: :status }
-  validate_action update_namespace_definition: { method: :put, request: :attribute_namespace_meta, response: :status }
-  validate_action update_namespace_definition: { method: :post, request: :attribute_namespace_meta, response: :status }
   validate_action show_attribute_definition: { method: :get, response: :attrib_type }
   validate_action delete_attribute_definition: { method: :delete, response: :status }
   validate_action update_attribute_definition: { method: :put, request: :attrib_type, response: :status }
   validate_action update_attribute_definition: { method: :post, request: :attrib_type, response: :status }
-  before_action :require_namespace, only: [:show_namespace_definition, :delete_namespace_definition, :update_namespace_definition]
-  before_action :require_admin, only: [:update_namespace_definition, :delete_namespace_definition]
   before_action :require_attribute_name, only: [:show_attribute_definition, :update_attribute_definition, :delete_attribute_definition]
-
-  def index
-    if params[:namespace]
-      an = AttribNamespace.find_by_name!(params[:namespace])
-      list = an.attrib_types.pluck(:name)
-    else
-      list = AttribNamespace.pluck(:name)
-    end
-
-    builder = Builder::XmlMarkup.new(indent: 2)
-    xml = builder.directory(count: list.length) do |dir|
-      list.each do |a|
-        dir.entry(name: a)
-      end
-    end
-
-    render xml: xml
-  end
-
-  def show_namespace_definition
-    if (@an = AttribNamespace.find_by_name!(@namespace))
-      render template: 'attribute/namespace_definition'
-    else
-      render_error message: "Unknown attribute namespace '#{@namespace}'",
-        status: 404, errorcode: 'unknown_attribute_namespace'
-    end
-  end
-
-  def delete_namespace_definition
-    AttribNamespace.where(name: @namespace).destroy_all
-    render_ok
-  end
-
-  # /attribute/:namespace/_meta
-  def update_namespace_definition
-    xml_element = Xmlhash.parse(request.raw_post)
-
-    unless xml_element['name'] == @namespace
-      render_error status: 400, errorcode: 'illegal_request',
-        message: "Illegal request: PUT/POST #{request.path}: path does not match content"
-      return
-    end
-
-    db = AttribNamespace.find_by_name(@namespace)
-    if db
-      db.update_from_xml(xml_element)
-    else
-      AttribNamespace.create(name: @namespace).update_from_xml(xml_element)
-    end
-
-    render_ok
-  end
 
   # GET /attribute/:namespace/:name/_meta
   def show_attribute_definition
