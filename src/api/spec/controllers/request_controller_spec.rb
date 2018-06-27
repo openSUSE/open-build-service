c43e09d830dc7b7425050ccb0948e84757ab55b5
Björn Geuken <bgeuken@suse.de>
2018-06-27 14:40:43 +0200

    [ci] Add controller test for creating requests via API
    Add test case for bsc#1098934.
    
    Co-authored-by: David Kang <dkang@suse.com>

--- src/api/spec/controllers/request_controller_spec.rb
+++ src/api/spec/controllers/request_controller_spec.rb
@@ -39,4 +39,18 @@ RSpec.describe RequestController, type: :controller, vcr: true do
       end
     end
   end
+
+  describe '#global_command (cmd=create)' do
+    context 'requesting creation of a source project that has a project link that is not owned by the requester' do
+      include_context 'a BsRequest that has a project link'
+
+      it 'prohibits creation of request' do
+        expect { post :global_command, params: { cmd: :create }, body: xml, format: :xml }.not_to change(BsRequest, :count)
+        expect(response).to have_http_status(:forbidden)
+        assert_select 'status[code=lacking_maintainership]' do
+          assert_select 'summary', text: 'Creating a submit request action with options requires maintainership in source package'
+        end
+      end
+    end
+  end
 end
