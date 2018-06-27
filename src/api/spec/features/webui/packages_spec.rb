178a953909e65b9363bfb97b5e3887da3d482a5e
Henne Vogelsang <hvogel@opensuse.org>
2018-06-27 15:00:05 +0200

    [ci] Remove backend cleanup from specs
    This would not remove anything from the backend. It would just
    set the package/project to deleted which will have strange side effects
    if you try to create it again.

--- src/api/spec/features/webui/packages_spec.rb
+++ src/api/spec/features/webui/packages_spec.rb
@@ -62,14 +62,6 @@ RSpec.feature 'Packages', type: :feature, js: true do
       click_link('Branch package')
     end
 
-    after do
-      # Cleanup backend
-      if CONFIG['global_write_through']
-        Backend::Connection.delete("/source/#{CGI.escape(other_user.home_project_name)}")
-        Backend::Connection.delete("/source/#{CGI.escape(user.branch_project_name(other_user.home_project_name))}")
-      end
-    end
-
     scenario 'with AutoCleanup' do
       click_button('Ok')
 
