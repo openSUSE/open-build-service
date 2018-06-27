78069f707336bd8f0884c099000e01090a5c51f4
Henne Vogelsang <hvogel@opensuse.org>
2018-06-27 14:15:12 +0200

    [ci] Fix casette sync problems

--- src/api/spec/features/webui/users/users_home_project_spec.rb
+++ src/api/spec/features/webui/users/users_home_project_spec.rb
@@ -1,7 +1,7 @@
 require 'browser_helper'
 
 RSpec.feature "User's home project creation", type: :feature, js: true do
-  let!(:user) { create(:confirmed_user, login: 'moi') }
+  let!(:user) { create(:confirmed_user, login: 'hans') }
 
   scenario 'creating a home project' do
     user.home_project.destroy
