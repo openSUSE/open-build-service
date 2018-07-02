4fa71c6328452d5c0e7cfaa610a8dea356312201
Christian Bruckmayer <cbruckmayer@suse.com>
2018-07-02 17:34:41 +0200

    [webui][api] Fix delete request diff for packages
    The if statements were completely messed up.
    This got introduced by a refactoring in
    214ba835ddcb6fc02a216dd3108c27c1ad12f724.
    
    The DiffError should only raise if there is no
    package and no repository (messed with the unless).
    Also the guard clause should only return and empty
    string if there is no target_package.
    In case there is package, we can do a normal diff.
    
    Fix #5107

--- src/api/spec/factories/bs_requests.rb
+++ src/api/spec/factories/bs_requests.rb
@@ -9,6 +9,7 @@ FactoryBot.define do
       source_package nil
       target_project nil
       target_package nil
+      target_repository nil
       reviewer nil
     end
 
@@ -79,7 +80,8 @@ FactoryBot.define do
         request.bs_request_actions << create(
           :bs_request_action_delete,
           target_project: evaluator.target_project,
-          target_package: evaluator.target_package
+          target_package: evaluator.target_package,
+          target_repository: evaluator.target_repository
         )
       end
     end
