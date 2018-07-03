511650770ed4aa0bc1c3b85280f5809e945118b2
David Kang <dkang@suse.com>
2018-07-03 16:31:18 +0200

    [webui] Add openURI requirement
    In `published_version` method we were using OpenURI that was not
    required and the application failed.
    
    Co-authored-by: Ana María Martínez Gómez <ammartinez@suse.de>

--- src/api/app/models/obs_factory/distribution_strategy_opensuse.rb
+++ src/api/app/models/obs_factory/distribution_strategy_opensuse.rb
@@ -1,5 +1,6 @@
-module ObsFactory
+require 'open-uri'
 
+module ObsFactory
   # this class tracks the differences between Factory and the upcoming release
   class DistributionStrategyOpenSUSE < DistributionStrategyFactory
     SIGNATURE = /openSUSE:(?<full_name>.+:(?<version>(?<major_version>\d+)\.(?<minor_version>\d+)))/
@@ -50,7 +51,7 @@ module ObsFactory
     def published_version
       begin
         f = open(repo_url)
-      rescue OpenURI::HTTPError => e
+      rescue ::OpenURI::HTTPError => e
         return 'unknown'
       end
       matchdata = %r{openSUSE-#{opensuse_leap_version}-#{published_arch}-Build(.*)}.match(f.read)
