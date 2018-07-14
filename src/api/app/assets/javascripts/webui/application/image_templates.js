5cf8493733a0400e02650678fea30991e1881d7b
Stephan Kulow <coolo@suse.de>
2018-07-14 08:11:18 +0200

    [frontend] Use ready_function in image_templates

--- src/api/app/assets/javascripts/webui/application/image_templates.js
+++ src/api/app/assets/javascripts/webui/application/image_templates.js
@@ -1,7 +1,7 @@
-$(function() {
+function setupImageTemplates() { // jshint ignore:line
   $(".image_template").on("click", function(){
     $('#target_package').val(this.getAttribute('data-package'));
     $('#linked_package').val(this.getAttribute('data-package'));
     $('#linked_project').val(this.getAttribute('data-project'));
   });
-});
+}
