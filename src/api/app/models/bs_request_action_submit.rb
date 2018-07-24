openSUSE/open-build-service

commit 990ef7cccef6f38fc1d1a1bb22a08e174dcba43b

[api][webui] Check access to source package
to make sure user can write to it.

https://bugzilla.suse.com/show_bug.cgi?id=1094819

Signed-off-by: Christian Bruckmayer <cbruckmayer@suse.com>
Co-authored-by: Christian Bruckmayer <cbruckmayer@suse.com>

src/api/app/models/bs_request_action_submit.rb
@@ -98,6 +98,26 @@ class BsRequestActionSubmit < BsRequestAction
     Project.find_by_name!(self.target_project).update_product_autopackages
   end
 
+  def check_action_permission!(skip_source = nil)
+    super(skip_source)
+    # only perform the following check, if we are called from
+    # BsRequest.permission_check_change_state! (that is, if
+    # skip_source is set to true). Always executing this check
+    # would be a regression, because this code is also executed
+    # if a new request is created (which could fail if User.current
+    # cannot modify the source_package).
+    return unless skip_source
+    target_project = Project.get_by_name(self.target_project)
+    return unless target_project && target_project.is_a?(Project)
+    target_package = target_project.packages.find_by_name(self.target_package)
+    initialize_devel_package = target_project.find_attribute('OBS', 'InitializeDevelPackage')
+    return if target_package || !initialize_devel_package
+    source_package = Package.get_by_project_and_name(source_project, self.source_package)
+    return if !source_package || User.current.can_modify_package?(source_package)
+    msg = 'No permission to initialize the source package as a devel package'
+    raise PostRequestNoPermission, msg
+  end
+
   #### Alias of methods
 end
 

src/api/test/functional/request_controller_test.rb
@@ -2038,6 +2038,18 @@ class RequestControllerTest < ActionDispatch::IntegrationTest
     assert_no_xml_tag(tag: 'person', attributes: { userid: 'Iggy', role: 'maintainer' })
     assert_no_xml_tag(tag: 'group', attributes: { groupid: 'test_group', role: 'reader' })
 
+    # Accept will fail because Fred does not have permissions on the source package
+    # which is required because of the InitializeDevelPackage attribute on kde4
+    login_fred
+    post "/request/#{id}?cmd=changestate&newstate=accepted"
+    assert_response 403
+    assert_match(/No permission to initialize the source package as a devel package/, @response.body)
+
+    # We need to set permissions on the source because of the InitializeDevelPackage attribute on kde4 project
+    login_king
+    put '/source/home:Iggy:branches:kde4/_meta', params: "<project name='home:Iggy:branches:kde4'><title/><description/><person userid='fred' role='maintainer'/></project>"
+    assert_response :success
+
     # Successful accept the request
     login_fred
     post "/request/#{id}?cmd=changestate&newstate=accepted"
@@ -2142,6 +2154,21 @@ class RequestControllerTest < ActionDispatch::IntegrationTest
     assert_xml_tag(tag: 'state', attributes: { name: 'review' },
                    parent: { tag: 'request' }) # switch to new after last review
 
+    # Accept will fail because adrian does not have permissions on the source
+    # which is required because of the InitializeDevelPackage attribute on kde4
+    post "/request/#{id}?cmd=changestate&newstate=accepted&force=1"
+    assert_response 403
+    assert_match(/No permission to initialize the source package as a devel package/, @response.body)
+
+    # Adding adrian as maintainoer of the source
+    login_Iggy
+    get '/source/home:Iggy/_meta'
+    iggy_meta = @response.body
+    put '/source/home:Iggy/_meta', params: "<project name='home:Iggy'><title/><description/><person userid='adrian' role='maintainer'/></project>"
+
+    assert_response :success
+
+    login_adrian
     # approve accepted and check initialized devel package
     post "/request/#{id}?cmd=changestate&newstate=accepted&force=1"
     assert_response :success
@@ -2155,6 +2182,8 @@ class RequestControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     delete '/source/kde4/Testing'
     assert_response :success
+    login_Iggy
+    put '/source/home:Iggy/_meta', params: iggy_meta
   end
 
   def test_reviewer_added_when_source_maintainer_is_missing

commit f57b660f49f830006766a8d4abc3b4af6e178063

Ignore a project link in BsRequestAction.check_action_permission!
This makes sure that we check the permissions of the correct package. For
instance, assume that the project "Staging" is a link project where the
link points to the "Base" project. Also, assume that there exists a
"Base/foo" package, but there exists no explicit "Staging/foo" package.
Moreover, assume we check the permissions for the following "submit"
action:

<action type="submit">
  <source project="Staging" package="foo"/>
  <target project="an_arbitrary_project" package="foo"/>
</action>

In this case, the old code checks if request acceptor can modify the
"Base/foo" package (since it follows the project link). This is wrong
because the "Staging/foo" package would be turned into a branch during
accept.
The new code checks the correct package because it does not follow the
project link and requires that the source package exists in the source
project. Requiring the existence of the source package potentially
breaks artificial requests (for instance, a request where the "submit"
action from above is preceded by a "submit" action that creates a
"Staging/foo" package).

Note: so far I was unable to exploit the old code - so this is just
to avoid a potential future headache.

Fixes: commit 990ef7ccc ("[api][webui] Check access to source package")

src/api/app/models/bs_request_action_submit.rb
@@ -115,8 +115,11 @@ class BsRequestActionSubmit < BsRequestAction
     target_package = target_project.packages.find_by_name(self.target_package)
     initialize_devel_package = target_project.find_attribute('OBS', 'InitializeDevelPackage')
     return if target_package || !initialize_devel_package
-    source_package = Package.get_by_project_and_name(source_project, self.source_package)
-    return if !source_package || User.current.can_modify?(source_package)
+    opts = { follow_project_links: false }
+    source_package = Package.get_by_project_and_name!(source_project,
+                                                      self.source_package,
+                                                      opts)
+    return if User.current.can_modify?(source_package)
     msg = 'No permission to initialize the source package as a devel package'
     raise PostRequestNoPermission, msg
   end