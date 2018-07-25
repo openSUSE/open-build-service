83aaac93c0c77728aee180df17718aabf3069fd5
Stephan Kulow <coolo@suse.de>
2018-07-25 06:03:09 +0200

    Stop cating test.log in rpm builds
    This creates a 100MB log file that is unusuable in browser
    logs and you need more time understanding what failed and
    the chances the test.log helps you more than the actual
    exception is slim

--- src/api/script/api_test_in_spec.sh
+++ src/api/script/api_test_in_spec.sh
@@ -74,11 +74,7 @@ for suite in "rake.ruby2.5 test:api" "rake.ruby2.5 test:spider" "rspec"; do
     perl -pi -e 's/source_host: backend/source_host: localhost/' config/options.yml
     perl -pi -e 's/source_port: 5352/source_port: 3200/' config/options.yml
   fi
-  if ! (set -x; bundle.ruby2.5 exec $suite); then
-    # dump log only in package builds
-    [[ -n "$RPM_BUILD_ROOT" ]] && cat log/test.log
-    exit 1
-  fi
+  bundle.ruby2.5 exec $suite
 done
 
 #cleanup
