40ec244213eda110dc17eb8fdefa0d33a3407ce1
Eduardo Navarro <enavarro@suse.com>
2018-07-26 17:25:34 +0200

    Fix Lint/UnusedBlockArgument RuboCop offense
    ... manually. auto-correct renames the variable ex to _ex.

--- src/api/app/controllers/webui/obs_factory/application_controller.rb
+++ src/api/app/controllers/webui/obs_factory/application_controller.rb
@@ -2,7 +2,7 @@ module Webui::ObsFactory
   class ApplicationController < ::Webui::WebuiController
     layout 'webui/obs_factory/application'
 
-    rescue_from ::ObsFactory::OpenqaApi::OpenqaFailure do |ex|
+    rescue_from ::ObsFactory::OpenqaApi::OpenqaFailure do
       render text: "failure in openQA"
     end
   end
