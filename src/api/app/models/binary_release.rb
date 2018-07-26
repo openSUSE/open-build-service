openSUSE/open-build-service

commit 9c0bc2cce4825f2b325d8ece3b388b0f27fccc16

[frontend] Refactor update_binary_releases_via_json
* Rename variable: existing -> matching_binaries
* Simplify condition

src/api/app/models/binary_release.rb
@@ -47,14 +47,15 @@ class BinaryRelease < ApplicationRecord
                  obsolete_time:  nil,
                  modify_time:    nil }
         # check for existing entry
-        existing = oldlist.where(hash)
-        Rails.logger.info "ERROR: multiple matches, cleaning up: #{existing.inspect}" if existing.count > 1
+        matching_binaries = oldlist.where(hash)
+        Rails.logger.info "ERROR: multiple matches, cleaning up: #{matching_binaries.inspect}" if matching_binaries.exists?
         # double definition means broken DB entries
-        existing.offset(1).destroy_all
+        matching_binaries.offset(1).destroy_all
 
         # compare with existing entry
-        if existing.count == 1
-          entry = existing.first
+        entry = matching_binaries.first
+
+        if entry
           if entry.indentical_to?(binary)
             # same binary, don't touch
             processed_item[entry.id] = true

commit 78db2b94ea6651c9eb18ca1a012523543a37d214

[api] avoid invalid corrupt database message
one match is okay, multiple are not (regression from 9c0bc2cce4)

src/api/app/models/binary_release.rb
@@ -48,9 +48,11 @@ class BinaryRelease < ApplicationRecord
                  modify_time:    nil }
         # check for existing entry
         matching_binaries = oldlist.where(hash)
-        Rails.logger.info "ERROR: multiple matches, cleaning up: #{matching_binaries.inspect}" if matching_binaries.exists?
-        # double definition means broken DB entries
-        matching_binaries.offset(1).destroy_all
+        if matching_binaries.count > 1
+          Rails.logger.info "ERROR: multiple matches, cleaning up: #{matching_binaries.inspect}"
+          # double definition means broken DB entries
+          matching_binaries.offset(1).destroy_all
+        end
 
         # compare with existing entry
         entry = matching_binaries.first