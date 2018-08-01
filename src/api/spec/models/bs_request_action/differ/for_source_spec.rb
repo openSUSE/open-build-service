fcb4edf406d1dd345acaa8b22d4955a9e10e5aac
Stephan Kulow <coolo@suse.de>
2018-08-01 19:35:47 +0200

    Request differ: Fix one more test case that is invalid without cassettes
    The cassettes only contained a 404 for a package that the
    test didn't create due to missing global_write_through

--- src/api/spec/models/bs_request_action/differ/for_source_spec.rb
+++ src/api/spec/models/bs_request_action/differ/for_source_spec.rb
@@ -71,7 +71,13 @@ RSpec.describe BsRequestAction::Differ::ForSource, vcr: true do
     end
 
     context 'with error' do
-      it { expect { subject.perform }.to raise_error(BsRequestAction::DiffError) }
+      let(:path) { "#{CONFIG['source_url']}/source/#{source_project}/#{source_package}" }
+      let(:no_such_revision) { '<status code="404"><summary>no such revision</summary><details>404 no such revision</details></status>' }
+      before do
+        stub_request(:post, path).with(query: hash_including('cmd' => 'diff', 'opackage' => target_package)).and_return(body: no_such_revision, status: 404)
+      end
+
+      it { expect { subject.perform }.to raise_error(BsRequestAction::DiffError, %r{The diff call for source_project/source_package failed: no such revision}) }
     end
 
     context 'with superseded request' do
