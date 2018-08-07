1659569528495bbc815c70be006510a62f36c8e0
Björn Geuken <bgeuken@suse.de>
2018-08-07 16:58:12 +0200

    Add test for validations of announcement model

--- src/api/spec/models/announcement_spec.rb
+++ src/api/spec/models/announcement_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Announcement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_presence_of :content }
 end
