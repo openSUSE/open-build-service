91263398a5cead4cbb0423db1e506d133c650b59
Christian Bruckmayer <cbruckmayer@suse.com>
2018-08-07 10:31:47 +0200

    [api][webui] Add validations for Status models
    Added presence and inclusion validations to Status::Check and Status::RepositoryPublish
    models.

--- src/api/spec/models/status/check_spec.rb
+++ src/api/spec/models/status/check_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Status::Check, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    validates :state, :name, :checkable, presence: true
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:checkable) }
+
+    should validate_inclusion_of(:state).in_array(%w(pending error failure success))
+  end
 end
--- src/api/spec/models/status/repository_publish_spec.rb
+++ src/api/spec/models/status/repository_publish_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Status::RepositoryPublish, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    validates :state, :name, :checkable, presence: true
+    it { should validate_presence_of(:build_id) }
+    it { should validate_presence_of(:repository) }
+  end
 end
