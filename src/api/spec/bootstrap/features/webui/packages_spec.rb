a51c69cd3ff4ac47d6bc99dac948eb143fb3ae8f
Moises Deniz Aleman <mdeniz@suse.com>
2018-08-14 10:39:10 +0200

    Package specs for Bootstrap

---
+++ src/api/spec/bootstrap/features/webui/packages_spec.rb
@@ -0,0 +1,136 @@
+require 'browser_helper'
+require 'webmock/rspec'
+
+RSpec.feature 'Bootstrap_Packages', type: :feature, js: true, vcr: true do
+  it_behaves_like 'user tab' do
+    let(:package) do
+      create(:package, name: 'group_test_package',
+        project_id: user_tab_user.home_project.id)
+    end
+    let!(:maintainer_user_role) { create(:relationship, package: package, user: user_tab_user) }
+    let(:project_path) { package_show_path(project: user_tab_user.home_project, package: package) }
+  end
+
+  let!(:user) { create(:confirmed_user, login: 'package_test_user') }
+  let!(:package) { create(:package_with_file, name: 'test_package', project: user.home_project) }
+  let(:other_user) { create(:confirmed_user, login: 'other_package_test_user') }
+  let!(:other_users_package) { create(:package_with_file, name: 'branch_test_package', project: other_user.home_project) }
+  let(:package_with_develpackage) { create(:package, name: 'develpackage', project: user.home_project, develpackage: other_users_package) }
+  let(:third_project) { create(:project_with_package, package_name: 'develpackage') }
+
+  describe 'branching a package from another users project' do
+    before do
+      allow(Configuration).to receive(:cleanup_after_days).and_return(14)
+      login user
+      visit package_show_path(project: other_user.home_project, package: other_users_package)
+      click_link('Branch package')
+    end
+
+    scenario 'with AutoCleanup' do
+      click_button('Accept')
+
+      expect(page).to have_text('Successfully branched package')
+      expect(page).to have_current_path(
+        package_show_path(project: user.branch_project_name(other_user.home_project_name), package: other_users_package)
+      )
+      visit index_attribs_path(project: user.branch_project_name(other_user.home_project_name))
+      expect(page).to have_text('OBS:AutoCleanup')
+    end
+
+    scenario 'without AutoCleanup' do
+      find('summary').click
+      find('label[for=disable-autocleanup]').click
+      click_button('Accept')
+
+      expect(page).to have_text('Successfully branched package')
+      expect(page).to have_current_path(
+        package_show_path(project: user.branch_project_name(other_user.home_project_name), package: other_users_package)
+      )
+      visit index_attribs_path(project: user.branch_project_name(other_user.home_project_name))
+      expect(page).to have_text('No attributes set')
+    end
+  end
+
+  scenario 'deleting a package' do
+    login user
+    visit package_show_path(package: package, project: user.home_project)
+    click_link('delete-package')
+
+    expect(find('#del-dialog')).to have_text('Do you really want to delete this package?')
+    click_button('Accept')
+
+    expect(find('#flash-messages')).to have_text('Package was successfully removed.')
+  end
+
+  scenario 'requesting package deletion' do
+    login user
+
+    visit package_show_path(package: other_users_package, project: other_user.home_project)
+    click_link('Request deletion')
+
+    expect(page).to have_text('Do you really want to request the deletion of package ')
+    click_button('Accept')
+
+    expect(page).to have_text('Created delete request')
+    find('a', text: /delete request \d+/).click
+    expect(page).to have_current_path(/\/request\/show\/\d+/)
+  end
+
+  scenario "changing the package's devel project" do
+    login user
+
+    visit package_show_path(package: package_with_develpackage, project: user.home_project)
+
+    click_link('Request devel project change')
+
+    fill_in 'devel_project', with: third_project.name
+    fill_in 'description', with: 'Hey, why not?'
+    click_button 'Accept'
+
+    find('#flash-messages', visible: false)
+    request = BsRequest.where(description: 'Hey, why not?', creator: user.login, state: 'review')
+    expect(request).to exist
+    expect(page).to have_current_path("/request/show/#{request.first.number}")
+    expect(page).to have_text(/Created by\s+#{user.login}/)
+    expect(page).to have_text('In state review')
+    expect(page).to have_text("Set the devel project to package #{third_project.name} / develpackage for package #{user.home_project} / develpackage")
+  end
+
+  context 'log' do
+    let(:repository) { create(:repository, name: 'package_test_repository', project: user.home_project, architectures: ['i586']) }
+
+    before do
+      login(user)
+      stub_request(:get, "#{CONFIG['source_url']}/build/#{user.home_project}/#{repository.name}/i586/#{package}/_log?end=1&nostream=1&start=0")
+        .and_return(body: '[1] this is my dummy logfile -> ümlaut')
+      result = %(<resultlist state="8da2ae1e32481175f43dc30b811ad9b5">
+                              <result project="#{user.home_project}" repository="#{repository.name}" arch="i586" code="published" state="published">
+                                <status package="#{package}" code="succeeded" />
+                              </result>
+                            </resultlist>
+                            )
+      result_path = "#{CONFIG['source_url']}/build/#{user.home_project}/_result?"
+      stub_request(:get, result_path + 'view=status&package=test_package&multibuild=1&locallink=1')
+        .and_return(body: result)
+      stub_request(:get, result_path + "arch=i586&package=#{package}&repository=#{repository.name}&view=status")
+        .and_return(body: result)
+      stub_request(:get, "#{CONFIG['source_url']}/build/#{user.home_project}/#{repository.name}/i586/#{package}/_log?view=entry")
+        .and_return(headers: { 'Content-Type' => 'text/plain' }, body: '<directory><entry size="1"/></directory>')
+      stub_request(:get, "#{CONFIG['source_url']}/build/#{user.home_project}/#{repository.name}/i586/#{package}/_log")
+        .and_return(headers: { 'Content-Type' => 'text/plain' }, body: '[1] this is my dummy logfile -> ümlaut')
+      path = "#{CONFIG['source_url']}/build/#{user.home_project}/#{repository.name}/i586/_builddepinfo?package=#{package}&view=revpkgnames"
+      stub_request(:get, path).and_return(body: '<builddepinfo />')
+    end
+
+    scenario 'download logfile succesfully' do
+      visit package_show_path(project: user.home_project, package: package)
+      # test reload and wait for the build to finish
+      find('#build-reload').click
+      find('.buildstatus a', text: 'succeeded').click
+      expect(page).to have_text('[1] this is my dummy logfile -> ümlaut')
+      first(:link, 'Download logfile').click
+      # don't bother with the umlaut
+      expect(page.source).to have_text('[1] this is my dummy logfile')
+    end
+  end
+end
