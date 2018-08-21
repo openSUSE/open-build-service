849a08dd5f80dbcc4f91e0efcb48fbf4e7ca2b2d
Saray Cabrera Padrón <scabrerapadron@suse.de>
2018-08-21 14:36:53 +0200

    Prevent package view failure due to incorrect link to remote
    Missing data don't allow us to build links to remote (detected on an old project).
    Fixes #5670
    
    Co-authored-by: David Kang <dkang@suse.com>

--- src/api/app/controllers/webui/package_controller.rb
+++ src/api/app/controllers/webui/package_controller.rb
@@ -359,6 +359,8 @@ class Webui::PackageController < Webui::WebuiController
 
   def set_remote_linkinfo
     linkinfo = @package.linkinfo
+
+    return unless linkinfo['package'] && linkinfo['project']
     return unless Package.exists_on_backend?(linkinfo['package'], linkinfo['project'])
 
     @linkinfo = { remote_project: linkinfo['project'], package: linkinfo['package'] }
