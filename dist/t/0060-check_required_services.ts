0aa0ae819941a8aa32f24ce4eadba0bf583eaed8
Eduardo Navarro <enavarro@suse.com>
2018-08-23 14:43:43 +0200

    Adapt test for working with targets
    Introduce obs-api-support.target unit to be checked by the tests.
    
    Adapt the test to work with both systemd types of unit: services and
    targets.

--- dist/t/0060-check_required_services.ts
+++ dist/t/0060-check_required_services.ts
@@ -6,21 +6,20 @@ use Test::More 'tests' => 16;
 
 my $max_wait = 300;
 
-my @daemons = qw/obsapidelayed  obsdispatcher  	obspublisher  	obsrepserver
-		 obsscheduler  	obssrcserver	apache2  	mysql/;
+my @daemons = qw/obs-api-support.target obsdispatcher.service 	obspublisher.service  	obsrepserver.service
+		 obsscheduler.service  	obssrcserver.service	apache2.service  	mysql.service/;
 
 foreach my $srv (@daemons) {
-	my @state=`systemctl is-enabled $srv\.service 2>/dev/null`;
+	my @state=`systemctl is-enabled $srv 2>/dev/null`;
 	chomp($state[-1]);
-	is($state[-1],"enabled","Checking if recommended service $srv is enabled");
+	is($state[-1],"enabled","Checking if recommended systemd unit $srv is enabled");
 }
 
 my %srv_state=();
 
 while ($max_wait > 0) {
-	
 	foreach my $srv (@daemons) {
-		my @state=`systemctl is-active $srv\.service 2>/dev/null`;
+		my @state=`systemctl is-active $srv 2>/dev/null`;
 		chomp($state[0]);
 		if ( $state[0] eq 'active') {
 			$srv_state{$srv} = 'active';
@@ -34,7 +33,7 @@ while ($max_wait > 0) {
 }
 
 foreach my $srv ( @daemons ) {
-	is($srv_state{$srv} || 'timeout','active',"Checking recommended service '$srv' status");
+	is($srv_state{$srv} || 'timeout','active',"Checking recommended systemd unit '$srv' status");
 }
 
 
