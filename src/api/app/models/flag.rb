6e7256d08f29f9297c9dbbed4e23857a682ee400
Amr Abdelwahab <amr.abdel-wahab@20180320-001.local>
2018-08-26 00:29:19 +0200

    Flip the condition in flag#validate_custom_save
    * I noticed the issue during @Ana06 talk at #EuRuKo18 and decided to open a minor pr for it :)

--- src/api/app/models/flag.rb
+++ src/api/app/models/flag.rb
@@ -19,7 +19,7 @@ class Flag < ApplicationRecord
 
   validate :validate_custom_save
   def validate_custom_save
-    errors.add(:name, 'Please set either project or package') unless project.nil? ^ package.nil?
+    errors.add(:name, 'Please set either project or package') if project ^ package
     errors.add(:flag, 'There needs to be a valid flag') unless FlagHelper::TYPES.key?(flag)
     errors.add(:status, 'Status needs to be enable or disable') unless status && (status.to_sym == :enable || status.to_sym == :disable)
   end
