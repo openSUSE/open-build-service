6417e80b4d6f3901ae57027826fecf51a18eecd1
David Kang <dkang@suse.com>
2018-08-27 16:02:29 +0200

    Add some route helper
    We add routes helper for `change_devel_request` and `delete_request`.

--- src/api/config/routes.rb
+++ src/api/config/routes.rb
@@ -329,13 +329,13 @@ OBSApi::Application.routes.draw do
       get 'request/diff/:number' => :diff
       get 'request/list_small' => :list_small
       get 'request/delete_request_dialog' => :delete_request_dialog, as: 'request_delete_dialog'
-      post 'request/delete_request/:project' => :delete_request, constraints: cons
+      post 'request/delete_request/:project' => :delete_request, constraints: cons, as: 'delete_request'
       get 'request/add_role_request_dialog' => :add_role_request_dialog, as: 'request_add_role_dialog'
       post 'request/add_role_request/:project' => :add_role_request, constraints: cons
       get 'request/set_bugowner_request_dialog' => :set_bugowner_request_dialog
       post 'request/set_bugowner_request' => :set_bugowner_request
       get 'request/change_devel_request_dialog/:project/:package' => :change_devel_request_dialog, constraints: cons, as: 'request_change_devel_dialog'
-      post 'request/change_devel_request/:project/:package' => :change_devel_request, constraints: cons
+      post 'request/change_devel_request/:project/:package' => :change_devel_request, constraints: cons, as: 'change_devel_request'
       get 'request/set_incident_dialog' => :set_incident_dialog
       post 'request/set_incident' => :set_incident
     end
