b5654ddf72246d6317c0c0dca10e70dfaed0d163
David Kang <dkang@suse.com>
2018-08-29 14:03:43 +0200

    Add FIXME's
    Co-authored-by: Saray Cabrera Padrón <scabrerapadron@suse.de>

--- src/api/app/controllers/webui2/request_controller.rb
+++ src/api/app/controllers/webui2/request_controller.rb
@@ -1,5 +1,6 @@
 module Webui2::RequestController
   def webui2_change_devel_request_dialog
+    # FIXME: don't use render_dialog
     render_dialog(nil, project: @project, package: @package, current_devel_project: @current_devel_project,
                   current_devel_package: @current_devel_package)
   end
