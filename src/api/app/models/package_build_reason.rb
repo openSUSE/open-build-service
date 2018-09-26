6d69c7b28901a23894e25bfb412e669542a6b067
Christian Bruckmayer <cbruckmayer@suse.com>
2018-09-26 17:01:33 +0200

    Implement time accessor in PackageBuildReason
    The backend response with the time as integer since epoch.
    This is not really useful and we always need to parse it
    to a Time object with Time.at. We changed now the default
    accessor to a custom accesor which does this conversion.

--- src/api/app/models/package_build_reason.rb
+++ src/api/app/models/package_build_reason.rb
@@ -2,5 +2,10 @@ class PackageBuildReason
   include ActiveModel::Model
 
   validates :explain, :time, presence: true
-  attr_accessor :explain, :time, :oldsource, :packagechange
+  attr_accessor :explain, :oldsource, :packagechange
+  attr_writer :time
+
+  def time
+    Time.at(@time.to_i)
+  end
 end
