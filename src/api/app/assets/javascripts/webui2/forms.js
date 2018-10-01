263db63a9fc09fc779c1ff3a6fd97441eff6ef0f
Christian Bruckmayer <cbruckmayer@suse.com>
2018-10-01 15:11:53 +0200

    Remove path from file input
    Most modern browser don't allow JS to access
    the full path of the file and show e.g. C:\fakepath.
    As this is confusing for our users, we only show the
    filename and strip the path instead.

--- src/api/app/assets/javascripts/webui2/forms.js
+++ src/api/app/assets/javascripts/webui2/forms.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
   $('.custom-file-input').on('change',function() {
       //get the file name
       var fileName = $(this).val();
+      // Most modern browser don't allow JS to access
+      // the full path of the file and show e.g. C:\fakepath
+      // As this is confusing we only show the filename and strip the path
+      fileName = fileName.replace(/^.*[\\\/]/, '');
       //replace the "Choose a file" label
       $(this).next('.custom-file-label').html(fileName);
   });
