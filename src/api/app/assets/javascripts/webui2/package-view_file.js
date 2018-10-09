47f3b52baa7abb4fdc94e972fbc4096b74676cd2
Ana María Martínez Gómez <ammartinez@suse.de>
2018-10-09 11:12:14 +0200

    Refactor package-view_file.js day
    Do not convert day to string to be able to add a leading space and use
    `slice` instead, similarly as it is done with times.

--- src/api/app/assets/javascripts/webui2/package-view_file.js
+++ src/api/app/assets/javascripts/webui2/package-view_file.js
@@ -2,20 +2,18 @@
 var DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 var MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-var toTwoDigits = function(number) {
-  return ('0' + number).slice(-2);
+var toTwoChars = function(number, leadingChar) {
+  return (leadingChar + number).slice(-2);
 };
 
 // The jshint ignore:line is needed to ignore the offense: `'addChangesEntryTemplate' is defined but never used.` This is used in the view directly
 function addChangesEntryTemplate() { // jshint ignore:line
   var date = new Date(),
-      day = date.getUTCDate().toString(),
-      hours = toTwoDigits(date.getUTCHours()),
-      minutes = toTwoDigits(date.getUTCMinutes()),
-      seconds = toTwoDigits(date.getUTCSeconds()),
+      day = toTwoChars(date.getUTCDate(), ' '),
+      hours = toTwoChars(date.getUTCHours(), '0'),
+      minutes = toTwoChars(date.getUTCMinutes(), '0'),
+      seconds = toTwoChars(date.getUTCSeconds(), '0'),
       template;
-
-  if (day.length === 1) { day = " " + day; } // Pad single character day value
 
   template = "-------------------------------------------------------------------\n" +
     DAYS[date.getUTCDay()] + " " + MONTHS[date.getUTCMonth()] + " " + day + " " +
