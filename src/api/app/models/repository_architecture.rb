bb10b6d6bd35bd68f7fdbbdb36917677e06608da
Stephan Kulow <coolo@suse.de>
2018-10-25 17:50:42 +0200

    Allow status checks to be attached to built repositories
    I left required check controller alone as it needs
    a little more work as it's currently has required checks
    for /published in /repositories - which collides with
    what I want to achieve here

--- src/api/app/models/repository_architecture.rb
+++ src/api/app/models/repository_architecture.rb
@@ -1,4 +1,5 @@
 class RepositoryArchitecture < ApplicationRecord
+  serialize :required_checks, Array
   belongs_to :repository,   inverse_of: :repository_architectures
   belongs_to :architecture, inverse_of: :repository_architectures
 
@@ -6,6 +7,16 @@ class RepositoryArchitecture < ApplicationRecord
 
   validates :repository, :architecture, :position, presence: true
   validates :repository, uniqueness: { scope: :architecture }
+
+  has_many :status_reports, as: :checkable, class_name: 'Status::Report', dependent: :destroy do
+    def for_uuid(uuid)
+      where(status_reports: { uuid: uuid })
+    end
+
+    def latest
+      for_uuid(proxy_association.owner.build_id)
+    end
+  end
 end
 
 # == Schema Information
