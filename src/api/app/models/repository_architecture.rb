9bda073b8f6ab1cfee3b98f8e618f31116ad3ea5
Stephan Kulow <coolo@suse.de>
2018-10-25 18:27:33 +0200

    Add a build_id function to RepositoryArchitecture
    This is a direct mapping to view=status in the backend

--- src/api/app/models/repository_architecture.rb
+++ src/api/app/models/repository_architecture.rb
@@ -8,14 +8,10 @@ class RepositoryArchitecture < ApplicationRecord
   validates :repository, :architecture, :position, presence: true
   validates :repository, uniqueness: { scope: :architecture }
 
-  has_many :status_reports, as: :checkable, class_name: 'Status::Report', dependent: :destroy do
-    def for_uuid(uuid)
-      where(status_reports: { uuid: uuid })
-    end
+  has_many :status_reports, as: :checkable, class_name: 'Status::Report', dependent: :destroy
 
-    def latest
-      for_uuid(proxy_association.owner.build_id)
-    end
+  def build_id
+    Backend::Api::Build::Repository.build_id(repository.project.name, repository.name, architecture.name)
   end
 end
 
