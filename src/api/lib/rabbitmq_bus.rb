2662760fff670aa610bf4708c1840751e604f33c
Stephan Kulow <coolo@suse.de>
2018-11-04 09:55:34 +0100

    Cover sending events to rabbitmq
    I was mildly annoyed that I couldn't cover the new events to
    be sent to rabbitmq, so I did a little research on how to test
    bunny. It's not so easy, but good enough to mock

--- src/api/lib/rabbitmq_bus.rb
+++ src/api/lib/rabbitmq_bus.rb
@@ -1,4 +1,6 @@
 class RabbitmqBus
+  cattr_accessor :connection, :exchange
+
   def self.send_to_bus(channel, data)
     publish("#{Configuration.amqp_namespace}.#{channel}", data)
   rescue => e
@@ -9,21 +11,30 @@ class RabbitmqBus
   def self.publish(event_routing_key, event_payload)
     return unless CONFIG['amqp_options']
     start_connection
-
-    $rabbitmq_exchange.publish(event_payload, routing_key: event_routing_key)
+    wrapped_exchange.publish(event_payload, routing_key: event_routing_key)
   end
 
   # Start one connection, channel and exchange per rails process
   # and reuse them
-  def self.start_connection
-    $rabbitmq_conn ||= Bunny.new(CONFIG['amqp_options'].try(:symbolize_keys))
-    $rabbitmq_conn.start
-    $rabbitmq_channel ||= $rabbitmq_conn.create_channel
-    $rabbitmq_exchange = if CONFIG['amqp_exchange_name']
-      $rabbitmq_channel.exchange(CONFIG['amqp_exchange_name'], CONFIG['amqp_exchange_options'].try(:symbolize_keys) || {})
+  def self.wrapped_exchange
+    return exchange if exchange
+    connection.start
+    rabbitmq_channel = connection.create_channel
+    if CONFIG['amqp_exchange_name']
+      self.exchange = rabbitmq_channel.exchange(CONFIG['amqp_exchange_name'], CONFIG['amqp_exchange_options'].try(:symbolize_keys) || {})
     else
-      $rabbitmq_channel.default_exchange
+      # can't cover due to https://github.com/arempe93/bunny-mock/pull/25
+      #:nocov:
+      self.exchange = rabbitmq_channel.default_exchange
+      #:nocov:
     end
+  end
+
+  # this function is skipped in tests by putting a BunnyMock in self.connection
+  def self.start_connection
+    #:nocov:
+    self.connection ||= Bunny.new(CONFIG['amqp_options'].try(:symbolize_keys))
+    #:nocov:
   end
   private_class_method :start_connection
 end
