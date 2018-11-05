e54a9a6c4f2aa988ee7ca649ae6c43ef3db26041
Stephan Kulow <coolo@suse.de>
2018-11-05 07:24:06 +0100

    Avoid a cassette trick in worker status

--- src/api/spec/controllers/worker/status_controller_spec.rb
+++ src/api/spec/controllers/worker/status_controller_spec.rb
@@ -10,9 +10,17 @@ RSpec.describe Worker::StatusController, vcr: true do
   end
 
   describe 'GET /index' do
+    let(:response) { file_fixture('worker_status_response.xml') }
+
+    before do
+      stub_request(:get, CONFIG['source_url'] + '/build/_workerstatus').and_return(body: response)
+    end
+
     subject! { get :index, params: { format: :xml } }
 
     it { is_expected.to have_http_status(:success) }
-    it { assert_select 'workerstatus[clients=2]' }
+    it 'finds 2 workers' do
+      assert_select 'workerstatus[clients=2]'
+    end
   end
 end
