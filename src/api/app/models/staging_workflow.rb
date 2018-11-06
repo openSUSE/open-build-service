9f062390ef23f5161b37f74e79c695ecd8e626a2
Ana María Martínez Gómez <ammartinez@suse.de>
2018-11-06 16:05:31 +0100

    Store Staging Projects in the Backend
    Staging projects were created but not stored in the Backend.

--- src/api/app/models/staging_workflow.rb
+++ src/api/app/models/staging_workflow.rb
@@ -1,6 +1,6 @@
 class StagingWorkflow < ApplicationRecord
   belongs_to :project, inverse_of: :staging
-  has_many :staging_projects, class_name: 'Project', inverse_of: :staging_workflow, dependent: :nullify, autosave: true do
+  has_many :staging_projects, class_name: 'Project', inverse_of: :staging_workflow, dependent: :nullify do
     def without_staged_requests
       includes(:staged_requests).where(bs_requests: { id: nil })
     end
@@ -8,7 +8,7 @@ class StagingWorkflow < ApplicationRecord
   has_many :target_of_bs_requests, through: :project
   has_many :staged_requests, class_name: 'BsRequest', through: :staging_projects
 
-  after_initialize :init_staging_projects
+  after_create :create_staging_projects
 
   def unassigned_requests
     target_of_bs_requests.in_states(['new', 'review']) - staged_requests - ignored_requests
@@ -20,10 +20,12 @@ class StagingWorkflow < ApplicationRecord
 
   private
 
-  def init_staging_projects
-    return unless new_record?
+  def create_staging_projects
     ['A', 'B'].each do |letter|
-      staging_projects << Project.find_or_initialize_by(name: "#{project.name}:Staging:#{letter}")
+      staging_project = Project.find_or_initialize_by(name: "#{project.name}:Staging:#{letter}")
+      next if staging_project.staging_workflow # if it belongs to another staging workflow skip it
+      staging_project.staging_workflow = self
+      staging_project.store
     end
   end
 end
