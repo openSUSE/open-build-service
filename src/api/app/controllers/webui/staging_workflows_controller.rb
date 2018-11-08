38020a47595a44171d7c41440256c860dbf8fb17
Moises Deniz Aleman <mdeniz@suse.com>
2018-11-08 15:46:12 +0100

    Fix a wrongly merge in StagingWorkflowController
    Some of the instance variables calculated in the show ended up in the
    edit LoL
    
    Co-authored-by: David Kang <dkang@suse.com>

--- src/api/app/controllers/webui/staging_workflows_controller.rb
+++ src/api/app/controllers/webui/staging_workflows_controller.rb
@@ -32,12 +32,6 @@ class Webui::StagingWorkflowsController < Webui::WebuiController
 
   def show
     @project = @staging_workflow.project
-  end
-
-  def edit
-    authorize @staging_workflow
-
-    @project = @staging_workflow.project
     @unassigned_requests = @staging_workflow.unassigned_requests.first(5)
     @more_unassigned_requests = @staging_workflow.unassigned_requests.count - @unassigned_requests.size
     @ready_requests = @staging_workflow.ready_requests.first(5)
@@ -45,6 +39,12 @@ class Webui::StagingWorkflowsController < Webui::WebuiController
     @ignored_requests = @staging_workflow.ignored_requests.first(5)
     @more_ignored_requests = @staging_workflow.ignored_requests.count - @ignored_requests.size
     @empty_projects = @staging_workflow.staging_projects.without_staged_requests
+  end
+
+  def edit
+    authorize @staging_workflow
+
+    @project = @staging_workflow.project
   end
 
   private
