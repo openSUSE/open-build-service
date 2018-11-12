61ecdd9220a3c6f1399cb431ca1ea37457898f2a
Frank Schreiner <schreiner@suse.de>
2018-11-12 08:49:28 +0000

    [ci] improve OBS::Test::Util::get_package_version
    remove everthing from version string which is not digit or dot

--- dist/t/lib/OBS/Test/Utils.pm
+++ dist/t/lib/OBS/Test/Utils.pm
@@ -5,7 +5,10 @@ use warnings;
 sub get_package_version {
   my ($pkg, $num) = @_;
   my @pkg_ver = `rpm -q --qf '%{version}' $pkg`;
-  my @ver_raw = split(/\./,$pkg_ver[0]);
+  my $raw_ver = $pkg_ver[0];
+  chomp $raw_ver;
+  $raw_ver =~ s/^([\d\.]*).*/$1/;
+  my @ver_raw = split(/\./, $raw_ver);
   my @ver = splice(@ver_raw, 0, $num);
   return join('.',@ver);
 }
