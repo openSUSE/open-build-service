openSUSE/open-build-service

commit e9f6f907bc6af777fe244745baf3d5067c17ca4d

[api] fix container content tracking

src/api/app/models/binary_release.rb
@@ -40,7 +40,7 @@ class BinaryRelease < ApplicationRecord
         # identifier
         hash = { binary_name:    binary['name'],
                  binary_version: binary['version'],
-                 binary_release: binary['release'],
+                 binary_release: binary['release'] || 0, # container have no tracked release number atm
                  binary_epoch:   binary['epoch'],
                  binary_arch:    binary['binaryarch'],
                  medium:         binary['medium'],
@@ -187,7 +187,6 @@ class BinaryRelease < ApplicationRecord
   def indentical_to?(binary_hash)
     binary_disturl == binary_hash['disturl'] &&
       binary_supportstatus == binary_hash['supportstatus'] &&
-      binary_buildtime &&
       binary_buildtime == DateTime.strptime(binary_hash['buildtime'].to_s, '%s')
   end
   #### Alias of methods

commit 0fa9e854fe7a5ae6faae08c96fcf9ab36a81eaf8

Avoid calling strptime with an empty string
But still return `false` if both build times are nil.

This was caused by:
https://github.com/openSUSE/open-build-service/commit/e9f6f907bc6af777fe244745baf3d5067c17ca4d#diff-49563098f1992cafd3f7c8195f53a383

Although the code was wrong at that point.

Fixes https://github.com/openSUSE/open-build-service/issues/6240

src/api/app/models/binary_release.rb
@@ -202,9 +202,11 @@ class BinaryRelease < ApplicationRecord
   end
 
   def indentical_to?(binary_hash)
+    time = Time.strptime(binary_hash['buildtime'].to_s, '%s') if binary_hash['buildtime'].present?
+
     binary_disturl == binary_hash['disturl'] &&
       binary_supportstatus == binary_hash['supportstatus'] &&
-      binary_buildtime == Time.strptime(binary_hash['buildtime'].to_s, '%s')
+      binary_buildtime == time
   end
   #### Alias of methods
 end