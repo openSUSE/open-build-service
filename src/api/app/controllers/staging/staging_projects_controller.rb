346287a737868f54951861fecd09eebbb5a0f458
Ana María Martínez Gómez <ammartinez@suse.de>
2018-11-22 09:56:02 +0100

    Use Project#get_by_name instead of #find_by in Staging
    Staging::StagingProject is a subclass of Project. `get_by_name` should
    be used always that a project is required instead of `find_by!` as it
    considers more cases where an exception should be raised.

--- src/api/app/controllers/staging/staging_projects_controller.rb
+++ src/api/app/controllers/staging/staging_projects_controller.rb
@@ -2,6 +2,6 @@ class Staging::StagingProjectsController < ApplicationController
   skip_before_action :require_login
 
   def show
-    @staging_project = Staging::StagingProject.find_by!(name: params[:name])
+    @staging_project = Staging::StagingProject.get_by_name(params[:name])
   end
 end
