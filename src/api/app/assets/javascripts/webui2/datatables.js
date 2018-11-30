b86a1ee86af353c42ce7acfc2b31eafe80634e9c
Ana María Martínez Gómez <ammartinez@suse.de>
2018-11-30 13:25:10 +0100

    Generalize DataTable inizalization so that it can be reused
    This help us fixing the bug with input search field not being
    responsible in the whole application. It also helps us being consistent.

--- src/api/app/assets/javascripts/webui2/datatables.js
+++ src/api/app/assets/javascripts/webui2/datatables.js
@@ -2,3 +2,12 @@
 //= require datatables/dataTables.bootstrap4
 //= require datatables/extensions/Responsive/dataTables.responsive
 //= require datatables/extensions/Responsive/responsive.bootstrap4
+
+function initializeDataTable(cssSelector, params){ // jshint ignore:line
+    var defaultParams = {
+        language: { search: '', searchPlaceholder: "Search..." },
+    };
+    var newParams = $.extend(defaultParams, params);
+    $(cssSelector).DataTable(newParams);
+}
+
