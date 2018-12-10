d37bfd650febac4f9baa6b1c8351c9453001977c
Christian Bruckmayer <cbruckmayer@suse.com>
2018-12-10 14:16:54 +0100

    Remove horizontal scrolling from project table
    because it is an overview page, adding scrolling
    did not make much sense here.

--- src/api/app/assets/javascripts/webui2/project_monitor.js
+++ src/api/app/assets/javascripts/webui2/project_monitor.js
@@ -1,7 +1,6 @@
 function setupProjectMonitor() { // jshint ignore:line
   initializeDataTable('#project-monitor-table', { // jshint ignore:line
     scrollX: true,
-    scrollY: "50vh",
     fixedColumns: true,
     pageLength: 50,
     lengthMenu: [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
