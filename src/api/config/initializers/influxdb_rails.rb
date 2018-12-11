343f91956e0eadfcb245404c0614b3fbb2628c4d
Christian Bruckmayer <cbruckmayer@suse.com>
2018-12-11 13:49:05 +0100

    Do not initialize InfluxDB when there is no host
    The empty array also defaults to localhost.
    
    Fix #6556.

--- src/api/config/initializers/influxdb_rails.rb
+++ src/api/config/initializers/influxdb_rails.rb
@@ -1,8 +1,10 @@
+return if CONFIG['influxdb_hosts'].blank? # defaults to localhost otherwise
+
 InfluxDB::Rails.configure do |config|
   config.influxdb_database   = CONFIG['influxdb_database']
   config.influxdb_username   = CONFIG['influxdb_username']
   config.influxdb_password   = CONFIG['influxdb_password']
-  config.influxdb_hosts      = CONFIG['influxdb_hosts'] || [] # default is otherwise localhost
+  config.influxdb_hosts      = CONFIG['influxdb_hosts']
   config.influxdb_port       = CONFIG['influxdb_port']
   config.retry               = CONFIG['influxdb_retry']
   config.use_ssl             = CONFIG['influxdb_ssl']
