0c245958e7ef45490d52fbc3401df1177962f41a
Christian Bruckmayer <cbruckmayer@suse.com>
2018-12-11 13:49:45 +0100

    Set influxdb retry default to false
    otherwise it can end up in an infinite loop.

--- src/api/config/initializers/influxdb_rails.rb
+++ src/api/config/initializers/influxdb_rails.rb
@@ -6,7 +6,7 @@ InfluxDB::Rails.configure do |config|
   config.influxdb_password   = CONFIG['influxdb_password']
   config.influxdb_hosts      = CONFIG['influxdb_hosts']
   config.influxdb_port       = CONFIG['influxdb_port']
-  config.retry               = CONFIG['influxdb_retry']
+  config.retry               = CONFIG['influxdb_retry'] || false # default is infinite otherwise
   config.use_ssl             = CONFIG['influxdb_ssl']
   config.time_precision      = CONFIG['influxdb_time_precision']
   config.series_name_for_sql = 'rails.sql'
