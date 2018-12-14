be95e084321842be715ffbfb8d637e7ce7369abd
Stephan Kulow <coolo@suse.de>
2018-12-14 14:56:26 +0100

    Move the flag calculation into a model
    To make the webui code we did for repositories controller
    also useful for the API to avoid having 2 ways for the same

--- src/api/app/models/flag.rb
+++ src/api/app/models/flag.rb
@@ -40,14 +40,7 @@ class Flag < ApplicationRecord
   end
 
   def self.default_status(flag_name)
-    case flag_name.to_sym
-    when :lock, :debuginfo
-      'disable'
-    when :build, :publish, :useforbuild, :binarydownload, :access
-      'enable'
-    else
-      'disable'
-    end
+    FlagHelper.default_for(flag_name)
   end
 
   def discard_forbidden_project_cache
@@ -121,20 +114,6 @@ class Flag < ApplicationRecord
     builder.send(status.to_s, options)
   end
 
-  def is_explicit_for?(in_repo, in_arch)
-    return false unless is_relevant_for?(in_repo, in_arch)
-
-    arch = architecture ? architecture.name : nil
-
-    return false if arch.nil? && in_arch
-    return false if arch && in_arch.nil?
-
-    return false if repo.nil? && in_repo
-    return false if repo && in_repo.nil?
-
-    true
-  end
-
   # returns true when flag is relevant for the given repo/arch combination
   def is_relevant_for?(in_repo, in_arch)
     arch = architecture ? architecture.name : nil
@@ -153,13 +132,6 @@ class Flag < ApplicationRecord
     count += 2 if architecture
     count += 4 if repo
     count
-  end
-
-  def to_s
-    ret = status
-    ret += " arch=#{architecture.name}" unless architecture.nil?
-    ret += " repo=#{repo}" unless repo.nil?
-    ret
   end
 
   # TODO: used by bento. Remove when dropping old UI.
