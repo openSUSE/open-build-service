3ce8e076188f042600d6c659b24609fff04aae9f
Ana María Martínez Gómez <ammartinez@suse.de>
2018-12-17 17:38:45 +0100

    Improve efficience of PackageController#buildresults
    Pre calculate in the controller if a build result is outdated (not in database
    but still temporarily in backend) instead of in the view, generating and SQL
    query per result.
    
    MASTER
    real=47.93206156800443
    
    NEW CODE
    real=21.93289869299042
    
    Co-authored-by: Stephan Kulow <coolo@suse.de>

--- src/api/app/models/local_build_result.rb
+++ src/api/app/models/local_build_result.rb
@@ -1,4 +1,4 @@
 class LocalBuildResult
   include ActiveModel::Model
-  attr_accessor :repository, :architecture, :code, :state, :details, :summary
+  attr_accessor :repository, :architecture, :code, :state, :details, :summary, :is_repository_in_db
 end
