77c0b329269d8d6f2b558edaa8b9de5397179a98
Björn Geuken <bgeuken@suse.de>
2018-12-19 18:10:08 +0100

    Refactor autocompletion code for repository search

--- src/api/app/assets/javascripts/webui2/autocomplete.js
+++ src/api/app/assets/javascripts/webui2/autocomplete.js
@@ -13,6 +13,44 @@ $(document).ready(function() {
       }
     });
   });
+
+  $('.repository-autocomplete').on('autocompleteselect autocompletechange', function(event, ui) {
+    var projectName,
+        dropdown        = $(this).find('.repository-dropdown'),
+        repoNameElement = $(this).find('.repository-name');
+
+    // Get project name
+    if (event.type === 'autocompleteselect') {
+      projectName = ui.item.value;
+    } else {
+      projectName = $(this).find('.ui-autocomplete-input').val();
+    }
+
+    // Clear form
+    dropdown.html('').prop('disabled', true);
+    repoNameElement.val('');
+
+    if (projectName === '') return;
+
+    // Update dropdown
+    $.ajax({
+      url: dropdown.data('source'),
+      data: { project: projectName },
+      success: function (data) {
+        if(data.length === 0) {
+          dropdown.append(new Option('No repositories found'));
+        } else {
+          $.each(data, function (_, val) {
+            dropdown.append(new Option(val));
+          });
+
+          repoNameElement.val(projectName.replace(/:/g, '_') + '_' + data[0]);
+
+          dropdown.prop('disabled', false);
+        }
+      }
+    });
+  });
 });
 
 function autocomleteBranchPackageName(linkedPackageSource) { // jshint ignore:line
@@ -49,65 +87,4 @@ function autocompleteSpinner(spinnerId, searching) {
     icon.removeClass('d-none');
     icon.next().addClass('d-none');
   }
-}
-
-function suggestRepositoryName(id, projectName, repositoryName){
-  $(id + ' #repo_name').val(projectName.replace(/:/g, '_') + '_' + repositoryName);
-}
-
-function autocompleteRepositories(id, projectName) {
-  var repositoriesId = (id + ' #target_repo');
-
-  $(repositoriesId).html('');
-  $(id + ' #repo_name').val('');
-  $(repositoriesId).prop('disabled', true);
-
-  if (projectName === '') return;
-
-  $.ajax({
-    url: $(repositoriesId).data('ajaxurl'),
-    data: { project: projectName },
-    success: function (data) {
-      if(data.length === 0) {
-        $(repositoriesId).append(new Option('No repositories found'));
-      } else {
-      $.each(data, function (idx, val) {
-        $(repositoriesId).append(new Option(val));
-      });
-
-      suggestRepositoryName(id, projectName, data[0]);
-
-      $(repositoriesId).prop('disabled', false);
-      }
-    }
-  });
-}
-
-function repositoriesSetupAutocomplete(id) { // jshint ignore:line
-  var inputId = (id + ' #target_project');
-  var icon = $(id + ' .project-search-icon i:first-child');
-
-  $(inputId).autocomplete({
-    appendTo: (id + ' .modal-body'),
-    source: $(inputId).data('ajaxurl'),
-    minLength: 2,
-    select: function(event, ui) {
-      autocompleteRepositories(id, ui.item.value);
-    },
-    change: function() {
-      autocompleteRepositories(id, $(inputId).val());
-    },
-    search: function() {
-      icon.addClass('d-none');
-      icon.next().removeClass('d-none');
-    },
-    response: function() {
-      icon.removeClass('d-none');
-      icon.next().addClass('d-none');
-    }
-  });
-
-  $(id + ' #target_repo').change(function () {
-    suggestRepositoryName(id, $(inputId).val(), $(this).val());
-  });
 }
