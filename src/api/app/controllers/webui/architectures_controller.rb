82893df947b15d68037939eb9183aa777701696f
Victor Pereira <vpereira@suse.de>
2019-01-07 09:36:36 +0100

    Move code to service
    To reduce logic in the controller, move some update relate code
    from action bulk_update_availability to its own service

--- src/api/app/controllers/webui/architectures_controller.rb
+++ src/api/app/controllers/webui/architectures_controller.rb
@@ -7,17 +7,12 @@ class Webui::ArchitecturesController < Webui::WebuiController
   end
 
   def bulk_update_availability
-    all_valid = true
-    params[:archs].each do |name, value|
-      arch = Architecture.find_by(name: name)
-      arch.available = value
-      all_valid &&= arch.save
-    end
+    result = ::ArchitecturesControllerService::ArchitectureUpdater.new(params[:archs]).call
 
     ::Configuration.write_to_backend
 
     respond_to do |format|
-      if all_valid
+      if result.valid?
         format.html { redirect_to architectures_path, notice: 'Architectures successfully updated.' }
       else
         format.html { redirect_back(fallback_location: root_path, error: 'Not all architectures could be saved') }
