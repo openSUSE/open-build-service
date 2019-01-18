cba7191035f49fd3731a96d3d0aefb0a07314b3b
Lukas Krause <lkrause@suse.de>
2019-01-18 16:45:42 +0100

    Improve efficiency of review_progress
    missing_review use staged_requests when being calculated, so there is no need
    to check it again, which has an impact in performance.
    
    Co-authored-by: Ana María Martínez Gómez <ammartinez@suse.de>

--- src/api/app/helpers/webui/staging/workflow_helper.rb
+++ src/api/app/helpers/webui/staging/workflow_helper.rb
@@ -15,9 +15,8 @@ module Webui::Staging::WorkflowHelper
   end
 
   def review_progress(staging_project)
-    staged_requests_numbers = staging_project.staged_requests.pluck(:number)
     total = Review.where(bs_request: staging_project.staged_requests).size
-    missing = staging_project.missing_reviews.count { |missing_review| staged_requests_numbers.include?(missing_review[:request]) }
+    missing = staging_project.missing_reviews.size
 
     100 - missing * 100 / total
   end
