73f6a0b0c74712d6b3361304da5644f9f9e0ea0e
Björn Geuken <bgeuken@suse.de>
2019-02-03 15:08:23 +0100

    Add group icon to groups shown on the user tab
    Since recently (as part of the staging workflow changes) groups
    have an icon as well.

--- src/api/app/helpers/webui/user_or_groups_roles_helper.rb
+++ src/api/app/helpers/webui/user_or_groups_roles_helper.rb
@@ -1,4 +1,16 @@
 module Webui::UserOrGroupsRolesHelper
+  def display_name(object)
+    if object.is_a?(User) && object.realname.present?
+      "#{object.name} (#{object.login})"
+    else
+      object.name
+    end
+  end
+
+  def user_or_group_show_path(object)
+    object.is_a?(User) ? user_show_path(object) : group_show_path(object)
+  end
+
   def user_or_groups_roles_delete_path(project, type, object, package)
     if package
       package_remove_role_path(project: project, "#{type}id": object, package: package)
