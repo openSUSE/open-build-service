6ee2be8772edcf5aae488f7d70c536baf43e568f
Björn Geuken <bgeuken@suse.de>
2019-03-04 19:11:33 +0100

    Fix indentation users_groups.js

--- src/api/app/assets/javascripts/webui2/users_groups.js
+++ src/api/app/assets/javascripts/webui2/users_groups.js
@@ -14,49 +14,49 @@ function initializeUserConfigurationDatatable(ldapEnabled) { // jshint ignore:li
 }
 
 function changeUserRole(obj) { // jshint ignore:line
-    var type = obj.data("type");
-    var role = obj.data("role");
-    var spinner = obj.siblings('.fa-spinner');
+  var type = obj.data("type");
+  var role = obj.data("role");
+  var spinner = obj.siblings('.fa-spinner');
 
-    var url;
-    var data = {
-        project: $('#involved-users').data("project"), 
-        package: $('#involved-users').data("package"), 
-        role: role
-    };
-    data[type + 'id'] = obj.data(type);
-    if (obj.is(':checked')) {
-        url = $('#involved-users').data("save-" + type);
-    } else {
-        url = $('#involved-users').data("remove");
+  var url;
+  var data = {
+    project: $('#involved-users').data("project"),
+    package: $('#involved-users').data("package"),
+    role: role
+  };
+  data[type + 'id'] = obj.data(type);
+  if (obj.is(':checked')) {
+    url = $('#involved-users').data("save-" + type);
+  } else {
+    url = $('#involved-users').data("remove");
+  }
+
+  $.ajax({
+    url: url,
+    type: 'POST',
+    data: data,
+    beforeSend: function() {
+      spinner.removeClass('d-none');
+    },
+    complete: function() {
+      spinner.addClass('d-none');
     }
-
-    $.ajax({
-        url: url,
-        type: 'POST',
-        data: data,
-        beforeSend: function() {
-          spinner.removeClass('d-none');
-        },
-        complete: function() {
-          spinner.addClass('d-none');
-        }
-    });
+  });
 }
 
 function setDataTableForUsersAndGroups() { // jshint ignore:line
-    $('#user-table').dataTable({
-        responsive: true,
-        info: false,
-        paging: false,
-    });
+  $('#user-table').dataTable({
+    responsive: true,
+    info: false,
+    paging: false,
+  });
 
-    $('#group-table').dataTable({
-        responsive: true,
-        searching: false,
-        info: false,
-        paging: false
-    });
+  $('#group-table').dataTable({
+    responsive: true,
+    searching: false,
+    info: false,
+    paging: false
+  });
 }
 
 function initializeGroupTokenfield() { // jshint ignore:line
