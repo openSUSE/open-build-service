44c9f8bab09e589ea9c523a87c8adbe7b4795405
Victor Pereira <vpereira@suse.de>
2019-03-04 16:35:50 +0100

    Raise exception if TS guard file exists while running FullTextIndexJob
    From time to time, ThinkingSphinx aborts keeping the guard file there
    making impossible to rebuild the index. We want to have the exception on errbit
    
    Co-authored-by: Henne Vogelsang <hvogel@opensuse.org>

--- src/api/app/jobs/full_text_index_job.rb
+++ src/api/app/jobs/full_text_index_job.rb
@@ -1,9 +1,17 @@
+class ThinkingSphinx::GuardfileExistsError < StandardError
+  def message
+    "Guardfile #{File.join(ThinkingSphinx::Configuration.instance.indices_location, 'ts---all.tmp')} exists already"
+  end
+end
+
 class FullTextIndexJob < ApplicationJob
   queue_as :quick
 
   def perform
     return unless Rails.env.production?
-
+    # From time to time, ThinkingSphinx aborts keeping the guard file there
+    # making impossible to rebuild the index. We want to have the exception on errbit
+    raise ThinkingSphinx::GuardfileExistsError if ThinkingSphinx::Guard::File.new('--all').locked?
     # Ensure the connection
     ApplicationRecord.connection_pool.with_connection do |_|
       # Use the RakeInterface provided by ThinkingSphinx
