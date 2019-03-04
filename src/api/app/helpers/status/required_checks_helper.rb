18bed5ace7884c8a2fbd8e30cacc370acb11e344
Victor Pereira <vpereira@suse.de>
2019-03-04 16:52:14 +0100

    Improve ruby dialect on required_checks_helper.rb
    Use case/when instead of if/elsif/else

--- src/api/app/helpers/status/required_checks_helper.rb
+++ src/api/app/helpers/status/required_checks_helper.rb
@@ -1,10 +1,11 @@
 module Status::RequiredChecksHelper
   def build_header(project, checkable)
-    if checkable.is_a?(Repository)
+    case checkable
+    when Repository
       { project: project.name, repository: checkable.name }
-    elsif checkable.is_a?(Package)
+    when Package
       { project: project.name, package: checkable.name }
-    elsif checkable.is_a?(RepositoryArchitecture)
+    when RepositoryArchitecture
       build_header(project, checkable.repository).merge(architecture: checkable.architecture.name)
     else
       { project: checkable.name }
