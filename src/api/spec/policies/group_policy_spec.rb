2469509a60f2936bde1d019b6b421e4f86bc1b1e
Björn Geuken <bgeuken@suse.de>
2019-03-20 10:45:17 +0100

    Add tests for group policy

---
+++ src/api/spec/policies/group_policy_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe GroupPolicy do
+  let(:group) { create(:group) }
+  let(:user) { create(:confirmed_user) }
+  let(:admin) { create(:admin_user) }
+  let(:group_member) { create(:groups_user, group: group).user }
+  let(:group_maintainer) { create(:group_maintainer, group: group).user }
+
+  subject { GroupPolicy }
+
+  # rubocop:disable RSpec/RepeatedExample
+  # This cop is currently not recognizing the permissions block as separate test
+  permissions :create?, :index? do
+    it { is_expected.not_to permit(user, group) }
+    it { is_expected.not_to permit(group_member, group) }
+    it { is_expected.not_to permit(group_maintainer, group) }
+    it { is_expected.to permit(admin, group) }
+  end
+
+  permissions :update?, :destroy? do
+    it { is_expected.not_to permit(user, group) }
+    it { is_expected.not_to permit(group_member, group) }
+    it { is_expected.to permit(group_maintainer, group) }
+    it { is_expected.to permit(admin, group) }
+  end
+  # rubocop:enable RSpec/RepeatedExample
+end
