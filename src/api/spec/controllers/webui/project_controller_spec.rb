6f1a700b1d9b6b9d9204ef42ad07de59e5055bf3
Dany Marcoux <danymarcoux@gmail.com>
2019-03-20 20:13:35 +0100

    Authorize move_path
    Additionally turn the after_action around and maintain a whitelist instead of a blacklist. This requires new methods authorized by default.
    
    new_package and its dialog are just informal authorizations, so people do not even try to do this.
    
    Co-authored-by: Henne Vogelsang <hvogel@opensuse.org>
    Co-authored-by: Saray Cabrera Padrón <scabrerapadron@suse.de>
    Co-authored-by: David Kang <dkang@suse.com>

--- src/api/spec/controllers/webui/project_controller_spec.rb
+++ src/api/spec/controllers/webui/project_controller_spec.rb
@@ -417,7 +417,7 @@ RSpec.describe Webui::ProjectController, vcr: true do
 
   describe 'GET #new_package_branch' do
     it 'branches the package' do
-      login user
+      login(admin_user)
       @remote_projects_created = create_list(:remote_project, 3)
       get :new_package_branch, params: { project: apache_project }
       expect(assigns(:remote_projects)).to match_array(@remote_projects_created.map { |r| [r.id, r.name, r.title] })
@@ -1297,6 +1297,10 @@ RSpec.describe Webui::ProjectController, vcr: true do
       let(:repository) { create(:repository, project: apache_project) }
       let(:path_element) { create(:path_element, repository: repository) }
 
+      before do
+        login(admin_user)
+      end
+
       context 'without direction' do
         it { expect { post :move_path, params: { project: apache_project } }.to raise_error ActionController::ParameterMissing }
       end
@@ -1375,6 +1379,10 @@ RSpec.describe Webui::ProjectController, vcr: true do
     end
 
     context 'with non existing project' do
+      before do
+        login(admin_user)
+      end
+
       it { expect { post :move_path, params: { project: 'non:existent:project' } }.to raise_error ActiveRecord::RecordNotFound }
     end
   end
