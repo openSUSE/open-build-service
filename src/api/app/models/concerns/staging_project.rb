8d40c963f2a77dac891f863695521d9ad949fa12
Saray Cabrera Padrón <scabrerapadron@suse.de>
2019-04-04 15:39:36 +0200

    Move accept method to the delayed job
    Since the accepting state relies on the existence of delayed jobs, we
    need to make sure the staging projects acceptance only happens via
    delayed jobs.
    
    Co-authored-by: Björn Geuken <bgeuken@suse.de>

--- src/api/app/models/concerns/staging_project.rb
+++ src/api/app/models/concerns/staging_project.rb
@@ -142,25 +142,6 @@ module StagingProject
     project_log_entry.save!
   end
 
-  def accept_staged_requests
-    clear_memoized_data
-    return unless overall_state.in?([:acceptable, :accepting])
-
-    accepted_packages = []
-    staged_requests.each do |staged_request|
-      if staged_request.reviews.where(by_project: name).exists?
-        staged_request.change_review_state(:accepted, by_project: name, comment: "Staging Project #{name} got accepted.")
-      end
-      staged_request.change_state(newstate: 'accepted', comment: "Staging Project #{name} got accepted.")
-      accepted_packages.concat(staged_request.bs_request_actions.map(&:target_package))
-    end
-
-    packages.where(name: accepted_packages).find_each(&:destroy)
-    staged_requests.delete_all
-  ensure
-    clear_memoized_data
-  end
-
   private
 
   def clear_memoized_data
