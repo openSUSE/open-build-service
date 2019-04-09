ffabce81d01f98333f9e341997994b036c33aef8
Björn Geuken <bgeuken@suse.de>
2019-04-09 13:49:42 +0200

    Encapsulate ValidationHelper methods
    Because we were including the ValidationHelper outside of the
    MaintenanceHelper module, any object was including these methods.
    Moving the include into the module scope fixes this.
    
    Before this change:
    
    ```ruby
      irb(main):004:0> Project # Not necessary in production mode
      => Project (call 'Project.connection' to establish a connection)
      irb(main):005:0> "".methods.include?(:valid_project_name?)
      => true
    ```

--- src/api/app/helpers/maintenance_helper.rb
+++ src/api/app/helpers/maintenance_helper.rb
@@ -1,6 +1,6 @@
-include ValidationHelper
-
 module MaintenanceHelper
+  include ValidationHelper
+
   class MissingAction < APIError
     setup 400, 'The request contains no actions. Submit requests without source changes may have skipped!'
   end
