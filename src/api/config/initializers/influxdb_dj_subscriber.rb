f5468ca8daecc3a7ff82864c2df6240d136d1ec6
Henne Vogelsang <hvogel@opensuse.org>
2019-04-10 13:29:31 +0200

    Add some DelayedJob instrumentation
    So we can know what's going on in the queue and how
    jobs perform.

---
+++ src/api/config/initializers/influxdb_dj_subscriber.rb
@@ -0,0 +1,36 @@
+if CONFIG['influxdb_hosts']
+  ActiveSupport::Notifications.subscribe(/active_job/) do |*args|
+    event = ActiveSupport::Notifications::Event.new(*args)
+    job = event.payload[:job]
+    exception = event.payload[:exception_object]
+
+    def write_queue_stats(state, job)
+      InfluxDB::Rails.client.write_point('active_job.queue',
+                                         tags: { state: state, job: job.class.name, queue: job.queue_name },
+                                         values: { value: 1 })
+    end
+
+    def write_performance_stats(job, event)
+      InfluxDB::Rails.client.write_point('active_job.performance',
+                                         tags: { job: job.class.name, queue: job.queue_name },
+                                         values: { value: event.duration })
+    end
+
+    if event.name == 'perform.active_job' && exception
+      InfluxDB::Rails.client.write_point('active_job.queue',
+                                         tags: { state: :failed, job: job.class.name, queue: job.queue_name },
+                                         values: { value: 1, id: job.job_id })
+      return true
+    end
+
+    case event.name
+    when 'enqueue.active_job'
+      write_queue_stats(:queued, job)
+    when 'perform_start.active_job'
+      write_queue_stats(:running, job)
+    when 'perform.active_job'
+      write_queue_stats(:succeeded, job)
+      write_performance_stats(job, event)
+    end
+  end
+end
