6857f513b60fb78a81d3d5736432b95f4dcd6518
Björn Geuken <bgeuken@suse.de>
2019-04-11 15:51:00 +0200

    Move check for release target extension into SQL
    Maintenance incident packages usually have an extension that
    matches their release target.
    The changed code was checking if the package names matche the
    $name.$target pattern. This can also be done as part of the SQL query.

--- src/api/app/models/project.rb
+++ src/api/app/models/project.rb
@@ -1397,12 +1397,10 @@ class Project < ApplicationRecord
     # One catch, currently there's only one patchinfo per incident, but things keep changing every
     # other day, so it never hurts to have a look into the future:
     package_count = 0
-    packages.where.not(id: global_patchinfo_package).select(:name, :id).each do |pkg|
+    packages.where.not(id: global_patchinfo_package).where("name like '%.%'").select(:name, :id).each do |pkg|
       # Current ui is only showing the first found package and a symbol for any additional package.
       break if package_count > 2
 
-      rt_name = pkg.name.split('.', 2).last
-      next unless rt_name
       # Here we try hard to find the release target our current package is build for:
       rt_name = guess_release_target_from_package(pkg, release_targets_ng)
 
