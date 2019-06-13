46dec075e634d5f800cf7445c832b9063db4cffe
David Kang <dkang@suse.com>
2019-06-13 12:48:57 +0200

    Handle exception in `Feature.active?` method
    We decide to use the `dig` method to make the `Feature.active?` more robust
    if the configuration isn't in a proper format.
    
    Co-authored-by: Victor Pereira <vpereira@suse.com>

---
+++ src/api/spec/lib/feature_switch/feature_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Feature do
+  let(:user) { create(:confirmed_user, :in_beta, login: 'Tom') }
+  subject { Feature.active?('bootstrap') }
+
+  context 'with beta users' do
+    before do
+      User.session = user
+      Feature.instance_variable_set(:@perform_initial_refresh_for_user, true)
+    end
+
+    context 'when the file has "beta" key' do
+      it { expect(subject).to be_truthy }
+    end
+
+    context 'when the file doesn\'t have "beta" key' do
+      before do
+        allow(YAML).to receive(:load_file).and_return({})
+      end
+
+      it { expect(subject).to be_falsey }
+    end
+  end
+end
