1af091a163a936171b418ad259fff4801c35079c
Henne Vogelsang <hvogel@opensuse.org>
2019-07-12 17:07:04 +0200

    Show title of announcements
    Show the title of the announcement instead of a fixed sentence,
    no need to tease people to click.

--- src/api/spec/features/webui/announcements_spec.rb
+++ src/api/spec/features/webui/announcements_spec.rb
@@ -5,13 +5,13 @@ RSpec.feature 'Announcements for users', type: :feature, js: true do
   let!(:announcement_a) { create(:announcement, title: 'New terms of services A', content: 'Lorem ipsum A', created_at: 1.minute.ago) }
   let!(:announcement_b) { create(:announcement, title: 'New terms of services B', content: 'Lorem ipsum B') }
 
-  scenario "logged in users see a 'new announcements' notification and can acknowledge it" do
+  scenario 'logged in users see an announcement notification and can acknowledge it' do
     login(user)
-    visit '/'
-    expect(page).to have_text('There has been new announcements!')
+    visit '/project'
+    expect(page).to have_text(announcement_b.title)
     expect(page).to have_link('Read more')
     click_button('Got it')
-    expect(page).not_to have_text('There has been new announcements!')
+    expect(page).not_to have_text(announcement_b.title)
     expect(page).not_to have_button('Got it')
   end
 end
