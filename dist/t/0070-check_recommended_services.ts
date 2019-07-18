a191fce92e31e1d1bb27aae48c9ce0a3f9fece33
Eduardo Navarro <enavarro@suse.com>
2019-07-18 16:34:15 +0200

    Substitute tabs by spaces
    Also remove some empty lines not needed, and add spaces before commas.

--- dist/t/0070-check_recommended_services.ts
+++ dist/t/0070-check_recommended_services.ts
@@ -29,32 +29,30 @@ if ( Build::Rpm::verscmp($pkg_ver, "2.8.99") > 0) {
 plan tests => $test_count;
 
 foreach my $srv (@daemons) {
-	my @state=`systemctl is-enabled $srv\.service 2>/dev/null`;
-	chomp($state[-1]);
-	is($state[-1],"enabled","Checking if recommended service $srv is enabled");
+  my @state=`systemctl is-enabled $srv\.service 2>/dev/null`;
+  chomp($state[-1]);
+  s($state[-1], 'enabled', "Checking if recommended service $srv is enabled");
 }
 
 my %srv_state=();
 
 while ($max_wait > 0) {
-	
-	foreach my $srv (@daemons) {
-		my @state=`systemctl is-active $srv\.service 2>/dev/null`;
-		chomp($state[0]);
-		if ( $state[0] eq 'active') {
-			$srv_state{$srv} = 'active';
-		}
-	}
-	if ( keys(%srv_state) == scalar(@daemons) ) {
-		last;
-	}
-	$max_wait--;
-	sleep 1;
+  foreach my $srv (@daemons) {
+    my @state=`systemctl is-active $srv\.service 2>/dev/null`;
+    chomp($state[0]);
+    if ( $state[0] eq 'active') {
+      $srv_state{$srv} = 'active';
+    }
+  }
+  if ( keys(%srv_state) == scalar(@daemons) ) {
+    last;
+  }
+  $max_wait--;
+  sleep 1;
 }
 
 foreach my $srv ( @daemons ) {
-	is($srv_state{$srv} || 'timeout','active',"Checking recommended service '$srv' status");
+  is($srv_state{$srv} || 'timeout', 'active', "Checking recommended service '$srv' status");
 }
-
 
 exit 0;
