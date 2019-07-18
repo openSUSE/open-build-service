89574c642a5f83382ea82222366922b3f4dc8c61
Eduardo Navarro <enavarro@suse.com>
2019-07-18 16:43:48 +0200

    Add obs-api-support.target to dist tests
    Adopt the systemd naming, changing from services to units.
    
    Rename the file to units, because we are checking systemd services
    and targets.
    
    Co-authored-by: David Kang <dkang@suse.com>
    Co-authored-by: Frank Schreiner <schreiner@suse.de>

--- dist/t/0070-check_recommended_units.ts
+++ dist/t/0070-check_recommended_units.ts
@@ -19,26 +19,30 @@ use Build::Rpm;
 my $test_count = 6;
 my $max_wait = 300;
 
-my @daemons = qw/obsdodup obssigner obsdeltastore/;
+my @daemons = qw/obsdodup.service obssigner.service obsdeltastore.service/;
 my $pkg_ver = OBS::Test::Utils::get_package_version('obs-server', 2);
 if ( Build::Rpm::verscmp($pkg_ver, "2.8.99") > 0) {
-  $test_count = 8;
-  push (@daemons, 'obsservicedispatch');
+  $test_count += 2;
+  push (@daemons, 'obsservicedispatch.service');
+}
+if ( Build::Rpm::verscmp($pkg_ver, "2.9.99") > 0) {
+  $test_count += 2;
+  push (@daemons, 'obs-api-support.target');
 }
 
 plan tests => $test_count;
 
 foreach my $srv (@daemons) {
-  my @state=`systemctl is-enabled $srv\.service 2>/dev/null`;
+  my @state=`systemctl is-enabled $srv 2>/dev/null`;
   chomp($state[-1]);
-  s($state[-1], 'enabled', "Checking if recommended service $srv is enabled");
+  is($state[-1], 'enabled', "Checking if recommended unit $srv is enabled");
 }
 
 my %srv_state=();
 
 while ($max_wait > 0) {
   foreach my $srv (@daemons) {
-    my @state=`systemctl is-active $srv\.service 2>/dev/null`;
+    my @state=`systemctl is-active $srv 2>/dev/null`;
     chomp($state[0]);
     if ( $state[0] eq 'active') {
       $srv_state{$srv} = 'active';
@@ -52,7 +56,7 @@ while ($max_wait > 0) {
 }
 
 foreach my $srv ( @daemons ) {
-  is($srv_state{$srv} || 'timeout', 'active', "Checking recommended service '$srv' status");
+  is($srv_state{$srv} || 'timeout', 'active', "Checking recommended unit '$srv' status");
 }
 
 exit 0;
