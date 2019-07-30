77f3002e05432a2c25f2d480c4ecd60d5bbdad50
Dany Marcoux <danymarcoux@gmail.com>
2019-07-30 11:14:09 +0200

    Fix LeakyConstantDeclaration RuboCop offenses

--- src/api/spec/lib/api_error_spec.rb
+++ src/api/spec/lib/api_error_spec.rb
@@ -1,24 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe APIError do
-  before do
-    class MyTestError < APIError; setup('error_foo', 403, 'error foo'); end
+  let(:api_error_class) do
+    Class.new(described_class) do
+      setup('error_foo', 403, 'error foo')
+    end
   end
 
   context 'with custom error class' do
-    it { expect { raise MyTestError }.to raise_error(MyTestError.new.message) }
-    it { expect(MyTestError.new.default_message).to eq('error foo') }
-    it { expect(MyTestError.new.status).to eq(403) }
-    it { expect(MyTestError.new.errorcode).to eq('error_foo') }
+    it { expect { raise api_error_class }.to raise_error(api_error_class.new.message) }
+    it { expect(api_error_class.new.default_message).to eq('error foo') }
+    it { expect(api_error_class.new.status).to eq(403) }
+    it { expect(api_error_class.new.errorcode).to eq('error_foo') }
   end
 
   context 'with custom setup call' do
     before do
-      MyTestError.setup('error_bar', 404, 'error bar')
+      api_error_class.setup('error_bar', 404, 'error bar')
     end
 
-    it { expect(MyTestError.new.default_message).to eq('error bar') }
-    it { expect(MyTestError.new.status).to eq(404) }
-    it { expect(MyTestError.new.errorcode).to eq('error_bar') }
+    it { expect(api_error_class.new.default_message).to eq('error bar') }
+    it { expect(api_error_class.new.status).to eq(404) }
+    it { expect(api_error_class.new.errorcode).to eq('error_bar') }
   end
 end
