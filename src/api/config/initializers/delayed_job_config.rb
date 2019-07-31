2c97009441e9cd5d36e1e1c3215b5d63c6e7c717
Henne Vogelsang <hvogel@opensuse.org>
2019-07-31 12:31:01 +0200

    Switching the reserve SQL strategy for DelayedJob
    There are too many problems with the optimized SQL for locking jobs,
    if there are over 10K jobs it takes about 2 seconds on build.o.o.
    
    Co-authored-by: Stephan Kulow <coolo@suse.de>

--- src/api/config/initializers/delayed_job_config.rb
+++ src/api/config/initializers/delayed_job_config.rb
@@ -1,2 +1,5 @@
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.default_queue_name = 'quick'
+# There are too many problems with the optimized SQL for locking jobs,
+# it takes about 2 seconds on build.o.o, if there are over 10K jobs.
+Delayed::Backend::ActiveRecord.configuration.reserve_sql_strategy = :default_sql
