71f67649eb80eae0ff576af26b581f2c915d825e
Eduardo Navarro <enavarro@suse.com>
2019-08-29 17:34:13 +0200

    Remove group_management_label Bento helpers
    Now that we are only using the new web interface, we want to get rid
    of all the helpers only used with Bento.

--- src/api/app/helpers/webui/group_helper.rb
+++ src/api/app/helpers/webui/group_helper.rb
@@ -1,12 +1,4 @@
 module Webui::GroupHelper
-  def group_management_label
-    if User.admin_session?
-      link_to('Group Management', groups_path)
-    else
-      'Group Management'
-    end
-  end
-
   def group_with_icon(group_title)
     group = Group.find_by(title: group_title)
     user_image_tag(group, alt: group_title) + ' ' + link_to(group_title, group_show_path(group))
