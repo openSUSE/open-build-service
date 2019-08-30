62cb9e06f5f72716c5ea794946ce10ff49be3274
Eduardo Navarro <enavarro@suse.com>
2019-08-30 17:45:22 +0200

    Remove package_bread_crumb Bento helper and
    ... its skipped test.
    
    Now that we are only using the new web interface, we want to get rid
    of all the helpers only used with Bento.
    
    Remove also unused repo_type_and_priority helper.

--- src/api/app/helpers/webui/package_helper.rb
+++ src/api/app/helpers/webui/package_helper.rb
@@ -45,11 +45,6 @@ module Webui::PackageHelper
 
   include Webui::ProjectHelper
 
-  def package_bread_crumb(*args)
-    args.insert(0, link_to_if(params['action'] != 'show', @package_name || @package, package_show_path(project: @project, package: @package)))
-    project_bread_crumb(*args)
-  end
-
   def nbsp(text)
     result = ''.html_safe
     text.split(' ').each do |text_chunk|
@@ -74,10 +69,6 @@ module Webui::PackageHelper
         "#{n.to_i}#{name}"
       end
     end.compact.reverse.join(' ')
-  end
-
-  def repo_type_and_priority(repository)
-    [repository.repo_type, repository.priority].compact.join(', Priority: ')
   end
 
   def uploadable?(filename, architecture)
