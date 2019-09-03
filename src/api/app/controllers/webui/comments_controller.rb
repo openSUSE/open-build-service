752b93f62c7f657e20804a6536da5d600a48ce8f
David Kang <dkang@suse.com>
2019-09-03 15:44:50 +0200

    Remove Webui2::CommentsController module
    The coexistence between bento and bootstrap is no longer necessary. We
    started integrating the controller module into the respective controller.

--- src/api/app/controllers/webui/comments_controller.rb
+++ src/api/app/controllers/webui/comments_controller.rb
@@ -1,57 +1,50 @@
 class Webui::CommentsController < Webui::WebuiController
-  include Webui2::CommentsController
-
   before_action :require_login
   before_action :find_commentable, only: :create
 
   def create
-    return if params[:no_switch].nil? && switch_to_webui2
+    switch_to_webui2
+
     comment = @commented.comments.new(permitted_params)
     User.session!.comments << comment
-
-    # required for the form construction
-    @comment = Comment.new
+    @commentable = comment.commentable
 
     respond_to do |format|
       if comment.save
-        format.html { redirect_back(fallback_location: root_path, success: 'Comment was successfully created.') }
-        format.json { render json: 'ok' }
-        format.js { render partial: 'webui/comment/show', locals: { commentable: comment.commentable }, status: :ok }
+        flash.now[:success] = 'Comment created successfully.'
+        status = :ok
       else
-        format.html { redirect_back(fallback_location: root_path, error: "Comment can't be saved: #{comment.errors.full_messages.to_sentence}.") }
-        format.json { render json: comment.errors, status: :unprocessable_entity }
+        flash.now[:error] = "Failed to create comment: #{comment.errors.full_messages.to_sentence}."
+        status = :unprocessable_entity
+      end
+      format.html do
+        render(partial: 'webui2/webui/comment/comment_list', locals: { commentable: @commentable }, status: status)
       end
     end
   end
 
   def destroy
-    return if params[:no_switch].nil? && switch_to_webui2
+    switch_to_webui2
 
     comment = Comment.find(params[:id])
     authorize comment, :destroy?
+    @commentable = comment.commentable
 
     respond_to do |format|
       if comment.blank_or_destroy
-        flash[:success] = 'Comment deleted successfully.'
-        format.json { render json: { flash: render_flash } }
+        flash.now[:success] = 'Comment deleted successfully.'
+        status = :ok
       else
-        flash[:error] = "Failed to delete comment: #{comment.errors.full_messages.to_sentence}."
-        format.json { render json: { flash: render_flash }, status: :unprocessable_entity }
+        flash.now[:error] = "Failed to delete comment: #{comment.errors.full_messages.to_sentence}."
+        status = :unprocessable_entity
       end
-      format.html { redirect_back(fallback_location: root_path) }
+      format.html do
+        render(partial: 'webui2/webui/comment/comment_list', locals: { commentable: @commentable }, status: status)
+      end
     end
   end
 
   private
-
-  def render_flash
-    render_to_string(
-      partial: 'layouts/webui/flash',
-      formats: :html,
-      layout: false,
-      object: flash
-    )
-  end
 
   def permitted_params
     params.require(:comment).permit(:body, :parent_id)
