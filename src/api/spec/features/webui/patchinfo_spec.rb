8a06bcfd770130e4220d5b7394e0cd1c08b02989
Dany Marcoux <danymarcoux@gmail.com>
2019-09-05 14:07:04 +0200

    Merge Bento and Bootstrap feature specs for patchinfo

--- src/api/spec/features/webui/patchinfo_spec.rb
+++ src/api/spec/features/webui/patchinfo_spec.rb
@@ -39,4 +39,27 @@ RSpec.feature 'Patchinfo', type: :feature, js: true do
       expect(page).to have_text('Successfully edited patchinfo')
     end
   end
+
+  feature 'delete Patchinfo' do
+    let(:patchinfo_package) do
+      Patchinfo.new.create_patchinfo(user.home_project_name, nil) unless user.home_project.packages.where(name: 'patchinfo').exists?
+      Package.get_by_project_and_name(user.home_project_name, 'patchinfo', use_source: false)
+    end
+
+    before do
+      login(user)
+      patchinfo_package
+    end
+
+    scenario 'delete' do
+      visit patchinfo_show_path(project: project, package: 'patchinfo')
+      expect(page).to have_link('Delete patchinfo')
+      click_link('Delete patchinfo')
+      within('#delete-patchinfo-modal') do
+        expect(page).to have_text('Do you really want')
+        click_button('Delete')
+      end
+      expect(page).to have_text('Patchinfo was successfully removed.')
+    end
+  end
 end
