038b5a2d563cecdbf8c1aad0e238b1785d014fa3
Saray Cabrera Padrón <scabrerapadron@suse.de>
2019-09-05 09:51:39 +0200

    Merge webui2_* actions in the main package controller

--- src/api/app/controllers/webui/package_controller.rb
+++ src/api/app/controllers/webui/package_controller.rb
@@ -6,7 +6,6 @@ class Webui::PackageController < Webui::WebuiController
   include Webui::PackageHelper
   include Webui::ManageRelationships
   include BuildLogSupport
-  include Webui2::PackageController
 
   before_action :set_project, only: [:show, :index, :users, :linking_packages, :dependency, :binary, :binaries,
                                      :requests, :statistics, :commit, :revisions, :submit_request_dialog,
@@ -150,23 +149,15 @@ class Webui::PackageController < Webui::WebuiController
   end
   # rubocop:enable Lint/NonLocalExitFromIterator
 
-  # TODO: bento_only
   def statistics
-    return if switch_to_webui2
-    @arch = params[:arch]
+    switch_to_webui2
     @repository = params[:repository]
     @package_name = params[:package]
 
-    begin
-      xml = Backend::Api::BuildResults::Status.statistics(@project, params[:package], @repository, @arch)
-      if xml
-        @statistics = Xmlhash.parse(xml)
-        return
-      end
-    rescue Backend::Error
-    end
-
-    raise ActiveRecord::RecordNotFound, 'Not Found'
+    @statistics = LocalBuildStatistic::ForPackage.new(package: @package_name,
+                                                      project: @project.name,
+                                                      repository: @repository,
+                                                      architecture: params[:arch]).results
   end
 
   def binary
@@ -546,7 +537,19 @@ class Webui::PackageController < Webui::WebuiController
   end
 
   def branch_diff_info
-    switch_to_webui2
+    linked_package = @package.backend_package.links_to
+    if linked_package
+      target_project = linked_package.project.name
+      target_package = linked_package.name
+      description = @package.commit_message(target_project, target_package)
+    end
+
+    render json: {
+      'targetProject': defined?(target_project) ? target_project : '',
+      'targetPackage': defined?(target_package) ? target_package : '',
+      'description': defined?(description) ? description : '',
+      'cleanupSource': @project.branch? # We should remove the package if this request is a branch
+    }
   end
 
   private :check_package_name_for_new
@@ -633,12 +636,11 @@ class Webui::PackageController < Webui::WebuiController
     @package.description = params[:description]
     if @package.save
       flash[:success] = "Package data for '#{@package.name}' was saved successfully"
-      redirect_to action: :show, project: params[:project], package: params[:package]
     else
       flash[:error] = "Failed to save package '#{@package.name}': #{@package.errors.full_messages.to_sentence}"
-      return if switch_to_webui2
-      redirect_to action: :edit, project: params[:project], package: params[:package]
     end
+    switch_to_webui2
+    redirect_to action: :show, project: params[:project], package: params[:package]
   end
 
   def delete_dialog
@@ -983,12 +985,12 @@ class Webui::PackageController < Webui::WebuiController
       [repo_name, valid_xml_id(elide(repo_name, 30))]
     end
 
-    return if switch_to_webui2
-
+    switch_to_webui2
     if @repo_list.empty?
       render partial: 'no_repositories', locals: { project: @project }
     else
-      render partial: 'rpmlint_result', locals: { index: params[:index] }
+      render partial: 'rpmlint_result', locals: { index: params[:index], project: @project, package: @package,
+                                                  repository_list: @repo_list, repo_arch_hash: @repo_arch_hash }
     end
   end
 
