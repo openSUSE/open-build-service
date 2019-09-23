e332b2c0cc8fb739e80bc279b44c650146df03f4
Lukas Krause <lkrause@suse.de>
2019-09-23 23:15:34 +0200

    Remove deprecated methods from flag model
    Certain methods were only needed by the removed bento ui
    and are not needed anymore.

--- src/api/app/models/flag.rb
+++ src/api/app/models/flag.rb
@@ -21,23 +21,6 @@ class Flag < ApplicationRecord
 
   validates :flag, uniqueness: { scope: [:project_id, :package_id, :architecture_id, :repo] }
 
-  def self.default_status(flag_name)
-    FlagHelper.default_for(flag_name)
-  end
-
-  def default_status
-    compute_status('default')
-  end
-
-  # TODO: used by bento. Remove when dropping old UI.
-  def effective_status
-    compute_status('effective')
-  end
-
-  # TODO: used by bento. Remove when dropping old UI.
-  def has_children
-    repo.blank? || architecture.blank?
-  end
 
   def to_xml(builder)
     raise "FlagError: No flag-status set. \n #{inspect}" if status.nil?
@@ -47,63 +30,11 @@ class Flag < ApplicationRecord
     builder.send(status.to_s, options)
   end
 
-  # TODO: used by bento. Remove when dropping old UI.
-  def fullname
-    ret = flag
-    ret += "_#{repo}" if repo.present?
-    ret += "_#{architecture.name}" if architecture_id.present?
-    ret
-  end
-
   def arch
     architecture.try(:name).to_s
   end
 
   private
-
-  def compute_status(variant)
-    all_flag = main_object.flags.find_by('flag = ? AND repo IS NULL AND architecture_id IS NULL', flag)
-    repo_flag = main_object.flags.find_by('flag = ? AND repo = ? AND architecture_id IS NULL', flag, repo)
-    arch_flag = main_object.flags.find_by('flag = ? AND repo IS NULL AND architecture_id = ?', flag, architecture_id)
-    same_flag = main_object.flags.find_by('flag = ? AND repo = ? AND architecture_id = ?', flag, repo, architecture_id)
-    if main_object.is_a?(Package)
-      if variant == 'effective'
-        same_flag = main_object.project.flags.find_by('flag = ? AND repo = ? AND architecture_id = ?', flag, repo, architecture_id) unless
-          all_flag || same_flag || repo_flag || arch_flag
-        repo_flag = main_object.project.flags.find_by('flag = ? AND repo = ? AND architecture_id IS NULL', flag, repo) unless
-          all_flag || repo_flag || arch_flag
-        arch_flag = main_object.project.flags.find_by('flag = ? AND repo IS NULL AND architecture_id = ?', flag, architecture_id) unless
-          all_flag || arch_flag
-        all_flag ||= main_object.project.flags.find_by('flag = ? AND repo IS NULL AND architecture_id IS NULL', flag)
-      elsif variant == 'default'
-        same_flag ||= main_object.project.flags.find_by('flag = ? AND repo = ? AND architecture_id = ?', flag, repo, architecture_id)
-        repo_flag ||= main_object.project.flags.find_by('flag = ? AND repo = ? AND architecture_id IS NULL', flag, repo)
-        arch_flag ||= main_object.project.flags.find_by('flag = ? AND repo IS NULL AND architecture_id = ?', flag, architecture_id)
-        all_flag ||= main_object.project.flags.find_by('flag = ? AND repo IS NULL AND architecture_id IS NULL', flag)
-      end
-    end
-
-    if variant == 'effective'
-      return same_flag.status if same_flag
-      return repo_flag.status if repo_flag
-      return arch_flag.status if arch_flag
-      return all_flag.status if all_flag
-    elsif variant == 'default'
-      if same_flag
-        return repo_flag.status if repo_flag
-        return arch_flag.status if arch_flag
-      end
-      if same_flag || arch_flag || repo_flag
-        return all_flag.status if all_flag
-      end
-      if main_object.is_a?(Package)
-        all_flag = main_object.project.flags.find_by('flag = ? AND repo IS NULL AND architecture_id IS NULL', flag)
-        return all_flag.status if all_flag
-      end
-    end
-
-    Flag.default_status(flag)
-  end
 
   def discard_forbidden_project_cache
     Relationship.discard_cache if flag == 'access'
