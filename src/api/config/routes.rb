242fb720b2c2c589c93b5bce491a7a62f4daaa9d
Victor Pereira <vpereira@suse.de>
2019-09-25 15:37:03 +0200

    Use File.read to force the new routes files to be reloaded
    In development environment, after a file change, the routes.rb were
    reloaded, but the files under config/routes were not.

--- src/api/config/routes.rb
+++ src/api/config/routes.rb
@@ -1,8 +1,15 @@
+require_relative 'routes/routes_helper'
+
+class ActionDispatch::Routing::Mapper
+  def draw(routes_name)
+    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}_routes.rb")))
+  end
+end
+
 OBSApi::Application.routes.draw do
   mount Peek::Railtie => '/peek'
-  require_relative 'routes/routes_helper'
-  require_relative 'routes/webui_routes'
-  require_relative 'routes/api_routes'
+  draw :webui
+  draw :api
 end
 
 OBSEngine::Base.subclasses.each(&:mount_it)
