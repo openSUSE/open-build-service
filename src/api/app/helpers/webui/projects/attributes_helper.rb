663943d317f4d305e9239b35028361d02ba34e07
Dani Donisa <dani.doni@gmail.com>
2019-09-30 15:15:17 +0200

    Show the Quality category in the project detail page also

--- src/api/app/helpers/webui/projects/attributes_helper.rb
+++ src/api/app/helpers/webui/projects/attributes_helper.rb
@@ -1,7 +1,6 @@
 module Webui::Projects::AttributesHelper
   def category_badge(category)
     return unless category
-    
     badge_type = case category
                  when 'Stable'
                    'badge-success'
@@ -12,7 +11,6 @@ module Webui::Projects::AttributesHelper
                  else
                    'badge-dark'
                  end
-    
     content_tag(:span, category, class: ['quality-category', 'badge', badge_type].join(' '))
   end
 end
