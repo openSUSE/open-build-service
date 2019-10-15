20be7fb58769fdad3977efb76af4a0a0629a8590
Dany Marcoux <danymarcoux@gmail.com>
2019-10-15 12:16:03 +0200

    Align usage of icons for projects/packages in the UI
    To follow what we already do in the 'Latest Updates' section on the homepage.
    
    Fixes #8480

--- src/api/app/helpers/webui/staging/workflow_helper.rb
+++ src/api/app/helpers/webui/staging/workflow_helper.rb
@@ -43,19 +43,22 @@ module Webui::Staging::WorkflowHelper
     end
   end
 
-  def reviewers_gravatar(request, users_hash, groups_hash)
+  def reviewers_icon(request, users_hash, groups_hash)
     missing_reviews = request[:missing_reviews]
-    image_tags = []
+    tags = []
     missing_reviews.each do |review|
       case review[:review_type]
       when 'by_group'
-        object = groups_hash[review[:by]]
+        tags << image_tag_for(groups_hash[review[:by]], size: 20)
       when 'by_user'
-        object = users_hash[review[:by]]
+        tags << image_tag_for(users_hash[review[:by]], size: 20)
+      when 'by_project'
+        tags << content_tag(:i, nil, class: 'fa fa-cubes text-secondary ml-1', title: review[:by])
+      when 'by_package'
+        tags << content_tag(:i, nil, class: 'fa fa-archive text-dark ml-1', title: review[:by])
       end
-      image_tags << image_tag_for(object, size: 20)
     end
-    image_tags
+    tags
   end
 
   def create_request_links(request, users_hash, groups_hash)
@@ -63,7 +66,7 @@ module Webui::Staging::WorkflowHelper
     css = 'review' if request[:missing_reviews].present?
     css = 'obsolete' if request[:state].in?(BsRequest::OBSOLETE_STATES)
     link_content = [request[:package]]
-    link_content << reviewers_gravatar(request, users_hash, groups_hash) if request[:missing_reviews].present?
+    link_content << reviewers_icon(request, users_hash, groups_hash) if request[:missing_reviews].present?
     content_tag(:span, class: "badge state-#{css}") do
       link_to(request_show_path(request[:number]), class: 'request') do
         safe_join(link_content)
