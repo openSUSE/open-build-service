c53958e76c6612c45ed136b66b92591cdec2b1a4
Henne Vogelsang <hvogel@opensuse.org>
2019-10-16 16:30:44 +0200

    Add codemirror sprockets configuration
    We inject codemirror differently depending on the page we show so
    we can skip loading ~120 modes on pages where we know the displayed
    content type.

--- src/api/app/assets/config/manifest.js
+++ src/api/app/assets/config/manifest.js
@@ -2,6 +2,9 @@
 //
 //= link webui/application.css
 //= link webui/application.js
+//= link webui/cm2/codemirror-file.js
+//= link webui/cm2/codemirror-prjconf.js
+//= link webui/cm2/codemirror-xml.js
 //= link_tree ../images
 
 
--- src/api/app/assets/javascripts/webui/cm2/codemirror-xml.js
+++ src/api/app/assets/javascripts/webui/cm2/codemirror-xml.js
@@ -1,4 +1,4 @@
-// This is a manifest file that'll be compiled into codemirror, which will include all the files
+// This is a manifest file that'll be compiled into codemirror-xml.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
