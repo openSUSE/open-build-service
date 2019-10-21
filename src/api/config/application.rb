9c286271aaf548af06358e9757f9a58cdb8caa9b
Stephan Kulow <coolo@suse.de>
2019-10-21 15:15:18 +0200

    Disable use of concurrency in asset compilation
    sassc is not thread safe and there are tons of crashes while
    building packages. Unfortunately #8555 was merged even though
    the problem was noted there

--- src/api/config/application.rb
+++ src/api/config/application.rb
@@ -90,6 +90,11 @@ module OBSApi
     # avoid a warning
     I18n.enforce_available_locales = true
 
+    config.assets.configure do |env|
+      # https://github.com/rails/sprockets/issues/581
+      env.export_concurrent = false
+    end
+
     # we're not threadsafe
     config.allow_concurrency = false
 
