3cea8e42adad784103e0d2bc44256da837796851
Stephan Kulow <coolo@suse.de>
2019-11-28 10:27:54 +0100

    Add nick function to Staging::ProjectCategory

--- src/api/app/models/staging/project_category.rb
+++ src/api/app/models/staging/project_category.rb
@@ -7,6 +7,26 @@ class Staging::ProjectCategory < ApplicationRecord
 
   validates :staging_workflow, :title, :name_pattern, presence: true
   validates :title, length: { maximum: 30 }
+  validate :valid_regexp
 
-  validates :name_pattern, format: { with: /\(\?\<nick\>/, message: "needs to have a capture group for 'nick'" }
+  def nick(project)
+    match = compiled.match(project.to_s)
+    return unless match
+    match[:nick]
+  end
+
+  private
+
+  def compiled
+    @compiled || Regexp.new(name_pattern)
+  end
+
+  def valid_regexp
+    begin
+      regexp = Regexp.new(name_pattern.to_s)
+      return if regexp.inspect =~ /\(\?<nick>.*\)/
+    rescue RegexpError, TypeError
+    end
+    errors.add(:name_pattern, 'needs to be a regexp with capture group for "nick"')
+  end
 end
--- src/api/spec/models/staging/project_category_spec.rb
+++ src/api/spec/models/staging/project_category_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Staging::ProjectCategory, type: :model do
-  let(:staging_workflow) { create(:staging_workflow) }
-
-  before do
-    staging_workflow.project_categories.create(title: 'Letter', name_pattern: "#{staging_workflow.project.name}:Staging:{(?<nick>\w}")
-  end
+  let(:project) { create(:project_with_package, name: 'MyProject') }
+  let(:staging_workflow) { create(:staging_workflow, project: project) }
+  let(:staging_project_category) { staging_workflow.project_categories.create(title: 'Letter', name_pattern: '.*:Staging:(?<nick>\w)') }
 
   describe '#title' do
     it { is_expected.to validate_presence_of(:title) }
@@ -14,7 +12,15 @@ RSpec.describe Staging::ProjectCategory, type: :model do
 
   describe '#name_pattern' do
     it { is_expected.to validate_presence_of(:name_pattern) }
-    it { is_expected.not_to(allow_value('Just:So').for(:name_pattern)) }
+
+    it { is_expected.not_to allow_value('Just:So').for(:name_pattern) }
+    it { is_expected.not_to allow_value('*Hallo').for(:name_pattern) }
+    it { is_expected.not_to allow_value('Hallo:(?<nick>').for(:name_pattern) }
     it { is_expected.to allow_value('Just:(?<nick>So)').for(:name_pattern) }
+  end
+
+  describe '#nick' do
+    it { expect(staging_project_category.nick(project.name)).to be_nil }
+    it { expect(staging_project_category.nick('MyProject:Staging:A')).to eq('A') }
   end
 end
