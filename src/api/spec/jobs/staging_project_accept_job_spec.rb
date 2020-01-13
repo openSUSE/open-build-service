7b41719538590f34c2b35395531d8c1de1e6cc04
David Kang <dkang@suse.com>
2020-01-13 13:14:52 +0100

    Add test and update cassettes
    Co-authored-by: Dany Marcoux <dmarcoux@suse.com>

--- src/api/spec/jobs/staging_project_accept_job_spec.rb
+++ src/api/spec/jobs/staging_project_accept_job_spec.rb
@@ -52,19 +52,46 @@ RSpec.describe StagingProjectAcceptJob, type: :job, vcr: true do
     subject { StagingProjectAcceptJob.perform_now(project_id: staging_project.id, user_login: user.login) }
 
     context "when the staging project is in 'acceptable' state" do
+      let!(:project_log_entry_comment_for_project) do
+        create(:project_log_entry_comment_for_project, project: staging_project, user_name: user.login)
+      end
+      let!(:project_log_entry_staging_project_created) do
+        create(:project_log_entry_staging_project_created, project: staging_project, user_name: user.login)
+      end
+      let!(:project_log_entry_staged_request) do
+        create(:project_log_entry_staged_request,
+               project: staging_project,
+               user_name: user.login,
+               package_name: target_package.title,
+               bs_request: staged_request)
+      end
+      let!(:project_log_entry_unstaged_request) do
+        create(:project_log_entry_unstaged_request,
+               project: staging_project,
+               user_name: user.login,
+               package_name: target_package.title,
+               bs_request: staged_request)
+      end
       let!(:user_relationship) { create(:relationship, project: target_project, user: user) }
 
-      before do
-        subject
-        # Ensure we test the current state.
-        staging_project.send(:clear_memoized_data)
+      context 'staging project should be accepted' do
+        before do
+          subject
+          # Ensure we test the current state.
+          staging_project.send(:clear_memoized_data)
+        end
+
+        it { expect(staging_project.overall_state).to eq(:empty) }
+        it { expect(staging_project.reload.packages).to contain_exactly(package) }
+        it { expect(staged_request.reload.state).to eq(:accepted) }
+        it { expect(staged_request_with_by_project_review.reload.state).to eq(:accepted) }
+        it { expect(staged_request_with_by_project_review.reviews.where.not(state: 'accepted')).not_to exist }
       end
 
-      it { expect(staging_project.overall_state).to eq(:empty) }
-      it { expect(staging_project.reload.packages).to contain_exactly(package) }
-      it { expect(staged_request.reload.state).to eq(:accepted) }
-      it { expect(staged_request_with_by_project_review.reload.state).to eq(:accepted) }
-      it { expect(staged_request_with_by_project_review.reviews.where.not(state: 'accepted')).not_to exist }
+      context 'should remove staging project log entries' do
+        it { expect { subject }.to change { staging_project.project_log_entries.count }.from(4).to(1) }
+        it { expect { subject }.to change { staging_project.project_log_entries.staging_history.count }.from(3).to(0) }
+      end
     end
 
     context 'when the staging project has missing reviews' do
