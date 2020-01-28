c1a8c22ccfaf62eafb33ef2e8891efd776313909
Saray Cabrera Padrón <scabrerapadron@suse.de>
2020-01-28 19:34:21 +0100

    Add navigation bar at the bottom
    The bottom bar includes the most important actions:
      - Go to home project
      - Go to tasks
      - Handle watchlist

--- src/api/app/assets/javascripts/webui/offcanvas.js
+++ src/api/app/assets/javascripts/webui/offcanvas.js
@@ -2,5 +2,12 @@ $(function () {
   $('[data-toggle="offcanvas"]').on('click', function () {
     $('.offcanvas-collapse').toggleClass('open');
     $('.navbar-toggler').toggleClass('open');
+    $('.watchlist-collapse').removeClass('open');
+  });
+
+  $('[data-toggle="watchlist"]').on('click', function () {
+    $('.watchlist-collapse').toggleClass('open');
+    $('.navbar-toggler').removeClass('open');
+    $('.offcanvas-collapse').removeClass('open');
   });
 });
