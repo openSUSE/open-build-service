8ffbc87e19a892128f0b0ee98070546cd8591c71
Saray Cabrera Padrón <scabrerapadron@suse.de>
2020-01-28 19:01:57 +0100

    Add beta helpers for responsive-ux feature
    Co-authored-by: Daniel Donisa <daniel.donisa@suse.com>
    Co-authored-by: David Kang <dkang@suse.com>
    Co-authored-by: Eduardo Navarro <enavarro@suse.com>

--- src/api/app/helpers/webui/webui_helper.rb
+++ src/api/app/helpers/webui/webui_helper.rb
@@ -355,5 +355,15 @@ module Webui::WebuiHelper
     build_problems = remaining_build_problems.shift(show_builds)
     return checks, build_problems, remaining_checks, remaining_build_problems
   end
+
+  # responsive_ux:
+  def flipper_responsive?
+    Flipper.enabled?(:responsive_ux, User.possibly_nobody)
+  end
+
+  # responsive_ux:
+  def responsive_namespace
+    flipper_responsive? ? 'webui/responsive_ux' : 'webui'
+  end
 end
 # rubocop:enable Metrics/ModuleLength
