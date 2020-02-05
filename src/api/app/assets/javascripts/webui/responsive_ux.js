efbbe8f2a9698af923ff245707e361a133ad3c78
Dani Donisa <daniel.donisa@suse.com>
2020-02-05 17:58:46 +0100

    Move the actions on the main page to a contextual menu
    The actions present under the Welcome section take precious space on mobile
    layouts. We move them under a contextual menu that pops up from the bottom of
    the screen.
    
    Those actions go back to its original place on bigger screens as there's
    plenty of space.
    
    Co-authored-by: David Kang <dkang@suse.com>

--- src/api/app/assets/javascripts/webui/responsive_ux.js
+++ src/api/app/assets/javascripts/webui/responsive_ux.js
@@ -4,6 +4,7 @@ $(function () {
     $('.offcanvas-collapse').toggleClass('open');
     $('.watchlist-collapse').removeClass('open');
     $('a[data-toggle="watchlist"]').removeClass('active');
+    $('.contextual-collapse').removeClass('open');
   });
 
   $('[data-toggle="watchlist"]').on('click', function () {
@@ -11,6 +12,7 @@ $(function () {
     $('.watchlist-collapse').toggleClass('open');
     $('.offcanvas-collapse').removeClass('open');
     $('a[data-toggle="offcanvas"]').removeClass('active');
+    $('.contextual-collapse').removeClass('open');
   });
 
   $('.access-modal').on('show.bs.modal', function () {
@@ -20,5 +22,12 @@ $(function () {
   $('.access-modal').on('shown.bs.modal', function () {
     $('#login').focus();
     $('#username').focus();
+  });
+
+  $('[data-toggle="contextual"]').on('click', function () {
+    $('.contextual-collapse').toggleClass('open');
+    $('.watchlist-collapse').removeClass('open');
+    $('.navbar-toggler').removeClass('open');
+    $('.offcanvas-collapse').removeClass('open');
   });
 });
