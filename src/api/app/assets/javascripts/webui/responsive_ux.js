a8d75664dcea2f31a00b6a641e1b05b43b7b7fe2
Eduardo Navarro <enavarro@suse.com>
2020-02-05 17:18:09 +0100

    Improve responsive_ux layout
    Co-authored-by: David Kang <dkang@suse.com>
    Co-authored-by: Saray Cabrera Padrón <scabrerapadron@suse.de>

--- src/api/app/assets/javascripts/webui/responsive_ux.js
+++ src/api/app/assets/javascripts/webui/responsive_ux.js
@@ -1,14 +1,16 @@
 $(function () {
   $('[data-toggle="offcanvas"]').on('click', function () {
+    $('a[data-toggle="offcanvas"]').toggleClass('active');
     $('.offcanvas-collapse').toggleClass('open');
-    $('.navbar-toggler').toggleClass('open');
     $('.watchlist-collapse').removeClass('open');
+    $('a[data-toggle="watchlist"]').removeClass('active');
   });
 
   $('[data-toggle="watchlist"]').on('click', function () {
+    $('a[data-toggle="watchlist"]').toggleClass('active');
     $('.watchlist-collapse').toggleClass('open');
-    $('.navbar-toggler').removeClass('open');
     $('.offcanvas-collapse').removeClass('open');
+    $('a[data-toggle="offcanvas"]').removeClass('active');
   });
 
   $('.access-modal').on('show.bs.modal', function () {
