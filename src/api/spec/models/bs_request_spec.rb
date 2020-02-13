c9e20af4600a4a16252d9f07cfe9a768f3208b1c
Lukas Krause <lkrause@suse.de>
2020-02-13 16:36:55 +0100

    Adjust rspec tests for the usage of recyclable cache key

--- src/api/spec/models/bs_request_spec.rb
+++ src/api/spec/models/bs_request_spec.rb
@@ -300,13 +300,13 @@ RSpec.describe BsRequest, vcr: true do
     RSpec.shared_examples "the subject's cache is reset when it's request changes" do
       before do
         Timecop.travel(1.minute)
-        @cache_key = user.cache_key
+        @cache_version = user.cache_version
         request.state = :review
         request.save
         user.reload
       end
 
-      it { expect(user.cache_key).not_to eq(@cache_key) }
+      it { expect(user.cache_version).not_to eq(@cache_version) }
     end
 
     context 'creator of bs_request' do
