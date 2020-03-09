573e1eed1235f588ad8d7e0fa477e6c98bfe6d6c
Dany Marcoux <dmarcoux@posteo.de>
2020-03-09 13:30:58 +0100

    Create menu for actions specific to a page

--- src/api/app/assets/javascripts/webui/responsive_ux.js
+++ src/api/app/assets/javascripts/webui/responsive_ux.js
@@ -3,14 +3,27 @@ $(function () {
     $('a[data-toggle="offcanvas"]').toggleClass('active');
     $('.offcanvas-collapse').toggleClass('open');
     $('.watchlist-collapse').removeClass('open');
+    $('.actions-collapse').removeClass('open');
     $('a[data-toggle="watchlist"]').removeClass('active');
+    $('a[data-toggle="actions"]').removeClass('active');
   });
 
   $('[data-toggle="watchlist"]').on('click', function () {
     $('a[data-toggle="watchlist"]').toggleClass('active');
     $('.watchlist-collapse').toggleClass('open');
     $('.offcanvas-collapse').removeClass('open');
+    $('.actions-collapse').removeClass('open');
     $('a[data-toggle="offcanvas"]').removeClass('active');
+    $('a[data-toggle="actions"]').removeClass('active');
+  });
+
+  $('[data-toggle="actions"]').on('click', function () {
+    $('a[data-toggle="actions"]').toggleClass('active');
+    $('.actions-collapse').toggleClass('open');
+    $('.offcanvas-collapse').removeClass('open');
+    $('.watchlist-collapse').removeClass('open');
+    $('a[data-toggle="offcanvas"]').removeClass('active');
+    $('a[data-toggle="watchlist"]').removeClass('active');
   });
 
   $('.access-modal').on('show.bs.modal', function () {
