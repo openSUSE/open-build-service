61c45c174388d4970362d6f20be1df900d574e75
Dani Donisa <daniel.donisa@suse.com>
2020-03-10 17:13:59 +0100

    Refactoring in place editing helper

--- src/api/app/helpers/webui/in_place_editing_helper.rb
+++ src/api/app/helpers/webui/in_place_editing_helper.rb
@@ -1,8 +1,11 @@
 module Webui::InPlaceEditingHelper
   class InPlaceEditor
-    attr_reader :object_name, :editable_id, :trigger_id, :trigger_classes, :form_id, :input_id, :update_url, :cancel_button_id, :refresh_target
+    attr_reader :object_name, :editable_id, :trigger_id, :trigger_classes, :form_id, :input_id, :update_url, :cancel_button_id, :refresh_target, :editable_object, :editable_field_name, :view_context
 
-    def initialize(object, field_name, options)
+    def initialize(object, field_name, view_context, options)
+      @editable_object = object
+      @editable_field_name = field_name
+      @view_context = view_context
       @object_name = object.class.model_name.to_s.underscore
       @editable_id = "#{object_name}-#{field_name}"
       @trigger_id = "#{@editable_id}-trigger" # The pencil-shaped control to trigger the edition
@@ -13,48 +16,67 @@ module Webui::InPlaceEditingHelper
       @cancel_button_id = "#{@editable_id}-cancel"
       @refresh_target = options[:refresh_target_id]
     end
-  end
 
-  def build_form_element(object, field_name, editor, &editing_control_builder)
-    form_for(object,
-             url: editor.update_url,
-             remote: true,
-             method: :patch,
-             data: { type: 'html' },
-             html: { id: editor.form_id, class: 'd-none' }) do |form|
-      concat(hidden_field_tag(:id, object.id))
-      concat(editing_control_builder.call(form, field_name, editor))
-      concat(form.submit("Save changes", class: 'btn btn-primary btn-sm ml-3'))
-      concat(form.button("Cancel changes",
-                         id: editor.cancel_button_id,
-                         class: 'btn btn-outline-danger btn-sm ml-3'));
+    def build_form_element_with_custom_component(&editing_control_builder)
+      view_context.form_for(editable_object,
+                            url: update_url,
+                            remote: true,
+                            method: :patch,
+                            data: { type: 'html' },
+                            html: { id: form_id, class: 'd-none' }) do |form|
+        view_context.concat(view_context.hidden_field_tag(:id, editable_object.id))
+        view_context.concat(editing_control_builder.call(form, editable_field_name, self))
+        view_context.concat(form.submit("Save changes", class: 'btn btn-primary btn-sm ml-3'))
+        view_context.concat(form.button("Cancel changes",
+                                id: cancel_button_id,
+                                class: 'btn btn-outline-danger btn-sm ml-3'));
+      end
+    end
+
+    def build_auxiliar_markup!(form_element, &custom_element_builder)
+      view_context.content_tag(:div, class: 'd-flex in-place-editing align-items-end', data: { id: editable_id, 'refresh-target-id': refresh_target }) do
+        view_context.concat(view_context.content_tag(:div, class: 'triggering-wrapper') { custom_element_builder.call if block_given? })
+        trigger_element = view_context.content_tag(:i, nil,
+                                                   id: trigger_id,
+                                                   class: trigger_classes)
+        view_context.concat(trigger_element)
+        view_context.concat(form_element)
+      end
+    end
+
+    def build_form!
+      raise NotImplementedError
+    end
+
+    def build!(&custom_element_builder)
+      form = build_form!
+      build_auxiliar_markup!(form, &custom_element_builder)
     end
   end
 
-  def build_auxiliar_markup(editor, form_element, &custom_element_builder)
-    content_tag(:div, class: 'd-flex in-place-editing align-items-end', data: { id: editor.editable_id, 'refresh-target-id': editor.refresh_target }) do
-      concat(content_tag(:div, class: 'triggering-wrapper') { custom_element_builder.call if block_given? })
-      trigger_element = content_tag(:i, nil,
-                                    id: editor.trigger_id,
-                                    class: editor.trigger_classes)
-      concat(trigger_element)
-      concat(form_element)
+  class InPlaceTextAreaEditor < InPlaceEditor
+    def build_form!
+      build_form_element_with_custom_component do |form, field_name, editor|
+        form.text_area(field_name.to_sym, id: editor.input_id)
+      end
+    end
+  end
+
+  class InPlaceTextFieldEditor < InPlaceEditor
+    def build_form!
+      build_form_element_with_custom_component do |form, field_name, editor|
+        form.text_field(field_name.to_sym, id: editor.input_id)
+      end
     end
   end
 
   def in_place_text_field(object, field_name, options = {}, &custom_element_builder)
-    editor = InPlaceEditor.new(object, field_name, options)
-    form_element = build_form_element(object, field_name, editor) do |form, field_name, editor|
-      form.text_field(field_name.to_sym, id: editor.input_id)
-    end
-    build_auxiliar_markup(editor, form_element, custom_element_builder)
+    editor = InPlaceTextFieldEditor.new(object, field_name, self, options)
+    editor.build!(&custom_element_builder)
   end
 
   def in_place_text_area(object, field_name, options = {}, &custom_element_builder)
-    editor = InPlaceEditor.new(object, field_name, options)
-    form_element = build_form_element(object, field_name, editor) do |form, field_name, editor|
-      form.text_area(field_name.to_sym, id: editor.input_id)
-    end
-    build_auxiliar_markup(editor, form_element, custom_element_builder)
+    editor = InPlaceTextAreaEditor.new(object, field_name, self, options)
+    editor.build!(&custom_element_builder)
   end
 end
