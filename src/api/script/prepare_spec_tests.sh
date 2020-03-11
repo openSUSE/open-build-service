887273fa7ffc2ea8d24ba8125ada714aafea2693
Ruediger Oertel <ro@suse.de>
2020-03-11 14:42:09 +0100

    Revert "[api] fix build for mariadb-10.4 using the current user"
    This reverts commit 7162bf27e770df993c23828249ef37610a9276c7.

--- src/api/script/prepare_spec_tests.sh
+++ src/api/script/prepare_spec_tests.sh
@@ -5,7 +5,6 @@ BASE_DIR=$PWD
 TEMP_DIR=$BASE_DIR/tmp
 MYSQL_BASEDIR=$TEMP_DIR/mysql/
 MYSQL_DATADIR=$MYSQL_BASEDIR/data
-MYSQL_EXTRA_CONF=$TEMP_DIR/my.cnf.add
 MYSQL_SOCKET_DIR=`mktemp -d`
 MYSQL_SOCKET=$MYSQL_SOCKET_DIR/mysql.socket
 
@@ -26,20 +25,16 @@ if [ -z "$MYSQL_SERVER" ]; then
   exit 1
 fi
 
-cat << EOF > $MYSQL_EXTRA_CONF
-[mysqld]
-plugin-load-add = auth_socket.so
-EOF
 
 ### do testing now
 
 rm -rf $MYSQL_DATADIR $MYSQL_SOCKET
 mkdir -p $MYSQL_BASEDIR
 chown -R $MYSQLD_USER $MYSQL_BASEDIR
-mysql_install_db --user=$MYSQLD_USER --datadir=$MYSQL_DATADIR --auth-root-authentication-method=socket --auth-root-socket-user=$MYSQLD_USER --defaults-extra-file=$MYSQL_EXTRA_CONF
+mysql_install_db --user=$MYSQLD_USER --datadir=$MYSQL_DATADIR
 $MYSQL_SERVER --user=$MYSQLD_USER --datadir=$MYSQL_DATADIR --skip-networking --socket=$MYSQL_SOCKET --pid-file=$TEMP_DIR/mysqld.pid &
 sleep 2
-echo "install plugin unix_socket soname 'auth_socket';" | mysql -u $MYSQLD_USER --socket=$MYSQL_SOCKET
+
 ##################### api
 
 # setup files
@@ -51,14 +46,14 @@ development:
   adapter:  mysql2
   host:     localhost
   database: api_25
-  username: $MYSQLD_USER
+  username: root
   encoding: utf8mb4
   socket:   $MYSQL_SOCKET
 test:
   adapter:  mysql2
   host:     localhost
   database: api_test
-  username: $MYSQLD_USER
+  username: root
   encoding: utf8mb4
   socket:   $MYSQL_SOCKET
   # disable timeout, required on SLES 11 SP3 at least
@@ -69,5 +64,4 @@ EOF
 echo "MYSQL_BASEDIR=$MYSQL_BASEDIR" > mysql_environment
 echo "MYSQL_SOCKET_DIR=$MYSQL_SOCKET_DIR" >> mysql_environment
 echo "MYSQL_SOCKET=$MYSQL_SOCKET" >> mysql_environment
-echo "MYSQLD_USER=$MYSQLD_USER" >> mysql_environment
 
