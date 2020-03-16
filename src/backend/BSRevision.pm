openSUSE/open-build-service

commit 3596c87381c898b7f32ed3c69145f8826c623008

[backend] add blocked file support for BSDBIndex
use it to block the linkinfo master directory, as it gets much
too big and is not used at all.

src/backend/BSRevision.pm
@@ -319,6 +319,7 @@ sub updatelinkinfodb {
   return if $packid eq '_project';	# no links allowed
   mkdir_p($sourcedb) unless -d $sourcedb;
   my $linkdb = BSDB::opendb($sourcedb, 'linkinfo');
+  $linkdb->{'blocked'} = 'linkinfo';
   my $linkinfo;
   if ($files && $files->{'_link'}) {
     my $l = revreadxml($rev, '_link', $files->{'_link'}, $BSXML::link, 1);

src/backend/BSDBIndex.pm
@@ -104,6 +104,7 @@ sub modify {
     $usedfiles{$file} = undef;
     push @{$addbyfile{$file}}, $rel;
   }
+  delete $usedfiles{$_} for @{$db->{'blocked'} || []};
 
   # sort so we do not run into deadlocks
   my @usedfiles = sort keys %usedfiles;

commit f060999da9e66c5c7065e291be6008a0d9e49ea8

[backend] fix getlinkpackages for old BSDB
This was broken in 2016 with commit 3596c87381c898b7f32ed3c69145f8826c623008
in 2016, which dropped the 'linkinfo' key index.

src/backend/BSRevision.pm
@@ -636,7 +636,7 @@ sub getlinkpackages {
   } else {
     return() unless -d $sourcedb;
     my $db = BSDB::opendb($sourcedb, 'linkinfo');
-    return grep {s/\Q$projid\E\///} $db->keys();
+    return map {grep {s/\Q$projid\E\///} $db->keys('project', $_)} $db->values('project');
   }
 }
 