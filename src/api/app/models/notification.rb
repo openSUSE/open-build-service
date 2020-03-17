40ad02201c1fe88ee598250b44edf396ac27d969
Victor Pereira <vpereira@suse.de>
2020-03-17 12:42:30 +0100

    Add Notification methods to check if subscriber should become notifications
    Users or Groups are just get new notifications if:
    
    - User logged-in within 3 months
    - Group with at least one User which logged-in within 3 months

--- src/api/app/models/notification.rb
+++ src/api/app/models/notification.rb
@@ -13,4 +13,12 @@ class Notification < ApplicationRecord
   def self.cleanup
     Notification.stale.delete_all
   end
+
+  def user_active?
+    !subscriber.away?
+  end
+
+  def any_user_in_group_active?
+    !subscriber.users.recently_seen.empty?
+  end
 end
