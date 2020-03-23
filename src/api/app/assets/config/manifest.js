88cb50904ed02edfeb93c641003248b5e49a8ad2
David Kang <dkang@suse.com>
2020-03-23 13:19:46 +0100

    Add sprocket configuration

---
+++ src/api/app/assets/config/manifest.js
@@ -0,0 +1,20 @@
+// This is a sprockets 4.0 manifest file
+//
+//= link webui/application.css
+//= link webui/application.js
+//= link webui2/webui2.css
+//= link webui2/application.js
+//= link webui/obs_factory/application.css
+//
+// CODEMIRROR
+//= link webui2/cm2/show-prjconf.js
+//= link webui2/cm2/index-xml.js
+//= link webui2/cm2/default.js
+//= link webui2/cm2/index-diff.js
+//= link webui/application/cm2/index.js
+//= link webui/application/cm2/index-xml.js
+//= link webui/application/cm2/index-diff.js
+//= link webui/application/cm2/index-prjconf.js
+//
+//= link_tree ../images
+//= link_tree ../icons
