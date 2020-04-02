b976d49909d9e1d98288465ad1582e40cfeff7a9
Saray Cabrera Padrón <scabrerapadron@suse.de>
2020-04-02 20:59:54 +0200

    Add data migration to re-generate notifications
    Recent refactoring on notification creation code and database structure
    left some inconsistent data in the database.
    
    To solve the problem, we are going to remove the affected notifications (in the
    previous data migration) and then we are going to re-generate the notifications
    (with the current data migration).
    
    In this case, we are taking objects existing in the database with certain states
    and conditions (requests, reviews and comments) and we are creating a
    notification for each of them.
    
    The notifications affected are those related to the events 'ReviewWanted',
    'RequestCreate', 'RequestStatechange' and 'CommentForRequest'.
    
    This data migration makes use of the recently created NotificationCreator and
    refactored code.
    
    Data migration's tests included.
    
    Co-authored-by: David Kang <dkang@suse.com>
    Co-authored-by: Eduardo Navarro <enavarro@suse.com>

---
+++ src/api/db/data/20200326221616_regenerate_notifications.rb
@@ -0,0 +1,99 @@
+#  After some changes in Notification's database structure, some data needs to
+#  be updated. But, instead of fixing the existing Notifications, we are going
+#  to delete the affected ones and regenerate them with the correct values.
+#
+#  The deletion is made in a previous data migration. This one re-generates the
+#  notifications.
+#
+#  Steps:
+
+#  - Take all the existing BsRequests in state 'new' and create a RequestCreate
+#    Notification for each of them.
+#  - Take the BsRequests in state 'declined' that where created in the last 100 days and
+#    create a RequesStatechange Notification for each of them.
+#  - Take all the existing BsRequests in state 'review' with reviews in state 'new' and
+#    create a ReviewWanted Notification for each of the reviews.
+#  - Take the BsRequests created in the last 2 weeks and create a CommentForRequest
+#    Notification for each comment on each BsRequest.
+
+class RegenerateNotifications < ActiveRecord::Migration[5.2]
+  def up
+    create_request_create_notifications
+    create_request_statechange_notifications
+    create_review_wanted_notifications
+    create_comment_for_request_notifications
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+
+  # RequestCreated Notifications
+
+  def create_request_create_notifications
+    new_requests = BsRequest.in_states(:new)
+
+    new_requests.each do |request|
+      event = Event::RequestCreate.new(request.event_parameters)
+      NotificationCreator.new(event).call
+    end
+  end
+
+  # RequestStatechange Notifications
+
+  def create_request_statechange_notifications
+    declined_requests = BsRequest.in_states(:declined).where('created_at >= ?', 100.days.ago.midnight)
+
+    declined_requests.each do |request|
+      event = Event::RequestStatechange.new(request.event_parameters)
+      event.payload['oldstate'] = request_old_state(request)
+      NotificationCreator.new(event).call
+    end
+  end
+
+  def request_old_state(request)
+    # Check history elements to guess the previous state
+    # assuming the last one is always HistoryElement::RequestDeclined
+    previous_history_element, last_history_element = request.history_elements.last(2)
+
+    case previous_history_element.try(:type)
+    when nil, 'HistoryElement::RequestAllReviewsApproved'
+      'new'
+    when 'HistoryElement::RequestReviewAdded', 'HistoryElement::ReviewDeclined'
+      'review'
+    when 'HistoryElement::RequestDeclined'
+      'declined'
+    when 'HistoryElement::RequestReopened'
+      return 'review' if request.reviews.find_by(state: :new).present?
+      'new' # when no reviews or all of them were accepted
+    when 'HistoryElement::ReviewAccepted'
+      return 'declined' if last_history_element.description == 'Declined via staging workflow.'
+      'review'
+    else # less accurate result for any other case that was overlooked
+      'review'
+    end
+  end
+
+  # ReviewWanted Notifications
+
+  def create_review_wanted_notifications
+    new_reviews = Review.where(state: 'new').joins(:bs_request).where('bs_requests.state = ?', 'review')
+
+    new_reviews.each do |review|
+      params = review.event_parameters(review.bs_request.event_parameters)
+      event = Event::ReviewWanted.new(params)
+      NotificationCreator.new(event).call
+    end
+  end
+
+  # CommentForRequest Notifications
+
+  def create_comment_for_request_notifications
+    recent_request_comments = Comment.where('commentable_type = ? AND created_at >= ?', 'BsRequest', 2.weeks.ago.midnight)
+
+    recent_request_comments.each do |comment|
+      event = Event::CommentForRequest.new(comment.event_parameters)
+      NotificationCreator.new(event).call
+    end
+  end
+end
--- src/api/spec/factories/event_subscriptions.rb
+++ src/api/spec/factories/event_subscriptions.rb
@@ -13,9 +13,37 @@ FactoryBot.define do
       channel { :instant_email }
     end
 
+    factory :event_subscription_comment_for_package do
+      eventtype { 'Event::CommentForPackage' }
+      receiver_role { 'commenter' }
+      channel { :instant_email }
+      user
+    end
+
+    factory :event_subscription_comment_for_request do
+      eventtype { 'Event::CommentForRequest' }
+      receiver_role { 'commenter' }
+      channel { :instant_email }
+      user
+    end
+
     factory :event_subscription_request_created do
       eventtype { 'Event::RequestCreate' }
       receiver_role { 'target_maintainer' }
+      channel { :instant_email }
+      user
+    end
+
+    factory :event_subscription_request_statechange do
+      eventtype { 'Event::RequestStatechange' }
+      receiver_role { 'target_maintainer' }
+      channel { :instant_email }
+      user
+    end
+
+    factory :event_subscription_review_wanted do
+      eventtype { 'Event::ReviewWanted' }
+      receiver_role { 'reviewer' }
       channel { :instant_email }
       user
     end
