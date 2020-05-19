433baa80b79d7b2e494d70d4927fd1d44351880f
Saray Cabrera Padrón <scabrerapadron@suse.de>
2020-05-19 23:49:25 +0200

    Adapt announcements and status messages tests
    Announcements no longer contain a title and the field has been renamed
    from "content" to "message". Status messages severity is now saved using
    the string and not its corresponding number. Test has been updated
    according to this.
    
    A duplicated text has been removed.

--- src/api/spec/models/announcement_spec.rb
+++ src/api/spec/models/announcement_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Announcement, type: :model do
-  it { is_expected.to validate_presence_of :title }
-  it { is_expected.to validate_presence_of :content }
+  it { is_expected.to validate_presence_of :message }
 end
