7baa54509bbe4bd7b317fe56d7292b2e13a22878
Lukas Krause <lkrause@suse.de>
2020-05-29 16:32:37 +0200

    Extract toggle_custom_checkbox helper for feature tests
    There is not an easy wway to check custom checkbox without labels or
    with label that contains a link. For that reason, we moved the method
    `toggle_checkbox` to be used when it is needed on features tests.

--- src/api/spec/support/shared_examples/features/beta/user_tab.rb
+++ src/api/spec/support/shared_examples/features/beta/user_tab.rb
@@ -6,19 +6,6 @@ RSpec.shared_examples 'user tab' do
   let!(:package) { nil }
   let!(:project) { nil }
 
-  def toggle_checkbox(html_id)
-    # Workaround: Bootstrap's custom-control causes the checkbox's input field to be at
-    # a different location than it visually appears. For browsers this is not an issue, but
-    # capybara throws an error because "is not clickable at point (596, 335). Other element
-    # would receive the click".
-    # Thus we have to remove bootstraps custom-control classes.
-    page.execute_script("$('##{html_id}').removeClass('custom-control-input')")
-    page.execute_script("$('label[for=#{html_id}]').removeClass('custom-control-label')")
-    find_field(html_id).click
-    # FIXME: Needed to wait for the Ajax call to perform
-    sleep(1)
-  end
-
   describe 'user roles' do
     let!(:bugowner_user_role) do
       create(:relationship,
