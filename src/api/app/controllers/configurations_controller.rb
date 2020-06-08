9412451f0b5845550f6d79be00dc4092f3321970
Dany Marcoux <dmarcoux@posteo.de>
2020-06-08 16:11:00 +0200

    Auto-correct RuboCop offenses for Rails/IndexWith

--- src/api/app/controllers/configurations_controller.rb
+++ src/api/app/controllers/configurations_controller.rb
@@ -30,8 +30,8 @@ class ConfigurationsController < ApplicationController
     attribs = {}
     # scheduler architecture list
     archs = nil
-    archs = Hash[xml['schedulers']['arch'].map { |a| [a, 1] }] if xml.dig('schedulers', 'arch').class == Array
-    archs = Hash[params['arch'].map { |a| [a, 1] }] if params['arch'].class == Array
+    archs = xml['schedulers']['arch'].index_with { |_a| 1 } if xml.dig('schedulers', 'arch').class == Array
+    archs = params['arch'].index_with { |_a| 1 } if params['arch'].class == Array
     if archs
       Architecture.all.each do |arch|
         if arch.available != (archs[arch.name] == 1)
