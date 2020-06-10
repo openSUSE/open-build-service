ebf810c76d585af443332f7a6b14367c75ad31e0
Dany Marcoux <dmarcoux@posteo.de>
2020-06-10 18:09:16 +0200

    Auto-correct RuboCop offenses in dist/openQA_mail_notification.rb

--- dist/openQA_mail_notification.rb
+++ dist/openQA_mail_notification.rb
@@ -17,18 +17,16 @@ VERSIONS = ['Unstable', '2.9', '2.8'].freeze
 GROUP = '17'.freeze
 
 def get_build_information(version)
-  begin
-    uri = URI.parse("#{OPEN_QA}api/v1/jobs?distri=#{DISTRIBUTION}&version=#{version}")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(uri.request_uri)
-    response = http.request(request)
-    JSON.parse(response.body)['jobs'].last
-  rescue Exception => e
-    $stderr.puts "Error while fetching openQA data: #{e.inspect}"
-    abort
-  end
+  uri = URI.parse("#{OPEN_QA}api/v1/jobs?distri=#{DISTRIBUTION}&version=#{version}")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.request(request)
+  JSON.parse(response.body)['jobs'].last
+rescue Exception => e
+  warn "Error while fetching openQA data: #{e.inspect}"
+  abort
 end
 
 def modules_to_sentence(modules)
@@ -50,21 +48,19 @@ def build_message(build, successful_modules, failed_modules, version)
 end
 
 def send_notification(from, to, subject, message)
-  begin
-    mail = Mail.new do
-      from    from
-      to      to
-      subject subject
-      body    message
-    end
-    settings = { address: SMTP_SERVER, port: 25, enable_starttls_auto: false }
-    settings[:domain] = ENV["HOSTNAME"] if ENV["HOSTNAME"].present?
-    mail.delivery_method :smtp, settings
-    mail.deliver
-  rescue Exception => e
-    $stderr.puts "#{SMTP_SERVER}: #{e.inspect}"
-    abort
+  mail = Mail.new do
+    from    from
+    to      to
+    subject subject
+    body    message
   end
+  settings = { address: SMTP_SERVER, port: 25, enable_starttls_auto: false }
+  settings[:domain] = ENV['HOSTNAME'] if ENV['HOSTNAME'].present?
+  mail.delivery_method :smtp, settings
+  mail.deliver
+rescue Exception => e
+  warn "#{SMTP_SERVER}: #{e.inspect}"
+  abort
 end
 
 VERSIONS.each do |version|
@@ -73,22 +69,22 @@ VERSIONS.each do |version|
   last_build = store.transaction { store[:name] }
   result = last_build <=> build['name']
 
-  if result != 0 && build['state'] == 'done'
-    modules = build['modules']
-    successful_modules = modules.select { |m| m['result'] == 'passed' }
-    failed_modules = modules.select { |m| m['result'] == 'failed' }
-    successful_modules = modules_to_sentence(successful_modules)
-    failed_modules = modules_to_sentence(failed_modules)
+  next unless result != 0 && build['state'] == 'done'
 
-    subject = "Build #{build['result']} in openQA: #{build['name']}"
-    message = build_message(build['settings']['BUILD'], successful_modules, failed_modules, version)
-    to = TO_SUCCESS
-    to = TO_FAILED unless failed_modules.empty?
-    send_notification(FROM, to, subject, message)
+  modules = build['modules']
+  successful_modules = modules.select { |m| m['result'] == 'passed' }
+  failed_modules = modules.select { |m| m['result'] == 'failed' }
+  successful_modules = modules_to_sentence(successful_modules)
+  failed_modules = modules_to_sentence(failed_modules)
 
-    store.transaction do
-      store[:name] = build['name']
-      store[:last_run] = build['t_finished']
-    end
+  subject = "Build #{build['result']} in openQA: #{build['name']}"
+  message = build_message(build['settings']['BUILD'], successful_modules, failed_modules, version)
+  to = TO_SUCCESS
+  to = TO_FAILED unless failed_modules.empty?
+  send_notification(FROM, to, subject, message)
+
+  store.transaction do
+    store[:name] = build['name']
+    store[:last_run] = build['t_finished']
   end
 end
