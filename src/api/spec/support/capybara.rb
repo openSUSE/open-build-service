520ca9a6d4c021ec3f861be374a03580692e75cc
Henne Vogelsang <hvogel@opensuse.org>
2020-06-19 17:36:13 +0200

    Switch to apparition capybara driver
    This driver is being developed by the current developer of
    Capybara and will attempt to keep up to date with new Capybara releases.
    We expect this to be more stable.
    
    Esecially as it uses CDP to communicate with Chrome, thereby obviating the need
    for chromedriver.

--- src/api/spec/support/capybara.rb
+++ src/api/spec/support/capybara.rb
@@ -1,3 +1,5 @@
+require 'capybara/apparition'
+
 Capybara.default_max_wait_time = 6
 Capybara.save_path = Rails.root.join('tmp', 'capybara')
 Capybara.server = :puma, { Silent: true }
@@ -6,66 +8,14 @@ Capybara.javascript_driver = :desktop
 # Attempt to click the associated label element if a checkbox/radio button are non-visible (This is especially useful for Bootstrap custom controls)
 Capybara.automatic_label_click = true
 
-# we use RSPEC_HOST as trigger to use remote selenium
-if ENV['RSPEC_HOST'].blank?
-  Selenium::WebDriver::Chrome::Service.driver_path = '/usr/lib64/chromium/chromedriver'
+Capybara.register_driver :desktop do |app|
+  options = { window_size: [1280, 1024] }
+  Capybara::Apparition::Driver.new(app, options)
+end
 
-  Capybara.register_driver :desktop do |app|
-    Capybara::Selenium::Driver.load_selenium
-    browser_options = ::Selenium::WebDriver::Chrome::Options.new
-    browser_options.args << '--headless'
-    browser_options.args << '--no-sandbox' # to run in docker
-    browser_options.args << '--window-size=1280,1024'
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-  end
-
-  Capybara.register_driver :mobile do |app|
-    Capybara::Selenium::Driver.load_selenium
-    browser_options = ::Selenium::WebDriver::Chrome::Options.new
-    browser_options.args << '--headless'
-    browser_options.args << '--no-sandbox' # to run in docker
-    browser_options.add_emulation(device_metrics: { width: 320, height: 568, pixelRatio: 1, touch: true })
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
-  end
-else
-  Capybara.register_driver :desktop do |app|
-    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-      'goog:chromeOptions' => {
-        'args' => ['--no-sandbox', '--start-maximized']
-      },
-      browserName: 'chrome'
-    )
-
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: :remote,
-      url: 'http://selenium:4444/wd/hub',
-      desired_capabilities: caps
-    )
-  end
-
-  Capybara.register_driver :mobile do |app|
-    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
-      'goog:chromeOptions' => {
-        'args' => ['--no-sandbox'],
-        mobileEmulation: { deviceMetrics: { width: 320, height: 568, pixelRatio: 1, touch: true } }
-      }
-    )
-
-    Capybara::Selenium::Driver.new(
-      app,
-      browser: :remote,
-      url: 'http://selenium:4444/wd/hub',
-      desired_capabilities: caps
-    )
-  end
-
-  Capybara.configure do |config|
-    config.app_host = "http://#{ENV['RSPEC_HOST']}:3005"
-  end
-
-  Capybara.server_host = '0.0.0.0'
-  Capybara.server_port = 3005
+Capybara.register_driver :mobile do |app|
+  options = { window_size: [320, 568] }
+  Capybara::Apparition::Driver.new(app, options)
 end
 
 # Automatically save the page a test fails
@@ -82,7 +32,7 @@ RSpec.configure do |config|
       save_page("#{example_filename}.html")
       # rubocop:disable Lint/Debugger
       # TODO: The RuboCop comments can be removed once this is merged upstream: https://github.com/rubocop-hq/rubocop/pull/7853
-      save_screenshot("#{example_filename}.png")
+      save_screenshot("#{example_filename}.png", full: true)
       # rubocop:enable Lint/Debugger
     end
   end
