6a4815794a4022b4e6e7377aaddbe61fcf667295
Dani Donisa <daniel.donisa@suse.com>
2020-06-30 11:12:57 +0200

    Skip notifier types not in the whitelist

--- src/api/app/services/outdated_notifications/destroyer.rb
+++ src/api/app/services/outdated_notifications/destroyer.rb
@@ -1,10 +1,14 @@
 class OutdatedNotifications::Destroyer
+  DESTROYABLE_NOTIFIABLES = ['BsRequest', 'Comment']
+
   def initialize(subscriber, notification)
     @subscriber = subscriber
     @notification = notification
   end
 
   def call
+    return unless DESTROYABLE_NOTIFIABLES.include?(@notification.notifiable_type)
+
     scope = NotificationsFinder.new(@subscriber.notifications.for_web).with_notifiable
     klass = "OutdatedNotificationsFinder::#{@notification.notifiable_type}"
     klass.constantize.new(scope, @notification.notifiable).call.each(&:destroy)
