62aa6ee0dc22b94d12488b36ec84b3fc36a4aadf
Frank Schreiner <schreiner@suse.de>
2020-07-02 22:52:00 +0200

    [dist] added db:schema:load to initial db setup
    Fixes: #9779

--- dist/setup-appliance.sh
+++ dist/setup-appliance.sh
@@ -277,7 +277,7 @@ function prepare_database_setup {
   if [ -n "$RUN_INITIAL_SETUP" ]; then
     logline "Initialize OBS api database (first time only)"
     cd $apidir
-    RAKE_COMMANDS="db:setup writeconfiguration"
+    RAKE_COMMANDS="db:setup writeconfiguration data:schema:load"
   else
     logline "Migrate OBS api database"
     cd $apidir
