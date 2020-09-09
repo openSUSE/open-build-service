5029f87b1872c30b136b46e7071fe8014c46849a
Eduardo Navarro <enavarro@suse.com>
2020-09-09 10:34:05 +0200

    Auto-correct Performance/RedundantMerge cop offenses

--- src/api/app/helpers/webui/notification_helper.rb
+++ src/api/app/helpers/webui/notification_helper.rb
@@ -4,7 +4,7 @@ module Webui::NotificationHelper
     if params['show_all'] # already showing all
       link_to('Show less', my_notifications_path(parameters), class: 'btn btn-sm btn-secondary ml-2')
     else
-      parameters.merge!({ show_all: 1 })
+      parameters[:show_all] = 1
       link_to('Show all', my_notifications_path(parameters), class: 'btn btn-sm btn-secondary ml-2')
     end
   end
