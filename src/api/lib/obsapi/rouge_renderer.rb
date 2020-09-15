4056d9451efb0677f9a0afab2f622396428bfddd
pk12 <panagiwthskan@gmail.com>
2020-09-15 11:59:02 +0300

    Adds explanatory comment

--- src/api/lib/obsapi/rouge_renderer.rb
+++ src/api/lib/obsapi/rouge_renderer.rb
@@ -2,6 +2,8 @@ require "#{Rails.root}/lib/obsapi/markdown_renderer"
 
 module OBSApi
   class RougeRenderer < OBSApi::MarkdownRenderer
+    # This module includes a block_code definition
+    # That overrides the Coderay one from the parent class
     include Rouge::Plugins::Redcarpet
   end
 end
