99b7922fb5469491d3f44050281e3dd61bc2530e
arharovets <alpalladii@gmail.com>
2020-10-06 22:01:17 +0200

    Add UI elements to `package#show`
    In order to show and use snippets in package comments:
    - add comment snippets data to `show` action in package controller
    - add dropdown component with comment snippets list to choose from
    - render dropdown component in the `comment/_new` if snippets present so
      if snippets were not created, component won't be rendered

--- src/api/app/controllers/webui/package_controller.rb
+++ src/api/app/controllers/webui/package_controller.rb
@@ -105,6 +105,8 @@ class Webui::PackageController < Webui::WebuiController
     @comments = @package.comments.includes(:user)
     @comment = Comment.new
 
+    @comment_snippets = User.session.comment_snippets
+
     @current_notification = NotificationsFinder.new.for_subscribed_user_by_id(params[:notification_id])
 
     @services = @files.any? { |file| file[:name] == '_service' }
