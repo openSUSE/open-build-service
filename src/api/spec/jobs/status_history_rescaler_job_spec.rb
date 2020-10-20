865bd283b4be74d033b2f9d3f06dcdfe985dd85d
Victor Pereira <vpereira@suse.de>
2020-10-20 19:05:04 +0200

    Refactor StatusHistoryRescaler
    - Delete unused code
    - Use human friendly time representation
    - Extract variables to method
    - Do the averaging using activerecord

--- src/api/spec/jobs/status_history_rescaler_job_spec.rb
+++ src/api/spec/jobs/status_history_rescaler_job_spec.rb
@@ -4,31 +4,31 @@ RSpec.describe StatusHistoryRescalerJob, type: :job do
   include ActiveJob::TestHelper
 
   describe '#rescale' do
+    let(:now) { Time.now.to_i - 2.days }
+    let(:idle_status_histories) { StatusHistory.where(key: 'idle_x86_64') }
+    let(:busy_status_histories) { StatusHistory.where(key: 'busy_x86_64') }
+
     before do
-      Timecop.freeze(2010, 7, 12)
-
-      now = Time.now.to_i - 2.days
       StatusHistory.transaction do
-        1000.times do |i|
-          StatusHistory.create(time: now + i, key: 'idle_x86_64', value: i)
-        end
+        10.times { |i| StatusHistory.create(time: now + i, key: 'idle_x86_64', value: i) }
+        2.times { |i| StatusHistory.create(time: now + i, key: 'busy_x86_64', value: i) }
       end
-
-      StatusHistory.create(time: Time.now.to_i, key: 'busy_x86_64', value: 100)
     end
 
-    after do
-      Timecop.return
+    subject! { StatusHistoryRescalerJob.perform_now }
+
+    context 'StatusHistory Total' do
+      it { expect(StatusHistory.count).to eq(2) }
     end
 
-    subject! { StatusHistoryRescalerJob.new.perform }
+    context 'Status histories for idle_x86_64' do
+      it { expect(idle_status_histories.count).to eq(1) }
+      it { expect(idle_status_histories.first.value).to eq(4.5) }
+    end
 
-    it { expect(StatusHistory.count).to eq(2) }
-
-    it 'keeps the StatusHistory with key = idle_x86_64' do
-      status_histories = StatusHistory.where(key: 'idle_x86_64')
-      expect(status_histories.count).to eq(1)
-      expect(status_histories.first.value).to eq(499.5)
+    context 'Status histories for busy_x86_64' do
+      it { expect(busy_status_histories.count).to eq(1) }
+      it { expect(busy_status_histories.first.value).to eq(0.5) }
     end
   end
 end
