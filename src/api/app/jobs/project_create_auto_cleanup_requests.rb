c57fd52173066945f2ee8aa9a3234ca0a086658f
Victor Pereira <vpereira@suse.de>
2020-10-24 08:38:41 +0200

    Refactor ProjectCreateAutoCleanupRequests job

--- src/api/app/jobs/project_create_auto_cleanup_requests.rb
+++ src/api/app/jobs/project_create_auto_cleanup_requests.rb
@@ -1,9 +1,35 @@
-class ProjectCreateAutoCleanupRequests < ApplicationJob
+class ProjectCreateAutoCleanupRequestsJob < ApplicationJob
   DESCRIPTION = "This is a humble request to remove this project.
 Accepting this request will free resources on our always crowded server.
 Please decline this request if you want to keep this repository nevertheless. Otherwise this request
 will get accepted automatically in near future.
 Such requests get not created for projects with open requests or if you remove the OBS:AutoCleanup attribute.".freeze
+
+  class CleanupRequestTemplate
+    attr_accessor :project, :description, :cleanup_time
+
+    def initialize(project:, description:, cleanup_time:)
+      @project = project
+      @description = description
+      @cleanup_time = cleanup_time
+      @erb = ERB.new(template)
+    end
+
+    def template
+      <<~XML
+        <request>
+          <action type="delete"><target project="<%= project %>"/></action>
+          <description><%= description %></description>
+          <state />
+          <accept_at><%= cleanup_time %></accept_at>
+        </request>
+      XML
+    end
+
+    def render
+      @erb.result(binding)
+    end
+  end
 
   def perform
     # disabled ?
@@ -13,7 +39,7 @@ Such requests get not created for projects with open requests or if you remove t
     # defaults
     User.find_by!(login: 'Admin').run_as do
       @cleanup_attribute = AttribType.find_by_namespace_and_name!('OBS', 'AutoCleanup')
-      @cleanup_time = Time.now + cleanup_days.days
+      @cleanup_time = Time.zone.now + cleanup_days.days
 
       Project.find_by_attribute_type(@cleanup_attribute).each do |prj|
         autoclean_project(prj)
@@ -21,27 +47,22 @@ Such requests get not created for projects with open requests or if you remove t
     end
   end
 
-  def autoclean_project(prj)
-    # project may be locked?
-    return if prj.nil? || prj.is_locked? || !prj.check_weak_dependencies?
+  private
 
+  def autoclean_project(prj)
     # open requests do block the cleanup
-    open_requests_count = BsRequest.in_states([:new, :review, :declined])
-                                   .joins(:bs_request_actions)
-                                   .where('bs_request_actions.target_project = ? OR bs_request_actions.source_project = ?', prj.name, prj.name)
-                                   .count
-    return if open_requests_count.positive?
+    return if open_requests_count(prj.name).positive?
 
     # check the time in project attribute
-    time = nil
+    return unless project_ready_to_autoclean?(prj)
+
     begin
       attribute = prj.attribs.find_by_attrib_type_id(@cleanup_attribute.id)
       return unless attribute
 
-      time = Time.parse(attribute.values.first.value)
-      return unless time
-    rescue ArgumentError
-      # not parseable time
+      time = Time.zone.parse(attribute.values.first.value)
+    rescue TypeError, ArgumentError
+      # nil time raises TypeError
       return
     end
     # not yet
@@ -50,15 +71,29 @@ Such requests get not created for projects with open requests or if you remove t
     # create request, but add some time between to avoid an overload
     @cleanup_time += 5.minutes
 
-    req = BsRequest.new_from_xml('<request>
-                                       <action type="delete">
-                                          <target project="' + prj.name + '" />
-                                       </action>
-                                       <description>' + DESCRIPTION + '</description>
-                                       <state />
-                                       <accept_at>' + @cleanup_time.to_s + '</accept_at>
-                                     </request>')
+    req = create_request(project: prj.name, cleanup_time: @cleanup_time)
     req.save!
     Event::RequestCreate.create(req.event_parameters)
+  end
+
+  def create_request(project:, description: DESCRIPTION, cleanup_time: 5)
+    BsRequest.new_from_xml(CleanupRequestTemplate.new(project: project,
+                                                      description: description,
+                                                      cleanup_time: cleanup_time).render)
+  end
+
+  def project_ready_to_autoclean?(project)
+    # project may be locked?
+    return false if project.nil? || project.is_locked?
+    return false unless project.check_weak_dependencies?
+
+    true
+  end
+
+  def open_requests_count(project)
+    BsRequest.in_states([:new, :review, :declined])
+             .joins(:bs_request_actions)
+             .where('bs_request_actions.target_project = ? OR bs_request_actions.source_project = ?', project, project)
+             .count
   end
 end
