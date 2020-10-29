2b6c66a3ea11fe85ab2f0bbadfdd80bb4ddb88c5
Eduardo Navarro <enavarro@suse.com>
2020-10-29 17:22:21 +0100

    Introduce metrics for user global roles
    Co-authored-by: Saray Cabrera Padrón <scabrerapadron@suse.de>

--- src/api/app/jobs/measurements_job.rb
+++ src/api/app/jobs/measurements_job.rb
@@ -3,10 +3,14 @@ class MeasurementsJob < ApplicationJob
 
   def perform
     RabbitmqBus.send_to_bus('metrics', "group count=#{Group.count}")
-    RabbitmqBus.send_to_bus('metrics', "user in_beta=#{User.in_beta.count},in_rollout=#{User.in_rollout.count},count=#{User.count},#{state_fields}")
+    RabbitmqBus.send_to_bus('metrics', "user in_beta=#{User.in_beta.count},in_rollout=#{User.in_rollout.count},count=#{User.count},#{role_fields},#{state_fields}")
   end
 
   private
+
+  def role_fields
+    Role.global.pluck(:title).map { |role_title| "#{role_title.downcase}=#{Role.find_by(title: role_title).users.count}" }.join(',')
+  end
 
   def state_fields
     User::STATES.map { |state| "#{state}=#{User.where(state: state).count}" }.join(',')
