89321a9f6f1d9b7b625908c26c7fea6723329213
Eduardo Navarro <enavarro@suse.com>
2020-10-29 15:52:45 +0100

    Introduce metrics for users in rollout
    Co-authored-by: Saray Cabrera Padrón <scabrerapadron@suse.de>

--- src/api/app/jobs/measurements_job.rb
+++ src/api/app/jobs/measurements_job.rb
@@ -3,6 +3,6 @@ class MeasurementsJob < ApplicationJob
 
   def perform
     RabbitmqBus.send_to_bus('metrics', "group count=#{Group.count}")
-    RabbitmqBus.send_to_bus('metrics', "user in_beta=#{User.in_beta.count},count=#{User.count}")
+    RabbitmqBus.send_to_bus('metrics', "user in_beta=#{User.in_beta.count},in_rollout=#{User.in_rollout.count},count=#{User.count}")
   end
 end
