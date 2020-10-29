0e911e74aa2ca82691f7d565def3b9ca2a9f46d8
Eduardo Navarro <enavarro@suse.com>
2020-10-29 17:09:57 +0100

    Introduce user state metrics
    Co-authored-by: Saray Cabrera Padrón <scabrerapadron@suse.de>

--- src/api/app/jobs/measurements_job.rb
+++ src/api/app/jobs/measurements_job.rb
@@ -3,6 +3,12 @@ class MeasurementsJob < ApplicationJob
 
   def perform
     RabbitmqBus.send_to_bus('metrics', "group count=#{Group.count}")
-    RabbitmqBus.send_to_bus('metrics', "user in_beta=#{User.in_beta.count},in_rollout=#{User.in_rollout.count},count=#{User.count}")
+    RabbitmqBus.send_to_bus('metrics', "user in_beta=#{User.in_beta.count},in_rollout=#{User.in_rollout.count},count=#{User.count},#{state_fields}")
+  end
+
+  private
+
+  def state_fields
+    User::STATES.map { |state| "#{state}=#{User.where(state: state).count}" }.join(',')
   end
 end
