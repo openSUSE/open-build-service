50342e69176c65076f99eb169bd2eda549dbe836
Henne Vogelsang <hvogel@opensuse.org>
2020-10-30 19:46:52 +0100

    Revert "Refactor status history rescaler job"

--- src/api/app/jobs/status_history_rescaler_job.rb
+++ src/api/app/jobs/status_history_rescaler_job.rb
@@ -3,43 +3,57 @@ class StatusHistoryRescalerJob < ApplicationJob
 
   # this is called from a delayed job triggered by clockwork
   def perform
-    distinct_status_history_keys.each do |key|
+    maxtime = StatusHistory.maximum(:time)
+    StatusHistory.where('time < ?', maxtime - 365 * 24 * 3600).delete_all if maxtime
+
+    keys = StatusHistory.distinct.pluck(:key)
+    keys.each do |key|
       StatusHistory.transaction do
         # first rescale a month old
-        cleanup(key, offset_from_now(12.hours.ago), 1.month.ago.to_i)
+        cleanup(key, 3600 * 12, 24 * 3600 * 30)
         # now a week old
-        cleanup(key, offset_from_now(6.hours.ago), 7.days.ago.to_i)
+        cleanup(key, 3600 * 6, 24 * 3600 * 7)
         # now rescale yesterday
-        cleanup(key, offset_from_now(1.hour.ago), 24.hours.ago.to_i)
+        cleanup(key, 3600, 24 * 3600)
         # 2h stuff
-        cleanup(key, offset_from_now(4.minutes.ago), 2.hours.ago.to_i)
+        cleanup(key, 200, 3600 * 2)
       end
     end
   end
 
   private
 
-  def distinct_status_history_keys
-    StatusHistory.distinct.pluck(:key)
+  def find_items_for_maxtime(key, maxtimeoffset)
+    maxtime = StatusHistory.maximum(:time) - maxtimeoffset
+
+    StatusHistory.where('`key` = ? and `time` < ?', key, maxtime).order(:time).to_a
   end
 
-  def offset_from_now(date)
-    (Time.now - date).to_i
+  def find_start_items(allitems, max)
+    items = []
+
+    items << allitems.shift while allitems.present? && allitems[0].time < max
+    items
   end
 
-  def find_items(key, mintime, maxtime)
-    StatusHistory.where('`key` = ? and `time` > ? and `time` < ?', key, mintime, maxtime).order(:time)
-  end
-
-  def cleanup(key, mintime, maxtime_offset)
-    # we try to make sure all keys are in the same time slots,
-    # so start with the overall time
-    allitems = find_items(key, mintime, maxtime_offset)
+  def cleanup(key, offset, maxtimeoffset)
+    # we try to make sure all keys are in the same time slots, so start with the overall time
+    allitems = find_items_for_maxtime(key, maxtimeoffset)
     return if allitems.empty?
 
-    time_average = allitems.average(:time)
-    value_average = allitems.average(:value)
-    StatusHistory.delete(allitems.map(&:id))
-    StatusHistory.create(key: key, time: time_average, value: value_average)
+    curmintime = StatusHistory.minimum(:time)
+
+    until allitems.empty?
+      items = find_start_items(allitems, curmintime + offset)
+
+      if items.length > 1
+        timeavg = curmintime + offset / 2
+        valuavg = (items.inject(0) { |sum, item| sum + item.value }).to_f / items.length
+        Rails.logger.debug "scaling #{key} #{curmintime} #{items.length} #{Time.at(timeavg)} #{valuavg}"
+        StatusHistory.delete(items.map(&:id))
+        StatusHistory.create(key: key, time: timeavg, value: valuavg)
+      end
+      curmintime += offset
+    end
   end
 end
