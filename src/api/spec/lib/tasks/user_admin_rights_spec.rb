3bd43fd8aa4367fec3d552f32f0d8ac4a8fd376b
Daniel Donisa <daniel.donisa@suse.com>
2020-12-04 16:13:43 +0100

    Add a rake task to toggle admin rights on some user
    In order to avoid repeating some mistakes made previously when dealing with data
    on production, we've automated the process of granting and removing admin rights
    to some specific user.
    
    Fixes #10524

---
+++ src/api/spec/lib/tasks/user_admin_rights_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+require 'tasks/user_admin_rights'
+
+RSpec.describe UserAdminRights do
+  let(:user) { create(:confirmed_user) }
+  let(:admin_role) { Role.global.where(title: 'Admin').first }
+
+  subject do
+    UserAdminRights.new(user).toggle!
+  end
+
+  context 'when user has no admin rights' do
+    it 'grants the admin rights' do
+      expect(subject.roles).to contain_exactly(admin_role)
+    end
+  end
+
+  context 'when the user already has admin rights' do
+    before do
+      user.roles << admin_role
+    end
+
+    it 'removes the admin rights' do
+      expect(subject.reload.roles).to be_empty
+    end
+  end
+
+  context 'when there is no user' do
+    let(:user) { nil }
+
+    it 'fails with an error' do
+      expect { subject.roles }.to raise_error(NotFoundError)
+    end
+  end
+end
