3aa67759e30caeccb099cfa3a4d5a4447a6218e0
Rubhan Azeem <rubhanazeem@users.noreply.github.com>
2021-01-24 00:15:35 +0100

    Add edit and update actions for saved replies

--- src/api/app/controllers/webui/users/saved_replies_controller.rb
+++ src/api/app/controllers/webui/users/saved_replies_controller.rb
@@ -2,6 +2,7 @@ module Webui
   module Users
     class SavedRepliesController < WebuiController
       before_action :kerberos_auth
+      before_action :set_saved_reply_by_id, only: [:edit, :update, :destroy]
 
       def index
         @saved_replies = User.session.saved_replies
@@ -14,11 +15,23 @@ module Webui
       def create
         @saved_reply = User.session.saved_replies.new(saved_reply_params)
         if @saved_reply.valid? && @saved_reply.save
-          flash[:success] = "Reply was created successfully"
+          flash[:success] = 'Reply was created successfully'
           redirect_to action: 'index'
         else
           flash[:error] = "Failed to save reply. #{@saved_reply.errors.full_messages.to_sentence}."
           render :new
+        end
+      end
+
+      def edit ; end
+
+      def update
+        if @saved_reply.update(saved_reply_params)
+          flash[:success] = 'Reply was updated successfully'
+          redirect_to action: 'index'
+        else
+          flash[:error] = "Failed to update reply. #{@saved_reply.errors.full_messages.to_sentence}."
+          render :edit
         end
       end
 
@@ -28,7 +41,11 @@ module Webui
       private
 
       def saved_reply_params
-        params.require(:saved_reply).permit(:title,:body)
+        params.require(:saved_reply).permit(:title, :body)
+      end
+
+      def set_saved_reply_by_id
+        @saved_reply = User.session.saved_replies.find(params[:id])
       end
     end
   end
