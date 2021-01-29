0dbd85ed60e2bcb1cf9fa69f9e893400242b86bb
Adrian Schröter <adrian@suse.de>
2021-01-29 18:30:11 +0100

    [api] catch error on wrong releasetarget
    we checked only for requests, but we failed for commands to check
    the setup of release targets early enough.
    
    commands crashed an error while working on the release

--- src/api/spec/controllers/webui/project_controller_spec.rb
+++ src/api/spec/controllers/webui/project_controller_spec.rb
@@ -699,7 +699,7 @@ RSpec.describe Webui::ProjectController, vcr: true do
        BsRequestAction::BuildNotFinished,
        BsRequestActionMaintenanceRelease::RepositoryWithoutReleaseTarget,
        BsRequestActionMaintenanceRelease::RepositoryWithoutArchitecture,
-       BsRequestActionMaintenanceRelease::ArchitectureOrderMissmatch,
+       ArchitectureOrderMissmatch,
        BsRequestAction::VersionReleaseDiffers,
        BsRequestAction::Errors::UnknownTargetProject,
        BsRequestAction::Errors::UnknownTargetPackage].each do |error_class|
