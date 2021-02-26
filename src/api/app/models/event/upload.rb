84356d436f41e8c3b8e5bb20c3bd483b97e21810
Adrian Schröter <adrian@suse.de>
2021-02-26 10:07:06 +0100

    [api][backend] add notification on data meta change
    OBS-82

--- src/api/app/models/event/upload.rb
+++ src/api/app/models/event/upload.rb
@@ -2,7 +2,7 @@ module Event
   class Upload < Base
     self.message_bus_routing_key = 'package.upload'
     self.description = 'Package sources were uploaded'
-    payload_keys :project, :package, :sender, :comment, :filename, :requestid, :target, :user
+    payload_keys :project, :package, :sender, :comment, :filename, :requestid, :target, :user, :meta
   end
 end
 
