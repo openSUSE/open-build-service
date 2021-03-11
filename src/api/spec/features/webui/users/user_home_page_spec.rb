a61e5a8601e83f53d9012c4bd05f86e6646d05b0
Dany Marcoux <dmarcoux@posteo.de>
2021-03-11 14:46:38 +0100

    Merge feature specs for user_home_page

--- src/api/spec/features/webui/users/user_home_page_spec.rb
+++ src/api/spec/features/webui/users/user_home_page_spec.rb
@@ -17,8 +17,8 @@ RSpec.describe "User's home project creation", type: :feature, js: true do
       expect(page).to have_css('#home-realname', text: 'Jim Knopf')
       expect(page).not_to have_css("a[href='mailto:jim.knopf@puppenkiste.com']", text: 'jim.knopf@puppenkiste.com')
 
-      expect(page).not_to have_text('Edit your account')
-      expect(page).not_to have_text('Change your password')
+      expect(page).not_to have_text('Edit Your account')
+      expect(page).not_to have_text('Change Your password')
 
       expect(page).to have_link('Involved Packages')
       expect(page).to have_link('Involved Projects')
@@ -36,8 +36,9 @@ RSpec.describe "User's home project creation", type: :feature, js: true do
       expect(page).to have_css('#home-realname', text: 'Jim Knopf')
       expect(page).to have_css("a[href='mailto:jim.knopf@puppenkiste.com']", text: 'jim.knopf@puppenkiste.com')
 
-      expect(page).to have_text('Edit your account')
-      expect(page).to have_text('Change your password')
+      within('#bottom-navigation-area') { click_link('Actions') } if mobile?
+      expect(page).to have_text('Edit Your Account')
+      expect(page).to have_text('Change Your Password')
 
       expect(page).to have_link('Involved Packages')
       expect(page).to have_link('Involved Projects')
@@ -48,6 +49,8 @@ RSpec.describe "User's home project creation", type: :feature, js: true do
       visit my_tasks_path
 
       expect(page).to have_link('Incoming Requests')
+      # TODO: Remove this line if the dropdown is changed to a scrollable tab, and responsive_ux is out of beta.
+      within('#requests') { find('.nav-link.dropdown-toggle').click } if mobile?
       expect(page).to have_link('Outgoing Requests')
       expect(page).to have_link('Declined Requests')
       expect(page).to have_link('All Requests')
@@ -56,7 +59,12 @@ RSpec.describe "User's home project creation", type: :feature, js: true do
     end
 
     it 'edit account information' do
-      click_link('Edit your account')
+      if mobile?
+        within('#bottom-navigation-area') { click_link('Actions') }
+        within('#bottom-navigation-area') { click_link('Edit Your Account') }
+      else
+        click_link('Edit Your Account')
+      end
 
       fill_in('user_realname', with: 'John Doe')
       fill_in('user_email', with: 'john.doe@opensuse.org')
