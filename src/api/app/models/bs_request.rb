a91515d136b8f93c9c224bfd655921957518c039
Dany Marcoux <dmarcoux@posteo.de>
2021-03-15 17:21:51 +0100

    Migrate requests out of responsive_ux

--- src/api/app/models/bs_request.rb
+++ src/api/app/models/bs_request.rb
@@ -1176,7 +1176,7 @@ class BsRequest < ApplicationRecord
     review_comment
   end
 
-  # TODO: Remove once responsive_ux is out of beta
+  # TODO: Once the feature flag notifications_redesign is removed, remove this method if we decide to delete the "Tasks" page.
   def update_cache
     target_package_ids = bs_request_actions.with_target_package.pluck(:target_package_id)
     target_project_ids = bs_request_actions.with_target_project.pluck(:target_project_id)
