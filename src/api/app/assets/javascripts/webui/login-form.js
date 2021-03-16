c8eece9b502cd2aa1412db54358ba47351da1e7a
Saray Cabrera Padrón <scabrerapadron@suse.de>
2021-03-16 15:25:58 +0100

    Remove remains of old login form

--- src/api/app/assets/javascripts/webui/login-form.js
+++
@@ -1,6 +0,0 @@
-// responsive_ux: remove after migration to responsive_ux
-$(document).ready(function() {
-  $('#login-form-dropdown').on('shown.bs.dropdown', function() {
-    $("#username").focus();
-  });
-});
