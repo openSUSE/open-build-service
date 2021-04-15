27ab3e6c30b8c4e309822fff64795c2f7cbd05ec
Victor Pereira <vpereira@suse.de>
2021-04-15 15:45:54 +0200

    WIP initial commit

--- src/api/app/controllers/trigger_controller.rb
+++ src/api/app/controllers/trigger_controller.rb
@@ -3,9 +3,11 @@ class TriggerController < ApplicationController
   validate_action release: { method: :post, response: :status }
   validate_action runservice: { method: :post, response: :status }
 
+  before_action :validate_token, :set_package, :set_user, only: [:create]
+
   before_action :disallow_project_param, only: [:release]
 
-  before_action :extract_auth_from_request, :validate_auth_token, :require_valid_token
+  before_action :extract_auth_from_request, :validate_auth_token, :require_valid_token, except: [:create]
   #
   # Authentication happens with tokens, so no login is required
   #
@@ -52,6 +54,18 @@ class TriggerController < ApplicationController
 
     @pkg.sources_changed
   end
+
+  def create
+    if !@user.is_active? || !@user.can_modify?(@package)
+      render_error message: 'Token not found or not valid.', status: 404
+      return
+    end
+
+    Backend::Api::Sources::Package.trigger_services(@package.project.name, @package.name, @user.login)
+    render_ok
+  end
+
+
 
   private
 
@@ -100,5 +114,38 @@ class TriggerController < ApplicationController
              end
       @pkg = Package.get_by_project_and_name(params[:project].to_s, params[:package].to_s, opts)
     end
+  end
+
+
+  def set_package
+    @package = @token.package || Package.get_by_project_and_name(params[:project], params[:package], use_source: true)
+  end
+
+  def validate_token
+    @token = Token::Service.find_by(id: params[:id])
+    return if @token && @token.valid_signature?(signature, request.body.read)
+
+    render_error message: 'Token not found or not valid.', status: 403
+    false
+  end
+
+  def set_user
+    @user = @token.user
+  end
+
+  # To trigger the webhook, the sender needs to
+  # generate a signature with a secret token.
+  # The signature needs to be generated over the
+  # payload of the HTTP request and stored
+  # in a HTTP header.
+  # GitHub: HTTP_X_HUB_SIGNATURE
+  # https://developer.github.com/webhooks/securing/
+  # Pagure: HTTP_X-Pagure-Signature-256
+  # https://docs.pagure.org/pagure/usage/using_webhooks.html
+  # Custom signature: HTTP_X_OBS_SIGNATURE
+  def signature
+    request.env['HTTP_X_OBS_SIGNATURE'] ||
+      request.env['HTTP_X_HUB_SIGNATURE'] ||
+      request.env['HTTP_X-Pagure-Signature-256']
   end
 end
