9bb7779512e2c5d3a372b6412f74992c73a92145
Dany Marcoux <dmarcoux@posteo.de>
2021-04-22 14:54:02 +0200

    Move associations and trait inside base token factory

--- src/api/spec/factories/tokens.rb
+++ src/api/spec/factories/tokens.rb
@@ -1,14 +1,15 @@
 FactoryBot.define do
   factory :token do
     string { Faker::Lorem.characters(number: 32) }
+    user
+    package
+
+    trait :with_package_from_association_or_param do
+      package_from_association_or_params { package }
+    end
 
     factory :service_token, class: 'Token::Service' do
       type { 'Token::Service' }
-      user
-      package
-      trait :with_package_from_association_or_param do
-        package_from_association_or_params { package }
-      end
     end
 
     factory :rss_token, class: 'Token::Rss' do
@@ -17,20 +18,10 @@ FactoryBot.define do
 
     factory :rebuild_token, class: 'Token::Rebuild' do
       type { 'Token::Rebuild' }
-      user
-      package
-      trait :with_package_from_association_or_param do
-        package_from_association_or_params { package }
-      end
     end
 
     factory :release_token, class: 'Token::Release' do
       type { 'Token::Release' }
-      user
-      package
-      trait :with_package_from_association_or_param do
-        package_from_association_or_params { package }
-      end
     end
   end
 end
