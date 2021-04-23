78d690c9fa11a519982a2cd030727266006625a4
Victor Pereira <vpereira@suse.de>
2021-04-23 13:23:33 +0200

    Refactor Token policies to use shared examples

--- src/api/spec/policies/token/rebuild_policy_spec.rb
+++ src/api/spec/policies/token/rebuild_policy_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Token::RebuildPolicy do
-  let!(:user) { create(:confirmed_user, login: 'foo') }
-  let!(:project) { create(:project, maintainer: user) }
-  let!(:package) { create(:package, project: project) }
-  let!(:other_user) { build(:confirmed_user, login: 'bar') }
-  let!(:rebuild_token) { create(:rebuild_token, :with_package_from_association_or_param, user: user, package: package) }
-  let!(:other_user_rebuild_token) { create(:rebuild_token, :with_package_from_association_or_param, user: other_user) }
-
   subject { described_class }
 
   describe '#create' do
-    permissions :create? do
-      it { expect(subject).to permit(user, rebuild_token) }
-      it { expect(subject).not_to permit(other_user, other_user_rebuild_token) }
+    context 'user inactive' do
+      let(:user_token) { create(:rebuild_token, :with_package_from_association_or_param, user: user) }
+
+      include_examples 'non-active users cannot use a token'
+    end
+
+    context 'user active' do
+      let(:user_token) { create(:rebuild_token, :with_package_from_association_or_param, user: user, package: package) }
+      let(:other_user_token) { create(:rebuild_token, :with_package_from_association_or_param, user: other_user) }
+
+      include_examples 'active users token basic tests'
     end
   end
 end
