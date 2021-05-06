db6b5484265326ee155b2f98cd74d2b3daa66589
Dany Marcoux <dmarcoux@posteo.de>
2021-05-06 17:03:18 +0200

    Create Workflow model
    Co-authored-by: Daniel Donisa <daniel.donisa@suse.com>
    Co-authored-by: Eduardo Navarro <enavarro@suse.com>
    Co-authored-by: Lukas Krause <lkrause@suse.de>

--- src/api/app/models/workflow.rb
+++ src/api/app/models/workflow.rb
@@ -1,6 +1,31 @@
 class Workflow
   include ActiveModel::Model
 
-  # TODO: Placeholder to be able to test the YAMLToWorkflowsService. The code for this class will be added in another PR.
-  def initialize(workflow:, scm_extractor_payload:); end
+  SUPPORTED_EVENTS = %w[pull_request].freeze
+  SUPPORTED_STEPS = { 'branch_package' => Workflow::Step::BranchPackageStep }.freeze
+
+  validates :event, inclusion: { in: SUPPORTED_EVENTS, allow_nil: false }
+
+  def initialize(workflow:, scm_extractor_payload:)
+    @workflow = workflow
+    @scm_extractor_payload = scm_extractor_payload
+  end
+
+  def event
+    @workflow['filters']['event']
+  end
+
+  def steps
+    steps = []
+
+    @workflow['steps'].each do |step|
+      step.each do |step_name, step_instructions|
+        next if SUPPORTED_STEPS[step_name].blank?
+
+        new_step = SUPPORTED_STEPS[step_name].new(step_instructions: step_instructions, scm_extractor_payload: @scm_extractor_payload)
+        steps << new_step if new_step.allowed_event_and_action?
+      end
+    end
+    steps
+  end
 end
