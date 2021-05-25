441d71f5cc472bedc3df32d0e51a7599c544f936
Lukas Krause <lkrause@suse.de>
2021-05-25 17:54:08 +0200

    Ignore package_id column in events table
    Preperation for the db migration in order to remove the package_id
    column from the events table.
    See https://github.com/ankane/strong_migrations#removing-a-column

--- src/api/app/models/event/base.rb
+++ src/api/app/models/event/base.rb
@@ -4,6 +4,7 @@ module Event
   class Base < ApplicationRecord
     self.inheritance_column = 'eventtype'
     self.table_name = 'events'
+    self.ignored_columns = ['package_id']
 
     after_create :create_project_log_entry_job, if: -> { (PROJECT_CLASSES | PACKAGE_CLASSES).include?(self.class.name) }
 
