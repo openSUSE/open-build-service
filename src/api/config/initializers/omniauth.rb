dd65c4c97afa7347713411a1ce70b159de4f2eb6
Andrej Shadura <andrew.shadura@collabora.co.uk>
2021-05-27 13:43:22 +0200

    Parse SSO authentication config on startup
    Signed-off-by: Andrej Shadura <andrew.shadura@collabora.co.uk>
    
    Gbp-Pq: Topic collabora/sso
    Gbp-Pq: Name Parse-SSO-authentication-config-on-startup.patch

---
+++ src/api/config/initializers/omniauth.rb
@@ -0,0 +1,20 @@
+OmniAuth.config.path_prefix = '/session/sso'
+
+path = Rails.root.join("config", "auth.yml")
+
+CONFIG['sso_auth'] = Hash.new
+
+if File.exist? path
+  begin
+      CONFIG['sso_auth'] = YAML.load_file(path)
+  rescue Exception
+      puts "Error while parsing config file #{path}"
+  end
+
+  Rails.application.config.middleware.use OmniAuth::Builder do
+      CONFIG['sso_auth'].each do |name, options|
+          options[:name] = name
+          provider (options['strategy'] || name), options
+      end
+  end
+end
