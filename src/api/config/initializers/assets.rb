79347861dcb564f912de0d7c0a4d95ae37269768
Dany Marcoux <dmarcoux@posteo.de>
2021-06-16 16:55:41 +0200

    Apply changes in initializers from Rails' update task
    Merging with the code of existing initializers

--- src/api/config/initializers/assets.rb
+++ src/api/config/initializers/assets.rb
@@ -3,9 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Rails.root.join('node_modules')
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
 Rails.application.config.assets.precompile += ['webui.js']
