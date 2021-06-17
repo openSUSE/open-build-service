2dba9fd89ce9a97b5eea5cd7f97a899930a2b9d8
Dany Marcoux <dmarcoux@posteo.de>
2021-06-17 15:48:17 +0200

    Pass file extension to "render" call in FlipperFeature
    This change wasn't needed in Rails 6.0.x, but it is somehow in Rails
    6.1.x.

--- src/api/app/mixins/flipper_feature.rb
+++ src/api/app/mixins/flipper_feature.rb
@@ -2,6 +2,6 @@ module FlipperFeature
   def feature_enabled?(feature)
     return if Flipper.enabled?(feature.to_sym, User.possibly_nobody)
 
-    render file: Rails.root.join('public/404'), status: :not_found, layout: false
+    render file: Rails.root.join('public/404.html'), status: :not_found, layout: false
   end
 end
