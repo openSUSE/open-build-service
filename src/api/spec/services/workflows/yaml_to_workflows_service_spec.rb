04b8ec93595bef2c187bdffd86f06aeab9e1faa3
Victor Pereira <vpereira@suse.de>
2021-07-06 15:30:12 +0200

    Handle the Psych::SyntaxError exception in case workflows.yml isnt a valid YAML

--- src/api/spec/services/workflows/yaml_to_workflows_service_spec.rb
+++ src/api/spec/services/workflows/yaml_to_workflows_service_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Workflows::YAMLToWorkflowsService, type: :service do
   include_context 'a scm payload hash'
   let(:workflows_yml_file) { File.expand_path(Rails.root.join('spec/support/files/workflows.yml')) }
+  let(:unparsable_workflows_yml_file) { File.expand_path(Rails.root.join('spec/support/files/unparsable_workflows.yml')) }
   let(:token) { create(:workflow_token) }
 
   describe '#call' do
@@ -17,6 +18,13 @@ RSpec.describe Workflows::YAMLToWorkflowsService, type: :service do
       it 'initializes a workflow object' do
         service = Workflows::YAMLToWorkflowsService.new(yaml_file: workflows_yml_file, scm_extractor_payload: github_extractor_payload, token: token)
         expect(service.call.first).to be_a(Workflow)
+      end
+    end
+
+    context 'with a invalid workflows.yml' do
+      it 'raises a user-friendly error' do
+        service = Workflows::YAMLToWorkflowsService.new(yaml_file: unparsable_workflows_yml_file, scm_extractor_payload: github_extractor_payload, token: token)
+        expect { service.call }.to raise_error(Token::Errors::WorkflowsYamlNotParsable)
       end
     end
   end
