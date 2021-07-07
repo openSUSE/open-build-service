8e5aca5b94f821578f5ac1f57566a0cb3ba68637
Rubhan Azeem <rubhanazeem@gmail.com>
2021-07-07 12:48:51 +0200

    Add data migration to handle old event_subscriptions

---
+++ src/api/db/data/20210707102506_add_api_endpoints_to_payload.rb
@@ -0,0 +1,21 @@
+class AddApiEndpointsToPayload < ActiveRecord::Migration[6.1]
+  def up
+    EventSubscription.where(channel: 'scm').each do |event_sub|
+      next if event_sub.payload['api_endpoint'].present?
+
+      if event_sub.payload['scm'] == 'github'
+        event_sub.payload['api_endpoint'] = 'https://api.github.com'
+      else
+        http_url = event_sub.payload['http_url']
+        uri = URI.parse(http_url)
+        api_endpoint = "#{uri.scheme}://#{uri.host}"
+        event_sub.payload['api_endpoint'] = api_endpoint
+      end
+      event_sub.save!
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
