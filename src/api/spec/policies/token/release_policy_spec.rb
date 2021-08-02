b9220808bebdc51e3a01bd07720c657febc78d80
Eduardo Navarro <enavarro@suse.com>
2021-08-02 10:27:24 +0200

    Modify Token policy tests

--- src/api/spec/policies/token/release_policy_spec.rb
+++ src/api/spec/policies/token/release_policy_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe Token::ReleasePolicy do
   subject { described_class }
 
-  describe '#create' do
+  describe '#trigger' do
     context 'user inactive' do
       let(:user_token) { create(:release_token, user: user) }
 
-      include_examples 'non-active users cannot use a token'
+      include_examples 'non-active users cannot trigger a token'
     end
 
     context 'user active' do
       let(:user_token) { create(:release_token, user: user, package: package) }
       let(:other_user_token) { create(:release_token, user: other_user) }
 
-      include_examples 'active users token basic tests'
+      include_examples 'active users can trigger a token'
     end
   end
 end
