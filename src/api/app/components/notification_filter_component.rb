cc54af41f360504112cacd5f62d7d417ca831c93
Lukas Krause <lkrause@suse.de>
2021-09-16 10:56:41 +0200

    Turn NotificationFilterPresenter into a view component
    Presenters and view components are really similar, but the template code
    (Haml) is testable in view components and not in presenters. Having
    design patterns which are too similar is confusing. This is why we're
    retiring presenters in favor of view components.

---
+++ src/api/app/components/notification_filter_component.rb
@@ -0,0 +1,20 @@
+class NotificationFilterComponent < ApplicationComponent
+  def initialize(selected_filter:, projects_for_filter: ProjectsForFilterFinder.new.call, groups_for_filter: GroupsForFilterFinder.new.call)
+    super
+
+    @projects_for_filter = projects_for_filter
+    @groups_for_filter = groups_for_filter
+    @count = notifications_count
+    @selected_filter = selected_filter
+  end
+
+  # TODO: Turn this into a query object `NotificationsCounter` and pass this as a default value
+  #       to a keyword argument `count` in the `initialize` method
+  def notifications_count
+    finder = NotificationsFinder.new(User.session.notifications.for_web)
+    counted_notifications = finder.unread.group(:notifiable_type).count
+    counted_notifications['incoming_requests'] = finder.for_incoming_requests.count
+    counted_notifications['outgoing_requests'] = finder.for_outgoing_requests.count
+    counted_notifications.merge!('unread' => User.session.unread_notifications)
+  end
+end
