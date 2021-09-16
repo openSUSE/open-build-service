9dedf071e95f91a84a6065971cfe1bbf4bb3d597
Lukas Krause <lkrause@suse.de>
2021-09-16 10:56:41 +0200

    Turn NotificationFilterPresenter into a view component
    Presenters and view components are really similar, but the template code
    (Haml) is testable in view components and not in presenters. Having
    design patterns which are too similar is confusing. This is why we're
    retiring presenters in favor of view components.

---
+++ src/api/spec/components/notification_filter_component_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe NotificationFilterComponent, type: :component do
+  let(:user) { create(:user) }
+
+  before do
+    User.session = user
+  end
+
+  context 'without projects and groups notifications' do
+    before do
+      render_inline(described_class.new(selected_filter: { type: 'unread' }))
+    end
+
+    ['Unread', 'Read', 'Comments', 'Requests', 'Incoming Requests', 'Outgoing Requests'].each do |filter_name|
+      it "displays a '#{filter_name}' filter" do
+        expect(rendered_component).to have_link(filter_name)
+      end
+    end
+
+    it "doesn't display project filters" do
+      expect(rendered_component).not_to have_css('h5', text: 'Projects')
+    end
+
+    it "doesn't display group filters" do
+      expect(rendered_component).not_to have_css('h5', text: 'Groups')
+    end
+  end
+
+  context 'with projects and groups notifications' do
+    let!(:notification_for_projects_comment) { create(:web_notification, :comment_for_project, subscriber: user) }
+    let(:project) { notification_for_projects_comment.notifiable.commentable }
+    let!(:group) { create(:groups_user, user: user, group: create(:group, title: 'Les_Colocs')).group }
+
+    before do
+      project.notifications << notification_for_projects_comment
+      group.created_notifications << notification_for_projects_comment
+      render_inline(described_class.new(selected_filter: { type: 'unread' }))
+    end
+
+    ['Unread', 'Read', 'Comments', 'Requests', 'Incoming Requests', 'Outgoing Requests'].each do |filter_name|
+      it "displays a '#{filter_name}' filter" do
+        expect(rendered_component).to have_link(filter_name)
+      end
+    end
+
+    it 'displays project filters' do
+      expect(rendered_component).to have_css('h5', text: 'Projects')
+      expect(rendered_component).to have_link(project.name)
+    end
+
+    it 'displays group filters' do
+      expect(rendered_component).to have_css('h5', text: 'Groups')
+      expect(rendered_component).to have_link(group.title)
+    end
+  end
+end
