9ca20d6ab97c8df094151b56cfea0d9ef959072c
Saray Cabrera Padrón <scabrerapadron@suse.de>
2021-09-17 09:38:52 +0200

    Modify Triggerable to not use params
    Since it's going to be used from a model too.
    Whatever use it, should implement project_name and package_name methods.

--- src/api/app/controllers/concerns/triggerable.rb
+++ src/api/app/controllers/concerns/triggerable.rb
@@ -4,11 +4,11 @@ module Triggerable
   def set_project
     # By default we operate on the package association
     @project = @token.package.try(:project)
-    # If the token has no package, let's find one from the parameters
-    @project ||= Project.get_by_name(params[:project])
+    # If the token has no package, let's find one from the parameters or the step intructions
+    @project ||= Project.get_by_name(@project_name)
     # Remote projects are read-only, can't trigger something for them.
     # See https://github.com/openSUSE/open-build-service/wiki/Links#project-links
-    raise Project::Errors::UnknownObjectError, "Sorry, triggering tokens for remote project \"#{params[:project]}\" is not possible." unless @project.is_a?(Project)
+    raise Project::Errors::UnknownObjectError, "Sorry, triggering tokens for remote project \"#{@project_name}\" is not possible." unless @project.is_a?(Project)
   end
 
   def set_package
@@ -16,14 +16,14 @@ module Triggerable
     @package = @token.package
     # If the token has no package, let's find one from the parameters
     @package ||= Package.get_by_project_and_name(@project,
-                                                 params[:package],
+                                                 @package_name,
                                                  @token.package_find_options)
     return unless @project.links_to_remote?
 
     # The token has no package, we did not find a package in the database but the project has a link to remote.
     # See https://github.com/openSUSE/open-build-service/wiki/Links#project-links
     # In this case, we will try to trigger with the user input, no matter what it is
-    @package ||= params[:package]
+    @package ||= @package_name
     # TODO: This should not happen right? But who knows...
     raise ActiveRecord::RecordNotFound unless @package
   end
@@ -34,7 +34,7 @@ module Triggerable
 
   def set_multibuild_flavor
     # Do NOT use @package.multibuild_flavor? here because the flavor need to be checked for the right source revision
-    @multibuild_container = params[:package].gsub(/.*:/, '') if params[:package].present? && params[:package].include?(':')
+    @multibuild_container = @package_name.gsub(/.*:/, '') if @package_name.present? && @package_name.include?(':')
   end
 
   def package_from_project_link?
