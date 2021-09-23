4b9927a04aa3ba072f24d2b3d55cffbbd0b4e84c
Rubhan Azeem <rubhanazeem@gmail.com>
2021-09-23 11:33:16 +0200

    Fix rubocop offenses for RSpec/ExcessiveDocstringSpacing

--- src/api/spec/routing/webui/projects/public_key_spec.rb
+++ src/api/spec/routing/webui/projects/public_key_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ' projects/:project/public_key routes' do
+RSpec.describe 'projects/:project/public_key routes' do
   it do
     expect(get('/projects/TestProject/public_key'))
       .to route_to('webui/projects/public_key#show', project_name: 'TestProject')
--- src/api/spec/routing/webui/projects/ssl_certificate_spec.rb
+++ src/api/spec/routing/webui/projects/ssl_certificate_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ' projects/:project/ssl_certificate routes' do
+RSpec.describe 'projects/:project/ssl_certificate routes' do
   it do
     expect(get('/projects/TestProject/ssl_certificate'))
       .to route_to('webui/projects/ssl_certificate#show', project_name: 'TestProject')
