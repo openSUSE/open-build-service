a8e3325f26dfd8a0d627783a02783f62ba4e22a1
Rubhan Azeem <rubhanazeem@gmail.com>
2021-10-01 17:09:36 +0200

    Add view component for notification action bar

---
+++ src/api/app/components/notification_action_bar_component.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class NotificationActionBarComponent < ApplicationComponent
+  attr_accessor :type, :update_path, :show_read_all_button
+
+  def initialize(type:, update_path:, show_read_all_button: false)
+    super
+
+    @type = type
+    @update_path = add_params(update_path)
+    @show_read_all_button = show_read_all_button
+  end
+
+  def button_text(all: false)
+    text = type == 'read' ? 'Unread' : 'Read'
+    if all
+      "Mark all as '#{text}'"
+    else
+      "Mark selected as '#{text}'"
+    end
+  end
+
+  def disable_with_content(all: false)
+    spinner = tag.i(class: 'fas fa-spinner fa-spin ml-2')
+    button_text(all: all) + spinner
+  end
+
+  private
+
+  def add_params(path)
+    return path + '&update_all=true' if path.include?('?')
+
+    path + '?update_all=true'
+  end
+end
--- src/api/app/models/notification.rb
+++ src/api/app/models/notification.rb
@@ -1,6 +1,7 @@
 class Notification < ApplicationRecord
   MAX_RSS_ITEMS_PER_USER = 10
   MAX_RSS_ITEMS_PER_GROUP = 10
+  MAX_PER_PAGE = 300
 
   belongs_to :subscriber, polymorphic: true
   belongs_to :notifiable, polymorphic: true
