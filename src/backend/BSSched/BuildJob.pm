6c6877dab7b4043dfb8414efcfc66bb837469f21
Adrian Schröter <adrian@suse.de>
2021-10-04 15:50:06 +0200

    [backend] add hdrmd5 as part of buildinfo
    osc needs it to validate it's cache for repositories which do not increase
    build numbers on rebuild.

--- src/backend/BSSched/BuildJob.pm
+++ src/backend/BSSched/BuildJob.pm
@@ -1161,6 +1161,7 @@ sub create {
         $_->{'version'}    = $d->{'version'};
         $_->{'release'}    = $d->{'release'} if defined $d->{'release'};
         $_->{'arch'}       = $d->{'arch'} if $d->{'arch'};
+        $_->{'hdrmd5'}     = $d->{'hdrmd5'} if $d->{'hdrmd5'};
         $_->{'preimghdrmd5'} = $d->{'hdrmd5'} if !$_->{'noinstall'} && $d->{'hdrmd5'};
 	$_->{'repoarch'}   = $BSConfig::localarch if $myarch eq 'local' && $BSConfig::localarch;
       }
