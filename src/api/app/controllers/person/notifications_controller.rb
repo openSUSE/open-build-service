d9f2a53e171085ceb3e8e94221bb4cd894771127
Victor Pereira <vpereira@suse.de>
2021-10-08 10:56:17 +0200

    Add check for feature flag and beta to the notifications API endpoint

--- src/api/app/controllers/person/notifications_controller.rb
+++ src/api/app/controllers/person/notifications_controller.rb
@@ -1,6 +1,15 @@
 module Person
   class NotificationsController < ApplicationController
+    # TODO
+    # rename it
+    # move to the right errors.rb
+    class NotFoundError < APIError
+      setup 'not_found', 404, 'Make sure you are in the beta program'
+    end
+
     MAX_PER_PAGE = 300
+
+    before_action :check_feature_and_beta_toggles
 
     # GET /my/notifications
     def index
@@ -29,6 +38,10 @@ module Person
       notifications = fetch_notifications
       params[:page] = notifications.page(params[:page]).total_pages if notifications.page(params[:page]).out_of_range?
       params[:show_all] ? show_all(notifications) : notifications.page(params[:page])
+    end
+
+    def check_feature_and_beta_toggles
+      raise NotFoundError unless Flipper.enabled?(:notifications_redesign, User.session) && User.session.in_beta?
     end
   end
 end
