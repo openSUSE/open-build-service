7603d31498048f8bbe84775cf842f08a128ba01b
Michael Schroeder <mls@suse.de>
2021-10-11 11:10:17 +0200

    [backend] KiwiProduct/Image/Docker: reuse the context pool if possible

--- src/backend/BSSched/BuildJob/KiwiProduct.pm
+++ src/backend/BSSched/BuildJob/KiwiProduct.pm
@@ -155,36 +155,41 @@ sub check {
   my %dep2pkg;
   if ($myarch eq $buildarch || $myarch eq $localbuildarch) {
     # calculate packages needed for building
-    $pool = BSSolv::pool->new();
-    $pool->settype('deb') if $bconf->{'binarytype'} eq 'deb';
-    $pool->setmodules($bconf->{'modules'}) if $bconf->{'modules'} && defined &BSSolv::pool::setmodules;
-    my $delayed_errors = '';
-    for my $aprp (@bprps) {
-      if (!$ctx->checkprpaccess($aprp)) {
-	my $error = "repository '$aprp' is unavailable";
-	if ($ctx->{'verbose'}) {
-	  print "      - $packid (kiwi-product)\n";
-	  print "        $error\n";
+    if ($ctx->{'pool'} && BSUtil::identical(\@bprps, $ctx->{'prpsearchpath'})) {
+      $pool = $ctx->{'pool'};	# we can reuse the ctx pool, nice!
+    } else {
+      $pool = BSSolv::pool->new();
+      $pool->settype('deb') if $bconf->{'binarytype'} eq 'deb';
+      $pool->settype('arch') if $bconf->{'binarytype'} eq 'arch';
+      $pool->setmodules($bconf->{'modules'}) if $bconf->{'modules'} && defined &BSSolv::pool::setmodules;
+      my $delayed_errors = '';
+      for my $aprp (@bprps) {
+	if (!$ctx->checkprpaccess($aprp)) {
+	  my $error = "repository '$aprp' is unavailable";
+	  if ($ctx->{'verbose'}) {
+	    print "      - $packid (kiwi-product)\n";
+	    print "        $error\n";
+	  }
+	  return ('broken', $error);
 	}
-	return ('broken', $error);
+	my $r = $ctx->addrepo($pool, $aprp, $localbuildarch);
+	if (!$r) {
+	  my $error = "repository '$aprp' is unavailable";
+	  $error .= " (delayed)" if defined $r;
+	  if ($ctx->{'verbose'}) {
+	    print "      - $packid (kiwi-product)\n";
+	    print "        $error\n";
+	  }
+	  if (defined $r) {
+	    $delayed_errors .= ", $error";
+	    next;
+	  }
+	  return ('broken', $error);
+	}
       }
-      my $r = $ctx->addrepo($pool, $aprp, $localbuildarch);
-      if (!$r) {
-	my $error = "repository '$aprp' is unavailable";
-	$error .= " (delayed)" if defined $r;
-	if ($ctx->{'verbose'}) {
-	  print "      - $packid (kiwi-product)\n";
-	  print "        $error\n";
-	}
-	if (defined $r) {
-	  $delayed_errors .= ", $error";
-	  next;
-	}
-	return ('broken', $error);
-      }
+      return ('delayed', substr($delayed_errors, 2)) if $delayed_errors;
+      $pool->createwhatprovides();
     }
-    return ('delayed', substr($delayed_errors, 2)) if $delayed_errors;
-    $pool->createwhatprovides();
     my $xp = BSSolv::expander->new($pool, $bconf);
     no warnings 'redefine';
     local *Build::expand = sub { $_[0] = $xp; goto &BSSolv::expander::expand; };
