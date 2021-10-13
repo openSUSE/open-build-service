5bcc878c782665016506339310f965871d6bf18d
Michael Schroeder <mls@suse.de>
2021-10-13 16:16:55 +0200

    [backend] Refactor extra pool handling for Kiwi/Docker builds

--- src/backend/BSSched/BuildJob/KiwiImage.pm
+++ src/backend/BSSched/BuildJob/KiwiImage.pm
@@ -114,44 +114,13 @@ sub check {
   if ($ctx->{'pool'} && !$unorderedrepos && BSUtil::identical(\@aprps, $ctx->{'prpsearchpath'})) {
     $pool = $ctx->{'pool'};	# we can reuse the ctx pool, nice!
   } else {
-    $pool = BSSolv::pool->new();
-    $pool->settype('deb') if $bconf->{'binarytype'} eq 'deb';
-    $pool->settype('arch') if $bconf->{'binarytype'} eq 'arch';
-    $pool->setmodules($bconf->{'modules'}) if $bconf->{'modules'} && defined &BSSolv::pool::setmodules;
-
-    my $delayed_errors = '';
-    for my $aprp (@aprps) {
-      if (!$ctx->checkprpaccess($aprp)) {
-	if ($ctx->{'verbose'}) {
-	  print "      - $packid (kiwi-image)\n";
-	  print "        repository $aprp is unavailable";
-	}
-	return ('broken', "repository $aprp is unavailable");
-      }
-      my $r = $ctx->addrepo($pool, $aprp);
-      if (!$r) {
-	my $error = "repository '$aprp' is unavailable";
-	if (defined $r) {
-	  $error .= " (delayed)";
-	  $delayed_errors .= ", $error";
-	  next;
-	}
-	if ($ctx->{'verbose'}) {
-	  print "      - $packid (kiwi-image)\n";
-	  print "        $error\n";
-	}
-	return ('broken', $error);
-      }
+    my ($error, $delayed);
+    ($pool, $error, $delayed) = BSSched::BuildJob::createextrapool($ctx, $bconf, \@aprps, $unorderedrepos, \%aprpprios);
+    if ($error && $ctx->{'verbose'}) {
+      print "      - $packid (kiwi-image)\n";
+      print $delayed ? "        $error (delayed)\n" : "        $error\n";
     }
-    return ('delayed', substr($delayed_errors, 2)) if $delayed_errors;
-
-    if ($unorderedrepos) {
-      return('broken', 'perl-BSSolv does not support unordered repos') unless defined &BSSolv::repo::setpriority;
-      $_->setpriority($aprpprios{$_->name()} || 0) for $pool->repos();
-      $pool->createwhatprovides(1);
-    } else {
-      $pool->createwhatprovides();
-    }
+    return (($delayed ? 'delayed' : 'broken'), $error) if $error;
   }
 
   my $bconfignore = $bconf->{'ignore'};
@@ -174,17 +143,10 @@ sub check {
     # setup container pool
     $cpool = $ctx->{'pool'};
     if (@{$info->{'containerpath'} || []}) {
-      $cpool = BSSolv::pool->new();
       my @cprps = map {"$_->{'project'}/$_->{'repository'}"} @{$info->{'containerpath'}};
-      for my $aprp (@cprps) {
-        return ('broken', "repository $aprp is unavailable") if !$ctx->checkprpaccess($aprp);
-        my $r = $ctx->addrepo($cpool, $aprp);
-        if (!$r) {
-	  return ('delayed', "repository '$aprp' is unavailable (delayed)") if defined $r;
-	  return ('broken', "repository '$aprp' is unavailable");
-        }
-      }
-      $cpool->createwhatprovides();
+      my ($error, $delayed);
+      ($cpool, $error, $delayed) = BSSched::BuildJob::createextrapool($ctx, undef, \@cprps);
+      return (($delayed ? 'delayed' : 'broken'), $error) if $error;
     }
 
     # expand the container dependency
