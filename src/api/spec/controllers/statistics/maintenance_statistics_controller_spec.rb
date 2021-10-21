1be7a4c7945347f2a95a4227a446523711c8b51e
Dani Donisa <dani.doni@gmail.com>
2021-10-21 10:12:21 +0200

    Remove decorators by refactoring them out of existence

--- src/api/spec/controllers/statistics/maintenance_statistics_controller_spec.rb
+++ src/api/spec/controllers/statistics/maintenance_statistics_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 require 'webmock/rspec'
 
 RSpec.describe Statistics::MaintenanceStatisticsController, type: :controller do
+  render_views
+
   describe 'GET #index' do
     context 'with a project with maintenance statistics' do
       include_context 'a project with maintenance statistics'
@@ -20,6 +22,9 @@ RSpec.describe Statistics::MaintenanceStatisticsController, type: :controller do
         it 'assigns the maintenance_statistics array to an instance variable' do
           expect(assigns[:maintenance_statistics]).to be_an(Array)
         end
+
+        it { expect(response.body).to include("<maintenanceincident project=\"#{project.name}\">") }
+        it { expect(response.body).to include("<entry type=\"project_created\" when=\"#{project.created_at}\"/>") }
       end
 
       context 'but access got disabled' do
