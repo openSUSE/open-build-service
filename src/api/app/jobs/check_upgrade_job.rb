027ffbb410a9f7d20db15f3925769f57093696a8
Domenico Panella <pandom79@gmail.com>
2021-10-25 15:00:39 +0200

    Apply suggested changes

--- src/api/app/jobs/check_upgrade_job.rb
+++ src/api/app/jobs/check_upgrade_job.rb
@@ -1,7 +1,7 @@
 class CheckUpgradeJob < ApplicationJob
   
   def perform
-    puts "Running check upgrade job ...."
+    Rails.logger.info "Running check upgrade job ...."
     #Get OBS Instance Super User
     user = User.find_by(realname: 'OBS Instance Superuser', state: 'confirmed')
     return if user.blank?
@@ -16,17 +16,16 @@ class CheckUpgradeJob < ApplicationJob
         if document.present?
           Xmlhash.parse(document).elements('service').each do |service|
             if service['name'] == 'check_upgrade'
-              puts "Check_upgrade found!"
-              print "Running all services for "
-              print package.name
-              print "....\n"
+              Rails.logger.info "Check_upgrade found!"
+              Rails.logger.info "Running all services for "
+              Rails.logger.info package.name
               Backend::Api::Sources::Package.run_service(project.name, package.name, user.login)
             end  
           end
         end
       end
     end
-    puts "Check upgrade job finished!"
+    Rails.logger.info "Check upgrade job finished!"
   end
 
 end
