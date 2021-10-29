c65cd02892705d49e6186b19d266b843d33e0fe4
Domenico Panella <pandom79@gmail.com>
2021-10-29 13:09:01 +0200

    Verify call to check_upgrade script

--- src/api/app/jobs/check_upgrade_job.rb
+++ src/api/app/jobs/check_upgrade_job.rb
@@ -13,49 +13,12 @@ class CheckUpgradeJob < ApplicationJob
     end
 
     packages.each do |package|
-
       result = Backend::Api::Sources::Package.check_upgrade(package.project.name, package.name, user)
-
       puts "Result = ", result
-
-=begin
-      #Initializing the script input parameters
-      urlsrc = nil
-      regexurl = nil
-      regexver = nil
-      currentver = nil
-      separator = "."
-      debug = "false"
-
-      #Get all services
-      document = Backend::Api::Sources::Package.service(package.project.name, package.name)
-      if document.present?
-        Xmlhash.parse(document).elements('service').each do |service|
-          if service['name'] == 'check_upgrade'
-            #Preparing parameters...
-            service['param'].each do |param|
-                case param['name']
-                  when 'urlsrc'
-                    urlsrc = param['_content']                
-                  when 'regexurl'
-                    regexurl = param['_content']                
-                  when 'regexver'
-                    regexver = param['_content']                
-                  when 'currentver'
-                    currentver = param['_content']                
-                  when 'separator'
-                    separator = param['_content']                
-                  when 'debug'
-                    debug = param['_content']                
-                end
-            end
-          end  
-        end
-      end
-=end      
     end
 
     Rails.logger.info "Check upgrade job finished!"
+    
   end
 
 end
