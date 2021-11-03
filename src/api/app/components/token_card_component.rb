cba4e437467ae52c3849927e2e88833d417a3b7e
Lukas Krause <lkrause@suse.de>
2021-11-03 16:04:35 +0100

    Refactor out table of token index UI
    We want to move away from simply showing db data in a table.

---
+++ src/api/app/components/token_card_component.rb
@@ -0,0 +1,18 @@
+class TokenCardComponent < ApplicationComponent
+  with_collection_parameter :token
+
+  def initialize(token:)
+    super
+
+    @token = token
+  end
+
+  def operation
+    "Operation: #{@token.class.token_name.capitalize}"
+  end
+
+  def token_package_link
+    link_to("#{truncate(@token.package.project.name, length: 32)}/#{truncate(@token.package.name, length: 32)}",
+            package_show_path(project: @token.package.project, package: @token.package))
+  end
+end
