7ce972c9db07ccecb5916e7cbaebd8e3167cbd6c
Domenico Panella <pandom79@gmail.com>
2021-11-05 09:31:51 +0100

    Update

--- src/api/app/models/package_checkupgrade.rb
+++
@@ -1,6 +0,0 @@
-class PackageCheckUpgrade < ApplicationRecord
-  belongs_to :package
-  belongs_to :checkupgrade
-  
-end
-
--- src/api/db/migrate/20211104130533_add_foreign_key_to_packages_table.rb
+++
@@ -1,5 +0,0 @@
-class AddForeignKeyToPackagesTable < ActiveRecord::Migration[6.1]
-  def change
-	  add_foreign_key :package_checkupgrade, :packages, name: :package_checkupgrade_ibfk_1
-  end
-end
