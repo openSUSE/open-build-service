4c6f49b69d2d1408ffaee2506cf539151397a49b
Domenico Panella <pandom79@gmail.com>
2021-11-05 15:34:34 +0100

    Update

--- src/api/app/controllers/webui/requests/checkupgrade_controller.rb
+++ src/api/app/controllers/webui/requests/checkupgrade_controller.rb
@@ -5,16 +5,14 @@ module Webui
       before_action :set_package
       before_action :set_project
 
-      #after_action :verify_authorized 
+      after_action :verify_authorized 
 
       def show
       end
 
       def new
         packageCheckUpgrade = PackageCheckUpgrade.new
-        #authorize packageCheckUpgrade, :new?
-        #puts "@packageCheckUpgrade = ", @packageCheckUpgrade
-
+        authorize packageCheckUpgrade, :new?
       end
 
 
