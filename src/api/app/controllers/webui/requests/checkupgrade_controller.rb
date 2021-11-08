6b45ef7276a2820acf9bffbcda301b8b031ddbcb
Domenico Panella <pandom79@gmail.com>
2021-11-08 18:42:33 +0100

    Update

--- src/api/app/controllers/webui/requests/checkupgrade_controller.rb
+++ src/api/app/controllers/webui/requests/checkupgrade_controller.rb
@@ -27,27 +27,31 @@ module Webui
       end
 
       def create
+        flash.clear
         @packageCheckUpgrade = PackageCheckUpgrade.new(packageCheckUpgrade_params)
         authorize @packageCheckUpgrade, :create?
 
-        @commit = params[:commit]
-        if @commit == 'Run check'
-          run_check(@packageCheckUpgrade)
-        else
-          #Re-execute the check because the user could change something before the
-          #submit
-          result = execute_check(@packageCheckUpgrade)
-          if result.present?
-            @packageCheckUpgrade.output = result.gsub("\n", "\\n")
-          end
-          set_state(@packageCheckUpgrade)
-          if @packageCheckUpgrade.state == 'error' or !@packageCheckUpgrade.save
-            flash[:error] = 'An internal error has occurred'
-          else
-            flash[:success] = 'Check upgrade saved successfully'
-          end
+        #Get action
+        @create_action = params[:commit] == 'Create' ? true : false
+
+        #Execute check
+        result = execute_check(@packageCheckUpgrade)
+        #Set state
+        set_state(@packageCheckUpgrade)
+
+        #Respond
+        respond_to do |format|
+            if @packageCheckUpgrade.state == 'error' or (@create_action and !@packageCheckUpgrade.save)
+              format.js { flash.now[:error] = 'An error has occurred' }
+            else
+              if @create_action
+                format.js { flash[:success] = 'Check upgrade saved successfully' }
+              else
+                format.js {}
+              end
+            end
         end
-        
+
       end
 
       def packageCheckUpgrade_params
@@ -60,37 +64,14 @@ module Webui
         result = packageCheckUpgrade.run_checkupgrade(packageCheckUpgrade.urlsrc, packageCheckUpgrade.regexurl, 
                                                       packageCheckUpgrade.regexver, packageCheckUpgrade.currentver, 
                                                       packageCheckUpgrade.separator, 'false', User.session.login)
-        return result
-      end
-
-      def run_check(packageCheckUpgrade)
         
-        flash.clear
-        result = execute_check(packageCheckUpgrade)
-        #If result is present, replace new line character
         if result.present?
           packageCheckUpgrade.output = result.gsub("\n", "\\n")
         else
           packageCheckUpgrade.output = nil
         end
 
-        #Set state
-        set_state(packageCheckUpgrade)
-
-        #Respond
-        check_upgrade_respond(packageCheckUpgrade)
-
-      end
-
-      def check_upgrade_respond(packageCheckUpgrade)
-        respond_to do |format|
-            format.json { render json: packageCheckUpgrade }
-            if packageCheckUpgrade.state == 'error'
-              format.js { flash.now[:error] = 'An internal error has occurred' }
-            elsif 
-              format.js {}
-            end
-        end
+        return result
       end
 
       def set_state(packageCheckUpgrade)
