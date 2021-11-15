21a7b3637e76174e52e079bd7b4deb310d876591
Lukas Krause <lkrause@suse.de>
2021-11-15 18:51:01 +0100

    Create workflow runs index view
    We are tracking executed workflow runs and the current state of them
    through the `WorkflowRun` model.
    Now that this data is available for the users we need the corresponding
    webui elements in order to access those infos.

---
+++ src/api/app/policies/workflow_run_policy.rb
@@ -0,0 +1,21 @@
+class WorkflowRunPolicy < ApplicationPolicy
+  class Scope
+    def initialize(user, scope, opts = {})
+      @user = user
+      @scope = scope
+      @opts = opts
+    end
+
+    def resolve
+      token = Token.find_by(id: opts[:token_id])
+      raise Pundit::NotAuthorizedError, 'you are not authorized to access those workflow runs' unless token.user == user
+      raise Pundit::NotAuthorizedError, 'the token is not of type workflow' unless token.type == 'Token::Workflow'
+
+      scope.where(token_id: token.id).order(created_at: :desc)
+    end
+
+    private
+
+    attr_reader :user, :scope, :opts
+  end
+end
