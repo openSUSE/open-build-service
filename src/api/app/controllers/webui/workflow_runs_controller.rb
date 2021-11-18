d1ef669c371146722c6de6568e918b8efadd4898
Lukas Krause <lkrause@suse.de>
2021-11-18 14:54:04 +0100

    Add pagination to workflow run index view

--- src/api/app/controllers/webui/workflow_runs_controller.rb
+++ src/api/app/controllers/webui/workflow_runs_controller.rb
@@ -1,5 +1,5 @@
 class Webui::WorkflowRunsController < Webui::WebuiController
   def index
-    @workflow_runs = WorkflowRunPolicy::Scope.new(User.session, WorkflowRun, { token_id: params[:token_id] }).resolve
+    @workflow_runs = WorkflowRunPolicy::Scope.new(User.session, WorkflowRun, { token_id: params[:token_id] }).resolve.page(params[:page])
   end
 end
