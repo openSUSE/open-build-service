f6a55d1fde2c5faef19eab292d58b1fa643d81a0
Lukas Krause <lkrause@suse.de>
2021-11-19 15:17:52 +0100

    Add controller specs for workflow runs webui

--- src/api/spec/policies/workflow_run_policy_spec.rb
+++ src/api/spec/policies/workflow_run_policy_spec.rb
@@ -4,41 +4,35 @@ RSpec.describe WorkflowRunPolicy do
   describe '#resolve' do
     subject { WorkflowRunPolicy::Scope }
 
-    let(:token_user) { create(:confirmed_user) }
+    let(:token_user) { create(:confirmed_user, login: 'foo') }
     let(:workflow_token) { create(:workflow_token, user: token_user) }
     let!(:workflow_run) { create(:workflow_run, token: workflow_token) }
 
-    context 'when the user has permission' do
-      before do
-        User.session = token_user
-      end
-
-      it 'returns the workflow runs' do
-        expect(subject.new(token_user, WorkflowRun, { token_id: workflow_token.id }).resolve.count).to eq(1)
-      end
+    before do
+      User.session = token_user
     end
 
-    context 'when the user does not have permission' do
-      let(:another_user) { create(:confirmed_user) }
-
-      before do
-        User.session = another_user
-      end
-
-      it 'raises a not authorized error' do
-        expect { subject.new(another_user, WorkflowRun, { token_id: workflow_token.id }).resolve }.to raise_error(Pundit::NotAuthorizedError)
+    context 'when the user has permission' do
+      it 'returns the workflow runs' do
+        expect(subject.new(User.session, WorkflowRun, { token_id: workflow_token.id }).resolve.count).to eq(1)
       end
     end
 
     context 'when token type is not of type workflow' do
       let(:release_token) { create(:release_token, user: token_user) }
 
+      it 'raise a not authorized error' do
+        expect { subject.new(User.session, WorkflowRun, { token_id: release_token.id }).resolve }.to raise_error(Pundit::NotAuthorizedError)
+      end
+    end
+
+    context 'when the user does not have permission' do
       before do
-        User.session = token_user
+        User.session = create(:confirmed_user, login: 'bar')
       end
 
-      it 'raise a not authorized error' do
-        expect { subject.new(token_user, WorkflowRun, { token_id: release_token.id }).resolve }.to raise_error(Pundit::NotAuthorizedError)
+      it 'raises a not authorized error' do
+        expect { subject.new(User.session, WorkflowRun, { token_id: workflow_token.id }).resolve }.to raise_error(Pundit::NotAuthorizedError)
       end
     end
   end
