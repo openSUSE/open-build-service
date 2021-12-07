5213c3127e3ba292bfa260fd88bf92a11cd3a802
Joop Boonen <joop.boonen@boonen.org>
2021-12-07 19:32:21 +0100

    Added suggested fix

--- src/api/app/models/project/remote_url.rb
+++ src/api/app/models/project/remote_url.rb
@@ -4,11 +4,11 @@ class Project::RemoteURL
   def self.load(remote_project, path)
     if File.file?('/etc/sysconfig/proxy')
       proxysettings = Hash[File.read('/etc/sysconfig/proxy').scan(/(\S+)\s*=\s*"([^"]+)/)]
-      Rails.logger.info "var HTTP_PROXY: #{proxysettings.fetch("HTTP_PROXY")} var HTTPS_PROXY: #{proxysettings.fetch("HTTPS_PROXY")} var NO_PROXY: #{proxysettings.fetch("NO_PROXY")}"
-      ENV['http_proxy'] = proxysettings.fetch("HTTPS_PROXY") if proxysettings.has_key?("HTTPS_PROXY")
-      ENV['https_proxy'] = proxysettings.fetch("HTTPS_PROXY") if proxysettings.has_key?("HTTPS_PROXY")
-      ENV['ftp_proxy'] = proxysettings.fetch("HTTPS_PROXY") if proxysettings.has_key?("FTP_PROXY")
-      ENV['no_Proxy'] = proxysettings.fetch("NO_PROXY") if proxysettings.has_key?("NO_PROXY")
+      Rails.logger.info "var HTTP_PROXY: #{proxysettings("HTTP_PROXY")} var HTTPS_PROXY: #{proxysettings("HTTPS_PROXY")} var NO_PROXY: #{proxysettings("NO_PROXY")}"
+      ENV['http_proxy'] = proxysettings['HTTP_PROXY'] if proxysettings.has_key?("HTTP_PROXY")
+      ENV['https_proxy'] = proxysettings['HTTPS_PROXY'] if proxysettings.has_key?("HTTPS_PROXY")
+      ENV['ftp_proxy'] = proxysettings['FTP_PROXY'] if proxysettings.has_key?("FTP_PROXY")
+      ENV['no_proxy'] = proxysettings['NO_PROXY'] if proxysettings.has_key?("NO_PROXY")
     end
 
     uri = URI.parse(remote_project.remoteurl + path)
@@ -21,7 +21,7 @@ class Project::RemoteURL
       uri.open.read
     rescue OpenURI::HTTPError, SocketError, Errno::EINTR, Errno::EPIPE, EOFError, Net::HTTPBadResponse, IOError, Errno::ENETUNREACH,
            Errno::EADDRNOTAVAIL, Errno::ETIMEDOUT, Errno::ECONNREFUSED, Timeout::Error, OpenSSL::SSL::SSLError => e
-      Rails.logger.info "#{e} when fetching #{path} from #{remote_project.remoteurl}"
+      Rails.logger.info "#{e} whening #{path} from #{remote_project.remoteurl}"
       Airbrake.notify(e)
       nil
     end
