fb3141ac520fc6631363f1f6bb08ddca6f229285
Dani Donisa <dani.doni@gmail.com>
2021-12-28 15:42:07 +0100

    Disable Rails/ReversibleMigration cops on older migrations

--- src/api/db/migrate/20170704133728_remove_source_project_id_and_source_package_id_from_bs_request_actions.rb
+++ src/api/db/migrate/20170704133728_remove_source_project_id_and_source_package_id_from_bs_request_actions.rb
@@ -1,6 +1,8 @@
 class RemoveSourceProjectIdAndSourcePackageIdFromBsRequestActions < ActiveRecord::Migration[5.1]
   def change
+    # rubocop:disable Rails/ReversibleMigration
     remove_reference(:bs_request_actions, :source_package, index: true)
     remove_reference(:bs_request_actions, :source_project, index: true)
+    # rubocop:enable Rails/ReversibleMigration
   end
 end
