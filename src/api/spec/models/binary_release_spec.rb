e9b991f789c8692688cbe133207edd760452feb9
Victor Pereira <vpereira@suse.de>
2021-12-29 19:21:41 +0100

    Add specs for BinaryRelease.update_binary_releases_via_json

--- src/api/spec/models/binary_release_spec.rb
+++ src/api/spec/models/binary_release_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe BinaryRelease do
-  let(:binary_hash) do 
+  let(:binary_hash) do
     {
       'disturl' => '/foo/bar',
       'supportstatus' => 'foo',
@@ -10,9 +10,52 @@ RSpec.describe BinaryRelease do
     }
   end
 
+  describe '.update_binary_releases_via_json' do
+    let(:user) { create(:confirmed_user, :with_home, login: 'foo') }
+    let!(:repository) { create(:repository, project: user.home_project) }
+
+    context 'with empty json' do
+      it { expect { described_class.update_binary_releases_via_json(repository, []) }.not_to raise_error }
+    end
+
+    context 'with a repository to be released' do
+      let!(:binary_release) { create(:binary_release, repository: repository) }
+      let(:repeated_binary_hash) do
+        {
+          'disturl' => binary_release.binary_disturl,
+          'supportstatus' => binary_release.binary_supportstatus,
+          'binaryid' => binary_release.binary_id,
+          'buildtime' => binary_release.binary_buildtime,
+          'name' => binary_release.binary_name,
+          'binaryarch' => binary_release.binary_arch
+        }
+      end
+
+      it { expect { described_class.update_binary_releases_via_json(repository, [repeated_binary_hash]) }.not_to raise_error }
+    end
+
+    context 'with repeated binary_releases' do
+      let!(:binary_releases) { create_list(:binary_release, 2, repository: repository) }
+      let(:repeated_binary_hash) do
+        {
+          'disturl' => binary_releases.first.binary_disturl,
+          'supportstatus' => binary_releases.first.binary_supportstatus,
+          'binaryid' => binary_releases.first.binary_id,
+          'buildtime' => binary_releases.first.binary_buildtime.to_i,
+          'name' => binary_releases.first.binary_name,
+          'binaryarch' => binary_releases.first.binary_arch
+        }
+      end
+
+      subject { described_class.update_binary_releases_via_json(repository, [repeated_binary_hash]) }
+
+      it { expect { subject }.to change(BinaryRelease, :count).by(-1) }
+    end
+  end
+
   describe '#identical_to?' do
     context 'binary_release and binary_hash are identical' do
-      let(:binary_release) do 
+      let(:binary_release) do
         BinaryRelease.new(
           binary_disturl: binary_hash['disturl'],
           binary_supportstatus: binary_hash['supportstatus'],
@@ -23,8 +66,9 @@ RSpec.describe BinaryRelease do
 
       it { expect(binary_release).to be_identical_to(binary_hash) }
     end
+
     context 'binary_release and binary_hash are not identical' do
-      let(:binary_release) do 
+      let(:binary_release) do
         BinaryRelease.new(
           binary_disturl: binary_hash['disturl'],
           binary_supportstatus: binary_hash['supportstatus'],
@@ -37,4 +81,3 @@ RSpec.describe BinaryRelease do
     end
   end
 end
- 
