97440999721c529497c11d9497074773c7fd7b9b
Lukas Krause <lkrause@suse.de>
2022-01-04 16:33:32 +0100

    Refactor comments presenter to a view component
    We've decided to use view components in favor of presenters.
    
    Fixes #11559

---
+++ src/api/app/components/comment_component.rb
@@ -0,0 +1,31 @@
+class CommentComponent < ApplicationComponent
+  def initialize(comment:, obj_is_user:, builder:)
+    super
+
+    @comment = comment
+    @obj_is_user = obj_is_user
+    @builder = builder
+  end
+
+  def user?
+    @obj_is_user
+  end
+
+  def attributes
+    attrs = { who: comment.user.login, when: comment.created_at, id: comment.id }
+    if user?
+      attrs[comment.commentable.class.name.downcase.to_sym] = comment.commentable.to_param
+      attrs[:project] = comment.commentable.project if comment.commentable.is_a?(Package)
+    end
+    attrs[:parent] = comment.parent_id if comment.parent_id
+    attrs
+  end
+
+  def body
+    comment.body.delete("\u0000")
+  end
+
+  private
+
+  attr_accessor :comment
+end
--- src/api/app/views/comments/_comment.xml.builder
+++
@@ -1,5 +0,0 @@
-comment_presenter = CommentsControllerPresenters::CommentPresenter.new(comment, obj_is_user)
-
-builder.comment_(comment_presenter.attributes) do
-  builder.text(comment_presenter.body)
-end
