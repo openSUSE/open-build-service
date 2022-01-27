0460f9009d4b42095af7d8f4378e45006f4c557b
Lukas Krause <lkrause@suse.de>
2022-01-27 14:15:20 +0100

    Add spec to check that issues are created correctly
    Just a little test to check that the mentioned issues are
    interpreted correctly after adding the word boundary to
    the regex of the trackers.

--- src/api/spec/jobs/package_update_if_dirty_job_spec.rb
+++ src/api/spec/jobs/package_update_if_dirty_job_spec.rb
@@ -10,12 +10,27 @@ RSpec.describe PackageUpdateIfDirtyJob, type: :job, vcr: true do
 
   describe '#perform' do
     let!(:project) { create(:project, name: 'apache') }
-    let!(:package) { create(:package_with_file, name: 'mod_ssl', project: project) }
+    let(:changes_file) { file_fixture('mod_ssl.changes').read }
+    let!(:package) { create(:package_with_changes_file, project: project, name: 'mod_ssl', changes_file_content: changes_file) }
 
     subject { PackageUpdateIfDirtyJob.new.perform(package.id) }
 
+    # We have to enable the issue tracker first and update them
+    # on the backend in order to make the test work
+    before do
+      # rubocop:disable Rails/SkipsModelValidations
+      IssueTracker.update_all(enable_fetch: true)
+      # rubocop:enable Rails/SkipsModelValidations
+      IssueTrackerWriteToBackendJob.new.perform_now
+    end
+
     it 'creates a BackendPackge for the Package' do
       expect { subject }.to change(BackendPackage, :count).by(1)
+    end
+
+    it 'creates issues from the text in the changes file' do
+      # The changes file contains: gh#cli/cli#1, CVE-2021-12345 and bsc#3
+      expect { subject }.to change(Issue, :count).by(3)
     end
   end
 end
