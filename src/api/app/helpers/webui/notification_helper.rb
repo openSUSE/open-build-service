4b6c8b067c5265b8d1d97851ac70d2e5571857b8
Dany Marcoux <dmarcoux@posteo.de>
2022-02-14 17:17:11 +0100

    Use dom_id for mark_as_read_or_unread_button
    This is what ActionView uses to associate records with DOM elements.
    Let's use it too!
    
    https://api.rubyonrails.org/classes/ActionView/RecordIdentifier.html#method-i-dom_id
    
    The generated ID is slightly different, `dom_id` uses underscores
    instead of dashes. So before, it was `update-notification-123`, now it's
    `update_notification_123`. It's not an issue, as we don't refer to this
    ID in any code.

--- src/api/app/helpers/webui/notification_helper.rb
+++ src/api/app/helpers/webui/notification_helper.rb
@@ -48,7 +48,7 @@ module Webui::NotificationHelper
   def mark_as_read_or_unread_button(notification)
     update_path = my_notifications_path(notification_ids: [notification.id])
     title, icon = notification.unread? ? ['Mark as read', 'fa-check'] : ['Mark as unread', 'fa-undo']
-    link_to(update_path, id: "update-notification-#{notification.id}", method: :put,
+    link_to(update_path, id: dom_id(notification, :update), method: :put,
                          class: 'btn btn-sm btn-outline-success', title: title) do
       concat(tag.i(class: "#{icon} fas"))
       concat(" #{title}")
