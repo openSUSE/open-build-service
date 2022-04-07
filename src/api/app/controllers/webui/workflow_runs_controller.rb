00a0bf59faf06f775c41676a775e3eb28f8570ff
Rubhan Azeem <rubhanazeem@gmail.com>
2022-04-07 13:17:22 +0200

    Add view component for workflow_run request action filter

--- src/api/app/controllers/webui/workflow_runs_controller.rb
+++ src/api/app/controllers/webui/workflow_runs_controller.rb
@@ -2,11 +2,11 @@ class Webui::WorkflowRunsController < Webui::WebuiController
   def index
     relation = WorkflowRunPolicy::Scope.new(User.session, WorkflowRun, { token_id: params[:token_id] })
     @workflow_runs_finder = WorkflowRunsFinder.new(relation.resolve)
-
+    @request_action = params[:request_action] if params[:request_action].present? && params[:request_action] != 'all'
     @workflow_runs = if params[:status]
                        @workflow_runs_finder.with_status(params[:status])
                      elsif params[:generic_event_type]
-                       @workflow_runs_finder.with_generic_event_type(params[:generic_event_type])
+                       @workflow_runs_finder.with_generic_event_type(params[:generic_event_type], @request_action)
                      else
                        @workflow_runs_finder.all
                      end
