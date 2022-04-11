f534c864cd06ef83b5a73c8146e27f78665af6b8
Rubhan Azeem <rubhanazeem@gmail.com>
2022-04-11 13:42:05 +0200

    New specs for workflow_run_request_action_filter_component_spec and workflow_runs_controller_spec

---
+++ src/api/spec/components/workflow_run_request_action_filter_component_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe WorkflowRunRequestActionFilterComponent, type: :component do
+  let(:workflow_token) { create(:workflow_token) }
+
+  it 'shows correct filter options' do
+    render_inline(described_class.new(token_id: workflow_token.id))
+    filters = ['all'] + ScmWebhook::ALLOWED_PULL_REQUEST_ACTIONS + ScmWebhook::ALLOWED_MERGE_REQUEST_ACTIONS
+
+    filters.each do |filter|
+      expect(rendered_component).to have_text(filter)
+    end
+  end
+end
--- src/api/spec/controllers/webui/workflow_runs_controller_spec.rb
+++ src/api/spec/controllers/webui/workflow_runs_controller_spec.rb
@@ -8,9 +8,30 @@ RSpec.describe Webui::WorkflowRunsController, type: :controller do
 
     before do
       login token_user
-      get :index, params: { token_id: workflow_token.id }
     end
 
-    it { expect(assigns(:workflow_runs).count).to eq(1) }
+    context 'when action_filter is not available' do
+      before do
+        get :index, params: { token_id: workflow_token.id }
+      end
+
+      it { expect(assigns(:workflow_runs).count).to eq(1) }
+    end
+
+    context 'when action_filter is available' do
+      it 'finds workflow runs when the action is available' do
+        get :index, params: { token_id: workflow_token.id, request_action: 'opened', generic_event_type: 'pull_request' }
+
+        expect(assigns(:workflow_runs).count).to eq(1)
+        expect(assigns(:request_action)).to eq('opened')
+      end
+
+      it 'does not find workflow runs when action is not available' do
+        get :index, params: { token_id: workflow_token.id, request_action: 'closed', generic_event_type: 'pull_request' }
+
+        expect(assigns(:workflow_runs).count).to eq(0)
+        expect(assigns(:request_action)).to eq('closed')
+      end
+    end
   end
 end
