11109e65d8a4f31f795b18cfc1dbafde699812d3
Henne Vogelsang <hvogel@opensuse.org>
2022-04-12 17:31:17 +0200

    Switch all shebangs to Ruby 2.7

--- dist/setup-appliance.sh
+++ dist/setup-appliance.sh
@@ -516,7 +516,7 @@ function prepare_apache2 {
 function prepare_passenger {
 
   perl -p -i -e \
-    's#^(\s*)PassengerRuby "/usr/bin/ruby"#$1\PassengerRuby "/usr/bin/ruby.ruby2.5"#' \
+    's#^(\s*)PassengerRuby "/usr/bin/ruby"#$1\PassengerRuby "/usr/bin/ruby.ruby2.7"#' \
       /etc/apache2/conf.d/mod_passenger.conf
 
 
