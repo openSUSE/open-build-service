182b2dd6759409409a705813a6d0bd9efed9f939
Lukas Krause <lkrause@suse.de>
2022-04-14 10:25:01 +0200

    Don't add exception from SCMStatusReporter to logfile
    We inform the users directly now, no need to track the errors
    in the logfile anymore.

--- src/api/app/services/scm_exception_handler.rb
+++ src/api/app/services/scm_exception_handler.rb
@@ -24,8 +24,6 @@ class SCMExceptionHandler
               Octokit::PathDiffTooLarge,
               Octokit::ServiceUnavailable,
               Octokit::InternalServerError do |exception|
-    # FIXME: Inform users about the exceptions
-    log(exception)
     log_to_workflow_run(exception, 'GitHub') if @workflow_run.present?
   end
 
@@ -37,8 +35,6 @@ class SCMExceptionHandler
               Gitlab::Error::ServiceUnavailable,
               Gitlab::Error::TooManyRequests,
               Gitlab::Error::Unauthorized do |exception|
-    # FIXME: Inform users about the exceptions
-    log(exception)
     log_to_workflow_run(exception, 'GitLab') if @workflow_run.present?
   end
 
@@ -50,11 +46,6 @@ class SCMExceptionHandler
   end
 
   private
-
-  def log(exception)
-    token = Token::Workflow.find_by(scm_token: @scm_token)
-    Rails.logger.error "#{exception.class}: #{exception.message}. TokenID: #{token.id}, User: #{token.user.login}, Event Subscription Payload: #{@event_subscription_payload}"
-  end
 
   def log_to_workflow_run(exception, scm)
     @workflow_run.update_to_fail("Failed to report back to #{scm}: #{exception.message}")
