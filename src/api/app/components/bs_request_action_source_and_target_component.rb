cf718293664b0efc23e45173efe11042027d457a
Saray Cabrera Padrón <scabrerapadron@suse.de>
2022-04-26 12:53:24 +0100

    Move request action info to a component
    In more than one place in the application, we need to know the source
    and target packages related to the request action, if any.
    
    So we extract the code from NotificationActionDescriptionComponent
    and create an independent component for the source and target
    information.

---
+++ src/api/app/components/bs_request_action_source_and_target_component.rb
@@ -0,0 +1,37 @@
+class BsRequestActionSourceAndTargetComponent < ApplicationComponent
+  attr_reader :bs_request_action, :number_of_bs_request_actions
+
+  def initialize(bs_request)
+    super
+
+    @bs_request = bs_request
+    @bs_request_action = @bs_request.bs_request_actions.first
+    @number_of_bs_request_actions = @bs_request.bs_request_actions.size
+  end
+
+  def call
+    capture do
+      if source.present?
+        concat(tag.span(source))
+        concat(tag.i(nil, class: 'fas fa-long-arrow-alt-right text-info mx-2'))
+      end
+      concat(tag.span(target))
+    end
+  end
+
+  private
+
+  def source
+    @source ||= if number_of_bs_request_actions > 1
+                  ''
+                else
+                  [bs_request_action.source_project, bs_request_action.source_package].compact.join(' / ')
+                end
+  end
+
+  def target
+    return bs_request_action.target_project if number_of_bs_request_actions > 1
+
+    [bs_request_action.target_project, bs_request_action.target_package].compact.join(' / ')
+  end
+end
--- src/api/app/components/notification_action_description_component.rb
+++ src/api/app/components/notification_action_description_component.rb
@@ -9,7 +9,7 @@ class NotificationActionDescriptionComponent < ApplicationComponent
     tag.div(class: ['smart-overflow']) do
       case @notification.event_type
       when 'Event::RequestStatechange', 'Event::RequestCreate', 'Event::ReviewWanted', 'Event::CommentForRequest'
-        source_and_target
+        BsRequestActionSourceAndTargetComponent.new(bs_request).call
       when 'Event::CommentForProject'
         "#{@notification.notifiable.commentable.name}"
       when 'Event::CommentForPackage'
@@ -21,39 +21,7 @@ class NotificationActionDescriptionComponent < ApplicationComponent
 
   private
 
-  def source_and_target
-    capture do
-      if source.present?
-        concat(tag.span(source))
-        concat(tag.i(nil, class: 'fas fa-long-arrow-alt-right text-info mx-2'))
-      end
-      concat(tag.span(target))
-    end
-  end
-
-  def source
-    @source ||= if number_of_bs_request_actions > 1
-                  ''
-                else
-                  [bs_request_action.source_project, bs_request_action.source_package].compact.join(' / ')
-                end
-  end
-
-  def target
-    return bs_request_action.target_project if number_of_bs_request_actions > 1
-
-    [bs_request_action.target_project, bs_request_action.target_package].compact.join(' / ')
-  end
-
   def bs_request
     @bs_request ||= @notification.notifiable_type == 'BsRequest' ? @notification.notifiable : @notification.notifiable.commentable
-  end
-
-  def bs_request_action
-    @bs_request_action ||= bs_request.bs_request_actions.first
-  end
-
-  def number_of_bs_request_actions
-    @number_of_bs_request_actions ||= bs_request.bs_request_actions.size
   end
 end
