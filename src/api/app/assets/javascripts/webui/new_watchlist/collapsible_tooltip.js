ad876b2e35b063cd2f55c007e0928b0dae1e4bc3
Saray Cabrera Padrón <scabrerapadron@suse.de>
2022-04-28 17:40:36 +0200

    Rename duplicated JS function toggleTooltip
    A JS function named toggleTooltip already existed in another JS file.
    So the function is renamed to toggleCollapsibleTooltip and the classes
    are renamed accordingly.

--- src/api/app/assets/javascripts/webui/new_watchlist/collapsible_tooltip.js
+++ src/api/app/assets/javascripts/webui/new_watchlist/collapsible_tooltip.js
@@ -1,15 +1,15 @@
-function toggleTooltip() { // jshint ignore:line
-  $('.toggle-tooltip').on('click', function(){
+function toggleCollapsibleTooltip() { // jshint ignore:line
+  $('.collapsible-tooltip').on('click', function(){
     var replaceTitle = $(this).attr('title') === 'Click to keep it open' ? 'Click to close it' : 'Click to keep it open';
-    var infoContainer = $(this).parents('.toggle-tooltip-parent').next();
+    var infoContainer = $(this).parents('.collapsible-tooltip-parent').next();
     $(infoContainer).toggleClass('collapsed');
     $(infoContainer).removeClass('hover');
     $(this).attr('title', replaceTitle);
   });
-  $('.toggle-tooltip').on('mouseover', function(){
-    $(this).parents('.toggle-tooltip-parent').next().addClass('hover');
+  $('.collapsible-tooltip').on('mouseover', function(){
+    $(this).parents('.collapsible-tooltip-parent').next().addClass('hover');
   });
-  $('.toggle-tooltip').on('mouseout', function(){
-    $(this).parents('.toggle-tooltip-parent').next().removeClass('hover');
+  $('.collapsible-tooltip').on('mouseout', function(){
+    $(this).parents('.collapsible-tooltip-parent').next().removeClass('hover');
   });
 }
