747b98d9650e38cfdc3ed5924867315e6f97de7d
Dani Donisa <dani.doni@gmail.com>
2022-05-23 14:01:12 +0200

    Show the Workflow run's scm status reports
    We need to create a new tab to collect the SCM responses when we send the
    status reports.

--- src/api/app/components/workflow_run_detail_component.rb
+++ src/api/app/components/workflow_run_detail_component.rb
@@ -1,7 +1,7 @@
 class WorkflowRunDetailComponent < ApplicationComponent
   attr_reader :id, :request_headers, :pretty_request_payload,
               :response_url, :response_body, :artifacts,
-              :scm_vendor
+              :scm_vendor, :status_reports
 
   def initialize(workflow_run:)
     super
@@ -12,6 +12,7 @@ class WorkflowRunDetailComponent < ApplicationComponent
     @response_body = workflow_run.response_body
     @artifacts = workflow_run.artifacts # collection of WorkflowArtifactsPerStep
     @scm_vendor = workflow_run.scm_vendor.to_s.humanize
+    @status_reports = workflow_run.scm_status_reports
   end
 
   private
