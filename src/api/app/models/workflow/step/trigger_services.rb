bf44a2ae90dca42ed99bed82c9dcb9ddb7c41f05
Saray Cabrera Padrón <scabrerapadron@suse.de>
2022-06-03 12:37:19 +0200

    Add comment when triggering service from step
    When we trigger a service we can pass a comment.
    We do so on the trigger_services workflow step, so we will have
    information about what triggered the service.

--- src/api/app/models/workflow/step/trigger_services.rb
+++ src/api/app/models/workflow/step/trigger_services.rb
@@ -18,7 +18,7 @@ class Workflow::Step::TriggerServices < Workflow::Step
     Pundit.authorize(@token.user, @token, :trigger_service?)
 
     begin
-      Backend::Api::Sources::Package.trigger_services(@project_name, @package_name, @token.user.login)
+      Backend::Api::Sources::Package.trigger_services(@project_name, @package_name, @token.user.login, trigger_service_comment)
     rescue Backend::NotFoundError => e
       raise NoSourceServiceDefined, "Package #{@project_name}/#{@package_name} does not have a source service defined: #{e.summary}"
     end
@@ -28,5 +28,34 @@ class Workflow::Step::TriggerServices < Workflow::Step
 
   def package_find_options
     { use_source: true, follow_project_links: false, follow_multibuild: false }
+  end
+
+  # Examples of comments:
+  # "Service triggered by a workflow token via $scm PR/MR $number ($event)"
+  # "Service triggered by the '$token' token via $scm push $sha on $tag || $branch"
+  def trigger_service_comment
+    'Service triggered by ' \
+      "#{@token.description.blank? ? 'a workflow token ' : "the '#{@token.description}' token "}" \
+      "via #{@scm_webhook.payload[:scm].titleize} " \
+      "#{details}."
+  end
+
+  def details
+    case @scm_webhook.payload[:event]
+    when 'pull_request', 'Merge Request Hook'
+      "PR/MR ##{@scm_webhook.payload[:pr_number]} (#{@scm_webhook.payload[:event]})"
+    when 'push', 'Push Hook'
+      push_details
+    when 'Tag Push Hook'
+      "push #{@scm_webhook.payload[:commit_sha]&.slice(0, SHORT_COMMIT_SHA_LENGTH)} on #{@scm_webhook.payload[:tag_name]}"
+    end
+  end
+
+  def push_details
+    if @scm_webhook.payload[:scm] == 'github' && @scm_webhook.payload[:ref].start_with?('refs/tags')
+      "push #{@scm_webhook.payload[:commit_sha]&.slice(0, SHORT_COMMIT_SHA_LENGTH)} on #{@scm_webhook.payload[:tag_name]}"
+    else
+      "push #{@scm_webhook.payload[:commit_sha]&.slice(0, SHORT_COMMIT_SHA_LENGTH)} on #{@scm_webhook.payload[:target_branch]}"
+    end
   end
 end
