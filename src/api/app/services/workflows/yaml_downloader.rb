899bc36905162557ef58b4d6041d176177142665
Saray Cabrera Padrón <scabrerapadron@suse.de>
2022-06-06 17:00:40 +0200

    Set commit sha on target_branch for tag pushes
    On tag push events from GitHub, we tried to extract the branch name to
    set the 'target_branch' field. However, the value was not always correct
    (the tag name instead of the branch name). So we now extract the
    commit sha as we do with GitLab.

--- src/api/app/services/workflows/yaml_downloader.rb
+++ src/api/app/services/workflows/yaml_downloader.rb
@@ -17,7 +17,8 @@ module Workflows
     def download_yaml_file
       Down.download(download_url, max_size: MAX_FILE_SIZE)
     rescue Down::Error => e
-      raise Token::Errors::NonExistentWorkflowsFile, ".obs/workflows.yml could not be downloaded from the SCM branch #{@scm_payload[:target_branch]}." \
+      # 'target_branch' can contain a commit sha (when tag push) instead of a branch name
+      raise Token::Errors::NonExistentWorkflowsFile, ".obs/workflows.yml could not be downloaded from the SCM branch or commit #{@scm_payload[:target_branch]}." \
                                                      "\nIs the configuration file in the expected place? Check #{DOCUMENTATION_LINK}\n#{e.message}"
     end
 
@@ -25,14 +26,17 @@ module Workflows
       case @scm_payload[:scm]
       when 'github'
         client = Octokit::Client.new(access_token: @token.scm_token, api_endpoint: @scm_payload[:api_endpoint])
+        # :ref can be the name of the commit, branch or tag.
         client.content("#{@scm_payload[:target_repository_full_name]}", path: '/.obs/workflows.yml', ref: @scm_payload[:target_branch])[:download_url]
       when 'gitlab'
+        # This GitLab URL admits both a branch name and a commit sha.
         "#{@scm_payload[:api_endpoint]}/#{@scm_payload[:path_with_namespace]}/-/raw/#{@scm_payload[:target_branch]}/.obs/workflows.yml"
       end
     rescue Octokit::InvalidRepository => e
       raise Token::Errors::NonExistentRepository, e.message
     rescue Octokit::NotFound => e
-      raise Token::Errors::NonExistentWorkflowsFile, ".obs/workflows.yml could not be downloaded from the SCM branch #{@scm_payload[:target_branch]}: #{e.message}"
+      # 'target_branch' can contain a commit sha (when tag push) instead of a branch name
+      raise Token::Errors::NonExistentWorkflowsFile, ".obs/workflows.yml could not be downloaded from the SCM branch or commit #{@scm_payload[:target_branch]}: #{e.message}"
     end
   end
 end
