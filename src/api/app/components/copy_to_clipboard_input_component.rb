af251e7c694530edfeba64f6ddaf4bd7af2a39f6
Dani Donisa <dani.doni@gmail.com>
2022-06-20 10:42:51 +0200

    CopyToClipboardInputComponent supports several instances

--- src/api/app/components/copy_to_clipboard_input_component.rb
+++ src/api/app/components/copy_to_clipboard_input_component.rb
@@ -1,6 +1,8 @@
 class CopyToClipboardInputComponent < ApplicationComponent
-  def initialize(input_text:)
+  def initialize(input_text:, input_id: 'copy-to-clipboard')
     super
     @input_text = input_text
+    @input_id = input_id
+    @readonly_input_id = "#{input_id}-readonly"
   end
 end
