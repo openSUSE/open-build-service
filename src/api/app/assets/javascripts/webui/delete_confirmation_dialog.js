9e728d43be9e45963b14dd357c6d24d864a6a5c5
Saray Cabrera Padrón <scabrerapadron@suse.de>
2022-06-22 18:04:11 +0200

    Set JS code in DeleteConfirmationDialogComponent
    When we need to set the content of the dialog dynamically, we pass data
    attributes with the values and call the JS function that sets them.
    
    Instead of calling that function from every view, we include the JS code
    in DeleteConfirmationDialogComponent, which collects all the modals to
    confirm deletion we find in the DOM and set the values coming from the
    corresponding data attributes if any.

--- src/api/app/assets/javascripts/webui/delete_confirmation_dialog.js
+++ src/api/app/assets/javascripts/webui/delete_confirmation_dialog.js
@@ -1,4 +1,18 @@
-function setValuesOnDeleteConfirmationDialog(modalId) { // jshint ignore:line
+function collectDeleteConfirmationModalsAndSetValues() { // jshint ignore:line
+  $.each(modalIds(), function( _index, modalId ) {
+    setValuesOnDeleteConfirmationDialog(modalId);
+  });
+}
+
+function modalIds() {
+  var targets = $('a[data-toggle="modal"][data-target^="#delete"]').toArray().map(
+    function(e) { return $(e).data('target');
+  });
+
+  return $.unique(targets);
+}
+
+function setValuesOnDeleteConfirmationDialog(modalId) {
   $(modalId).on('show.bs.modal', function (event) {
     var link = $(event.relatedTarget);
     var modal = $(this);
