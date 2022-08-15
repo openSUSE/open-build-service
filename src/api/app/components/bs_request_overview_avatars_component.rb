e28b4b3ec5c87da602315559854a68732a8db2d8
Dany Marcoux <dmarcoux@posteo.de>
2022-08-15 15:59:29 +0200

    Display count of hidden avatars when listing reviewers
    Co-authored-by: Lukas Krause <lkrause@suse.de>

--- src/api/app/components/bs_request_overview_avatars_component.rb
+++ src/api/app/components/bs_request_overview_avatars_component.rb
@@ -13,11 +13,19 @@ class BsRequestOverviewAvatarsComponent < ApplicationComponent
     @avatar_objects ||= if @review.for_user?
                           [@review.user]
                         elsif @review.for_group?
-                          [@review.group.users.first(MAXIMUM_DISPLAYED_AVATARS), @review.group].flatten
+                          [@review.group.users, @review.group].flatten
                         elsif @review.for_package?
-                          @review.package.project.users.first(MAXIMUM_DISPLAYED_AVATARS)
+                          @review.package.project.users
                         elsif @review.for_project?
-                          @review.project.users.first(MAXIMUM_DISPLAYED_AVATARS)
+                          @review.project.users
                         end
+  end
+
+  def avatars_to_display
+    avatar_objects.first(MAXIMUM_DISPLAYED_AVATARS).reverse
+  end
+
+  def number_of_hidden_avatars
+    [0, avatar_objects.size - MAXIMUM_DISPLAYED_AVATARS].max
   end
 end
