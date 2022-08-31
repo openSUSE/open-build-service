5ff8682ed16b740b9587ff4aa05049b3bfc3610c
Dany Marcoux <dmarcoux@posteo.de>
2022-08-31 17:23:51 +0200

    Configure parameter parsers with ActionDispatch::Request.parameter_parsers=
    This reverts commit a473625dc4b58da2eafd7caf301ec51c6281469a.

--- src/api/config/initializers/wrap_parameters.rb
+++ src/api/config/initializers/wrap_parameters.rb
@@ -2,10 +2,8 @@
 
 # This file contains settings for ActionController::ParamsWrapper which
 # is enabled by default.
-require 'active_support/core_ext/hash/conversions'
 require 'action_dispatch/http/request'
 require 'active_support/core_ext/hash/indifferent_access'
-require 'api_error'
 
 # Disable all default rails parameter parsing
 # Context: https://github.com/rails/rails/blob/39413de44c0e2c0dd2d964be5985b03d8f968a7b/guides/source/3_1_release_notes.md#configinitializerswrap_parametersrb
@@ -19,54 +17,24 @@ ActiveSupport.on_load(:active_record) do
   self.include_root_in_json = false
 end
 
-OBSApi::Application.config.middleware.delete 'ActionDispatch::ParamsParser'
+# Redefine JSON parser to use ActiveSupport::HashWithIndifferentAccess
+# Define XML parser, since Rails' default parsers only include JSON. This XML parser also uses ActiveSupport::HashWithIndifferentAccess
+# Context: https://github.com/rails/rails/blob/04972d9b9ef60796dc8f0917817b5392d61fcf09/actionpack/lib/action_dispatch/http/parameters.rb#L35-L46
+original_parsers = ActionDispatch::Request.parameter_parsers
 
-# custom params parser (modified form of ActionDispatch::ParamsParser)
+json_parser = lambda do |raw_post|
+  original_parsers[Mime[:json].symbol].call(raw_post).with_indifferent_access
+end
 
-class MyParamsParser
-  class ParseError < APIError
-  end
-
-  def initialize(app)
-    @app = app
-  end
-
-  def call(env)
-    params = parse_parameters(env)
-    env['action_dispatch.request.request_parameters'] = params if params
-
-    env['HTTP_ACCEPT'] ||= 'application/xml'
-    @app.call(env)
-  end
-
-  def parse_parameters(env)
-    request = ActionDispatch::Request.new(env)
-
-    return false if request.content_length.zero?
-
-    case request.content_mime_type
-    when Mime[:json]
-      begin
-        data = ActiveSupport::JSON.decode(request.raw_post)
-      rescue JSON::ParserError => e
-        Rails.logger.info "failed to parse JSON: #{e.message}"
-        return false
-      end
-      request.body.rewind if request.body.respond_to?(:rewind)
-      data = { _json: data } unless data.is_a?(Hash)
-      data.with_indifferent_access
-    when Mime[:xml]
-      data = Xmlhash.parse(request.body.read)
-      request.body.rewind if request.body.respond_to?(:rewind)
-      if data
-        { xmlhash: data }.with_indifferent_access
-      else
-        false
-      end
-    else
-      false
-    end
+xml_parser = lambda do |raw_post|
+  data = Xmlhash.parse(raw_post)
+  if data
+    { xmlhash: data }.with_indifferent_access
+  else
+    {}
   end
 end
 
-OBSApi::Application.config.middleware.use(MyParamsParser)
+new_parsers = original_parsers.merge({ Mime[:json].symbol => json_parser, Mime[:xml].symbol => xml_parser })
+
+ActionDispatch::Request.parameter_parsers = new_parsers
