f421f13d525443d50165def523cc037f1d472360
Eduardo Navarro <enavarro@suse.com>
2022-10-14 10:45:21 +0200

    Camelize variables to comply with jshint

--- src/api/app/assets/javascripts/webui/nav_tabs.js
+++ src/api/app/assets/javascripts/webui/nav_tabs.js
@@ -1,17 +1,17 @@
-var hash_prefix = 'tab-pane-';
+var HASH_PREFIX = 'tab-pane-';
 
 $(document).ready(function () {
   // Show tab-pane comming from the url hash. If the url hash is empty, show first tab-pane.
-  var tab_pane_id = document.location.hash.replace('#' + hash_prefix, '#') || $('.nav-tabs .nav-item:first-child .nav-link').attr('href');
-  $('.nav-tabs .nav-link[href="' + tab_pane_id + '"]').tab('show');
+  var tabPaneId = document.location.hash.replace('#' + HASH_PREFIX, '#') || $('.nav-tabs .nav-item:first-child .nav-link').attr('href');
+  $('.nav-tabs .nav-link[href="' + tabPaneId + '"]').tab('show');
 
   // Change url hash for page-reload
   $('.nav-tabs .nav-item .nav-link').on('shown.bs.tab', function (event) {
     if ($(event.target).parent('.nav-item').is(':first-child')) {
-      history.pushState('', document.title, window.location.pathname + window.location.search);
+      window.history.pushState('', document.title, window.location.pathname + window.location.search);
     }
     else {
-      document.location.hash = event.target.hash.replace('#', '#' + hash_prefix);
+      document.location.hash = event.target.hash.replace('#', '#' + HASH_PREFIX);
     }
   });
 });
