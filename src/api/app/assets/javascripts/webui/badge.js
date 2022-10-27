322b9c284054278943289483191a016e19a34126
Jacob Michalskie <jacob.michalskie@suse.com>
2022-10-27 15:05:33 +0200

    Remove output type selector

--- src/api/app/assets/javascripts/webui/badge.js
+++ src/api/app/assets/javascripts/webui/badge.js
@@ -3,12 +3,7 @@ function buildBadge() {
   var badgeImageUrl = new URL(BASE_BADGE_URL); // jshint ignore:line
   badgeImageUrl.searchParams.set('type', $('#badge-style-selector').val());
   $('#badge-preview').attr("src", badgeImageUrl);
-  if ($('#badge-format-selector').val() === 'markdown') {
-    return '[![build result](' + badgeImageUrl + ')](' + BASE_PACKAGE_URL + ')'; // jshint ignore:line
-  } else if ($('#badge-format-selector').val() === 'html') {
-    return '<a href="' + BASE_PACKAGE_URL + '"><img src="' + badgeImageUrl + '" alt="build result"/></a>'; // jshint ignore:line
-  }
-  return badgeImageUrl;
+  return '[![build result](' + badgeImageUrl + ')](' + BASE_PACKAGE_URL + ')'; // jshint ignore:line
 }
 
 function badgeTextCopy() { // jshint ignore:line
