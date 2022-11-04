dbe9f71204cc2596f655888f55ae2e58982aa9a3
Jacob Michalskie <jacob.michalskie@suse.com>
2022-11-04 08:59:50 +0100

    Fix the badge#show error

--- src/api/app/controllers/webui/packages/badge_controller.rb
+++ src/api/app/controllers/webui/packages/badge_controller.rb
@@ -8,28 +8,8 @@ module Webui
         results = @package.buildresult(@project, false, true).results[@package.name]
         results = results.select { |r| r.architecture == params[:architecture] } if params[:architecture]
         results = results.select { |r| r.repository == params[:repository] } if params[:repository]
-        send_data(create_badge(results), type: 'image/svg+xml', disposition: 'inline')
-      end
-
-      private
-
-      def create_badge(results)
-        filename = 'badge-unknown.svg'
-        return Rails.application.assets[filename].source if results.blank?
-
-        filename = 'badge-failed.svg' if results.any? { |r| r.code == 'failed' }
-        filename = 'badge-succeeded.svg' if results.all? { |r| r.code == 'succeeded' }
-        filename = 'badge-percent.svg' if params[:type] == 'percent'
-        get_badge_file(filename, results)
-      end
-
-      def get_badge_file(filename, results)
-        file = Rails.application.assets[filename].source
-        return file unless params[:type] == 'percent'
-
-        succeeded = results.select { |r| r.code == 'succeeded' }.try(:length).to_i
-        file.gsub!('#e05d44', '#4c1') if succeeded == results.length
-        file.gsub('%PERCENTAGE%', "#{100 * succeeded / results.length}%")
+        badge = Badge.new(params[:type], results)
+        send_data(badge.xml, type: 'image/svg+xml', disposition: 'inline')
       end
     end
   end
