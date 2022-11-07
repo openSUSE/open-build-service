7c3c7bd998233ba7ec3a444469330a24ed44b907
Lukas Krause <lkrause@suse.de>
2022-11-07 18:47:21 +0100

    Correct mistakes from unsafe autocorrection of rubocop `Style/StringConcatenation` cop
    The auto correction concatenated html tags, which manully had to be
    corrected and santized in order to keep working.

--- src/api/app/helpers/webui/user_helper.rb
+++ src/api/app/helpers/webui/user_helper.rb
@@ -58,7 +58,7 @@ module Webui::UserHelper
       if opts[:no_icon]
         link_to(printed_name, user_path(user))
       else
-        "#{image_tag_for(user, size: 20)} #{link_to(printed_name, user_path(user))}"
+        sanitize("#{image_tag_for(user, size: 20)} #{link_to(printed_name, user_path(user))}")
       end
     end
   end
