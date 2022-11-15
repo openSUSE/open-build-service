1e72604672807cc408503056e385efd389afb734
Eduardo Navarro <enavarro@suse.com>
2022-11-15 16:51:19 +0100

    Rename rpm-lint-result to rpm-lint
    There is content of the "RPM Lint" tab that is static, only rendered
    when the page is loaded. And there is some other content that is
    rendered via AJAX.
    
    To prevent confusion on what is retrieved with AJAX (the result), rename
    the references to the content of the tab to rpm-lint and leave
    rpm-lint-result for the content related to each result.

--- src/api/app/assets/javascripts/webui/request_show_redesign/rpm_lint_results.js
+++ src/api/app/assets/javascripts/webui/request_show_redesign/rpm_lint_results.js
@@ -1,17 +1,18 @@
 // TODO: rename without "Beta" after the rollout of 'request_show_redesign'.
 function updateRpmLintResultBeta() { // jshint ignore:line
-  var ajaxDataShow = $('.rpm-lint-results-content').data();
-  var rpmLintResultsUrl = $('.rpm-lint-results-content .rpm-lint-refresh').data('rpm-lint-results-url');
+  var ajaxDataShow = $('.rpm-lint-content').data();
+  ajaxDataShow.inRequestShowRedesign = true;
+  var rpmLintResultsUrl = $('.rpm-lint-content .rpm-lint-refresh').data('rpm-lint-results-url');
 
   $('#rpm-lint-reload').addClass('fa-spin');
   $.ajax({
     url: rpmLintResultsUrl,
     data: ajaxDataShow,
     success: function(data) {
-      $('.rpm-lint-results-content .result').html(data);
+      $('.rpm-lint-content .result').html(data);
     },
     error: function() {
-      $('.rpm-lint-results-content .result').html('<p>No RPM lint results available</p>');
+      $('.rpm-lint-content .result').html('<p>No RPM lint results available</p>');
     },
     complete: function() {
       $('#rpm-lint-reload').removeClass('fa-spin');
