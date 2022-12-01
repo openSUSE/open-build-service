94bc1d8eb1a955d9263f1c82c3157bf115bd8d93
Lukas Krause <lkrause@suse.de>
2022-12-01 12:45:39 +0100

    Adapt specs to use new receiver role `project_watcher` for `EventSubscription`

--- src/api/spec/models/event_subscription/for_event_form_spec.rb
+++ src/api/spec/models/event_subscription/for_event_form_spec.rb
@@ -7,13 +7,15 @@ RSpec.describe EventSubscription::ForEventForm do
 
   describe '#call' do
     let(:roles) { subject.roles }
+    # TODO: Remove this after all event subscriptions are migrated to the new receiver role name `project_watcher`
+    let(:deprecated_roles) { [:watcher, :source_watcher, :target_watcher] }
 
     subject { EventSubscription::ForEventForm.new(event_class, subscriber).call }
 
     context 'for Event::CommentForProject' do
       let(:event_class) { Event::CommentForProject }
       let(:expected_receiver_roles) do
-        event_class.receiver_roles
+        event_class.receiver_roles - deprecated_roles
       end
 
       it { expect(subject.event_class).to eq(event_class) }
@@ -23,7 +25,7 @@ RSpec.describe EventSubscription::ForEventForm do
     context 'for Event::CommentForRequest' do
       let(:event_class) { Event::CommentForRequest }
       let(:expected_receiver_roles) do
-        event_class.receiver_roles
+        event_class.receiver_roles - deprecated_roles
       end
 
       it { expect(subject.event_class).to eq(event_class) }
