3442a0f844d81d3f7c9c65c24843faf4e779822a
Lukas Krause <lkrause@suse.de>
2022-12-13 15:36:48 +0100

    Set correct commit_sha on tag push for github in scm payload
    We use the `:commit_sha` in the branch_request file to
    update/overwrite the one used in the `_service` file for the
    branch_package and link_package step in order to pull the updated/pushed
    sources. This is generated in the steps model with the
    `branch_request_content_github` method.
    We have to point here to the commit the tag is refering
    to, not the sha of the tag itself. Otherwise the service
    will fail to pull the sources since there in no correct reference
    to the commit.

--- src/api/app/services/trigger_controller_service/scm_extractor.rb
+++ src/api/app/services/trigger_controller_service/scm_extractor.rb
@@ -150,7 +150,8 @@ module TriggerControllerService
       # We need this for Workflow::Step#target_package_name
       # 'target_branch' will contain a commit SHA
       payload.merge!({ tag_name: payload_ref.sub('refs/tags/', ''),
-                       target_branch: @payload.dig(:head_commit, :id) })
+                       target_branch: @payload.dig(:head_commit, :id),
+                       commit_sha: @payload.dig(:head_commit, :id) })
     end
 
     def gitlab_payload_tag(payload)
