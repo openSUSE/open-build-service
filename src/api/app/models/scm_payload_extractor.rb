274ec0554d8f4efd17163d2f790ec0c63bc2685d
Dani Donisa <dani.doni@gmail.com>
2022-12-21 15:35:08 +0100

    Extract a GitHub payload extractor

---
+++ src/api/app/models/scm_payload_extractor.rb
@@ -0,0 +1,5 @@
+class ScmPayloadExtractor
+  def payload
+    raise AbstractMethodCalled
+  end
+end
--- src/api/app/services/trigger_controller_service/scm_extractor.rb
+++ src/api/app/services/trigger_controller_service/scm_extractor.rb
@@ -12,7 +12,7 @@ module TriggerControllerService
     def call
       case @scm
       when 'github'
-        SCMWebhook.new(payload: github_extractor_payload)
+        SCMWebhook.new(payload: GithubPayloadExtractor.new(@event, @payload).payload)
       when 'gitlab'
         SCMWebhook.new(payload: gitlab_extractor_payload)
       when 'gitea'
@@ -21,28 +21,6 @@ module TriggerControllerService
     end
 
     private
-
-    def github_extractor_payload
-      payload = {
-        scm: 'github',
-        event: @event,
-        api_endpoint: github_api_endpoint
-      }
-
-      case @event
-      when 'pull_request'
-        payload.merge!({ commit_sha: @payload.dig(:pull_request, :head, :sha),
-                         pr_number: @payload[:number],
-                         source_branch: @payload.dig(:pull_request, :head, :ref),
-                         target_branch: @payload.dig(:pull_request, :base, :ref),
-                         action: @payload[:action],
-                         source_repository_full_name: @payload.dig(:pull_request, :head, :repo, :full_name),
-                         target_repository_full_name: @payload.dig(:pull_request, :base, :repo, :full_name) })
-      when 'push' # GitHub doesn't have different push events for commits and tags
-        github_payload_push(payload)
-      end
-      payload
-    end
 
     def gitlab_extractor_payload
       http_url = @payload.dig(:project, :http_url)
@@ -105,18 +83,6 @@ module TriggerControllerService
       payload
     end
 
-    def github_api_endpoint
-      sender_url = @payload.dig(:sender, :url)
-      return unless sender_url
-
-      host = URI.parse(sender_url).host
-      if host.start_with?('api.github.com')
-        "https://#{host}"
-      else
-        "https://#{host}/api/v3/"
-      end
-    end
-
     def gitea_api_endpoint(http_url)
       url = URI.parse(http_url)
 
@@ -128,30 +94,6 @@ module TriggerControllerService
 
       uri = URI.parse(http_url)
       "#{uri.scheme}://#{uri.host}"
-    end
-
-    def github_payload_push(payload)
-      payload_ref = @payload.fetch(:ref, '')
-      payload.merge!({
-                       # We need this for Workflow::Step#branch_request_content_github
-                       source_repository_full_name: @payload.dig(:repository, :full_name),
-                       # We need this for SCMStatusReporter#call
-                       target_repository_full_name: @payload.dig(:repository, :full_name),
-                       ref: payload_ref,
-                       # We need this for Workflow::Step#branch_request_content_{github,gitlab}
-                       commit_sha: @payload[:after],
-                       # We need this for Workflows::YAMLDownloader#download_url
-                       # when the push event is for commits, we get the branch name from ref.
-                       target_branch: payload_ref.sub('refs/heads/', '')
-                     })
-
-      return unless payload_ref.start_with?('refs/tags/')
-
-      # We need this for Workflow::Step#target_package_name
-      # 'target_branch' will contain a commit SHA
-      payload.merge!({ tag_name: payload_ref.sub('refs/tags/', ''),
-                       target_branch: @payload.dig(:head_commit, :id),
-                       commit_sha: @payload.dig(:head_commit, :id) })
     end
 
     def gitlab_payload_tag(payload)
