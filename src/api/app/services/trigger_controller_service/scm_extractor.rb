8df92d5d677fdbad4a105be011dea8dcdbe5be0b
Dani Donisa <dani.doni@gmail.com>
2022-12-21 15:42:51 +0100

    Minor refactoring

--- src/api/app/services/trigger_controller_service/scm_extractor.rb
+++ src/api/app/services/trigger_controller_service/scm_extractor.rb
@@ -1,6 +1,12 @@
 module TriggerControllerService
   # NOTE: this class is coupled to GitHub pull requests events and GitLab merge requests events.
   class SCMExtractor
+    SCM_EXTRACTORS = {
+      'github' => GithubPayloadExtractor,
+      'gitlab' => GitlabPayloadExtractor,
+      'gitea' => GiteaPayloadExtractor
+    }.freeze
+
     def initialize(scm, event, payload)
       # TODO: What should we do when the user sends a wwwurlencoded payload? Raise an exception?
       @payload = payload.deep_symbolize_keys
@@ -10,14 +16,10 @@ module TriggerControllerService
 
     # TODO: What happens when some of the keys are missing?
     def call
-      case @scm
-      when 'github'
-        SCMWebhook.new(payload: GithubPayloadExtractor.new(@event, @payload).payload)
-      when 'gitlab'
-        SCMWebhook.new(payload: GitlabPayloadExtractor.new(@event, @payload).payload)
-      when 'gitea'
-        SCMWebhook.new(payload: GiteaPayloadExtractor.new(@event, @payload).payload)
-      end
+      extractor = SCM_EXTRACTORS[@scm]
+      return unless extractor
+
+      SCMWebhook.new(payload: extractor.new(@event, @payload).payload)
     end
   end
 end
