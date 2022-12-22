2705401ae9e688227113d245009e563d824cddfa
Dani Donisa <dani.doni@gmail.com>
2022-12-22 09:19:29 +0100

    Extract a github push event class

--- src/api/app/models/github_payload_extractor.rb
+++ src/api/app/models/github_payload_extractor.rb
@@ -18,7 +18,7 @@ class GithubPayloadExtractor < ScmPayloadExtractor
     when 'pull_request'
       return Github::PullRequest.new(event, webhook_payload).payload
     when 'push' # GitHub doesn't have different push events for commits and tags
-      github_payload_push(payload)
+      return Github::Push.new(event, webhook_payload).payload
     end
 
     payload
@@ -36,29 +36,5 @@ class GithubPayloadExtractor < ScmPayloadExtractor
     else
       "https://#{host}/api/v3/"
     end
-  end
-
-  def github_payload_push(payload)
-    payload_ref = @webhook_payload.fetch(:ref, '')
-    payload.merge!({
-                     # We need this for Workflow::Step#branch_request_content_github
-                     source_repository_full_name: @webhook_payload.dig(:repository, :full_name),
-                     # We need this for SCMStatusReporter#call
-                     target_repository_full_name: @webhook_payload.dig(:repository, :full_name),
-                     ref: payload_ref,
-                     # We need this for Workflow::Step#branch_request_content_{github,gitlab}
-                     commit_sha: @webhook_payload[:after],
-                     # We need this for Workflows::YAMLDownloader#download_url
-                     # when the push event is for commits, we get the branch name from ref.
-                     target_branch: payload_ref.sub('refs/heads/', '')
-                   })
-
-    return unless payload_ref.start_with?('refs/tags/')
-
-    # We need this for Workflow::Step#target_package_name
-    # 'target_branch' will contain a commit SHA
-    payload.merge!({ tag_name: payload_ref.sub('refs/tags/', ''),
-                     target_branch: @webhook_payload.dig(:head_commit, :id),
-                     commit_sha: @webhook_payload.dig(:head_commit, :id) })
   end
 end
