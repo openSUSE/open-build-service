99812ece392184316569a499b4e7a3394a1fd7fb
Dani Donisa <dani.doni@gmail.com>
2022-12-22 11:45:58 +0100

    Extract a gitea pull request event class

--- src/api/app/models/gitea_payload_extractor.rb
+++ src/api/app/models/gitea_payload_extractor.rb
@@ -19,13 +19,7 @@ class GiteaPayloadExtractor < ScmPayloadExtractor
 
     case event
     when 'pull_request'
-      payload.merge!({ commit_sha: webhook_payload.dig(:pull_request, :head, :sha),
-                       pr_number: webhook_payload[:number],
-                       source_branch: webhook_payload.dig(:pull_request, :head, :ref),
-                       target_branch: webhook_payload.dig(:pull_request, :base, :ref),
-                       action: webhook_payload[:action],
-                       source_repository_full_name: webhook_payload.dig(:pull_request, :head, :repo, :full_name),
-                       target_repository_full_name: webhook_payload.dig(:pull_request, :base, :repo, :full_name) })
+      return GiteaPayload::PullRequest.new(event, webhook_payload).payload
     when 'push' # GitHub doesn't have different push events for commits and tags
       gitea_payload_push(payload)
     end
