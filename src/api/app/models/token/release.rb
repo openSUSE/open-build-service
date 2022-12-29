b519d7aeff39ad19241de90e52095a007c0371f7
Eduardo Navarro <enavarro@suse.com>
2022-12-29 16:14:40 +0100

    Solve Metrics/CyclomaticComplexity RuboCop offense

--- src/api/app/models/token/release.rb
+++ src/api/app/models/token/release.rb
@@ -1,18 +1,6 @@
 class Token::Release < Token
   include MaintenanceHelper
 
-  def release(package_to_release, source_repository, target_repository, time_now, options)
-    opts = { filter_source_repository: source_repository,
-             manual: true,
-             comment: 'Releasing via trigger event' }
-    opts[:multibuild_container] = options[:multibuild_flavor] if options[:multibuild_flavor].present?
-    opts[:filter_architecture] = options[:arch] if options[:arch].present?
-    release_package(package_to_release,
-                    target_repository,
-                    package_to_release.release_target_name(target_repository, time_now),
-                    opts)
-  end
-
   def call(options)
     set_triggered_at
     return unless options[:package]
@@ -31,6 +19,28 @@ class Token::Release < Token
       return
     end
 
+    release_manually_triggered_targets(package_to_release, time_now, options)
+  end
+
+  def package_find_options
+    { use_source: true, follow_project_links: false, follow_multibuild: true }
+  end
+
+  private
+
+  def release(package_to_release, source_repository, target_repository, time_now, options)
+    opts = { filter_source_repository: source_repository,
+             manual: true,
+             comment: 'Releasing via trigger event' }
+    opts[:multibuild_container] = options[:multibuild_flavor] if options[:multibuild_flavor].present?
+    opts[:filter_architecture] = options[:arch] if options[:arch].present?
+    release_package(package_to_release,
+                    target_repository,
+                    package_to_release.release_target_name(target_repository, time_now),
+                    opts)
+  end
+
+  def release_manually_triggered_targets(package_to_release, time_now, options)
     manual_release_targets = package_to_release.project.release_targets.where(trigger: 'manual')
     raise NoReleaseTargetFound, "#{package_to_release.project} has no release targets that are triggered manually" unless manual_release_targets.any?
 
@@ -38,10 +48,6 @@ class Token::Release < Token
     manual_release_targets.each do |release_target|
       release(package_to_release, release_target.repository, release_target.target_repository, time_now, options)
     end
-  end
-
-  def package_find_options
-    { use_source: true, follow_project_links: false, follow_multibuild: true }
   end
 end
 
