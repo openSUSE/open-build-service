95c5ebe5633981160688aaf7c690d689343bd590
ncounter <dleidi@suse.com>
2023-01-19 15:45:47 +0100

    Show and dynamically update action_seen flag in the dropdown component too

--- src/api/app/components/action_seen_by_user_component.rb
+++ src/api/app/components/action_seen_by_user_component.rb
@@ -1,9 +1,10 @@
 class ActionSeenByUserComponent < ApplicationComponent
-  def initialize(action:, user:)
+  def initialize(action:, user:, render_only: false)
     super
 
     @action = action
     @user = user
+    @render_only = render_only
   end
 
   def seen_by_user
