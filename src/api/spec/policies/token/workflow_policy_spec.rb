71011526abd70cc915f45f92d31e62ab27229c98
Saray Cabrera Padrón <scabrerapadron@suse.de>
2023-02-08 12:18:36 +0100

    Adapt policy spec to drop trigger_workflow toggle

--- src/api/spec/policies/token/workflow_policy_spec.rb
+++ src/api/spec/policies/token/workflow_policy_spec.rb
@@ -8,11 +8,6 @@ RSpec.describe Token::WorkflowPolicy do
 
   subject { described_class }
 
-  before do
-    Flipper.enable(:trigger_workflow, user)
-    Flipper.enable(:trigger_workflow, other_user)
-  end
-
   describe '#trigger' do
     context 'user inactive' do
       include_examples 'non-active users cannot trigger a token'
@@ -29,72 +24,56 @@ RSpec.describe Token::WorkflowPolicy do
   end
 
   permissions :create? do
-    context 'when the user has the feature enabled' do
-      context 'when the user is the owner of the token' do
-        it { is_expected.to permit(user, user_token) }
-      end
-
-      context 'when the user belongs to a group which owns the token' do
-        let(:group_token) { create(:workflow_token, executor: user) }
-
-        before { group.shared_workflow_tokens << group_token }
-
-        it { is_expected.to permit(other_user, group_token) }
-      end
-
-      context 'when the user is not the owner of the token' do
-        context 'and the token has not been shared with that user' do
-          it { is_expected.not_to permit(other_user, user_token) }
-        end
-
-        context 'but the token has been shared with that user' do
-          before do
-            other_user.shared_workflow_tokens << user_token
-          end
-
-          it { is_expected.to permit(other_user, user_token) }
-        end
-
-        context "but the token has been shared with that users's group" do
-          before do
-            group.shared_workflow_tokens << user_token
-          end
-
-          it { is_expected.to permit(other_user, user_token) }
-        end
-      end
+    context 'when the user is the owner of the token' do
+      it { is_expected.to permit(user, user_token) }
     end
 
-    context 'when the user does not have the feature enabled' do
-      before { Flipper.disable(:trigger_workflow, user) }
+    context 'when the user belongs to a group which owns the token' do
+      let(:group_token) { create(:workflow_token, executor: user) }
 
-      it { is_expected.not_to permit(user, user_token) }
+      before { group.shared_workflow_tokens << group_token }
+
+      it { is_expected.to permit(other_user, group_token) }
+    end
+
+    context 'when the user is not the owner of the token' do
+      context 'and the token has not been shared with that user' do
+        it { is_expected.not_to permit(other_user, user_token) }
+      end
+
+      context 'but the token has been shared with that user' do
+        before do
+          other_user.shared_workflow_tokens << user_token
+        end
+
+        it { is_expected.to permit(other_user, user_token) }
+      end
+
+      context "but the token has been shared with that users's group" do
+        before do
+          group.shared_workflow_tokens << user_token
+        end
+
+        it { is_expected.to permit(other_user, user_token) }
+      end
     end
   end
 
   permissions :destroy? do
-    context 'when the user has the feature enabled' do
-      context 'when the user is the owner of the token' do
-        it { is_expected.to permit(user, user_token) }
-      end
-
-      context 'when the user belongs to a group which owns the token' do
-        let(:group_token) { create(:workflow_token, executor: user) }
-
-        before { group.shared_workflow_tokens << group_token }
-
-        it { is_expected.to permit(other_user, group_token) }
-      end
-
-      context 'when the user does not own the token' do
-        it { is_expected.not_to(permit(other_user, user_token)) }
-      end
+    context 'when the user is the owner of the token' do
+      it { is_expected.to permit(user, user_token) }
     end
 
-    context 'when the user does not have the feature enabled' do
-      before { Flipper.disable(:trigger_workflow, user) }
+    context 'when the user belongs to a group which owns the token' do
+      let(:group_token) { create(:workflow_token, executor: user) }
 
-      it { is_expected.not_to permit(user, user_token) }
+      before { group.shared_workflow_tokens << group_token }
+
+      it { is_expected.to permit(other_user, group_token) }
+    end
+
+    context 'when the user does not own the token' do
+      it { is_expected.not_to(permit(other_user, user_token)) }
     end
   end
 end
