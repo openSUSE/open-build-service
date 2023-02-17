a6b2686447b738c471767cbe20da2b5ae1c5abcb
Lukas Krause <lkrause@suse.de>
2023-02-17 11:38:45 +0100

    Don't inherit from GithubPayload::Push class in GiteaPayload::Push
    Now that we going to use the `:deleted` field from the
    Github payload for pushes, we have to handle them seperate
    again, since Gitea doesn't provide this in the payload.

--- src/api/app/models/gitea_payload/push.rb
+++ src/api/app/models/gitea_payload/push.rb
@@ -1,21 +1,28 @@
 # This class is used in TriggerControllerServicve::ScmExtractor to handle push events coming from Gitea.
 # It's basically the same than the pushes coming from Github but with some customizations on top.
-class GiteaPayload::Push < GithubPayload::Push
+class GiteaPayload::Push < GiteaPayload
   def payload
-    super.merge(scm: 'gitea',
-                http_url: http_url,
-                api_endpoint: api_endpoint)
-  end
+    payload_ref = webhook_payload.fetch(:ref, '')
+    payload = default_payload.merge(
+      event: 'push',
+      # We need this for Workflow::Step#branch_request_content_github
+      source_repository_full_name: webhook_payload.dig(:repository, :full_name),
+      # We need this for SCMStatusReporter#call
+      target_repository_full_name: webhook_payload.dig(:repository, :full_name),
+      ref: payload_ref,
+      # We need this for Workflow::Step#branch_request_content_{github,gitlab}
+      commit_sha: webhook_payload[:after],
+      # We need this for Workflows::YAMLDownloader#download_url
+      # when the push event is for commits, we get the branch name from ref.
+      target_branch: payload_ref.sub('refs/heads/', '')
+    )
 
-  private
+    return payload unless payload_ref.start_with?('refs/tags/')
 
-  def http_url
-    webhook_payload.dig(:repository, :clone_url)
-  end
-
-  def api_endpoint
-    url = URI.parse(http_url)
-
-    "#{url.scheme}://#{url.host}"
+    # We need this for Workflow::Step#target_package_name
+    # 'target_branch' will contain a commit SHA
+    payload.merge(tag_name: payload_ref.sub('refs/tags/', ''),
+                  target_branch: webhook_payload.dig(:head_commit, :id),
+                  commit_sha: webhook_payload.dig(:head_commit, :id))
   end
 end
