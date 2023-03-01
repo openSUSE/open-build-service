dd107b982f95945c45b4ea0c63bb934f0f6aef77
Dani Donisa <dani.doni@gmail.com>
2023-03-01 18:06:39 +0100

    Feature specs to test most used request show features
    This PR adds a feature spec to test the main features of the request
    show page under the beta flag.
    - Multiple actions
    - Staging request

--- src/api/spec/support/beta.rb
+++ src/api/spec/support/beta.rb
@@ -2,5 +2,6 @@ RSpec.configure do |config|
   config.before(:example, beta: true) do
     # Add here the feature flags you want to enable on your beta features' tests
     # Example: Flipper.enable(:foo)
+    Flipper.enable(:request_show_redesign)
   end
 end
