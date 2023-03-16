af3e5e9f647fc058d3328a8a1588fc428422bc40
Jacob Michalskie <jacob.michalskie@suse.com>
2023-03-16 12:22:49 +0100

    Perform a single query to check which lines have comments

--- src/api/app/components/diff_component.rb
+++ src/api/app/components/diff_component.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class DiffComponent < ApplicationComponent
-  attr_reader :diff, :file_index, :commentable
+  attr_reader :diff, :file_index, :commentable, :commented_lines
 
-  def initialize(diff:, file_index:, commentable: nil)
+  def initialize(diff:, file_index:, commentable: nil, commented_lines: [])
     super
     @diff = parse_diff(diff)
     @file_index = file_index
     @commentable = commentable
+    @commented_lines = commented_lines
   end
 
   def render?
