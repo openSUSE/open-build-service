df568cfbcbad1d5fa6d62fb8e90ca5413d3160d8
Jacob Michalskie <jacob.michalskie@suse.com>
2023-03-23 15:20:55 +0100

    Display diff comments in the timeline

--- src/api/app/components/bs_request_activity_timeline_component.rb
+++ src/api/app/components/bs_request_activity_timeline_component.rb
@@ -10,11 +10,24 @@ class BsRequestActivityTimelineComponent < ApplicationComponent
     super
     @bs_request = bs_request
     @creator = User.find_by_login(bs_request.creator) || User.nobody
+    action_comments = @bs_request.bs_request_actions.flat_map { |a| a.comments.without_parent.includes(:user) }
+    commented_actions = action_comments.map { |c| c.commentable.id }.uniq.compact
+    @diffs = commented_actions.flat_map { |a| @bs_request.webui_actions(action_id: a, diffs: true, cacheonly: 1) }
 
     # IDEA: Forge the first item to remove the need of having the hand-crafted "created request" haml fragment
     @timeline = (
+      action_comments +
       @bs_request.comments.without_parent.includes(:user) +
       @bs_request.history_elements.includes(:user)
     ).compact.sort_by(&:created_at)
+  end
+
+  def diff_for_ref(comment)
+    return unless comment.diff_ref
+
+    file_index = comment.diff_ref.match(/diff_([0-9]*)/).captures.first
+    sourcediff = @diffs.find { |d| d[:id] == comment.commentable.id }[:sourcediff].first
+    filename = sourcediff.dig('filenames', file_index.to_i)
+    sourcediff.dig('files', filename)
   end
 end
