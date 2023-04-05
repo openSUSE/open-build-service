b949f35f40a165bca35c0522efb7a06c1d63a2c1
Jacob Michalskie <jacob.michalskie@suse.com>
2023-04-05 12:32:49 +0200

    Move url retrieval to binaries helper

--- src/api/app/controllers/webui/package_controller.rb
+++ src/api/app/controllers/webui/package_controller.rb
@@ -218,7 +218,7 @@ class Webui::PackageController < Webui::WebuiController
     @fileinfo = Backend::Api::BuildResults::Binaries.fileinfo_ext(@project, params[:package], @repository.name, @architecture.name, @filename)
     raise ActiveRecord::RecordNotFound, 'Not Found' unless @fileinfo
 
-    @download_url = download_url_for_binary(architecture_name: @architecture.name, file_name: @filename)
+    @download_url = download_url_for_binary(@project, @repository, @package, @architecture.name, @filename)
   end
 
   # FIXME: This is Webui::Packages::BinariesController#index
@@ -237,7 +237,7 @@ class Webui::PackageController < Webui::WebuiController
           build_results_set[:binaries] << { filename: binary['filename'],
                                             size: binary['size'],
                                             links: { details?: QUERYABLE_BUILD_RESULTS.any? { |regex| regex.match?(binary['filename']) },
-                                                     download_url: download_url_for_binary(architecture_name: result['arch'], file_name: binary['filename']),
+                                                     download_url: download_url_for_binary(@project, @repository, @package, result['arch'], binary['filename']),
                                                      cloud_upload?: uploadable?(binary['filename'], result['arch']) } }
         end
       end
@@ -636,31 +636,6 @@ class Webui::PackageController < Webui::WebuiController
   end
 
   private
-
-  # Get an URL to a binary produced by the build.
-  # In the published repo for everyone, in the backend directly only for logged in users.
-  def download_url_for_binary(architecture_name:, file_name:)
-    if publishing_enabled(architecture_name: architecture_name)
-      published_url = Backend::Api::BuildResults::Binaries.download_url_for_file(@project.name, @repository.name, params[:package], architecture_name, file_name)
-      return published_url if published_url
-    end
-
-    return "/build/#{@project.name}/#{@repository.name}/#{architecture_name}/#{params[:package]}/#{file_name}" if User.session
-  end
-
-  def publishing_enabled(architecture_name:)
-    if @project == @package.project
-      @package.enabled_for?('publish', @repository.name, architecture_name)
-    else
-      # We are looking at a package coming through a project link
-      # Let's see if we rebuild linked packages.
-      # NOTE: linkedbuild=localdep||alldirect would be too much hassle to figure out...
-      return false if @repository.linkedbuild != 'all'
-
-      # If we are rebuilding packages, let's ask @project if it publishes.
-      @project.enabled_for?('publish', @repository.name, architecture_name)
-    end
-  end
 
   def package_params
     params.require(:package).permit(:name, :title, :description)
--- src/api/app/helpers/webui/packages/binaries_helper.rb
+++ src/api/app/helpers/webui/packages/binaries_helper.rb
@@ -4,4 +4,31 @@ module Webui::Packages::BinariesHelper
   def uploadable?(filename, architecture)
     ::Cloud::UploadJob.new(filename: filename, arch: architecture).uploadable?
   end
+
+  # Get an URL to a binary produced by the build.
+  # In the published repo for everyone, in the backend directly only for logged in users.
+  def download_url_for_binary(project, repository, package, architecture_name, file_name)
+    if publishing_enabled(project, repository, package, architecture_name)
+      published_url = Backend::Api::BuildResults::Binaries.download_url_for_file(project.name, repository.name, package.name, architecture_name, file_name)
+      return published_url if published_url
+    end
+
+    return "/build/#{project.name}/#{repository.name}/#{architecture_name}/#{package.name}/#{file_name}" if User.session
+  end
+
+  private
+
+  def publishing_enabled(project, repository, package, architecture_name)
+    if project == package.project
+      package.enabled_for?('publish', repository.name, architecture_name)
+    else
+      # We are looking at a package coming through a project link
+      # Let's see if we rebuild linked packages.
+      # NOTE: linkedbuild=localdep||alldirect would be too much hassle to figure out...
+      return false if repository.linkedbuild != 'all'
+
+      # If we are rebuilding packages, let's ask project if it publishes.
+      project.enabled_for?('publish', repository.name, architecture_name)
+    end
+  end
 end
