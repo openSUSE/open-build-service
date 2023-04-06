59b5deb57a5e0748403ada37a678b1ca30c06d68
ncounter <dleidi@suse.com>
2023-04-06 16:57:42 +0200

    Differentiate request state badge icon

--- src/api/app/components/bs_request_state_badge_component.rb
+++ src/api/app/components/bs_request_state_badge_component.rb
@@ -9,7 +9,7 @@ class BsRequestStateBadgeComponent < ApplicationComponent
   def call
     content_tag(
       :span,
-      tag.i(class: 'fas fa-code-pull-request me-1').concat(@bs_request.state),
+      icon_state_tag.concat(@bs_request.state),
       class: ['badge', "bg-#{decode_state_color(@bs_request.state)}", @css_class]
     )
   end
@@ -18,14 +18,45 @@ class BsRequestStateBadgeComponent < ApplicationComponent
     case state
     when :review, :new
       'secondary'
-    when :declined, :revoke
+    when :declined
       'danger'
     when :superseded
       'warning'
     when :accepted
       'success'
+    when :revoked
+      'dismissed'
     else
       'dark'
+    end
+  end
+
+  def decode_state_icon(state)
+    case state
+    when :new
+      'code-branch'
+    when :review, :declined, :revoked
+      'code-pull-request'
+    when :superseded
+      'code-compare'
+    when :accepted
+      'code-merge'
+    else
+      'code-fork'
+    end
+  end
+
+  def icon_state_tag
+    if [:declined, :revoked].include?(@bs_request.state)
+      content_tag(
+        :span,
+        tag.i(class: "fas fa-#{decode_state_icon(@bs_request.state)}").concat(
+          tag.i(class: 'fas fa-slash fa-stack-1x fa-stack-slash top-icon')
+        ),
+        class: 'position-relative me-1'
+      )
+    else
+      tag.i(class: "fas fa-#{decode_state_icon(@bs_request.state)} me-1")
     end
   end
 end
