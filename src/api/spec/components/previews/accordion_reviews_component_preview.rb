88cc3c3bd4daa0cd4163b5d604c7ab1396be0c9d
Saray Cabrera Padrón <scabrerapadron@suse.de>
2023-04-12 16:34:08 +0200

    Adapt AccordionReviewsComponentPreview
    Don't persist the data to be used by the component.

--- src/api/spec/components/previews/accordion_reviews_component_preview.rb
+++ src/api/spec/components/previews/accordion_reviews_component_preview.rb
@@ -1,9 +1,14 @@
 class AccordionReviewsComponentPreview < ViewComponent::Preview
+  include FactoryBot::Syntax::Methods
+
   # Preview at http://HOST:PORT/rails/view_components/accordion_reviews_component/preview
   def preview
-    pending_reviews = Review.opened.take(3)
-    accepted_reviews = Review.accepted.take(2)
-    declined_review = Review.declined.take(1)
+    # TODO: revisit the component so we are able to simply instantiate the user instead of using one from DB.
+    preview_tester = User.first
+
+    pending_reviews = build_list(:user_review, 3, state: :new, by_user: preview_tester.login, user: preview_tester)
+    accepted_reviews = build_list(:user_review, 2, state: :accepted, by_user: preview_tester.login, user: preview_tester)
+    declined_review = build_list(:user_review, 1, state: :declined, by_user: preview_tester.login, user: preview_tester)
     render(AccordionReviewsComponent.new(pending_reviews + accepted_reviews + declined_review, :review, can_handle_request: true))
   end
 end
