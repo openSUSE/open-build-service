e36265c823b28311e039b887032455bcde06862f
Jacob Michalskie <jacob.michalskie@suse.com>
2023-04-13 12:11:04 +0200

    Hide api_key from logs

--- src/api/app/models/issue_tracker.rb
+++ src/api/app/models/issue_tracker.rb
@@ -17,7 +17,7 @@ class IssueTracker < ApplicationRecord
   after_save :update_package_meta
 
   # FIXME: issues_updated should not be hidden, but it should also not break our api
-  DEFAULT_RENDER_PARAMS = { except: [:id, :password, :user, :issues_updated], dasherize: true, skip_types: true, skip_instruct: true }.freeze
+  DEFAULT_RENDER_PARAMS = { except: [:id, :password, :user, :issues_updated, :api_key], dasherize: true, skip_types: true, skip_instruct: true }.freeze
 
   before_validation(on: :create) do
     self.issues_updated ||= Time.now
