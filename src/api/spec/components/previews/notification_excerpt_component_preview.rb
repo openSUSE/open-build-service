e6ce3d7e7e551cb73400a02b0d8538337ffa07e3
Saray Cabrera Padrón <scabrerapadron@suse.de>
2023-04-14 14:28:28 +0200

    Simplify NotificationExcerptComponent
    So the object is passed to the component and not retrieved inside.
    That makes possible to not persist any object to use its preview.

--- src/api/spec/components/previews/notification_excerpt_component_preview.rb
+++ src/api/spec/components/previews/notification_excerpt_component_preview.rb
@@ -1,6 +1,19 @@
 class NotificationExcerptComponentPreview < ViewComponent::Preview
-  # Preview at http://HOST:PORT/rails/view_components/notification_excerpt_component/preview
-  def preview
-    render(NotificationExcerptComponent.new(Notification.last))
+  # Preview at http://HOST:PORT/rails/view_components/notification_excerpt_component/with_comment_notifiable
+  def with_comment_notifiable
+    notifiable = Comment.new(body: Faker::Lorem.paragraph_by_chars(number: 200))
+    render(NotificationExcerptComponent.new(notifiable))
+  end
+
+  # Preview at http://HOST:PORT/rails/view_components/notification_excerpt_component/with_request_notifiable
+  def with_request_notifiable
+    notifiable = BsRequest.new(description: Faker::Lorem.paragraph_by_chars(number: 200))
+    render(NotificationExcerptComponent.new(notifiable))
+  end
+
+  # Preview at http://HOST:PORT/rails/view_components/notification_excerpt_component/with_review_notifiable
+  def with_review_notifiable
+    notifiable = Review.new
+    render(NotificationExcerptComponent.new(notifiable))
   end
 end
