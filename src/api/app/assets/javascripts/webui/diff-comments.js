4d57d01aac5673cbb93c6e10a416507deda14982
Rubhan Azeem <rubhanazeem@gmail.com>
2023-05-09 16:12:20 +0200

    Checkpoint

--- src/api/app/assets/javascripts/webui/diff-comments.js
+++ src/api/app/assets/javascripts/webui/diff-comments.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   $('.line-new-comment').on('click', function(){
-    console.log('diff comments')
-    var id = $(this).attr('id')
-    console.log(id)
+    var lineNumber = $(this).data('id')
+    var requestNumber = $(this).data('request-number')
+    var actionId = $(this).data('action-id')
+    var url = '/request/' + requestNumber + '/request_action/' + actionId + '/inline_comment/' + lineNumber
+
+    $.ajax({
+      url: url
+    });
   })
 })
