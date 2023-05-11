babde8f2e05e08724d3b4e1c65b7f0edf7c5d570
Lukas Krause <lkrause@suse.de>
2023-05-11 20:17:32 +0200

    Use pundit policy for `handle_request?` in request beta view components
    We already have a pundit policy for this, no need to add this logic
    again to the view component.

--- src/api/app/components/request_decision_component.rb
+++ src/api/app/components/request_decision_component.rb
@@ -9,7 +9,7 @@ class RequestDecisionComponent < ApplicationComponent
   end
 
   def render?
-    can_handle_request?
+    policy(@bs_request).handle_request?
   end
 
   def single_action_request
@@ -29,10 +29,6 @@ class RequestDecisionComponent < ApplicationComponent
   end
 
   # TODO: Move all those "can_*" checks to a pundit policy
-  def can_handle_request?
-    @bs_request.state.in?([:new, :review, :declined]) && (@is_target_maintainer || @is_author)
-  end
-
   def can_revoke_request?
     @is_author && @bs_request.state.in?([:new, :review, :declined])
   end
