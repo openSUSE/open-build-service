821704819cd33474b85526a79d9b54c1a446f62d
Lukas Krause <lkrause@suse.de>
2023-05-11 18:17:20 +0200

    Move `can_add_reviews` logic to a pundit policy and use in request beta views
    This code is for authorization and therefore should be in a
    pundit policy.

--- src/api/app/policies/bs_request_policy.rb
+++ src/api/app/policies/bs_request_policy.rb
@@ -13,4 +13,11 @@ class BsRequestPolicy < ApplicationPolicy
     is_author = record.creator == user.login
     record.state.in?([:new, :review, :declined]) && (is_target_maintainer || is_author)
   end
+
+  def add_reviews?
+    is_target_maintainer = record.is_target_maintainer?(user)
+    is_author = record.creator == user.login
+    has_open_reviews = record.reviews.where(state: 'new').select { |review| review.matches_user?(user) }.present?
+    record.state.in?([:new, :review]) && (is_author || is_target_maintainer || has_open_reviews)
+  end
 end
