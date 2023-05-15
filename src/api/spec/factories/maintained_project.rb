6c8c710408a3e6e832f2205f37f2a9575db4ce5f
Saray Cabrera Padrón <scabrerapadron@suse.de>
2023-05-15 16:08:32 +0200

    Clarify what maintained_project factory does
    Co-authored-by: Eduardo Navarro <enavarro@suse.com>

--- src/api/spec/factories/maintained_project.rb
+++ src/api/spec/factories/maintained_project.rb
@@ -1,3 +1,7 @@
+# Create a relationship between an update project and a maintenance project. Example:
+#   project: openSUSE:Leap:15.4:Update
+#   maintenance_project: openSUSE:Maintenance
+
 FactoryBot.define do
   factory :maintained_project do
     project
