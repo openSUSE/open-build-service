27ee0280b5e476c60c33b59a684dff7c0b99ac0d
Eduardo Navarro <enavarro@suse.com>
2023-06-02 11:20:30 +0200

    Adapt spider tests to new /apidocs-old path

--- src/api/test/functional/webui/spider_test.rb
+++ src/api/test/functional/webui/spider_test.rb
@@ -27,8 +27,8 @@ class Webui::SpiderTest < Webui::IntegrationTest
     return true if link =~ %r{/live_build_log}
     # we do not really serve binary packages in the test environment
     return true if link =~ %r{/package/binary/}
-    # apidocs is not configured in test environment
-    return true if link.end_with?('/apidocs/index')
+    # old apidocs are not configured in test environment
+    return true if link.end_with?('/apidocs-old/index')
     # no need to visit flipper
     return true if link.end_with?('/flipper')
   end
