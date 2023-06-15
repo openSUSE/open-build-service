ce899b79cf1b6345cb7efe76c86432d71f252219
Saray Cabrera Padrón <scabrerapadron@suse.de>
2023-06-15 15:55:19 +0200

    Display workflow config file URL or path
    In the workflow run view there is a new tab, `Workflow
    configuration`, where we are going to display all the information
    related to the workflow configuration file: url, path, content etc.

--- src/api/app/components/workflow_run_detail_component.rb
+++ src/api/app/components/workflow_run_detail_component.rb
@@ -1,11 +1,12 @@
 class WorkflowRunDetailComponent < ApplicationComponent
-  attr_reader :id, :request_headers, :pretty_request_payload,
+  attr_reader :id, :workflow_run, :request_headers, :pretty_request_payload,
               :response_url, :response_body, :artifacts,
               :scm_vendor, :status_reports
 
   def initialize(workflow_run:)
     super
     @id = workflow_run.id
+    @workflow_run = workflow_run
     @request_headers = workflow_run.request_headers
     @pretty_request_payload = parse_payload(workflow_run)
     @response_url = workflow_run.response_url
@@ -21,5 +22,19 @@ class WorkflowRunDetailComponent < ApplicationComponent
     JSON.pretty_generate(JSON.parse(workflow_run.request_payload))
   rescue JSON::ParserError
     workflow_run.request_payload
+  end
+
+  # Old workflow run entries didn't store the configuration-related information
+  def workflow_configuration_data
+    configuration_source = [workflow_run.workflow_configuration_url,
+                            workflow_run.workflow_configuration_path].filter_map(&:presence).first
+
+    return content_tag(:p, 'This information is not available.') unless configuration_source
+
+    content_tag(:h5, "Workflow Configuration File #{workflow_run.workflow_configuration_url.present? ? 'URL' : 'Path'}").concat(
+      content_tag(:pre,
+                  configuration_source,
+                  class: 'border p-2')
+    )
   end
 end
