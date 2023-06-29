614049e1c0543d9668c5989853fccaf277cc2468
Adrian Schröter <adrian@suse.de>
2023-06-29 09:45:11 +0200

    [api] fix delivering of delete package meta data
    api blocked requests for deleted packages always.
    
    Co-Author: Eduardo Navarro <enavarro@suse.com>

--- src/api/app/controllers/source_project_package_meta_controller.rb
+++ src/api/app/controllers/source_project_package_meta_controller.rb
@@ -20,13 +20,18 @@ class SourceProjectPackageMetaController < SourceController
 
   # GET /source/:project/:package/_meta
   def show
-    pack = Package.get_by_project_and_name(@project_name, @package_name, use_source: false)
+    if params.key?(:deleted)
+      validate_read_access_of_deleted_package(@project_name, @package_name)
+    else
+      pack = Package.get_by_project_and_name(@project_name, @package_name, use_source: false)
+    end
 
-    if params.key?(:meta) || params.key?(:rev) || params.key?(:view) || pack.nil?
+    if params.key?(:deleted) || params.key?(:meta) || params.key?(:rev) || params.key?(:view) || pack.nil?
       # check if this comes from a remote project, also true for _project package
-      # or if meta is specified we need to fetch the meta from the backend
+      # deleted package meta gets also delivered if former existing
+      # meta parameter is used as a hack to use back as authorative instead
       path = request.path_info
-      path += build_query_from_hash(params, [:meta, :rev, :view])
+      path += build_query_from_hash(params, [:deleted, :meta, :rev, :view])
       pass_to_backend(path)
       return
     end
