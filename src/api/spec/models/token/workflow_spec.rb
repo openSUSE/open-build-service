e333aaefa6444d1115a138d32a6a711d3e31f8a3
hudeng <hudeng@deepin.org>
2023-06-30 14:38:01 +0800

    [src/api]: update test for token workflow

--- src/api/spec/models/token/workflow_spec.rb
+++ src/api/spec/models/token/workflow_spec.rb
@@ -70,7 +70,7 @@ RSpec.describe Token::Workflow do
       let(:scm_extractor) { TriggerControllerService::SCMExtractor.new(scm, event, github_payload) }
       let(:scm_webhook) { SCMWebhook.new(payload: github_extractor_payload) }
       let(:yaml_downloader) { Workflows::YAMLDownloader.new(scm_webhook.payload, token: workflow_token) }
-      let(:yaml_file) { Rails.root.join('spec/support/files/workflows.yml').expand_path }
+      let(:yaml_file) { Rails.root.join('spec/support/files/workflows_with_repository_configuration.yml').expand_path }
       let(:yaml_to_workflows_service) { Workflows::YAMLToWorkflowsService.new(yaml_file: yaml_file, scm_webhook: scm_webhook, token: workflow_token, workflow_run: workflow_run) }
       let(:workflow) do
         Workflow.new(scm_webhook: scm_webhook, token: workflow_token,
