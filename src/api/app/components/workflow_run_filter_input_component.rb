c6d4632557a6653d175ab2f7b626c99584b188ae
ncounter <dleidi@suse.com>
2023-08-02 15:36:28 +0200

    Workflow run input filter to look&feel like the existing ones

--- src/api/app/components/workflow_run_filter_input_component.rb
+++ src/api/app/components/workflow_run_filter_input_component.rb
@@ -10,16 +10,8 @@ class WorkflowRunFilterInputComponent < ApplicationComponent
     @token_id = token_id
   end
 
-  def css_for_label
-    @selected_input_filter ? 'fs-6 fw-bold' : 'fs-6'
-  end
-
-  def css_for_input
-    @selected_input_filter ? 'form-control form-control-sm mx-2 fw-bold' : 'form-control form-control-sm mx-2'
-  end
-
-  def css_for_button
-    @selected_input_filter ? 'btn btn-sm btn-outline-success px-3 bg-primary text-light' : 'btn btn-sm btn-outline-success px-3'
+  def css_for_filter_item
+    'active' if @selected_input_filter
   end
 
   def selected_filter_value
