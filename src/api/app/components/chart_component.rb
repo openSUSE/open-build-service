d1252f44e9b9a4f714e7a321c14c70b11251f81e
ncounter <dleidi@suse.com>
2023-09-01 16:14:56 +0200

    Do not show chart if data are not many

--- src/api/app/components/chart_component.rb
+++ src/api/app/components/chart_component.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 class ChartComponent < ApplicationComponent
   attr_reader :actions
 
@@ -8,11 +7,8 @@ class ChartComponent < ApplicationComponent
     @actions = actions
   end
 
-  def build_results_chart_data
-    success = {}
-    failed = {}
-    building = {}
-    refused = {}
+  def build_results_data
+    raw_data = []
 
     # take all the build results for all the actions (only actions where it makes sense to have a build status)
     @actions.where(type: [:submit, :maintenance_incident, :maintenance_release]).each do |action|
@@ -30,24 +26,46 @@ class ChartComponent < ApplicationComponent
 
       next unless action_build_results
 
-      action_build_results.each do |result_entry|
-        final_status = Buildresult.new(result_entry.code)
-        key = result_entry.repository
-
-        if final_status.successful_final_status? # success results
-          success[key] ? success.store(key, success[key] + 1) : success.store(key, 1)
-        elsif final_status.unsuccessful_final_status? # failed results
-          failed[key] ? failed.store(key, failed[key] + 1) : failed.store(key, 1)
-        elsif final_status.in_progress_status? # in progress results
-          building[key] ? building.store(key, building[key] + 1) : building.store(key, 1)
-        elsif final_status.refused_status? # non building results
-          refused[key] ? refused.store(key, refused[key] + 1) : refused.store(key, 1)
+      # carry over and expose relevant information
+      raw_data.push(
+        action_build_results.map do |result_entry|
+          {
+            architecture: result_entry.architecture,
+            repository: result_entry.repository,
+            status: result_entry.code,
+            package_name: src_pkg_obj.name,
+            project_name: src_prj_obj.name
+          }
         end
-      end
+      )
     end
 
-    # no build results available
-    return nil if success.empty? && failed.empty? && building.empty? && refused.empty?
+    raw_data.flatten
+  end
+
+  # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+  def chart_data(raw_data)
+    success = {}
+    failed = {}
+    building = {}
+    refused = {}
+
+    # reshape data in subsets to feed the chart
+    # shape of each dataset: {repository name, build count occurrencies}
+    raw_data.each do |result_entry|
+      final_status = Buildresult.new(result_entry[:status])
+      key = result_entry[:repository]
+
+      if final_status.successful_final_status? # success results
+        success[key] ? success.store(key, success[key] + 1) : success.store(key, 1)
+      elsif final_status.unsuccessful_final_status? # failed results
+        failed[key] ? failed.store(key, failed[key] + 1) : failed.store(key, 1)
+      elsif final_status.in_progress_status? # in progress results
+        building[key] ? building.store(key, building[key] + 1) : building.store(key, 1)
+      elsif final_status.refused_status? # non building results
+        refused[key] ? refused.store(key, refused[key] + 1) : refused.store(key, 1)
+      end
+    end
 
     # collect all the datasets
     [
@@ -57,6 +75,28 @@ class ChartComponent < ApplicationComponent
       { name: 'Excluded' }.merge({ data: refused })
     ]
   end
-end
+  # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
-# rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+  def distinct_repositories(raw_data)
+    raw_data.pluck(:repository).to_set
+  end
+
+  def status_color(status)
+    build_result = Buildresult.new(status)
+    return 'bg-success text-light' if build_result.successful_final_status?
+    return 'bg-danger text-light' if build_result.unsuccessful_final_status?
+    return 'bg-warning text-dark' if build_result.in_progress_status?
+
+    'bg-light text-dark border border-1' if build_result.refused_status?
+  end
+
+  def legend
+    content_tag(:div, 'Published', class: 'bg-success text-light ps-2 pe-2 m-1').concat(
+      content_tag(:div, 'Failed', class: 'bg-danger text-light ps-2 pe-2 m-1').concat(
+        content_tag(:div, 'Building', class: 'bg-warning text-dark ps-2 pe-2 m-1').concat(
+          content_tag(:div, 'Excluded', class: 'bg-light text-dark border border-1 ps-2 pe-2 m-1')
+        )
+      )
+    )
+  end
+end
