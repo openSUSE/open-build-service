cbf2312fff1a9dbed0ef229d4b36aca2d14d9423
Jacob Michalskie <jacob.michalskie@suse.com>
2023-09-21 13:08:01 +0200

    Simplify the reporters iteration in the component preview

--- src/api/spec/components/previews/reports_notice_component_preview.rb
+++ src/api/spec/components/previews/reports_notice_component_preview.rb
@@ -2,8 +2,8 @@ class ReportsNoticeComponentPreview < ViewComponent::Preview
   # Preview at http://HOST:PORT/rails/view_components/reports_notice_component/user_reportable_as_admin
   def user_reportable_as_admin
     reportable = User.last
-    User.take(3).each do |reporter|
-      Report.new(reportable:, user: reporter).save!
+    User.take(3).each do |user|
+      Report.new(reportable:, user:).save!
     end
     render(ReportsNoticeComponent.new(reportable:, user: User.admins.first))
   end
@@ -12,8 +12,8 @@ class ReportsNoticeComponentPreview < ViewComponent::Preview
   def user_reportable_as_reporter
     reportable = User.last
     reporters = User.take(3)
-    reporters.each do |reporter|
-      Report.new(reportable:, user: reporter).save!
+    reporters.each do |user|
+      Report.new(reportable:, user:).save!
     end
     render(ReportsNoticeComponent.new(reportable:, user: reporters.last))
   end
