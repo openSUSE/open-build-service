ebff6965ad9e852e350d192178d2eea00420ea67
Jacob Michalskie <jacob.michalskie@suse.com>
2023-09-21 11:32:23 +0200

    Use instance variables for reports and reportable name

--- src/api/app/components/reports_notice_component.rb
+++ src/api/app/components/reports_notice_component.rb
@@ -1,19 +1,21 @@
 class ReportsNoticeComponent < ApplicationComponent
-  attr_reader :reportable, :user
+  attr_reader :reportable, :reportable_name, :user, :reports
 
   def initialize(reportable:, user:)
     super
 
     @reportable = reportable
+    @reportable_name = reportable.class.name.downcase
     @user = user
+    @reports = reportable&.reports
   end
 
   def by_user?
     Report.exists?(user:, reportable:)
   end
 
-  def reports
-    reportable&.reports
+  def report_amount
+    pluralize(reports.count, 'report')
   end
 
   def render?
