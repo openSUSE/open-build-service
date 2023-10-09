3b8888a952b08a9e5205e9db044bbb9eebe5c591
Dani Donisa <dani.doni@gmail.com>
2023-10-09 14:20:06 +0200

    Only run the data migration if the name column exist
    Due to https://github.com/openSUSE/open-build-service/pull/12434, Token
    does not have a name column anymore.

--- src/api/db/data/20211027143134_backfill_name_from_tokens.rb
+++ src/api/db/data/20211027143134_backfill_name_from_tokens.rb
@@ -4,6 +4,9 @@ class BackfillNameFromTokens < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def up
+    # If there is actually a 'name' column, update it
+    return unless Token.columns.any? { |c| c.name == 'name' }
+
     Token.unscoped.in_batches do |relation|
       # rubocop:disable Rails/SkipsModelValidations
       relation.update_all name: ''
