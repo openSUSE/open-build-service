bab4bd7715fd60504dd527293fb521622d5bc786
Lukas Krause <lkrause@suse.de>
2023-10-11 11:48:24 +0200

    Create STI report events on report creation

--- src/api/app/models/event/report_for_project.rb
+++ src/api/app/models/event/report_for_project.rb
@@ -1,5 +1,6 @@
 module Event
   class ReportForProject < Report
     self.description = 'Report for a project has been created'
+    payload_keys :project_name
   end
 end
--- src/api/app/models/event/report_for_user.rb
+++ src/api/app/models/event/report_for_user.rb
@@ -1,5 +1,6 @@
 module Event
   class ReportForUser < Report
     self.description = 'Report for a user has been created'
+    payload_keys :user_login
   end
 end
