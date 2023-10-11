db218042e71518f64a53eac2c7adee67ab0bbabe
Lukas Krause <lkrause@suse.de>
2023-10-11 14:03:12 +0200

    Adapt web notification links to use the STI report events

--- src/api/app/components/notification_notifiable_link_component.rb
+++ src/api/app/components/notification_notifiable_link_component.rb
@@ -94,9 +94,22 @@ class NotificationNotifiableLinkComponent < ApplicationComponent
     when 'Event::BuildFail'
       Rails.application.routes.url_helpers.package_live_build_log_path(package: @notification.event_payload['package'], project: @notification.event_payload['project'],
                                                                        repository: @notification.event_payload['repository'], arch: @notification.event_payload['arch'])
+    # TODO: Remove `Event::CreateReport` after all existing records are migrated to the new STI classes
     when 'Event::CreateReport'
       reportable = @notification.notifiable.reportable
       link_for_reportables(reportable)
+    when 'Event::ReportForComment'
+      path_to_commentables_on_reports(event_payload: @notification.event_payload, notification_id: @notification.id)
+    when 'Event::ReportForPackage'
+      Rails.application.routes.url_helpers.package_show_path(package: @notification.event_payload['package_name'],
+                                                             project: @notification.event_payload['project_name'],
+                                                             notification_id: @notification.id,
+                                                             anchor: 'comments-list')
+    when 'Event::ReportForProject'
+      Rails.application.routes.url_helpers.project_show_path(@notification.event_payload['project_name'],
+                                                             notification_id: @notification.id)
+    when 'Event::ReportForUser'
+      Rails.application.routes.url_helpers.user_path(@notification.event_payload['user_login'])
     when 'Event::ClearedDecision', 'Event::FavoredDecision'
       reportable = @notification.notifiable.reports.first.reportable
       link_for_reportables(reportable)
@@ -114,6 +127,9 @@ class NotificationNotifiableLinkComponent < ApplicationComponent
     end
   end
 
+  # TODO: Remove `Event::CreateReport` after all existing records are migrated to the new STI classes.
+  # This method is also used by 'Event::ClearedDecision' and 'Event::FavoredDecision', this need to
+  # be adapted
   def link_for_reportables(reportable)
     case @notification.event_payload['reportable_type']
     when 'Comment'
@@ -144,6 +160,26 @@ class NotificationNotifiableLinkComponent < ApplicationComponent
                                                              anchor: 'comments-list')
     when Project
       Rails.application.routes.url_helpers.project_show_path(commentable, notification_id: @notification.id, anchor: 'comments-list')
+    end
+  end
+
+  def path_to_commentables_on_reports(event_payload:, notification_id:)
+    case event_payload['commentable_type']
+    when 'BsRequest'
+      Rails.application.routes.url_helpers.request_show_path(event_payload['bs_request_number'],
+                                                             notification_id: notification_id, anchor: 'comments-list')
+    when 'BsRequestAction'
+      Rails.application.routes.url_helpers.request_show_path(number: event_payload['bs_request_number'],
+                                                             request_action_id: event_payload['bs_request_action_id'],
+                                                             notification_id: notification_id, anchor: 'tab-pane-changes')
+    when 'Package'
+      Rails.application.routes.url_helpers.package_show_path(package: event_payload['package_name'],
+                                                             project: event_payload['project_name'],
+                                                             notification_id: notification_id,
+                                                             anchor: 'comments-list')
+    when 'Project'
+      Rails.application.routes.url_helpers.project_show_path(event_payload['project_name'], notification_id: notification_id,
+                                                                                            anchor: 'comments-list')
     end
   end
 end
