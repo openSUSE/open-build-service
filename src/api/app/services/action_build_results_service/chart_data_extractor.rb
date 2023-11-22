e386506ca8feb419f7a0b669eaaeff1720f92b5c
ncounter <dleidi@suse.com>
2023-11-22 11:21:57 +0100

    Do not use shared variable, return values instead

--- src/api/app/services/action_build_results_service/chart_data_extractor.rb
+++ src/api/app/services/action_build_results_service/chart_data_extractor.rb
@@ -1,39 +1,35 @@
 module ActionBuildResultsService
   class ChartDataExtractor
-    attr_accessor :actions, :raw_data
+    attr_accessor :actions
 
     def initialize(actions:)
       @actions = actions
-      @raw_data = []
     end
 
     def call
       @actions.where(type: [:submit, :maintenance_incident, :maintenance_release])
               .map { |action| sources_from_action(action) }
               .select { |sources| sources[:source_project].present? && sources[:source_package].present? }
-              .each do |sources|
-        package_build_results = package_build_results(sources[:source_package], sources[:source_project])
+              .map do |sources|
+        build_results = package_build_results(sources[:source_package], sources[:source_project])
 
-        add_request_build_results(package_build_results, sources[:source_package], sources[:source_project])
+        request_build_results(build_results, sources[:source_package], sources[:source_project])
       end
-
-      @raw_data.flatten
+      .flatten
     end
 
     private
 
-    def add_request_build_results(package_build_results, source_package, source_project)
-      @raw_data.push(
-        package_build_results.map do |result_entry|
-          {
-            architecture: result_entry.architecture,
-            repository: result_entry.repository,
-            status: result_entry.code,
-            package_name: source_package.name,
-            project_name: source_project.name
-          }
-        end
-      )
+    def request_build_results(build_results, source_package, source_project)
+      build_results.map do |result_entry|
+        {
+          architecture: result_entry.architecture,
+          repository: result_entry.repository,
+          status: result_entry.code,
+          package_name: source_package.name,
+          project_name: source_project.name
+        }
+      end
     end
 
     def project_from_action(action)
