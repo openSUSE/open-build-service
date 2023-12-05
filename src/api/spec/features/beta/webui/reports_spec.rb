06de805ccd99ba1ff7e34dc642735ad5d7d02154
Lukas Krause <lkrause@suse.de>
2023-12-05 17:13:19 +0100

    Extend reports feature spec by more user roles
    Test that user cannot report themself or the comments, project and
    packages they own.

--- src/api/spec/features/beta/webui/reports_spec.rb
+++ src/api/spec/features/beta/webui/reports_spec.rb
@@ -13,105 +13,206 @@ RSpec.describe 'Reports', :js, :vcr do
     end
   end
 
-  describe 'after reporting a comment on a project' do
-    let(:user) { create(:confirmed_user, login: 'jane_doe') }
+  describe 'reporting a comment on a project' do
     let(:project) { create(:project, name: 'some_random_project') }
-    let!(:comment) { create(:comment, commentable: project) }
 
-    before do
-      login user
-      visit project_show_path(project)
+    context 'for a user who is not the author' do
+      let(:user) { create(:confirmed_user, login: 'jane_doe') }
+      let!(:comment) { create(:comment, commentable: project) }
+
+      before do
+        login user
+        visit project_show_path(project)
+      end
+
+      it 'displays the "You reported this comment." message instantly' do
+        click_link('Report', id: "js-comment-#{comment.id}")
+        fill_and_submit_report_form
+        expect(page).to have_text('You reported this comment.')
+      end
     end
 
-    it 'displays the "You reported this comment." message instantly' do
-      click_link('Report', id: "js-comment-#{comment.id}")
-      fill_and_submit_report_form
-      expect(page).to have_text('You reported this comment.')
+    context 'for a user who is the author' do
+      let(:author) { create(:confirmed_user, login: 'foo') }
+      let!(:comment) { create(:comment, commentable: project, user: author) }
+
+      before do
+        login author
+        visit project_show_path(project)
+      end
+
+      it 'does not display the report link' do
+        within('div#comments') { expect(page).not_to have_link('Report') }
+      end
     end
   end
 
-  describe 'after reporting a comment on a package' do
-    let(:user) { create(:confirmed_user, login: 'jane_doe') }
+  describe 'reporting a comment on a package' do
     let(:project) { create(:project, name: 'some_random_project') }
     let(:package) { create(:package, project: project, name: 'some_random_package') }
-    let!(:comment) { create(:comment, commentable: package) }
 
-    before do
-      login user
-      visit package_show_path(project, package)
+    context 'for a user who is not the author' do
+      let(:user) { create(:confirmed_user, login: 'jane_doe') }
+      let!(:comment) { create(:comment, commentable: package) }
+
+      before do
+        login user
+        visit package_show_path(project, package)
+      end
+
+      it 'displays the "You reported this comment." message instantly' do
+        click_link('Report', id: "js-comment-#{comment.id}")
+        fill_and_submit_report_form
+        expect(page).to have_text('You reported this comment.')
+      end
     end
 
-    it 'displays the "You reported this comment." message instantly' do
-      click_link('Report', id: "js-comment-#{comment.id}")
-      fill_and_submit_report_form
-      expect(page).to have_text('You reported this comment.')
+    context 'for a user who is the author' do
+      let(:author) { create(:confirmed_user, login: 'foo') }
+      let!(:comment) { create(:comment, commentable: package, user: author) }
+
+      before do
+        login author
+        visit package_show_path(project, package)
+      end
+
+      it 'does not display the report link' do
+        within('div#comments') { expect(page).not_to have_link('Report') }
+      end
     end
   end
 
-  describe 'after reporting a comment on a request' do
+  describe 'reporting a comment on a request' do
     let(:user) { create(:confirmed_user, login: 'jane_doe') }
     let(:project) { create(:project, name: 'some_random_project') }
     let(:bs_request) { create(:delete_bs_request, target_project: project, description: 'Delete this project!', creator: user) }
-    let!(:comment) { create(:comment, commentable: bs_request) }
 
-    before do
-      login user
-      visit request_show_path(bs_request)
+    context 'for a user who is not the author' do
+      let!(:comment) { create(:comment, commentable: bs_request) }
+
+      before do
+        login user
+        visit request_show_path(bs_request)
+      end
+
+      it 'displays the "You reported this comment." message instantly' do
+        click_link('Report', id: "js-comment-#{comment.id}")
+        fill_and_submit_report_form
+        expect(page).to have_text('You reported this comment.')
+      end
     end
 
-    it 'displays the "You reported this comment." message instantly' do
-      click_link('Report', id: "js-comment-#{comment.id}")
-      fill_and_submit_report_form
-      expect(page).to have_text('You reported this comment.')
+    context 'for a user who is the author' do
+      let(:author) { create(:confirmed_user, login: 'foo') }
+      let!(:comment) { create(:comment, commentable: bs_request, user: author) }
+
+      before do
+        login author
+        visit request_show_path(bs_request)
+      end
+
+      it 'does not display the report link' do
+        within('div.comments-thread') { expect(page).not_to have_link('Report') }
+      end
     end
   end
 
-  describe 'after reporting a project' do
-    let(:user) { create(:confirmed_user, login: 'jane_doe') }
-    let(:project) { create(:project, name: 'some_random_project') }
+  describe 'reporting a project' do
+    context 'for a user who is not a maintainer' do
+      let(:user) { create(:confirmed_user, login: 'jane_doe') }
+      let(:project) { create(:project, name: 'some_random_project') }
 
-    before do
-      login user
-      visit project_show_path(project)
+      before do
+        login user
+        visit project_show_path(project)
+      end
+
+      it 'displays the "You reported this project." message instantly' do
+        desktop? ? click_link('Report Project') : click_menu_link('Actions', 'Report Project')
+        fill_and_submit_report_form
+        expect(page).to have_text('You reported this project.')
+      end
     end
 
-    it 'displays the "You reported this project." message instantly' do
-      desktop? ? click_link('Report Project') : click_menu_link('Actions', 'Report Project')
-      fill_and_submit_report_form
-      expect(page).to have_text('You reported this project.')
+    context 'for a user who is a maintainer' do
+      let(:maintainer) { create(:confirmed_user, login: 'foo') }
+      let(:project) { create(:project, name: 'some_random_project', maintainer: maintainer) }
+
+      before do
+        login maintainer
+        visit project_show_path(project)
+      end
+
+      it 'does not display the report link' do
+        click_link('Actions') if mobile?
+        expect(page).not_to have_link('Report Project')
+      end
     end
   end
 
-  describe 'after reporting a package' do
-    let(:user) { create(:confirmed_user, login: 'jane_doe') }
+  describe 'reporting a package' do
     let(:project) { create(:project, name: 'some_random_project') }
     let(:package) { create(:package, project: project, name: 'some_random_package') }
 
-    before do
-      login user
-      visit package_show_path(project, package)
+    context 'for a user who is not a maintainer' do
+      let(:user) { create(:confirmed_user, login: 'jane_doe') }
+      let(:package) { create(:package, project: project, name: 'some_random_package') }
+
+      before do
+        login user
+        visit package_show_path(project, package)
+      end
+
+      it 'displays the "You reported this package." message instantly' do
+        desktop? ? click_link('Report Package') : click_menu_link('Actions', 'Report Package')
+        fill_and_submit_report_form
+        expect(page).to have_text('You reported this package.')
+      end
     end
 
-    it 'displays the "You reported this package." message instantly' do
-      desktop? ? click_link('Report Package') : click_menu_link('Actions', 'Report Package')
-      fill_and_submit_report_form
-      expect(page).to have_text('You reported this package.')
+    context 'for a user who is maintainer' do
+      let(:maintainer) { create(:confirmed_user, login: 'foo') }
+      let(:package) { create(:package_with_maintainer, project: project, name: 'some_random_package', maintainer: maintainer) }
+
+      before do
+        login maintainer
+        visit package_show_path(project, package)
+      end
+
+      it 'does not display the report link' do
+        click_link('Actions') if mobile?
+        expect(page).not_to have_link('Report Package')
+      end
     end
   end
 
-  describe 'after reporting a user' do
+  describe 'reporting a user' do
     let(:user) { create(:confirmed_user, login: 'jane_doe') }
-    let(:another_user) { create(:confirmed_user) }
 
-    before do
-      login user
-      visit user_path(another_user)
+    context 'for a user visiting another users page' do
+      let(:another_user) { create(:confirmed_user) }
+
+      before do
+        login user
+        visit user_path(another_user)
+      end
+
+      it 'displays the "You reported this user." message instantly' do
+        click_link('Report', id: "js-user-#{another_user.id}")
+        fill_and_submit_report_form
+        expect(page).to have_text('You reported this user.')
+      end
     end
 
-    it 'displays the "You reported this user." message instantly' do
-      click_link('Report', id: "js-user-#{another_user.id}")
-      fill_and_submit_report_form
-      expect(page).to have_text('You reported this user.')
+    context 'for a user visiting their own users page' do
+      before do
+        login user
+        visit user_path(user)
+      end
+
+      it 'does not display the report link' do
+        within('div.basic-info') { expect(page).not_to have_link('Report') }
+      end
     end
   end
 end
