b499256b631f8d178a3afcbe5957602e641b61e6
Henne Vogelsang <hvogel@opensuse.org>
2023-12-06 14:27:31 +0100

    Simplify proxy mode methods

--- src/api/app/components/sign_up_component.rb
+++ src/api/app/components/sign_up_component.rb
@@ -3,18 +3,17 @@
 class SignUpComponent < ApplicationComponent
   attr_accessor :submit_btn_text
 
-  def initialize(submit_btn_text: 'Sign Up', create_page: false, config: CONFIG)
+  def initialize(submit_btn_text: 'Sign Up', create_page: false)
     super
 
-    @config = config
     @submit_btn_text = sanitize(create_page ? 'Create' : submit_btn_text)
   end
 
-  def proxy_auth_enabled?
-    @config['proxy_auth_mode'] == :on
+  def proxy_auth_mode_enabled?
+    ::Configuration.proxy_auth_mode_enabled?
   end
 
   def proxy_auth_register_page
-    @config['proxy_auth_register_page']
+    CONFIG['proxy_auth_register_page']
   end
 end
