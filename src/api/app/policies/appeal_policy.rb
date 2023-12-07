19ba4eaa570a968b4bb117575f42acf8dc9dc95e
Lukas Krause <lkrause@suse.de>
2023-12-07 20:26:27 +0100

    Create pundit policies for appeals

---
+++ src/api/app/policies/appeal_policy.rb
@@ -0,0 +1,40 @@
+class AppealPolicy < ApplicationPolicy
+  def new?
+    create?
+  end
+
+  def show?
+    return false unless Flipper.enabled?(:content_moderation, user)
+    return true if record.appellant == user
+
+    user.is_admin? || user.is_moderator? || user.is_staff?
+  end
+
+  # A user can create an appeal for a decision which is against them
+  # A decision is against them if:
+  # 1. They reported a project/package/etc, but a moderator took a decision which cleared the report
+  #   OR
+  # 2. They did something was reported (a spam comment, etc...) and a moderator took a decision which favored the report
+  def create?
+    return false unless Flipper.enabled?(:content_moderation, user)
+
+    decision_cleared_report_from_user? || decision_favored_report_of_action_from_user? ||
+      user.is_admin? || user.is_staff?
+  end
+
+  private
+
+  def decision_cleared_report_from_user?
+    return false unless record.appellant == user
+
+    record.decision.kind == 'cleared' && record.decision.reports.pluck(:user_id).include?(user.id)
+  end
+
+  def decision_favored_report_of_action_from_user?
+    return false unless record.appellant == user
+
+    report = record.decision.reports.first
+
+    record.decision.kind == 'favor' && "#{report.reportable_type}Policy".constantize.new(user, report.reportable).update?
+  end
+end
