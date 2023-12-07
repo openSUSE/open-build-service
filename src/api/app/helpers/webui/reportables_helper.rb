4627ee1dc207b67cf33b6cd79e059e1c50faab66
Eduardo Navarro <enavarro@suse.com>
2023-12-07 16:27:11 +0100

    Fix absolute URL's in link_to_reportables helper
    Use `*_url` helpers instead of `*_path` helpers. `*_path` helpers don't
    recognize the `host` parameter to make URL's absolute.

--- src/api/app/helpers/webui/reportables_helper.rb
+++ src/api/app/helpers/webui/reportables_helper.rb
@@ -8,28 +8,28 @@ module Webui::ReportablesHelper
     when 'Comment'
       link_to_commentables_on_reportables(commentable: reportable.commentable, host: host)
     when 'Package'
-      link_to("#{reportable.name}", Rails.application.routes.url_helpers.package_show_path(package: reportable, project: reportable.project,
-                                                                                           anchor: 'comments-list', only_path: false, host: host))
+      link_to("#{reportable.name}", Rails.application.routes.url_helpers.package_show_url(package: reportable, project: reportable.project,
+                                                                                          anchor: 'comments-list', only_path: false, host: host))
     when 'Project'
-      link_to("#{reportable.name}", Rails.application.routes.url_helpers.project_show_path(reportable, anchor: 'comments-list', only_path: false, host: host))
+      link_to("#{reportable.name}", Rails.application.routes.url_helpers.project_show_url(reportable, anchor: 'comments-list', only_path: false, host: host))
     when 'User'
-      link_to("#{reportable.login}", Rails.application.routes.url_helpers.user_path(reportable, only_path: false, host: host))
+      link_to("#{reportable.login}", Rails.application.routes.url_helpers.user_url(reportable, only_path: false, host: host))
     end
   end
 
   def link_to_commentables_on_reportables(commentable:, host:)
     case commentable
     when BsRequest
-      link_to("Request #{commentable.number}", Rails.application.routes.url_helpers.request_show_path(commentable.number, anchor: 'comments-list', only_path: false, host: host))
+      link_to("Request #{commentable.number}", Rails.application.routes.url_helpers.request_show_url(commentable.number, anchor: 'comments-list', only_path: false, host: host))
     when BsRequestAction
-      link_to("Request #{commentable.bs_request.number}", Rails.application.routes.url_helpers.request_show_path(number: commentable.bs_request.number,
-                                                                                                                 request_action_id: commentable.id,
-                                                                                                                 anchor: 'tab-pane-changes', only_path: false, host: host))
+      link_to("Request #{commentable.bs_request.number}", Rails.application.routes.url_helpers.request_show_url(number: commentable.bs_request.number,
+                                                                                                                request_action_id: commentable.id,
+                                                                                                                anchor: 'tab-pane-changes', only_path: false, host: host))
     when Package
-      link_to("#{commentable.name}", Rails.application.routes.url_helpers.package_show_path(package: commentable, project: commentable.project,
-                                                                                            anchor: 'comments-list', only_path: false, host: host))
+      link_to("#{commentable.name}", Rails.application.routes.url_helpers.package_show_url(package: commentable, project: commentable.project,
+                                                                                           anchor: 'comments-list', only_path: false, host: host))
     when Project
-      link_to("#{commentable.name}", Rails.application.routes.url_helpers.project_show_path(commentable, anchor: 'comments-list', only_path: false, host: host))
+      link_to("#{commentable.name}", Rails.application.routes.url_helpers.project_show_url(commentable, anchor: 'comments-list', only_path: false, host: host))
     end
   end
 end
