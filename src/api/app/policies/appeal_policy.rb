d3ec0bfdca8db188bcb41d4011f90ae62a355909
Dany Marcoux <git@dmarcoux.com>
2023-12-13 11:43:04 +0100

    Prevent appealing a decision for reports with a now-deleted reportable
    Fixes #15358

--- src/api/app/policies/appeal_policy.rb
+++ src/api/app/policies/appeal_policy.rb
@@ -18,6 +18,10 @@ class AppealPolicy < ApplicationPolicy
   def create?
     return false unless Flipper.enabled?(:content_moderation, user)
 
+    # Prevent appealing a decision for reports with a deleted reportable, since this wouldn't have any effect anyway.
+    @report = record.decision.reports.first
+    return false if @report.reportable_type.nil?
+
     decision_cleared_report_from_user? || decision_favored_report_of_action_from_user? ||
       user.is_admin? || user.is_staff?
   end
@@ -33,8 +37,6 @@ class AppealPolicy < ApplicationPolicy
   def decision_favored_report_of_action_from_user?
     return false unless record.appellant == user
 
-    report = record.decision.reports.first
-
-    record.decision.kind == 'favor' && "#{report.reportable_type}Policy".constantize.new(user, report.reportable).update?
+    record.decision.kind == 'favor' && "#{@report.reportable_type}Policy".constantize.new(user, @report.reportable).update?
   end
 end
