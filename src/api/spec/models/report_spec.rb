5988d2ea63a41fe3542dc32adcfedb6fea133d70
Rubhan Azeem <rubhanazeem@gmail.com>
2023-12-15 15:52:23 +0100

    Adding validation for unique reportables
    Stop creating reports if decision has more reports pointig towards
    different reportables

---
+++ src/api/spec/models/report_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Report do
+  describe '#reports_pointing_to_same_reportable' do
+    context 'when different reportables' do
+      let(:decision) { create(:decision) }
+      let(:report) { build(:report, decision: decision) }
+
+      before do
+        report.save
+      end
+
+      it { expect(report.valid?).to be(false) }
+      it { expect(report.errors.full_messages).to eq(['Decision has reports pointing to a different reportable. All decision reports should point to same reportable.']) }
+    end
+
+    context 'when reports are pointing to same trportable' do
+      let(:report) { create(:report) }
+      let(:decision) { create(:decision, reports: [report]) }
+      let(:new_report) { build(:report, reportable: report.reportable, decision: decision) }
+
+      it { expect(new_report.valid?).to be(true) }
+    end
+  end
+end
