b634057ccfebfe5464072428a605191a2d0361b8
Eduardo Navarro <enavarro@suse.com>
2023-12-20 11:43:04 +0100

    Don't ignore notifying ActionView::MissingTemplate
    When we return an ActionView::MissingTemplate exception, we developers
    want to be notified. This way we can fix the exception.

--- src/api/config/initializers/airbrake.rb
+++ src/api/config/initializers/airbrake.rb
@@ -102,7 +102,7 @@ end
 def ignore_by_class?(notice)
   exceptions_to_ignore = ['ActiveRecord::RecordNotFound', 'ActionController::InvalidAuthenticityToken',
                           'CGI::Session::CookieStore::TamperedWithCookie', 'ActionController::UnknownAction',
-                          'AbstractController::ActionNotFound', 'ActionView::MissingTemplate', 'Bunny::TCPConnectionFailedForAllHosts',
+                          'AbstractController::ActionNotFound', 'Bunny::TCPConnectionFailedForAllHosts',
                           'Timeout::Error', 'Net::HTTPBadResponse', 'Errno::ECONNRESET', 'Interrupt',
                           'RoutesHelper::WebuiMatcher::InvalidRequestFormat', 'ActionDispatch::Http::MimeNegotiation::InvalidType',
                           'ActionController::UnknownFormat', 'Backend::NotFoundError', 'AMQ::Protocol::EmptyResponseError']
