e30ce1680715e08a2c3bccbc1cffcf870ac23835
Saray Cabrera Padrón <scabrerapadron@suse.de>
2023-12-22 14:29:41 +0100

    Revert "Revert "Introduces triggering Project operations""
    This reverts commit 5c5e5f180661d127b3641e0e33d84925ca1810c1.

--- src/api/app/models/token/release.rb
+++ src/api/app/models/token/release.rb
@@ -34,10 +34,14 @@ class Token::Release < Token
     opts[:multibuild_container] = options[:multibuild_flavor] if options[:multibuild_flavor].present?
     opts[:filter_architecture] = options[:arch] if options[:arch].present?
 
-    release_package(package_to_release,
-                    target_repository,
-                    package_to_release.release_target_name(target_repository, time_now),
-                    opts)
+    if package_to_release.present?
+      release_package(package_to_release,
+                      target_repository,
+                      package_to_release.release_target_name(target_repository, time_now),
+                      opts)
+    else
+      @project.do_project_release(opts)
+    end
   end
 
   def release_manually_triggered_targets(package_to_release, time_now, options)
