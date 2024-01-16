a28f3067ed0b47a49ae956c5d0edebcdef8d554c
Dany Marcoux <git@dmarcoux.com>
2024-01-16 09:35:14 +0100

    Increase code coverage for PatchinfoComponent

--- src/api/spec/components/patchinfo_component_spec.rb
+++ src/api/spec/components/patchinfo_component_spec.rb
@@ -13,11 +13,38 @@ RSpec.describe PatchinfoComponent, type: :component do
         <package>package_a</package>
         <issue tracker="cve" id="2023-1111"/>
         <releasetarget project="openSUSE:Factory" repository="15.4" />
+        <retracted>1</retracted>
+        <stopped>1</stopped>
+        <reboot_needed>1</reboot_needed>
+        <relogin_needed></relogin_needed>
+        <zypp_restart_needed/>
       </patchinfo>'
     end
 
     before do
       render_inline(described_class.new(patchinfo_text, 'path-to-request-changes'))
+    end
+
+    it 'displays a badge for the category' do
+      expect(rendered_content).to have_css('span.badge.text-bg-info', text: 'Recommended')
+    end
+
+    it 'displays a badge for the rating' do
+      expect(rendered_content).to have_css('span.badge.text-bg-secondary', text: 'Low priority')
+    end
+
+    it 'displays a badge for the retracted element when it is present' do
+      expect(rendered_content).to have_css('span.badge.text-bg-danger', text: 'Retracted')
+    end
+
+    it 'displays a badge for the stopped element when it is present' do
+      expect(rendered_content).to have_css('span.badge.text-bg-danger', text: 'Stopped')
+    end
+
+    it 'displays a badge for properties' do
+      ['reboot_needed', 'relogin_needed', 'zypp_restart_needed'].each do |property|
+        expect(rendered_content).to have_css('span.badge.text-bg-info', text: property)
+      end
     end
 
     it do
@@ -56,8 +83,8 @@ RSpec.describe PatchinfoComponent, type: :component do
   context 'with multiple tags per type' do
     let(:patchinfo_text) do
       '<patchinfo>
-        <category>recommended</category>
-        <rating>low</rating>
+        <category>feature</category>
+        <rating>critical</rating>
         <packager>tom</packager>
         <summary/>
         <description/>
@@ -74,6 +101,14 @@ RSpec.describe PatchinfoComponent, type: :component do
 
     before do
       render_inline(described_class.new(patchinfo_text, 'path-to-request-changes'))
+    end
+
+    it 'displays a badge for the category' do
+      expect(rendered_content).to have_css('span.badge.text-bg-success', text: 'Feature')
+    end
+
+    it 'displays a badge for the rating' do
+      expect(rendered_content).to have_css('span.badge.text-bg-danger', text: 'Critical priority')
     end
 
     it do
@@ -126,6 +161,20 @@ RSpec.describe PatchinfoComponent, type: :component do
 
     before do
       render_inline(described_class.new(patchinfo_text, 'path-to-request-changes'))
+    end
+
+    it 'does not display a badge for the retracted element when it is missing' do
+      expect(rendered_content).to have_no_css('span.badge.text-bg-danger', text: 'Retracted')
+    end
+
+    it 'does not display a badge for the stopped element when it is missing' do
+      expect(rendered_content).to have_no_css('span.badge.text-bg-danger', text: 'Stopped')
+    end
+
+    it 'does not display a badge for properties when they are missing' do
+      ['reboot_needed', 'relogin_needed', 'zypp_restart_needed'].each do |property|
+        expect(rendered_content).to have_no_css('span.badge.text-bg-info', text: property)
+      end
     end
 
     it do
