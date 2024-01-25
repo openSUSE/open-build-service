e89637f8f8f9c8d4544356a175d943994ef00f54
ncounter <dleidi@suse.com>
2024-01-25 14:35:11 +0100

    Bugfix: do not create link over a non-existing object

--- src/api/app/helpers/webui/reportables_helper.rb
+++ src/api/app/helpers/webui/reportables_helper.rb
@@ -4,6 +4,8 @@ module Webui::ReportablesHelper
 
   def link_to_reportables(report_id:, reportable_type:, host:)
     reportable = Report.find(report_id).reportable
+    return if reportable.blank?
+
     case reportable_type
     when 'Comment'
       link_to_commentables_on_reportables(commentable: reportable.commentable, host: host)
