d4b1f403ff63d43484363840fad67a1babef9a24
Rubhan Azeem <rubhanazeem@gmail.com>
2024-01-29 13:48:58 +0100

    Show which commentable is locked in the locked alert

---
+++ src/api/app/helpers/webui/comment_locks_helper.rb
@@ -0,0 +1,14 @@
+module Webui::CommentLocksHelper
+  def comment_lock_alert(commentable)
+    alert = 'You can remove the lock by clicking on the button below.'
+    return alert if commentable.comment_lock
+
+    if commentable.is_a?(Package) && commentable.project.comment_lock
+      text = 'You can remove the lock by visiting'
+      link = link_to(commentable.project.name, project_show_path(commentable.project))
+      safe_join([text, ' ', content_tag(:a, link)])
+    else
+      alert
+    end
+  end
+end
