82ba3450964af14de1860ef52a31d7a3d9944bb0
ncounter <dleidi@suse.com>
2024-02-06 10:26:21 +0100

    Strong migration wrapped by safety_assured catch

--- src/api/db/migrate/20230224114624_add_diff_ref_to_comments.rb
+++ src/api/db/migrate/20230224114624_add_diff_ref_to_comments.rb
@@ -1,5 +1,13 @@
 class AddDiffRefToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_column :comments, :diff_ref, :string
+  def up
+    safety_assured do
+      add_column :comments, :diff_ref, :string
+    end
+  end
+
+  def down
+    safety_assured do
+      remove_column :comments, :diff_ref
+    end
   end
 end
