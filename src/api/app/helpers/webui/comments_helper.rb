a7c57af8fa5e35a4aa334bbf9b237bb38917dd23
ncounter <dleidi@suse.com>
2024-03-03 23:07:03 +0100

    Move relationship queries to a proper place

--- src/api/app/helpers/webui/comments_helper.rb
+++ src/api/app/helpers/webui/comments_helper.rb
@@ -32,39 +32,23 @@ module Webui::CommentsHelper
   def roles_for_request(comment)
     roles = []
     roles << 'author' if comment.commentable.creator == comment.user.login
-    roles << 'reviewer' if comment.commentable.reviews.pluck(:user_id).include?(comment.user.id) ||
-                           comment.commentable.reviews.pluck(:group_id).intersect?(comment.user.groups.ids)
-    source_roles = []
-    target_roles = []
-    comment.commentable.bs_request_actions.each do |action|
-      source_roles += Relationship.joins(:role).joins(:project)
-                                  .where(projects: { name: action.source_project })
-                                  .where(user: comment.user)
-                                  .where('roles.title': 'maintainer')
-                                  .pluck('roles.title')
-                                  .union(Relationship.joins(:role).joins(:package).joins('LEFT JOIN projects ON projects.id = packages.project_id')
-                                                     .where(packages: { name: action.source_package })
-                                                     .where(projects: { name: action.source_project })
-                                                     .where(user: comment.user)
-                                                     .where('roles.title': 'maintainer')
-                                                     .pluck('roles.title'))
-
-      target_roles += Relationship.joins(:role).joins(:project)
-                                  .where(projects: { name: action.target_project })
-                                  .where(user: comment.user)
-                                  .where('roles.title': 'maintainer')
-                                  .pluck('roles.title')
-                                  .union(Relationship.joins(:role).joins(:package).joins('LEFT JOIN projects ON projects.id = packages.project_id')
-                                                     .where(packages: { name: action.target_package })
-                                                     .where(projects: { name: action.target_project })
-                                                     .where(user: comment.user)
-                                                     .where('roles.title': 'maintainer')
-                                                     .pluck('roles.title'))
-    end
-
-    roles << 'source maintainer' if source_roles.any?
-    roles << 'target maintainer' if target_roles.any?
-
+    roles << 'reviewer' if review_assigned_to_user(comment)
+    roles << 'source maintainer' if comment.commentable.bs_request_actions.any? { |action| source_maintainer(action, comment.user) }
+    roles << 'target maintainer' if comment.commentable.bs_request_actions.any? { |action| target_maintainer(action, comment.user) }
     roles
+  end
+
+  def review_assigned_to_user(comment)
+    comment.commentable.reviews.map(&:user).include?(comment.user) || comment.commentable.reviews.pluck(:group_id).intersect?(comment.user.groups.ids)
+  end
+
+  def source_maintainer(action, user)
+    RelationshipsFinder.new.user_has_role_for_project(action.source_project, user, 'maintainer') ||
+      RelationshipsFinder.new.user_has_role_for_package(action.source_package, action.source_project, user, 'maintainer')
+  end
+
+  def target_maintainer(action, user)
+    RelationshipsFinder.new.user_has_role_for_project(action.target_project, user, 'maintainer') ||
+      RelationshipsFinder.new.user_has_role_for_package(action.target_package, action.target_project, user, 'maintainer')
   end
 end
