a7b07480bfca90fddffa0c57a263fd0d726c8210
Saray Cabrera Padrón <scabrerapadron@suse.de>
2024-03-04 12:41:27 +0100

    Add CannedResponsesDropdownComponent spec

--- src/api/spec/factories/canned_responses.rb
+++ src/api/spec/factories/canned_responses.rb
@@ -1,3 +1,16 @@
 FactoryBot.define do
-  factory :canned_response
+  factory :canned_response do
+    title { Faker::Lorem.word }
+    content { Faker::Lorem.paragraph }
+    user { association :confirmed_user }
+
+    factory :cleared_canned_response do
+      decision_kind { 'cleared' }
+      user { association :moderator }
+    end
+    factory :favor_canned_response do
+      decision_kind { 'favor' }
+      user { association :moderator }
+    end
+  end
 end
