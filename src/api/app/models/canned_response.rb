42820c3d7819fdbcb9d758603250e5c5d597ace2
Saray Cabrera Padrón <scabrerapadron@suse.de>
2024-03-05 10:40:56 +0100

    Adjust validations of canned responses' attributes
    The `title` and `content` should be present, it makes little sense to have nothing
    to show in the dropdown and nothing to add to the comment.

--- src/api/app/models/canned_response.rb
+++ src/api/app/models/canned_response.rb
@@ -6,8 +6,8 @@ class CannedResponse < ApplicationRecord
   #### Constants
 
   #### Self config
-  validates :title, length: { maximum: 255 }
-  validates :content, length: { maximum: 65_535 }
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :content, presence: true, length: { maximum: 65_535 }
 
   enum decision_kind: {
     cleared: 0,
