9ef496c61c10c81d1a80b6337b486292d83eabfc
Dani Donisa <dani.doni@gmail.com>
2024-03-11 16:47:14 +0100

    Make login with Github work

--- src/api/config/initializers/omniauth.rb
+++ src/api/config/initializers/omniauth.rb
@@ -3,5 +3,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider :developer,
              :fields => [:username, :password],
              :uid_field => :username
+    provider :github,
+             ENV['GITHUB_KEY'],
+             ENV['GITHUB_SECRET'],
+             {:provider_ignores_state => true}
   end
 end
