b543f9f8e5448134800c1accd83f58f42c0773f5
Hugo Ferreira <hugomsf89@gmail.com>
2024-03-17 16:28:07 +0000

    remove unnecessary cache

--- src/api/app/controllers/webui/worker_capabilities_controller.rb
+++ src/api/app/controllers/webui/worker_capabilities_controller.rb
@@ -2,14 +2,10 @@
 
 class Webui::WorkerCapabilitiesController < Webui::WebuiController
   def show
-    cache_key = "worker_capabilities_#{params[:arch]}_#{params[:id]}"
-
-    xml_data = Rails.cache.fetch(cache_key, expires_in: 30.minutes) do
-      Backend::Api::BuildResults::Worker.capabilities(params[:arch], params[:id])
-    end || ''
-
+    xml_data = Backend::Api::BuildResults::Worker.capabilities(params[:arch], params[:id])
     capabilities = Xmlhash.parse(xml_data)
-    @num_of_processors = capabilities&.dig('hardware', 'processors')
-    @num_of_jobs = capabilities&.dig('hardware', 'jobs')
+
+    @num_of_processors = capabilities.dig('hardware', 'processors')
+    @num_of_jobs = capabilities.dig('hardware', 'jobs')
   end
 end
