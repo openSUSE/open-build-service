e4cefcb62bb4281804fd4abaee68afa3af2d0fe2
ncounter <dleidi@suse.com>
2024-03-21 17:29:32 +0100

    Revert "Simplify error message and HTML generation"
    This reverts commit c61d078041fd3b59917c222fc6c30a6015a0144e.

--- src/api/app/assets/javascripts/webui/flash.js
+++ src/api/app/assets/javascripts/webui/flash.js
@@ -6,22 +6,52 @@ $(window).scroll(function() {
   }
 });
 
+// function to toggle visibility to the additional flash message information
+function toggleMoreInfo(element, collapsibleContent) {
+  collapsibleContent.classList.toggle("d-none");
+  element.textContent = collapsibleContent.classList.contains('d-none') ? 'more info' : 'less info';
+}
+
 // Create a flash error message on the fly receiving
 // a generic error message and the detailed response of an ajax call
-function generateFlashError(message) { // jshint ignore:line
+function generateFlashError(xhdr, message) {
   var row = document.createElement("div");
   row.className = "row";
 
-  row.innerHTML =
-    `
-      <div class='col-12'>
-        <div class='alert alert-dismissible fade show alert-error'>
-          ${message}
-          <i class='fas'></i>
-          <button class='btn btn-close float-end' type='button' data-bs-dismiss='alert' aria-label='Close' />
-        </div>
-      </div>
-    `;
+  var col = document.createElement("div");
+  col.className = "col-12";
+  row.appendChild(col);
+
+  var alert = document.createElement("div");
+  alert.className = "alert alert-dismissible fade show alert-error";
+  alert.textContent = message;
+  col.appendChild(alert);
+
+  var icon = document.createElement("i");
+  icon.className = "fas";
+  alert.appendChild(icon);
+
+  var moreInfoLink = document.createElement("button");
+  moreInfoLink.className = "btn btn-link alert-link";
+  moreInfoLink.textContent = "more info";
+  alert.appendChild(moreInfoLink);
+
+  var moreInfo = document.createElement("div");
+  moreInfo.className = "moreInfo d-none";
+  moreInfoLink.onclick = () => toggleMoreInfo(moreInfoLink, moreInfo);
+  alert.appendChild(moreInfo);
+
+  var moreInfoContent = document.createElement("div");
+  moreInfoContent.className = "more-info-content";
+  moreInfoContent.textContent = xhdr.responseText;
+  moreInfo.appendChild(moreInfoContent);
+
+  var closeButton = document.createElement("button");
+  closeButton.className = "btn btn-close float-end";
+  closeButton.setAttribute("type", "button");
+  closeButton.setAttribute("data-bs-dismiss", "alert");
+  closeButton.setAttribute("aria-label", "Close");
+  alert.appendChild(closeButton);
 
   return row;
 }
--- src/api/app/assets/javascripts/webui/network.js
+++ src/api/app/assets/javascripts/webui/network.js
@@ -1,3 +1,3 @@
 $(document).on( "ajaxError", function(event, xhdr, settings, thrownError) {
-  $('#flash').show().append(generateFlashError(`An issue occurred while loading '${settings.url}': '${thrownError}'. Please try again or reload the page.`)); // jshint ignore:line
+  $('#flash').show().append(generateFlashError(xhdr, `An issue occurred while loading '${settings.url}': '${thrownError}'. Please try again or reload the page.`));
 });
