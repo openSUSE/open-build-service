openSUSE/open-build-service

commit 1549a4c94a755c4b393ec7c0e18a30a9e256271f

[api] Fix rebuild command
Uses now the package variable to query the repositories because p got removed in a previous refactoring.
Anyway, the action is depracted, use instead /build/<project>?cmd=rebuild.

src/api/app/controllers/source_controller.rb
@@ -1386,7 +1386,7 @@ class SourceController < ApplicationController
 
     path = "/build/#{@project.name}?cmd=rebuild&package=#{@package.name}"
     if repo_name
-      if p.repositories.find_by_name(repo_name).nil?
+      if @package && @package.repositories.find_by_name(repo_name).nil?
         render_error :status => 400, :errorcode => 'unknown_repository',
           :message=> "Unknown repository '#{repo_name}'"
         return

commit 2f1eb811a75902c101d0875acdaa8b87f152486c

Fix bugs surfaced by spec'ing package_command_rebuild
- package_name got dropped in bba7aebd25642b8bca56e09c82b7450aa916e36f
- Package has no repositories, broke in 1549a4c94a755c4b393ec7c0e18a30a9e256271f

src/api/app/controllers/source_controller.rb
@@ -880,17 +880,17 @@ class SourceController < ApplicationController
     # check for sources in this or linked project
     unless @package
       # check if this is a package on a remote OBS instance
-      answer = Backend::Connection.get(request.path_info)
+      answer = Package.exists_on_backend?(params[:package], params[:project])
       unless answer
         render_error status: 400, errorcode: 'unknown_package',
-                     message: "Unknown package '#{package_name}'"
+                     message: "Unknown package '#{params[:package]}'"
         return
       end
     end
 
     options = {}
     if repo_name
-      if @package && @package.repositories.find_by_name(repo_name).nil?
+      if @package && @project.repositories.find_by_name(repo_name).nil?
         render_error status: 400, errorcode: 'unknown_repository',
                      message: "Unknown repository '#{repo_name}'"
         return