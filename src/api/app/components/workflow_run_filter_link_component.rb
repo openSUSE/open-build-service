f4c083849ef7667e02c8b39f6af0e58a14037359
ncounter <dleidi@suse.com>
2024-04-04 23:34:51 +0200

    Make the filters a form with checkboxes and input values

--- src/api/app/components/workflow_run_filter_link_component.rb
+++ src/api/app/components/workflow_run_filter_link_component.rb
@@ -10,26 +10,13 @@ class WorkflowRunFilterLinkComponent < ApplicationComponent
     @icon = icon
   end
 
-  def css_for_link
-    workflow_run_filter_matches? ? 'active' : ''
-  end
-
   def icon_tag
-    tag.i(class: ['me-1', @icon, ('text-light' if workflow_run_filter_matches?)]) if @icon != ''
+    tag.i(class: ['me-1', @icon]) if @icon != ''
   end
 
   private
 
   def workflow_run_filter_matches?
-    # None of the filter links is active when filtering by PR/MR number or commit SHA
-    return false if @selected_filter[:pr_mr] || @selected_filter[:commit]
-
-    if @selected_filter[:status].present?
-      @filter_item[:status] == @selected_filter[:status]
-    elsif @selected_filter[:generic_event_type].present?
-      @filter_item[:generic_event_type] == @selected_filter[:generic_event_type]
-    elsif @selected_filter.empty?
-      @filter_item.empty?
-    end
+    @selected_filter[@filter_item].present?
   end
 end
