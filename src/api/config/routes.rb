36ea1d4c268891eb4b223f45009c4adc85ea1c84
Eduardo Navarro <enavarro@suse.com>
2024-04-04 12:57:55 +0200

    Routing, don't redefine `draw` method
    Back in the time, this method was retired from Rails. It was a nice idea
    to introduce it in the code.
    
    Now that this method has been reintroduced in Rails, we don't need to
    overwrite it.

--- src/api/config/routes.rb
+++ src/api/config/routes.rb
@@ -1,9 +1,3 @@
-class ActionDispatch::Routing::Mapper
-  def draw(routes_name)
-    instance_eval(Rails.root.join("config/routes/#{routes_name}_routes.rb").read)
-  end
-end
-
 OBSApi::Application.routes.draw do
   draw :webui
   draw :api
