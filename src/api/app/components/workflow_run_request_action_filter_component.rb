1a99ddcd0b53d70dda78a61d1bbd758a60c5d939
ncounter <dleidi@suse.com>
2024-04-09 15:36:25 +0200

    Drop no longer used parameters

--- src/api/app/components/workflow_run_request_action_filter_component.rb
+++ src/api/app/components/workflow_run_request_action_filter_component.rb
@@ -1,10 +1,7 @@
 class WorkflowRunRequestActionFilterComponent < ApplicationComponent
-  def initialize(token_id:, filter_item:, request_action: '')
+  def initialize(filter_item:)
     super
 
-    @request_action = request_action
-    @token_id = token_id
-    @generic_event_type = 'pull_request'
     @filter_options = ['all'] + SCMWebhook::ALLOWED_PULL_REQUEST_ACTIONS + SCMWebhook::ALLOWED_MERGE_REQUEST_ACTIONS
     @filter_item = filter_item
   end
