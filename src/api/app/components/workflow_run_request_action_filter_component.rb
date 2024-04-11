a7658e044a9b99218bdb48de6d7b5b3e37b13349
ncounter <dleidi@suse.com>
2024-04-11 17:05:49 +0200

    Keep track of the filtered selected value

--- src/api/app/components/workflow_run_request_action_filter_component.rb
+++ src/api/app/components/workflow_run_request_action_filter_component.rb
@@ -1,8 +1,9 @@
 class WorkflowRunRequestActionFilterComponent < ApplicationComponent
-  def initialize(filter_item:)
+  def initialize(filter_item:, selected_filter:)
     super
 
     @filter_options = ['all'] + SCMWebhook::ALLOWED_PULL_REQUEST_ACTIONS + SCMWebhook::ALLOWED_MERGE_REQUEST_ACTIONS
     @filter_item = filter_item
+    @selected_value = selected_filter
   end
 end
