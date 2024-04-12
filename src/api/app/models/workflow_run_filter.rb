fb320554138c86e13d92e4b3ae1b7985bb7664e0
ncounter <dleidi@suse.com>
2024-04-12 11:46:29 +0200

    Address CyclomaticComplexity

--- src/api/app/models/workflow_run_filter.rb
+++ src/api/app/models/workflow_run_filter.rb
@@ -1,16 +1,19 @@
 class WorkflowRunFilter
   attr_reader :status, :event_types, :request_action, :pr_mr, :commit
 
+  STATUS_FILTER_NAMES = %i[success running fail].freeze
+  EVENT_TYPE_FILTER_NAMES = %i[pull_request push tag_push].freeze
+
   def initialize(params)
     @status = []
-    @status << 'success' if params[:success]
-    @status << 'running' if params[:running]
-    @status << 'fail' if params[:fail]
+    STATUS_FILTER_NAMES.each do |filter_name|
+      @status << filter_name.to_s if params[filter_name]
+    end
 
     @event_types = []
-    @event_types << 'pull_request' if params[:pull_request]
-    @event_types << 'push' if params[:push]
-    @event_types << 'tag_push' if params[:tag_push]
+    EVENT_TYPE_FILTER_NAMES.each do |filter_name|
+      @event_types << filter_name.to_s if params[filter_name]
+    end
 
     @request_action = []
     @request_action << params[:request_action] unless params[:request_action] == 'all'
