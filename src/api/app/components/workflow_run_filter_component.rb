79b6dddb1b913b3aea0a980aeccd12d89d88fd52
Lukas Krause <lkrause@suse.de>
2024-04-15 17:11:08 +0200

    Use workflow_runs scoped to user and token
    We need to use the workflow_runs coming from the scope for the
    corresponding user and token.

--- src/api/app/components/workflow_run_filter_component.rb
+++ src/api/app/components/workflow_run_filter_component.rb
@@ -1,15 +1,16 @@
 class WorkflowRunFilterComponent < ApplicationComponent
-  def initialize(token:, selected_filter:)
+  def initialize(token:, selected_filter:, workflow_runs:)
     super
 
     @count = {
-      'success' => WorkflowRun.success.count,
-      'running' => WorkflowRun.running.count,
-      'fail' => WorkflowRun.fail.count,
-      'pull_request' => WorkflowRun.pull_request.count,
-      'push' => WorkflowRun.push.count,
-      'tag_push' => WorkflowRun.tag_push.count
+      'success' => workflow_runs.success.count,
+      'running' => workflow_runs.running.count,
+      'fail' => workflow_runs.fail.count,
+      'pull_request' => workflow_runs.pull_request.count,
+      'push' => workflow_runs.push.count,
+      'tag_push' => workflow_runs.tag_push.count
     }
+
     @selected_filter = selected_filter
     @token = token
   end
