c8d1c4073268201abaae4cff0abe4a5d11f42fe4
Eduardo Navarro <enavarro@suse.com>
2024-04-18 15:15:59 +0200

    Reapply "Include message of exception in error definition"
    This reverts commit b30221633749fbdf44449673f886d715bb4feba1.

--- src/api/app/models/token/errors.rb
+++ src/api/app/models/token/errors.rb
@@ -1,6 +1,8 @@
 require 'api_error'
 
 module Token::Errors
+  AUTHENTICATION_DOCUMENTATION_LINK = "#{::Workflow::SCM_CI_DOCUMENTATION_URL}#sec.obs.obs_scm_ci_workflow_integration.setup.token_authentication.how_to_authenticate_scm_with_obs".freeze
+
   class NoReleaseTargetFound < APIError
     setup 404
   end
@@ -18,7 +20,7 @@ module Token::Errors
   end
 
   class SCMTokenInvalid < APIError
-    setup 401
+    setup 401, "Your SCM token secret is not properly set in your OBS workflow token.\nCheck #{AUTHENTICATION_DOCUMENTATION_LINK}"
   end
 
   class WorkflowsYamlNotParsable < APIError
