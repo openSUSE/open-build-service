7c1a34331442fdf4275e4e3388b6ba3981820029
ncounter <dleidi@suse.com>
2024-04-19 10:45:39 +0200

    Discard disabled build results for the badge

--- src/api/app/controllers/webui/packages/badge_controller.rb
+++ src/api/app/controllers/webui/packages/badge_controller.rb
@@ -8,6 +8,10 @@ module Webui
         results = @package.buildresult(@project, false, true).results[@package.name]
         results = results.select { |r| r.architecture == params[:architecture] } if params[:architecture]
         results = results.select { |r| r.repository == params[:repository] } if params[:repository]
+        # discard results with excluded and disabled status
+        results = results.reject { |r| Buildresult.new(r.code).refused_status? }
+        # discard possible disabled results with previous failed status
+        results = results.reject { |r| @package.disabled_for?('build', r.repository, r.architecture) } unless results.nil? # discard disabled
         badge = Badge.new(params[:type], results)
         send_data(badge.xml, type: 'image/svg+xml', disposition: 'inline')
       end
