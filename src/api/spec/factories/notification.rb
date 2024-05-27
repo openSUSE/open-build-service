e770fb0f7193b569dfb19502ff13700ce7830d4e
Saray Cabrera Padrón <scabrerapadron@suse.de>
2024-05-27 14:03:20 +0200

    Adapt notification factory

--- src/api/spec/factories/notification.rb
+++ src/api/spec/factories/notification.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     subscription_receiver_role { 'owner' }
     title { Faker::Lorem.sentence }
     delivered { false }
+    type { 'NotificationRequestStatechange' }
 
     transient do
       originator { nil } # User login
@@ -27,60 +28,72 @@ FactoryBot.define do
       event_type { 'Event::RequestStatechange' }
       notifiable factory: [:bs_request_with_submit_action]
       bs_request_oldstate { :new }
+      type { 'NotificationRequestStatechange' }
     end
 
     trait :request_created do
       event_type { 'Event::RequestCreate' }
       notifiable factory: [:bs_request_with_submit_action]
+      type { 'NotificationRequestCreate' }
     end
 
     trait :review_wanted do
       event_type { 'Event::ReviewWanted' }
       notifiable factory: [:bs_request_with_submit_action]
+      type { 'NotificationReviewWanted' }
     end
 
     trait :comment_for_project do
       event_type { 'Event::CommentForProject' }
       notifiable factory: [:comment_project]
+      type { 'NotificationCommentForProject' }
     end
 
     trait :comment_for_package do
       event_type { 'Event::CommentForPackage' }
       notifiable factory: [:comment_package]
+      type { 'NotificationCommentForPackage' }
     end
 
     trait :comment_for_request do
       event_type { 'Event::CommentForRequest' }
       notifiable factory: [:comment_request]
+      type { 'NotificationCommentForRequest' }
     end
 
     trait :relationship_create_for_project do
       event_type { 'Event::RelationshipCreate' }
       notifiable factory: [:project]
+      type { 'NotificationRelationshipCreate' }
     end
 
     trait :relationship_delete_for_project do
       event_type { 'Event::RelationshipDelete' }
       notifiable factory: [:project]
+      type { 'NotificationRelationshipDelete' }
     end
     trait :relationship_create_for_package do
       event_type { 'Event::RelationshipCreate' }
       notifiable factory: [:package]
+      type { 'NotificationRelationshipCreate' }
     end
 
     trait :relationship_delete_for_package do
       event_type { 'Event::RelationshipDelete' }
       notifiable factory: [:package]
+      type { 'NotificationRelationshipDelete' }
     end
 
     trait :build_failure do
       event_type { 'Event::BuildFail' }
       notifiable factory: [:package]
+      type { 'NotificationBuildFail' }
     end
 
     trait :create_report do
       event_type { 'Event::CreateReport' }
       notifiable factory: [:report]
+      type { 'NotificationCreateReport' }
 
       transient do
         reason { nil }
@@ -95,6 +108,7 @@ FactoryBot.define do
     trait :cleared_decision do
       event_type { 'Event::ClearedDecision' }
       notifiable { association(:decision_cleared) }
+      type { 'NotificationClearedDecision' }
 
       after(:build) do |notification|
         notification.event_payload['reportable_type'] ||= notification.notifiable.reports.first.reportable.class.to_s
@@ -104,6 +118,7 @@ FactoryBot.define do
     trait :favored_decision do
       event_type { 'Event::FavoredDecision' }
       notifiable { association(:decision_favored) }
+      type { 'NotificationFavoredDecision' }
 
       after(:build) do |notification|
         notification.event_payload['reportable_type'] ||= notification.notifiable.reports.first.reportable.class.to_s
