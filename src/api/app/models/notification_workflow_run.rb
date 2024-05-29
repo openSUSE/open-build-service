d7cfddcd1769ceae268740b3d1edb3fcec7d6fcd
Saray Cabrera Padrón <scabrerapadron@suse.de>
2024-05-29 10:29:33 +0200

    Implement NotificationWorkflowRun methods

--- src/api/app/models/notification_workflow_run.rb
+++ src/api/app/models/notification_workflow_run.rb
@@ -1,14 +1,18 @@
 class NotificationWorkflowRun < Notification
   # TODO: rename to title once we get rid of Notification#title
   def summary
+    'Workflow Run'
   end
 
   def description
+    ''
   end
 
   def excerpt
+    "In repository #{notifiable.repository_full_name}"
   end
 
   def involved_users
+    [Token.find(event_payload['token_id'])&.executor].compact
   end
 end
