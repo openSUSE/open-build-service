6ae4f241bfc14f4270e987721fe299ab518b6e6e
Steven R. Baker <steven@stevenrbaker.com>
2024-05-30 16:31:51 +0200

    Appease the linter on RSpec syntax.
    RSpec syntax confuses me.  Why do people even use this thing?

--- src/api/spec/models/groups_user_spec.rb
+++ src/api/spec/models/groups_user_spec.rb
@@ -3,12 +3,18 @@ RSpec.describe GroupsUser do
   let(:user) { create(:confirmed_user, login: 'eisendieter') }
 
   it 'creates an AddedToGroupEvent when a user is added to a group' do
-    expect(Event::AddedUserToGroup).to receive(:create).with(group: group, user: user)
+    allow(Event::AddedUserToGroup).to receive(:create)
+
     group.add_user(user)
+
+    expect(Event::AddedUserToGroup).to have_received(:create).with(group: group, user: user)
   end
 
   it 'creates a RemovedFromGroupEvent when a user is removed from a group' do
+    allow(Event::RemovedUserFromGroup).to receive(:create)
+
     group.add_user(user)
+
     group.remove_user(user)
     expect(Event::RemovedUserFromGroup).to have_received(:create).with(group: group, user: user)
   end
