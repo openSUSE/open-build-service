45daf28bf1ffe37c98cc680554e23309def03d50
Steven R. Baker <steven@stevenrbaker.com>
2024-05-30 15:11:25 +0200

    Appease the linter.

--- src/api/spec/models/groups_user_spec.rb
+++ src/api/spec/models/groups_user_spec.rb
@@ -2,14 +2,14 @@ RSpec.describe GroupsUser do
   let(:group) { create(:group) }
   let(:user) { create(:confirmed_user, login: 'eisendieter') }
 
-  it "creates an AddedToGroupEvent when a user is added to a group" do
+  it 'creates an AddedToGroupEvent when a user is added to a group' do
     expect(Event::AddedUserToGroup).to receive(:create).with(group: group, user: user)
-    group.add_user user
+    group.add_user(user)
   end
 
-  it "creates a RemovedFromGroupEvent when a user is removed from a group" do
-    group.add_user user
-    expect(Event::RemovedUserFromGroup).to receive(:create).with(group: group, user: user)
-    group.remove_user user
+  it 'creates a RemovedFromGroupEvent when a user is removed from a group' do
+    group.add_user(user)
+    group.remove_user(user)
+    expect(Event::RemovedUserFromGroup).to have_received(:create).with(group: group, user: user)
   end
 end
