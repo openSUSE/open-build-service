3ed75db9cc2819018a8ecace6bc54916b126b5a9
Saray Cabrera Padrón <scabrerapadron@suse.de>
2024-06-04 09:56:13 +0200

    Use a group icon for the new events
    For both AddedUserToGroup and RemovedUserFromGroup (which was missing
    before).

--- src/api/app/components/notification_component.rb
+++ src/api/app/components/notification_component.rb
@@ -3,14 +3,15 @@ class NotificationComponent < ApplicationComponent
     'BsRequest' => 'fa-code-pull-request', 'Comment' => 'fa-comments',
     'Package' => 'fa-xmark text-danger',
     'Report' => 'fa-flag', 'Decision' => 'fa-clipboard-check',
-    'Appeal' => 'fa-hand', 'WorkflowRun' => 'fa-book-open'
+    'Appeal' => 'fa-hand', 'WorkflowRun' => 'fa-book-open',
+    'Group' => 'fa-people-group'
   }.freeze
 
   NOTIFICATION_TITLE = {
     'BsRequest' => 'Request notification', 'Comment' => 'Comment notification',
     'Package' => 'Package notification', 'Report' => 'Report notification',
     'Decision' => 'Report decision', 'Appeal' => 'Decision appeal',
-    'WorkflowRun' => 'Workflow run'
+    'WorkflowRun' => 'Workflow run', 'Group' => 'Group members changed'
   }.freeze
 
   def initialize(notification:, selected_filter:, page:, show_more:)
@@ -23,7 +24,7 @@ class NotificationComponent < ApplicationComponent
   end
 
   def notification_icon
-    if @notification.event_type.in?(['Event::RelationshipCreate', 'Event::RelationshipDelete', 'Event::AddedUserToGroup'])
+    if @notification.event_type.in?(['Event::RelationshipCreate', 'Event::RelationshipDelete'])
       tag.i(class: %w[fas fa-user-tag], title: 'Relationship notification')
     elsif NOTIFICATION_ICON[@notification.notifiable_type].present?
       tag.i(class: ['fas', NOTIFICATION_ICON[@notification.notifiable_type]], title: NOTIFICATION_TITLE[@notification.notifiable_type])
