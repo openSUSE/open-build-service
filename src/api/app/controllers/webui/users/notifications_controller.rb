ba3562116e2e2044193357b2549f4959359d7a87
ncounter <dleidi@suse.com>
2024-06-04 12:49:56 +0200

    Add 'kind' notifications counting

--- src/api/app/controllers/webui/users/notifications_controller.rb
+++ src/api/app/controllers/webui/users/notifications_controller.rb
@@ -77,6 +77,16 @@ class Webui::Users::NotificationsController < Webui::WebuiController
     @counted_notifications['all'] = @notifications.count
     @counted_notifications['unread'] = @notifications.unread.count
     @counted_notifications['read'] = @notifications.read.count
+    @counted_notifications['comments'] = @notifications.for_comments.count
+    @counted_notifications['requests'] = @notifications.for_requests.count
+    @counted_notifications['incoming_requests'] = @notifications.for_incoming_requests(User.session).count
+    @counted_notifications['outgoing_requests'] = @notifications.for_outgoing_requests(User.session).count
+    @counted_notifications['relationships_created'] = @notifications.for_relationships_created.count
+    @counted_notifications['relationships_deleted'] = @notifications.for_relationships_deleted.count
+    @counted_notifications['build_failures'] = @notifications.for_build_failures.count
+    @counted_notifications['reports'] = @notifications.for_reports.count
+    @counted_notifications['workflow_runs'] = @notifications.for_workflow_runs.count
+    @counted_notifications['appealed_decisions'] = @notifications.for_appealed_decisions.count
   end
 
   def update_counted_notifications
