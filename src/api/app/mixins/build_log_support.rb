d02da96ad891601fa641229a42f7fb5ea8b2cff1
Jacob Michalskie <jacob.michalskie@suse.com>
2024-06-06 16:04:26 +0200

    Escape html before parsing the log

--- src/api/app/mixins/build_log_support.rb
+++ src/api/app/mixins/build_log_support.rb
@@ -9,6 +9,7 @@ module BuildLogSupport
   def get_log_chunk(project, package_name, repo, arch, start, theend)
     log = raw_log_chunk(project, package_name, repo, arch, start, theend)
     log.encode!(invalid: :replace, undef: :replace, cr_newline: true)
+    log = CGI.escapeHTML(log)
     log = ansi_escaped(log, theend - start + 1)
     log.gsub(%r{([^a-zA-Z0-9&;<>/\n\r \t()])}) do |c|
       if c.ord < 32
