36fbaf96fb9e2c1ba96a15001bd4b500e094c896
Saray Cabrera Padrón <scabrerapadron@suse.de>
2024-06-07 15:16:33 +0200

    Implement NotificationComment methods

--- src/api/app/models/notification_comment.rb
+++ src/api/app/models/notification_comment.rb
@@ -1,14 +1,34 @@
 class NotificationComment < Notification
   # TODO: rename to title once we get rid of Notification#title
   def summary
+    case notifiable.commentable_type
+    when 'BsRequest'
+      "Comment on #{request_type_of_action(bs_request)} Request ##{bs_request.number}"
+    when 'Project'
+      'Comment on Project'
+    when 'Package'
+      'Comment on Package'
+    end
   end
 
   def description
+    case notifiable.commentable_type
+    when 'BsRequest'
+      "From #{request_source} to #{request_target}"
+    when 'Project'
+      notifiable.commentable.name
+    when 'Package'
+      commentable = notifiable.commentable
+      "#{commentable.project.name} / #{commentable.name}"
+    end
   end
 
   def excerpt
+    notifiable.body
   end
 
   def involved_users
+    comments = notifiable.commentable.comments
+    comments.select { |comment| comment.updated_at >= unread_date }.map(&:user).uniq
   end
 end
