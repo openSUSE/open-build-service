5cc2122714571f85e468cd747618019a70bb6b96
Saray Cabrera Padrón <scabrerapadron@suse.de>
2024-06-07 15:14:45 +0200

    Define methods on parent model

--- src/api/app/models/notification.rb
+++ src/api/app/models/notification.rb
@@ -88,6 +88,41 @@ class Notification < ApplicationRecord
     RabbitmqBus.send_to_bus('metrics',
                             "notification,action=#{delivered ? 'read' : 'unread'} value=1")
   end
+
+  def bs_request
+    if notifiable_type == 'BsRequest'
+      notifiable
+    elsif notifiable.commentable.is_a?(BsRequestAction)
+      notifiable.commentable.bs_request
+    else
+      notifiable.commentable
+    end
+  end
+
+  # TODO: move to BsRequest
+  # FIXME: Duplicated from RequestHelper
+  # Returns strings like "Add Role", "Submit", etc.
+  def request_type_of_action(bs_request)
+    return 'Multiple Actions' if bs_request.bs_request_actions.size > 1
+
+    bs_request.bs_request_actions.first.type.titleize
+  end
+
+  def first_bs_request_action
+    bs_request.bs_request_actions.first
+  end
+
+  def request_source
+    return '' if bs_request.bs_request_actions.size > 1
+
+    [first_bs_request_action.source_project, first_bs_request_action.source_package].compact.join(' / ')
+  end
+
+  def request_target
+    return first_bs_request_action.target_project if bs_request.bs_request_actions.size > 1
+
+    [first_bs_request_action.target_project, first_bs_request_action.target_package].compact.join(' / ')
+  end
 end
 
 # == Schema Information
