70f3c65c7612522651f16a2e874bc3c73bc38356
Saray Cabrera Padrón <scabrerapadron@suse.de>
2024-06-10 14:09:53 +0200

    Implement NotificationBsRequest methods

--- src/api/app/models/notification_bs_request.rb
+++ src/api/app/models/notification_bs_request.rb
@@ -1,14 +1,19 @@
 class NotificationBsRequest < Notification
   # TODO: rename to title once we get rid of Notification#title
   def summary
+    "#{request_type_of_action(notifiable)} Request ##{notifiable.number}"
   end
 
   def description
+    "From #{request_source} to #{request_target}"
   end
 
   def excerpt
+    notifiable.description.to_s # description can be nil
   end
 
   def involved_users
+    reviews = notifiable.reviews
+    reviews.select(&:new?).map(&:reviewed_by) + User.where(login: notifiable.creator)
   end
 end
