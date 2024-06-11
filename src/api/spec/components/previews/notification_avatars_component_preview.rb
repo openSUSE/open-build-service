02b3096da05d7d5a3438f74107e32bc0d3951e45
Saray Cabrera Padrón <scabrerapadron@suse.de>
2024-06-11 18:37:20 +0200

    Replace components by model methods or helpers

--- src/api/spec/components/previews/notification_avatars_component_preview.rb
+++
@@ -1,6 +0,0 @@
-class NotificationAvatarsComponentPreview < ViewComponent::Preview
-  # Preview at http://HOST:PORT/rails/view_components/notification_avatars_component/preview
-  def preview
-    render(NotificationAvatarsComponent.new(Notification.last))
-  end
-end
