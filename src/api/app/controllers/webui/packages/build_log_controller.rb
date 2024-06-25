28d72ff3e53b3edc94fe8355a160f76bf9a661d9
Henne Vogelsang <hvogel@opensuse.org>
2024-06-25 17:53:58 +0200

    Drop redundant begin

--- src/api/app/controllers/webui/packages/build_log_controller.rb
+++ src/api/app/controllers/webui/packages/build_log_controller.rb
@@ -21,41 +21,39 @@ module Webui
       end
 
       def update_build_log
-        begin
-          @maxsize = 1024 * 64
-          @first_request = params[:initial] == '1'
-          @offset = params[:offset].to_i
-          @status = get_status(@project, @package_name, @repository, @architecture)
-          @finished = Buildresult.final_status?(@status)
-          @size = get_size_of_log(@project, @package_name, @repository, @architecture)
+        @maxsize = 1024 * 64
+        @first_request = params[:initial] == '1'
+        @offset = params[:offset].to_i
+        @status = get_status(@project, @package_name, @repository, @architecture)
+        @finished = Buildresult.final_status?(@status)
+        @size = get_size_of_log(@project, @package_name, @repository, @architecture)
 
-          chunk_start = @offset
-          chunk_end = @offset + @maxsize
+        chunk_start = @offset
+        chunk_end = @offset + @maxsize
 
-          # Start at the most recent part to not get the full log from the begining just the last 64k
-          if @first_request && (@finished || @size >= @maxsize)
-            chunk_start = [0, @size - @maxsize].max
-            chunk_end = @size
-          end
+        # Start at the most recent part to not get the full log from the begining just the last 64k
+        if @first_request && (@finished || @size >= @maxsize)
+          chunk_start = [0, @size - @maxsize].max
+          chunk_end = @size
+        end
 
-          @log_chunk = get_log_chunk(@project, @package_name, @repository, @architecture, chunk_start, chunk_end)
+        @log_chunk = get_log_chunk(@project, @package_name, @repository, @architecture, chunk_start, chunk_end)
 
-          old_offset = @offset
-          @offset = [chunk_end, @size].min
-        rescue Timeout::Error, IOError
+        old_offset = @offset
+        @offset = [chunk_end, @size].min
+      rescue Timeout::Error, IOError
+        @log_chunk = ''
+      rescue Backend::Error => e
+        case e.summary
+        when /Logfile is not that big/
           @log_chunk = ''
-        rescue Backend::Error => e
-          case e.summary
-          when /Logfile is not that big/
-            @log_chunk = ''
-          when /start out of range/
-            # probably build compare has cut log and offset is wrong, reset offset
-            @log_chunk = ''
-            @offset = old_offset
-          else
-            @log_chunk = "No live log available: #{e.summary}\n"
-            @finished = true
-          end
+        when /start out of range/
+          # probably build compare has cut log and offset is wrong, reset offset
+          @log_chunk = ''
+          @offset = old_offset
+        else
+          @log_chunk = "No live log available: #{e.summary}\n"
+          @finished = true
         end
       end
 
