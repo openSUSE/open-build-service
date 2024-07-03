fb5303510741e4ede879c871bfb77b96c454d711
Eduardo Navarro <enavarro@suse.com>
2024-07-03 16:07:06 +0200

    Fix parameter key for outdated notifications finders

--- src/api/app/queries/outdated_notifications_finder/project.rb
+++ src/api/app/queries/outdated_notifications_finder/project.rb
@@ -5,6 +5,6 @@ class OutdatedNotificationsFinder::Project
   end
 
   def call
-    @scope.where(notifiable_type: 'Project', notifiable_id: @parameters['notifiable_id'])
+    @scope.where(notifiable_type: 'Project', notifiable_id: @parameters[:notifiable_id])
   end
 end
