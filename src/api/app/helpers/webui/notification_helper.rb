e7a891702eb748838973cd27810b41f471bda8cb
Saray Cabrera Padrón <scabrerapadron@suse.de>
2024-07-09 13:11:50 +0200

    Add code to NotificationHelper
    Add code needed in views that come from components.

--- src/api/app/helpers/webui/notification_helper.rb
+++ src/api/app/helpers/webui/notification_helper.rb
@@ -2,6 +2,37 @@ module Webui::NotificationHelper
   TRUNCATION_LENGTH = 100
   TRUNCATION_ELLIPSIS_LENGTH = 3 # `...` is the default ellipsis for String#truncate
 
+  MAXIMUM_DISPLAYED_AVATARS = 6
+
+  def truncate_to_first_new_line(text)
+    first_new_line_index = text.index("\n")
+    truncation_index = !first_new_line_index.nil? && first_new_line_index < TRUNCATION_LENGTH ? first_new_line_index + TRUNCATION_ELLIPSIS_LENGTH : TRUNCATION_LENGTH
+    text.truncate(truncation_index)
+  end
+
+  def avatars(notification)
+    capture do
+      tag.ul(class: 'list-inline d-flex flex-row-reverse avatars m-0') do
+        hidden_avatars(notification)
+
+        avatars_to_display(notification.avatar_objects).each do |avatar_object|
+          concat(
+            tag.li(class: 'list-inline-item') do
+              case avatar_object.class.name
+              when 'User', 'Group'
+                render(AvatarComponent.new(name: avatar_object.name, email: avatar_object.email, size: 23, shape: :circle))
+              when 'Package'
+                tag.span(class: 'fa fa-archive text-warning rounded-circle bg-body-secondary border simulated-avatar', title: "Package #{avatar_object.project}/#{avatar_object}")
+              when 'Project'
+                tag.span(class: 'fa fa-cubes text-secondary rounded-circle bg-body-secondary border simulated-avatar', title: "Project #{avatar_object}")
+              end
+            end
+          )
+        end
+      end
+    end
+  end
+
   private
 
   def mark_as_read_or_unread_button(notification)
@@ -15,9 +46,23 @@ module Webui::NotificationHelper
     end
   end
 
-  def truncate_to_first_new_line(text)
-    first_new_line_index = text.index("\n")
-    truncation_index = !first_new_line_index.nil? && first_new_line_index < TRUNCATION_LENGTH ? first_new_line_index + TRUNCATION_ELLIPSIS_LENGTH : TRUNCATION_LENGTH
-    text.truncate(truncation_index)
+  def number_of_hidden_avatars(avatar_objects)
+    [0, avatar_objects.size - MAXIMUM_DISPLAYED_AVATARS].max
+  end
+
+  def hidden_avatars(notification)
+    return unless number_of_hidden_avatars(notification.avatar_objects).positive?
+
+    concat(
+      tag.li(class: 'list-inline-item') do
+        tag.span("#{number_of_hidden_avatars(notification.avatar_objects)}",
+                 class: 'rounded-circle bg-body-secondary border avatars-counter',
+                 title: "#{number_of_hidden_avatars(notification.avatar_objects)} more users involved")
+      end
+    )
+  end
+
+  def avatars_to_display(avatar_objects)
+    avatar_objects.first(MAXIMUM_DISPLAYED_AVATARS).reverse
   end
 end
--- src/api/spec/helpers/webui/notification_helper_spec.rb
+++ src/api/spec/helpers/webui/notification_helper_spec.rb
@@ -20,4 +20,82 @@ RSpec.describe Webui::NotificationHelper do
       it { expect(link).to include('fa-undo fas') }
     end
   end
+
+  describe '#truncate_to_first_new_line' do
+    context 'when text has no newline' do
+      it {
+        expect(truncate_to_first_new_line('some text without newline'))
+          .to eql('some text without newline')
+      }
+    end
+
+    context 'when text has newline' do
+      it {
+        expect(truncate_to_first_new_line('some text with a first line here\nthis is the second line'))
+          .to eql('some text with a first line here\\nthis is the second line')
+      }
+    end
+
+    context 'when text is long' do
+      it {
+        expect(truncate_to_first_new_line('some text with a long long long long long long long long long long long long long long long long long first line\nand a second line'))
+          .to eql('some text with a long long long long long long long long long long long long long long long long ...')
+      }
+    end
+  end
+
+  describe '#avatars' do
+    let(:admin) { create(:admin_user) }
+    let(:iggy) { create(:staff_user, login: 'Iggy') }
+    let(:factory) { create(:project, name: 'openSUSE:Factory') }
+    let(:staging_workflow) { create(:staging_workflow, project: factory) }
+    let(:leap) { create(:project, name: 'openSUSE:Leap:15.0') }
+    let(:leap_apache) { create(:package_with_file, name: 'apache2', project: leap) }
+    let(:notification) { create(:notification, :request_created, notifiable: bs_request) }
+
+    before { User.session = admin }
+
+    context 'when displaying users or groups' do
+      let(:notification) { create(:notification, :request_created) }
+
+      it { expect(avatars(notification.for_notifiable)).to include 'gravatar' }
+    end
+
+    context 'when displaying packages' do
+      let(:bs_request) do
+        bs_request = create(
+          :bs_request_with_submit_action,
+          creator: iggy,
+          target_package: 'inreview',
+          target_project: factory,
+          source_package: leap_apache
+        )
+        bs_request.save
+        bs_request
+      end
+
+      it { expect(avatars(notification.for_notifiable)).to include 'fa-archive' }
+    end
+
+    context 'when displaying projects' do
+      let(:bs_request) do
+        bs_request = create(
+          :bs_request_with_submit_action,
+          creator: iggy,
+          target_package: 'inreview',
+          target_project: factory,
+          source_package: leap_apache
+        )
+        bs_request.staging_project = staging_workflow.staging_projects.first
+        5.times do
+          bs_request.reviews << create(:review, by_project: bs_request.staging_project)
+        end
+        bs_request.save
+        bs_request
+      end
+
+      it { expect(avatars(notification.for_notifiable)).to include 'fa-cubes' }
+      it { expect(avatars(notification.for_notifiable)).to include 'avatars-counter' }
+    end
+  end
 end
