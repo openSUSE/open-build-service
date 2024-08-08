57dc8c3f677e036974c3da7430eb93a3847e4db5
Dani Donisa <dani.doni@gmail.com>
2024-08-08 09:52:19 +0200

    Make the form compatible with Bootstrap server side validation tools

---
+++ src/api/config/initializers/field_with_errors.rb
@@ -0,0 +1,8 @@
+# Rails form helpers wrap controls with a .field_with_errors class that prevents the validation styles from bootstrap to work.
+# This snippet replaces the wrapping with that class with an injection of .is-invalid class, which is bootstrap compatible.
+ActionView::Base.field_error_proc = proc do |html|
+  frag = Nokogiri::HTML5::DocumentFragment.parse(html)
+  klass = frag.children[0].attributes['class']
+  frag.children[0].attributes['class'].value = [klass, 'is-invalid'].join(' ')
+  frag.to_html.html_safe # rubocop:disable Rails/OutputSafety The input is code from our views, so it's safe to disable this
+end
