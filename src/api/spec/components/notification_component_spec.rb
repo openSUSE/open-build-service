43605b9434837801e9ba0ab55d9838170c8b473d
Rubhan Azeem <rubhanazeem@gmail.com>
2024-08-19 13:32:50 +0200

    Add specs to check presence of user state and number of reports on notifications view

--- src/api/spec/components/notification_component_spec.rb
+++ src/api/spec/components/notification_component_spec.rb
@@ -29,4 +29,36 @@ RSpec.describe NotificationComponent, type: :component do
       expect(rendered_content).to have_css("i.fa-comments[title='Comment notification']")
     end
   end
+
+  context 'reports' do
+    let(:accused) { create(:confirmed_user, login: 'accused') }
+    let(:notification) { create(:notification_for_report, event_payload: event_payload, event_type: event_type, notifiable: notifiable) }
+
+    before do
+      render_inline(described_class.new(notification: notification, selected_filter: selected_filter, page: 1))
+    end
+
+    context 'when the notification is about user' do
+      let(:event_payload) { { reporter: user.login, reportable_id: accused.id, reportable_type: 'User', reason: 'some sample text for reason field', category: 'spam', accused: accused.login } }
+      let(:event_type) { 'Event::ReportForUser' }
+      let(:notifiable) { create(:report, reportable: accused, reason: 'Some sample text') }
+
+      it 'renders information about user state' do
+        expect(rendered_content).to have_css('span', text: accused.state, class: 'badge')
+      end
+    end
+
+    context 'when the notification is about comment' do
+      let(:project) { create(:project, maintainer: user) }
+      let(:comment) { create(:comment, commentable: project) }
+      let(:event_type) { 'Event::ReportForComment' }
+      let(:notifiable) { create(:report, reportable: comment, reason: 'Some sample text') }
+      let(:event_payload) { { reporter: user.login, reportable_id: comment.id, reportable_type: 'Comment', reason: 'some sample text for reason field', category: 'spam' } }
+
+      it 'renders information about user state and its existing reports' do
+        expect(rendered_content).to have_css('span', text: accused.state, class: 'badge')
+        expect(rendered_content).to have_css('span', text: '+1', class: 'badge')
+      end
+    end
+  end
 end
