2942e86f6633d0fdf97b83ca8875e9ca46337d68
Eduardo Navarro <enavarro@suse.com>
2024-08-23 10:51:59 +0200

    Drop blocked_from_commenting from users table

--- src/api/db/data/20240814142031_backfill_censored_on_users.rb
+++ src/api/db/data/20240814142031_backfill_censored_on_users.rb
@@ -2,6 +2,8 @@
 
 class BackfillCensoredOnUsers < ActiveRecord::Migration[7.0]
   def up
+    return unless User.columns.any? { |c| c.name == 'blocked_from_commenting' }
+
     User.where(blocked_from_commenting: true).in_batches do |batch|
       batch.find_each do |user|
         user.update(censored: user.blocked_from_commenting)
