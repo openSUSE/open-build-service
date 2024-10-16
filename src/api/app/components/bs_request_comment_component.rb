d8430735280b1d82fc0428855149b4708f2c6f7b
Jay Michalska <jay.michalska@suse.com>
2024-10-16 15:32:26 +0200

    Display outdated badge on outdated comments

--- src/api/app/components/bs_request_comment_component.rb
+++ src/api/app/components/bs_request_comment_component.rb
@@ -19,4 +19,17 @@ class BsRequestCommentComponent < ApplicationComponent
     line_index = @comment.diff_ref.match(/diff_[0-9]+_n([0-9]+)/).captures.first
     ((line_index.to_i - 4).clamp(0..)..(line_index.to_i - 1))
   end
+
+  def outdated?
+    return false if @comment.diff_ref.blank?
+    return false if @comment.source_rev.nil? && @comment.target_rev.nil?
+
+    target_package = Package.find_by_project_and_name(@commentable.target_project, @commentable.target_package)
+    return true unless target_package.dir_hash['srcmd5'] == @comment.target_rev
+
+    source_package = Package.find_by_project_and_name(@commentable.source_project, @commentable.source_package)
+    return true unless source_package.dir_hash({ rev: @commentable.source_rev }.compact)['srcmd5'] == @comment.source_rev
+
+    false
+  end
 end
