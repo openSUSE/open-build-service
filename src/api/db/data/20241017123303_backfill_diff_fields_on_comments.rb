fd1cc2d54e10767e8e5b9e1356be54382a04ce55
Eduardo Navarro <enavarro@suse.com>
2024-10-17 14:51:54 +0200

    Backfill diff_file_index and diff_line_number columns...
    ... on table comments

---
+++ src/api/db/data/20241017123303_backfill_diff_fields_on_comments.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class BackfillDiffFieldsOnComments < ActiveRecord::Migration[7.0]
+  def up
+    Comment.where.not(diff_ref: nil).where(diff_file_index: nil).in_batches do |batch|
+      batch.find_each do |comment|
+        diff_file_index, diff_line_number = comment.diff_ref.match(/diff_([0-9]+)_n([0-9]+)/).captures
+
+        comment.update!(diff_file_index:, diff_line_number:)
+      end
+    end
+  end
+
+  def down
+    Comment.where.not(diff_file_index: nil).in_batches do |batch|
+      batch.find_each do |comment|
+        comment.update!(diff_file_index: nil, diff_line_number: nil)
+      end
+    end
+  end
+end
