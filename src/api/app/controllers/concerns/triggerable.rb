a0ea6db50aac0f0a5a2608131bca4201c05158ef
Eduardo Navarro <enavarro@suse.com>
2024-11-12 15:04:20 +0100

    Fix RuboCop offenses and refactor the code slightly

--- src/api/app/controllers/concerns/triggerable.rb
+++ src/api/app/controllers/concerns/triggerable.rb
@@ -11,17 +11,16 @@ module Triggerable
     raise Project::Errors::UnknownObjectError, "Sorry, triggering tokens for remote project \"#{@project_name}\" is not possible." unless @project.is_a?(Project)
   end
 
-  def set_package(package_find_options: {})
-    package_find_options = @token.package_find_options if package_find_options.blank?
+  def set_package(package_find_options: {}) # rubocop:disable Metrics/CyclomaticComplexity
     # By default we operate on the package association
     @package = @token.package
     validate_token_package
 
     # If the token has no package, let's find one from the parameters
     if @package_name.present?
-      @package ||= Package.get_by_project_and_name(@project.name,
-                                                   @package_name,
-                                                   package_find_options)
+      package_find_options = @token.package_find_options if package_find_options.blank?
+
+      return if (@package = Package.get_by_project_and_name(@project.name, @package_name, package_find_options))
     end
     return if @package
     return unless project_links_to_remote?
