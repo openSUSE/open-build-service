d3baf41b25762ecad8e4a029504555f5e7258141
Dani Donisa <dani.doni@gmail.com>
2024-12-02 15:32:23 +0100

    Implement user requests listing under new controllers
    We want it to be more "the rails way" and to move specific code out of generic concerns

--- src/api/app/controllers/webui/users/bs_requests_controller.rb
+++ src/api/app/controllers/webui/users/bs_requests_controller.rb
@@ -4,6 +4,8 @@ module Webui
       before_action :require_login
       before_action :set_user
 
+      include Webui::RequestsFilter
+
       REQUEST_METHODS = {
         'all_requests_table' => :requests,
         'requests_out_table' => :outgoing_requests,
@@ -14,19 +16,56 @@ module Webui
 
       def index
         if Flipper.enabled?(:request_index, User.session)
-          redirect_to requests_path(involvement: params[:involvement], state: params[:state])
-        else
-          parsed_params = BsRequest::DataTable::ParamsParser.new(params).parsed_params
-          requests_query = BsRequest::DataTable::FindForUserOrGroupQuery.new(@user_or_group, request_method, parsed_params)
-          @requests_data_table = BsRequest::DataTable::Table.new(requests_query, parsed_params[:draw])
+          set_filter_involvement
+          set_filter_state
+          set_filter_action_type
+          set_filter_creators
 
-          respond_to do |format|
-            format.json { render 'webui/shared/bs_requests/index' }
-          end
+          filter_requests
+          set_selected_filter
+
+          # TODO: Temporarily disable list of creators due to performance issues
+          # @bs_requests_creators = @bs_requests.distinct.pluck(:creator)
+          @bs_requests = @bs_requests.order('number DESC').page(params[:page])
+          @bs_requests = @bs_requests.includes(:bs_request_actions, :comments, :reviews)
+          @bs_requests = @bs_requests.includes(:labels) if Flipper.enabled?(:labels, User.session)
+        else
+          index_legacy
         end
       end
 
       private
+
+      def filter_requests
+        if params[:requests_search_text].present?
+          initial_bs_requests = filter_by_text(params[:requests_search_text])
+          params[:ids] = filter_by_users_involvement(@filter_involvement).ids
+        else
+          initial_bs_requests = filter_by_users_involvement(@filter_involvement)
+        end
+
+        params[:creator] = @filter_creators if @filter_creators.present?
+        params[:states] = @filter_state if @filter_state.present?
+        params[:types] = @filter_action_type if @filter_action_type.present?
+
+        @bs_requests = BsRequest::FindFor::Query.new(params, initial_bs_requests).all
+      end
+
+      def filter_by_users_involvement(filter_involvement)
+        case filter_involvement
+        when 'all'
+          User.session.requests
+        when 'incoming'
+          User.session.incoming_requests
+        when 'outgoing'
+          User.session.outgoing_requests
+        end
+      end
+
+      def set_selected_filter
+        @selected_filter = { involvement: @filter_involvement, action_type: @filter_action_type, search_text: params[:requests_search_text],
+                             state: @filter_state, creators: @filter_creators }
+      end
 
       def set_user
         @user_or_group = User.session
@@ -34,6 +73,17 @@ module Webui
 
       def request_method
         REQUEST_METHODS[params[:dataTableId]] || :requests
+      end
+
+      # TODO: Remove this old index action when request_index feature is rolled-over
+      def index_legacy
+        parsed_params = BsRequest::DataTable::ParamsParser.new(params).parsed_params
+        requests_query = BsRequest::DataTable::FindForUserOrGroupQuery.new(@user_or_group, request_method, parsed_params)
+        @requests_data_table = BsRequest::DataTable::Table.new(requests_query, parsed_params[:draw])
+
+        respond_to do |format|
+          format.json { render 'webui/shared/bs_requests/index' }
+        end
       end
     end
   end
