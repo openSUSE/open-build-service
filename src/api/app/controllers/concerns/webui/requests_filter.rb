6559d4af45f0b2bcf1a7248234a3b314b85db74c
Lukas Krause <lkrause@suse.de>
2024-12-04 15:41:33 +0100

    Remove code duplication of `filter_requests` method

--- src/api/app/controllers/concerns/webui/requests_filter.rb
+++ src/api/app/controllers/concerns/webui/requests_filter.rb
@@ -23,6 +23,21 @@ module Webui::RequestsFilter
     @filter_creators = params[:creators].presence || []
   end
 
+  def filter_requests
+    if params[:requests_search_text].present?
+      initial_bs_requests = filter_by_text(params[:requests_search_text])
+      params[:ids] = filter_by_involvement(@filter_involvement).ids
+    else
+      initial_bs_requests = filter_by_involvement(@filter_involvement)
+    end
+
+    params[:creator] = @filter_creators if @filter_creators.present?
+    params[:states] = @filter_state if @filter_state.present?
+    params[:types] = @filter_action_type if @filter_action_type.present?
+
+    @bs_requests = BsRequest::FindFor::Query.new(params, initial_bs_requests).all
+  end
+
   def filter_by_text(text)
     if BsRequest.search_count(text) > TEXT_SEARCH_MAX_RESULTS
       flash[:error] = 'Your text search pattern matches too many results. Please, try again with a more restrictive search pattern.'
--- src/api/app/controllers/webui/projects/bs_requests_controller.rb
+++ src/api/app/controllers/webui/projects/bs_requests_controller.rb
@@ -31,21 +31,6 @@ module Webui
 
       private
 
-      def filter_requests
-        if params[:requests_search_text].present?
-          initial_bs_requests = filter_by_text(params[:requests_search_text])
-          params[:ids] = filter_by_involvement(@filter_involvement).ids
-        else
-          initial_bs_requests = filter_by_involvement(@filter_involvement)
-        end
-
-        params[:creator] = @filter_creators if @filter_creators.present?
-        params[:states] = @filter_state if @filter_state.present?
-        params[:types] = @filter_action_type if @filter_action_type.present?
-
-        @bs_requests = BsRequest::FindFor::Query.new(params, initial_bs_requests).all
-      end
-
       def set_selected_filter
         @selected_filter = { involvement: @filter_involvement, action_type: @filter_action_type, search_text: params[:requests_search_text],
                              state: @filter_state, creators: @filter_creators }
