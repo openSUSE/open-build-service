2ccab00fde49d7af223e6d0525f9861c0cd91b39
Lukas Krause <lkrause@suse.de>
2024-12-06 11:56:55 +0100

    Remove outdated method from `RequestsFilter` concern
    The RequestsListings controller is gone, those methods are not
    in use anymore.

--- src/api/app/controllers/concerns/webui/requests_filter.rb
+++ src/api/app/controllers/concerns/webui/requests_filter.rb
@@ -43,30 +43,4 @@ module Webui::RequestsFilter
 
     BsRequest.with_actions.where(id: BsRequest.search_for_ids(text, per_page: TEXT_SEARCH_MAX_RESULTS))
   end
-
-  def filter_by_involvement(filter_involvement, project = nil, package = nil)
-    return filter_by_involvement_for_package(filter_involvement, project, package) if package
-
-    case filter_involvement
-    when 'all'
-      User.session.requests
-    when 'incoming'
-      User.session.incoming_requests
-    when 'outgoing'
-      User.session.outgoing_requests
-    end
-  end
-
-  def filter_by_involvement_for_package(filter_by_involvement, project, package)
-    target = BsRequest.with_actions.where(bs_request_actions: { target_project: project.name, target_package: package.name })
-    source = BsRequest.with_actions.where(bs_request_actions: { source_project: project.name, source_package: package.name })
-    case filter_by_involvement
-    when 'all'
-      target.or(source)
-    when 'incoming'
-      target
-    when 'outgoing'
-      source
-    end
-  end
 end
