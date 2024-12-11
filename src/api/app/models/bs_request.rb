93882667efa8e0ef59a98c88719f4fa05d0b6b74
Henne Vogelsang <hvogel@opensuse.org>
2024-12-11 16:51:21 +0100

    Drop with_action_type scope
    It's unused.

--- src/api/app/models/bs_request.rb
+++ src/api/app/models/bs_request.rb
@@ -60,8 +60,6 @@ class BsRequest < ApplicationRecord
       .includes(:reviews)
   }
 
-  scope :with_action_type, ->(action_type) { joins(:bs_request_actions).where(bs_request_actions: { type: action_type }).distinct }
-
   has_many :bs_request_actions, dependent: :destroy
   has_many :reviews, dependent: :delete_all
   has_many :comments, as: :commentable, dependent: :destroy
