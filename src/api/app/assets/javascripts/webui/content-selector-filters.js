f8c98c2c0ef406fe781f30dd8db25b7f94e5cdfb
ncounter <dleidi@suse.com>
2025-01-20 17:00:31 +0100

    Refactoring: use document.ready only when needed

--- src/api/app/assets/javascripts/webui/content-selector-filters.js
+++ src/api/app/assets/javascripts/webui/content-selector-filters.js
@@ -1,38 +1,38 @@
-$(document).ready(function(){
-  function highlightSelectedFilters() {
-    var filters = $('#content-selector-filters .accordion .accordion-item');
-    filters.each(function() {
-      var currentFilter = $(this);
-      var selectedContentWrapper = currentFilter.find('.selected-content');
-      if (selectedContentWrapper.length > 0) {
-        var anySelected = [];
-        $(this).find('input').each(function() {
-          if ($(this).is(':checked')) {
-            anySelected.push($(this).next('label').html());
-          }
-        });
-        if (anySelected.length > 0) {
-          currentFilter.find('.selected-content').html(anySelected.join(', '));
+function highlightSelectedFilters() {
+  var filters = $('#content-selector-filters .accordion .accordion-item');
+  filters.each(function() {
+    var currentFilter = $(this);
+    var selectedContentWrapper = currentFilter.find('.selected-content');
+    if (selectedContentWrapper.length > 0) {
+      var anySelected = [];
+      $(this).find('input').each(function() {
+        if ($(this).is(':checked')) {
+          anySelected.push($(this).next('label').html());
         }
-        else {
-          currentFilter.find('.selected-content').text("");
-        }
+      });
+      if (anySelected.length > 0) {
+        currentFilter.find('.selected-content').html(anySelected.join(', '));
       }
-    });
-  }
-  highlightSelectedFilters();
-
-  function submitFilters() {
-    $('#content-selector-filters-form').submit();
-    $('#content-selector-filters input').attr('disabled', 'disabled');
-    $('.content-list').hide();
-    $('.content-list-loading').removeClass('d-none');
-  }
-  let submitFiltersTimeout;
-
-  $(document).on('change keyup', '#content-selector-filters-form input, #content-selector-filters-form select', function() {
-    highlightSelectedFilters();
-    window.clearTimeout(submitFiltersTimeout);
-    submitFiltersTimeout = window.setTimeout(submitFilters, 2000);
+      else {
+        currentFilter.find('.selected-content').text("");
+      }
+    }
   });
+}
+function submitFilters() {
+  $('#content-selector-filters-form').submit();
+  $('#content-selector-filters input').attr('disabled', 'disabled');
+  $('.content-list').hide();
+  $('.content-list-loading').removeClass('d-none');
+}
+
+let submitFiltersTimeout;
+$(document).on('change keyup', '#content-selector-filters-form input, #content-selector-filters-form select', function() {
+  highlightSelectedFilters();
+  window.clearTimeout(submitFiltersTimeout);
+  submitFiltersTimeout = window.setTimeout(submitFilters, 2000);
+});
+
+$(document).ready(function(){
+  highlightSelectedFilters();
 });
