8839ad0118339aefb5b9a4b462128f5241e24f76
Lukas Krause <lkrause@suse.de>
2025-01-20 14:38:31 +0100

    Show global project labels only to users in the beta program

--- src/api/app/assets/javascripts/webui/project.js
+++ src/api/app/assets/javascripts/webui/project.js
@@ -26,6 +26,25 @@ function initializeProjectDatatable() { // jshint ignore:line
       }, "responsive" : true,
       "columns": [
         { "data": "name" },
+        { "data": "title" }
+      ], "dom": "ftpi"
+    }
+  );
+  $(".toggle-projects").click(function() { toggleProjectsDatatable(); });
+}
+
+function initializeProjectDatatableLabelBeta() { // jshint ignore:line
+  initializeRemoteDatatable( // jshint ignore:line
+    "#projects-datatable",
+    {
+      "ajax": {
+        "url": $("#projects-datatable").data("source"),
+        "data": function (d) {
+          d.all = $("#projects-datatable").data("all");
+        }
+      }, "responsive" : true,
+      "columns": [
+        { "data": "name" },
         { "data": "labels"},
         { "data": "title" }
       ], "dom": "ftpi"
