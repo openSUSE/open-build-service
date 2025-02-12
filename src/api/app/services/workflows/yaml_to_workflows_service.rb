65f775a8df9af8b94a64765140aac599655c1143
Rubhan Azeem <rubhanazeem@gmail.com>
2025-02-12 12:18:18 +0100

    Add support for 'LABEL' placeholder variable

--- src/api/app/services/workflows/yaml_to_workflows_service.rb
+++ src/api/app/services/workflows/yaml_to_workflows_service.rb
@@ -3,7 +3,7 @@ module Workflows
     include WorkflowPlaceholderVariablesInstrumentation # for track_placeholder_variables
 
     # If the order of the values in this constant change, do not forget to change the mapping of the placeholder variable values
-    SUPPORTED_PLACEHOLDER_VARIABLES = %i[SCM_ORGANIZATION_NAME SCM_REPOSITORY_NAME SCM_PR_NUMBER SCM_COMMIT_SHA].freeze
+    SUPPORTED_PLACEHOLDER_VARIABLES = %i[SCM_ORGANIZATION_NAME SCM_REPOSITORY_NAME SCM_PR_NUMBER SCM_COMMIT_SHA LABEL].freeze
 
     def initialize(yaml_file:, scm_webhook:, token:, workflow_run:)
       @yaml_file = yaml_file
@@ -42,11 +42,12 @@ module Workflows
       pr_number = @scm_webhook.payload.fetch(:pr_number, 'NO_PR_NUMBER')
 
       commit_sha = @scm_webhook.payload.fetch(:commit_sha)
+      label = @scm_webhook.payload.fetch(:label)
 
       track_placeholder_variables(workflow_configuration)
 
       # Mapping the placeholder variables to their values from the webhook event payload
-      placeholder_variables = SUPPORTED_PLACEHOLDER_VARIABLES.zip([scm_organization_name, scm_repository_name, pr_number, commit_sha]).to_h
+      placeholder_variables = SUPPORTED_PLACEHOLDER_VARIABLES.zip([scm_organization_name, scm_repository_name, pr_number, commit_sha, label]).to_h
       begin
         format(workflow_configuration, placeholder_variables)
       rescue ArgumentError => e
