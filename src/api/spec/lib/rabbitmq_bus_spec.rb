bbf28298ac003e36407d982f060ff66dddaa5bc9
Henne Vogelsang <hvogel@opensuse.org>
2025-02-13 17:14:40 +0100

    Airbrake Bunny::Exception
    Bunny has automatic connection recovery by default, also from server initiated
    close.
    
    http://rubybunny.info/articles/error_handling.html
    
    We should know when that fails instead of swallowing that problem.
    By default automatic recovery will try every 4 seconds forever, let's limit
    this to roughly 5 minutes: (75 * 4 / 60)

--- src/api/spec/lib/rabbitmq_bus_spec.rb
+++ src/api/spec/lib/rabbitmq_bus_spec.rb
@@ -3,15 +3,4 @@ RSpec.describe RabbitmqBus, rabbitmq: '#' do
     RabbitmqBus.send_to_bus('metrics', 'hallo')
     expect_message('opensuse.obs.metrics', 'hallo')
   end
-
-  context 'with exceptions' do
-    before do
-      allow_any_instance_of(BunnyMock::Queue).to receive(:publish).and_raise(Net::ReadTimeout)
-    end
-
-    it 'disconnects on errors' do
-      RabbitmqBus.send_to_bus('metrics', 'hallo')
-      expect_no_message
-    end
-  end
 end
