ee5620cadd812beb2b8974eab342526ad0d85bcc
Henne Vogelsang <hvogel@opensuse.org>
2025-02-14 17:50:36 +0100

    Introduces Event::Group
    To DRY AddedUserToGroup / RemovedUserFromGroup

---
+++ src/api/app/models/event/group.rb
@@ -0,0 +1,25 @@
+module Event
+  class Group < Base
+    self.abstract_class = true
+    payload_keys :group, :member, :who
+    receiver_roles :member
+
+    def subject
+      raise AbstractMethodCalled
+    end
+
+    def members
+      [User.find_by(login: payload['member'])]
+    end
+
+    def originator
+      payload_address('who')
+    end
+
+    def parameters_for_notification
+      super.merge({ notifiable_type: 'Group',
+                    notifiable_id: ::Group.find_by(title: payload['group']).id,
+                    type: 'NotificationGroup' })
+    end
+  end
+end
