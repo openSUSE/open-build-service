b160c11f5fad5a7cdc843f8a9b5b6cb5a8760861
Michael Schroeder <mls@suse.de>
2025-03-14 14:12:42 +0100

    [backend] logging: always log the process pid
    We now use a sub-pid when there is a reqid (AJAX) or we have
    a keepalive request.

--- src/backend/BSServer.pm
+++ src/backend/BSServer.pm
@@ -671,7 +671,8 @@ sub parse_error_string {
 
 sub request_infostr {
   my ($req) = @_;
-  my $id = $req->{'reqid'} || $$;
+  my $id = $req->{'reqid'} || $req->{'keepalive_count'};
+  $id = $id ? "$$.$id" : $$;
   return sprintf("%s: %3ds %-7s %-22s %s%s\n", BSUtil::isotime($req->{'starttime'}), time() - $req->{'starttime'}, "[$id]",
       "$req->{'action'} ($req->{'peer'})", $req->{'path'}, ($req->{'query'}) ? "?$req->{'query'}" : '');
 }
