d5fd88a9dd7d0df0d0232f34f58638e9c9af5a4a
Eduardo Navarro <enavarro@suse.com>
2025-03-21 13:41:55 +0100

    Move user permissions checks to a before filter

--- src/api/app/controllers/person_controller.rb
+++ src/api/app/controllers/person_controller.rb
@@ -9,6 +9,7 @@ class PersonController < ApplicationController
   skip_before_action :require_login, only: %i[command register]
 
   before_action :set_user, only: %i[post_userinfo change_my_password watchlist put_watchlist]
+  before_action :user_permission_check, only: [:post_userinfo]
 
   def show
     @list = if params[:prefix]
@@ -41,12 +42,6 @@ class PersonController < ApplicationController
   end
 
   def post_userinfo
-    authorize @user, :update?
-
-    login = params[:login]
-    # just for permission checking
-    User.find_by_login!(login)
-
     if params[:cmd] == 'change_password'
       login ||= User.session.login
       password = request.raw_post.to_s.chomp
@@ -219,6 +214,14 @@ class PersonController < ApplicationController
 
   def set_user
     @user = User.find_by(login: params[:login])
+  end
+
+  def user_permission_check
+    authorize @user, :update?
+
+    login = params[:login]
+    # just for permission checking
+    User.find_by_login!(login)
   end
 
   def update_watchlist(user, xml)
