openSUSE/open-build-service

commit e18fbe134eeec11ee6a6642f8db35980fce63203

[backend] rename 'partition' to 'client' in /lastevents
The lasteventsproxy already uses client.

src/backend/BSSched/EventSource/RemoteWatcher.pm
@@ -72,15 +72,14 @@ sub new {
     'formurlencode' => 1,
   };
   my @args;
+  push @args, $BSConfig::partition ? "client=$BSConfig::partition/$myarch" : "client=$myarch" if $remoteurl eq $BSConfig::srcserver;
   if ($uselasteventsproxy && $remoteurl ne $BSConfig::srcserver) {
     $param->{'uri'} = "$BSConfig::srcserver/lasteventsproxy";
-    push @args, $BSConfig::partition ? "client=$BSConfig::partition/$myarch" : "client=$myarch";
     push @args, "remoteurl=$remoteurl";
   } else {
     $param->{'proxy'} = $conf{'remoteproxy'};
     my $obsname = $conf{'obsname'};
-    push @args, "partition=$BSConfig::partition" if $remoteurl eq $BSConfig::srcserver && $BSConfig::partition;
-    push @args, "obsname=$obsname/$myarch" if $obsname;
+    push @args, "obsname=$obsname/$myarch" if $obsname && $remoteurl ne $BSConfig::srcserver;
   }
   push @args, map {"filter=$_"} @filter;
   push @args, "start=$start" if $start;

src/backend/bs_srcserver
@@ -6388,10 +6388,11 @@ sub lastevents {
   my $block = !exists($cgi->{'block'}) || $cgi->{'block'};
   if (!$BSStdServer::isajax && $block) {
     my @args;
+    push @args, "client=$cgi->{'client'}" if $cgi->{'client'};
     push @args, "obsname=$cgi->{'obsname'}" if $cgi->{'obsname'};
     push @args, map {"filter=$_"} @{$filter || []};
     push @args, "start=$cgi->{'start'}";
-    if ($cgi->{'partition'}) {
+    if ($cgi->{'client'}) {
       BSHandoff::handoff_part('lastevents', '/lastevents', undef, @args);
     } else {
       BSHandoff::handoff_part('interconnect_in_lastevents', '/lastevents', undef, @args);
@@ -7959,14 +7960,14 @@ my $dispatches = [
   '/getsignkey $project withpubkey:bool? autoextend:bool? withalgo:bool? signtype:? signflavor:?' => \&getsignkey,
 
   # event/notification handling
-  '!- /lastevents $filter:* start:num? obsname:? block:bool?' => \&lastevents,
+  '!- /lastevents $filter:* start:num? obsname:? client:? block:bool?' => \&lastevents,
   '!- /lasteventsproxy $filter:* start:num? remoteurl: client:?' => \&lasteventsproxy,
   '/lastnotifications start:num? view:? block:bool? noprune:bool?' => \&lastnotifications,
   '/notificationpayload/$payloadkey:filename' => \&getnotificationpayload,
   'DELETE:/notificationpayload/$payloadkey:filename' => \&deletenotificationpayload,
   'POST:/event type: project: package:? repository:? arch:? job:?' => \&newevent,
   # tmp until lightty gets fixed
-  '/public/lastevents $filter:* start:num? obsname:? block:bool?' => \&lastevents,
+  '/public/lastevents $filter:* start:num? obsname:? block:bool? client:?' => \&lastevents,
 
   # search interface
   '/search $in: $match: return:? values:bool?' => \&search,
@@ -8084,7 +8085,7 @@ my $dispatches_ajax = [
   '/getbinaries $project $repository $arch binaries: nometa:bool? raw:bool? now:num? module*' => \&worker_getbinaries,
   '/getbinaryversions $project $repository $arch binaries: nometa:bool? now:num? module* withevr:bool?' => \&worker_getbinaryversions,
   '/getconfig $project $repository path:prp* workerid?' => \&getbuildconfig_ajax,
-  '/lastevents $filter:* start:num? obsname:? partition:?' => \&lastevents,
+  '/lastevents $filter:* start:num? obsname:? client:?' => \&lastevents,
   '/lasteventsproxy $filter:* start:num? remoteurl: client:?' => \&lasteventsproxy,
   '/lastnotifications start:num? view:? block:bool?' => \&lastnotifications,
   '/source/$project/$package cmd=waitservice servicemark:' => \&waitservicerun,

commit 57ad5b906a81a91a599aadc0cbfa0d1c2082fbf1

[backend] fix client no longer be sent to the lasteventsproxy
Broken by accident with commit e18fbe134eeec11ee6a6642f8db35980fce63203

src/backend/BSSched/EventSource/RemoteWatcher.pm
@@ -71,26 +71,25 @@ sub new {
     'request' => 'POST',
     'formurlencode' => 1,
   };
+  my $client = $BSConfig::partition ? "$BSConfig::partition/$myarch" : $myarch;
   my @args;
-  push @args, $BSConfig::partition ? "client=$BSConfig::partition/$myarch" : "client=$myarch" if $remoteurl eq $BSConfig::srcserver;
-  if ($uselasteventsproxy && $remoteurl ne $BSConfig::srcserver) {
+  if ($remoteurl eq $BSConfig::srcserver) {
+    push @args, "client=$client";
+  } elsif ($uselasteventsproxy) {
     $param->{'uri'} = "$BSConfig::srcserver/lasteventsproxy";
+    push @args, "client=$client";
     push @args, "remoteurl=$remoteurl";
   } else {
     $param->{'proxy'} = $conf{'remoteproxy'};
-    my $obsname = $conf{'obsname'};
-    push @args, "obsname=$obsname/$myarch" if $obsname && $remoteurl ne $BSConfig::srcserver;
+    push @args, "obsname=$conf{'obsname'}/$myarch" if $conf{'obsname'};
   }
   push @args, map {"filter=$_"} @filter;
   push @args, "start=$start" if $start;
-  my $ret;
-  eval {
-    $ret = BSRPC::rpc($param, $BSXML::events, @args);
-  };
+  my $ret = eval { BSRPC::rpc($param, $BSXML::events, @args) };
   if ($@) {
     warn($@);
     print "retrying in 60 seconds\n";
-    $ret = {'retry' => time() + 60};
+    $ret = { 'retry' => time() + 60 };
   } else {
     $ret = { 'rpc' => $ret, 'socket' => $ret->{'socket'} };
   }