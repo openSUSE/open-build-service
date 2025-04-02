91fbda06050bec705b61cc4f1df96195bf759eff
Eduardo Navarro <enavarro@suse.com>
2025-04-02 16:05:19 +0200

    Linters blank lines in initializer
    Co-authored-by: Dario Leidi <dleidi@suse.com>

--- src/api/config/initializers/new_framework_defaults_7_0.rb
+++ src/api/config/initializers/new_framework_defaults_7_0.rb
@@ -92,7 +92,6 @@
 #   "Referrer-Policy" => "strict-origin-when-cross-origin"
 # }
 
-
 # ** Please read carefully, this must be configured in config/application.rb **
 # Change the format of the cache entry.
 # Changing this default means that all new cache entries added to the cache
@@ -101,7 +100,6 @@
 # and you have no plans to rollback.
 # When you're ready to change format, add this to `config/application.rb` (NOT this file):
 #  config.active_support.cache_format_version = 7.0
-
 
 # Cookie serializer: 2 options
 #
