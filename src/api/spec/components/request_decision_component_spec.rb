6b8ca36268407a34d7f14ca8004ce2886196f13a
Daniel Donisa <dani.doni@gmail.com>
2025-04-10 10:01:23 +0200

    Improve the request decision component spec

--- src/api/spec/components/request_decision_component_spec.rb
+++ src/api/spec/components/request_decision_component_spec.rb
@@ -4,18 +4,22 @@ RSpec.describe RequestDecisionComponent, :vcr, type: :component do
   let(:target_package) { create(:package, name: 'target_package', project: target_project) }
   let(:source_project) { create(:project, :as_submission_source, name: 'source_project') }
   let(:source_package) { create(:package, name: 'source_package', project: source_project) }
+  let(:package_maintainers) do
+    distinct_bs_request_actions = submit_request.bs_request_actions.select(:target_project, :target_package).distinct
+    distinct_bs_request_actions.flat_map do |action|
+      Package.find_by_project_and_name(action.target_project, action.target_package).try(:maintainers)
+    end.compact.uniq
+  end
+  let(:another_target_project) { create(:project, name: 'another_target_project') }
+  let(:another_target_package) { create(:package, name: 'another_target_package', project: another_target_project) }
+  let(:devel_project) { create(:project, name: 'devel_project', maintainer: maintainer) }
+  let(:devel_package) { create(:package, name: 'devel_package', project: devel_project) }
 
   context 'when we cannot forward the request nor make the creator a maintainer' do
     let(:submit_request) do
       create(:bs_request_with_submit_action,
              target_package: target_package,
              source_package: source_package)
-    end
-    let(:package_maintainers) do
-      distinct_bs_request_actions = submit_request.bs_request_actions.select(:target_project, :target_package).distinct
-      distinct_bs_request_actions.flat_map do |action|
-        Package.find_by_project_and_name(action.target_project, action.target_package).try(:maintainers)
-      end.compact.uniq
     end
 
     before do
@@ -35,12 +39,6 @@ RSpec.describe RequestDecisionComponent, :vcr, type: :component do
       create(:bs_request_with_submit_action,
              target_package: target_package,
              source_package: source_package)
-    end
-    let(:package_maintainers) do
-      distinct_bs_request_actions = submit_request.bs_request_actions.select(:target_project, :target_package).distinct
-      distinct_bs_request_actions.flat_map do |action|
-        Package.find_by_project_and_name(action.target_project, action.target_package).try(:maintainers)
-      end.compact.uniq
     end
 
     before do
@@ -62,31 +60,25 @@ RSpec.describe RequestDecisionComponent, :vcr, type: :component do
   end
 
   context 'when we can forward the request to the developed project' do
-    let(:another_target_project) { create(:project, name: 'another_target_project') }
-    let(:another_target_package) { create(:package, name: 'another_target_package', project: another_target_project) }
-    let(:devel_project1) { create(:project, name: 'devel_project1', maintainer: maintainer) }
-    let(:devel_package1) { create(:package, name: 'devel_package1', project: devel_project1) }
     let(:submit_request) do
       create(:bs_request_with_submit_action,
              creator: maintainer,
-             target_package: devel_package1,
+             target_package: devel_package,
              source_package: source_package)
     end
-    let(:package_maintainers) do
-      distinct_bs_request_actions = submit_request.bs_request_actions.select(:target_project, :target_package).distinct
-      distinct_bs_request_actions.flat_map do |action|
-        Package.find_by_project_and_name(action.target_project, action.target_package).try(:maintainers)
-      end.compact.uniq
-    end
 
-    before do
-      User.session = maintainer
+    let!(:another_action) do
       another_action = submit_request.bs_request_actions.last
       another_action.target_project_object = another_target_project
       another_action.target_package_object = another_target_package
       another_action.save!
-      target_package.update(develpackage: devel_package1)
-      another_target_package.update(develpackage: devel_package1)
+      another_action
+    end
+
+    before do
+      target_package.update(develpackage: devel_package)
+      another_target_package.update(develpackage: devel_package)
+      User.session = maintainer
       render_inline(described_class.new(bs_request: submit_request, package_maintainers: package_maintainers, show_project_maintainer_hint: true))
     end
 
@@ -104,26 +96,16 @@ RSpec.describe RequestDecisionComponent, :vcr, type: :component do
   end
 
   context 'when we can forward the request and make the creator a maintainer' do
-    let(:another_target_project) { create(:project, name: 'another_target_project') }
-    let(:another_target_package) { create(:package, name: 'another_target_package', project: another_target_project) }
-    let(:devel_project) { create(:project, name: 'devel_project', maintainer: maintainer) }
-    let(:devel_package) { create(:package, name: 'devel_package', project: devel_project) }
     let(:submit_request) do
       create(:bs_request_with_submit_action,
              target_package: devel_package,
              source_package: source_package)
     end
-    let(:package_maintainers) do
-      distinct_bs_request_actions = submit_request.bs_request_actions.select(:target_project, :target_package).distinct
-      distinct_bs_request_actions.flat_map do |action|
-        Package.find_by_project_and_name(action.target_project, action.target_package).try(:maintainers)
-      end.compact.uniq
-    end
 
     before do
-      User.session = maintainer
       target_package.update(develpackage: devel_package)
       another_target_package.update(develpackage: devel_package)
+      User.session = maintainer
       render_inline(described_class.new(bs_request: submit_request, package_maintainers: package_maintainers, show_project_maintainer_hint: true))
     end
 
