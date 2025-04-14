7f9a81432064c893a70fe6b541e4167093ffa525
Lukas Krause <lkrause@suse.de>
2025-04-14 16:01:53 +0200

    Ignore `user_id` column in report model
    In order to safely drop the column later on, we first need to
    ignore it.
    See https://github.com/ankane/strong_migrations?tab=readme-ov-file#removing-a-column

--- src/api/app/models/report.rb
+++ src/api/app/models/report.rb
@@ -13,8 +13,7 @@ class Report < ApplicationRecord
   validates :reportable_type, length: { maximum: 255 }
   validates :reportable, presence: true, on: :create
 
-  # TODO: Remove user association as soon as `reporter` is fully established
-  belongs_to :user, optional: true
+  self.ignored_columns += [:user_id] # TODO: Remove once user_id column is dropped
   belongs_to :reporter, class_name: 'User', optional: false
   belongs_to :reportable, polymorphic: true, optional: true
   has_many :comments, as: :commentable, dependent: :destroy
