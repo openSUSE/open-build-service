49bd9955b25161cadb085efabdef9f6287518bd5
Daniel Donisa <dani.doni@gmail.com>
2025-05-08 11:11:30 +0200

    Happy path spec to cover the creation of assignments from the package page

--- src/api/spec/features/beta/webui/assignments_spec.rb
+++ src/api/spec/features/beta/webui/assignments_spec.rb
@@ -1,17 +1,18 @@
 require 'browser_helper'
 
 RSpec.describe 'Assignments', :vcr do
+  let!(:user) { create(:confirmed_user, :with_home, login: 'tom') }
+  let!(:package) { create(:package_with_file, name: 'test_package', project: user.home_project) }
+  let(:assignee) { create(:confirmed_user, login: 'mal') }
+
   before do
     Flipper.enable(:foster_collaboration)
+    login(user)
   end
 
   describe 'showing the assigment of a package' do
-    let!(:user) { create(:confirmed_user, :with_home, login: 'tom') }
-    let!(:package) { create(:package_with_file, name: 'test_package', project: user.home_project) }
-
     context 'when having no assignment' do
       it 'shows nothing' do
-        login(user)
         visit package_show_path(user.home_project, package)
         expect(page).to have_css('ul.side_links')
         expect(page).to(have_no_text('Assigned'))
@@ -19,14 +20,23 @@ RSpec.describe 'Assignments', :vcr do
     end
 
     context 'when having an assignment' do
-      let(:assignee) { create(:confirmed_user, login: 'mal') }
-
       it 'shows the login name of the assigned user' do
         create(:assignment, assignee: assignee, package: package)
-        login(user)
         visit package_show_path(user.home_project, package)
-        expect(page).to have_text('Assigned to: mal')
+        expect(page).to have_text('Assigned to:')
       end
+    end
+  end
+
+  describe 'creating an assignment for a package' do
+    before { assignee }
+
+    it 'creates an assignment for the package' do
+      visit package_show_path(user.home_project, package)
+      click_button('Assign someone')
+      fill_in('assignments_search', with: 'mal')
+      click_button('Assign')
+      expect(page).to have_text('Assigned to:')
     end
   end
 end
