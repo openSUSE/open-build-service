229939bfb7d510865128e61dd7e6de6ba5d7dbdc
Eduardo Navarro <enavarro@suse.com>
2025-05-09 08:42:03 +0200

    Introduce missing check for "labels" flag in labels' policies

--- src/api/app/policies/label_policy.rb
+++ src/api/app/policies/label_policy.rb
@@ -1,9 +1,13 @@
 class LabelPolicy < ApplicationPolicy
   def index?
+    return false unless Flipper.enabled?(:labels, user)
+
     true
   end
 
   def create?
+    return false unless Flipper.enabled?(:labels, user)
+
     Pundit.policy!(user, record.labelable).update_labels?
   end
 
@@ -12,6 +16,8 @@ class LabelPolicy < ApplicationPolicy
   end
 
   def update?
+    return false unless Flipper.enabled?(:labels, user)
+
     Pundit.policy!(user, record).update_labels? # record is a labelable, can be a package or a request
   end
 end
