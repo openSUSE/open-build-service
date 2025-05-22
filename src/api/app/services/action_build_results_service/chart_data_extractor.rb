460f8b09a2f6f11e7d7ea5f1eab8767f6b27ca4b
Rubhan Azeem <rubhanazeem@gmail.com>
2025-05-22 13:51:35 +0200

    Sort build results so that repositories matching the target project appear at the top of the list

--- src/api/app/services/action_build_results_service/chart_data_extractor.rb
+++ src/api/app/services/action_build_results_service/chart_data_extractor.rb
@@ -9,12 +9,17 @@ module ActionBuildResultsService
     def call
       return [] if @actions.blank?
 
-      @actions.where(type: %i[submit maintenance_incident maintenance_release]).map do |action|
+      @actions.where(type: %i[submit maintenance_incident maintenance_release]).filter_map do |action|
         sources = sources_from_action(action)
         next unless sources[:source_project].present? && sources[:source_package].present?
 
-        package_build_results(sources[:source_package], sources[:source_project])
-      end.flatten.compact
+        source_build_results = package_build_results(sources[:source_package], sources[:source_project])
+
+        target_project = Project.find(action.target_project_id) if BsRequest.find(action.bs_request_id).staging_project_id.nil?
+        next source_build_results if target_project.nil?
+
+        sort_build_results(source_build_results, target_project.repositories)
+      end.flatten
     end
 
     private
@@ -32,8 +37,8 @@ module ActionBuildResultsService
       { source_project: source_project, source_package: source_package }
     end
 
-    def package_build_results(source_package, source_project)
-      results = source_package.buildresult(source_project, show_all: true).results
+    def package_build_results(package, project)
+      results = package.buildresult(project, show_all: true).results
       results.flat_map do |pkg, build_results|
         build_results.map do |result|
           {
@@ -41,13 +46,25 @@ module ActionBuildResultsService
             repository: result.repository,
             status: result.code,
             package_name: pkg,
-            project_name: source_project.name,
+            project_name: project.name,
             repository_status: result.state,
             is_repository_in_db: result.is_repository_in_db,
             details: result.details
           }
         end
       end
+    end
+
+    # Sort build results so that repositories matching the target project appear at the top of the list
+    def sort_build_results(source_build_results, target_project_repositories)
+      return source_build_results if target_project_repositories.blank?
+
+      target_repos = target_project_repositories.pluck(:name)
+      matching, non_matching = source_build_results.partition do |result|
+        target_repos.include?(result[:repository])
+      end
+      matching_sorted = matching.sort_by { |r| r[:repository] }
+      matching_sorted + non_matching
     end
   end
 end
