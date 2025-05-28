75fc7a6d8e6882fe74e51411d6212cacbb6837b1
Adrian Schröter <adrian@suse.de>
2025-05-28 14:46:25 +0200

    [api] fix behaviour on project meta change of scmsync project
    We must only decline requests using the project as target,
    but not as source.
    
    Otherwise any random meta change leads to revoke requests when
    a project meta change happens.

--- src/api/app/queries/open_requests_with_project_as_source_or_target_finder.rb
+++ src/api/app/queries/open_requests_with_project_as_source_or_target_finder.rb
@@ -1,9 +1,4 @@
 class OpenRequestsWithProjectAsSourceOrTargetFinder < OpenRequestsFinder
-  def initialize(relation, project_name)
-    @relation = relation
-    @project_name = project_name
-  end
-
   def requests_finder
     @relation.where('bs_request_actions.source_project = ? or bs_request_actions.target_project = ?', @project_name, @project_name)
   end
