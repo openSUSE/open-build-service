1f74e5ec438b7aa96aa07cc564c7026e47158829
Saray Cabrera Padrón <scabrerapadron@suse.de>
2025-06-24 00:01:41 +0200

    Use #accepts_more_values? in another condition

--- src/api/app/controllers/webui/attribute_controller.rb
+++ src/api/app/controllers/webui/attribute_controller.rb
@@ -34,7 +34,7 @@ class Webui::AttributeController < Webui::WebuiController
 
     value_count = @attribute.attrib_type.value_count
     values_length = @attribute.values.length
-    (value_count - values_length).times { @attribute.values.build(attrib: @attribute) } if value_count && (value_count > values_length)
+    (value_count - values_length).times { @attribute.values.build(attrib: @attribute) } if value_count && @attribute.accepts_more_values?
 
     @issue_trackers = IssueTracker.order(:name).all if @attribute.attrib_type.issue_list
 
