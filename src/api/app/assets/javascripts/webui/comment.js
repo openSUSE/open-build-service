6bcb07ded5537abdcaddc4858579e523748073ad
ncounter <dleidi@suse.com>
2025-06-30 14:45:35 +0200

    Drop no longer used .comment-field class
    All textarea for comments are now generated in the child-tree of a
    `.write-and-preview` HTML element from the component.

--- src/api/app/assets/javascripts/webui/comment.js
+++ src/api/app/assets/javascripts/webui/comment.js
@@ -30,10 +30,7 @@ function validateForm(e) {
 
 function handlingCommentEvents() {
   // Disable submit button if textarea is empty and enable otherwise
-  const commentFieldSelector = `.comments-list .comment-field, .comment_new .comment-field,
-                                .timeline .comment-field, .diff .comment-field,
-                                .diff-accordion .comment-field, .write-and-preview textarea`;
-  $(document).on('input', commentFieldSelector, function(e) {
+  $(document).on('input', '.write-and-preview textarea', function(e) {
     validateForm(e);
     resizeTextarea(this);
   });
