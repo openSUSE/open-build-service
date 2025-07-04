6a099734cbab5be955e462b1feff3f5f4fe81a8e
Saray Cabrera Padrón <scabrerapadron@suse.de>
2025-07-04 16:16:47 +0200

    Stop creating the values upfront

--- src/api/spec/models/issue_spec.rb
+++ src/api/spec/models/issue_spec.rb
@@ -16,13 +16,13 @@ RSpec.describe Issue do
   end
 
   describe 'validate' do
-    let!(:issue_tracker) { create(:issue_tracker) }
-    let!(:issue_tracker_v1) { create(:issue_tracker, name: 'v1_tracker', regex: '([BD]-[\d]+)', label: '(B-@@@)') }
-    let!(:issue_tracker_cve) { create(:issue_tracker, name: 'cve_tracker', regex: '^(?:cve|CVE)-(\d\d\d\d-\d+)', label: 'CVE-@@@') }
+    let(:issue_tracker) { create(:issue_tracker) }
+    let(:issue_tracker_v1) { create(:issue_tracker, name: 'v1_tracker', regex: '([BD]-[\d]+)', label: '(B-@@@)') }
+    let(:issue_tracker_cve) { create(:issue_tracker, name: 'cve_tracker', regex: '^(?:cve|CVE)-(\d\d\d\d-\d+)', label: 'CVE-@@@') }
 
-    let!(:issue) { create(:issue, name: '1234', issue_tracker: issue_tracker) }
-    let!(:issue_v1) { create(:issue, name: '1234', issue_tracker: issue_tracker_v1) }
-    let!(:issue_cve) { build(:issue, name: 'CVE-2019-12345', issue_tracker: issue_tracker_cve) }
+    let(:issue) { create(:issue, name: '1234', issue_tracker: issue_tracker) }
+    let(:issue_v1) { create(:issue, name: '1234', issue_tracker: issue_tracker_v1) }
+    let(:issue_cve) { build(:issue, name: 'CVE-2019-12345', issue_tracker: issue_tracker_cve) }
 
     it 'issue name should be valid' do
       expect(issue).to be_valid
