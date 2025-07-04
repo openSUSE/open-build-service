431b9108115209c22861dc0104456dd1167dcaed
Saray Cabrera Padrón <scabrerapadron@suse.de>
2025-07-04 17:13:42 +0200

    Add relevant tests for issue name validation
    Rely on the issue tracker seeded in database. This way we always use the
    most realistic version of regex.

--- src/api/spec/models/issue_spec.rb
+++ src/api/spec/models/issue_spec.rb
@@ -18,11 +18,13 @@ RSpec.describe Issue do
   describe 'name_validation' do
     let(:issue_tracker) { create(:issue_tracker) }
     let(:issue_tracker_v1) { create(:issue_tracker, name: 'v1_tracker', regex: '([BD]-[\d]+)', label: '(B-@@@)') }
-    let(:issue_tracker_cve) { create(:issue_tracker, name: 'cve_tracker', regex: '^(?:cve|CVE)-(\d\d\d\d-\d+)', label: 'CVE-@@@') }
+    let(:issue_tracker_cve) { IssueTracker.find_by(name: 'cve') } # Seeded in database. Regex: 'CVE-(\d\d\d\d-\d+)' label: 'CVE-@@@'
 
     let(:issue) { create(:issue, name: '1234', issue_tracker: issue_tracker) }
     let(:issue_v1) { create(:issue, name: '1234', issue_tracker: issue_tracker_v1) }
-    let(:issue_cve) { build(:issue, name: 'CVE-2019-12345', issue_tracker: issue_tracker_cve) }
+    let(:issue_cve_no_prefix) { build(:issue, name: '2019-12345', issue_tracker: issue_tracker_cve) }
+    let(:issue_cve_uppercase) { build(:issue, name: 'CVE-2019-12345', issue_tracker: issue_tracker_cve) }
+    let(:issue_cve_lowercase) { build(:issue, name: 'cve-2019-12345', issue_tracker: issue_tracker_cve) }
 
     it 'issue name should be valid' do
       expect(issue).to be_valid
@@ -32,8 +34,18 @@ RSpec.describe Issue do
       expect(issue_v1).to be_valid
     end
 
-    it 'CVE-XXXX-YYYY should be an invalid name' do
-      expect { issue_cve.save! }.to raise_error(ActiveRecord::RecordInvalid, /does not match defined regex/)
+    it 'CVE issue with pattern XXXX-YYYY should be valid name' do
+      expect(issue_cve_no_prefix).to be_valid
+    end
+
+    # TODO: this pattern shouldn't be valid at creation time
+    it 'CVE issue with pattern CVE-XXXX-YYYY should be valid name' do
+      expect(issue_cve_uppercase).to be_valid
+    end
+
+    it 'cve-XXXX-YYYY should be an invalid name' do
+      expect(issue_cve_lowercase).not_to be_valid
+      expect(issue_cve_lowercase.errors.full_messages).to contain_exactly("Name with value '#{issue_cve_lowercase.name}' does not match defined regex #{issue_tracker_cve.regex}")
     end
   end
 end
