1dd43eaad7c870a3a8add6d70610a53aff8d2d4b
Eduardo Navarro <enavarro@suse.com>
2025-07-08 15:27:53 +0200

    Add `status` field to table `bs_requests`
    This is the first step to change the type of the field `state` from
    `string` to `enum`.

--- src/api/db/schema.rb
+++ src/api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_12_085047) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_08_130643) do
   create_table "active_storage_attachments", id: :integer, charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -277,10 +277,12 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_12_085047) do
     t.datetime "updated_when", precision: nil
     t.string "approver"
     t.integer "staging_project_id"
+    t.integer "status"
     t.index ["creator"], name: "index_bs_requests_on_creator"
     t.index ["number"], name: "index_bs_requests_on_number", unique: true
     t.index ["staging_project_id"], name: "index_bs_requests_on_staging_project_id"
     t.index ["state"], name: "index_bs_requests_on_state"
+    t.index ["status"], name: "index_bs_requests_on_status"
     t.index ["superseded_by"], name: "index_bs_requests_on_superseded_by"
   end
 
