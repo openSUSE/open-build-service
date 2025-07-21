0cd562bb6858324b066a893187136634442920ca
Henne Vogelsang <hvogel@opensuse.org>
2025-07-21 15:40:43 +0200

    Use refactored Authenticator in Webui
    Instead of WebuiControllerService::UserChecker

--- src/api/app/controllers/webui/session_controller.rb
+++ src/api/app/controllers/webui/session_controller.rb
@@ -1,23 +1,18 @@
 class Webui::SessionController < Webui::WebuiController
-  before_action :authenticate_user, only: [:create]
-  before_action :check_user_active, only: [:create]
-
-  skip_before_action :check_anonymous, only: [:create]
-
+  skip_before_action :extract_user
   def new; end
 
   def create
-    session[:login] = @user.login
-    User.session = @user
+    user = User.find_with_credentials(params.fetch(:username, ''), params.fetch(:password, ''))
 
-    RabbitmqBus.send_to_bus('metrics', 'login,access_point=webui value=1')
+    raise AuthenticationFailed unless user
 
+    session[:login] = user.login
     redirect_on_login
   end
 
   def reset
     reset_session
-    User.session = nil
 
     RabbitmqBus.send_to_bus('metrics', 'logout,access_point=webui value=1')
 
@@ -26,25 +21,9 @@ class Webui::SessionController < Webui::WebuiController
 
   private
 
-  def check_user_active
-    return if @user.active?
-
-    RabbitmqBus.send_to_bus('metrics', 'login,access_point=webui,failure=disabled value=1')
-    redirect_to(root_path, error: 'Your account is disabled. Please contact the administrator for details.')
-  end
-
-  def authenticate_user
-    @user = User.find_with_credentials(params.fetch(:username, ''), params.fetch(:password, ''))
-
-    return if @user
-
-    RabbitmqBus.send_to_bus('metrics', 'login,access_point=webui,failure=unauthenticated value=1')
-    redirect_to(new_session_path, error: 'Authentication failed')
-  end
-
   def redirect_on_login
     if referer_was_login?
-      redirect_to user_path(User.session)
+      redirect_to user_path(session[:login])
     else
       redirect_back_or_to root_path
     end
