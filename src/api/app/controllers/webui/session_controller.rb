97ad8e096d63dd25b4ca37b1ccbbe39e46c8887b
Henne Vogelsang <hvogel@opensuse.org>
2025-07-22 15:45:58 +0200

    Refactor SessionControllerService::SessionCreator away
    A "service" model to call *one* method on User? What a waste...

--- src/api/app/controllers/webui/session_controller.rb
+++ src/api/app/controllers/webui/session_controller.rb
@@ -1,6 +1,5 @@
 class Webui::SessionController < Webui::WebuiController
-  before_action :session_creator, only: [:create]
-  before_action :authenticate, only: [:create]
+  before_action :authenticate_user, only: [:create]
   before_action :check_user_active, only: [:create]
 
   skip_before_action :check_anonymous, only: [:create]
@@ -8,34 +7,36 @@ class Webui::SessionController < Webui::WebuiController
   def new; end
 
   def create
-    User.session = @session_creator.user
-    session[:login] = @session_creator.user.login
+    session[:login] = @user.login
+    User.session = @user
+
     RabbitmqBus.send_to_bus('metrics', 'login,access_point=webui value=1')
+
     redirect_on_login
   end
 
   def destroy
     reset_session
-    RabbitmqBus.send_to_bus('metrics', 'logout,access_point=webui value=1')
     User.session = nil
+
+    RabbitmqBus.send_to_bus('metrics', 'logout,access_point=webui value=1')
+
     redirect_on_logout
   end
 
   private
 
-  def session_creator
-    @session_creator = SessionControllerService::SessionCreator.new(params.slice(:username, :password))
-  end
-
   def check_user_active
-    return if @session_creator.user.active?
+    return if @user.active?
 
     RabbitmqBus.send_to_bus('metrics', 'login,access_point=webui,failure=disabled value=1')
     redirect_to(root_path, error: 'Your account is disabled. Please contact the administrator for details.')
   end
 
-  def authenticate
-    return if @session_creator.valid? && @session_creator.exist?
+  def authenticate_user
+    @user = User.find_with_credentials(params.fetch(:username, ''), params.fetch(:password, ''))
+
+    return if @user
 
     RabbitmqBus.send_to_bus('metrics', 'login,access_point=webui,failure=unauthenticated value=1')
     redirect_to(new_session_path, error: 'Authentication failed')
