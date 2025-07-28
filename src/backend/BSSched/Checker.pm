openSUSE/open-build-service

commit 0a6aaed1f9c04c1cb904ea2bb4000206e5099a52

[backend] implement automatic timeout of project suspension
We drop suspensions after 8 hours.

src/backend/BSSched/Checker.pm
@@ -304,6 +304,35 @@ sub check_for_running_src_updates {
   return undef;
 }
 
+sub timeout_suspends {
+  my ($ctx) = @_;
+  my $prp = $ctx->{'prp'};
+  my $gctx = $ctx->{'gctx'};
+  my $projid = $ctx->{'project'};
+  my $suspend = $gctx->{'projsuspended'}->{$projid} || [];
+  my $cutoff = time() - 8 * 3600;
+  my $timedout = 0;
+  for (splice @$suspend) {
+    if ($_->{'time'} > $cutoff) {
+      $timedout++;
+      print "project suspension timed out: $_->{'job'}\n";
+    } else {
+      push @$suspend, $_;
+    }
+  }
+  if (@$suspend) {
+    if ($timedout) {
+      my @suspendjob = map {$_->{'job'}} @$suspend;
+      print "not yet resuming project $projid: @suspendjob\n";
+    }
+    return $suspend;
+  }
+  print "resuming project $projid\n";
+  delete $gctx->{'projsuspended'}->{$projid};
+  push @{$gctx->{'lookat_med'}}, grep {$_ ne $prp} @{$gctx->{'project_prps'}->{$projid} || []};
+  return undef;
+}
+
 sub setup {
   my ($ctx) = @_;
   my $prp = $ctx->{'prp'};
@@ -330,6 +359,7 @@ sub setup {
     return ('disabled', undef);
   }
   my $suspend = $gctx->{'projsuspended'}->{$projid};
+  $suspend = timeout_suspends($ctx) if $suspend;
   return ('blocked', join(', ', @$suspend)) if $suspend;
   $ctx->{'repo'} = $repo;
 

src/backend/BSSched/EventHandler.pm
@@ -539,10 +539,12 @@ sub event_suspendproject {
   my $gctx = $ectx->{'gctx'};
   my $evjob = $ev->{'job'};
   return unless $evjob;
-  print "suspending project $projid: @{$gctx->{'projsuspended'}->{$projid} || []} + $evjob\n";
-  push @{$gctx->{'projsuspended'}->{$projid}}, $evjob;
+  $ev->{'time'} = time();
+  my @suspendjob = map {$_->{'job'}} @{$gctx->{'projsuspended'}->{$projid} || []};
+  print "suspending project $projid: @suspendjob + $evjob\n";
+  push @{$gctx->{'projsuspended'}->{$projid}}, $ev;
+  push @suspendjob, $evjob;
 
-  my $suspend = $gctx->{'projsuspended'}->{$projid};
   # try to set the repo state right away
   my $projpacks = $gctx->{'projpacks'};
   my $proj = $projpacks->{$projid};
@@ -550,7 +552,7 @@ sub event_suspendproject {
   for my $repo (@{$proj->{'repository'} || []}) {
     next unless grep {$_ eq $gctx->{'arch'}} @{$repo->{'arch'} || []};
     my $ctx = BSSched::Checker->new($gctx, "$projid/$repo->{'name'}");
-    $ctx->set_repo_state('blocked', join(', ', @$suspend));
+    $ctx->set_repo_state('blocked', join(', ', @suspendjob));
   }
 }
 
@@ -564,15 +566,16 @@ sub event_resumeproject {
     print "ignoring resumeproject for project $projid ($evjob)\n";
     return;
   }
+  my @suspendjob = map {$_->{'job'}} @$suspend;
   if (@$suspend > 1) {
-    print "not yet resuming project $projid: @$suspend - $evjob\n";
+    print "not yet resuming project $projid: @suspendjob - $evjob\n";
     my $first = 0;
     # remove first matching element; if not found pop last element
-    @$suspend = grep {$_ ne $evjob || $first++} @$suspend;
+    @$suspend = grep {$_->{'job'} ne $evjob || $first++} @$suspend;
     pop @$suspend unless $first;
     return;
   }
-  print "resuming project $projid: @$suspend - $evjob\n";
+  print "resuming project $projid: @suspendjob - $evjob\n";
   delete $gctx->{'projsuspended'}->{$projid};
   my $changed_high = $gctx->{'changed_high'};
   my $changed_dirty = $gctx->{'changed_dirty'};

src/backend/bs_sched
@@ -667,7 +667,10 @@ if (!$testprojid && -s "$_rundir/$_runname.state") {
 
     # update projsuspended hash
     my $oldprojsuspended = $schedstate->{'projsuspended'} || {};
-    $_ = ref($_) ? $_ : [ $_ ] for values %$oldprojsuspended;	# convert to array refs
+    for my $suspend (values %$oldprojsuspended) {
+      $suspend = [ $suspend ] unless ref($suspend);
+      $_ = ref($_) ? $_ : { 'job' => $_, 'time' => time() } for @$suspend;
+    }
     %{$gctx->{'projsuspended'}} = %$oldprojsuspended;
 
     # use old start values
@@ -722,6 +725,9 @@ if (!$gctx->{'projpacks'}) {
   @{$gctx->{'lookat_low'}} = sort keys %{$gctx->{'projpacks'}};
   push @{$gctx->{'lookat_low'}}, @{$gctx->{'prps'}};
 }
+
+# look at suspended projects to check the timeout
+push @{$gctx->{'lookat_med'}}, @{$gctx->{'project_prps'}->{$_} || []} for sort keys %{$gctx->{'projsuspended'}};
 
 # reset booting flag
 writeschedulerinfo($gctx);

commit fc2bbe1cca2eadfdeb86b0b6cdd8dd8003983951

[backend] fix blocked message for suspended projects
Broken in commit 0a6aaed1f9c04c1cb904ea2bb4000206e5099a52

src/backend/BSSched/Checker.pm
@@ -360,7 +360,7 @@ sub setup {
   }
   my $suspend = $gctx->{'projsuspended'}->{$projid};
   $suspend = timeout_suspends($ctx) if $suspend;
-  return ('blocked', join(', ', @$suspend)) if $suspend;
+  return ('blocked', join(', ', map {$_->{'job'}} @$suspend)) if $suspend;
   $ctx->{'repo'} = $repo;
 
   if ($ctx->{'alllocked'}) {