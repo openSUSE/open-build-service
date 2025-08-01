4246c992a1fc75ef022b87739e1cee4f379bd95c
Michael Schroeder <mls@suse.de>
2025-08-01 09:55:23 +0200

    [backend] fix taking the signflavor from the _buildenv file
    The scheduler das not have the content of the _buildenv file
    as it is only read by the worker. So we put "_buildenv" as
    signflavor in the job and have the worker and signer take the
    real signflavor from the generated _buildenv file in the
    build result.

--- src/backend/BSSched/BuildJob.pm
+++ src/backend/BSSched/BuildJob.pm
@@ -1254,6 +1254,7 @@ sub create {
     if ($BSConfig::sign_flavor) {
       my $signflavor = $ctx->{'override_signflavor'} || $bconf->{'buildflags:signflavor'};
       return ('broken', "illegal sign flavor '$signflavor'") if $signflavor && !grep {$_ eq $signflavor} @$BSConfig::sign_flavor;
+      $signflavor = '_buildenv' if ($pdata->{'hasbuildenv'} || $info->{'hasbuildenv'}) && !$pdata->{'buildenv'};
       $binfo->{'signflavor'} = $signflavor if $signflavor;
     }
     $binfo->{'nouseforbuild'} = 1 if $info->{'nouseforbuild'};
