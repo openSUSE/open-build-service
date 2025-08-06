4387cd8c3e52f3ec37ee8dc7ecee7587512eed50
Jay Michalska <jay.michalska@suse.com>
2025-08-06 15:07:43 +0200

    Improve the request description and add links

--- src/api/app/components/bs_request_description_component.rb
+++ src/api/app/components/bs_request_description_component.rb
@@ -8,24 +8,24 @@ class BsRequestDescriptionComponent < ApplicationComponent
   delegate :requester_str, to: :helpers
   delegate :creator_intentions, to: :helpers
 
-  def initialize(bs_request:)
+  def initialize(bs_request:, links: false)
     super
     @bs_request = bs_request
+    @links = links
   end
 
   # rubocop:disable Metrics/CyclomaticComplexity
   # rubocop:disable Metrics/PerceivedComplexity
   # rubocop:disable Metrics/BlockLength
-  # rubocop:disable Style/FormatString
   def call
     # creator = action.bs_request.creator
     types = bs_request.bs_request_actions.group_by(&:type)
     description = []
 
     types.each do |type, actions|
-      source_packages = actions.map(&:source_package).uniq.map { |a| tag.b(a) }
+      source_packages = actions.map { |a| [a.source_project, a.source_package] }.uniq.map { |pr, pk| highlight_package(pr, pk) }
       source_packages = shorten_list(source_packages)
-      source_projects = actions.map(&:source_project).uniq.map { |a| tag.b(a) }
+      source_projects = actions.map(&:source_project).uniq.map { |a| highlight_project(a) }
       source_projects = shorten_list(source_projects)
       source_container = if actions.length == 1 && source_packages.length == 1
                            "package #{source_projects.first} / #{source_packages.first}"
@@ -36,41 +36,46 @@ class BsRequestDescriptionComponent < ApplicationComponent
                          end
       source_container = tag.span(sanitize(source_container), data: { bs_toggle: 'popover', bs_content: actions.map { |a| tag.b("#{a.source_project} / #{a.source_package}") }.uniq.to_sentence })
 
-      target_projects = actions.map(&:target_project).uniq.map { |a| tag.b(a) }
+      target_projects = actions.map(&:target_project).uniq.map { |a| highlight_project(a) }
       target_projects = shorten_list(target_projects)
-      target_container = "#{'project'.pluralize(target_projects.count)} #{target_projects.to_sentence}"
+      target_container = if actions.length == 1 && actions.first.target_package
+                           "package #{highlight_project(actions.first.target_project)} / #{highlight_package(actions.first.target_project, actions.first.target_package)}"
+                         elsif actions.any?(&:target_package)
+                           "#{'package'.pluralize(actions.filter_map(&:target_package).uniq.count)} in #{'project'.pluralize(target_projects.count)} #{target_projects.to_sentence}"
+                         else
+                           "#{'project'.pluralize(target_projects.count)} #{target_projects.to_sentence}"
+                         end
       target_container = tag.span(sanitize(target_container), data: { bs_toggle: 'popover', bs_content: actions.map { |a| tag.b("#{a.target_project} / #{a.target_package}") }.uniq.to_sentence })
 
       source_and_target_container = [source_container, target_container].join(tag.i(nil, class: 'fas fa-long-arrow-alt-right text-info mx-2'))
 
       description << case type
                      when 'submit'
-                       'Submit %{source_and_target_container}' % { source_and_target_container: source_and_target_container }
+                       "Submit #{source_and_target_container}"
                      when 'delete'
                        target = actions.map do |a|
                          string = ''
                          string += "repository #{tag.b(a.target_repository)} for " if a.target_repository
                          string += a.target_package ? 'package ' : 'project '
-                         string += "#{tag.b(a.target_project)} "
-                         string += "/ #{tag.b(a.target_package)}"
+                         string += "#{highlight_project(a.target_project)} "
+                         string += "/ #{highlight_package(a.target_project, a.target_package)}" if a.target_package
                          string
                        end.to_sentence
-                       'Delete %{target}' %
-                       { target: target }
+                       "Delete #{target}"
                      when 'add_role', 'set_bugowner'
-                       'Change role for %{target_container}' % { target_container: target_container }
+                       target = actions.map do |a|
+                         "#{a.person_name ? 'user' : 'group'} #{highlight_user(a.person_name)} #{highlight_group(a.group_name)} as #{a.type == 'set_bugowner' ? "the #{tag.b('bugowner')}" : "a #{tag.b(a.role)}"}"
+                       end
+                       target = shorten_list(target)
+                       "Assign #{target.to_sentence} for #{target_container}"
                      when 'change_devel'
-                       'Set %{source_container} to be devel project/package of %{target_container}' %
-                       { source_container: source_container, target_container: target_container }
+                       "Set #{source_container} to be #{tag.b('devel')} project/package of #{target_container}"
                      when 'maintenance_incident'
-                       'Submit update from %{source_and_target_container}' %
-                       { source_and_target_container: source_and_target_container }
+                       "Submit update from #{source_and_target_container}"
                      when 'maintenance_release'
-                       'Maintenance release %{source_and_target_container}' %
-                       { source_and_target_container: source_and_target_container }
+                       "Maintenance release #{source_and_target_container}"
                      when 'release'
-                       'Release %{source_and_target_container}' %
-                       { source_and_target_container: source_and_target_container }
+                       "Release #{source_and_target_container}"
                      end
     end
 
@@ -80,6 +85,34 @@ class BsRequestDescriptionComponent < ApplicationComponent
   end
 
   private
+
+  def highlight_project(project)
+    return unless project
+    return tag.b(project) unless @links
+
+    link_to(project, project_show_path(project))
+  end
+
+  def highlight_package(project, package)
+    return unless project && package
+    return tag.b(package) unless @links
+
+    link_to(package, package_show_path(project, package))
+  end
+
+  def highlight_user(user)
+    return unless user
+    return tag.b(user) unless @links
+
+    link_to(user, user_path(user))
+  end
+
+  def highlight_group(group)
+    return unless group
+    return tag.b(group) unless @links
+
+    link_to(group, group_path(group))
+  end
 
   def shorten_list(array, limit = 3)
     if array.count > limit
@@ -92,5 +125,4 @@ class BsRequestDescriptionComponent < ApplicationComponent
   # rubocop:enable Metrics/CyclomaticComplexity
   # rubocop:enable Metrics/PerceivedComplexity
   # rubocop:enable Metrics/BlockLength
-  # rubocop:enable Style/FormatString
 end
