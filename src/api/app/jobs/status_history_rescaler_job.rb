925294626d79a9a99d1b5198e00c5b045b60b781
Eduardo Navarro <enavarro@suse.com>
2025-08-06 09:05:58 +0200

    Autocorrect Style/RedundantParentheses RuboCop offense

--- src/api/app/jobs/status_history_rescaler_job.rb
+++ src/api/app/jobs/status_history_rescaler_job.rb
@@ -48,7 +48,7 @@ class StatusHistoryRescalerJob < ApplicationJob
 
       if items.length > 1
         timeavg = curmintime + (offset / 2)
-        valuavg = (items.inject(0) { |sum, item| sum + item.value }).to_f / items.length
+        valuavg = items.inject(0) { |sum, item| sum + item.value }.to_f / items.length
         Rails.logger.debug { "scaling #{key} #{curmintime} #{items.length} #{Time.at(timeavg)} #{valuavg}" }
         StatusHistory.delete(items.map(&:id))
         StatusHistory.create(key: key, time: timeavg, value: valuavg)
