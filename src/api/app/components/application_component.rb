30598b90c9dfd3292fbccf767477fbd30e0ef35e
ncounter <dleidi@suse.com>
2025-08-08 11:12:22 +0200

    Add explicit initializer to take arbitrary parameters
    ViewComponent::Base explicitly takes no parameters since
    https://github.com/ViewComponent/view_component/commit/74fc048f596e9ee5ebb59ce7b45a9ba6cadb9de4
    
    A breaking change introduced in 4.0.0
    
    https://github.com/ViewComponent/view_component/blob/main/docs/CHANGELOG.md#400
    "Support compatibility with Dry::Initializer. As a result, EmptyOrInvalidInitializerError will no longer be raised."
    "Remove default initializer from ViewComponent::Base. Previously, ViewComponent::Base defined a catch-all initializer
    that allowed components without an initializer defined to be passed arbitrary arguments."

--- src/api/app/components/application_component.rb
+++ src/api/app/components/application_component.rb
@@ -1,4 +1,16 @@
 class ApplicationComponent < ViewComponent::Base
+  # ViewComponent::Base explicitly takes no parameters since
+  # https://github.com/ViewComponent/view_component/commit/74fc048f596e9ee5ebb59ce7b45a9ba6cadb9de4
+  #
+  # A breaking change introduced in 4.0.0
+  # https://github.com/ViewComponent/view_component/blob/main/docs/CHANGELOG.md#400
+  # "Support compatibility with Dry::Initializer. As a result, EmptyOrInvalidInitializerError will no longer be raised."
+  # "Remove default initializer from ViewComponent::Base. Previously, ViewComponent::Base defined a catch-all initializer
+  #  that allowed components without an initializer defined to be passed arbitrary arguments."
+  def initialize(*_args, **_options)
+    super()
+  end
+
   # To be able to use Pundit policies in view components
   def policy(record)
     # rubocop:disable ViewComponent/AvoidGlobalState
