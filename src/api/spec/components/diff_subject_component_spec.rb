5d1cf2554cd7307aa8ae3f651041ec2d75067640
Dani Donisa <dani.doni@gmail.com>
2025-08-12 11:08:48 +0200

    Test DiffSubjectComponent before refactoring

--- src/api/spec/components/diff_subject_component_spec.rb
+++ src/api/spec/components/diff_subject_component_spec.rb
@@ -12,4 +12,39 @@ RSpec.describe DiffSubjectComponent, type: :component do
       expect(rendered_content).to have_text(preview_name.capitalize)
     end
   end
+
+  context 'when the state is added' do
+    it 'renders the new filename' do
+      render_inline(described_class.new(state: 'added', new_filename: 'new_file.txt', old_filename: ''))
+      expect(rendered_content).to have_text('new_file.txt')
+    end
+  end
+
+  context 'when the state is deleted' do
+    it 'renders the new filename' do
+      render_inline(described_class.new(state: 'deleted', new_filename: 'new_file.txt', old_filename: ''))
+      expect(rendered_content).to have_text('new_file.txt')
+    end
+  end
+
+  context 'when the state is changed' do
+    it 'renders the new filename' do
+      render_inline(described_class.new(state: 'changed', new_filename: 'new_file.txt', old_filename: 'new_file.txt'))
+      expect(rendered_content).to have_text('new_file.txt')
+    end
+  end
+
+  context 'when the state is renamed' do
+    it 'renders the new filename' do
+      render_inline(described_class.new(state: 'renamed', new_filename: 'new_file.txt', old_filename: 'old_file.txt'))
+      expect(rendered_content).to have_text('old_file.txt')
+    end
+  end
+
+  context 'when the state is blank' do
+    it 'renders the new filename' do
+      render_inline(described_class.new(state: '', new_filename: 'new_file.txt', old_filename: ''))
+      expect(rendered_content).to have_text('new_file.txt')
+    end
+  end
 end
