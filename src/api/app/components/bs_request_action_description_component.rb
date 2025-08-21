50b1774e707edda5be93fdf0e4ac76b58777c527
ncounter <dleidi@suse.com>
2025-08-21 11:04:28 +0200

    Refactoring: moving html generation into the view
    Getting rid of html_safe, doing all the conditions and text output
    generation in the view, not in the component code logic.

--- src/api/app/components/bs_request_action_description_component.rb
+++ src/api/app/components/bs_request_action_description_component.rb
@@ -13,56 +13,26 @@ class BsRequestActionDescriptionComponent < ApplicationComponent
     @action = action
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity
-  # rubocop:disable Rails/OutputSafety
-  # rubocop:disable Style/FormatString
-  def description
-    creator = action.bs_request.creator
+  def source_and_target_container
+    BsRequestActionSourceAndTargetComponent.new(action.bs_request).combine(source_container, target_container)
+  end
 
+  def source_container
     source_project_hash = { project: action.source_project, package: action.source_package, trim_to: nil }
+
+    project_or_package_link(source_project_hash)
+  end
+
+  def target_container
     target_project_hash = { project: action.target_project, package: action.target_package, trim_to: nil }
 
-    source_and_target_component = BsRequestActionSourceAndTargetComponent.new(action.bs_request)
-    source_container = project_or_package_link(source_project_hash)
-    target_container = project_or_package_link(target_project_hash)
-
-    source_and_target_container = source_and_target_component.combine(source_container, target_container)
-
-    description = case action.type
-                  when 'submit'
-                    'Submit %{source_and_target_container}' % { source_and_target_container: source_and_target_container }
-                  when 'delete'
-                    repository_content = link_to(action.target_repository, repositories_path(target_project_hash))
-                    target_repository = "repository #{repository_content} for " if action.target_repository
-
-                    'Delete %{target_repository}%{target_container}' %
-                    { target_repository: target_repository, target_container: target_container }
-                  when 'add_role', 'set_bugowner'
-                    '%{creator} wants %{requester} to %{task} for %{target_container}' % {
-                      creator: user_with_realname_and_icon(creator),
-                      requester: requester_str(creator, action.person_name, action.group_name),
-                      task: creator_intentions(action.role),
-                      target_container: target_container
-                    }
-                  when 'change_devel'
-                    'Set %{source_container} to be devel project/package of %{target_container}' %
-                    { source_container: source_container, target_container: target_container }
-                  when 'maintenance_incident'
-                    'Submit update from %{source_and_target_container}' %
-                    { source_and_target_container: source_and_target_container }
-                  when 'maintenance_release'
-                    'Maintenance release %{source_and_target_container}' %
-                    { source_and_target_container: source_and_target_container }
-                  when 'release'
-                    'Release %{source_and_target_container}' %
-                    { source_and_target_container: source_and_target_container }
-                  end
-
-    # HACK: this is just a porting of the already existing way of passing the string to the view
-    # TODO: refactor in order to get rid of the `html_safe` tagging
-    description.html_safe
+    project_or_package_link(target_project_hash)
   end
-  # rubocop:enable Metrics/CyclomaticComplexity
-  # rubocop:enable Rails/OutputSafety
-  # rubocop:enable Style/FormatString
+
+  def target_repository
+    target_project_hash = { project: action.target_project, package: action.target_package, trim_to: nil }
+
+    repository_content = link_to(action.target_repository, repositories_path(target_project_hash))
+    "repository #{repository_content} for " if action.target_repository
+  end
 end
