00dc757ca34551c2ed6d8a46dc8a7a7b661895c7
Eduardo Navarro <enavarro@suse.com>
2025-09-02 17:52:00 +0200

    Refactor job to copy projects

--- src/api/app/jobs/project_do_project_copy_job.rb
+++ src/api/app/jobs/project_do_project_copy_job.rb
@@ -4,14 +4,8 @@ class ProjectDoProjectCopyJob < ApplicationJob
   attr_accessor :project, :params
 
   def perform(project_id, params)
-    self.project = Project.find(project_id)
-    self.params = params
-    do_project_copy
-  end
+    project = Project.find(project_id)
 
-  private
-
-  def do_project_copy
     User.find_by!(login: params[:user]).run_as do
       # copy entire project in the backend
       begin
@@ -20,19 +14,16 @@ class ProjectDoProjectCopyJob < ApplicationJob
         logger.debug "copy failed: #{e.summary}"
         # we need to check results of backend in any case (also timeout error eg)
       end
-      _update_backend_packages
-    end
-  end
 
-  def _update_backend_packages
-    # restore all package meta data objects in DB
-    backend_pkgs = Xmlhash.parse(Backend::Api::Search.packages_for_project(project.name))
-    backend_pkgs.elements('package') do |package|
-      pkg_name = package['name']
-      pkg = project.packages.where(name: pkg_name).first_or_initialize
-      pkg.update_from_xml(Xmlhash.parse(Backend::Api::Sources::Package.meta(project.name, pkg_name)))
-      pkg.save!
+      # restore all package meta data objects in DB
+      backend_pkgs = Xmlhash.parse(Backend::Api::Search.packages_for_project(project.name))
+      backend_pkgs.elements('package') do |package|
+        pkg_name = package['name']
+        pkg = project.packages.where(name: pkg_name).first_or_initialize
+        pkg.update_from_xml(Xmlhash.parse(Backend::Api::Sources::Package.meta(project.name, pkg_name)))
+        pkg.save!
+      end
+      project.all_sources_changed
     end
-    project.all_sources_changed
   end
 end
