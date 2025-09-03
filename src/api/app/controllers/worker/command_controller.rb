4ddbc0b140d8e1e22b18c094d6a0a0a4e4c3f405
Eduardo Navarro <enavarro@suse.com>
2025-09-03 17:38:56 +0200

    Refactor backend call listing workers filtered by constraints via Backend::Api

--- src/api/app/controllers/worker/command_controller.rb
+++ src/api/app/controllers/worker/command_controller.rb
@@ -1,12 +1,11 @@
 class Worker::CommandController < ApplicationController
   def run
-    params.require(%i[cmd project package repository arch])
+    params.require(%i[project package repository arch])
 
     # read permission checking
     Package.get_by_project_and_name(params[:project], params[:package], { follow_multibuild: true })
 
-    path = '/worker'
-    path += build_query_from_hash(params, %i[cmd project package repository arch])
-    pass_to_backend(path)
+    render xml: Backend::Api::Worker.check_constraints(params.slice(:project, :package, :repository, :arch).permit!.to_h,
+                                                       request.body.string.presence || '<constraints></constraints>')
   end
 end
