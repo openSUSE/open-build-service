9dd92b5b8ca0a08fce30b90fe3ce5f7045c28cc7
Lukas Krause <lkrause@suse.de>
2025-09-19 14:38:33 +0200

    Create `PackageVersionUpstream` model
    Co-authored-by: Rubhan Azeem <rubhan.azeem@suse.com>
    Co-authored-by: Saray Cabrera Padrón <scabrerapadron@suse.de>

---
+++ src/api/app/models/package_version_upstream.rb
@@ -0,0 +1,47 @@
+# Model to track the upstream version of a package.
+class PackageVersionUpstream < PackageVersion
+  #### Includes and extends
+
+  #### Constants
+
+  #### Self config
+
+  #### Attributes
+
+  #### Associations macros (Belongs to, Has one, Has many)
+
+  #### Callbacks macros: before_save, after_save, etc.
+
+  #### Scopes (first the default_scope macro if is used)
+
+  #### Validations macros
+
+  #### Class methods using self. (public and then private)
+
+  #### To define class methods as private use private_class_method
+  #### private
+
+  #### Instance methods (public and then protected/private)
+
+  #### Alias of methods
+end
+
+# == Schema Information
+#
+# Table name: package_versions
+#
+#  id         :bigint           not null, primary key
+#  type       :string(255)      not null
+#  version    :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  package_id :integer          not null, indexed
+#
+# Indexes
+#
+#  index_package_versions_on_package_id  (package_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (package_id => packages.id)
+#
