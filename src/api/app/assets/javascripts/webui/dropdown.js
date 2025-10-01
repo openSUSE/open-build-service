856008e211a3c413a54336550cdd6ea77e2da87e
Eduardo Navarro <enavarro@suse.com>
2025-10-01 10:33:16 +0200

    Refactor two JavaScript functions into a single dropdown filter function

--- src/api/app/assets/javascripts/webui/dropdown.js
+++ src/api/app/assets/javascripts/webui/dropdown.js
@@ -10,3 +10,26 @@ $(document).ready(function(){
     });
   });
 });
+
+function setupDropdownFilters() { // jshint ignore:line
+  initializePopovers('[data-bs-toggle="popover"]'); // jshint ignore:line
+
+  function setAllRelatedLinks(event) {
+    $(this).closest('.dropdown-menu').find('input').prop('checked', event.data.checked);
+  }
+
+  $('.monitor-no-filter-link').on('click', { checked: false }, setAllRelatedLinks);
+  $('.monitor-filter-link').on('click', { checked: true }, setAllRelatedLinks);
+  $('.dropdown-menu.keep-open').on('click', function (e) {
+    e.stopPropagation();
+  });
+  $('.monitor-search').on('input', function (e) {
+    var labels = $(this).closest('.dropdown-menu').find('.form-check-label');
+    Array.from(labels).forEach((label) => {
+      var element = label.closest('.dropdown-item');
+      element.classList.remove('d-none');
+      if (!label.innerText.includes(e.target.value))
+        element.classList.add('d-none');
+    });
+  });
+}
--- src/api/app/assets/javascripts/webui/request_show_redesign/build_results.js
+++ src/api/app/assets/javascripts/webui/request_show_redesign/build_results.js
@@ -14,30 +14,7 @@ function updateBuildResultBeta() { // jshint ignore:line
     },
     complete: function() {
       $('#build-reload').removeClass('fa-spin');
-      setupProjectMonitorPage();
+      setupDropdownFilters(); // jshint ignore:line
     }
-  });
-}
-
-function setupProjectMonitorPage() {
-  initializePopovers('[data-bs-toggle="popover"]'); // jshint ignore:line
-
-  function setAllRelatedLinks(event) {
-    $(this).closest('.dropdown-menu').find('input').prop('checked', event.data.checked);
-  }
-
-  $('.monitor-no-filter-link').on('click', { checked: false }, setAllRelatedLinks);
-  $('.monitor-filter-link').on('click', { checked: true }, setAllRelatedLinks);
-  $('.dropdown-menu.keep-open').on('click', function (e) {
-    e.stopPropagation();
-  });
-  $('.monitor-search').on('input', function (e) {
-    var labels = $(this).closest('.dropdown-menu').find('.form-check-label');
-    Array.from(labels).forEach((label) => {
-      var element = label.closest('.dropdown-item');
-      element.classList.remove('d-none');
-      if (!label.innerText.includes(e.target.value))
-        element.classList.add('d-none');
-    });
   });
 }
