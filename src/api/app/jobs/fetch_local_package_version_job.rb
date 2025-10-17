caa0a73c3b42f3723bd955ee8c878a0202ea2352
Saray Cabrera Padrón <scabrerapadron@suse.de>
2025-10-17 10:39:07 +0200

    Handle projects without packages in the Backend
    The job that creates instances of PackageVersionLocal was crashing when
    the project had no packages in the backed.
    
    Co-authored-by: Eduardo Navarro <enavarro@suse.com>

--- src/api/app/jobs/fetch_local_package_version_job.rb
+++ src/api/app/jobs/fetch_local_package_version_job.rb
@@ -5,9 +5,18 @@ class FetchLocalPackageVersionJob < ApplicationJob
     info = if package_name
              Backend::Api::Sources::Package.files(project_name, package_name, view: :info, parse: 1)
            else
-             Backend::Api::Sources::Project.packages(project_name, view: :info, parse: 1)
+             begin
+               Backend::Api::Sources::Project.packages(project_name, view: :info, parse: 1)
+             rescue Backend::NotFoundError
+               logger.info("FetchLocalPackageVersionJob: #{project_name} has no packages in the Backend")
+               nil
+             end
            end
 
+    create_local_version_from_backend_output(info) if info
+  end
+
+  def create_local_version_from_backend_output(info)
     Nokogiri::XML(info).xpath('//sourceinfo[@package]').each do |sourceinfo|
       next unless (package = Package.find_by_project_and_name(project_name, sourceinfo['package']))
       next unless (version = sourceinfo.at('version')&.content)
