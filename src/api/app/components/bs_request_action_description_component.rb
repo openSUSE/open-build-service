accc6a33b0eab91be044d7783f87f34c83d21151
ncounter <dleidi@suse.com>
2025-10-31 10:47:00 +0100

    Drop stale optional parameter condition

--- src/api/app/components/bs_request_action_description_component.rb
+++ src/api/app/components/bs_request_action_description_component.rb
@@ -1,17 +1,16 @@
 # This component renders the request action description based on the type of the action
 
 class BsRequestActionDescriptionComponent < ApplicationComponent
-  attr_reader :action, :text_only
+  attr_reader :action
 
   delegate :project_or_package_link, to: :helpers
   delegate :user_with_realname_and_icon, to: :helpers
   delegate :requester_str, to: :helpers
   delegate :creator_intentions, to: :helpers
 
-  def initialize(action:, text_only: false)
+  def initialize(action:)
     super()
     @action = action
-    @text_only = text_only
   end
 
   # rubocop:disable Metrics/CyclomaticComplexity
@@ -24,14 +23,8 @@ class BsRequestActionDescriptionComponent < ApplicationComponent
     target_project_hash = { project: action.target_project, package: action.target_package, trim_to: nil }
 
     source_and_target_component = BsRequestActionSourceAndTargetComponent.new(action.bs_request)
-
-    if text_only
-      source_container = source_and_target_component.source
-      target_container = source_and_target_component.target
-    else
-      source_container = project_or_package_link(source_project_hash)
-      target_container = project_or_package_link(target_project_hash)
-    end
+    source_container = project_or_package_link(source_project_hash)
+    target_container = project_or_package_link(target_project_hash)
 
     source_and_target_container = source_and_target_component.combine(source_container, target_container)
 
@@ -39,14 +32,14 @@ class BsRequestActionDescriptionComponent < ApplicationComponent
                   when 'submit'
                     'Submit %{source_and_target_container}' % { source_and_target_container: source_and_target_container }
                   when 'delete'
-                    repository_content = text_only ? action.target_repository : link_to(action.target_repository, repositories_path(target_project_hash))
+                    repository_content = link_to(action.target_repository, repositories_path(target_project_hash))
                     target_repository = "repository #{repository_content} for " if action.target_repository
 
                     'Delete %{target_repository}%{target_container}' %
                     { target_repository: target_repository, target_container: target_container }
                   when 'add_role', 'set_bugowner'
                     '%{creator} wants %{requester} to %{task} for %{target_container}' % {
-                      creator: text_only ? creator : user_with_realname_and_icon(creator),
+                      creator: user_with_realname_and_icon(creator),
                       requester: requester_str(creator, action.person_name, action.group_name),
                       task: creator_intentions(action.role),
                       target_container: target_container
