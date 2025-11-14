ce302e8aac835fdf94973feaf4201c77de89abaf
Lukas Krause <lkrause@suse.de>
2025-11-14 13:15:38 +0100

    Adapt and extend assignment model spec

--- src/api/spec/models/assignment_spec.rb
+++ src/api/spec/models/assignment_spec.rb
@@ -1,30 +1,39 @@
 RSpec.describe Assignment do
   describe '#assignee_has_required_role_to_be_assigned?' do
     subject do
-      assignment.assignee_has_required_role_to_be_assigned?
+      build(:assignment, package: package, assignee: user)
     end
 
-    context 'when the assignee is a package maintainer' do
-      let(:package) { create(:package_with_maintainer) }
-      let(:assignment) { create(:assignment, package: package, assignee: package.maintainers.first) }
+    let(:project) { create(:project_with_package) }
+    let(:package) { project.packages.first }
+    let(:user) { create(:confirmed_user) }
 
-      it { expect(subject).to be true }
+    context 'when the assignee is a package maintainer' do
+      let!(:relationship_package_maintainer) { create(:relationship_package_user, package: package, user: user) }
+
+      it { expect(subject).to be_valid }
     end
 
     context 'when the assignee is a project maintainer' do
-      let(:maintainer) { create(:confirmed_user) }
-      let(:project) { create(:project_with_package, maintainer: maintainer) }
-      let(:package) { project.packages.first }
-      let(:assignment) { create(:assignment, package: package, assignee: maintainer) }
+      let!(:relationship_project_maintainer) { create(:relationship_project_user, project: project, user: user) }
 
-      it { expect(subject).to be true }
+      it { expect(subject).to be_valid }
     end
 
-    context 'when the assignee is not a maintainer' do
-      let(:iggy) { create(:confirmed_user) }
-      let(:assignment) { build(:assignment, assignee: iggy) }
+    context 'when the assignee is a package bugowner' do
+      let!(:bugowner_relationship) { create(:relationship_package_user_as_bugowner, user: user, package: package) }
 
-      it { expect(subject).to be false }
+      it { expect(subject).to be_valid }
+    end
+
+    context 'when the assignee is a package reviewer' do
+      let!(:reviewer_relationship) { create(:relationship_package_user_as_reviewer, user: user, package: package) }
+
+      it { expect(subject).to be_valid }
+    end
+
+    context 'when the assignee has no role' do
+      it { expect(subject).not_to be_valid }
     end
   end
 end
