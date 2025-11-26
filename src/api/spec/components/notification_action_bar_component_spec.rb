930ebbafbe836e71de2ebb9e61e0b56054461f24
Lukas Krause <lkrause@suse.de>
2025-11-26 16:28:38 +0100

    Adapt notification_action_bar_component_spec

--- src/api/spec/components/notification_action_bar_component_spec.rb
+++ src/api/spec/components/notification_action_bar_component_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe NotificationActionBarComponent, type: :component do
   context 'for unread notifications' do
     before do
       User.session = create(:user)
-      render_inline(described_class.new(state: 'unread', update_path: 'my/notifications', counted_notifications: { all: 301 }.with_indifferent_access))
+      render_inline(described_class.new(state: 'unread', update_path: 'my/notifications', total_count_notifications: 301))
     end
 
     it do
@@ -25,7 +25,7 @@ RSpec.describe NotificationActionBarComponent, type: :component do
   context 'for read notifications' do
     before do
       User.session = create(:user)
-      render_inline(described_class.new(state: 'read', update_path: 'my/notifications?state=read', counted_notifications: { all: 301 }.with_indifferent_access))
+      render_inline(described_class.new(state: 'read', update_path: 'my/notifications?state=read', total_count_notifications: 301))
     end
 
     it do
