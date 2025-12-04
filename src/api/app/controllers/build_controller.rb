175a3dea02b595cff50f0f2940f5bedeb067f7e9
piyusshkumar <piyusshkumar9599@gmail.com>
2025-12-05 00:21:23 +0530

    fixes checks error

--- src/api/app/controllers/build_controller.rb
+++ src/api/app/controllers/build_controller.rb
@@ -143,17 +143,22 @@ class BuildController < ApplicationController
 
   def result
     # this route is mainly for checking submissions to a target project
-    # allowed value are set to true/false or 1/0
-     if params.key?(:lastsuccess)
-       allowed_values = ["1", "0", "true", "false", 1, 0 , true, false]
+    # allowed values are true/false or 1/0
+if params.key?(:lastsuccess)
+  allowed_values = ['1', '0', 'true', 'false', 1, 0, true, false]
 
-       unless allowed_values.include(params[:lastsuccess])
-         return render_error status:400,
-                             errorcode: "invalid_lastsuccess_value",
-                             message: "Parameter 'lastsuccess' must be true/false or 1/0"
-       end
-    return result_lastsuccess
-      end
+  unless allowed_values.include?(params[:lastsuccess])
+    return render_error(
+      status: 400,
+      errorcode: 'invalid_lastsuccess_value',
+      message: "Invalid value for lastsuccess. Expected true/false or 1/0."
+    )
+  end
+
+  result_lastsuccess = params[:lastsuccess].to_s == 'true' || params[:lastsuccess].to_s == '1'
+  return result_lastsuccess
+end
+
 
     # for permission check
     Project.get_by_name(params[:project])
