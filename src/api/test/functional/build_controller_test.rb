eac6ab602047f0abf63e5e9f25f7e8169c6a9f8a
piyusshkumar <piyusshkumar9599@gmail.com>
2025-12-07 14:00:57 +0530

    added separate test cases

--- src/api/test/functional/build_controller_test.rb
+++ src/api/test/functional/build_controller_test.rb
@@ -309,6 +309,12 @@ class BuildControllerTest < ActionDispatch::IntegrationTest
     assert_xml_tag tag: 'resultlist', children: { count: 2 }
 
     get '/build/home:Iggy/_result?lastsuccess&pathproject=kde4&package=TestPack'
+    assert_response :not_found
+    assert_xml_tag(tag: 'status', attributes: { code: 'no_repositories_found' })
+  end
+  
+  def test_result_invalid_lastsuccess_param
+    get '/build/home:Iggy/_result?lastsuccess=abc'
     assert_response :bad_request
     assert_xml_tag(tag: 'status', attributes: { code: 'invalid_lastsuccess_value' })
   end
