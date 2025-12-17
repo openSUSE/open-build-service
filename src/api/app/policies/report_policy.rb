7925abd510a6776bee0352f16a6b5f51f5912ef3
ddonisa <ddonisa@localhost.localdomain>
2025-12-17 12:31:52 +0100

    Fix the Report policy
    Instead of allowing the creator of the request to report the same
    request, we allow anybody but the creator to do it.

--- src/api/app/policies/report_policy.rb
+++ src/api/app/policies/report_policy.rb
@@ -25,7 +25,7 @@ class ReportPolicy < ApplicationPolicy
     when 'User'
       !UserPolicy.new(user, record.reportable).update?
     when 'BsRequest'
-      !BsRequestPolicy.new(user, record.reportable).report?
+      BsRequestPolicy.new(user, record.reportable).report?
     end
   end
   # rubocop:enable Metrics/CyclomaticComplexity
