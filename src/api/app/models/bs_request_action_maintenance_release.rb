3dd69e1693968cc5aaac0b2955c592d9fa4bf2ee
Steven R. Baker <steven@stevenrbaker.com>
2026-01-26 14:37:30 +0100

    Extract these validations to their appropriate subclasses.

--- src/api/app/models/bs_request_action_maintenance_release.rb
+++ src/api/app/models/bs_request_action_maintenance_release.rb
@@ -16,6 +16,14 @@ class BsRequestActionMaintenanceRelease < BsRequestAction
   #### Callbacks macros: before_save, after_save, etc.
   #### Scopes (first the default_scope macro if is used)
   #### Validations macros
+  def check_sanity
+    errors.add(:source_project, "should not be empty for #{action_type} requests") if source_project.blank?
+    errors.add(:source_package, "should not be empty for #{action_type} requests") if !maintenance_incident? && source_package.blank?
+    errors.add(:target_project, "should not be empty for #{action_type} requests") if target_project.blank?
+    errors.add(:target_package, 'No source changes are allowed, if source and target is identical') if source_package == target_package && source_project == target_project && (sourceupdate || updatelink)
+
+    super
+  end
 
   #### Class methods using self. (public and then private)
   def self.sti_name
