9e0bd3c11fbf0a68b00c402d7defc54472050dfb
Lukas Krause <lkrause@suse.de>
2026-01-28 17:37:00 +0100

    Render counters for request index involvement filters

--- src/api/app/controllers/concerns/webui/requests_count.rb
+++ src/api/app/controllers/concerns/webui/requests_count.rb
@@ -4,16 +4,27 @@ module Webui::RequestsCount
   FILTERABLE_BSREQUEST_TYPES = %w[set_bugowner change_devel delete maintenance_incident
                                   maintenance_release release add_role submit].freeze
 
-  def counts_for_states_and_types
-    @counts_grouped_by_state = group_and_fill(@bs_requests, :state, BsRequest::VALID_REQUEST_STATES.map(&:to_s))
-    @counts_grouped_by_type  = group_and_fill(@bs_requests, :type, FILTERABLE_BSREQUEST_TYPES)
+  def counts
+    counts_for_states_and_types
+    counts_for_involvements
 
     respond_to do |format|
-      format.turbo_stream { render 'webui/shared/bs_requests/counts_for_states_and_types' }
+      format.turbo_stream { render 'webui/shared/bs_requests/counts' }
     end
   end
 
   private
+
+  def counts_for_involvements
+    @count_for_incoming_requests = incoming_query.count
+    @count_for_outgoing_requests = outgoing_query.count
+    @count_for_reviews = review_query.count
+  end
+
+  def counts_for_states_and_types
+    @counts_grouped_by_state = group_and_fill(@bs_requests, :state, BsRequest::VALID_REQUEST_STATES.map(&:to_s))
+    @counts_grouped_by_type  = group_and_fill(@bs_requests, :type, FILTERABLE_BSREQUEST_TYPES)
+  end
 
   def group_and_fill(relation, column, keys)
     counts = relation.group(column).order(column).count
--- src/api/config/routes/webui.rb
+++ src/api/config/routes/webui.rb
@@ -348,9 +348,9 @@ resources :requests, only: [], param: :number, controller: 'webui/request' do
 end
 
 get 'projects/:project/requests' => 'webui/projects/bs_requests#index', constraints: cons, as: 'projects_requests'
-get 'projects/:project/requests/counts_for_states_and_types' => 'webui/projects/bs_requests#counts_for_states_and_types', constraints: cons
+get 'projects/:project/requests/counts' => 'webui/projects/bs_requests#counts', constraints: cons
 get 'projects/:project/packages/:package/requests' => 'webui/packages/bs_requests#index', constraints: cons, as: 'packages_requests'
-get 'projects/:project/packages/:package/requests/counts_for_states_and_types' => 'webui/packages/bs_requests#counts_for_states_and_types', constraints: cons
+get 'projects/:project/packages/:package/requests/counts' => 'webui/packages/bs_requests#counts', constraints: cons
 get 'notification/autocomplete_projects' => 'webui/users/notifications#autocomplete_projects', as: 'notification_autocomplete_projects'
 
 controller 'webui/search' do
@@ -378,7 +378,7 @@ scope :my do
   resources :tasks, only: [:index], controller: 'webui/users/tasks', as: :my_tasks
   resources :requests, only: [:index], controller: 'webui/users/bs_requests', as: :my_requests do
     collection do
-      get :counts_for_states_and_types
+      get :counts
     end
   end
 
@@ -445,7 +445,7 @@ resources :groups, only: %i[index show new create edit update], param: :title, c
   resources :user, only: %i[create destroy update], param: :user_login, constraints: cons, controller: 'webui/groups/users'
   resources :requests, only: [:index], controller: 'webui/groups/bs_requests' do
     collection do
-      get :counts_for_states_and_types
+      get :counts
     end
   end
 
