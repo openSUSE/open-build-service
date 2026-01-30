3f89086d648d8f6da896d7c4df03fd883c81c080
Saray Cabrera Padrón <scabrerapadron@suse.de>
2026-01-30 13:38:55 +0100

    Improve comments_count data migration
    To make it run in batches and skip commentables that are nil.
    There are thousands of comments related to projects, packages, reports,
    bs_requests and bs_request_actions.

--- src/api/db/data/20260127123201_prefill_comments_count_values.rb
+++ src/api/db/data/20260127123201_prefill_comments_count_values.rb
@@ -3,8 +3,10 @@
 class PrefillCommentsCountValues < ActiveRecord::Migration[7.2]
   def up
     # rubocop:disable Rails/SkipsModelValidations
-    Comment.group(:commentable_type, :commentable_id).select(:commentable_type, :commentable_id, 'COUNT(id) as comments_count').each do |comment|
-      comment.commentable.update_columns(comments_count: comment.comments_count)
+    Comment.group(:commentable_type, :commentable_id)
+           .select(:id, :commentable_type, :commentable_id, 'COUNT(id) as comments_count')
+           .find_each.each do |comment|
+      comment.commentable.update_columns(comments_count: comment.comments_count) if comment.commentable
     end
     # rubocop:enable Rails/SkipsModelValidations
   end
