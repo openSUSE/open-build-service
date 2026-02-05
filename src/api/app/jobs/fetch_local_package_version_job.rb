5cf2649e7a9abe6612261c5cb154b92f6a2dd034
Rubhan Azeem <rubhanazeem@gmail.com>
2026-02-05 17:46:37 +0100

    Delete package versions if anitya distribution is not available

--- src/api/app/jobs/fetch_local_package_version_job.rb
+++ src/api/app/jobs/fetch_local_package_version_job.rb
@@ -2,18 +2,36 @@ class FetchLocalPackageVersionJob < ApplicationJob
   queue_as :quick
 
   def perform(project_name, package_name: nil)
+    project = Project.find_by_name(project_name)
+    distribution_name = project.anitya_distribution_name
+
+    if distribution_name.blank?
+      delete_package_version_local(project)
+
+      return
+    end
+
     info = if package_name
              Backend::Api::Sources::Package.files(project_name, package_name, view: :info, parse: 1)
            else
              Backend::Api::Sources::Project.packages(project_name, view: :info, parse: 1)
            end
+    create_package_version_local(info: info, project_name: project_name)
+  end
 
+  def delete_package_version_local(project)
+    PackageVersionLocal.joins(:package).where(package_id: project.packages.ids).delete_all
+  end
+
+  def create_package_version_local(info:, project_name:)
     Nokogiri::XML(info).xpath('//sourceinfo[@package]').each do |sourceinfo|
       next unless (package = Package.find_by_project_and_name(project_name, sourceinfo['package']))
       next unless (version = sourceinfo.at('version')&.content)
 
       package_version_local = PackageVersionLocal.find_or_create_by(version: version, package: package)
-      package_version_local.touch if package_version_local.persisted? # rubocop:disable Rails/SkipsModelValidations
+      # rubocop:disable Rails/SkipsModelValidations
+      package_version_local.touch if package_version_local.persisted?
+      # rubocop:enable Rails/SkipsModelValidations
     end
   end
 end
