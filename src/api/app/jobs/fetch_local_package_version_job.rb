3a8ad1d6f3e64fbcb4a9279dab8f1184754413e8
Rubhan Azeem <rubhanazeem@gmail.com>
2026-02-05 17:46:37 +0100

    Delete package versions if anitya distribution is not available

--- src/api/app/jobs/fetch_local_package_version_job.rb
+++ src/api/app/jobs/fetch_local_package_version_job.rb
@@ -2,12 +2,28 @@ class FetchLocalPackageVersionJob < ApplicationJob
   queue_as :quick
 
   def perform(project_name, package_name: nil)
+    project = Project.find_by_name(project_name)
+    distribution_name = project.anitya_distribution_name
+
+    if distribution_name.blank?
+      delete_package_version_local(project.packages.ids)
+
+      return
+    end
+
     info = if package_name
              Backend::Api::Sources::Package.files(project_name, package_name, view: :info, parse: 1)
            else
              Backend::Api::Sources::Project.packages(project_name, view: :info, parse: 1)
            end
+    create_package_version_local(info: info, project_name: project_name)
+  end
 
+  def delete_package_version_local(package_ids)
+    PackageVersionLocal.where(package_id: package_ids).delete_all
+  end
+
+  def create_package_version_local(info:, project_name:)
     Nokogiri::XML(info).xpath('//sourceinfo[@package]').each do |sourceinfo|
       next unless (package = Package.find_by_project_and_name(project_name, sourceinfo['package']))
       next unless (version = sourceinfo.at('version')&.content)
--- src/api/app/jobs/fetch_upstream_package_version_job.rb
+++ src/api/app/jobs/fetch_upstream_package_version_job.rb
@@ -17,7 +17,11 @@ class FetchUpstreamPackageVersionJob < ApplicationJob
     return if project.blank?
 
     distribution_name = project.anitya_distribution_name
-    return if distribution_name.blank?
+    if distribution_name.blank?
+      delete_package_version_upstream(project.packages.ids)
+
+      return
+    end
 
     project.packages.each do |package|
       create_upstream_package_versions(package_name: package.name, distribution_name: distribution_name, package_ids: [package.id])
@@ -41,7 +45,7 @@ class FetchUpstreamPackageVersionJob < ApplicationJob
     response = fetch_upstream_package_info(package_name: package_name, distribution_name: distribution_name)
 
     # When we get empty result, we can’t rely on the past information we stored in the database anymore
-    PackageVersionUpstream.where(package_id: package_ids).delete_all && return if response&.dig('total_items')&.zero?
+    delete_package_version_upstream(package_ids) && return if response&.dig('total_items')&.zero?
 
     upstream_version = extract_version(response)
     return if upstream_version.blank?
@@ -66,5 +70,9 @@ class FetchUpstreamPackageVersionJob < ApplicationJob
     return if response.nil?
 
     response.dig('items', 0, 'stable_version')
+  end
+
+  def delete_package_version_upstream(package_ids)
+    PackageVersionUpstream.where(package_id: package_ids).delete_all
   end
 end
