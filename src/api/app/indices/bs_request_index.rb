6c156857d4463b106888c7ef2e89726c96948c5b
Lukas Krause <lkrause@suse.de>
2026-02-11 18:06:06 +0100

    Revert "Add Sphinx attribute to filter by BsRequest id"
    This reverts commit 3686c85eccaf3c4c8b61581778f6f67f23ca8ad1.

--- src/api/app/indices/bs_request_index.rb
+++ src/api/app/indices/bs_request_index.rb
@@ -1,6 +1,5 @@
 ThinkingSphinx::Index.define :bs_request, with: :real_time do
   indexes comment, description, comments_bodies, reviews_reasons
 
-  has id, as: :bs_request_id, type: :integer
   has updated_at, type: :timestamp
 end
