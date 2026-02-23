ce50562018ed85a09e676c7d157ebc7d5bcef164
Lukas Krause <lkrause@suse.de>
2026-02-23 14:25:54 +0100

    Create factory for `package_version_local`

--- src/api/spec/factories/package_version.rb
+++ src/api/spec/factories/package_version.rb
@@ -4,4 +4,10 @@ FactoryBot.define do
     type { 'PackageVersionUpstream' }
     version { Faker::App.version }
   end
+
+  factory :package_version_local do
+    package
+    type { 'PackageVersionLocal' }
+    version { Faker::App.version }
+  end
 end
