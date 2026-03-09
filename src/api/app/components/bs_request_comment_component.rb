22a4d5caadff4b821bab81db9f3bcc6a99407152
Rubhan Azeem <rubhanazeem@gmail.com>
2026-03-09 16:34:54 +0100

    Implement dynamic diff hunk calculation for diff comments
    Replaced hardcoded logic with a dynamic range calculation

--- src/api/app/components/bs_request_comment_component.rb
+++ src/api/app/components/bs_request_comment_component.rb
@@ -16,7 +16,35 @@ class BsRequestCommentComponent < ApplicationComponent
   end
 
   def range
-    line_index = @comment.diff_line_number
-    ((line_index - 4).clamp(0..)..(line_index - 1))
+    line_index = @comment.diff_line_number - 1
+
+    start = if @diff['old'].present?
+              find_index_of_previous_line
+            else # for new files just show the commented line
+              line_index
+            end
+    start..line_index
   end
+
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def find_index_of_previous_line
+    context_lines = 4
+    # If file has been removed then just show the 4 lines to provide context
+    return @comment.diff_line_number - context_lines if @diff['state'] == 'deleted'
+
+    lines = @diff.dig('diff', '_content').split("\n", -1)
+
+    current_line_index = @comment.diff_line_number - 1
+    search_start = current_line_index - 1
+
+    lookup_until = search_start > context_lines ? search_start - context_lines : 0
+    search_start.downto(lookup_until) do |i|
+      line = lines[i].to_s.strip
+      next if line.empty? || line.start_with?('+') || line.start_with?('\\') || line.include?('No newline at end of file')
+
+      return i if line.start_with?('-')
+    end
+    current_line_index
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
 end
