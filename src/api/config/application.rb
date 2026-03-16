0faae4026f64ce80631cde509a0e6eb389c37f2e
Eduardo Navarro <enavarro@suse.com>
2026-03-16 14:45:35 +0100

    Make specs runnable again for the next Rails version...
    ... in the development environment
    
    Otherwise, we obtain the following error:
    
    > bundle exec rspec
    An error occurred while loading spec_helper.
    Failure/Error:
      class InvalidFlag < APIError
        setup 'invalid_flag'
    
    NameError:
      uninitialized constant FlagHelper::APIError
    
    ./app/helpers/flag_helper.rb:2:in '<module:FlagHelper>'
    ./app/helpers/flag_helper.rb:1:in '<top (required)>'
    /usr/lib64/obs-api/ruby/3.4.0/gems/zeitwerk-2.7.5/lib/zeitwerk/core_ext/kernel.rb:26:in 'Kernel#require'
    /usr/lib64/obs-api/ruby/3.4.0/gems/activesupport-8.1.2/lib/active_support/inflector/methods.rb:290:in 'Module#const_get'
    /usr/lib64/obs-api/ruby/3.4.0/gems/activesupport-8.1.2/lib/active_support/inflector/methods.rb:290:in 'ActiveSupport::Inflector#constantize'
    /usr/lib64/obs-api/ruby/3.4.0/gems/activesupport-8.1.2/lib/active_support/core_ext/string/inflections.rb:74:in 'String#constantize'
    /usr/lib64/obs-api/ruby/3.4.0/gems/actionpack-8.1.2/lib/abstract_controller/helpers.rb:41:in 'block in AbstractController::Helpers::Resolution#modules_for_helpers'
    /usr/lib64/obs-api/ruby/3.4.0/gems/actionpack-8.1.2/lib/abstract_controller/helpers.rb:34:in 'Array#map!'
    /usr/lib64/obs-api/ruby/3.4.0/gems/actionpack-8.1.2/lib/abstract_controller/helpers.rb:34:in 'AbstractController::Helpers::Resolution#modules_for_helpers'
    /usr/lib64/obs-api/ruby/3.4.0/gems/actionpack-8.1.2/lib/action_controller/metal/helpers.rb:114:in 'ActionController::Helpers::ClassMethods#modules_for_helpers'
    /usr/lib64/obs-api/ruby/3.4.0/gems/actionpack-8.1.2/lib/abstract_controller/helpers.rb:199:in 'AbstractController::Helpers::ClassMethods#helper'
    /usr/lib64/obs-api/ruby/3.4.0/gems/actionpack-8.1.2/lib/action_controller/railties/helpers.rb:21:in 'ActionController::Railties::Helpers#inherited'
    /usr/lib64/obs-api/ruby/3.4.0/gems/actionview-8.1.2/lib/action_view/test_case.rb:16:in '<class:TestCase>'
    /usr/lib64/obs-api/ruby/3.4.0/gems/actionview-8.1.2/lib/action_view/test_case.rb:15:in '<module:ActionView>'
    /usr/lib64/obs-api/ruby/3.4.0/gems/actionview-8.1.2/lib/action_view/test_case.rb:10:in '<top (required)>'
    /usr/lib64/obs-api/ruby/3.4.0/gems/zeitwerk-2.7.5/lib/zeitwerk/core_ext/kernel.rb:34:in 'Kernel#require'
    /usr/lib64/obs-api/ruby/3.4.0/gems/zeitwerk-2.7.5/lib/zeitwerk/core_ext/kernel.rb:34:in 'Kernel#require'
    /usr/lib64/obs-api/ruby/3.4.0/gems/zeitwerk-2.7.5/lib/zeitwerk/core_ext/kernel.rb:34:in 'Kernel#require'
    /usr/lib64/obs-api/ruby/3.4.0/gems/zeitwerk-2.7.5/lib/zeitwerk/core_ext/kernel.rb:34:in 'Kernel#require'
    ./spec/spec_helper.rb:15:in '<top (required)>'

--- src/api/config/application.rb
+++ src/api/config/application.rb
@@ -45,6 +45,7 @@ module OBSApi
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %w[lib]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
