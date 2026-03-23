f1b3bd7a7ad80040a63f67ec5a73c928748ad4cb
Eduardo Navarro <enavarro@suse.com>
2026-03-23 14:13:57 +0100

    Display the version in the notifications' list

--- src/api/app/models/concerns/notification_request.rb
+++ src/api/app/models/concerns/notification_request.rb
@@ -12,13 +12,17 @@ module NotificationRequest
   def request_source
     return '' if size_of_bs_request_actions > 1
 
-    [first_bs_request_action.source_project, first_bs_request_action.source_package].compact.join(' / ')
+    text = [first_bs_request_action.source_project, first_bs_request_action.source_package].compact.join(' / ')
+    text << version_suffix(bs_request.source_package_latest_local_version)
+    text
   end
 
   def request_target
     return first_bs_request_action.target_project if size_of_bs_request_actions > 1
 
-    [first_bs_request_action.target_project, first_bs_request_action.target_package].compact.join(' / ')
+    text = [first_bs_request_action.target_project, first_bs_request_action.target_package].compact.join(' / ')
+    text << version_suffix(bs_request.target_package_latest_local_version)
+    text
   end
 
   private
@@ -29,5 +33,11 @@ module NotificationRequest
 
   def size_of_bs_request_actions
     @size_of_bs_request_actions ||= bs_request.bs_request_actions.size
+  end
+
+  def version_suffix(version)
+    return '' unless Flipper.enabled?(:package_version_tracking, User.session) && version.present?
+
+    " (#{version})"
   end
 end
