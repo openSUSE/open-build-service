d0be5af18a96ee84b620c4cc48f69efc164398d1
Eduardo Navarro <enavarro@suse.com>
2026-04-21 16:40:15 +0200

    Add event for user or group token membership updates
    Co-authored-by: Saray Cabrera Padrón <scabrerapadron@suse.de>

---
+++ src/api/app/models/event/token_membership_update.rb
@@ -0,0 +1,26 @@
+module Event
+  class TokenMembershipUpdate < Base
+    self.description = 'Share or unshare token with user or group'
+
+    payload_keys :token_id, :user_login, :group_title, :who, :action
+  end
+end
+
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :bigint           not null, primary key
+#  eventtype   :string(255)      not null, indexed
+#  mails_sent  :boolean          default(FALSE), indexed
+#  payload     :text(16777215)
+#  undone_jobs :integer          default(0)
+#  created_at  :datetime         indexed
+#  updated_at  :datetime
+#
+# Indexes
+#
+#  index_events_on_created_at  (created_at)
+#  index_events_on_eventtype   (eventtype)
+#  index_events_on_mails_sent  (mails_sent)
+#
