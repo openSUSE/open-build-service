69a0722e173564033095b01a8228859c96403683
Felix Gerling <felix.gerling@suse.com>
2026-05-18 19:40:05 +0200

    Show group reviewers in a modal

--- src/api/spec/features/beta/webui/requests_spec.rb
+++ src/api/spec/features/beta/webui/requests_spec.rb
@@ -33,6 +33,39 @@ RSpec.describe 'Requests', :vcr do
     end
   end
 
+  context 'with a group review', :js do
+    let(:review_member) { create(:confirmed_user, login: 'review_member') }
+    let(:another_review_member) { create(:confirmed_user, login: 'another_review_member') }
+    let(:review_group) { create(:group, title: 'factory-staging', users: [review_member, another_review_member]) }
+    let(:bs_request) { create(:delete_bs_request, creator: submitter, target_project: target_project) }
+    let(:review) { create(:review, by_group: review_group.title, bs_request: bs_request) }
+    let(:modal_id) { "review-group-members-#{review.id}-modal" }
+
+    before do
+      review
+      login submitter
+      visit request_show_path(bs_request.number)
+    end
+
+    it 'opens the group members modal from the reviewers list' do
+      expect(page).to have_css("##{modal_id}", visible: :hidden)
+      within('#side-links') { click_link review_group.title }
+
+      within("##{modal_id}", visible: :visible) do
+        expect(page).to have_css('.modal-title', text: "Members of #{review_group.title}")
+      end
+    end
+
+    it 'lists the group members in the modal' do
+      within('#side-links') { click_link review_group.title }
+
+      within("##{modal_id}") do
+        expect(page).to have_link(review_member.login, href: user_path(review_member))
+        expect(page).to have_link(another_review_member.login, href: user_path(another_review_member))
+      end
+    end
+  end
+
   it 'has all the action types partial view files' do
     missing_files = required_files.reject { |file| File.exist?(file) }
 
