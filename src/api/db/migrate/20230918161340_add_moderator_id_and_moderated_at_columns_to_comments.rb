65469bc85527c19391857ee8183fec5b77092b5a
Dani Donisa <dani.doni@gmail.com>
2026-05-21 17:36:59 +0200

    Wrap add_foreign_key in safety_assured in AddModeratorIdAndModeratedAtColumnsToComments migration
    strong_migrations 2.8.0 now flags add_foreign_key as a dangerous operation.
    Wrap it in safety_assured with foreign_key_checks disabled since rows are
    valid in this migration context.

--- src/api/db/migrate/20230918161340_add_moderator_id_and_moderated_at_columns_to_comments.rb
+++ src/api/db/migrate/20230918161340_add_moderator_id_and_moderated_at_columns_to_comments.rb
@@ -3,6 +3,10 @@ class AddModeratorIdAndModeratedAtColumnsToComments < ActiveRecord::Migration[7.
     add_column :comments, :moderated_at, :datetime
 
     add_column :comments, :moderator_id, :integer
-    add_foreign_key :comments, :users, column: :moderator_id, name: 'moderated_comments_fk'
+    safety_assured do
+      execute 'SET SESSION foreign_key_checks = 0'
+      add_foreign_key :comments, :users, column: :moderator_id, name: 'moderated_comments_fk'
+      execute 'SET SESSION foreign_key_checks = 1'
+    end
   end
 end
