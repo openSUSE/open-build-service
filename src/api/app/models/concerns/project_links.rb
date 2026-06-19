98897c792e56e80a389c0e310cc714aa90083944
Lukas Krause <lkrause@suse.de>
2026-06-19 15:05:06 +0200

    Fix link_project_step squash

--- src/api/app/models/concerns/project_links.rb
+++ src/api/app/models/concerns/project_links.rb
@@ -30,14 +30,14 @@ module ProjectLinks
     end
   end
 
-  def add_project_link(project_name_to_link_against:)
+  def add_project_link(source_project_name:)
     # The position is handled automatically by acts_as_list on LinkedProject,
     # which appends new links to the bottom of the list.
-    if Project.remote_project?(project_name_to_link_against)
-      linking_to.find_or_create_by(linked_remote_project_name: project_name_to_link_against)
+    if Project.remote_project?(source_project_name)
+      linking_to.find_or_create_by(linked_remote_project_name: source_project_name)
     else
-      project_to_link_against = Project.find_by_name(project_name_to_link_against)
-      linking_to.find_or_create_by(linked_db_project: project_to_link_against)
+      source_project = Project.find_by!(name: source_project_name)
+      linking_to.find_or_create_by(linked_db_project: source_project)
     end
   end
 
@@ -45,7 +45,7 @@ module ProjectLinks
     if Project.remote_project?(linked_project_name)
       linking_to.destroy_by(linked_remote_project_name: linked_project_name)
     else
-      linked_project = Project.find_by_name(linked_project_name)
+      linked_project = Project.find_by!(name: linked_project_name)
       linking_to.destroy_by(linked_db_project: linked_project)
     end
   end
