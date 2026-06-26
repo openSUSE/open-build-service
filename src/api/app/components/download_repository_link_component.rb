960f8019bff5367c2f9e321459146cba6d7dec3e
Dushyant Acharya <dushyantacharya873@gmail.com>
2026-06-26 15:09:04 +0530

    Fix broken download repository link when no published URL exists

--- src/api/app/components/download_repository_link_component.rb
+++ src/api/app/components/download_repository_link_component.rb
@@ -1,14 +1,21 @@
 class DownloadRepositoryLinkComponent < ApplicationComponent
-  attr_reader :published_repository_exist, :download_area_url
+  attr_reader :download_area_url
 
   def initialize(project:, repository:, configuration:)
     super()
 
-    @published_repository_exist = false
     download_url = configuration['download_url']
     return unless download_url
 
-    @published_repository_exist = Backend::Api::Published.published_repository_exist?(project.to_s, repository.to_s)
-    @download_area_url = "#{download_url}/#{project.to_s.gsub(':', ':/')}/#{repository}"
+    @download_area_url = published_repository_url(project: project, repository: repository)
+  end
+
+  private
+
+  def published_repository_url(project:, repository:)
+    xml = Xmlhash.parse(Backend::Api::Published.download_url_for_repository(project.to_s, repository.to_s))
+    xml.elements('url').last.to_s.presence
+  rescue Backend::NotFoundError
+    nil
   end
 end
