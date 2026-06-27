a625960f6063b018a93f12c9696bf2090236dbfa
Dushyant Acharya <dushyantacharya873@gmail.com>
2026-06-27 23:50:02 +0530

    Fix download repository link component and tests to pass CI

--- src/api/spec/components/download_repository_link_component_spec.rb
+++ src/api/spec/components/download_repository_link_component_spec.rb
@@ -3,9 +3,23 @@ RSpec.describe DownloadRepositoryLinkComponent, type: :component do
   let(:repository) { create(:repository, project: project, name: 'images') }
   let(:configuration) { { 'download_url' => 'https://download.opensuse.org/repositories' } }
 
+  def mock_published_repository_exist(return_value: nil, raise_error: nil)
+    if raise_error
+      allow(Backend::Api::Published).to receive(:published_repository_exist?).and_raise(raise_error)
+    else
+      allow(Backend::Api::Published).to receive(:published_repository_exist?).and_return(return_value)
+    end
+  end
+
+  shared_examples 'hides the download link' do
+    it 'renders nothing and hides the link' do
+      expect(rendered_content).to have_no_link('Go to download repository')
+    end
+  end
+
   context 'when published artifacts exist for the repository' do
     before do
-      allow(Backend::Api::Published).to receive(:published_repository_exist?).with(project.to_s, repository.to_s).and_return(true)
+      mock_published_repository_exist(return_value: true)
 
       render_inline(described_class.new(project: project, repository: repository, configuration: configuration))
     end
@@ -13,29 +27,29 @@ RSpec.describe DownloadRepositoryLinkComponent, type: :component do
     it 'renders the download repository link' do
       expect(rendered_content).to have_link('Go to download repository', href: 'https://download.opensuse.org/repositories/home:/Admin/images')
     end
+
+    it 'calls the backend with the correct project and repository names' do
+      expect(Backend::Api::Published).to have_received(:published_repository_exist?).with('home:Admin', 'images')
+    end
   end
 
   context 'when no published artifacts exist for the repository' do
     before do
-      allow(Backend::Api::Published).to receive(:published_repository_exist?).with(project.to_s, repository.to_s).and_return(false)
+      mock_published_repository_exist(return_value: false)
 
       render_inline(described_class.new(project: project, repository: repository, configuration: configuration))
     end
 
-    it 'renders nothing and hides the link' do
-      expect(rendered_content).to have_no_link('Go to download repository')
-    end
+    include_examples 'hides the download link'
   end
 
   context 'when the published repository is missing' do
     before do
-      allow(Backend::Api::Published).to receive(:published_repository_exist?).with(project.to_s, repository.to_s).and_raise(Backend::NotFoundError)
+      mock_published_repository_exist(raise_error: Backend::NotFoundError)
 
       render_inline(described_class.new(project: project, repository: repository, configuration: configuration))
     end
 
-    it 'renders nothing and hides the link' do
-      expect(rendered_content).to have_no_link('Go to download repository')
-    end
+    include_examples 'hides the download link'
   end
 end
