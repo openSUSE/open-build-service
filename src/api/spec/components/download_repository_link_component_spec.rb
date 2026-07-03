a791b5d1b736abebb178b4480b827e485ea7ccab
Dushyant Acharya <dushyantacharya873@gmail.com>
2026-07-04 04:13:38 +0530

    Fix settings name collision in component spec

--- src/api/spec/components/download_repository_link_component_spec.rb
+++ src/api/spec/components/download_repository_link_component_spec.rb
@@ -1,13 +1,13 @@
 RSpec.describe DownloadRepositoryLinkComponent, type: :component do
   let(:project) { create(:project, name: 'home:Admin') }
   let(:repository) { create(:repository, project: project, name: 'images') }
-  let(:configuration) { { 'download_url' => 'https://download.opensuse.org' } }
+  let(:app_configuration) { { 'download_url' => 'https://download.opensuse.org' } }
 
   context 'when published artifacts exist for the repository' do
     before do
       allow(Backend::Api::Published).to receive(:published_repository_exist?).with(project.to_s, repository.to_s).and_return(true)
 
-      render_inline(described_class.new(project: project, repository: repository, configuration: configuration))
+      render_inline(described_class.new(project: project, repository: repository, configuration: app_configuration))
     end
 
     it 'renders the download repository link' do
@@ -19,7 +19,7 @@ RSpec.describe DownloadRepositoryLinkComponent, type: :component do
     before do
       allow(Backend::Api::Published).to receive(:published_repository_exist?).with(project.to_s, repository.to_s).and_return(false)
 
-      render_inline(described_class.new(project: project, repository: repository, configuration: configuration))
+      render_inline(described_class.new(project: project, repository: repository, configuration: app_configuration))
     end
 
     it 'renders the fallback text instead of a broken link' do
