94f1305efff5dae2fb87fad33c7036f98d64ba7f
Dushyant Acharya <dushyantacharya873@gmail.com>
2026-07-04 03:20:46 +0530

    Refine published repository link checks

--- src/api/app/components/download_repository_link_component.rb
+++ src/api/app/components/download_repository_link_component.rb
@@ -4,19 +4,17 @@ class DownloadRepositoryLinkComponent < ApplicationComponent
   def initialize(project:, repository:, configuration:)
     super()
 
-    @download_area_url = get_published_url(project.name, repository.name)
+    download_url = configuration['download_url']
+    return unless download_url
+
+    return unless published_repository?(project: project, repository: repository)
+
+    @download_area_url = "#{download_url}/#{project.to_s.gsub(/:/, ':/')}/#{repository}"
   end
 
   private
 
-  def get_published_url(project_name, repository_name)
-    xml_data = Backend::Api::Published.download_url_for_repository(project_name, repository_name)
-    return nil if xml_data.blank?
-
-    xml = Xmlhash.parse(xml_data)
-    url = xml.elements('url').last&.to_s
-    url.presence
-  rescue Backend::NotFoundError, Backend::Error
-    nil
+  def published_repository?(project:, repository:)
+    Backend::Api::Published.published_repository_exist?(project.to_s, repository.to_s)
   end
 end
--- src/api/spec/components/previews/download_repository_link_component_preview.rb
+++ src/api/spec/components/previews/download_repository_link_component_preview.rb
@@ -3,9 +3,9 @@ class DownloadRepositoryLinkComponentPreview < ViewComponent::Preview
   def preview
     view_component = DownloadRepositoryLinkComponent.new(project: Project.new(name: 'home:Admin'),
                                                          repository: Repository.new(name: 'openSUSE_Tumbleweed'),
-                                                         configuration: {})
-    # Bypass the backend call for preview purposes since the project and repository don't exist.
-    view_component.instance_variable_set(:@download_area_url, 'https://download.opensuse.org/repositories/home:/Admin/openSUSE_Tumbleweed')
+                                                         configuration: { 'download_url' => 'https://download.opensuse.org' })
+    # Set the generated URL explicitly because preview records do not exist in the published backend.
+    view_component.instance_variable_set(:@download_area_url, 'https://download.opensuse.org/home:/Admin/openSUSE_Tumbleweed')
 
     render(view_component)
   end
