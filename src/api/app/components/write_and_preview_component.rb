f2397dd8dc61703dda1d3a5b9324c747abe2da5f
Henne Vogelsang <hvogel@opensuse.org>
2026-07-07 16:16:01 +0200

    Rename bs_request argument to canned_response_object
    So not more people get tricked into believing this can only be a BsRequest...

--- src/api/app/components/write_and_preview_component.rb
+++ src/api/app/components/write_and_preview_component.rb
@@ -1,7 +1,7 @@
 class WriteAndPreviewComponent < ApplicationComponent
   attr_reader :form, :preview_message_url, :message_body_param, :text_area_attributes, :canned_responses_enabled, :bs_request
 
-  def initialize(form:, preview_message_url:, message_body_param:, text_area_attributes: {}, canned_responses_enabled: false, bs_request: nil)
+  def initialize(form:, preview_message_url:, message_body_param:, text_area_attributes: {}, canned_responses_enabled: false, canned_response_object: nil)
     super()
 
     @form = form
@@ -9,7 +9,7 @@ class WriteAndPreviewComponent < ApplicationComponent
     @message_body_param = message_body_param
     @text_area_attributes = text_area_attributes_defaults.merge(text_area_attributes)
     @canned_responses_enabled = canned_responses_enabled
-    @bs_request = bs_request
+    @canned_response_object = canned_response_object
   end
 
   private
