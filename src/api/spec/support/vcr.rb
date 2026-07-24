6e0a91ffaf9fe13f35baaf46aaf988c837d77801
Henne Vogelsang <hvogel@opensuse.org>
2026-07-24 12:55:11 +0200

    Change config.action_dispatch.show_exceptions to :none
    Change `config.action_dispatch.show_exceptions` to not swallow all
    exceptions during the request cycle.
    
    This also surfaced the need to enable `allow_playback_repeats` in VCR because we
    frequently do something like this in specs:
    
    ```
    visit /package/show/home:someproject/somepackage
    click_link('Remove Whatever')
    expect(page).to have_text("Removed Whatever")
    visit /package/show/home:someproject/somepackage
    expect(page).to have_no_link('Whatever')
    ```
    
    Where both visits fire the exact same backend requests but the first visit
    already played back the cassette. And by default VCR does not allow a single
    HTTP interaction to be played back multiple times

--- src/api/spec/support/vcr.rb
+++ src/api/spec/support/vcr.rb
@@ -4,7 +4,7 @@ require 'webmock/rspec'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
-  config.default_cassette_options = { record: :once }
+  config.default_cassette_options = { record: :once, allow_playback_repeats: true }
   config.allow_http_connections_when_no_cassette = true
   config.configure_rspec_metadata!
   # config.debug_logger = File.open(Rails.root.join('log', 'vcr.log'), 'w')
