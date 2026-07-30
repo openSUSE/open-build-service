0f942e96b8365bbe0e13aff4f0527ff90db80b6b
Dushyant Acharya <dushyantacharya873@gmail.com>
2026-07-31 02:47:39 +0530

    refactor: apply reviewer feedback to download repository link component
    - Inline Backend::Api::Published.published_repository_exist? call
    - Remove redundant component spec as backend behavior is already tested

--- src/api/app/components/download_repository_link_component.rb
+++ src/api/app/components/download_repository_link_component.rb
@@ -7,14 +7,8 @@ class DownloadRepositoryLinkComponent < ApplicationComponent
     download_url = configuration['download_url']
     return unless download_url
 
-    return unless published_repository?(project: project, repository: repository)
+    return unless Backend::Api::Published.published_repository_exist?(project.to_s, repository.to_s)
 
     @download_area_url = "#{download_url}/#{project.to_s.gsub(/:/, ':/')}/#{repository}"
-  end
-
-  private
-
-  def published_repository?(project:, repository:)
-    Backend::Api::Published.published_repository_exist?(project.to_s, repository.to_s)
   end
 end
