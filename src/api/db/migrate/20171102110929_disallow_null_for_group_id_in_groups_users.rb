0a523e6e27c5b6b0d7ea155677587e6580988a6d
Saray Cabrera Padrón <scabrerapadron@suse.de>
2026-07-30 16:40:18 +0200

    Adapt DisallowNullForGroupIdInGroupsUsers migration
    By using GroupsUser.where(group_id: nil).destroy_all we are
    instantiating each object before removing it and therefore running
    callbacks and validations.
    
    With some recent changes, we are validating the user_id of the User
    model. But, at this point in the migration sequence (2017), the
    biography column does not yet exist in the users table (added in 2020),
    so it validates a User instance and fails.
    
    Replacing destroy_all by raw SQL stops running callbacks
    but also instantiating the objects and validating.

--- src/api/db/migrate/20171102110929_disallow_null_for_group_id_in_groups_users.rb
+++ src/api/db/migrate/20171102110929_disallow_null_for_group_id_in_groups_users.rb
@@ -1,7 +1,7 @@
 class DisallowNullForGroupIdInGroupsUsers < ActiveRecord::Migration[5.1]
   def up
     # Remove all dangling groups users that got created since 18680d611347d312
-    GroupsUser.where(group_id: nil).destroy_all
+    safety_assured { execute 'DELETE FROM groups_users WHERE group_id IS NULL' }
     change_column :groups_users, :group_id, :integer, null: false, default: 0, before: :user_id
   end
 
