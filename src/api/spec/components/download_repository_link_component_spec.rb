4e147e25c5097e800489fc61ecaa179a0d683519
Dushyant Acharya <dushyantacharya873@gmail.com>
2026-08-01 22:09:21 +0530

    test: restore component spec to fix codecov failure
    The previous commit removed this spec based on reviewer feedback that it redundantly tested the backend API. However, doing so removed all test coverage for DownloadRepositoryLinkComponent, causing the `codecov/patch` check to fail since the component's internal logic was no longer executed in the test suite.

---
+++ src/api/spec/components/download_repository_link_component_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe DownloadRepositoryLinkComponent, type: :component do
+  let(:project) { create(:project, name: 'home:Admin') }
+  let(:repository) { create(:repository, project: project, name: 'images') }
+  let(:app_configuration) { { 'download_url' => 'https://download.opensuse.org' } }
+
+  context 'when published artifacts exist for the repository' do
+    before do
+      allow(Backend::Api::Published).to receive(:published_repository_exist?).with(project.to_s, repository.to_s).and_return(true)
+
+      render_inline(described_class.new(project: project, repository: repository, configuration: app_configuration))
+    end
+
+    it 'renders the download repository link' do
+      expect(rendered_content).to have_link('Go to download repository', href: 'https://download.opensuse.org/home:/Admin/images')
+    end
+  end
+
+  context 'when no published artifacts exist for the repository' do
+    before do
+      allow(Backend::Api::Published).to receive(:published_repository_exist?).with(project.to_s, repository.to_s).and_return(false)
+
+      render_inline(described_class.new(project: project, repository: repository, configuration: app_configuration))
+    end
+
+    it 'renders the fallback text instead of a broken link' do
+      expect(rendered_content).to have_text('There are no published packages')
+      expect(rendered_content).to have_no_link('Go to download repository')
+    end
+  end
+end
