085db4ad1cd543e1f11bbbfe9b3176b5752d45b0
Jay Michalska <jay.michalska@suse.com>
2026-08-12 15:22:38 +0200

    Adapt the specs for Rails 8.1
    submissions_spec.rb: Staging Project value wasn't persisted
    requests_spec.rb: Missing User which was required for recreating casettes
    others: `nil.to_query("key")` now returns `key`, so some of the query parameters had to be adjusted

--- src/api/spec/models/backend/file_spec.rb
+++ src/api/spec/models/backend/file_spec.rb
@@ -254,7 +254,7 @@ RSpec.describe Backend::File, :vcr do
 
     it { expect(Backend::File.query_from_list({})).to be_empty }
     it { expect(Backend::File.query_from_list(hash)).to eq('?a=1&b=2') }
-    it { expect(Backend::File.query_from_list(hash_with_nil_values)).to eq('?a=1&b=2&c=&d=6') }
+    it { expect(Backend::File.query_from_list(hash_with_nil_values)).to eq('?a=1&b=2&c&d=6') }
     it { expect(Backend::File.query_from_list(hash, key_list)).to eq('?a=1') }
   end
 end
