2f3208b90632cb8064b13523b42e6b3dea42a306
ncounter <dleidi@suse.com>
2026-08-14 15:02:50 +0200

    Use attr_reader instead of a dedicated method

--- src/api/app/models/workflow.rb
+++ src/api/app/models/workflow.rb
@@ -15,6 +15,8 @@ class Workflow # rubocop:disable Metrics/ClassLength
   SUPPORTED_FILTERS = %i[branches event labels].freeze
 
   attr_accessor :workflow_instructions, :token, :workflow_run, :workflow_version_number, :workflow_name
+  attr_reader :filters_matched
+  alias filters_matched? filters_matched
 
   def initialize(attributes = {})
     run_callbacks(:initialize) do
@@ -32,18 +34,13 @@ class Workflow # rubocop:disable Metrics/ClassLength
 
   def call
     run_callbacks(:call) do
-      @filters_matched = filters_match?
-      return unless @filters_matched
+      return unless (@filters_matched = filters_match?)
 
       steps.each do |step|
         # ArtifactsCollector can only be called if the step.call doesn't return nil because of a validation error
         step.call && Workflows::ArtifactsCollector.new(step: step, workflow_run_id: workflow_run.id).call
       end
     end
-  end
-
-  def filters_matched?
-    @filters_matched
   end
 
   def event_supports_branches_filter?
