4a113e3f83bfe1e46e901948a6d54786642e66dd
Henne Vogelsang <hvogel@opensuse.org>
2026-08-19 17:36:14 +0200

    Correct FactoryBot/AssociationStyle

--- src/api/spec/factories/status_message.rb
+++ src/api/spec/factories/status_message.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     message { Faker::Lorem.paragraph }
     severity { :green }
     communication_scope { 'all_users' }
-    association :creator, factory: :user
+    creator factory: %i[user]
 
     trait :admins_only do
       communication_scope { 'admin_users' }
