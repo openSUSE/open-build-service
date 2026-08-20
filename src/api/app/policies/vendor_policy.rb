629b851c9f57f0bb13489e45f913d3b674df0f3a
Rubhan Azeem <rubhanazeem@gmail.com>
2026-08-20 15:44:47 +0200

    Add views and controller for vendors
    Co-authored-by: Dario Leidi <dleidi@suse.com>
    Co-authored-by: Jay Michalska <jay.michalska@suse.com>
    Co-authored-by: Lukas Krause <lkrause@suse.de>

---
+++ src/api/app/policies/vendor_policy.rb
@@ -0,0 +1,23 @@
+class VendorPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    ProjectPolicy.new(user, record.project).update?
+  end
+
+  def new?
+    create?
+  end
+
+  def destroy?
+    create?
+  end
+
+  def update?
+    create?
+  end
+end
