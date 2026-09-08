748e255baa5ebe2aee198a31725d92bc121fdd61
Dushyant Acharya <dushyantacharya873@gmail.com>
2026-09-08 18:53:41 +0530

    Hide the download repository link when no published URL exists
    Fixes #19912
    
    Co-authored-by: Henne Vogelsang <hvogel@opensuse.org>

--- src/api/spec/components/previews/download_repository_link_component_preview.rb
+++ src/api/spec/components/previews/download_repository_link_component_preview.rb
@@ -1,11 +1,9 @@
 class DownloadRepositoryLinkComponentPreview < ViewComponent::Preview
   # Preview at http://HOST:PORT/rails/view_components/download_repository_link_component/preview
   def preview
-    view_component = DownloadRepositoryLinkComponent.new(project: Project.new(name: 'home:Admin'),
-                                                         repository: Repository.new(name: 'openSUSE_Tumbleweed'),
-                                                         configuration: { 'download_url' => 'https://download.opensuse.org' })
-    # Just to make sure that the link is displayed, since the repository won't be published if the project and repository don't exist.
-    view_component.instance_variable_set(:@published_repository_exist, true)
+    view_component = DownloadRepositoryLinkComponent.new(project: Project.new(name: 'home:Admin'), repository: Repository.new(name: 'openSUSE_Tumbleweed'))
+    # Set the generated URL explicitly because preview records do not exist in the published backend.
+    view_component.instance_variable_set(:@project_repository_download_url, 'https://download.opensuse.org/home:/Admin/openSUSE_Tumbleweed')
 
     render(view_component)
   end
