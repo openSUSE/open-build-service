deefa01a4f4aa77a91dcb4b0003eeaa9adc7449e
Rubhan Azeem <rubhanazeem@gmail.com>
2026-09-14 12:59:19 +0200

    Add helper for distro_release_fifecycle

--- src/api/app/models/distro_release_lifecycle.rb
+++ src/api/app/models/distro_release_lifecycle.rb
@@ -49,23 +49,6 @@ class DistroReleaseLifecycle < ApplicationRecord
     STATUS_LABELS.fetch(status, status.humanize)
   end
 
-  def status_class
-    case status
-    when 'general_availability'
-      'text-bg-success'
-    when 'maintenance', 'extended_support'
-      'text-bg-primary'
-    when 'development', 'alpha', 'beta', 'release_candidate'
-      'text-bg-info'
-    when 'deprecated'
-      'text-bg-warning'
-    when 'end_of_life'
-      'text-bg-danger'
-    else
-      'text-bg-secondary'
-    end
-  end
-
   #### Alias of methods
 end
 
