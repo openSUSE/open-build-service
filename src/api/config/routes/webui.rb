45e1bc225f5e063742898338a1a5e89c82664c57
Rubhan Azeem <rubhanazeem@gmail.com>
2026-09-15 14:24:07 +0200

    Move distro_releases routes inside project namespace

--- src/api/config/routes/webui.rb
+++ src/api/config/routes/webui.rb
@@ -312,14 +312,13 @@ resources :projects, only: [], param: :name do
 
   resource :vendor, controller: 'webui/vendors', constraints: cons
   resources :distros, controller: 'webui/distros', except: %i[index new], constraints: cons, param: :distro_name
+  resources :distros, only: [], param: :name, constraints: cons do
+    resources :releases, controller: 'webui/distro_releases', param: :distro_release_name, except: %i[index], constraints: cons
+  end
 end
 
 resources :distro_releases, only: [], param: :name, constraints: cons do
   resources :lifecycles, controller: 'webui/distro_release_lifecycles', param: :lifecycle_name, except: %i[index new show edit], constraints: cons
-end
-
-resources :distros, only: [], param: :name, constraints: cons do
-  resources :releases, controller: 'webui/distro_releases', param: :distro_release_name, except: %i[index], constraints: cons
 end
 
 get 'request/show/:number/build_results', to: redirect('/requests/%{number}/build_results'), constraints: cons
