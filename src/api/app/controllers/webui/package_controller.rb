3c8267c9ffe30bafdedd1c9b95f17883bec883b3
Dushyant Acharya <dushyantacharya873@gmail.com>
2026-09-23 07:39:45 +0530

    feat(webui): inherit develpackage maintainers on package users page (#15591)

--- src/api/app/controllers/webui/package_controller.rb
+++ src/api/app/controllers/webui/package_controller.rb
@@ -155,6 +155,12 @@ class Webui::PackageController < Webui::WebuiController
     @users = [@project.users, @package.users].flatten.uniq
     @groups = [@project.groups, @package.groups].flatten.uniq
     @roles = Role.local_roles
+    @devel_package = @package.develpackage
+    if @devel_package
+      @devel_users = [@devel_package.project.users, @devel_package.users].flatten.uniq
+      @devel_groups = [@devel_package.project.groups, @devel_package.groups].flatten.uniq
+    end
+
     @current_request_action = BsRequestAction.find(params[:request_action_id]) if User.session && params[:request_action_id]
   end
 
